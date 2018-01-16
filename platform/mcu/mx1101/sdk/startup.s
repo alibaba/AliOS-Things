855abe3087be44d2e2dcd5d17163c5fe08a1c372
yangsw <yangsw@mxchip.com>
2018-01-16 09:40:07 +0800

    Y-Loop work on MX1101

--- platform/mcu/mx1101/sdk/startup.s
+++ platform/mcu/mx1101/sdk/startup.s
@@ -31,7 +31,7 @@ defined in linker script */
   .weak  Reset_Handler
   .type  Reset_Handler, %function
 Reset_Handler:  
-  ldr   sp, =_estack    		 /* set stack pointer */
+  ldr   sp, =0x20020000    		 /* set stack pointer */
 
 /* Copy the data segment initializers from flash to SRAM */  
   movs  r1, #0
