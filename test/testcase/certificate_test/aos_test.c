aed635dbba259df847c3db276e2fe0685066f125
少舒 <shaofa.lsf@alibaba-inc.com>
2018-01-17 21:09:45 +0800

    check None device info

--- test/testcase/certificate_test/aos_test.c
+++ test/testcase/certificate_test/aos_test.c
@@ -1064,16 +1064,20 @@ SUITE(test_yloop) = {
 
 void test_certificate(void)
 {
-    dump_test_config();
-
-    ADD_SUITE(test_mm);
-    ADD_SUITE(test_task);
-    ADD_SUITE(test_task_comm);
-    ADD_SUITE(test_timer);
-    ADD_SUITE(test_kv);
-    ADD_SUITE(test_yloop);
-
-    cut_main(0, NULL);
+    if (0 == dump_test_config()) {
+        printf("test start!\r\n");
+        ADD_SUITE(test_mm);
+        ADD_SUITE(test_task);
+        ADD_SUITE(test_task_comm);
+        ADD_SUITE(test_timer);
+        ADD_SUITE(test_kv);
+        ADD_SUITE(test_yloop);
+        cut_main(0, NULL);
+        printf("test finished!\r\n");
+    }
+    else {
+        printf("test error!\r\n");
+    }
 }
 AOS_TESTCASE(test_certificate);
 
