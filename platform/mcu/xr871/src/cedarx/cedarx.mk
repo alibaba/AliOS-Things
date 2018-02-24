f941ec64c695312abaf716da06ff12135cb85f18
Yemao <leafy.myeh@allwinnertech.com>
2018-02-25 00:30:06 +0800

    cedarx: update cedarx.a and disable amr encoder/decoder

--- platform/mcu/xr871/src/cedarx/cedarx.mk
+++ platform/mcu/xr871/src/cedarx/cedarx.mk
@@ -4,73 +4,77 @@ NAME := cedarx
 
 $(NAME)_TYPE := kernel
 
-#$(NAME)_SOURCES := awrecorder/AudioEncodeComponent.c \
-#                   awrecorder/awencoder.c \
-#                   awrecorder/EncDataComponent.c \
-#                   base/AwMessageQueue.c \
-#                   base/AwPool.c \
-#                   base/CdxBitReader.c \
-#                   base/CdxBuffer.c \
-#                   base/CdxDebug.c \
-#                   base/CdxISOLang.c \
-#                   base/CdxKeyedVector.c \
-#                   base/CdxList.c \
-#                   base/CdxMessage.c \
-#                   base/CdxMeta.c \
-#                   base/CdxParseUrl.c \
-#                   base/CdxQueue.c \
-#                   base/CdxSocketUtil.c \
-#                   base/CdxStrUtil.c \
-#                   base/CdxSysinfo.c \
-#                   base/CdxTime.c \
-#                   base/cdx_log.c \
-#                   base/SmartDnsService.c \
-#                   Cdx2.0Plugin/adecoder.c \
-#                   Cdx2.0Plugin/src/AudioDec_Decode.c \
-#                   Cdx2.0Plugin/src/cedar_abs_packet_hdr.c \
-#                   Cdx2.0Plugin/src/rw_data_api.c \
-#                   CdxEncPlugin/aencoder.c \
-#                   CdxEncPlugin/pcm_enc.c \
-#                   muxer/aac/CdxAacMuxer.c \
-#                   muxer/base/CdxMuxer.c \
-#                   muxer/base/CdxTsemaphore.c \
-#                   muxer/RecoderCbWriter.c \
-#                   muxer/RecoderWriter.c \
-#                   os_glue/atomic.c \
-#                   os_glue/file.c \
-#                   os_glue/pthread.c \
-#                   os_glue/sleep.c \
-#                   parser/amr/CdxAmrParser.c \
-#                   parser/base/CdxParser.c \
-#                   parser/base/id3base/CdxMetaData.c \
-#                   parser/base/id3base/CdxUtfCode.c \
-#                   parser/base/id3base/Id3Base.c \
-#                   parser/base/id3base/StringContainer.c \
-#                   parser/id3v2/CdxId3v2Parser.c \
-#                   parser/mp3/CdxMp3Parser.c \
-#                   playback/audioDecComponent.c \
-#                   playback/audioRenderComponent.c \
-#                   playback/avtimer.c \
-#                   playback/baseComponent.c \
-#                   playback/bitrateEstimater.c \
-#                   playback/framerateEstimater.c \
-#                   playback/player.c \
-#                   playback/soundControl_null.c \
-#                   playback/soundControl_rtos.c \
-#                   playback/streamManager.c \
-#                   record/captureControl_null.c \
-#                   record/captureControl_rtos.c \
-#                   stream/base/CdxStream.c \
-#                   stream/file/CdxFileStream.c \
-#                   stream/flash/CdxFlashStream.c \
-#                   stream/http/CdxHttpField.c \
-#                   stream/http/CdxHttpStream.c \
-#                   stream/http/CdxHttpUtils.c \
-#                   stream/tcp/CdxTcpStream.c \
-#                   xplayer/cache.c \
-#                   xplayer/demuxComponent.c \
-#                   xplayer/xplayer.c \
-#                   xrecoder/xrecoder.c
+cedarx_with_pribuild_lib=1
+
+ifneq ($(cedarx_with_pribuild_lib),1)
+$(NAME)_SOURCES := awrecorder/AudioEncodeComponent.c \
+                   awrecorder/awencoder.c \
+                   awrecorder/EncDataComponent.c \
+                   base/AwMessageQueue.c \
+                   base/AwPool.c \
+                   base/CdxBitReader.c \
+                   base/CdxBuffer.c \
+                   base/CdxDebug.c \
+                   base/CdxISOLang.c \
+                   base/CdxKeyedVector.c \
+                   base/CdxList.c \
+                   base/CdxMessage.c \
+                   base/CdxMeta.c \
+                   base/CdxParseUrl.c \
+                   base/CdxQueue.c \
+                   base/CdxSocketUtil.c \
+                   base/CdxStrUtil.c \
+                   base/CdxSysinfo.c \
+                   base/CdxTime.c \
+                   base/cdx_log.c \
+                   base/SmartDnsService.c \
+                   Cdx2.0Plugin/adecoder.c \
+                   Cdx2.0Plugin/src/AudioDec_Decode.c \
+                   Cdx2.0Plugin/src/cedar_abs_packet_hdr.c \
+                   Cdx2.0Plugin/src/rw_data_api.c \
+                   CdxEncPlugin/aencoder.c \
+                   CdxEncPlugin/pcm_enc.c \
+                   muxer/aac/CdxAacMuxer.c \
+                   muxer/base/CdxMuxer.c \
+                   muxer/base/CdxTsemaphore.c \
+                   muxer/RecoderCbWriter.c \
+                   muxer/RecoderWriter.c \
+                   os_glue/atomic.c \
+                   os_glue/file.c \
+                   os_glue/pthread.c \
+                   os_glue/sleep.c \
+                   parser/amr/CdxAmrParser.c \
+                   parser/base/CdxParser.c \
+                   parser/base/id3base/CdxMetaData.c \
+                   parser/base/id3base/CdxUtfCode.c \
+                   parser/base/id3base/Id3Base.c \
+                   parser/base/id3base/StringContainer.c \
+                   parser/id3v2/CdxId3v2Parser.c \
+                   parser/mp3/CdxMp3Parser.c \
+                   playback/audioDecComponent.c \
+                   playback/audioRenderComponent.c \
+                   playback/avtimer.c \
+                   playback/baseComponent.c \
+                   playback/bitrateEstimater.c \
+                   playback/framerateEstimater.c \
+                   playback/player.c \
+                   playback/soundControl_null.c \
+                   playback/soundControl_rtos.c \
+                   playback/streamManager.c \
+                   record/captureControl_null.c \
+                   record/captureControl_rtos.c \
+                   stream/base/CdxStream.c \
+                   stream/file/CdxFileStream.c \
+                   stream/flash/CdxFlashStream.c \
+                   stream/http/CdxHttpField.c \
+                   stream/http/CdxHttpStream.c \
+                   stream/http/CdxHttpUtils.c \
+                   stream/tcp/CdxTcpStream.c \
+                   xplayer/cache.c \
+                   xplayer/demuxComponent.c \
+                   xplayer/xplayer.c \
+                   xrecoder/xrecoder.c
+endif
 
 $(NAME)_INCLUDES := ./
 $(NAME)_INCLUDES += awrecorder
@@ -119,4 +123,6 @@ $(NAME)_INCLUDES += xplayer/include
 $(NAME)_INCLUDES += xrecoder
 $(NAME)_INCLUDES += xrecoder/include
 
+ifeq ($(cedarx_with_pribuild_lib),1)
 $(NAME)_PREBUILT_LIBRARY := cedarx.a
+endif
