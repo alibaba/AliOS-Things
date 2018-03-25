ab92667443b3a6af85b06ab677ae8f018da5ae67
yuanlu <yuanlu.gl@alibaba-inc.com>
2018-03-25 15:03:44 +0800

    starterkit support mqtt without tls, and the image of alios things stand a little bit longer

--- example/starterkitgui/app/GUIDEMO_Intro.c
+++ example/starterkitgui/app/GUIDEMO_Intro.c
@@ -110,7 +110,7 @@ extern GUI_CONST_STORAGE GUI_BITMAP bmAliOS_Things_logo;
 void GUIDEMO_Intro(void) {
 	BSP_LCD_Clear(0xFFFF);
 	GUI_DrawBitmap(&bmAliOS_Things_logo, 60, 60);
-	GUIDEMO_Delay(krhino_ms_to_ticks(5000));
+	GUIDEMO_Delay(krhino_ms_to_ticks(25000));
 }
 
 /*************************** End of file ****************************/
