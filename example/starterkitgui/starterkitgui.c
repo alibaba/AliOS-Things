0b7cfa1ddb7498f884c98adb6bf2a70b700498a4
Junjie Cai <junjiec@gmail.com>
2018-03-25 17:05:06 +0800

    [starterkitgui] put gui in a standalone task

--- example/starterkitgui/starterkitgui.c
+++ example/starterkitgui/starterkitgui.c
@@ -8,15 +8,21 @@ extern void GUIDEMO_Main(void);
 
 static void app_delayed_action(void *arg)
 {
-	GUIDEMO_Main();
     LOG("helloworld %s:%d %s\r\n", __func__, __LINE__, aos_task_name());
     aos_post_delayed_action(5000, app_delayed_action, NULL);
+}
+
+static void gui_loop(void *arg)
+{
+    GUIDEMO_Main();
 }
 
 int application_start(int argc, char *argv[])
 {
     aos_post_delayed_action(1000, app_delayed_action, NULL);
     BSP_GUI_init();
+
+    aos_task_new("gui", gui_loop, 0, 4096);
     aos_loop_run();
 
     return 0;
