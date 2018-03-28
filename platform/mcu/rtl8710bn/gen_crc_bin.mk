fec045a9b6d1a66367af07e47a4bf28e1511274f
FireWinter <cwhaiyi521@163.com>
2018-03-28 09:15:49 +0800

    supporting linux platform to generate bin file

--- platform/mcu/rtl8710bn/gen_crc_bin.mk
+++ platform/mcu/rtl8710bn/gen_crc_bin.mk
@@ -1,4 +1,9 @@
+ifneq ($(findstring $(HOST_OS),Win32 Win64),)
 AMEBAZ_DIR = $(SOURCE_ROOT)/platform/mcu/$(HOST_MCU_FAMILY)
+else 
+TOP_PATH = $(shell pwd)
+AMEBAZ_DIR = $(TOP_PATH)/platform/mcu/$(HOST_MCU_FAMILY)
+endif
 #AMEBAZ_DIR = C:/raul/code/AliOS/AliOS-Things/platform/mcu/rtl8710bn
 #AMEBAZ_DIR = C:\raul\code\AliOS\AliOS-Things\platform\mcu\rtl8710bn
 AMEBAZ_TOOLDIR	= $(AMEBAZ_DIR)/tools
@@ -16,7 +21,12 @@ EXTRA_POST_BUILD_TARGETS += gen_crc_bin
 #$(eval OUT_MSG := $(shell cmd /c %cd%\platform\mcu\$(HOST_MCU_FAMILY)\script\postbuild_img2.bat $(ota_offset) %cd% $(APP_FULL) $(PLATFORM)))	
 #$(QUIET) $(ECHO) $(HOST_MCU_FAMILY)
 
-gen_crc_bin:	
-	$(eval OUT_MSG := $(shell cmd /c %cd%\platform\mcu\$(HOST_MCU_FAMILY)\script\postbuild_img2.bat $(ota_offset) %cd% $(APP_FULL) $(PLATFORM)))	
-	
+gen_crc_bin:
+ifneq ($(findstring $(HOST_OS),Win32 Win64),)
+	@echo $(HOST_OS) $(ota_offset) $(BIN_DIR) $(APP_FULL) $(PLATFORM)
+	$(eval OUT_MSG := $(shell cmd /c %cd%\platform\mcu\$(HOST_MCU_FAMILY)\script\postbuild_img2.bat $(ota_offset) %cd% $(APP_FULL) $(PLATFORM)))
+else
+	@echo $(HOST_OS) $(ota_idx) $(TOP_PATH) $(APP_FULL) $(PLATFORM)
+	$(eval OUT_MSG := $(shell sh $(TOP_PATH)/platform/mcu/$(HOST_MCU_FAMILY)/script/manipulate_image.sh $(ota_idx) $(TOP_PATH) $(APP_FULL) $(PLATFORM)))
+endif
 	
