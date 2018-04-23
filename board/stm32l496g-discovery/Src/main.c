dee079f35e4f0bc43a237c126c972d824e7b6d38
junjiecatgmail <13916949192@163.com>
2018-04-23 16:02:39 +0800

    Merge branch 'dev_496discovery' into 'master'
    fix 496 discovery compile problem
    
    
    
    See merge request !3129

--- board/stm32l496g-discovery/Src/main.c
+++ board/stm32l496g-discovery/Src/main.c
@@ -201,6 +201,7 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_9;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_MSI;
+  RCC_OscInitStruct.PLL.PLLM = 5;
   RCC_OscInitStruct.PLL.PLLN = 71;
   RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
   RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
