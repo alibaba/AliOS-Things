a521de2b1fd845cdb8e60ab8f0ad4a2d256de97d
Li Cheng <lc122798@alibaba-inc.com>
2018-04-25 22:00:23 +0800

    code synchronization at 2018-04-25@22:00

--- example/lorawan/linkwan/linkwan.mk
+++ example/lorawan/linkwan/linkwan.mk
@@ -1,14 +1,17 @@
-NAME := lorawanapp
+NAME := linkwan
 
 GLOBAL_INCLUDES +=  .            
 
 GLOBAL_DEFINES          += USE_FULL_LL_DRIVER
 GLOBAL_DEFINES          += USE_B_EML3047
-GLOBAL_DEFINES          += REGION_CN470
+GLOBAL_DEFINES          += REGION_CN470A
 
+linkwan := 1
+ifeq ($(linkwantest), 1)
+GLOBAL_DEFINES += LOW_POWER_DISABLE
+endif
 
-$(NAME)_SOURCES := lorawanapp.c              \
-				     
+$(NAME)_SOURCES := linkwan.c              \
 
 $(NAME)_COMPONENTS := protocols.lorawan  \
 
