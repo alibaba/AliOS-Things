7c59cb95eaed2fe7a8a245d1e2858893c8893680
junlong.zhou <junlong.zhou@bekencorp.com>
2018-04-27 17:06:04 +0800

    Add bk7231 to alios.

---
+++ board/bk7231devkitc/board.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#define HARDWARE_REVISION   "V1.0"
+#define MODEL               "BK7231"
+
+#ifdef BOOTLOADER
+#define STDIO_UART 1
+#define STDIO_UART_BUADRATE 115200
+#else
+#define STDIO_UART 1
+#define STDIO_UART_BUADRATE 115200
+#endif
