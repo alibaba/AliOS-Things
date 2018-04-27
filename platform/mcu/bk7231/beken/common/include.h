d17fdffdc4b04d654d59c8d4cbc632e66323af21
junlong.zhou <junlong.zhou@bekencorp.com>
2018-04-27 17:06:04 +0800

    Add bk7231 to alios.

---
+++ platform/mcu/bk7231/beken/common/include.h
@@ -0,0 +1,9 @@
+#ifndef _INCLUDES_H_
+#define _INCLUDES_H_
+
+#include "sys_config.h"
+#include "typedef.h"
+#include "generic.h"
+#include "compiler.h"
+
+#endif // _INCLUDES_H_ 
---
+++ platform/mcu/bk7231/beken/driver/include/driver_pub.h
@@ -0,0 +1,8 @@
+#ifndef _DRIVER_PUB_H_
+#define _DRIVER_PUB_H_
+
+extern UINT32 driver_init(void);
+
+#endif // _DRIVER_PUB_H_
+// eof
+
