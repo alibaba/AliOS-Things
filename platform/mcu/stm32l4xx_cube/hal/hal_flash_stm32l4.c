cf766dc649a9bdbaca294e13ae1117da3b80e76f
Leon <msls@qq.com>
2018-05-25 17:00:00 +0800

    add supported for developerkit linkkit example

--- platform/mcu/stm32l4xx_cube/hal/hal_flash_stm32l4.c
+++ platform/mcu/stm32l4xx_cube/hal/hal_flash_stm32l4.c
@@ -78,9 +78,9 @@ int FLASH_get_pageInBank(uint32_t addr)
   * @retval  0:  Success.
             -1:  Failure.
   */
-int FLASH_unlock_erase(uint32_t address, uint32_t len_bytes)
+int FLASH_erase_at(uint32_t address, uint32_t len_bytes)
 {
-    int rc = -1;
+    int ret = -1;
     uint32_t PageError = 0;
     FLASH_EraseInitTypeDef EraseInit;
 
@@ -92,21 +92,21 @@ int FLASH_unlock_erase(uint32_t address, uint32_t len_bytes)
     EraseInit.TypeErase = FLASH_TYPEERASE_PAGES;
 
     EraseInit.Banks = FLASH_get_bank(address);
-    if (EraseInit.Banks != FLASH_get_bank(address + len_bytes)) {
+    if (EraseInit.Banks != FLASH_get_bank(address + len_bytes - 1)) {
         printf("Error: Cannot erase across FLASH banks.\n");
     } else {
         EraseInit.Page = FLASH_get_pageInBank(address);
         EraseInit.NbPages = FLASH_get_pageInBank(address + len_bytes - 1) - EraseInit.Page + 1;
 
-        HAL_FLASH_Unlock();
-
-        if (HAL_FLASHEx_Erase(&EraseInit, &PageError) == HAL_OK) {
-            rc = 0;
+		ret = HAL_FLASHEx_Erase(&EraseInit, &PageError);
+        if (ret == HAL_OK) {
+            ret = 0;
         } else {
             printf("Error erasing at 0x%08lx\n", address);
         }
     }
-    return rc;
+
+    return ret;
 }
 
 /**
@@ -121,6 +121,7 @@ int FLASH_write_at(uint32_t address, uint64_t *pData, uint32_t len_bytes)
 {
     int i;
     int ret = -1;
+
 #ifndef CODE_UNDER_FIREWALL
     /* irq already mask under firewall */
     __disable_irq();
@@ -183,27 +184,28 @@ int FLASH_update(uint32_t dst_addr, const void *data, uint32_t size)
     int ret = 0;
     int remaining = size;
     uint8_t *src_addr = (uint8_t *) data;
-
+    uint32_t fl_addr = 0;
+    int fl_offset = 0;
+    int len = 0;
     uint64_t *page_cache = NULL;
+
     page_cache = (uint64_t *)krhino_mm_alloc(FLASH_PAGE_SIZE);
     if (page_cache == NULL) {
         return -1;
     }
     memset(page_cache, 0, FLASH_PAGE_SIZE);
 
-    __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
-
     do {
-        uint32_t fl_addr = ROUND_DOWN(dst_addr, FLASH_PAGE_SIZE);
-        int fl_offset = dst_addr - fl_addr;
-        int len = MIN(FLASH_PAGE_SIZE - fl_offset, size);
+        fl_addr = ROUND_DOWN(dst_addr, FLASH_PAGE_SIZE);
+        fl_offset = dst_addr - fl_addr;
+        len = MIN(FLASH_PAGE_SIZE - fl_offset, remaining);
 
         /* Load from the flash into the cache */
         memcpy(page_cache, (void *) fl_addr, FLASH_PAGE_SIZE);
         /* Update the cache from the source */
         memcpy((uint8_t *)page_cache + fl_offset, src_addr, len);
         /* Erase the page, and write the cache */
-        ret = FLASH_unlock_erase(fl_addr, FLASH_PAGE_SIZE);
+        ret = FLASH_erase_at(fl_addr, FLASH_PAGE_SIZE);
         if (ret != 0) {
             printf("Error erasing at 0x%08lx\n", fl_addr);
         } else {
@@ -233,18 +235,32 @@ hal_logic_partition_t *hal_flash_get_info(hal_partition_t pno)
 
 int32_t hal_flash_write(hal_partition_t pno, uint32_t *poff, const void *buf , uint32_t buf_size)
 {
+    int ret = 0;
     uint32_t start_addr;
     hal_logic_partition_t *partition_info;
     hal_partition_t real_pno;
 
-    real_pno = pno;
+    ret = HAL_FLASH_Unlock();
+    if (ret != 0) {
+        printf("HAL_FLASH_Unlock return %d\n", ret);
+        return -1;
+    }
+    __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
 
+    real_pno = pno;
     partition_info = hal_flash_get_info( real_pno );
     start_addr = partition_info->partition_start_addr + *poff;
     if (0 != FLASH_update(start_addr, buf, buf_size)) {
         printf("FLASH_update failed!\n");
     }
     *poff += buf_size;
+
+    ret = HAL_FLASH_Lock();
+    if (ret != 0) {
+        printf("HAL_FLASH_Lock return %d\n", ret);
+        return -1;
+    }
+
     return 0;
 }
 
@@ -271,20 +287,39 @@ int32_t hal_flash_read(hal_partition_t pno, uint32_t *poff, void *buf, uint32_t 
 int32_t hal_flash_erase(hal_partition_t pno, uint32_t off_set,
                         uint32_t size)
 {
+    int ret = 0;
     uint32_t start_addr;
+    uint32_t erase_size;
     hal_logic_partition_t *partition_info;
     hal_partition_t real_pno;
 
-    real_pno = pno;
+    ret = HAL_FLASH_Unlock();
+    if (ret != 0) {
+        printf("HAL_FLASH_Unlock return %d\n", ret);
+        return -1;
+    }
+    __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
 
+    real_pno = pno;
     partition_info = hal_flash_get_info( real_pno );
     if (size + off_set > partition_info->partition_length) {
         return -1;
     }
 
     start_addr = ROUND_DOWN((partition_info->partition_start_addr + off_set), FLASH_PAGE_SIZE);
+    erase_size = partition_info->partition_start_addr + off_set - start_addr + size;
 
-    FLASH_unlock_erase(start_addr, size);
+    ret = FLASH_erase_at(start_addr, erase_size);
+    if (ret != 0) {
+        printf("FLASH_erase_at return failed\n");
+    }
+
+    ret = HAL_FLASH_Lock();
+    if (ret != 0) {
+        printf("HAL_FLASH_Lock return %d\n", ret);
+        return -1;
+    }
+
     return 0;
 }
 
