e206139e7b6ea2c4c49dee9ca66db9ba4c1e3d3a
minghung.chou <minghung.chou@icomm-semi.com>
2018-05-28 15:34:32 +0800

    turn mm_info on.

--- platform/mcu/sv6266/port/k_types.h
+++ platform/mcu/sv6266/port/k_types.h
@@ -12,6 +12,8 @@
 #define RHINO_MM_FRAG_ALLOCATED      0xabcddcabu     /* 32 bit value, if 64 bit system, you need change it to 64 bit */
 #define RHINO_MM_FRAG_FREE           0xfefdecdbu     /* 32 bit value, if 64 bit system, you need change it to 64 bit */
 #define RHINO_INLINE                 static __inline /* inline keyword, it may change under different compilers */
+#define RHINO_MM_FREE_DYE            0xABABABAB
+#define RHINO_MM_CORRUPT_DYE         0xFEFEFEFE
 
 typedef char     name_t;
 typedef uint32_t sem_count_t;
