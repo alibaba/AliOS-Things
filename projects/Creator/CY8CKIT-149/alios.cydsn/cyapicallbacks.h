3b018ceaff57bd1bffff84ae1929d37baa786109
cutworth <cutworth@163.com>
2018-05-29 19:11:26 +0800

    Add PSoC4A-S3 support

---
+++ projects/Creator/CY8CKIT-149/alios.cydsn/cyapicallbacks.h
@@ -0,0 +1,20 @@
+/* ========================================
+ *
+ * Copyright YOUR COMPANY, THE YEAR
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF your company.
+ *
+ * ========================================
+*/
+#ifndef CYAPICALLBACKS_H
+#define CYAPICALLBACKS_H
+    
+
+    /*Define your macro callbacks here */
+    /*For more information, refer to the Writing Code topic in the PSoC Creator Help.*/
+    
+#endif /* CYAPICALLBACKS_H */   
+/* [] */
