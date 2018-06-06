a65dcc2ba3352696ef28906bb250ed268bac4a88
Junjie Cai <siemn.cjj@alibaba-inc.com>
2018-06-06 17:59:31 +0800

    [autobuild] error fix

--- board/esp32devkitc/ucube.py
+++ board/esp32devkitc/ucube.py
@@ -6,5 +6,5 @@ if aos_global_config.get('hci_h4', 0):
     component.add_global_macros('CONFIG_BLE_HCI_H4_UART_PORT=1')
 
 
-supported_targets="alinkapp helloworld bluetooth.bleadv bluetooth.bleperipheral networkapp bluetooth.aisilopapp mqttapp acapp tinyengine_app"
+supported_targets="helloworld bluetooth.bleadv bluetooth.bleperipheral networkapp bluetooth.aisilopapp mqttapp acapp tinyengine_app"
 platform_options="wifi=1"
