df33c994265761fa780121b2e0ee5a76c31f956a
Junjie Cai <siemn.cjj@alibaba-inc.com>
2018-06-06 17:59:31 +0800

    [autobuild] error fix

--- board/lpcxpresso54102/ucube.py
+++ board/lpcxpresso54102/ucube.py
@@ -8,4 +8,4 @@ component = aos_board_component('board_lpcxpresso54102', 'lpc54102', src)
 component.add_global_macros('CPU_LPC54102J512BD64_cm4', '__USE_CMSIS', '__MULTICORE_MASTER', '__NEWLIB__')
 aos_global_config.arch = 'Cortex-M4'
 
-supported_targets="helloworld alinkapp mqttapp tls networkapp"
+supported_targets="helloworld mqttapp tls networkapp"
