37126a7b6f6e101ae6564b39238f6c41337f6d5e
xiehongjiang <xiehongjiang@notioni.com>
2018-06-22 16:34:55 +0800

    add camera code and create cameraappgui

--- board/developerkit/camera_hal/camera_demo.c
+++ board/developerkit/camera_hal/camera_demo.c
@@ -3,17 +3,13 @@
 #include <stdlib.h>
 #include "stm32l4xx_hal.h"
 #include "gc0329.h"
-
-#define ST7789_LCD_PIXEL_WIDTH ((uint16_t)240)
-#define ST7789_LCD_PIXEL_HEIGHT ((uint16_t)240)
-
+#include "camera_demo.h"
+ 
 CAMERA_DrvTypeDef *camera_drv;
 DCMI_HandleTypeDef *phdcmi;
-uint8_t camera_dis_on = 0;
-uint16_t pBuffer[ST7789_LCD_PIXEL_WIDTH * ST7789_LCD_PIXEL_HEIGHT];
 extern void camera_dispaly(uint16_t *data, uint32_t pixel_num);
-extern void BSP_LCD_Clear(uint16_t Color);
-
+uint16_t *camera_buff = NULL;
+uint8_t camera_dis_on = 0;
 /**
 * @brief This function handles DMA2 channel6 global interrupt.
 */
@@ -34,6 +30,11 @@ void DCMI_IRQHandler(void)
   krhino_intrpt_exit();
 }
 
+void enable_camera_display(uint8_t on)
+{
+	camera_dis_on = on;
+}
+
 void CAMERA_Init(uint32_t Resolution)
 {
 	camera_drv = &gc0329_drv;
@@ -51,23 +52,26 @@ void CAMERA_Init(uint32_t Resolution)
       HAL_DCMI_ConfigCROP(phdcmi,
                           150,                 /* Crop in the middle of the VGA picture */
                           120,                 /* Same height (same number of lines: no need to crop vertically) */
-                          (240 * 2) - 1,     /* 2 pixels clock needed to capture one pixel */
-                          (240 * 1) - 1);    /* All 240 lines are captured */
+                          (ST7789_WIDTH * 2) - 1,     /* 2 pixels clock needed to capture one pixel */
+                          (ST7789_HEIGHT * 1) - 1);    /* All 240 lines are captured */
       HAL_DCMI_EnableCROP(phdcmi);
     }
 }
 
-void CameraDEMO_Main(void)
+void CameraDEMO_Init(uint16_t *buff, uint32_t size)
 {
 	uint8_t  sensor_id;
 	HAL_StatusTypeDef hal_status = HAL_OK;
-	
-	printf("CameraDEMO_Main\n");
+	if(!buff || size < 2 * ST7789_WIDTH* ST7789_HEIGHT)
+		return;
+	camera_buff = buff;
+	memset(camera_buff, 0x0, (ST7789_WIDTH* ST7789_HEIGHT) * 2);
+	camera_dispaly(camera_buff, (ST7789_WIDTH* ST7789_HEIGHT));
+	//printf("CameraDEMO_Main\n");
 	gc0329_power_onoff(1);
 	sensor_id = gc0329_ReadID();
 	printf("sensor_id = 0x%x\n", sensor_id);
 	//gc0329_power_onoff(0);
-	
 	if(sensor_id == GC0329_ID){
 		CAMERA_Init(CAMERA_R640x480);
 
@@ -84,15 +88,18 @@ void CameraDEMO_Main(void)
 		     with words alignment, the last parameter of HAL_DCMI_Start_DMA is set to:
 		      (ST7789H2_LCD_PIXEL_WIDTH*ST7789H2_LCD_PIXEL_HEIGHT)/2, that is 240 * 240 / 2
 		   */   
-		  hal_status = HAL_DCMI_Start_DMA(phdcmi, DCMI_MODE_CONTINUOUS,  (uint32_t)pBuffer , (ST7789_LCD_PIXEL_WIDTH* ST7789_LCD_PIXEL_HEIGHT)/2 );
+		  hal_status = HAL_DCMI_Start_DMA(phdcmi, DCMI_MODE_CONTINUOUS,  (uint32_t)camera_buff , (ST7789_WIDTH* ST7789_HEIGHT)/2 );
 		 // OnError_Handler(hal_status != HAL_OK); 
 	}	
 }
 
 void GC0329_CAMERA_FrameEventCallback(void)
 {
-	if(camera_dis_on)
-		camera_dispaly(pBuffer, (ST7789_LCD_PIXEL_WIDTH* ST7789_LCD_PIXEL_HEIGHT));
+	if(camera_dis_on){
+		HAL_DCMI_Suspend(phdcmi);
+		camera_dispaly(camera_buff, (ST7789_WIDTH* ST7789_HEIGHT));
+		HAL_DCMI_Resume(phdcmi);
+	}
 }
 
 /**
@@ -104,5 +111,4 @@ void HAL_DCMI_FrameEventCallback(DCMI_HandleTypeDef *hdcmi)
 {
   GC0329_CAMERA_FrameEventCallback();
 }
-
 
