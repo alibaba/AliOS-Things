98c201ff9a1e9ba8a632522e1ad658b4c3a5fda7
Bob <bob.jb@alibaba-inc.com>
2018-06-26 11:15:59 +0800

    [QR]Put camera to Y-only capture mode, and reuse the buffer for both capturing and QR scanning, reduce the memory usage about 112KB.

--- example/qr/qr.mk
+++ example/qr/qr.mk
@@ -2,7 +2,7 @@ NAME := qr
 
 $(NAME)_SOURCES := qr.c decode.c identify.c quirc.c version_db.c
 
-GLOBAL_DEFINES += AOS_NO_WIFI DK_CAMERA_SNAPSHOP
+GLOBAL_DEFINES += AOS_NO_WIFI DK_CAMERA_SNAPSHOP CAMERA_GRAY_MIRROR
 
 $(NAME)_COMPONENTS := yloop cli board.developerkit.camera_hal
 
