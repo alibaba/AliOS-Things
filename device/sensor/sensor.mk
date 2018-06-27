0f61acd9a0acc390b91c4b7af211517eaeec1d3d
wangyuchang <wangyuchang777@sina.com>
2018-06-27 11:16:04 +0800

    add driver for sensor

--- device/sensor/sensor.mk
+++ device/sensor/sensor.mk
@@ -10,7 +10,12 @@ $(NAME)_SOURCES += \
         drv/drv_baro_bosch_bmp280.c \
         drv/drv_acc_gyro_st_lsm6dsl.c\
         drv/drv_baro_st_lps22hb.c \
+        drv/drv_acc_mir3_da213B.c \
+        drv/drv_acc_mir3_da215.c \
         drv/drv_acc_mir3_da217.c \
+        drv/drv_acc_mir3_da270.c \
+        drv/drv_acc_mir3_da312B.c \
+        drv/drv_acc_mir3_da380B.c \
         drv/drv_als_ps_liteon_ltr553.c \
         drv/drv_temp_humi_sensirion_shtc1.c \
         drv/drv_temp_humi_st_hts221.c \
@@ -30,8 +35,13 @@ GLOBAL_DEFINES      += AOS_SENSOR
 #GLOBAL_DEFINES      += AOS_SENSOR_ACC_SUPPORT_STEP
 #GLOBAL_DEFINES      += AOS_SENSOR_MAG_ST_LIS3MDL  
 
-
+#GLOBAL_DEFINES       += AOS_SENSOR_ACC_MIR3_DA213B
+#GLOBAL_DEFINES       += AOS_SENSOR_ACC_MIR3_DA215
 #GLOBAL_DEFINES       += AOS_SENSOR_ACC_MIR3_DA217
+#GLOBAL_DEFINES       += AOS_SENSOR_ACC_MIR3_DA270
+#GLOBAL_DEFINES       += AOS_SENSOR_ACC_MIR3_DA312B
+#GLOBAL_DEFINES       += AOS_SENSOR_ACC_MIR3_DA380B
+
 #GLOBAL_DEFINES       += AOS_SENSOR_ALS_LITEON_LTR553
 #GLOBAL_DEFINES       += AOS_SENSOR_PS_LITEON_LTR553
 #GLOBAL_DEFINES      += AOS_SENSOR_TEMP_SENSIRION_SHTC1
