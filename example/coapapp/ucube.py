b8e4098bf0ab115d83b3288ba0ff9850de338a34
zhangtian <dongren.zt@alibaba-inc.com>
2018-07-09 15:45:08 +0800

    coapapp:use new component iotx-sdk

--- example/coapapp/ucube.py
+++ example/coapapp/ucube.py
@@ -1,7 +1,32 @@
-src     = Split('''
-        coapapp.c
+src =Split(''' 
+    app_entry.c
+    coap-example.c
 ''')
 
-component = aos_component('coapapp', src)
-component.add_comp_deps('kernel/yloop', 'tools/cli','connectivity/coap','netmgr')
-component.add_global_macros('AOS_NO_WIFI')
+component =aos_component('coapapp', src)
+
+dependencis =Split(''' 
+    framework/protocol/iotx-rhino/iotx-sdk-c
+    framework/protocol/iotx-rhino/hal
+    framework/netmgr
+    framework/common
+    utility/cjson
+    tools/cli
+''')
+for i in dependencis:
+    component.add_comp_deps(i)
+
+global_macros =Split(''' 
+    MQTT_DIRECT
+    ALIOT_DEBUG
+    IOTX_DEBUG
+    USE_LPTHREAD
+    CONFIG_AOS_CLI
+''')
+for i in global_macros:
+    component.add_global_macros(i)
+
+if aos_global_config.get('LWIP') == 1:
+    component.add_comp_deps("kernel/protocols/net")
+    aos_global_config.set('no_with_lwip', 0)
+
