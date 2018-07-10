1b0c96ec6db12e11ebe74992af0da4a3f2ca9ab2
xuwenjie <291214203@qq.com>
2018-07-10 16:00:41 +0800

    add new component makefile

---
+++ example/mqttapp/mqttapp.mk
@@ -0,0 +1,21 @@
+NAME := mqttapp
+$(NAME)_SOURCES := app_entry.c \
+    			mqtt-example.c
+
+$(NAME)_COMPONENTS += framework/protocol/iotx-rhino/iotx-sdk-c \
+    				framework/protocol/iotx-rhino/hal \
+					framework/netmgr \
+					framework/common \
+					utility/cjson \
+					tools/cli
+
+GLOBAL_DEFINES += MQTT_DIRECT \
+				ALIOT_DEBUG \
+				IOTX_DEBUG \
+				USE_LPTHREAD \
+				CONFIG_AOS_CLI
+
+ifeq ($(LWIP),1)
+$(NAME)_COMPONENTS  += protocols.net
+no_with_lwip := 0
+endif
