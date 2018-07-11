131ea22e69b40e767a14c68ceefc62289818dc39
zhangtian <dongren.zt@alibaba-inc.com>
2018-07-11 10:52:32 +0800

    mk3060:adjust partitions size

--- board/mk3060/board.c
+++ board/mk3060/board.c
@@ -38,7 +38,7 @@ const hal_logic_partition_t hal_partitions[] =
 	    .partition_owner            = HAL_FLASH_EMBEDDED,
 	    .partition_description      = "Application",
 	    .partition_start_addr       = 0x13000,
-	    .partition_length           = 0x8E000, //568k bytes
+	    .partition_length           = 0xED000, //948k bytes
 	    .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
 	},
     [HAL_PARTITION_OTA_TEMP] =
@@ -46,7 +46,7 @@ const hal_logic_partition_t hal_partitions[] =
         .partition_owner           = HAL_FLASH_EMBEDDED,
         .partition_description     = "OTA Storage",
         .partition_start_addr      = 0xB7200,
-        .partition_length          = 0x8E000, //568k bytes
+        .partition_length          = 0xA6000, //664k bytes
         .partition_options         = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
     [HAL_PARTITION_PARAMETER_3] =
