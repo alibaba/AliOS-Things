3d32756d6719312ff510e311548ab6e5b03d0b8c
xuwenjie <291214203@qq.com>
2018-07-11 20:59:08 +0800

    auto checkout iotx sdk repo while make

---
+++ framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
@@ -0,0 +1,10 @@
+NAME := iotx-sdk-c
+$(NAME)_SELF_BUIlD_COMP_targets=output/release/lib/libiot_sdk.a
+$(NAME)_SELF_BUIlD_COMP_scripts=iotx_sdk_make.sh
+
+GLOBAL_INCLUDES += iotx-sdk-c_clone/src/sdk-impl \
+		iotx-sdk-c_clone/src/utils/digest \
+		iotx-sdk-c_clone/src/utils/misc \
+		iotx-sdk-c_clone/src/sdk-impl/exports \
+		iotx-sdk-c_clone/src/log \
+		iotx-sdk-c_clone/src/sdk-impl/imports
