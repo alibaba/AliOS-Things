3db9fa66ee8ab7282a0eeb21bde5549bcecbe5bb
Alen <yuezhen.xyz@alibaba-inc.com>
2018-07-13 13:58:41 +0800

    [AWSS] Enable AWSS with Linkkit.

--- example/linkkitapp/ucube.py
+++ example/linkkitapp/ucube.py
@@ -1,22 +1,23 @@
-src =Split(''' 
+src =Split('''
     linkkit_sample_single.c
     linkkit_entry.c
 ''')
 #linkkit_sample_gateway.c
 component =aos_component('linkkitapp', src)
 
-dependencis =Split(''' 
+dependencis =Split('''
     framework/protocol/iotx-rhino/iotx-sdk-c
     framework/protocol/iotx-rhino/hal
     framework/netmgr
     framework/common
+    framework/ywss4linkkit
     utility/cjson
     tools/cli
 ''')
 for i in dependencis:
     component.add_comp_deps(i)
 
-global_macros =Split(''' 
+global_macros =Split('''
     MQTT_DIRECT
     ALIOT_DEBUG
     IOTX_DEBUG
@@ -24,7 +25,7 @@ global_macros =Split('''
     CONFIG_DM_DEVTYPE_SINGLE
     TEST_ALCS
     CONFIG_AOS_CLI
-    MBEDTLS_SSL_MAX_CONTENT_LEN=6144 
+    MBEDTLS_SSL_MAX_CONTENT_LEN=6144
 ''')
 for i in global_macros:
     component.add_global_macros(i)
--- framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
+++ framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
@@ -4,7 +4,9 @@ $(NAME)_SELF_BUIlD_COMP_scripts=iotx_sdk_make.sh
 
 GLOBAL_INCLUDES += iotx-sdk-c_clone/src/sdk-impl \
 		iotx-sdk-c_clone/src/utils/digest \
+		iotx-sdk-c_clone/src/utils \
 		iotx-sdk-c_clone/src/utils/misc \
 		iotx-sdk-c_clone/src/sdk-impl/exports \
 		iotx-sdk-c_clone/src/log \
-		iotx-sdk-c_clone/src/sdk-impl/imports
+		iotx-sdk-c_clone/src/sdk-impl/imports \
+		iotx-sdk-c_clone/src/protocol/alcs
