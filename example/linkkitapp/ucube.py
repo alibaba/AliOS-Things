d64784e62dcaa0edaa63a5661dd18e58fa7ecd81
zhangtian <dongren.zt@alibaba-inc.com>
2018-07-16 15:55:42 +0800

    linkkitapp:modify for esp8266 and add heap print

--- example/linkkitapp/ucube.py
+++ example/linkkitapp/ucube.py
@@ -25,7 +25,6 @@ global_macros =Split('''
     CONFIG_DM_DEVTYPE_SINGLE
     TEST_ALCS
     CONFIG_AOS_CLI
-    MBEDTLS_SSL_MAX_CONTENT_LEN=6144
 ''')
 for i in global_macros:
     component.add_global_macros(i)
@@ -33,4 +32,15 @@ for i in global_macros:
 if aos_global_config.get('LWIP') == 1:
     component.add_comp_deps("kernel/protocols/net")
     aos_global_config.set('no_with_lwip', 0)
+
+if aos_global_config.get('print_heap') == 1:
+    component.add_global_macros('CONFIG_PRINT_HEAP')
+
+if aos_global_config.mcu_family == 'esp8266':
+    component.add_global_macros('FOTA_RAM_LIMIT_MODE')
+    component.add_global_macros('ESP8266_CHIPSET')
+else :
+    component.add_global_macros('CONFIG_AOS_CLI')
+    component.add_comp_deps("cli")
+
 
