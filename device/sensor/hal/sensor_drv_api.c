5939f0299153d99c095e5d62bbd589f836d1b5a7
PreCheck <AliOS-CI@alibaba-inc.com>
2018-07-17 18:41:41 +0800

    [coding-style] auto generated coding style fixes

--- device/sensor/hal/sensor_drv_api.c
+++ device/sensor/hal/sensor_drv_api.c
@@ -7,49 +7,53 @@
 #include "hal/soc/i2c.h"
 #include "hal/sensor.h"
 
-int32_t sensor_i2c_read(i2c_dev_t *i2c, uint16_t reg, uint8_t *data, uint16_t size, uint32_t timeout)
+int32_t sensor_i2c_read(i2c_dev_t *i2c, uint16_t reg, uint8_t *data,
+                        uint16_t size, uint32_t timeout)
 {
     int32_t ret = 0;
-    if(i2c == NULL){
+    if (i2c == NULL) {
         return -1;
     }
-    ret = hal_i2c_mem_read(i2c, i2c->config.dev_addr, reg, I2C_REG_LEN, data, size, AOS_WAIT_FOREVER);
+    ret = hal_i2c_mem_read(i2c, i2c->config.dev_addr, reg, I2C_REG_LEN, data,
+                           size, AOS_WAIT_FOREVER);
     return ret;
-}    
-
-int32_t sensor_i2c_write(i2c_dev_t *i2c, uint16_t reg, uint8_t *data, uint16_t size, uint32_t timeout)
-{
-    int32_t ret = 0;
-    if(i2c == NULL){
-        return -1;
-    }
-    ret = hal_i2c_mem_write(i2c, i2c->config.dev_addr, reg, I2C_REG_LEN, data, size, AOS_WAIT_FOREVER);
-    return ret;
-}    
-
-
-int32_t sensor_i2c_single_write(i2c_dev_t *i2c, uint8_t *data, uint16_t size, uint32_t timeout)
-{
-	int32_t ret = 0;
-	if(i2c == NULL){
-		return -1;
-	}
-
-	ret = hal_i2c_master_send(i2c, i2c->config.dev_addr, data, size, AOS_WAIT_FOREVER);
-
-	return ret;
-
 }
 
-int32_t sensor_i2c_single_read(i2c_dev_t *i2c, uint8_t *data, uint16_t size, uint32_t timeout)
+int32_t sensor_i2c_write(i2c_dev_t *i2c, uint16_t reg, uint8_t *data,
+                         uint16_t size, uint32_t timeout)
 {
     int32_t ret = 0;
-    if(i2c == NULL){
+    if (i2c == NULL) {
         return -1;
     }
-    ret = hal_i2c_master_recv(i2c, i2c->config.dev_addr, data, size, AOS_WAIT_FOREVER);
+    ret = hal_i2c_mem_write(i2c, i2c->config.dev_addr, reg, I2C_REG_LEN, data,
+                            size, AOS_WAIT_FOREVER);
     return ret;
-
 }
 
 
+int32_t sensor_i2c_single_write(i2c_dev_t *i2c, uint8_t *data, uint16_t size,
+                                uint32_t timeout)
+{
+    int32_t ret = 0;
+    if (i2c == NULL) {
+        return -1;
+    }
+
+    ret = hal_i2c_master_send(i2c, i2c->config.dev_addr, data, size,
+                              AOS_WAIT_FOREVER);
+
+    return ret;
+}
+
+int32_t sensor_i2c_single_read(i2c_dev_t *i2c, uint8_t *data, uint16_t size,
+                               uint32_t timeout)
+{
+    int32_t ret = 0;
+    if (i2c == NULL) {
+        return -1;
+    }
+    ret = hal_i2c_master_recv(i2c, i2c->config.dev_addr, data, size,
+                              AOS_WAIT_FOREVER);
+    return ret;
+}
