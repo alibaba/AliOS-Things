7e88d699740d74b7580404ae2df720b785723b41
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-07-18 06:36:34 -0700

    [iotx-sdk-c] modify iotx-sdk-c.mk to refer src/infra/log + src/infra/utils
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
+++ framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
@@ -3,14 +3,13 @@ $(NAME)_SELF_BUIlD_COMP_targets=output/release/lib/libiot_sdk.a
 $(NAME)_SELF_BUIlD_COMP_scripts=iotx_sdk_make.sh
 
 GLOBAL_INCLUDES += iotx-sdk-c_clone/src/sdk-impl \
-		iotx-sdk-c_clone/src/utils/digest \
-		iotx-sdk-c_clone/src/utils \
-		iotx-sdk-c_clone/src/utils/misc \
+		iotx-sdk-c_clone/src/infra/utils/digest \
+		iotx-sdk-c_clone/src/infra/utils \
+		iotx-sdk-c_clone/src/infra/utils/misc \
+		iotx-sdk-c_clone/src/infra/log \
 		iotx-sdk-c_clone/src/sdk-impl/exports \
-		iotx-sdk-c_clone/src/log \
 		iotx-sdk-c_clone/src/sdk-impl/imports \
 		iotx-sdk-c_clone/src/protocol/alcs \
-		iotx-sdk-c_clone/src/protocol/mqtt/Link-MQTT \
 		iotx-sdk-c_clone/src/services/linkkit/dm \
 		iotx-sdk-c_clone/src/services/linkkit/cm \
 		iotx-sdk-c_clone/src/utils/misc
