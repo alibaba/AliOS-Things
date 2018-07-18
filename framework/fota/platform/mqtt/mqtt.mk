f35be60467e13c4581a4ced5371f6190efb9a5f0
zhangtian <dongren.zt@alibaba-inc.com>
2018-07-18 21:20:44 +0800

    fota:delete activation report

--- framework/fota/platform/mqtt/mqtt.mk
+++ framework/fota/platform/mqtt/mqtt.mk
@@ -1,7 +1,7 @@
 NAME := fota_mqtt_transport
 
 $(NAME)_TYPE := kernel
-$(NAME)_SOURCES := ota_transport.c version_report.c
+$(NAME)_SOURCES := ota_transport.c
 $(NAME)_INCLUDES := ../ \
                     ../../ \
                     ../../connectivity/mqtt/ \
@@ -9,7 +9,6 @@ $(NAME)_INCLUDES := ../ \
 vcall ?= rhino
 ifeq ($(vcall),rhino)
 GLOBAL_DEFINES += VCALL_RHINO
-$(NAME)_COMPONENTS  += activation
 endif                  
 ifneq (,$(filter protocol.linkkit.cm,$(COMPONENTS)))    
 $(NAME)_DEFINES := WITH_CM          
