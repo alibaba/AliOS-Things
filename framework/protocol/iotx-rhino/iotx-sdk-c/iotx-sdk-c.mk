a6c741163fa426d4b690ac9731937a92a97b0e69
zhangtian <dongren.zt@alibaba-inc.com>
2018-07-20 11:31:04 +0800

    iotx-rhino:modify sdk include patch

--- framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
+++ framework/protocol/iotx-rhino/iotx-sdk-c/iotx-sdk-c.mk
@@ -7,8 +7,9 @@ GLOBAL_INCLUDES += iotx-sdk-c_clone/src/sdk-impl \
 		iotx-sdk-c_clone/src/infra/utils \
 		iotx-sdk-c_clone/src/infra/utils/misc \
 		iotx-sdk-c_clone/src/infra/log \
-		iotx-sdk-c_clone/src/sdk-impl/exports \
-		iotx-sdk-c_clone/src/sdk-impl/imports \
+		iotx-sdk-c_clone/include/exports \
+		iotx-sdk-c_clone/include/imports \
+		iotx-sdk-c_clone/include \
 		iotx-sdk-c_clone/src/protocol/alcs \
 		iotx-sdk-c_clone/src/services/linkkit/dm \
 		iotx-sdk-c_clone/src/services/linkkit/cm \
