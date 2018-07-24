6e5ce3aee65258b1275ef9c212613db72a097b55
magao.xwj <magao.xwj@alibaba-inc.com>
2018-07-24 15:37:18 +0800

    BugID:16302127:add linkkit CI prebuild targets

--- board/mk3060/ucube.py
+++ board/mk3060/ucube.py
@@ -4,5 +4,6 @@ component = aos_board_component('board_mk3060', 'moc108', src)
 aos_global_config.add_ld_files('memory.ld.S')
 
 supported_targets="helloworld linuxapp meshapp uDataapp networkapp"
+linux_only_targets="linkkitapp"
 build_types="release"
 
