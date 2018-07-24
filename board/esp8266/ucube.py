3413271798b39b0552cf5c32a7e3f8f9caa5a337
magao.xwj <magao.xwj@alibaba-inc.com>
2018-07-24 20:08:56 +0800

    BugID:16302127:sync linkkit_example_gateway modify

--- board/esp8266/ucube.py
+++ board/esp8266/ucube.py
@@ -5,4 +5,4 @@ src     = Split('''
 component = aos_board_component('board_esp8266', 'esp8266', src)
 
 linux_only_targets="helloworld linkkitapp mqttapp linkkit_gateway coapapp linkkit_sched"
-platform_options="wifi=1"
+
