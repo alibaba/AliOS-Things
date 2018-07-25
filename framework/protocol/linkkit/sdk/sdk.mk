4b6458b81e32168098070dd1a19206c9dbf58327
magao.xwj <magao.xwj@alibaba-inc.com>
2018-07-25 17:58:40 +0800

    BugID:16300593:add tips for not support windows build

--- framework/protocol/linkkit/sdk/sdk.mk
+++ framework/protocol/linkkit/sdk/sdk.mk
@@ -1,19 +1,23 @@
+ifeq ($(HOST_OS),Win32)
+$(error windows not support linkkit sdk yet, please use linux build)
+endif
+
 NAME := linkkit_sdk
 $(NAME)_SELF_BUIlD_COMP_targets=output/release/lib/libiot_sdk.a
 $(NAME)_SELF_BUIlD_COMP_scripts=iotx_sdk_make.sh
 
 GLOBAL_INCLUDES += iotx-sdk-c_clone/src/sdk-impl \
-		iotx-sdk-c_clone/src/infra/utils/digest \
-		iotx-sdk-c_clone/src/infra/utils \
-		iotx-sdk-c_clone/src/infra/utils/misc \
-		iotx-sdk-c_clone/src/infra/log \
-		iotx-sdk-c_clone/include/exports \
-		iotx-sdk-c_clone/include/imports \
-		iotx-sdk-c_clone/include \
-		iotx-sdk-c_clone/src/protocol/alcs \
-		iotx-sdk-c_clone/src/services/linkkit/dm \
-		iotx-sdk-c_clone/src/services/linkkit/cm \
-		iotx-sdk-c_clone/src/utils/misc  \
-                iotx-sdk-c_clone/src/protocol/mqtt/Link-MQTT \
-                iotx-sdk-c_clone/src/protocol/mqtt \
-                iotx-sdk-c_clone/src/protocol/mqtt/client
+        iotx-sdk-c_clone/src/infra/utils/digest \
+        iotx-sdk-c_clone/src/infra/utils \
+        iotx-sdk-c_clone/src/infra/utils/misc \
+        iotx-sdk-c_clone/src/infra/log \
+        iotx-sdk-c_clone/include/exports \
+        iotx-sdk-c_clone/include/imports \
+        iotx-sdk-c_clone/include \
+        iotx-sdk-c_clone/src/protocol/alcs \
+        iotx-sdk-c_clone/src/services/linkkit/dm \
+        iotx-sdk-c_clone/src/services/linkkit/cm \
+        iotx-sdk-c_clone/src/utils/misc  \
+        iotx-sdk-c_clone/src/protocol/mqtt/Link-MQTT \
+        iotx-sdk-c_clone/src/protocol/mqtt \
+        iotx-sdk-c_clone/src/protocol/mqtt/client
