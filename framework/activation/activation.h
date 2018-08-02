51cde7e46a4c08da18eb0f86efe0579b95a05b6d
xuwenjie <291214203@qq.com>
2018-08-02 20:43:06 +0800

    update activation Linux lib on branch aos1.3.3

--- framework/activation/activation.h
+++ framework/activation/activation.h
@@ -13,25 +13,20 @@
 #define UINT8 unsigned char
 #endif
 
-#define VERSION_NUM_SIZE 4
-#define RANDOM_NUM_SIZE 4
-#define MAC_ADDRESS_SIZE 8
-#define CHIP_CODE_SIZE 4
-#define INPUT_SIZE \
-    (VERSION_NUM_SIZE + RANDOM_NUM_SIZE + MAC_ADDRESS_SIZE + CHIP_CODE_SIZE)
-#define OUTPUT_SPACE_SIZE (INPUT_SIZE * 2 * 2 + 1)
+#define VERSION_NUM_SIZE    4
+#define RANDOM_NUM_SIZE     4
+#define MAC_ADDRESS_SIZE    8
+#define CHIP_CODE_SIZE      4
+#define INPUT_SIZE          (VERSION_NUM_SIZE+RANDOM_NUM_SIZE+MAC_ADDRESS_SIZE+CHIP_CODE_SIZE)
+#define OUTPUT_SPACE_SIZE   (INPUT_SIZE*2*2+1)
 
 
 /*
-    input: version 4byte + random 4 byte + mac 4byte + chip_code 4byte
-    output: output_buffer store the version info process. length at least
-   OUTPUT_SPACE_SIZE return: 0 success, 1 failed
+	input: version 4byte + random 4 byte + mac 8byte + chip_code 4byte
+	output: output_buffer store the version info process. length at least OUTPUT_SPACE_SIZE
+	return: 0 success, 1 failed	
 */
-extern UINT32 aos_get_version_info(UINT8  version_num[VERSION_NUM_SIZE],
-                                   UINT8  random_num[RANDOM_NUM_SIZE],
-                                   UINT8  mac_address[MAC_ADDRESS_SIZE],
-                                   UINT8  chip_code[CHIP_CODE_SIZE],
-                                   UINT8 *output_buffer,
-                                   UINT32 output_buffer_size);
+extern UINT32 aos_get_version_info (UINT8 version_num[VERSION_NUM_SIZE], UINT8 random_num[RANDOM_NUM_SIZE], UINT8 mac_address[MAC_ADDRESS_SIZE], UINT8 chip_code[CHIP_CODE_SIZE], UINT8 *output_buffer, UINT32 output_buffer_size);
 
 #endif
+
