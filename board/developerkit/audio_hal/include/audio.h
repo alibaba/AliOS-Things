0ad74f7a367238eb7ee5512c9f5096d50db4503e
Leon <msls@qq.com>
2018-08-29 16:48:34 +0800

    Developerkit audio data stored on SD card.

--- board/developerkit/audio_hal/include/audio.h
+++ board/developerkit/audio_hal/include/audio.h
@@ -6,6 +6,8 @@
 int flash_write(uint32_t flash_addr, void *src, int size);
 int record_to_flash(void);
 int playback_from_flash(void);
+int record_to_sdcard(const char *file_in_sd, int *stop_flag);
+int playback_from_sdcard(const char *file_in_sd);
 int audio_init(void);
 
 #endif /* __AUDIO_H */
