c35f824e21d5bc141ab27eb809d70b31fd1e568c
gonghui.hui <gonghui.hui@alibaba-inc.com>
2018-09-03 15:00:53 +0800

    stm32f429zi-nucleo: Sync generated files from CubeMX

--- board/stm32f429zi-nucleo/Inc/usart.h
+++ board/stm32f429zi-nucleo/Inc/usart.h
@@ -52,6 +52,7 @@
 /* USER CODE END Includes */
 
 extern UART_HandleTypeDef huart4;
+extern UART_HandleTypeDef huart5;
 extern UART_HandleTypeDef huart7;
 extern UART_HandleTypeDef huart3;
 extern UART_HandleTypeDef huart6;
@@ -63,6 +64,7 @@ extern UART_HandleTypeDef huart6;
 extern void _Error_Handler(char *, int);
 
 void MX_UART4_Init(void);
+void MX_UART5_Init(void);
 void MX_UART7_Init(void);
 void MX_USART3_UART_Init(void);
 void MX_USART6_UART_Init(void);
