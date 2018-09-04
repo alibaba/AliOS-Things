30eff97d248909d4419bcaf62132c51231277950
magao.xwj <magao.xwj@alibaba-inc.com>
2018-09-04 16:37:55 +0800

    fix meshapp build problem

--- network/umesh/umesh.mk
+++ network/umesh/umesh.mk
@@ -5,9 +5,9 @@ GLOBAL_INCLUDES += include
 GLOBAL_DEFINES += CONFIG_AOS_MESH
 
 ifeq ($(COMPILER),armcc)
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/keil/mesh.a
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/keil/umesh.a
 else ifeq ($(COMPILER),iar)
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/iar/mesh.a
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/iar/umesh.a
 else
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/gcc/mesh.a
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/gcc/umesh.a
 endif
