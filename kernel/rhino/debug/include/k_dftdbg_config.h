937048fe8f3a1b4647c01ec8183e190ad8199554
wuqi.hz <wuqi.hz@alibaba-inc.com>
2018-09-10 13:41:10 +0800

    BugID:16813529:Change for XM510, cpu_usage in dumpsys
    Change-Id: I7e38922e93c8a3ba378f0e8ef3521bb54cfd0f40

--- kernel/rhino/debug/include/k_dftdbg_config.h
+++ kernel/rhino/debug/include/k_dftdbg_config.h
@@ -6,27 +6,27 @@
 #define K_DFTDBG_CONFIG_H
 
 #ifndef RHINO_CONFIG_BACKTRACE
-#define RHINO_CONFIG_BACKTRACE 0
+#define RHINO_CONFIG_BACKTRACE               0
 #endif
 
 /* If AliOS task over the Exception/Fatal Error */
 #ifndef RHINO_CONFIG_PANIC
-#define RHINO_CONFIG_PANIC 0
+#define RHINO_CONFIG_PANIC                   0
 #endif
 
 #if (RHINO_CONFIG_PANIC > 0)
 /* If the mcu printf depends on isr */
 #ifndef RHINO_CONFIG_PANIC_PRT_INT
-#define RHINO_CONFIG_PANIC_PRT_INT 0
+#define RHINO_CONFIG_PANIC_PRT_INT           0
 #endif
 
 #ifndef RHINO_CONFIG_PANIC_OVERVIEW
-#define RHINO_CONFIG_PANIC_OVERVIEW 1
+#define RHINO_CONFIG_PANIC_OVERVIEW          1
 #endif
 #endif
 
 #ifndef RHINO_CONFIG_NORMAL_PRT
-#define RHINO_CONFIG_NORMAL_PRT 1
+#define RHINO_CONFIG_NORMAL_PRT              1
 #endif
 
 
