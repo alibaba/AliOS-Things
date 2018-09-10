c79e160898600812184b76c02dc43c09a99bf78d
xuhongNewPC <xuhongv@yeah.net>
2018-09-10 10:51:29 +0800

    alter gpio.c for esp8266 from xuhongv
    alter gpio.c for esp8266 from xuhongv

--- platform/mcu/esp8266/hal/gpio.c
+++ platform/mcu/esp8266/hal/gpio.c
@@ -32,6 +32,11 @@ int32_t hal_gpio_init(aos_gpio_dev_t *gpio)
         return EIO;
     }
 
+    if (gpio ->port > 16)
+    {
+        return EIO;
+    }
+
     if (gpio->port == 16)
     {
         gpio16_output_conf();
@@ -173,8 +178,6 @@ int32_t hal_gpio_input_get(aos_gpio_dev_t *gpio, uint32_t *value)
     return 0;
 }
 
-
-
 /*
 *   en: enable gpio interrupt function , maybe you will add  #include "esp8266/eagle_soc.h" in the gpio_register.h ,  and the more please see the sample
 *   ch: 这是一个串口中断初始化函数 , 如果编译报错请在gpio_register.h 添加 esp8266/eagle_soc.h , 8266的使用gpio中断有点特殊；
@@ -189,17 +192,17 @@ int32_t hal_gpio_enable_irq(aos_gpio_dev_t *gpio, gpio_irq_trigger_t trigger,
     {
         return EIO;
     }
-    GPIO_ConfigTypeDef gpio_in_cfg;                    //Define GPIO Init Structure
+    GPIO_ConfigTypeDef gpio_in_cfg;                     //Define GPIO Init Structure
     gpio_in_cfg.GPIO_IntrType = (GPIO_INT_TYPE)trigger; //set the type
-    gpio_in_cfg.GPIO_Mode = GPIO_Mode_Input;           //Input mode
+    gpio_in_cfg.GPIO_Mode = GPIO_Mode_Input;            //Input mode
     gpio_in_cfg.GPIO_Pullup = GPIO_PullUp_EN;
-    gpio_in_cfg.GPIO_Pin = BIT(gpio->port);  // Enable GPIO
-    gpio_config(&gpio_in_cfg);                        //Initialization function
-    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(gpio->port));//Forbidden ouput register
+    gpio_in_cfg.GPIO_Pin = BIT(gpio->port);                    // Enable GPIO
+    gpio_config(&gpio_in_cfg);                                 //Initialization function
+    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(gpio->port)); //Forbidden ouput register
     gpio_intr_handler_register(handler, arg);
-    _xt_isr_unmask(1 << ETS_GPIO_INUM);                                //enable
+    _xt_isr_unmask(1 << ETS_GPIO_INUM); //enable
 
-    return 0;   
+    return 0;
 }
 
 int32_t hal_gpio_disable_irq(aos_gpio_dev_t *gpio)
@@ -208,7 +211,7 @@ int32_t hal_gpio_disable_irq(aos_gpio_dev_t *gpio)
     {
         return EIO;
     }
-    _xt_isr_mask(1<<ETS_GPIO_INUM);    //disable interrupt  ETS_GPIO_INTR_DISABLE();  
+    _xt_isr_mask(1 << ETS_GPIO_INUM); //disable interrupt  ETS_GPIO_INTR_DISABLE();
     return 0;
 }
 
@@ -219,9 +222,9 @@ int32_t hal_gpio_clear_irq(aos_gpio_dev_t *gpio)
         return EIO;
     }
 
-    GPIO_REG_WRITE( GPIO_STATUS_W1TC_ADDRESS, BIT(gpio->port) ); //clear interrupt mask
-    _xt_isr_unmask(1 << ETS_GPIO_INUM); //Enable the GPIO interrupt
-   
+    GPIO_REG_WRITE(GPIO_STATUS_W1TC_ADDRESS, BIT(gpio->port)); //clear interrupt mask
+    _xt_isr_unmask(1 << ETS_GPIO_INUM);                        //Enable the GPIO interrupt
+
     return 0;
 }
 
