7eefe42ec09de00136ba20d10b2e77d293c72edb
chongling <yj144830@alibaba-inc.com>
2018-09-11 15:28:00 +0800

    BugID:16851932:add stm32f4 uart gpio flash hal 2
    Change-Id: I1544e3208e9bd4bdbb7859cb4ec86fba87a6e2f0

--- platform/mcu/stm32f4xx_cube/hal/hal_flash_stm32f4.c
+++ platform/mcu/stm32f4xx_cube/hal/hal_flash_stm32f4.c
@@ -3,224 +3,45 @@
  */
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal.h"
-
 #ifdef HAL_FLASH_MODULE_ENABLED
 
 #include <k_api.h>
 #include "hal/soc/soc.h"
 #include "stm32f4xx_hal_flash.h"
 
-#define ROUND_DOWN(a,b) (((a) / (b)) * (b))
-#define MIN(a,b)        (((a) < (b)) ? (a) : (b))
+/* Base address of the Flash sectors Bank 1 */
+#define ADDR_FLASH_SECTOR_0     ((uint32_t)0x08000000) /* Base @ of Sector 0, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_1     ((uint32_t)0x08004000) /* Base @ of Sector 1, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_2     ((uint32_t)0x08008000) /* Base @ of Sector 2, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_3     ((uint32_t)0x0800C000) /* Base @ of Sector 3, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_4     ((uint32_t)0x08010000) /* Base @ of Sector 4, 64 Kbytes */
+#define ADDR_FLASH_SECTOR_5     ((uint32_t)0x08020000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_6     ((uint32_t)0x08040000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_7     ((uint32_t)0x08060000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_8     ((uint32_t)0x08080000) /* Base @ of Sector 8, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_9     ((uint32_t)0x080A0000) /* Base @ of Sector 9, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_10    ((uint32_t)0x080C0000) /* Base @ of Sector 10, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_11    ((uint32_t)0x080E0000) /* Base @ of Sector 11, 128 Kbytes */
+
+/* Base address of the Flash sectors Bank 2 */
+#define ADDR_FLASH_SECTOR_12     ((uint32_t)0x08100000) /* Base @ of Sector 0, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_13     ((uint32_t)0x08104000) /* Base @ of Sector 1, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_14     ((uint32_t)0x08108000) /* Base @ of Sector 2, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_15     ((uint32_t)0x0810C000) /* Base @ of Sector 3, 16 Kbytes */
+#define ADDR_FLASH_SECTOR_16     ((uint32_t)0x08110000) /* Base @ of Sector 4, 64 Kbytes */
+#define ADDR_FLASH_SECTOR_17     ((uint32_t)0x08120000) /* Base @ of Sector 5, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_18     ((uint32_t)0x08140000) /* Base @ of Sector 6, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_19     ((uint32_t)0x08160000) /* Base @ of Sector 7, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_20     ((uint32_t)0x08180000) /* Base @ of Sector 8, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_21     ((uint32_t)0x081A0000) /* Base @ of Sector 9, 128 Kbytes  */
+#define ADDR_FLASH_SECTOR_22     ((uint32_t)0x081C0000) /* Base @ of Sector 10, 128 Kbytes */
+#define ADDR_FLASH_SECTOR_23     ((uint32_t)0x081E0000) /* Base @ of Sector 11, 128 Kbytes */
 
 extern const hal_logic_partition_t hal_partitions[];
-#if 0
-/**
-  * @brief  Get the bank of a given address.
-  * @param  In: addr      Address in the FLASH Memory.
-  * @retval Bank identifier.
-  *           FLASH_BANK_1
-  *           FLASH_BANK_2
-  */
-uint32_t FLASH_get_bank(uint32_t addr)
-{
-    uint32_t bank = 0;
 
-#if defined (STM32L471xx) || defined (STM32L475xx) || defined (STM32L476xx) || \
-    defined (STM32L485xx) || defined (STM32L486xx) || defined (STM32L496xx) || \
-    defined (STM32L4A6xx) || defined (STM32L4R5xx) || defined (STM32L4R7xx) || \
-    defined (STM32L4R9xx) || defined (STM32L4S5xx) || defined (STM32L4S7xx) || defined (STM32L4S9xx)
+static uint32_t GetSector(uint32_t Address);
+int FLASH_read_at(uint32_t address, uint8_t *pData, uint32_t len_bytes);
 
-    if (READ_BIT(SYSCFG->MEMRMP, SYSCFG_MEMRMP_FB_MODE) == 0) {
-        /* No Bank swap */
-        bank = (addr < (FLASH_BASE + FLASH_BANK_SIZE)) ? FLASH_BANK_1 : FLASH_BANK_2;
-    } else {
-        /* Bank swap */
-        bank = (addr < (FLASH_BASE + FLASH_BANK_SIZE)) ? FLASH_BANK_2 : FLASH_BANK_1;
-    }
-
-#else
-    bank = FLASH_BANK_1;
-#endif
-
-    return bank;
-}
-
-/**
-  * @brief  Get the page of a given address within its FLASH bank.
-  * @param  In: addr    Address in the FLASH memory.
-  * @retval >=0 Success: Page number.
-  *          <0 Failure: The address in not mapped in the internal FLASH memory.
-  */
-int FLASH_get_pageInBank(uint32_t addr)
-{
-    int page = -1;
-
-    if ( ((FLASH_BASE + FLASH_SIZE) > addr) && (addr >= FLASH_BASE) ) {
-        /* The address is in internal FLASH range. */
-        if ( addr < (FLASH_BASE + FLASH_BANK_SIZE) ) {
-            /* Addr in the first bank */
-            page = (addr - FLASH_BASE) / FLASH_PAGE_SIZE;
-        } else {
-            /* Addr in the second bank */
-            page = (addr - FLASH_BASE - FLASH_BANK_SIZE) / FLASH_PAGE_SIZE;
-        }
-    }
-
-    return page;
-}
-
-/**
-  * @brief  Erase FLASH memory page(s) at address.
-  * @note   The range to erase shall not cross the bank boundary.
-  * @param  In: address     Start address to erase from.
-  * @param  In: len_bytes   Length to be erased.
-  * @retval  0:  Success.
-            -1:  Failure.
-  */
-int FLASH_unlock_erase(uint32_t address, uint32_t len_bytes)
-{
-    int rc = -1;
-    uint32_t PageError = 0;
-    FLASH_EraseInitTypeDef EraseInit;
-
-    /* L4 ROM memory map, with 2 banks split into 2kBytes pages.
-     * WARN: ABW. If the passed address and size are not page-aligned,
-     * the start of the first page and the end of the last page are erased anyway.
-     * After erase, the flash is left in unlocked state.
-     */
-    EraseInit.TypeErase = FLASH_TYPEERASE_PAGES;
-
-    EraseInit.Banks = FLASH_get_bank(address);
-    if (EraseInit.Banks != FLASH_get_bank(address + len_bytes)) {
-        printf("Error: Cannot erase across FLASH banks.\n");
-    } else {
-        EraseInit.Page = FLASH_get_pageInBank(address);
-        EraseInit.NbPages = FLASH_get_pageInBank(address + len_bytes - 1) - EraseInit.Page + 1;
-
-        HAL_FLASH_Unlock();
-
-        if (HAL_FLASHEx_Erase(&EraseInit, &PageError) == HAL_OK) {
-            rc = 0;
-        } else {
-            printf("Error erasing at 0x%08lx\n", address);
-        }
-    }
-    return rc;
-}
-
-/**
-  * @brief  Write to FLASH memory.
-  * @param  In: address     Destination address.
-  * @param  In: pData       Data to be programmed: Must be 8 byte aligned.
-  * @param  In: len_bytes   Number of bytes to be programmed.
-  * @retval  0: Success.
-            -1: Failure.
-  */
-int FLASH_write_at(uint32_t address, uint64_t *pData, uint32_t len_bytes)
-{
-    int i;
-    int ret = -1;
-#ifndef CODE_UNDER_FIREWALL
-    /* irq already mask under firewall */
-    __disable_irq();
-#endif
-
-    for (i = 0; i < len_bytes; i += 8) {
-        if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_DOUBLEWORD,
-                              address + i,
-                              *(pData + (i / 8) )) != HAL_OK) {
-            break;
-        }
-    }
-    /* Memory check */
-    for (i = 0; i < len_bytes; i += 4) {
-        uint32_t *dst = (uint32_t *)(address + i);
-        uint32_t *src = ((uint32_t *) pData) + (i / 4);
-
-        if ( *dst != *src ) {
-#ifndef CODE_UNDER_FIREWALL
-            printf("Write failed @0x%08lx, read value=0x%08lx, expected=0x%08lx\n", (uint32_t) dst, *dst, *src);
-#endif
-            break;
-        }
-        ret = 0;
-    }
-#ifndef CODE_UNDER_FIREWALL
-    /* irq should never be enable under firewall */
-    __enable_irq();
-#endif
-    return ret;
-}
-
-/*****************************************************************/
-/**
-  * @brief  Read from FLASH memory.
-  * @param  In: address     Destination address.
-  * @param  In: pData       Data to be programmed: Must be 8 byte aligned.
-  * @param  In: len_bytes   Number of bytes to be programmed.
-  * @retval  0: Success.
-            -1: Failure.
-  */
-int FLASH_read_at(uint32_t address, uint64_t *pData, uint32_t len_bytes)
-{
-    int i;
-    int ret = -1;
-    uint32_t *src = (uint32_t *)(address);
-    uint32_t *dst = ((uint32_t *) pData);
-
-
-    for (i = 0; i < len_bytes; i += 4) {
-        *(dst + i / 4) = *(src++);
-    }
-
-    ret = 0;
-    return ret;
-}
-
-int FLASH_update(uint32_t dst_addr, const void *data, uint32_t size)
-{
-    int ret = 0;
-    int remaining = size;
-    uint8_t *src_addr = (uint8_t *) data;
-
-    uint64_t *page_cache = NULL;
-    page_cache = (uint64_t *)krhino_mm_alloc(FLASH_PAGE_SIZE);
-    if (page_cache == NULL) {
-        return -1;
-    }
-    memset(page_cache, 0, FLASH_PAGE_SIZE);
-
-    __HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_ALL_ERRORS);
-
-    do {
-        uint32_t fl_addr = ROUND_DOWN(dst_addr, FLASH_PAGE_SIZE);
-        int fl_offset = dst_addr - fl_addr;
-        int len = MIN(FLASH_PAGE_SIZE - fl_offset, size);
-
-        /* Load from the flash into the cache */
-        memcpy(page_cache, (void *) fl_addr, FLASH_PAGE_SIZE);
-        /* Update the cache from the source */
-        memcpy((uint8_t *)page_cache + fl_offset, src_addr, len);
-        /* Erase the page, and write the cache */
-        ret = FLASH_unlock_erase(fl_addr, FLASH_PAGE_SIZE);
-        if (ret != 0) {
-            printf("Error erasing at 0x%08lx\n", fl_addr);
-        } else {
-            ret = FLASH_write_at(fl_addr, page_cache, FLASH_PAGE_SIZE);
-            if (ret != 0) {
-                printf("Error writing %lu bytes at 0x%08lx\n", FLASH_PAGE_SIZE, fl_addr);
-            } else {
-                dst_addr += len;
-                src_addr += len;
-                remaining -= len;
-            }
-        }
-    } while ((ret == 0) && (remaining > 0));
-
-    krhino_mm_free(page_cache);
-    return ret;
-}
 
 hal_logic_partition_t *hal_flash_get_info(hal_partition_t pno)
 {
@@ -233,86 +54,249 @@ hal_logic_partition_t *hal_flash_get_info(hal_partition_t pno)
 
 int32_t hal_flash_write(hal_partition_t pno, uint32_t *poff, const void *buf , uint32_t buf_size)
 {
-    uint32_t start_addr;
-    hal_logic_partition_t *partition_info;
-    hal_partition_t real_pno;
+	int32_t ret=0;
+  uint32_t start_addr;
+	uint32_t byte_count=0;//count the number of programmed bytes
+  uint8_t* src_data_p;
+  hal_logic_partition_t *partition_info;
+	
+	partition_info = hal_flash_get_info( pno );
+  start_addr = partition_info->partition_start_addr + *poff;
+	if((*poff+buf_size) > partition_info->partition_length)
+		return -1;
 
-    real_pno = pno;
-
-    partition_info = hal_flash_get_info( real_pno );
-    start_addr = partition_info->partition_start_addr + *poff;
-    if (0 != FLASH_update(start_addr, buf, buf_size)) {
-        printf("FLASH_update failed!\n");
-    }
-    *poff += buf_size;
-    return 0;
+	/* Unlock the Flash to enable the flash control register access *************/
+  HAL_FLASH_Unlock();
+	src_data_p = (uint8_t*)buf;
+	while(byte_count<buf_size)
+	{
+		if(HAL_FLASH_Program(FLASH_TYPEPROGRAM_BYTE,start_addr+byte_count,*(src_data_p++))!=HAL_OK)
+		{
+			ret =-1;
+			break;
+		}
+		byte_count++;
+	}
+	*poff+=byte_count;
+  /* Lock the Flash to disable the flash control register access (recommended
+	 to protect the FLASH memory against possible unwanted operation) *********/
+	HAL_FLASH_Lock();
+  return ret;
 }
+
 
 int32_t hal_flash_read(hal_partition_t pno, uint32_t *poff, void *buf, uint32_t buf_size)
 {
-    uint32_t start_addr;
-    hal_logic_partition_t *partition_info;
-    hal_partition_t real_pno;
+	int32_t ret=0;
+	hal_logic_partition_t *partition_info;
+	uint32_t start_addr;
+	uint32_t i=0;
+	
+	partition_info = hal_flash_get_info( pno );
+	if (poff == NULL || buf == NULL || *poff + buf_size > partition_info->partition_length) 
+	{
+			return -1;
+	}
+	
+	start_addr = partition_info->partition_start_addr + *poff;	
+  FLASH_read_at(start_addr,(uint8_t*)buf,buf_size);
+	
+	*poff += buf_size;
+	
+  return ret;
+}
 
-    real_pno = pno;
+int32_t hal_flash_erase(hal_partition_t pno, uint32_t off_set, uint32_t size)
+{
+	int32_t ret=0;
+	hal_logic_partition_t *partition_info;
+	uint32_t FirstSector = 0, NbOfSectors = 0;
+	uint32_t SECTORError = 0;
+	FLASH_EraseInitTypeDef EraseInit;
 
-    partition_info = hal_flash_get_info( real_pno );
+	partition_info = hal_flash_get_info( pno );
+	if (size + off_set > partition_info->partition_length) {
+			return -1;
+	}
+	
+	FirstSector = GetSector(partition_info->partition_start_addr+off_set);
+	NbOfSectors = GetSector(partition_info->partition_start_addr+off_set + size -1)-FirstSector+1;
+	
+	EraseInit.TypeErase = FLASH_TYPEERASE_SECTORS;
+	EraseInit.VoltageRange = FLASH_VOLTAGE_RANGE_3;//require Device operating range: 2.7V to 3.6V 
+	EraseInit.Sector =FirstSector;
+	EraseInit.NbSectors =NbOfSectors;
+	
+	/* Unlock the Flash to enable the flash control register access *************/
+	HAL_FLASH_Unlock();
+	
+	if(HAL_FLASHEx_Erase(&EraseInit, &SECTORError) != HAL_OK)
+	{
+		ret =-1;
+	}
+	
+	 /* Lock the Flash to disable the flash control register access (recommended
+	 to protect the FLASH memory against possible unwanted operation) *********/
+	HAL_FLASH_Lock();
+	
+	return ret;
+}
 
-    if (poff == NULL || buf == NULL || *poff + buf_size > partition_info->partition_length) {
-        return -1;
+
+/**
+  * @brief  Read from FLASH memory.
+  * @param  In: address     Destination address.
+  * @param  In: pData       Data to be programmed: Must be 8 byte aligned.
+  * @param  In: len_bytes   Number of bytes to be programmed.
+  * @retval  0: Success.
+            -1: Failure.
+  */
+int FLASH_read_at(uint32_t address, uint8_t *pData, uint32_t len_bytes)
+{
+    uint32_t i;
+    int ret = -1;
+    uint8_t *src = (uint8_t *)(address);
+    uint8_t *dst = (uint8_t *)(pData);
+
+    for (i = 0; i < len_bytes; i++)
+    {
+        *(dst++) = *(src++);
     }
-    start_addr = partition_info->partition_start_addr + *poff;
-    FLASH_read_at(start_addr, buf, buf_size);
-    *poff += buf_size;
 
-    return 0;
+    ret = 0;
+    return ret;
 }
 
-int32_t hal_flash_erase(hal_partition_t pno, uint32_t off_set,
-                        uint32_t size)
+/**
+  * @brief  Gets the sector of a given address
+  * @param  None
+  * @retval The sector of a given address
+  */
+static uint32_t GetSector(uint32_t Address)
 {
-    uint32_t start_addr;
-    hal_logic_partition_t *partition_info;
-    hal_partition_t real_pno;
+  uint32_t sector = 0;
 
-    real_pno = pno;
-
-    partition_info = hal_flash_get_info( real_pno );
-    if (size + off_set > partition_info->partition_length) {
-        return -1;
-    }
-
-    start_addr = ROUND_DOWN((partition_info->partition_start_addr + off_set), FLASH_PAGE_SIZE);
-
-    FLASH_unlock_erase(start_addr, size);
-    return 0;
+  if((Address < ADDR_FLASH_SECTOR_1) && (Address >= ADDR_FLASH_SECTOR_0))
+  {
+    sector = FLASH_SECTOR_0;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_2) && (Address >= ADDR_FLASH_SECTOR_1))
+  {
+    sector = FLASH_SECTOR_1;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_3) && (Address >= ADDR_FLASH_SECTOR_2))
+  {
+    sector = FLASH_SECTOR_2;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_4) && (Address >= ADDR_FLASH_SECTOR_3))
+  {
+    sector = FLASH_SECTOR_3;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_5) && (Address >= ADDR_FLASH_SECTOR_4))
+  {
+    sector = FLASH_SECTOR_4;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_6) && (Address >= ADDR_FLASH_SECTOR_5))
+  {
+    sector = FLASH_SECTOR_5;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_7) && (Address >= ADDR_FLASH_SECTOR_6))
+  {
+    sector = FLASH_SECTOR_6;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_8) && (Address >= ADDR_FLASH_SECTOR_7))
+  {
+    sector = FLASH_SECTOR_7;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_9) && (Address >= ADDR_FLASH_SECTOR_8))
+  {
+    sector = FLASH_SECTOR_8;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_10) && (Address >= ADDR_FLASH_SECTOR_9))
+  {
+    sector = FLASH_SECTOR_9;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_11) && (Address >= ADDR_FLASH_SECTOR_10))
+  {
+    sector = FLASH_SECTOR_10;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_12) && (Address >= ADDR_FLASH_SECTOR_11))
+  {
+    sector = FLASH_SECTOR_11;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_13) && (Address >= ADDR_FLASH_SECTOR_12))
+  {
+    sector = FLASH_SECTOR_12;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_14) && (Address >= ADDR_FLASH_SECTOR_13))
+  {
+    sector = FLASH_SECTOR_13;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_15) && (Address >= ADDR_FLASH_SECTOR_14))
+  {
+    sector = FLASH_SECTOR_14;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_16) && (Address >= ADDR_FLASH_SECTOR_15))
+  {
+    sector = FLASH_SECTOR_15;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_17) && (Address >= ADDR_FLASH_SECTOR_16))
+  {
+    sector = FLASH_SECTOR_16;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_18) && (Address >= ADDR_FLASH_SECTOR_17))
+  {
+    sector = FLASH_SECTOR_17;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_19) && (Address >= ADDR_FLASH_SECTOR_18))
+  {
+    sector = FLASH_SECTOR_18;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_20) && (Address >= ADDR_FLASH_SECTOR_19))
+  {
+    sector = FLASH_SECTOR_19;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_21) && (Address >= ADDR_FLASH_SECTOR_20))
+  {
+    sector = FLASH_SECTOR_20;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_22) && (Address >= ADDR_FLASH_SECTOR_21))
+  {
+    sector = FLASH_SECTOR_21;
+  }
+  else if((Address < ADDR_FLASH_SECTOR_23) && (Address >= ADDR_FLASH_SECTOR_22))
+  {
+    sector = FLASH_SECTOR_22;
+  }
+  else /* (Address < FLASH_END_ADDR) && (Address >= ADDR_FLASH_SECTOR_23) */
+  {
+    sector = FLASH_SECTOR_23;
+  }  
+  return sector;
 }
 
-int32_t hal_flash_enable_secure(hal_partition_t partition, uint32_t off_set, uint32_t size)
+/**
+  * @brief  Gets sector Size
+  * @param  None
+  * @retval The size of a given sector
+  */
+static uint32_t GetSectorSize(uint32_t Sector)
 {
-    return 0;
+  uint32_t sectorsize = 0x00;
+  if((Sector == FLASH_SECTOR_0) || (Sector == FLASH_SECTOR_1) || (Sector == FLASH_SECTOR_2) ||\
+     (Sector == FLASH_SECTOR_3) || (Sector == FLASH_SECTOR_12) || (Sector == FLASH_SECTOR_13) ||\
+     (Sector == FLASH_SECTOR_14) || (Sector == FLASH_SECTOR_15))
+  {
+    sectorsize = 16 * 1024;
+  }
+  else if((Sector == FLASH_SECTOR_4) || (Sector == FLASH_SECTOR_16))
+  {
+    sectorsize = 64 * 1024;
+  }
+  else
+  {
+    sectorsize = 128 * 1024;
+  }  
+  return sectorsize;
 }
-
-int32_t hal_flash_dis_secure(hal_partition_t partition, uint32_t off_set, uint32_t size)
-{
-    return 0;
-}
-#else
-int32_t hal_flash_write(hal_partition_t pno, uint32_t *poff, const void *buf , uint32_t buf_size)
-{
-    return 0;
-}
-
-int32_t hal_flash_read(hal_partition_t pno, uint32_t *poff, void *buf, uint32_t buf_size)
-{
-    return 0;
-}
-
-int32_t hal_flash_erase(hal_partition_t pno, uint32_t off_set,
-                        uint32_t size)
-{
-    return 0;
-}
-
-#endif
 #endif
