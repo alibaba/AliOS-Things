46af6243cb8d9fdfbe179ea774dc4922022cc911
jianniu.ljg | lijianguo <jianniu.ljg@alibaba-inc.com>
2018-09-11 22:21:07 +0800

    BugID:16912068:check frame length more  strictly
    Change-Id: I442320bac44d36e725d34bd63a436d3977b13782

--- kernel/rhino/bus/mbmaster/mbmaster.mk
+++ kernel/rhino/bus/mbmaster/mbmaster.mk
@@ -3,21 +3,28 @@ NAME := mbmaster
 $(NAME)_TYPE 	    := kernel
 $(NAME)_MBINS_TYPE  := kernel
 
+$(NAME)_SOURCES     := functions/mbfunccoils.c
+$(NAME)_SOURCES     += functions/mbfuncholding.c
+$(NAME)_SOURCES     += port/port_event.c
+$(NAME)_SOURCES     += port/port_other.c
+$(NAME)_SOURCES     += port/port_timer.c
+$(NAME)_SOURCES     += rtu/mbcrc.c
+$(NAME)_SOURCES     += rtu/mbrtu.c
+$(NAME)_SOURCES     += mb.c
 
 ifeq ($(HOST_ARCH),linux)
 $(NAME)_DEFINES     += IO_NEED_TRAP
 endif
 
+#default gcc
+ifeq ($(COMPILER),)
+$(NAME)_CFLAGS      += -Wall -Werror
+else ifeq ($(COMPILER),gcc)
+$(NAME)_CFLAGS      += -Wall -Werror
+else ifeq ($(COMPILER),armcc)
+GLOBAL_DEFINES      += __BSD_VISIBLE
+endif
+
 GLOBAL_INCLUDES     += include
 GLOBAL_DEFINES      += AOS_MBMASTER
-
-ifeq ($(COMPILER),armcc)
-$(error no armcc lib provided!)
-else ifeq ($(COMPILER),iar)
-$(error no iar lib provided!)
-else ifeq ($(ENABLE_VFP),1)
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/gcc/mbmaster.a
-else
-$(error no none-vfp gcc lib provided!)
-endif
 
