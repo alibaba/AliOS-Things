7d59ee625d575dd6966fd114078242424ca1c76d
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2018-09-13 00:45:18 +0800

    BugID:16334045:add the breeze ota into uagent
    Change-Id: I1c7794949012ded5517631089c661d6f2a4405b2

---
+++ middleware/uagent/uota_ble/uota_ble.mk
@@ -0,0 +1,13 @@
+NAME := uOTA_BLE
+$(info $(NAME))
+$(NAME)_TYPE := framework
+
+$(NAME)_SOURCES += \
+    src/ota_breeze.c \
+    src/ota_breeze_service.c \
+    src/ota_breeze_transport.c  \
+    hal/ota_hal_os.c  \
+
+GLOBAL_INCLUDES += \
+                  inc \
+                  hal \
