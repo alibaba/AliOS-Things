586e114953f21a181470d6e0b119014588ffc250
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-09-14 00:38:18 +0800

    BugID:16855090:[build-rules] enable OSX build w/o OTA + awss
    Change-Id: I1e12966511b3a5e1aa887be51e69397f8dc04ae2
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/tools/mock_build_options.mk
+++ middleware/linkkit/sdk-c/src/tools/mock_build_options.mk
@@ -3,3 +3,8 @@ ifneq (,$(filter -D_PLATFORM_IS_WINDOWS_,$(CFLAGS)))
     CFLAGS := $(filter-out -DOTA_ENABLED,$(CFLAGS))
 endif
 
+ifeq (Darwin,$(shell uname))
+    CFLAGS := $(filter-out -DOTA_ENABLED,$(CFLAGS))
+    CFLAGS := $(filter-out -DWIFI_AWSS_ENABLED,$(CFLAGS))
+endif
+
