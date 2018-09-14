6bdc6fbe6ecd731aff35443b93bd9e206a8a90fd
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-09-14 13:56:56 +0800

    BugID:16846667:Code optimization
    Details:
        1. define SVC num in svc.h
        2. move arch_syscall.h into include dir
        3. Support returning u64 from syscall
        4. Code clean
    
    Change-Id: Id4e26b034a770eeb4ebc436a48ec321d6260f547

---
+++ platform/arch/arm/armv7m/gcc/syscall/include/svc.h
@@ -0,0 +1,11 @@
+#ifndef SVC_H
+#define SVC_H
+
+#define SVC_cpu_switch_to_usermode     0x01
+
+#define SVC_return_utask               0x02
+
+#define SVC_syscall                    0x03
+
+#endif // SVC_H
+
