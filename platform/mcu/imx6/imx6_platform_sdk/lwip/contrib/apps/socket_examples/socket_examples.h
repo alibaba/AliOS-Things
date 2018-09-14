8e0070d833a60808c98417019a899d9c3c15ee8f
zhuqing <zhuqing.zq@alibaba-inc.com>
2018-09-14 20:29:47 +0800

    BugID: 16935945:delete imx6dq lwip dir and compile ok
    Change-Id: I1b0e55a6b7807f58203db17eb26fbd1587ef2d18

--- platform/mcu/imx6/imx6_platform_sdk/lwip/contrib/apps/socket_examples/socket_examples.h
+++
@@ -1,6 +0,0 @@
-#ifndef __SOCKET_EXAMPLES_H__
-#define __SOCKET_EXAMPLES_H__
-
-void socket_examples_init(void);
-
-#endif /* __SOCKET_EXAMPLES_H__ */
--- platform/mcu/imx6/imx6_platform_sdk/lwip/contrib/ports/win32/check/unistd.h
+++
@@ -1,7 +0,0 @@
-#ifndef __UNISTD_H__
-#define __UNISTD_H__
-
-/* include io.h for read() and write() */
-#include <io.h>
-
-#endif
