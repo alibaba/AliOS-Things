03a46fc8d10c862c0a8e1ba1fdde35455e3ad4aa
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2018-09-15 17:32:14 +0800

    BugID:16976928:fix linkkit_gateway@mk3080 compile error
    Change-Id: Ib4ba0cf4e9e1e0468a8fa8b5ea21f13dd7b22649

--- app/example/linkkit_gateway/linkkit_gateway.mk
+++ app/example/linkkit_gateway/linkkit_gateway.mk
@@ -1,31 +1,25 @@
 NAME := linkkit_gateway
-$(NAME)_SOURCES := app_entry.c
+$(NAME)_SOURCES := linkkit_example_gateway.c \
+                   app_entry.c
+
 
 $(NAME)_COMPONENTS += network/netmgr \
                       middleware/common \
+                      middleware/uagent/uota  \
                       utility/cjson
 
-##ifeq ($(COMPILER),iar)
-###$(NAME)_COMPONENTS += feature.linkkit-gateway-nouota
-##else
 $(NAME)_COMPONENTS += feature.linkkit-gateway
-$(NAME)_INCLUDES   += ../../../middleware/uagent/uota/src/service
-$(NAME)_COMPONENTS += middleware/uagent/uota
-##endif
+
+$(NAME)_INCLUDES += \
+    ../../../middleware/uagent/uota/src/service
 
 GLOBAL_CFLAGS += -DCONFIG_DM_DEVTYPE_GATEWAY  \
                  -DMQTT_DIRECT   \
+                 -DDEPRECATED_LINKKIT
 
 ifeq ($(LWIP),1)
 $(NAME)_COMPONENTS  += protocols.net
 no_with_lwip := 0
-endif
-
-ifeq ($(newapi),1)
-$(NAME)_SOURCES += newapi/gateway.c
-else
-$(NAME)_SOURCES += linkkit_example_gateway.c
-GLOBAL_CFLAGS += -DDEPRECATED_LINKKIT
 endif
 
 ifeq ($(print_heap),1)
@@ -41,8 +35,8 @@ GLOBAL_DEFINES += ESP8266_CHIPSET
 endif
 
 #for test command
-#GLOBAL_CFLAGS += -DLINKKIT_GATEWAY_TEST_CMD
-#$(NAME)_SOURCES += simulate_subdev/testcmd.c simulate_subdev/testcmd_lock.c
+GLOBAL_CFLAGS += -DLINKKIT_GATEWAY_TEST_CMD
+$(NAME)_SOURCES += simulate_subdev/testcmd.c simulate_subdev/testcmd_lock.c
 #end
 
 GLOBAL_INCLUDES += ./
