ae29f9ccb94412053b01dc3c2a925a18e174861f
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-09-17 17:33:51 +0800

    BugID:16846667: Update syscall functions
    	modified:   rhino/syscall/include/syscall.h
    	modified:   rhino/syscall/ksyscall/ksyscall.c
    	modified:   rhino/syscall/usyscall/usyscall.c
    
    Change-Id: I9f95b3bddd42c69200c71bc761730122cbb95f52

--- kernel/rhino/syscall/usyscall/usyscall.c
+++ kernel/rhino/syscall/usyscall/usyscall.c
@@ -5,9 +5,9 @@
 
 #include <arch_syscall.h>
 #include <syscall.h>
-#include <u_task.h>
 
-#if (RHINO_CONFIG_USER_SPACE > 0)
+
+/* ------------------- task ------------------- */
 
 ktask_t *krhino_cur_task_get(void)
 {
@@ -30,36 +30,11 @@ kstat_t krhino_task_del(ktask_t *task)
     return SYSCALL(SYS_KRHINO_TASK_DEL, &arg);
 }
 
-int32_t hal_uart_send(uart_dev_t *uart, const void *data,
-                      uint32_t size, uint32_t timeout)
-{
-    volatile hal_uart_send_syscall_arg_t arg;
-
-    arg.uart = uart;
-    arg.data = data;
-    arg.size = size;
-    arg.timeout = timeout;
-
-    return SYSCALL(SYS_HAL_UART_SEND, &arg);
-}
-
-kstat_t krhino_utask_create(ktask_t *task,
-                            const name_t *name,
-                            void *arg,
-                            uint8_t pri,
-                            tick_t ticks,
-                            cpu_stack_t *ustack_buf,
-                            size_t ustack,
-                            size_t kstack,
-                            task_entry_t entry,
-                            uint8_t autorun);
-
-
-
-kstat_t krhino_utask_create(ktask_t *task, const name_t *name, void *arg,
-                           uint8_t prio, tick_t ticks, cpu_stack_t *stack_buf,
-                           size_t stack_size, size_t kstack_size,
-                           task_entry_t entry, uint8_t autorun)
+kstat_t krhino_utask_create(ktask_t *task, const name_t *name,
+                            void *arg, uint8_t prio, tick_t ticks,
+                            cpu_stack_t *stack_buf,
+                            size_t stack_size, size_t kstack_size,
+                            task_entry_t entry, uint8_t autorun)
 {
     volatile krhino_utask_create_syscall_arg_t syscall_arg;
 
@@ -76,6 +51,268 @@ kstat_t krhino_utask_create(ktask_t *task, const name_t *name, void *arg,
 
     return SYSCALL(SYS_KRHINO_UTASK_CREATE, &syscall_arg);
 }
+
+
+/* ------------------ time ------------------ */
+
+sys_time_t krhino_sys_tick_get(void)
+{
+    return SYSCALL(SYS_KRHINO_SYS_TICK_GET, NULL);
+}
+
+sys_time_t krhino_sys_time_get(void)
+{
+    return SYSCALL(SYS_KRHINO_SYS_TIME_GET, NULL);
+}
+
+tick_t krhino_ms_to_ticks(sys_time_t ms)
+{
+    volatile krhino_ms_to_ticks_syscall_arg_t arg;
+    arg.ms = ms;
+
+    return SYSCALL(SYS_KRHINO_MS_TO_TICKS, &arg);
+}
+
+sys_time_t krhino_ticks_to_ms(tick_t ticks)
+{
+    volatile krhino_ticks_to_ms_syscall_arg_t arg;
+
+    arg.ticks = ticks;
+    return SYSCALL(SYS_KRHINO_TICKS_TO_MS, &arg);
+
+}
+
+
+/* ------------------ mutex ------------------ */
+
+kstat_t krhino_mutex_create(kmutex_t *mutex, const name_t *name)
+{
+    volatile krhino_mutex_create_syscall_arg_t arg;
+
+    arg.mutex = mutex;
+    arg.name = name;
+
+    return SYSCALL(SYS_KRHINO_MUTEX_CREATE, &arg);
+}
+
+kstat_t krhino_mutex_del(kmutex_t *mutex)
+{
+    volatile krhino_mutex_del_syscall_arg_t arg;
+
+    arg.mutex = mutex;
+
+    return SYSCALL(SYS_KRHINO_MUTEX_DEL, &arg);
+}
+
+kstat_t krhino_mutex_lock(kmutex_t *mutex, tick_t ticks)
+{
+    volatile krhino_mutex_lock_syscall_arg_t arg;
+
+    arg.mutex = mutex;
+    arg.ticks = ticks;
+
+    return SYSCALL(SYS_KRHINO_MUTEX_LOCK, &arg);
+}
+
+kstat_t krhino_mutex_unlock(kmutex_t *mutex)
+{
+    volatile krhino_mutex_unlock_syscall_arg_t arg;
+
+    arg.mutex = mutex;
+
+    return SYSCALL(SYS_KRHINO_MUTEX_UNLOCK, &arg);
+}
+
+/* ------------------ semphore ------------------ */
+
+kstat_t krhino_sem_create(ksem_t *sem,
+                          const name_t *name,
+                          sem_count_t count)
+{
+    volatile krhino_sem_create_syscall_arg_t arg;
+
+    arg.sem = sem;
+    arg.name = name;
+    arg.count = count;
+
+    return SYSCALL(SYS_KRHINO_SEM_CREATE, &arg);
+}
+
+kstat_t krhino_sem_del(ksem_t *sem)
+{
+    volatile krhino_sem_del_syscall_arg_t arg;
+
+    arg.sem = sem;
+
+    return SYSCALL(SYS_KRHINO_SEM_DEL, &arg);
+}
+
+
+kstat_t krhino_sem_give(ksem_t *sem)
+{
+    volatile krhino_sem_give_syscall_arg_t arg;
+
+    arg.sem = sem;
+
+    return SYSCALL(SYS_KRHINO_SEM_GIVE, &arg);
+}
+
+
+kstat_t krhino_sem_take(ksem_t *sem, tick_t ticks)
+{
+    volatile krhino_sem_take_syscall_arg_t arg;
+
+    arg.sem = sem;
+    arg.ticks = ticks;
+
+    return SYSCALL(SYS_KRHINO_SEM_TAKE, &arg);
+}
+
+/* ------------------ buf queue -------------------*/
+
+kstat_t krhino_buf_queue_create(kbuf_queue_t *queue,
+                                const name_t *name,
+                                void *buf, size_t size,
+                                size_t max_msg)
+{
+    volatile krhino_buf_queue_create_syscall_arg_t arg;
+
+    arg.queue = queue;
+    arg.name = name;
+    arg.buf = buf;
+    arg.size = size;
+    arg.max_msg = max_msg;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_CREATE, &arg);
+}
+
+kstat_t krhino_buf_queue_del(kbuf_queue_t *queue)
+{
+    volatile krhino_buf_queue_del_syscall_arg_t arg;
+
+    arg.queue = queue;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_DEL, &arg);
+}
+
+kstat_t krhino_buf_queue_dyn_create(kbuf_queue_t **queue,
+                                    const name_t *name,
+                                    size_t size,
+                                    size_t max_msg)
+{
+    volatile krhino_buf_queue_dyn_create_syscall_arg_t arg;
+
+    arg.queue = queue;
+    arg.name = name;
+    arg.size = size;
+    arg.max_msg = max_msg;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_DYN_CREATE, &arg);
+}
+
+kstat_t krhino_buf_queue_dyn_del(kbuf_queue_t *queue)
+{
+    volatile krhino_buf_queue_dyn_del_syscall_arg_t arg;
+
+    arg.queue = queue;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_DYN_DEL, &arg);
+}
+
+kstat_t krhino_buf_queue_send(kbuf_queue_t *queue,
+                              void *msg, size_t size)
+{
+    volatile krhino_buf_queue_send_syscall_arg_t arg;
+
+    arg.queue = queue;
+    arg.msg = msg;
+    arg.size = size;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_SEND, &arg);
+}
+
+kstat_t krhino_buf_queue_recv(kbuf_queue_t *queue,
+                              tick_t ticks, void *msg,
+                              size_t *size)
+{
+    volatile krhino_buf_queue_recv_syscall_arg_t arg;
+
+    arg.queue = queue;
+    arg.ticks = ticks;
+    arg.msg = msg;
+    arg.size = size;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_RECV, &arg);
+}
+
+kstat_t krhino_buf_queue_flush(kbuf_queue_t *queue)
+{
+    volatile krhino_buf_queue_flush_syscall_arg_t arg;
+
+    arg.queue = queue;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_FLUSH, &arg);
+}
+
+kstat_t krhino_buf_queue_info_get(kbuf_queue_t *queue,
+                                  kbuf_queue_info_t *info)
+{
+    volatile krhino_buf_queue_info_get_syscall_arg_t arg;
+
+    arg.queue = queue;
+    arg.info = info;
+
+    return SYSCALL(SYS_KRHINO_BUF_QUEUE_INFO_GET, &arg);
+}
+
+
+/* ------------------ mm ------------------ */
+
+void *krhino_mm_alloc(size_t size)
+{
+    volatile krhino_mm_alloc_syscall_arg_t arg;
+
+    arg.size = size;
+
+    return SYSCALL(SYS_KRHINO_MM_ALLOC, &arg);
+}
+
+void krhino_mm_free(void *ptr)
+{
+    volatile krhino_mm_free_syscall_arg_t arg;
+
+    arg.ptr = ptr;
+
+    return SYSCALL(SYS_KRHINO_MM_FREE, &arg);
+}
+
+void *krhino_mm_realloc(void *oldmem, size_t newsize)
+{
+    volatile krhino_mm_realloc_syscall_arg_t arg;
+
+    arg.oldmem = oldmem;
+    arg.newsize = newsize;
+
+    return SYSCALL(SYS_KRHINO_MM_ALLOC, &arg);
+}
+
+
+/* ---------------- hal uart ---------------- */
+
+int32_t hal_uart_send(uart_dev_t *uart, const void *data,
+                      uint32_t size, uint32_t timeout)
+{
+    volatile hal_uart_send_syscall_arg_t arg;
+
+    arg.uart = uart;
+    arg.data = data;
+    arg.size = size;
+    arg.timeout = timeout;
+
+    return SYSCALL(SYS_HAL_UART_SEND, &arg);
+}
+
+/* ------------------ vfs ------------------ */
 
 off_t aos_lseek(int fd, off_t offset, int whence)
 {
@@ -122,33 +359,4 @@ ssize_t aos_write(int fd,
 
     return SYSCALL(SYS_AOS_WRITE, &arg);
 }
-
-void *krhino_mm_alloc(size_t size)
-{
-    volatile krhino_mm_alloc_syscall_arg_t arg;
-
-    arg.size = size;
-
-    return SYSCALL(SYS_KRHINO_MM_ALLOC, &arg);
-}
-
-void krhino_mm_free(void *ptr)
-{
-    volatile krhino_mm_free_syscall_arg_t arg;
-
-    arg.ptr = ptr;
-
-    return SYSCALL(SYS_KRHINO_MM_FREE, &arg);
-}
-
-void *krhino_mm_realloc(void *oldmem, size_t newsize)
-{
-    volatile krhino_mm_realloc_syscall_arg_t arg;
-
-    arg.oldmem = oldmem;
-    arg.newsize = newsize;
-
-    return SYSCALL(SYS_KRHINO_MM_ALLOC, &arg);
-}
-#endif // RHINO_CONFIG_USER_SPACE
 
