5bbcb725cc6d937e45451cae754de24f357eb4df
chenan.xxw <chenan.xxw@alibaba-inc.com>
2018-09-17 12:03:26 +0800

    BugID:16981187: LD webapp compatible with mobile phone page

--- example/linkdevelop/device_ctrl/webapp/app/pages/home/index.js
+++ example/linkdevelop/device_ctrl/webapp/app/pages/home/index.js
@@ -69,6 +69,10 @@ export default class Home extends Component {
     };
     
     render(){
+        var oMeta = document.createElement('meta');
+        oMeta.name = "viewport";
+        oMeta.content = 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no';
+        document.getElementsByTagName('head')[0].appendChild(oMeta);
         return (
             <div>
                 <span style={{fontSize: "20px"}}>设置开关</span>
--- example/linkdevelop/env_monitor/webapp/app/pages/home/index.js
+++ example/linkdevelop/env_monitor/webapp/app/pages/home/index.js
@@ -61,6 +61,11 @@ export default class Home extends Component {
     };
     
     render(){
+        var oMeta = document.createElement('meta');
+        oMeta.name = "viewport";
+        oMeta.content = 'width=device-width, initial-scale=1, maximum-scale=1, minimum-scale=1, user-scalable=no';
+        document.getElementsByTagName('head')[0].appendChild(oMeta);
+        
         return (
             <div>
                 <div>
