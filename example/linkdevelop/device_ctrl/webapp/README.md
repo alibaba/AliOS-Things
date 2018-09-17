0d6c146f8390688725acaea172acd9eb5a04a34e
chenan.xxw <chenan.xxw@alibaba-inc.com>
2018-09-17 10:22:33 +0800

    [BugID:16981187: add linkdevelop webapp demo]

---
+++ example/linkdevelop/device_ctrl/webapp/README.md
@@ -0,0 +1,52 @@
+## 设备控制 webapp
+
+本webapp配合`example/linkdevelop/device_ctrl`使用，实现`设备端`<==>`LD平台`<==>`webapp`3个通道。
+
+### 目录结构
+
+```
+webapp
+├── app
+│   └── pages
+│       ├── home
+│       │   ├── index.css
+│       │   └── index.js        # 业务相关代码
+│       └── not-found
+│           ├── index.css
+│           └── index.js
+├── app.js
+├── bone-config.json
+├── package.json
+├── public
+│   └── index.html
+└── README.md
+```
+
+### 服务编排
+本webapp需要调用`服务编排`创建的API实现相关逻辑，具体请参考LD平台的[文档中心](https://linkdevelop.aliyun.com).
+
+### 更改api域名地址
+更改`webapp/app/pages/home/index.js`中的`http://api.xiaxiaowen.top`为自己绑定的域名：
+```js
+const config = {
+    domain: 'http://api.xiaxiaowen.top'
+}
+```
+
+### 编译打包
+
+**安装依赖**
+```sh
+bnpm install
+```
+
+**调试**
+```sh
+bone start
+```
+
+**打包**
+```sh
+bone build
+bone pack
+```
---
+++ example/linkdevelop/env_monitor/webapp/app/pages/home/index.js
@@ -0,0 +1,84 @@
+import React, { Component } from "react";
+// 以CSS Modules方式引入Home页样式
+import style from "./index.css";
+
+import { APIGateway } from '@bone/linkdevelop-sdk';
+
+const config = {
+    domain: 'http://api.xiaxiaowen.top'
+}
+
+let xAxisData = [];
+let yAxisTemp = [];
+let yAxisHumi = [];
+for(let i =0 ; i < 60; i++)
+{
+    xAxisData.push(i + 's');
+}
+
+// 导出Home页组件
+export default class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            sensor: {},
+            devices: []
+        };
+    }
+
+    getSensor = () => {
+        APIGateway.request(`${config.domain}/getSensor`, {
+            version: "1.0.0",
+            data: {
+                // 接口参数
+            }
+        }).then(response => {
+            console.log(response);
+            this.setState({
+                sensor: response.data.value
+            })
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    componentDidMount() {
+        setInterval(this.getSensor, 1000);
+
+        APIGateway.request(`${config.domain}/getDevices`, {
+            version: "1.0.0",
+            data: {
+                // 接口参数
+            }
+        }).then(response => {
+            console.log(response);
+            this.setState({
+                devices: response.data.items
+            });
+        }).catch(error => {
+            console.log(error);
+        });
+    };
+    
+    render(){
+        return (
+            <div>
+                <div>
+                    <span style={{fontSize: "20px", marginRight: "10px"}}>设备列表</span>
+                    <br />
+                {
+                    this.state.devices.map((e, idx) => (
+                        <span key={idx} style={{fontSize: "20px", marginRight: "10px"}}>{e.name}</span>
+                        ))
+                }</div>
+                <div>
+                    <span>温度：{this.state.sensor.Temp}</span>
+                    <br />
+                    <span>湿度：{this.state.sensor.Humi}</span>
+                </div>
+                
+            </div>
+        )
+            
+    }
+}
