bb1f9adc58fb79dd36641d17ac0919aa4da6a2bc
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2018-09-18 11:09:19 +0800

    BugID:16729922: [NTP] move ntp to services from awss.
    Change-Id: I627faef4b4285da4268593b0b9dc296c7c54f788

--- middleware/linkkit/sdk-c/src/services/awss/awss.mk
+++ middleware/linkkit/sdk-c/src/services/awss/awss.mk
@@ -9,7 +9,7 @@ $(NAME)_SOURCES :=
 $(NAME)_SOURCES += awss.c           awss_cmp_mqtt.c  awss_report.c   registrar.c   zconfig_protocol.c
 $(NAME)_SOURCES += awss_packet.c    awss_main.c      awss_wifimgr.c  sha256.c      zconfig_utils.c
 $(NAME)_SOURCES += awss_cmp_coap.c  awss_notify.c    enrollee.c      passwd.c      zconfig_ieee80211.c  zconfig_vendor_common.c
-$(NAME)_SOURCES += ntp.c            awss_timer.c
+$(NAME)_SOURCES += awss_timer.c
 $(NAME)_SOURCES += os/os_misc.c
 
 ifeq ($(awss_ble),1)
