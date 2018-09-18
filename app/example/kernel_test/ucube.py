afbb87b0d0dc70ef1dbdf5ba4811245a1edfdeec
yx170385 <yx170385@alibaba-inc.com>
2018-09-18 17:00:03 +0800

    BugID:16850574 :add  kernel test demo
    Change-Id: I4852242b902f5a66f1012ede04b9b290ddd97537

---
+++ app/example/kernel_test/ucube.py
@@ -0,0 +1,7 @@
+src     = Split('''
+        kernel_test.c
+''')
+
+component = aos_component('kernel_test', src)
+component.add_comp_deps('tools/cli', 'kernel/rhino/test')
+component.add_global_macros('AOS_NO_WIFI')
