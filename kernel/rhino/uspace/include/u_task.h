5da2f75c21a21c756a1ae0a0c1ebff19c19a7fe3
lingjun chen <lingjun.clj@alibaba-inc.com>
2018-09-18 20:52:09 -0700

    BugID:16731946:add process support
    Change-Id: I80f4c0305e72a68e47e8cd903de44d7cb4c291cf

--- kernel/rhino/uspace/include/u_task.h
+++ kernel/rhino/uspace/include/u_task.h
@@ -12,5 +12,12 @@ kstat_t krhino_utask_create(ktask_t *task,
                             task_entry_t entry,
                             uint8_t autorun);
 
+
+kstat_t krhino_uprocess_create(ktask_t *task, const name_t *name, void *arg,
+                               uint8_t pri, tick_t ticks, cpu_stack_t *ustack_buf,
+                               size_t ustack, size_t kstack, task_entry_t entry, uint32_t pid,
+                               uint8_t autorun);
+
+
 #endif // U_TASK_H
 
