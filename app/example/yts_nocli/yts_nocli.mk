681e136e6790817bfa14d3c2b65d1f3d915a6726
wosayttn <wosayttn@gmail.com>
2018-09-19 18:22:14 +0800

    Add nano130ke3bn and m487jidae.

---
+++ app/example/yts_nocli/yts_nocli.mk
@@ -0,0 +1,9 @@
+NAME := yts_nocli
+
+#$(NAME)_SOURCES := main.c
+#$(NAME)_COMPONENTS := testcase testcase.certificate_test rhino.test log rhino.vfs yloop hal
+
+$(NAME)_SOURCES := main.c
+$(NAME)_COMPONENTS := testcase rhino.test log rhino.vfs yloop hal
+
+$(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable
