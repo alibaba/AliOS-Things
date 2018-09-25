0e55956e0e0b6a23f2aa4c03ac59601142ddf1b1
Wayne Lin <wosayttn@gmail.com>
2018-09-25 16:15:34 +0800

    Fix yts POSIX issue. Fix build fail using IAR.

--- app/example/yts_nocli/yts_nocli.mk
+++ app/example/yts_nocli/yts_nocli.mk
@@ -4,6 +4,6 @@ NAME := yts_nocli
 #$(NAME)_COMPONENTS := testcase testcase.certificate_test rhino.test log rhino.vfs yloop hal
 
 $(NAME)_SOURCES := main.c
-$(NAME)_COMPONENTS := testcase rhino.test log rhino.vfs yloop hal
+$(NAME)_COMPONENTS := testcase testcase.certificate_test rhino.test log rhino.vfs yloop hal
 
 $(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable
