856a92615a96f5346a90a59daad758b33edc6a6d
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-09-25 10:04:06 +0800

    BugID:16846667:support process & simplify app demo & support tlsf
    Change-Id: I7df25f4de8506dee798baed2ce03e7223d23b9dc

--- kernel/init/include/aos/init.h
+++ kernel/init/include/aos/init.h
@@ -19,6 +19,11 @@ extern int aos_kernel_init(kinit_t *kinit);
 
 #ifdef AOS_BINS
 #include <k_api.h>
+
+#define MAX_APP_BINS   (3)
+
+#define APP_INFO_MAGIC (0xA7FC429E)
+
 struct app_info_t {
 #if (RHINO_CONFIG_USER_SPACE > 0)
      void (*app_entry)(int argc, char *argv[]);
@@ -47,10 +52,12 @@ struct framework_info_t {
 
 struct m_app_info_t {
 #if (RHINO_CONFIG_USER_SPACE > 0)
-     void (*app_entry)(int argc, char *argv[]);
+     void (*app_entry)(int pid ,int argc, char *argv[]);
 #else
      void (*app_entry)(void *ksyscall_tbl, int argc, char *argv[]);
 #endif
+     unsigned int text_flash_begin;
+     unsigned int text_flash_end;
      unsigned int data_ram_start;
      unsigned int data_ram_end;
      unsigned int data_flash_begin;
@@ -59,7 +66,7 @@ struct m_app_info_t {
      unsigned int heap_start;
      unsigned int heap_end;
      /* reserve for other */
-     unsigned int reserve0;
+     unsigned int magic;
      unsigned int reserve1;
      /* this bin_type must be here, 0x28, uniform with single bin & kernel bin,
         arm cortex not used */
