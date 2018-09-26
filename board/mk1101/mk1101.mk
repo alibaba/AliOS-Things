0811eaa610333fb070414ce89436639c4d579236
yx170385 <yx170385@alibaba-inc.com>
2018-09-26 16:43:00 +0800

    BugID:17081466: cflags about mfloat-abi check
    Change-Id: I40437ed600743527fee195037b62464a2c5e0be7

--- board/mk1101/mk1101.mk
+++ board/mk1101/mk1101.mk
@@ -25,7 +25,7 @@ GLOBAL_LDFLAGS  += -L $(SOURCE_ROOT)/board/mx1101
 # Global defines
 # HSE_VALUE = STM32 crystal frequency = 26MHz (needed to make UART work correctly)
 GLOBAL_DEFINES += $$(if $$(NO_CRLF_STDIO_REPLACEMENT),,CRLF_STDIO_REPLACEMENT)
-GLOBAL_CFLAGS  += -mcpu=cortex-m3 -mthumb -mfloat-abi=soft
+GLOBAL_CFLAGS  += -mcpu=cortex-m3 -mthumb
 
 WIFI_FIRMWARE_SECTOR_START    := 2      #0x2000
 FILESYSTEM_IMAGE_SECTOR_START := 256    #0x100000
