643555504564e368fa0748c91ff4909c71a1b6c1
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-09-28 16:56:08 +0800

    BugID:16846667:add two userspace app demo
    	new file:   example/uapp1/k_app_config.h
    	new file:   example/uapp1/src/app.c
    	new file:   example/uapp1/system/app_mm.c
    	new file:   example/uapp1/system/app_mm.h
    	new file:   example/uapp1/system/entry.c
    	new file:   example/uapp1/test/buf_queue_test.c
    	new file:   example/uapp1/test/mutex_test.c
    	new file:   example/uapp1/test/semphore_test.c
    	new file:   example/uapp1/test/syscall_test.h
    	new file:   example/uapp1/test/test.c
    	new file:   example/uapp1/test/time_test.c
    	new file:   example/uapp1/uapp1.mk
    	new file:   example/uapp2/k_app_config.h
    	new file:   example/uapp2/src/app.c
    	new file:   example/uapp2/system/app_mm.c
    	new file:   example/uapp2/system/app_mm.h
    	new file:   example/uapp2/system/entry.c
    	new file:   example/uapp2/test/buf_queue_test.c
    	new file:   example/uapp2/test/mutex_test.c
    	new file:   example/uapp2/test/semphore_test.c
    	new file:   example/uapp2/test/syscall_test.h
    	new file:   example/uapp2/test/test.c
    	new file:   example/uapp2/test/time_test.c
    	new file:   example/uapp2/uapp2.mk
    
    Change-Id: Ia69aefed205f73d72955479076ca5c70c17e0049

---
+++ app/example/uapp2/test/test.c
@@ -0,0 +1,28 @@
+#include <syscall_test.h>
+
+/* run function @f, compare the return value with @e */
+#define RUN_TEST_CASE(f, e) \
+    do { \
+        if (!f) { \
+            printf("invalid func %s\r\n", #f); \
+        } else { \
+            int ret; \
+            printf("------------------------\r\n"); \
+            ret = f(); \
+            if (ret == e) { \
+                printf("%s pass!\r\n", #f); \
+            } else { \
+                printf("%s fail!, ret code 0x%x\r\n", \
+                       #f, ret); \
+            } \
+            printf("------------------------\r\n\r\n"); \
+        } \
+    } while (0)
+
+void run_test_case(void)
+{
+    RUN_TEST_CASE(mutex_test, 0);
+    RUN_TEST_CASE(semphore_test, 0);
+    RUN_TEST_CASE(time_test, 0);
+    RUN_TEST_CASE(buf_queue_test, 0);
+}
