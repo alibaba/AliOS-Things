42d3cba230901c67f256766bcb40d94c85db1028
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-09-28 16:56:08 +0800

    BugID:16846667:add two userspace app demo
    	new file:   example/uapp1/k_app_config.h
    	new file:   example/uapp1/src/app.c
    	new file:   example/uapp1/system/app_mm.c
    	new file:   example/uapp1/system/app_mm.h
    	new file:   example/uapp1/system/entry.c
    	new file:   example/uapp1/test/buf_queue_test.c
    	new file:   example/uapp1/test/mutex_test.c
    	new file:   example/uapp1/test/semphore_test.c
    	new file:   example/uapp1/test/syscall_test.h
    	new file:   example/uapp1/test/test.c
    	new file:   example/uapp1/test/time_test.c
    	new file:   example/uapp1/uapp1.mk
    	new file:   example/uapp2/k_app_config.h
    	new file:   example/uapp2/src/app.c
    	new file:   example/uapp2/system/app_mm.c
    	new file:   example/uapp2/system/app_mm.h
    	new file:   example/uapp2/system/entry.c
    	new file:   example/uapp2/test/buf_queue_test.c
    	new file:   example/uapp2/test/mutex_test.c
    	new file:   example/uapp2/test/semphore_test.c
    	new file:   example/uapp2/test/syscall_test.h
    	new file:   example/uapp2/test/test.c
    	new file:   example/uapp2/test/time_test.c
    	new file:   example/uapp2/uapp2.mk
    
    Change-Id: Ia69aefed205f73d72955479076ca5c70c17e0049

---
+++ app/example/uapp2/system/app_mm.h
@@ -0,0 +1,13 @@
+#ifndef APP_MM_H
+#define APP_MM_H
+
+
+void* app_mm_init(void *mem, size_t size);
+
+void* app_mm_malloc(size_t size);
+
+void* app_mm_realloc(void *ptr, size_t size);
+
+void app_mm_free(void *ptr);
+
+#endif // APP_MM_H
