0d8bdd5fc11e7b4df8cf04a63ba3f62997cfb320
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2018-09-28 19:49:11 +0800

    BugID:16966632: add some test case fot at mqtt client
    Change-Id: Ib2b9b72119883d4db1815eec774ac5d480d74966

--- app/example/atmqttapp/atmqttapp.c
+++ app/example/atmqttapp/atmqttapp.c
@@ -3,33 +3,47 @@
  */
 
 #include <aos/aos.h>
+#include <stdio.h>
+#include <string.h>
 #include "at_mqtt_ica_client.h"
+#include "at_mqtt_client.h"
+
 
 #define PRODUCT_KEY     "a1kecMbd2yb"
 #define DEVICE_NAME     "mqtttest"
 #define DEVICE_SECRET   "aWp2N4ObAk8SZfJ81SSpfArZz16jVG45"
-#define MQTT_SUB_TOPIC  "\"\/a1kecMbd2yb\/mqtttest\/get\""
-#define MQTT_PUB_TOPIC  "\"\/a1kecMbd2yb\/mqtttest\/update\""
-#define MQTT_PUB_MSG    "abcdefg"
+#define MQTT_SUB_TOPIC  "/a1kecMbd2yb/mqtttest/get"
+#define MQTT_PUB_TOPIC  "/a1kecMbd2yb/mqtttest/update"
+#define MQTT_PUB_MSG    "{\"\"id\"\":\"\"a1D3oxFDwQJ_333333_mid\"\",\"\"params\"\":{\"\"_sys_device_mid\"\":\"\"example.demo.module-id\"\",\"\"_sys_device_pid\"\":\"\"example.demo.partner-id\"\"}}"
 
+extern int sal_init(void);
 static void app_delayed_action(void *arg)
 {
-    LOG("helloworld %s:%d %s\r\n", __func__, __LINE__, aos_task_name());
+    char read_topic[128];
+    char read_msg[512];
+
+    if (at_mqtt_read_msg(read_topic, read_msg) == 0) {
+
+        printf("read topic: %s\r\n", read_topic);
+
+        printf("read msg_data: %s\r\n", read_msg);
+    }
+
     aos_post_delayed_action(5000, app_delayed_action, NULL);
 }
 
 int application_start(int argc, char *argv[])
 {
-    char mqtt_topic[128];
-    char mqtt_pub_msg[128];
-    uint8_t mqtt_qos;
-    int  mqtt_packet_id;
-    int  mqtt_status;
-    int  ret;
+    char        mqtt_topic[128];
+    char        mqtt_pub_msg[128];
+    uint8_t     mqtt_qos;
+    int         mqtt_packet_id;
+    int         mqtt_status;
+    int         ret;
 
     sal_init();
 
-    LOG("application started.");
+    aos_msleep(1000);
 
     // init the ica client module
     ret = at_mqtt_ica_client_init();
@@ -50,7 +64,7 @@ int application_start(int argc, char *argv[])
     // set topic info and qos
     memset(mqtt_topic, 0, 128);
     memcpy(mqtt_topic, MQTT_SUB_TOPIC, 128);
-    mqtt_qos = 2;
+    mqtt_qos = 1;
 
     aos_msleep(1000);
 
