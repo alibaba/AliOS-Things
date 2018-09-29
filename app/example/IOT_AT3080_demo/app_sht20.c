d31153a195cebf58f5a829633bc2ff07f843c591
NashZhangMCHP <Nash.Zhang@microchip>
2018-09-29 13:45:17 +0800

    SAML21: clean up code.

--- app/example/IOT_AT3080_demo/app_sht20.c
+++ app/example/IOT_AT3080_demo/app_sht20.c
@@ -1,3 +1,27 @@
+/**
+ * Copyright (c) 2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * \page License
+ *
+ * Subject to your compliance with these terms, you may use Microchip
+ * software and any derivatives exclusively with Microchip products.
+ * It is your responsibility to comply with third party license terms applicable
+ * to your use of third party software (including open source software) that
+ * may accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+ * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+ * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+ * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+ * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+ * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
 #include "mx_debug.h"
 #include "mx_common.h"
 #include "alicloud_sds.h"
@@ -24,10 +48,10 @@ mx_status SHT20_task_init(void)
 
 	sprintf(sensor_display[0], "Temp: %.2f C", temperature);
 	sprintf(sensor_display[1], "Humi: %.2f %%", humidity);
-	
+
 	OLED_ShowString(OLED_DISPLAY_COLUMN_START, OLED_DISPLAY_ROW_2, sensor_display[0]);
 	OLED_ShowString(OLED_DISPLAY_COLUMN_START, OLED_DISPLAY_ROW_3, sensor_display[1]);
-	
+
 	attr.name = "CurrentTemperature_1";
 	attr.type = ALI_ATT_TYPE_FLOAT;
 	attr.read_func = handle_read_cur_temperature;
@@ -39,7 +63,7 @@ mx_status SHT20_task_init(void)
 	attr.read_func = handle_read_cur_humidity;
 	attr.write_func = NULL;
 	alisds_attr_init(ALI_HANDLE_CURRENT_HUMIDITY, attr);
-	
+
 	return kNoErr;
 }
 
@@ -50,13 +74,13 @@ void SHT20_task(void)
 		humidity = SHT2x_GetHumiPoll();
 		sprintf(sensor_display[0], "Temp: %.2f C", temperature);
 		sprintf(sensor_display[1], "Humi: %.2f %%", humidity);
-		
+
 		OLED_ShowString(OLED_DISPLAY_COLUMN_START, OLED_DISPLAY_ROW_2, sensor_display[0]);
 		OLED_ShowString(OLED_DISPLAY_COLUMN_START, OLED_DISPLAY_ROW_3, sensor_display[1]);
 
 		alisds_attr_indicate_by_handle(ALI_HANDLE_CURRENT_TEMPERATURE);
 		alisds_attr_indicate_by_handle(ALI_HANDLE_CURRENT_HUMIDITY);
-		
+
 		latest_sensor_read_tick = mx_hal_ms_ticker_read();
 	}
 }
