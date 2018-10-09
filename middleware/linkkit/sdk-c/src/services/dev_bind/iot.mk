43fc2bebf9ac9e69f490c843b6cefe346549aeee
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2018-10-09 19:52:35 +0800

    BugID:17161893: Move all coap (local & cloud) to directory of coap.
    Change-Id: I227e91555c3c3acd3d4f7141e90dfc591e21d10d

--- middleware/linkkit/sdk-c/src/services/dev_bind/iot.mk
+++ middleware/linkkit/sdk-c/src/services/dev_bind/iot.mk
@@ -2,10 +2,6 @@ LIBA_TARGET     := libiot_bind.a
 
 HDR_REFS        := src/infra
 HDR_REFS        += src/protocol/mqtt
-HDR_REFS        += src/protocol/coap_local
+HDR_REFS        += src/protocol/coap/local
 HDR_REFS        += src/services/awss
 HDR_REFS        += src/services/linkkit/dev_reset
-
-ifeq (,$(filter -DALCS_ENABLED,$(CFLAGS)))
-LIB_SRCS_EXCLUDE    := awss_cmp_coap.c
-endif
