7ffe292edafa6515c2ca63d6af7a5a34a8c4c37e
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2018-10-09 18:00:57 +0800

    BugID:17289944: Split CoAPServer and ALCS
    Change-Id: I1630272537852871d1c206ee9f3925c643d41539

--- middleware/linkkit/sdk-c/src/protocol/alcs/alcs.mk
+++ middleware/linkkit/sdk-c/src/protocol/alcs/alcs.mk
@@ -1,21 +1,12 @@
 NAME := libiot_alcs
 
-$(NAME)_SOURCES := ./CoAPDeserialize.c \
-./alcs_coap.c \
-./CoAPResource.c \
-./CoAPNetwork.c \
-./CoAPPlatform.c \
-./alcs_mqtt.c \
-./alcs_api.c \
-./CoAPObserve.c \
-./CoAPServer.c \
-./CoAPMessage.c \
-./alcs_adapter.c \
-./alcs_server.c \
-./CoAPExport.c \
-./alcs_client.c \
-./CoAPSerialize.c \
- 
+$(NAME)_SOURCES := ./alcs_coap.c \
+                   ./alcs_mqtt.c \
+                   ./alcs_api.c \
+                   ./alcs_adapter.c \
+                   ./alcs_server.c \
+                   ./alcs_client.c \
+
 $(NAME)_COMPONENTS := middleware/linkkit/sdk-c/src/infra/utils \
 middleware/linkkit/sdk-c/src/infra/log \
 middleware/linkkit/sdk-c/src/infra/system \
@@ -23,12 +14,13 @@ middleware/linkkit/sdk-c/src/sdk-impl \
 
 
 $(NAME)_INCLUDES := ../../../src/infra \
-../../../src/infra/log \
-../../../src/infra/utils \
-../../../src/infra/utils/misc \
-../../../src/infra/utils/digest \
-../../../src/infra/system \
-../../../src/protocol/mqtt \
-../../../src/protocol/mqtt/client \
-../../../src/protocol/mqtt/MQTTPacket \
+                    ../../../src/infra/log \
+                    ../../../src/infra/utils \
+                    ../../../src/infra/utils/misc \
+                    ../../../src/infra/utils/digest \
+                    ../../../src/infra/system \
+                    ../../../src/protocol/coap_local \
+                    ../../../src/protocol/mqtt \
+                    ../../../src/protocol/mqtt/client \
+                    ../../../src/protocol/mqtt/MQTTPacket \
 
--- middleware/linkkit/sdk-c/src/services/awss/iot.mk
+++ middleware/linkkit/sdk-c/src/services/awss/iot.mk
@@ -2,5 +2,5 @@ LIBA_TARGET     := libiot_awss.a
 
 HDR_REFS        := src/infra
 HDR_REFS        += src/protocol/mqtt
-HDR_REFS        += src/protocol/alcs
+HDR_REFS        += src/protocol/coap_alcs
 HDR_REFS        += src/services/dev_bind
