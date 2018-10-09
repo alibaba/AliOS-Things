4d1caa609690dc4668b2f83ba08474741097d739
PreCheck <AliOS-CI@alibaba-inc.com>
2018-10-10 02:34:42 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: I86dde24e97f63314f5ae3d6b7f7287fee4b7a962

---
+++ board/bk7231udevkitc/ucube.py
@@ -0,0 +1,1 @@
+linux_only_targets="blink ldapp linkkitapp cryptotest otaapp uDataapp linuxapp athostapp vflashdemo coapapp acapp netmgrapp helloworld helloworld_nocli tls linkkit_gateway nano kernel_test networkapp mqttapp yts wifihalapp hdlcapp.hdlcserver"
--- board/mk3080/ucube.py
+++ board/mk3080/ucube.py
@@ -1,3 +1,3 @@
 
 supported_targets="alinkapp helloworld meshapp networkapp acapp linkkitapp mqttapp coapapp linkkit_gateway linkkit_sched"
-linux_only_targets="id2_app wifimonitor hdlcapp.hdlcserver sst_app nano helloworld_nocli tls linuxapp uDataapp blink vflashdemo netmgrapp itls_app wifihalapp prov_app athostapp"
+linux_only_targets="id2_app wifimonitor hdlcapp.hdlcserver sst_app nano helloworld_nocli tls linuxapp uDataapp blink vflashdemo netmgrapp itls_app wifihalapp prov_app athostapp yts kernel_test ldapp otaapp cryptotest"
