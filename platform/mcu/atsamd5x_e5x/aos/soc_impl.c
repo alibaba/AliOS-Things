d7675d05a1f577296180a208436943698e879c93
NashZhangMCHP <41287931+NashZhangMCHP@users.noreply.github.com>
2018-10-11 11:11:15 +0800

    Update SAME54 STDIO support for AOS2.0.0 regression with fixed uart port 0 for STDIO. (#633)
    2. Enable 'upload' in aos-studio for SAML21 with Atmel SAMICE.
    3. Clean up code.

--- platform/mcu/atsamd5x_e5x/aos/soc_impl.c
+++ platform/mcu/atsamd5x_e5x/aos/soc_impl.c
@@ -1,5 +1,26 @@
-/*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+/**
+ * Copyright (c) 2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * \page License
+ *
+ * Subject to your compliance with these terms, you may use Microchip
+ * software and any derivatives exclusively with Microchip products.
+ * It is your responsibility to comply with third party license terms applicable
+ * to your use of third party software (including open source software) that
+ * may accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+ * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+ * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+ * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+ * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+ * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
  */
 
 #include <k_api.h>
@@ -15,7 +36,6 @@ void soc_hw_timer_init(void)
 hr_timer_t soc_hr_hw_cnt_get(void)
 {
     return 0;
-    //return *(volatile uint64_t *)0xc0000120;
 }
 
 lr_timer_t soc_lr_hw_cnt_get(void)
@@ -65,7 +85,7 @@ int           g_region_num = 1;
 void aos_heap_set()
 {
     g_mm_region[0].start = (uint8_t*)&Image$$RW_IRAM1$$ZI$$Limit;
-    g_mm_region[0].len   = 
+    g_mm_region[0].len   =
         (g_iram1_start + g_iram1_total_size - (size_t)&Image$$RW_IRAM1$$ZI$$Limit);
     printf("g_mm_region[0].start is 0x%x, g_mm_region[0].len is 0x%x \r\n", (size_t)g_mm_region[0].start, g_mm_region[0].len);
 }
@@ -91,7 +111,7 @@ int           g_region_num = 1;
 void aos_heap_set()
 {
     g_mm_region[0].start = (uint8_t*)&__bss_end__;
-    g_mm_region[0].len   = 
+    g_mm_region[0].len   =
         ((uint8_t*)&_estack - (uint8_t*)&__bss_end__) - RHINO_CONFIG_SYSTEM_STACK_SIZE;
 }
 #endif
@@ -147,7 +167,7 @@ static void soc_print_stack()
 void soc_err_proc(kstat_t err)
 {
     (void)err;
-    
+
     #if (RHINO_CONFIG_TASK_STACK_CUR_CHECK > 0)
     soc_print_stack();
     #endif
