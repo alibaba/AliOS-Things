8528084ad0fa77c6a4b17892043e9c7ba9d62ee6
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-10-11 20:23:31 +0800

    BugID:16967023:[coap] remove passing unused CFLAGS
    Change-Id: I070a61ae306d1d4100d31f769f665c661f1c8f39
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/protocol/coap/local/CoAPPlatform.h
+++ middleware/linkkit/sdk-c/src/protocol/coap/local/CoAPPlatform.h
@@ -8,55 +8,12 @@
 #ifndef __COAP_PLATFORM_OS_H__
 #define __COAP_PLATFORM_OS_H__
 #include <stdio.h>
-#ifdef COAP_USE_PLATFORM_MEMORY
 #include "iotx_utils.h"
-#endif
 #include "iotx_log.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif /* __cplusplus */
-
-#if 0
-
-#ifdef COAP_USE_PLATFORM_MEMORY
-#define coap_malloc(size) LITE_malloc(size, MEM_MAGIC, "CoAP")
-#else
-#define coap_malloc malloc
-#endif
-
-#ifdef COAP_USE_PLATFORM_MEMORY
-#define coap_free LITE_free
-#else
-#define coap_free free
-#endif
-
-#ifdef COAP_USE_PLATFORM_LOG
-#define COAP_TRC   log_debug
-#define COAP_DUMP  log_debug
-#define COAP_DEBUG log_debug
-#define COAP_INFO  log_info
-#define COAP_WARN  log_warning
-#define COAP_ERR   log_err
-#else
-#define coap_log(level, ...) \
-    {\
-    fprintf(stderr, "\r\n[%s LINE #%d]   ", __FILE__, __LINE__); \
-    fprintf(stderr, __VA_ARGS__);\
-    }
-
-#define coap_dump(...)\
-{\
-    fprintf(stderr, __VA_ARGS__);\
-}
-
-#define COAP_DUMP(fmt, args, ...)  coap_log("<DUMP>",  fmt, ##__VA_ARGS__)
-#define COAP_TRC(fmt,  args, ...)  coap_log("<TRACE>", fmt, ##__VA_ARGS__)
-#define COAP_DEBUG(fmt,args, ...)  coap_log("<DEBUG>", fmt, ##__VA_ARGS__)
-#define COAP_INFO(fmt, args, ...)  coap_log("<INFO> ", fmt, ##__VA_ARGS__)
-#define COAP_ERR(fmt,  args, ...)  coap_log("<ERROR>", fmt, ##__VA_ARGS__)
-#endif
-#endif
 
 #define coap_malloc(size)       HAL_Malloc(size)
 #define coap_free(ptr)          HAL_Free(ptr)
