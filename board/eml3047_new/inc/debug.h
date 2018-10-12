44be8b8cdd6cf838bca047831c018e9a2f8dc6d5
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2018-10-12 17:16:14 +0800

    BugID:17395982: LoRaWAN upgrade add board support
    Change-Id: Ic989d9aabacf97fe8416d4c3f2fac5580dabeb33

---
+++ board/eml3047_new/inc/debug.h
@@ -0,0 +1,115 @@
+/******************************************************************************
+ * @file    debug.h
+ * @author  MCD Application Team
+ * @version V1.1.1
+ * @date    01-June-2017
+ * @brief   Header for driver debug.c module
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics International N.V.
+ * All rights reserved.</center></h2>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted, provided that the following conditions are met:
+ *
+ * 1. Redistribution of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of STMicroelectronics nor the names of other
+ *    contributors to this software may be used to endorse or promote products
+ *    derived from this software without specific written permission.
+ * 4. This software, including modifications and/or derivative works of this
+ *    software, must execute solely and exclusively on microcontroller or
+ *    microprocessor devices manufactured by or for STMicroelectronics.
+ * 5. Redistribution and use of this software other than as permitted under
+ *    this license is void and will automatically terminate your rights under
+ *    this license.
+ *
+ * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+ * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
+ * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __DEBUG_H__
+#define __DEBUG_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include <string.h>
+#include <stdio.h>
+#include "hw_conf.h"
+#include "vcom.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+/* Exported macros -----------------------------------------------------------*/
+#ifdef LORA_DEBUG
+
+#define DBG_GPIO_WRITE(gpio, n, x)  HW_GPIO_Write(gpio, n, (GPIO_PinState)(x))
+#define DBG_GPIO_SET(gpio, n)       gpio->BSRR = n
+#define DBG_GPIO_RST(gpio, n)       gpio->BRR = n
+#define DBG_RTC_OUTPUT LL_RTC_ALARMOUT_DISABLE /* LL_RTC_ALARMOUT_ALMA on PC13 */
+#define DBG(x)  do { x } while (0)
+
+#ifdef LORA_TRACE
+#include "hw_rtc.h"
+
+#define DBG_PRINTF(format, ...)    do {DBG_Send("[%d][%s:%4d] " format "", HW_RTC_GetTimerValue(), __FILE__, __LINE__, ##__VA_ARGS__);}while(0==1)//DBG_Send(__VA_ARGS__)//vcom_Send(__VA_ARGS__)
+
+#define LOG_PRINTF(format, ...) do {DBG_Send(format, HW_RTC_GetTimerValue(), ##__VA_ARGS__);}while(0==1)
+
+#define DBG_PRINTF_CRITICAL(...)  
+
+#else /*TRACE*/
+#define DBG_PRINTF(...)
+
+#define DBG_PRINTF_CRITICAL(...) 
+#endif /*TRACE*/
+
+#else /* DEBUG */
+
+#define DBG_GPIO_WRITE(gpio, n, x)
+#define DBG_GPIO_SET(gpio, n)
+#define DBG_GPIO_RST(gpio, n)
+#define DBG(x) do {  } while (0)
+#define DBG_PRINTF(...)
+#define DBG_PRINTF_CRITICAL(...) 
+#define DBG_RTC_OUTPUT LL_RTC_ALARMOUT_DISABLE
+
+#endif /* DEBUG */
+
+/* Exported functions ------------------------------------------------------- */
+
+/**
+ * @brief  Initializes the debug
+ * @param  None
+ * @retval None
+ */
+void DBG_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __DEBUG_H__*/
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ board/eml3047_new/inc/hw_spi.h
@@ -0,0 +1,134 @@
+/*
+ / _____)             _              | |
+( (____  _____ ____ _| |_ _____  ____| |__
+ \____ \| ___ |    (_   _) ___ |/ ___)  _ \
+ _____) ) ____| | | || |_| ____( (___| | | |
+(______/|_____)_|_|_| \__)_____)\____)_| |_|
+    (C)2013 Semtech
+
+Description: Header for driver hw spi module
+
+License: Revised BSD License, see LICENSE.TXT file include in the project
+
+Maintainer: Miguel Luis and Gregory Cristian
+*/
+/******************************************************************************
+ * @file    hw_spi.h
+ * @author  MCD Application Team
+ * @version V1.1.1
+ * @date    01-June-2017
+ * @brief   Header for driver hw_spi.c module
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2017 STMicroelectronics International N.V.
+ * All rights reserved.</center></h2>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted, provided that the following conditions are met:
+ *
+ * 1. Redistribution of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 3. Neither the name of STMicroelectronics nor the names of other
+ *    contributors to this software may be used to endorse or promote products
+ *    derived from this software without specific written permission.
+ * 4. This software, including modifications and/or derivative works of this
+ *    software, must execute solely and exclusively on microcontroller or
+ *    microprocessor devices manufactured by or for STMicroelectronics.
+ * 5. Redistribution and use of this software other than as permitted under
+ *    this license is void and will automatically terminate your rights under
+ *    this license.
+ *
+ * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
+ * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+ * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT
+ * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+ * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ******************************************************************************
+ */
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __HW_SPI_H__
+#define __HW_SPI_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "hw_conf.h"
+
+/* Exported types ------------------------------------------------------------*/
+/* Exported constants --------------------------------------------------------*/
+/* External variables --------------------------------------------------------*/
+/* Exported macros -----------------------------------------------------------*/
+/* Exported functions ------------------------------------------------------- */
+
+/**
+ * @brief Initializes the SPI object and MCU peripheral
+ *
+ * @param [IN] None
+ * @retval None
+ */
+void HW_SPI_Init(void);
+
+/**
+ * @brief De-initializes the SPI object and MCU peripheral
+ *
+ * @param  [IN] None
+ * @retval None
+ */
+void HW_SPI_DeInit(void);
+
+/**
+ * @brief Initializes the SPI IOs
+ *
+ * @param  [IN] None
+ * @retval None
+ */
+void HW_SPI_IoInit(void);
+
+/**
+ * @brief De-initializes the SPI IOs
+ *
+ * @param  [IN] None
+ * @retval None
+ */
+void HW_SPI_IoDeInit(void);
+
+/**
+ * @brief Sends outData and receives inData
+ *
+ * @param  [IN] outData Byte to be sent
+ * @retval inData      Received byte.
+ * @note This is an implementation of
+ *    HAL_SPI_TransmitReceive(&hspi, (uint8_t *)&txData, (uint8_t*)&rxData, 1, HAL_MAX_DELAY);
+ */
+uint16_t HW_SPI_InOut(uint16_t outData);
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* __HW_SPI_H__ */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
+
+
+
+
+
+
+
+
