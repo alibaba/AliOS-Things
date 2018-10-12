ca8b479a3ff364a40fa63effa85986e217fa424a
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-10-13 06:50:25 +0800

    BugID:16967023:[tools] add Post_Env hook to dump FEATURE_XXX settings when make env
    Change-Id: If20ade31e8920c6607dbbf118bdb127e281b5ac8
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/tools/default_settings.mk
+++ middleware/linkkit/sdk-c/src/tools/default_settings.mk
@@ -1,4 +1,5 @@
 DEFAULT_BLD             ?= $(CONFIG_DIR)/config.ubuntu.x86
+POST_ENV_HOOK           := Post_Env
 POST_FINAL_OUT_HOOK     := Post_Distro
 SUBDIRS                 := directory-not-exist-actually
 UTEST_PROG              := sdk-testsuites
@@ -6,17 +7,7 @@ ifeq (Darwin,$(shell uname))
 POST_FINAL_OUT_HOOK     :=
 endif
 
-FEATURE_MQTT_SHADOW             ?= n
-FEATURE_ALCS_ENABLED            ?= y
-FEATURE_OTA_ENABLED             ?= y
-FEATURE_MQTT_COMM_ENABLED       ?= n
-FEATURE_HTTP_COMM_ENABLED       ?= n
-FEATURE_HTTP2_COMM_ENABLED      ?= n
-FEATURE_SUBDEVICE_ENABLED       ?= n
-FEATURE_DEVICE_MODEL_GATEWAY    ?= n
-FEATURE_COAP_COMM_ENABLED       ?= n
 FEATURE_DEPRECATED_LINKKIT      ?= y
-FEATURE_DEV_BIND_ENABLED        ?= y
 
 CONFIG_LIB_EXPORT               ?= static
 
