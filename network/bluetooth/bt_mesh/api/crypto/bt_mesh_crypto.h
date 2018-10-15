09e4bab2bd0e63261add2872ed02fdb5713039be
Xiao Ma <xiaoma.mx@alibaba-inc.com>
2018-10-15 11:01:53 +0800

    bugID:17412849: add bt mesh crypto apis support.
    Change-Id: I6bbb269135fba60a0beee16fc671bf0bb97e6698

---
+++ network/bluetooth/bt_mesh/api/crypto/bt_mesh_crypto.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+
+#ifndef BT_MESH_CRYPTO_H
+#define BT_MESH_CRYPTO_H
+
+#include <bluetooth/bluetooth.h>
+
+#include "crypto.h"
+#include "ecc.h"
+
+/** @brief Generate random data.
+ *
+ *  A random number generation helper which utilizes the Bluetooth
+ *  controller's own RNG.
+ *
+ *  @param buf Buffer to insert the random data
+ *  @param len Length of random data to generate
+ *
+ *  @return Zero on success or error code otherwise, positive in case
+ *  of protocol error or negative (POSIX) in case of stack internal error
+ */
+int bt_mesh_rand(void *buf, size_t len);
+
+ /*big-endian*/
+int bt_mesh_aes_encrypt(const u8_t key[16], const u8_t plaintext[16],
+                        u8_t enc_data[16]);
+
+int bt_mesh_aes_cmac(const u8_t key[16], struct bt_mesh_sg *sg,
+                      size_t sg_len, u8_t mac[16]);
+
+/*  @brief Get the current Public Key.
+ *
+ *  Get the current ECC Public Key.
+ *
+ *  @return Current key, or NULL if not available.
+ */
+const u8_t *bt_mesh_pub_key_get(void);
+
+/*  @brief Calculate a DH Key from a remote Public Key.
+ *
+ *  Calculate a DH Key from the remote Public Key.
+ *
+ *  @param remote_pk Remote Public Key.
+ *  @param cb Callback to notify the calculated key.
+ *
+ *  @return Zero on success or negative error code otherwise
+ */
+int bt_mesh_dh_key_gen(const u8_t remote_pk[64], bt_dh_key_cb_t cb);
+
+/*  @brief Generate a new Public Key.
+ *
+ *  Generate a new ECC Public Key. The callback will persist even after the
+ *  key has been generated, and will be used to notify of new generation
+ *  processes (NULL as key).
+ *
+ *  @param cb Callback to notify the new key, or NULL to request an update
+ *            without registering any new callback.
+ *
+ *  @return Zero on success or negative error code otherwise
+ */
+int bt_mesh_pub_key_gen(struct bt_pub_key_cb *cb);
+
+#endif //BT_MESH_CRYPTO_H
