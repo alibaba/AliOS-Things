82ac47f85b25ed1f98d00233428d73a137dc7021
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2018-10-15 17:31:53 +0800

    BugID:16908783: Add switch for different types of awss in sdk.
    Change-Id: I07142a64ed8eeddfaecc8eabde0909518e29ef2b

--- middleware/linkkit/sdk-c/src/services/awss/iot.mk
+++ middleware/linkkit/sdk-c/src/services/awss/iot.mk
@@ -5,3 +5,9 @@ HDR_REFS        += src/protocol/mqtt
 HDR_REFS        += src/protocol/coap_alcs
 HDR_REFS        += src/services/dev_bind
 HDR_REFS        += src/sdk-impl
+
+CFLAGS          += -DAWSS_SUPPORT_SMARTCONFIG \
+                   -DAWSS_SUPPORT_SMARTCONFIG_WPS \
+                   -DAWSS_SUPPORT_AHA \
+                   -DAWSS_SUPPORT_ADHA \
+                   -DAWSS_SUPPORT_APLIST
