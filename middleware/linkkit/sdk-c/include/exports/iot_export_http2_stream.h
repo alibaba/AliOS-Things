152e98e74c286be39646603ccf1544bd9d6f0cc7
zhangtian <dongren.zt@alibaba-inc.com>
2018-10-17 11:25:44 +0800

    Bugid:16796279:http2_stream:uncoupling service and protocol
    Change-Id: Ibd16c204071f3c6d53f0bbcd307cc7261637c36f
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/include/exports/iot_export_http2_stream.h
+++ middleware/linkkit/sdk-c/include/exports/iot_export_http2_stream.h
@@ -19,8 +19,6 @@ extern "C" {
 
 #include "iotx_log.h"
 
-// #define fsupload_emerg(...)    log_emerg("fsup", __VA_ARGS__)
-// #define fsupload_crit(...)     log_crit("fsup", __VA_ARGS__)
 #define h2stream_err(...)      log_err("h2stream", __VA_ARGS__)
 #define h2stream_warning(...)  log_warning("h2stream", __VA_ARGS__)
 #define h2stream_info(...)     log_info("h2stream", __VA_ARGS__)
@@ -52,15 +50,58 @@ typedef struct {
     uint32_t            packet_len;         /* one packet length */
     char               *identify;           /* path string to identify a stream service */
     stream_type_t       stream_type;        /* check @stream_type_t */
-    char               *stream_id;          /* string return by server to identify a specific stream pipe, different from stream identifier which is a field in HTTP2 frame */
+    char
+    *stream_id;          /* string return by server to identify a specific stream pipe, different from stream identifier which is a field in HTTP2 frame */
 } stream_data_info_t;
 
-http2_connection_t *IOT_HTTP2_Stream_Connect( device_conn_info_t *conn_info,http2_user_cb_t *user_cb);
-int IOT_HTTP2_Stream_Open(http2_connection_t *connection, stream_data_info_t *info, header_ext_info_t *header);
-int IOT_HTTP2_Stream_Send(http2_connection_t *connection, stream_data_info_t *info);
-int IOT_HTTP2_Stream_Query(http2_connection_t *connection, stream_data_info_t *info);
-int IOT_HTTP2_Stream_Close(http2_connection_t *connection, stream_data_info_t *info);
-int IOT_HTTP2_Stream_Ping(http2_connection_t *connection);
+typedef void (*on_stream_header_callback)(char *stream_id, int cat, const uint8_t *name, size_t namelen,
+        const uint8_t *value, size_t valuelen, uint8_t flags);
+
+typedef void (*on_stream_chunk_recv_callback)(char *stream_id,
+        const uint8_t *data, size_t len, uint8_t flags);
+
+typedef void (*on_stream_close_callback)(char *stream_id, uint32_t error_code);
+
+typedef void (*on_stream_frame_send_callback)(char *stream_id, int type, uint8_t flags);
+
+typedef void (*on_stream_frame_recv_callback)(char *stream_id, int type, uint8_t flags);
+
+typedef struct {
+    on_stream_header_callback       on_stream_header_cb;
+    on_stream_chunk_recv_callback   on_stream_chunk_recv_cb;
+    on_stream_close_callback        on_stream_close_cb;
+    on_stream_frame_send_callback   on_stream_frame_send_cb;
+    on_stream_frame_recv_callback   on_stream_frame_recv_cb;
+} http2_stream_cb_t;
+
+typedef struct {
+    http2_connection_t   *http2_connect;
+    void                 *mutex;
+    void                 *semaphore;
+    void                 *thread_handle;
+    http2_list_t         stream_list;
+    int                  init_state;
+    http2_stream_cb_t    *cbs;
+} stream_handle_t;
+
+typedef struct {
+    unsigned int stream_id;         /* http2 protocol stream id */
+    char *app_stream_id;            /* string return by server to identify a specific stream pipe, different from stream identifier which is a field in http2 frame */
+    int stream_type;                /* upstream or downstream */
+    void *semaphore;                /* semaphore for http2 response sync */
+    char status_code[4];            /* http2 response status code */
+    http2_list_t list;              /* list_head */
+} http2_stream_node_t;
+
+
+
+stream_handle_t *IOT_HTTP2_Stream_Connect(device_conn_info_t *conn_info, http2_stream_cb_t *user_cb);
+int IOT_HTTP2_Stream_Open(stream_handle_t *handle, stream_data_info_t *info, header_ext_info_t *header);
+int IOT_HTTP2_Stream_Send(stream_handle_t *handle, stream_data_info_t *info);
+int IOT_HTTP2_Stream_Query(stream_handle_t *handle, stream_data_info_t *info);
+int IOT_HTTP2_Stream_Close(stream_handle_t *handle, stream_data_info_t *info);
+int IOT_HTTP2_Stream_Disconnect(stream_handle_t *handle);
+int IOT_HTTP2_Stream_Ping(stream_handle_t *handle);
 #ifdef __cplusplus
 }
 #endif
