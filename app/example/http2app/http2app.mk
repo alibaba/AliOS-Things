0432943d33665a5022bed4fab86696ed3a3b0a61
zhangtian <dongren.zt@alibaba-inc.com>
2018-10-17 21:34:37 +0800

    bugid:16796279:add http2 example
    Change-Id: Ib6aead33064684bb15cd785456b867aa92f0355e

---
+++ app/example/http2app/http2app.mk
@@ -0,0 +1,22 @@
+NAME := http2app
+$(NAME)_SOURCES := app_entry.c   http2_example_stream.c
+
+$(NAME)_COMPONENTS += feature.linkkit-http2 \
+                      network/netmgr \
+                      middleware/common \
+                      utility/cjson \
+                      tools/cli
+
+
+GLOBAL_DEFINES += CONFIG_AOS_CLI ON_DAILY
+
+
+ifeq ($(LWIP),1)
+$(NAME)_COMPONENTS  += protocols.net
+no_with_lwip := 0
+endif
+
+
+$(NAME)_INCLUDES += ./
+
+GLOBAL_DEFINES += CONFIG_DM_DEVTYPE_SINGLE  HTTP2_RECV_BUFFER_LENGHT=4096
