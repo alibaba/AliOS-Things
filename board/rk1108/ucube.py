c8e22331ecccf50e0047bf84cb22e68fc7e64960
PreCheck <AliOS-CI@alibaba-inc.com>
2018-10-19 02:37:26 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: I521c194012dd56d692e65fec663d517ee93c0ae3

--- board/rk1108/ucube.py
+++ board/rk1108/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="helloworld_nocli coapapp netmgrapp yts alinkapp tls hdlcapp.hdlcserver linuxapp nano meshapp kernel_test networkapp wifihalapp helloworld cryptotest"
+linux_only_targets="helloworld_nocli coapapp netmgrapp yts alinkapp tls hdlcapp.hdlcserver linuxapp nano meshapp kernel_test networkapp wifihalapp helloworld cryptotest http2app"
