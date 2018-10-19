9b0bd5b1bf7b3e5963d0a2e781d2755efaca1aab
yajun.syj <yajun.syj@alibaba-inc.com>
2018-10-19 13:48:48 +0800

    BugID:16966632:remove atmqttapp module
    Change-Id: I11a59537e5497c3523d48c744c085b89ff7eed38

--- app/example/atmqttapp/ucube.py
+++
@@ -1,7 +0,0 @@
-src     = Split('''
-        atmqttapp.c
-''')
-
-component = aos_component('atmqttapp', src)
-component.add_comp_deps('kernel/yloop', 'tools/cli')
-component.add_global_macros('AOS_NO_WIFI')
