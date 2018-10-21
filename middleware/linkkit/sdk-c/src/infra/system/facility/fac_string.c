19929e94697b8e1dc4714c3db5964456e381839e
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-10-21 08:03:57 +0800

    BugID:16967023:[mqtt] print downstream JSON messages as well as upstream
    Change-Id: I43448863ba4a8200f41c53bf597c7838f5fdfcfb
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/infra/system/facility/fac_string.c
+++ middleware/linkkit/sdk-c/src/infra/system/facility/fac_string.c
@@ -4,28 +4,54 @@
 
 #include "iotx_system_internal.h"
 
-#define JSON_INDENTATION            "    "
+#define JSON_NEWLINE            "\r\n"
+#define JSON_INDENT             "    "
 
-int iotx_facility_format_json(const char *str, char *outstr, int outlen)
+#define JSON_PRINT_NEWSTR       HAL_Printf("%s", newstr);
+#define JSON_PRINT_NEWLINE      \
+    do { \
+        HAL_Printf("%s", JSON_NEWLINE); \
+        if (mark == '>' || mark == '<' || mark == ':') { \
+            HAL_Printf("%c ", mark); \
+        } \
+    } while(0)
+
+int iotx_facility_json_print(const char *str, int level, ...)
 {
-    int length = 0;
-    char newstr[2];
-    char *INDENT = JSON_INDENTATION;
-    int quoted  = 0;
-    int escaped = 0;
-    int indent  = 0;
-    int i = 0, j = 0;
+    int             length = 0;
+    char            newstr[2];
+    int             quoted  = 0;
+    int             escaped = 0;
+    int             indent  = 0;
+    int             i = 0, j = 0;
+    int             curr_level = LITE_get_loglevel();
+    int             res = -1;
+    lite_cjson_t    lite;
+    va_list         ap;
+    int             mark = ' ';
 
     newstr[0] = 0x00;
     newstr[1] = 0x00;
+
+    if (curr_level < level) {
+        return 1;
+    }
 
     if (str == NULL || strlen(str) == 0) {
         return -1;
     }
 
-    length = strlen(str);
+    res = lite_cjson_parse(str, strlen(str), &lite);
+    if (res != SUCCESS_RETURN || !lite_cjson_is_object(&lite)) {
+        return -2;
+    }
 
-    memset(outstr, 0, outlen);
+    length = strlen(str);
+    HAL_Printf("%s%s", "\033", lvl_color[level]);
+    va_start(ap, level);
+    mark = va_arg(ap, int);
+    JSON_PRINT_NEWLINE;
+    va_end(ap);
 
     for (i = 0 ; i < length ; i++) {
         char ch = str[i];
@@ -33,15 +59,16 @@ int iotx_facility_format_json(const char *str, char *outstr, int outlen)
             case '{':
             case '[':
                 newstr[0] = ch;
-                strncat(outstr, newstr, outlen - strlen(outstr));
+                JSON_PRINT_NEWSTR;
+
                 if (!quoted) {
-                    strncat(outstr, "\n", outlen - strlen(outstr));
+                    JSON_PRINT_NEWLINE;
 
                     if (!(str[i + 1] == '}' || str[i + 1] == ']')) {
                         ++indent;
 
                         for (j = 0 ; j < indent ; j++) {
-                            strncat(outstr, INDENT, outlen - strlen(outstr));
+                            HAL_Printf("%s", JSON_INDENT);
                         }
                     }
                 }
@@ -52,27 +79,27 @@ int iotx_facility_format_json(const char *str, char *outstr, int outlen)
             case ']':
                 if (!quoted) {
                     if ((i > 0) && (!(str[i - 1] == '{' || str[i - 1] == '['))) {
-                        strncat(outstr, "\n", outlen - strlen(outstr));
+                        JSON_PRINT_NEWLINE;
                         --indent;
 
                         for (j = 0 ; j < indent ; j++) {
-                            strncat(outstr, INDENT, outlen - strlen(outstr));
+                            HAL_Printf("%s", JSON_INDENT);
                         }
                     } else if ((i > 0) && ((str[i - 1] == '[' && ch == ']') || (str[i - 1] == '{' && ch == '}'))) {
                         for (j = 0 ; j < indent ; j++) {
-                            strncat(outstr, INDENT, outlen - strlen(outstr));
+                            HAL_Printf("%s", JSON_INDENT);
                         }
                     }
                 }
 
                 newstr[0] = ch;
-                strncat(outstr, newstr, outlen - strlen(outstr));
+                JSON_PRINT_NEWSTR;
 
                 break;
 
             case '"':
                 newstr[0] = ch;
-                strncat(outstr, newstr, outlen - strlen(outstr));
+                JSON_PRINT_NEWSTR;
                 escaped = 1;
 
                 if (i > 0 && str[i - 1] == '\\') {
@@ -87,12 +114,12 @@ int iotx_facility_format_json(const char *str, char *outstr, int outlen)
 
             case ',':
                 newstr[0] = ch;
-                strncat(outstr, newstr, outlen - strlen(outstr));
+                JSON_PRINT_NEWSTR;
                 if (!quoted) {
-                    strncat(outstr, "\n", outlen - strlen(outstr));
+                    JSON_PRINT_NEWLINE;
 
                     for (j = 0 ; j < indent ; j++) {
-                        strncat(outstr, INDENT, outlen - strlen(outstr));
+                        HAL_Printf("%s", JSON_INDENT);
                     }
                 }
 
@@ -100,20 +127,23 @@ int iotx_facility_format_json(const char *str, char *outstr, int outlen)
 
             case ':':
                 newstr[0] = ch;
-                strncat(outstr, newstr, outlen - strlen(outstr));
+                JSON_PRINT_NEWSTR;
                 if (!quoted) {
-                    strncat(outstr, "", outlen - strlen(outstr));
+                    HAL_Printf("%s", " ");
                 }
 
                 break;
 
             default:
                 newstr[0] = ch;
-                strncat(outstr, newstr, outlen - strlen(outstr));
+                JSON_PRINT_NEWSTR;
 
                 break;
         }
     }
 
+    HAL_Printf("%s", JSON_NEWLINE JSON_NEWLINE);
+    HAL_Printf("%s", "\033[0m");
     return 0;
 }
+
