be765c30d11d47e4b66685317d9a5e8d4ab1bea9
zhangtian <dongren.zt@alibaba-inc.com>
2018-10-22 20:07:22 +0800

    bugid:16796279:[http2_example]add file upload example for linkkit sdk
    Change-Id: Icaafb1d98bfa34cb15168b58cc1218abd7ed5ef5

---
+++ app/example/http2app/http2_example_uploadfile.c
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include "iot_export.h"
+#include "iot_export_http2_stream.h"
+#include "app_entry.h"
+#include "utils_httpc.h"
+
+#define  ON_DAILY
+#if defined(ON_DAILY)
+	#define HTTP2_ONLINE_SERVER_URL       "10.101.12.205"
+	#define HTTP2_ONLINE_SERVER_PORT      9999
+	#define HTTP2_PRODUCT_KEY             "a1L5EUOh21s"
+	#define HTTP2_DEVICE_NAME             "zhangmei_test01"
+	#define HTTP2_DEVICE_SECRET           "KQCftfEDCx35LChyEwZoYY6FCYidTOp0"
+#elif defined(ON_PRE)
+	#define HTTP2_ONLINE_SERVER_URL       "100.67.141.158/"
+	#define HTTP2_ONLINE_SERVER_PORT      8443
+	#define HTTP2_PRODUCT_KEY             "b1XVhqfan1X"
+	#define HTTP2_DEVICE_NAME             "YvhjziEQmKusCFUgRpeo"
+	#define HTTP2_DEVICE_SECRET           "QjkhCrAX0SbNWgKpIamuiDdLkk23Q1r7"
+#else
+	#define HTTP2_ONLINE_SERVER_URL       NULL
+	#define HTTP2_ONLINE_SERVER_PORT      443
+	#define HTTP2_PRODUCT_KEY             "DM5b8zbTWJs"
+	#define HTTP2_DEVICE_NAME             "mydevice1"
+	#define HTTP2_DEVICE_SECRET           "q4tiwQuICYfr6JQ8aUFjWxocuXJ5ruEx"
+#endif
+
+#define FILE_NAME "test.tar.gz"
+
+#define EXAMPLE_TRACE(fmt, ...)                        \
+    do {                                               \
+        HAL_Printf("%s|%03d :: ", __func__, __LINE__); \
+        HAL_Printf(fmt, ##__VA_ARGS__);                \
+        HAL_Printf("%s", "\r\n");                      \
+    } while (0)
+
+
+static int upload_end = 0;
+static void on_header(char *stream_id,int cat,const uint8_t *name,size_t namelen, 
+                              const uint8_t *value,size_t valuelen, uint8_t flags)
+{
+    EXAMPLE_TRACE("~~~~~name = %s namelen=%d ,value =%s ,valuelen =%d flag =%d\n", name,namelen,value,valuelen,flags);
+}
+
+static void on_chunk_recv(char *stream_id,const uint8_t *data, size_t len,uint8_t flags)
+{
+     EXAMPLE_TRACE("~~~~~stream_id = %s data=%s ,len =%d flag =%d\n", stream_id,data,len,flags);
+}
+static void on_stream_close(char *stream_id,uint32_t error_code)
+{
+     EXAMPLE_TRACE("~~~~~stream_id = %s error_code =%d\n", stream_id,error_code);
+}
+
+static http2_stream_cb_t my_cb = {
+    .on_stream_header_cb = on_header,
+    .on_stream_chunk_recv_cb = on_chunk_recv,
+    .on_stream_close_cb = on_stream_close,
+};
+
+void upload_file_result(const char * path,int result, void * user_data)
+{
+    EXAMPLE_TRACE("===========path = %s,result =%d=========", path,result);
+    upload_end =1;
+}
+
+static int http2_stream_test()
+{
+    int ret;
+    device_conn_info_t conn_info;
+    stream_handle_t *handle;
+    memset(&conn_info, 0, sizeof( device_conn_info_t));
+    conn_info.product_key = HTTP2_PRODUCT_KEY;
+    conn_info.device_name = HTTP2_DEVICE_NAME;
+    conn_info.device_secret = HTTP2_DEVICE_SECRET;
+    conn_info.url = HTTP2_ONLINE_SERVER_URL;
+    conn_info.port = HTTP2_ONLINE_SERVER_PORT;
+
+    handle = IOT_HTTP2_Stream_Connect(&conn_info,&my_cb);
+    if(handle == NULL) {
+        return -1;
+    }
+    
+    ret = IOT_HTTP2_Stream_UploadFile(handle,FILE_NAME,"iotx/vision/voice/intercom/live",NULL, 
+                                upload_file_result, NULL);
+    if(ret < 0) {
+        return -1;
+    }                        
+    while(!upload_end) {
+        HAL_SleepMs(200);
+    }
+    ret = IOT_HTTP2_Stream_Disconnect(handle);
+    EXAMPLE_TRACE("close connect %d\n",ret);
+    return 0;
+}
+
+int linkkit_main(void *paras)
+{
+    int ret;
+
+    IOT_SetLogLevel(IOT_LOG_DEBUG);
+
+    ret = http2_stream_test();
+    return ret;
+}
