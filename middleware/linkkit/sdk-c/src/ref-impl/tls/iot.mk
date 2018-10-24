9b25e10c965f65fe0abdbfdc16cbd17c3caa03a5
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-10-24 17:29:41 +0800

    BugID:16967023:[ref-impl/tls] fix missing DLL_API_EXPORTS definition in CMakeLists.txt
    Change-Id: Idc8a0c00b28c536c390e551dbd5f4bb7eb25237d
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/ref-impl/tls/iot.mk
+++ middleware/linkkit/sdk-c/src/ref-impl/tls/iot.mk
@@ -3,3 +3,8 @@ CFLAGS      := $(filter-out -ansi,$(CFLAGS))
 HDR_REFS    += src/infra
 
 LIB_SRC_PATTERN := *.c */*.c
+
+define Extra_CMake_Head
+    echo 'ADD_DEFINITIONS (-DDLL_API_EXPORTS)' $(1)
+    echo '' $(1)
+endef
