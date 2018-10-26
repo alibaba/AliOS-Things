bc1df9e84edc9dbe11e30572df9b1effcf5affa0
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-10-26 14:35:27 +0800

    BugID:16846667: Remove privileged mode code from app
    	modified:   example/uapp1/src/app.c
    	modified:   example/uapp1/system/entry.c
    	modified:   example/uapp1/test/mutex_test.c
    	modified:   example/uapp2/src/app.c
    	modified:   example/uapp2/system/entry.c
    	modified:   example/uapp2/uapp2.mk
    
    Change-Id: I98e2defa16f488f05723979fde76c6720ddf5e83

--- app/example/uapp2/system/entry.c
+++ app/example/uapp2/system/entry.c
@@ -4,8 +4,8 @@
 
 #include <aos/aos.h>
 #include "hal/soc/soc.h"
-#include <app_mm.h>
 #include <uapp.h>
+#include <app_mm.h>
 
 extern unsigned int _app_text_flash_begin;
 extern unsigned int _app_text_flash_end;
@@ -17,19 +17,15 @@ extern unsigned int _app_bss_end;
 extern unsigned int _app_heap_start;
 extern unsigned int _app_heap_end;
 extern unsigned int app_type;
-extern int application_start(int argc, char **argv);
 
 #define UTASK_STACK_SIZE 0x200
 
-static ktask_t utask_obj;
-cpu_stack_t utask_ubuf[UTASK_STACK_SIZE];
+static cpu_stack_t app_stack[UTASK_STACK_SIZE * sizeof(cpu_stack_t)];
+static ktask_t app_task_struct;
 
-static void utask_run(void *arg)
-{
-    application_start(0, NULL);
-}
+extern int application_start(int argc, char **argv);
 
-static void app_entry(int id, int argc, char *argv[])
+static void app_entry(int argc, char *argv[])
 {
     ktask_t *cur;
     unsigned int mm_start;
@@ -39,21 +35,17 @@ static void app_entry(int id, int argc, char *argv[])
     mm_start = &_app_heap_start;
     mm_size = (size_t)&_app_heap_end - (size_t)&_app_heap_start;
     if (NULL == app_mm_init(mm_start, mm_size)) {
-        goto out;
+        return;
     }
 
     // Note: printf doesn't work in app untill app_mm_init is done
 
-    krhino_uprocess_create(&utask_obj, "utask", 0, AOS_DEFAULT_APP_PRI,
-        (tick_t)0, utask_ubuf, UTASK_STACK_SIZE, UTASK_STACK_SIZE,
-        (task_entry_t)utask_run, id, 1);
-
-out:
-    cur = krhino_cur_task_get();
-    krhino_task_del(cur);
+    application_start(argc, argv);
 }
 
 __attribute__ ((used, section(".app_info"))) uapp_info_t app_info = {
+    APP_INFO_MAGIC,
+    &app_type,
     app_entry,
     &_app_text_flash_begin,
     &_app_text_flash_end,
@@ -64,8 +56,10 @@ __attribute__ ((used, section(".app_info"))) uapp_info_t app_info = {
     &_app_bss_end,
     &_app_heap_start,
     &_app_heap_end,
-    APP_INFO_MAGIC,
-    0x0,
-    &app_type
+    &app_task_struct,
+    app_stack,
+    UTASK_STACK_SIZE,
+    AOS_DEFAULT_APP_PRI,
+    0, 0, 0, 0
 };
 
