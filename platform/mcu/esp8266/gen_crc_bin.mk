2701a8fe29c8076f7171934354ff07804e7eddf3
zixun.lyq <zixun.lyq@alibaba-inc.com>
2018-10-29 19:55:18 +0800

    BugID:17511352:[OTA] generate common MD5 bin
    1) remove cmds from platform
    2) generate common MD5 bin in build aos_target_build.mk
    
    Change-Id: I6f99711af076cda43e048db7fad2c504a5370c91

--- platform/mcu/esp8266/gen_crc_bin.mk
+++ platform/mcu/esp8266/gen_crc_bin.mk
@@ -1,20 +1,13 @@
 EXTRA_POST_BUILD_TARGETS += gen_crc_bin
 OTA_BIN_OUTPUT_FILE := $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINTYPE_LOWER)$(BIN_OUTPUT_SUFFIX)
-
-ifeq ($(HOST_OS),Win32)
-	SETENV = set
-else
-	SETENV = export
-endif
-
 gen_crc_bin:
-	cd $(OUTPUT_DIR)/binary/ && \
-	$(OBJCOPY) --only-section .text -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.text.bin && \
-	$(OBJCOPY) --only-section .data -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.data.bin && \
-    $(OBJCOPY) --only-section .rodata -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.rodata.bin && \
-    $(OBJCOPY) --only-section .irom0.text -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.irom0text.bin && \
-	$(SETENV) ESP8266_NM=${NM} && \
-    python ../../../platform/mcu/esp8266/tools/gen_appbin.py $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) 2 1 15 5 && \
-	$(CP) eagle.app.flash.bin $(OTA_BIN_OUTPUT_FILE) && \
-	$(RM) eagle.app.flash.bin eagle.app.v6.text.bin eagle.app.v6.data.bin eagle.app.v6.rodata.bin eagle.app.v6.irom0text.bin && \
-	python ../../../$(SCRIPTS_PATH)/ota_gen_md5_bin.py $(OTA_BIN_OUTPUT_FILE)
+	cd $(OUTPUT_DIR)/binary/; \
+	$(OBJCOPY) --only-section .text -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.text.bin; \
+	$(OBJCOPY) --only-section .data -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.data.bin; \
+    $(OBJCOPY) --only-section .rodata -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.rodata.bin; \
+    $(OBJCOPY) --only-section .irom0.text -O binary $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) eagle.app.v6.irom0text.bin; \
+	export ESP8266_NM=${NM}; \
+    python2 ../../../platform/mcu/esp8266/tools/gen_appbin.py $(CLEANED_BUILD_STRING)$(RADIXPOINT)$(BINSTYPE_LOWER)$(LINK_OUTPUT_SUFFIX) 2 1 15 5; \
+	unset ESP8266_NM; \
+	mv eagle.app.flash.bin $(OTA_BIN_OUTPUT_FILE); \
+	rm eagle.app.v6.text.bin eagle.app.v6.data.bin eagle.app.v6.rodata.bin eagle.app.v6.irom0text.bin; \
