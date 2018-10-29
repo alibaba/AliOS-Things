2190a227f603ef218bbd82a12868adda0f5538b4
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2018-10-29 19:57:39 +0800

    bugID:17132047:WHiteScan fix#17132047, fix#17132001, fix#17131992.
    Change-Id: I71129d637a59ff88b7513ce35e23ab1e1ee3dc0e

--- network/bluetooth/bt/controller/hal/rand.h
+++ network/bluetooth/bt/controller/hal/rand.h
@@ -13,8 +13,8 @@
 
 void rand_init(u8_t *context, u8_t context_len, u8_t threshold);
 void rand_isr_init(u8_t *context, u8_t context_len, u8_t threshold);
-size_t rand_get(size_t octets, u8_t *rand);
-size_t rand_isr_get(size_t octets, u8_t *rand);
+size_t rand_get(u8_t octets, u8_t *rand);
+size_t rand_isr_get(u8_t octets, u8_t *rand);
 void isr_rand(void *param);
 
 #endif /* _RAND_H_ */
