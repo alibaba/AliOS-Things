f0115c4b75151aacb1faa6888da1b540d66cdd9f
jiaoqu.yf <jiaoqu.yf@alibaba-inc.com>
2018-10-29 11:36:32 +0800

    BugID:17131004:remove yloop from udata example
    Change-Id: I2fba23b9b6b345bb2c62b728f379ef13d1f0c280

--- app/example/udataapp/udata_example.c
+++ app/example/udataapp/udata_example.c
@@ -298,106 +298,6 @@ do{\
     UDATA_PRINT("uData_application:::::::::timestamp = (%d)\n", (unsigned int)(TIME)); \
 }while(0);
 
-#ifdef UDATA_YLOOP
-
-void uData_report_demo(input_event_t *event, void *priv_data)
-{
-    udata_pkg_t buf;
-    if ((event == NULL) || (event->type != EV_UDATA)) {
-        return;
-    }
-
-    if (event->code == CODE_UDATA_REPORT_PUBLISH) {
-        int ret = 0;
-        ret = uData_report_publish(event, &buf);
-        if (ret != 0) {
-            return;
-        }
-        switch (buf.type) {
-
-            case UDATA_SERVICE_ACC: {
-                accel_data_t *acc = (accel_data_t *)buf.payload;
-                UDATA_SHOW_UINT_3(buf.type, (uint32_t)acc->timestamp, acc->data[0], acc->data[1], acc->data[2]);
-                break;
-            }
-
-
-            case UDATA_SERVICE_MAG: {
-                mag_data_t *mag = (mag_data_t *)buf.payload;
-                UDATA_SHOW_UINT_3(buf.type, (uint32_t)mag->timestamp, mag->data[0], mag->data[1], mag->data[2]);
-                break;
-            }
-
-            case UDATA_SERVICE_GYRO: {
-                gyro_data_t *gyro = (gyro_data_t *)buf.payload;
-                UDATA_SHOW_UINT_3(buf.type, (uint32_t)gyro->timestamp, gyro->data[0], gyro->data[1], gyro->data[2]);
-                break;
-            }
-
-            case UDATA_SERVICE_ALS: {
-                als_data_t *als = (als_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, als->timestamp, als->lux);
-                break;
-            }
-
-            case UDATA_SERVICE_PS: {
-                proximity_data_t *ps = (proximity_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, ps->timestamp, ps->present);
-                break;
-            }
-
-            case UDATA_SERVICE_BARO: {
-                barometer_data_t *baro = (barometer_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, baro->timestamp, baro->p);
-                break;
-            }
-
-            case UDATA_SERVICE_TEMP: {
-                temperature_data_t *temp = (temperature_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, temp->timestamp, temp->t);
-                break;
-            }
-
-            case UDATA_SERVICE_UV: {
-                uv_data_t *uv = (uv_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, uv->timestamp, uv->uvi);
-                break;
-            }
-
-            case UDATA_SERVICE_HUMI: {
-                humidity_data_t *humi = (humidity_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, humi->timestamp, humi->h);
-                break;
-            }
-
-            case UDATA_SERVICE_HALL: {
-                hall_data_t *hall = (hall_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, hall->timestamp, hall->hall_level);
-                break;
-            }
-            case UDATA_SERVICE_HR: {
-                heart_rate_data_t *heart = (heart_rate_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, heart->timestamp, heart->hear_rate);
-                break;
-            }
-
-            case UDATA_SERVICE_GPS: {
-                gps_data_t *gps = (gps_data_t *)buf.payload;
-                UDATA_SHOW_FLOAT_3(buf.type, (uint32_t)gps->timestamp, gps->lat, gps->lon, gps->elv);
-                break;
-            }
-
-
-            default:
-                break;
-
-        }
-
-    }
-}
-
-
-#else
 
 void uData_report_demo(sensor_msg_pkg_t *msg)
 {
@@ -496,14 +396,9 @@ void uData_report_demo(sensor_msg_pkg_t *msg)
 }
 
 
-#endif
 int udata_sample(void)
 {
     int ret = 0;
-#ifdef UDATA_YLOOP
-
-    aos_register_event_filter(EV_UDATA, uData_report_demo, NULL);
-#else
     ret = uData_register_msg_handler(uData_report_demo);
     LOG("uData_queue_registerslot service_dtc_handle ret=%d\n", ret);
     if (ret == -1) {
@@ -511,13 +406,11 @@ int udata_sample(void)
         return ret;
     }
 
-#endif
     ret = uData_subscribe(UDATA_SERVICE_TEMP);
     if (ret != 0) {
         LOG("%s %s %s %d\n", uDATA_STR, __func__, ERROR_LINE, __LINE__);
         return -1;
     }
-
     return 0;
 }
 
