8955c550b399a4c8fcd8679c0e9e6a5082c927e0
zixun.lyq <zixun.lyq@alibaba-inc.com>
2018-10-29 19:55:18 +0800

    BugID:17511352:[OTA] generate common MD5 bin
    1) remove cmds from platform
    2) generate common MD5 bin in build aos_target_build.mk
    
    Change-Id: I6f99711af076cda43e048db7fad2c504a5370c91

--- platform/mcu/stm32l4xx_cube/gen_crc_bin.mk
+++ platform/mcu/stm32l4xx_cube/gen_crc_bin.mk
@@ -1,5 +1,4 @@
 OTA_BIN_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=$(BIN_OUTPUT_SUFFIX))
 EXTRA_POST_BUILD_TARGETS += gen_crc_bin
 gen_crc_bin:
-	python $(SCRIPTS_PATH)/ota_gen_md5_bin.py $(OTA_BIN_OUTPUT_FILE)
 	python $(SCRIPTS_PATH)/st_mcu_bin_pack_tool.py 0x80000 0x4000 $(SOURCE_ROOT)/platform/mcu/$(HOST_MCU_FAMILY)/l496_bootloader.bin $(OTA_BIN_OUTPUT_FILE)
