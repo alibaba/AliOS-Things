a06c635ccecad9f96303f33405d2f3aca2515c23
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-10-30 15:01:45 +0800

    BugID:17613246:[SAL][STM32L412] wifi ssid pwd can not be stored after connected
    Change-Id: I32375700f927116272042fe92b6bc1533605e104

--- board/stm32f412zg-nucleo/aos/board_partition.c
+++ board/stm32f412zg-nucleo/aos/board_partition.c
@@ -6,53 +6,17 @@ hal_logic_partition_t hal_partitions[HAL_PARTITION_MAX];
 
 void board_init(void)
 {
-    hal_partitions[HAL_PARTITION_BOOTLOADER].partition_owner             = HAL_FLASH_EMBEDDED;
-    hal_partitions[HAL_PARTITION_BOOTLOADER].partition_description       = "Bootloader";
-    hal_partitions[HAL_PARTITION_BOOTLOADER].partition_start_addr        = 0x08000000;
-    hal_partitions[HAL_PARTITION_BOOTLOADER].partition_length            = 0x4000; //16k bytes
-    hal_partitions[HAL_PARTITION_BOOTLOADER].partition_options           = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
-
     hal_partitions[HAL_PARTITION_APPLICATION].partition_owner            = HAL_FLASH_EMBEDDED;
     hal_partitions[HAL_PARTITION_APPLICATION].partition_description      = "Application";
-    hal_partitions[HAL_PARTITION_APPLICATION].partition_start_addr       = 0x08004000;
-    hal_partitions[HAL_PARTITION_APPLICATION].partition_length           = 0x5C000; //368k bytes
+    hal_partitions[HAL_PARTITION_APPLICATION].partition_start_addr       = 0x08000000;
+    hal_partitions[HAL_PARTITION_APPLICATION].partition_length           = 0xC0000; //768k bytes
     hal_partitions[HAL_PARTITION_APPLICATION].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
-
-    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_owner               = HAL_FLASH_EMBEDDED;
-    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_description         = "OTA Storage";
-    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_start_addr          = 0x08084000;
-    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_length              = 0x5C000; //368k bytes
-    hal_partitions[HAL_PARTITION_OTA_TEMP].partition_options             = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
-
-    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_owner            = HAL_FLASH_EMBEDDED;
-    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_description      = "PARAMETER1";
-    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_start_addr       = 0x08060000;
-    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_length           = 0x1000; // 4k bytes
-    hal_partitions[HAL_PARTITION_PARAMETER_1].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 
     hal_partitions[HAL_PARTITION_PARAMETER_2].partition_owner            = HAL_FLASH_EMBEDDED;
     hal_partitions[HAL_PARTITION_PARAMETER_2].partition_description      = "PARAMETER2";
-    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_start_addr       = 0x08061000;
-    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_length           = 0x2000; //8k bytes
+    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_start_addr       = 0x080C0000;
+    hal_partitions[HAL_PARTITION_PARAMETER_2].partition_length           = 0x40000; //128 * 2k bytes
     hal_partitions[HAL_PARTITION_PARAMETER_2].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
-
-    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_owner            = HAL_FLASH_EMBEDDED;
-    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_description      = "PARAMETER3";
-    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_start_addr       = 0x08063000;
-    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_length           = 0x1000; //4k bytes
-    hal_partitions[HAL_PARTITION_PARAMETER_3].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
-
-    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_owner            = HAL_FLASH_EMBEDDED;
-    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_description      = "PARAMETER4";
-    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_start_addr       = 0x08064000;
-    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_length           = 0x1000; //4k bytes
-    hal_partitions[HAL_PARTITION_PARAMETER_4].partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
-
-    hal_partitions[HAL_PARTITION_SPIFFS].partition_owner                 = HAL_FLASH_EMBEDDED;
-    hal_partitions[HAL_PARTITION_SPIFFS].partition_description           = "spiffs";
-    hal_partitions[HAL_PARTITION_SPIFFS].partition_start_addr            = 0x080E0000;
-    hal_partitions[HAL_PARTITION_SPIFFS].partition_length                = 0x20000; //128k bytes
-    hal_partitions[HAL_PARTITION_SPIFFS].partition_options               = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN;
 }
 
 
