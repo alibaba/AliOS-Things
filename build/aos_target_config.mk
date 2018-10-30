54119133a95a855d4d7fe63e21fab02b3c5364ca
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2018-10-30 09:37:24 +0800

    BugID:17526504:Move yloop from kernel to network
    Change-Id: Ie99ba6dbaa084bef9c59ecaf8755345f3712da13

--- build/aos_target_config.mk
+++ build/aos_target_config.mk
@@ -374,7 +374,7 @@ AOS_SDK_INCLUDES += -I$(SOURCE_ROOT)kernel/hal/include \
                     -I$(SOURCE_ROOT)kernel/hal/include/hal \
                     -I$(SOURCE_ROOT)kernel/hal/include/hal/soc \
                     -I$(SOURCE_ROOT)kernel/rhino/vfs/include \
-                    -I$(SOURCE_ROOT)kernel/yloop/include \
+                    -I$(SOURCE_ROOT)network/yloop/include \
                     -I$(SOURCE_ROOT)kernel/rhino/fs/kv/include \
                     -I$(SOURCE_ROOT)tools/cli/include \
                     -I$(SOURCE_ROOT)utility/log/include
