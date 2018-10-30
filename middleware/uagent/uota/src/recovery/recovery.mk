5ba80909937d52556e69a39b7e89cb5897fc594f
wan_guojian <william.wgj@alibaba-inc.com>
2018-10-30 20:02:29 +0800

    BugID:17115519:[OTA]support second bootloader
    Change-Id: I58cc51f6239de54b562add826dfa15575b6e72e3

--- middleware/uagent/uota/src/recovery/recovery.mk
+++ middleware/uagent/uota/src/recovery/recovery.mk
@@ -9,18 +9,30 @@ else ifeq ($(COMPILER),gcc)
 $(NAME)_CFLAGS      += -Wall -Werror
 endif
 
-ifeq ($(strip $(CONFIG_SYSINFO_DEVICE_NAME)), $(filter $(CONFIG_SYSINFO_DEVICE_NAME), MK3060 ESP8266))
-$(NAME)_SOURCES := \
-    rec_main.c \
-    rec_libc.c
+OTA_RECOVERY_TYPE_DIRECT := 0  #
+OTA_RECOVERY_TYPE_ABBACK := 1  #
+OTA_RECOVERY_TYPE_ABBOOT := 2  #
 
-$(NAME)_COMPONENTS += middleware.uagent.uota.src.recovery.nbpatch \
-                      middleware.uagent.uota.src.recovery.xz-embedded
+ifeq ($(strip $(CONFIG_SYSINFO_DEVICE_NAME)), $(filter $(CONFIG_SYSINFO_DEVICE_NAME), ESP3060))
+$(NAME)_SOURCES := rec_common.c
 endif
+
+ifeq ($(strip $(CONFIG_SYSINFO_DEVICE_NAME)), $(filter $(CONFIG_SYSINFO_DEVICE_NAME), ESP8266))
+$(NAME)_SOURCES += rec_main.c rec_libc.c
+
+$(NAME)_COMPONENTS +=   middleware.uagent.uota.src.recovery.nbpatch \
+                        middleware.uagent.uota.src.recovery.xz-embedded
+endif
+
+$(NAME)_INCLUDES += nbpatch
 
 ifeq ($(CONFIG_SYSINFO_DEVICE_NAME), ESP8266)
 GLOBAL_DEFINES += IS_ESP8266
-GLOBAL_DEFINES += RECOVERY_FLASH_COPY
+GLOBAL_DEFINES += OTA_RECOVERY_TYPE=$(OTA_RECOVERY_TYPE_ABBOOT)
 endif
 
-GLOBAL_INCLUDES += .
+ifeq ($(CONFIG_SYSINFO_DEVICE_NAME), MK3060)
+GLOBAL_DEFINES += OTA_RECOVERY_TYPE=$(OTA_RECOVERY_TYPE_ABBACK)
+endif
+
+GLOBAL_INCLUDES += ./ ../service
