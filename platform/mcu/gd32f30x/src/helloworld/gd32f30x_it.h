772504722f6d3fe671f3e946d8ea4c07a283c029
leek3 <30288185+leek3@users.noreply.github.com>
2018-10-30 11:39:26 +0800

    add gd32f30x and gd32f3x0 driver, update gd32f4xx driver (#677)
    * modify mcu/gd32f4xx folder structure
    * Add gd32f30x and gd32f3x0 driver, update gd32f4xx driver
    * add BSD license for gd32f30x/gd32f3x0/gd32f4xx
    * delete .exe and .pack, including in the gd32 driver

---
+++ platform/mcu/gd32f30x/src/helloworld/gd32f30x_it.h
@@ -0,0 +1,26 @@
+#ifndef GD32F30X_IT_H
+#define GD32F30X_IT_H
+
+#include "gd32f30x.h"
+
+/* function declarations */
+/* this function handles NMI exception */
+void NMI_Handler(void);
+/* this function handles HardFault exception */
+void HardFault_Handler(void);
+/* this function handles MemManage exception */
+void MemManage_Handler(void);
+/* this function handles BusFault exception */
+void BusFault_Handler(void);
+/* this function handles UsageFault exception */
+void UsageFault_Handler(void);
+/* this function handles SVC exception */
+void SVC_Handler(void);
+/* this function handles DebugMon exception */
+void DebugMon_Handler(void);
+/* this function handles PendSV exception */
+void PendSV_Handler(void);
+/* this function handles SysTick exception */
+void SysTick_Handler(void);
+
+#endif /* GD32F30X_IT_H */
