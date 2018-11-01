dc513e97aa82426ea94214305413fa45d906a6de
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2018-11-01 19:31:44 +0800

    BugID:17646789:Move wifi hal from kernel to network
    Change-Id: Ifcd97c9e4a834c98275e86ab367f3507e48a30e5

--- kernel/hal/hal.mk
+++ kernel/hal/hal.mk
@@ -3,9 +3,6 @@ NAME := hal
 $(NAME)_TYPE := kernel
 $(NAME)_MBINS_TYPE := kernel
 
-$(NAME)_SOURCES     := wifi.c
-$(NAME)_SOURCES     += ota.c
-
 #default gcc
 ifeq ($(COMPILER),)
 $(NAME)_CFLAGS      += -Wall -Werror
