a496666fd372cf8372b9838d02438dd6dc06bad7
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-11-03 11:29:24 +0800

    BugID:16846667: Ajusted app's dir structure
    	modified:   example/uapp1/src/app.c
    	renamed:    example/uapp2/system/entry.c -> example/uapp1/src/entry.c
    	deleted:    example/uapp1/system/app_mm.c
    	deleted:    example/uapp1/system/app_mm.h
    	modified:   example/uapp1/test/buf_queue_test.c
    	modified:   example/uapp1/uapp1.mk
    	renamed:    example/uapp1/system/entry.c -> example/uapp2/src/entry.c
    	deleted:    example/uapp2/system/app_mm.c
    	deleted:    example/uapp2/system/app_mm.h
    	modified:   example/uapp2/uapp2.mk
    
    Change-Id: I50a806e5d9ba51bd0e4787cf6d189703f1a045ce

--- app/example/uapp1/src/app.c
+++ app/example/uapp1/src/app.c
@@ -11,6 +11,8 @@
 static ktask_t app_obj;
 cpu_stack_t app_stack[APP_STACK_SIZE];
 
+static ktimer_t app_timer;
+
 static void app_run(void *arg)
 {
     int cnt = 0;
@@ -24,6 +26,13 @@ static void app_run(void *arg)
     }
 }
 
+static void app_timer_cb(void *timer, void *arg)
+{
+    static int cnt = 0;
+
+    printf("app timer cb called %d\r\n", cnt++);
+}
+
 int application_start(int argc, char *argv[])
 {
     int cnt = 0;
@@ -35,6 +44,9 @@ int application_start(int argc, char *argv[])
     printf(" app1 start to run\r\n");
 
     run_test_case();
+
+    krhino_timer_create(&app_timer, "app_timer", app_timer_cb,
+                        10, 100, NULL, 1);
 
     krhino_utask_create(&app_obj, "application1", 0,
                         AOS_DEFAULT_APP_PRI,
