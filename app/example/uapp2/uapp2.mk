fe00f84afb7aa2b945b79d50689567135c6706ef
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-11-03 11:29:24 +0800

    BugID:16846667: Ajusted app's dir structure
    	modified:   example/uapp1/src/app.c
    	renamed:    example/uapp2/system/entry.c -> example/uapp1/src/entry.c
    	deleted:    example/uapp1/system/app_mm.c
    	deleted:    example/uapp1/system/app_mm.h
    	modified:   example/uapp1/test/buf_queue_test.c
    	modified:   example/uapp1/uapp1.mk
    	renamed:    example/uapp1/system/entry.c -> example/uapp2/src/entry.c
    	deleted:    example/uapp2/system/app_mm.c
    	deleted:    example/uapp2/system/app_mm.h
    	modified:   example/uapp2/uapp2.mk
    
    Change-Id: I50a806e5d9ba51bd0e4787cf6d189703f1a045ce

--- app/example/uapp2/uapp2.mk
+++ app/example/uapp2/uapp2.mk
@@ -4,16 +4,8 @@ $(NAME)_TYPE := app
 $(NAME)_MBINS_TYPE := app
 
 ifeq ($(ENABLE_USPACE),1)
-$(NAME)_SOURCES := system/entry.c \
-                   system/app_mm.c \
+$(NAME)_SOURCES := src/entry.c \
                    src/app.c
-
-GLOBAL_INCLUDES += system
-
-ifeq ($(MBINS),app)
-GLOBAL_DEFINES += AOS_APP_MM
-endif
-
 else
 $(NAME)_SOURCES := src/app.c
 endif
