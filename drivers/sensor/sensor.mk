41b2c54dec76ad3d7ebe3e432fcdb4fae6d45296
andy.zwj <andy.zwj@alibaba-inc.com>
2018-11-05 11:05:55 +0800

    BugID: 17115493:add sensor compile file
    Change-Id: Ia5a63e0fdae5224ac214f06a91d49da465c44831

--- drivers/sensor/sensor.mk
+++ drivers/sensor/sensor.mk
@@ -4,7 +4,18 @@ $(NAME)_MBINS_TYPE := kernel
 
 $(NAME)_SOURCES += \
         hal/sensor_hal.c \
-        hal/sensor_drv_api.c 
+        hal/sensor_drv_api.c \
+        drv/drv_humi_bosch_bme280.c \
+        drv/drv_acc_bosch_bma253.c \
+        drv/drv_baro_bosch_bmp280.c \
+        drv/drv_acc_gyro_st_lsm6dsl.c\
+        drv/drv_baro_st_lps22hb.c \
+        drv/drv_acc_mir3_da217.c \
+        drv/drv_als_ps_liteon_ltr553.c \
+        drv/drv_temp_humi_sensirion_shtc1.c \
+        drv/drv_temp_humi_st_hts221.c \
+        drv/drv_mag_st_lis3mdl.c \
+        drv/drv_mag_temp_memsic_mmc3680kj.c 
 
 
 CONFIG_SENSOR =  $(addsuffix .c,$(CONFIG_SENSOR_DRV_NAME))
