09716c9738a46910bef2e737c98932309a8f15fb
xicai.cxc <xicai.cxc@alibaba-inc.com>
2018-11-06 19:14:17 +0800

    BugID:17700072:[ota][fix]add offline subscribe ability--move ota sub forward
    Change-Id: I80c376f490e26e182c5813daab292df1381807f8

--- middleware/linkkit/sdk-c/src/services/linkkit/dm/client/dm_client.c
+++ middleware/linkkit/sdk-c/src/services/linkkit/dm/client/dm_client.c
@@ -102,21 +102,6 @@ static void _dm_client_event_cloud_connected_handle(void)
     awss_report_cloud();
 #endif
     dm_msg_cloud_connected();
-
-#ifdef OTA_ENABLED
-    int res = 0;
-
-    /* DM OTA Module Init */
-    res = dm_ota_sub();
-    if (res == SUCCESS_RETURN) {
-        /* DM Config OTA Module Init */
-        dm_cota_init();
-
-        /* DM Firmware OTA Mudule Init */
-        dm_fota_init();
-    }
-#endif
-
 }
 
 static void _dm_client_event_cloud_disconnect_handle(void)
