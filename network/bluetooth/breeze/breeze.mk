60a895e185e9d5e072a4959cec005562323be7c1
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2018-11-07 16:34:36 +0800

    bugID:16974841: breeze: extract breeze_awss with marcos.
    Change-Id: I287dec8b2b26ecfa9bda6b3ad6fa9de5d52c54e0

--- network/bluetooth/breeze/breeze.mk
+++ network/bluetooth/breeze/breeze.mk
@@ -6,7 +6,6 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_SOURCES += core/auth.c
 $(NAME)_SOURCES += core/core.c
 $(NAME)_SOURCES += core/transport.c
-$(NAME)_SOURCES += core/extcmd.c
 $(NAME)_SOURCES += core/ble_service.c
 $(NAME)_SOURCES += core/sha256.c
 $(NAME)_SOURCES += core/utils.c
@@ -16,7 +15,7 @@ GLOBAL_INCLUDES += api include hal/include
 $(NAME)_COMPONENTS := chip_code
 
 secure_adv ?= 1
-ifeq ($(secure_adv),1)
+ifeq ($(secure_adv), 1)
 GLOBAL_DEFINES += CONFIG_AIS_SECURE_ADV
 endif
 
@@ -27,7 +26,11 @@ endif
 
 $(NAME)_SOURCES += api/breeze_export.c
 
-breeze_awss ?= 0
-ifeq ($(breeze_awss),1)
+breeze_awss ?= 1
+ifeq ($(breeze_awss), 1)
+GLOBAL_DEFINES += EN_COMBO_NET
+$(NAME)_SOURCES += core/extcmd.c
 $(NAME)_SOURCES += api/breeze_awss_export.c
 endif
+
+
