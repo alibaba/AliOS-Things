1c9edf7e9b87718de4a893103081229220ef8a26
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-11-07 15:45:20 +0800

    BugID:16846667: added imx6sl_svc board
    	new file:   imx6sl_svc/app1.ld.S
    	new file:   imx6sl_svc/app2.ld.S
    	new file:   imx6sl_svc/imx6sl_svc.ld.S
    	new file:   imx6sl_svc/imx6sl_svc.mk
    	new file:   imx6sl_svc/imx6sl_svc_kernel.ld.S
    	new file:   imx6sl_svc/include/board.h
    	new file:   imx6sl_svc/include/k_config.h
    	new file:   imx6sl_svc/include/uapp.h
    	new file:   imx6sl_svc/src/board.c
    	new file:   imx6sl_svc/src/main.c
    	new file:   imx6sl_svc/src/uspace_init.c
    	new file:   imx6sl_svc/ucube.py
    
    Change-Id: I024aa9e29d28e08aa1139a1b35fbb345c2d81e28

---
+++ board/imx6sl_svc/imx6sl_svc.mk
@@ -0,0 +1,38 @@
+NAME := board_imx6sl_svc
+
+$(NAME)_MBINS_TYPE   := kernel
+
+MODULE               := imx6sl_svc
+HOST_ARCH            := Cortex-A9
+HOST_MCU_FAMILY      := imx6sl_svc
+SUPPORT_BINS         := no
+SUPPORT_MBINS        := yes
+ENABLE_USPACE        := 1
+
+$(NAME)_SOURCES := src/board.c \
+                   src/main.c \
+                   src/uspace_init.c
+
+GLOBAL_INCLUDES += include
+
+GLOBAL_DEFINES += STDIO_UART=0 CONFIG_NO_TCPIP  CHIP_MX6SL  BOARD_EVK  BOARD_REV_A
+
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_IMX6SL
+CONFIG_SYSINFO_DEVICE_NAME := IMX6SL
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
+
+ifeq ($(MBINS),)
+GLOBAL_LDS_FILES += board/imx6sl_svc/imx6sl_svc.ld.S
+else ifeq ($(MBINS),kernel)
+GLOBAL_LDS_FILES +=  board/imx6sl_svc/imx6sl_svc_kernel.ld.S
+else ifeq ($(MBINS), app)
+ifneq ($(MBINS_APP),)
+GLOBAL_LDS_FILES +=  board/imx6sl_svc/$(MBINS_APP).ld.S
+else
+GLOBAL_LDS_FILES +=  board/imx6sl_svc/imx6sl_svc_app.ld.S
+endif
+endif
+
+SUBGDBINIT_STRING := "b main"
