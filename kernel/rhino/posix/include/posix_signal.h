c5ac4ee7c65c5b4f96d328a52e9c727e29a780b3
chongling <yj144830@alibaba-inc.com>
2018-11-08 14:59:55 +0800

    BugID:16851920:add posix timer support
    Change-Id: Id0052bea5b74bbcff176f253458f53cc265d7954

---
+++ kernel/rhino/posix/include/posix_signal.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef POSIX_SIGNAL_H
+#define POSIX_SIGNAL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <k_api.h>
+#include <pthread.h>
+
+#define SIGEV_NONE      0
+#define SIGEV_SIGNAL    1
+#define SIGEV_THREAD    2
+
+union sigval
+{
+    int sival_int;
+    void * sival_ptr;
+};
+
+struct sigevent {
+    int           sigev_notify;
+    int           sigev_signo;
+    union sigval  sigev_value;
+    void         (*sigev_notify_function)(union sigval);
+    pthread_attr_t *sigev_notify_attributes;
+};
+
+#ifdef __cplusplus
+}
+#endif /* __cplusplus */
+
+#endif
--- kernel/rhino/posix/include/semaphore.h
+++ kernel/rhino/posix/include/semaphore.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
 #ifndef POSIX_SEM_H
 #define POSIX_SEM_H
 
