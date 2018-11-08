f2aeedf1e896bc0a2b7e33ef7340b54bc19bfdda
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-08 17:01:06 +0800

    BugID:17711949: Update kv directory reference for build
    Change-Id: I5b668270a6433595141569f3f519770a3d663389

--- build/aos_target_config_2boot.mk
+++ build/aos_target_config_2boot.mk
@@ -319,7 +319,7 @@ AOS_SDK_INCLUDES += -I$(SOURCE_ROOT)kernel/hal/include \
                     -I$(SOURCE_ROOT)kernel/hal/include/hal/soc \
                     -I$(SOURCE_ROOT)kernel/rhino/vfs/include \
                     -I$(SOURCE_ROOT)kernel/yloop/include \
-                    -I$(SOURCE_ROOT)kernel/rhino/fs/kv/include \
+                    -I$(SOURCE_ROOT)kernel/fs/kv/include \
                     -I$(SOURCE_ROOT)tools/cli/include \
                     -I$(SOURCE_ROOT)utility/log/include \
 		    -I$(SOURCE_ROOT)kernel/rhino
