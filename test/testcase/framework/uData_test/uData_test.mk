63422f6ae91eb22c49a4fa7335267533102d600f
Li Cheng <lc122798@alibaba-inc.com>
2018-11-08 17:10:45 +0800

    BugID:16857751:[test] fixed dependent module name errors in .mk file
    Change-Id: I09085777d48b80f37054ac521b1dda94c82ff026

--- test/testcase/framework/uData_test/uData_test.mk
+++ test/testcase/framework/uData_test/uData_test.mk
@@ -3,7 +3,7 @@ NAME := uData_test
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 0.0.1
 $(NAME)_SUMMARY :=
-$(NAME)_COMPONENTS  += middleware.common middle.udata
+$(NAME)_COMPONENTS  += middleware.common middleware.udata
 
 $(NAME)_SOURCES     += uData_test.c
 
