b386fe8dd65f88d51e3103bc77fa874edfea620f
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-11-08 15:15:04 +0800

    BugID:17738554: add osal Config.in/mk/README.md
    Change-Id: I3463c45108879cf53008f138a5c2b18266871a01

--- osal/espos/espos.mk
+++ osal/espos/espos.mk
@@ -1,14 +1,15 @@
-NAME := espos
 
-GLOBAL_INCLUDES += include
-$(NAME)_SOURCES := \
-                    platform/rhino/espos_misc.c      \
-                    platform/rhino/espos_mutex.c     \
-                    platform/rhino/espos_queue.c     \
-                    platform/rhino/espos_scheduler.c \
-                    platform/rhino/espos_semaphore.c \
-                    platform/rhino/espos_spinlock.c  \
-                    platform/rhino/espos_task.c      \
-                    platform/rhino/espos_time.c      \
-                    platform/rhino/espos_timer.c
+GLOBAL_INCLUDES += espos/include
+$(NAME)_SOURCES += \
+                    espos/platform/rhino/espos_misc.c      \
+                    espos/platform/rhino/espos_mutex.c     \
+                    espos/platform/rhino/espos_queue.c     \
+                    espos/platform/rhino/espos_scheduler.c \
+                    espos/platform/rhino/espos_semaphore.c \
+                    espos/platform/rhino/espos_task.c      \
+                    espos/platform/rhino/espos_time.c      \
+                    espos/platform/rhino/espos_timer.c
 
+ifeq ($(HOST_MCU_FAMILY),esp32)
+$(NAME)_SOURCES += espos/platform/rhino/espos_spinlock.c
+endif
