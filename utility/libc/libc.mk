4edcbf5dcff7f15115c20dcc52d254c14484aebf
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-11-09 14:14:55 +0800

    BugID:17758167: add libc mk & config.in & readme
    Change-Id: Ib4d429d0b0ac168ba7991144864137e227e4f9f0

--- utility/libc/libc.mk
+++ utility/libc/libc.mk
@@ -1,28 +1,23 @@
 NAME := newlib_stub
 
+$(NAME)_VERSION := 0.0.1.0
+$(NAME)_SUMMARY := libc adaptation layer
+$(NAME)_MBINS_TYPE := share
+
 ifeq ($(COMPILER),armcc)
-$(NAME)_TYPE := share
 $(NAME)_SOURCES := compilers/armlibc/armcc_libc.c
-GLOBAL_INCLUDES += compilers/armlibc 
+GLOBAL_INCLUDES += compilers/armlibc
 else ifeq ($(COMPILER),rvct)
-$(NAME)_TYPE := share
 $(NAME)_SOURCES := compilers/armlibc/armcc_libc.c
-GLOBAL_INCLUDES += compilers/armlibc 
+GLOBAL_INCLUDES += compilers/armlibc
 else ifeq ($(COMPILER),iar)
-$(NAME)_TYPE := share
 GLOBAL_INCLUDES += compilers/iar
 $(NAME)_SOURCES := compilers/iar/iar_libc.c
 else ifeq ($(HOST_MCU_FAMILY),rda8955)
-$(NAME)_TYPE := share
-$(NAME)_MBINS_TYPE := share
 $(NAME)_SOURCES := mips_rda_stub.c
 else ifeq ($(HOST_MCU_FAMILY),freedom-e.e310)
-$(NAME)_TYPE := share
-$(NAME)_MBINS_TYPE := share
 $(NAME)_SOURCES := newlib_riscv_stub.c
 else ifneq ($(HOST_MCU_FAMILY),linux)
-$(NAME)_TYPE := share
-$(NAME)_MBINS_TYPE := share
 $(NAME)_SOURCES := newlib_stub.c
 endif
 
