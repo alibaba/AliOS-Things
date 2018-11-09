88197376610d1f87217b14b9422cfece9cfb7d8b
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-09 22:23:20 +0800

    BugID:17711858: Update vfs directory reference for platform/mcu
    Change-Id: I32bb50c3cdeaa0a8fc47527b2745641f36d2ba82

--- platform/mcu/mkl43z4/mkl43z4.mk
+++ platform/mcu/mkl43z4/mkl43z4.mk
@@ -4,7 +4,7 @@ NAME := mkl43z4impl
 $(NAME)_TYPE := kernel
 
 $(NAME)_COMPONENTS += platform/arch/arm/armv6m
-$(NAME)_COMPONENTS += rhino libc kernel.fs.kv middleware.common cli rhino.vfs digest_algorithm
+$(NAME)_COMPONENTS += rhino libc kernel.fs.kv middleware.common cli kernel.fs.vfs digest_algorithm
 
 GLOBAL_CFLAGS += -DCPU_MKL43Z256VLH4
 GLOBAL_CFLAGS += -Wall -fno-common -ffunction-sections -fdata-sections -ffreestanding -fno-builtin -mthumb -mapcs -std=gnu99
