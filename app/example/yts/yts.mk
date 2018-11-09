da423390750443204b55b37f9eb9ce2423a165d3
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-09 22:21:20 +0800

    BugID:17711858: Update vfs directory reference for example/yts
    Change-Id: Ie18124b5473c7dad367f5ae60dc0f00ca9a14c5d

--- app/example/yts/yts.mk
+++ app/example/yts/yts.mk
@@ -1,12 +1,12 @@
 NAME := yts
 
 $(NAME)_SOURCES := main.c
-$(NAME)_COMPONENTS := testcase rhino.test log rhino.vfs yloop hal
+$(NAME)_COMPONENTS := testcase rhino.test log kernel.fs.vfs yloop hal
 
 $(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable
 
 ifneq (,$(findstring linux, $(BUILD_STRING)))
-$(NAME)_COMPONENTS += network.lwip netmgr 3rdparty.experimental.fs.fatfs middleware.common
+$(NAME)_COMPONENTS += network.lwip network.umesh dda netmgr 3rdparty.experimental.fs.fatfs middleware.common
 
 GLOBAL_LDFLAGS += -lreadline -lncurses
 GLOBAL_DEFINES += CONFIG_AOS_MESHYTS DEBUG YTS_LINUX
@@ -14,3 +14,4 @@ else
 $(NAME)_COMPONENTS += cli
 endif
 
+GLOBAL_INCLUDES += ./
