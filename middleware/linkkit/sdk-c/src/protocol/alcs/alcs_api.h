45f5bb0c98898078b1b59858d320ceaffaf39354
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-11-10 13:18:21 +0800

    BugID:17646749:[system] mark heap usage with module name
    Change-Id: Iad701214573993464f43ebd8759af9bb20c9e5f4
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/protocol/alcs/alcs_api.h
+++ middleware/linkkit/sdk-c/src/protocol/alcs/alcs_api.h
@@ -7,20 +7,20 @@
 #include "alcs_coap.h"
 
 #ifndef __ALCS_API_H__
-#define __ALCS_API_H__ 
+#define __ALCS_API_H__
 
 #define ALCS_SUPPORT_MEMORY_MAGIC
 #ifdef ALCS_SUPPORT_MEMORY_MAGIC
-#define ALCS_malloc(size) LITE_malloc(size, MEM_MAGIC, "ALCS")
+    #define ALCS_malloc(size) LITE_malloc(size, MEM_MAGIC, "alcs")
 #else
-#define ALCS_malloc(size) LITE_malloc(size)
+    #define ALCS_malloc(size) LITE_malloc(size)
 #endif
 
 #define ALCS_ADAPTER_SUPPORT_MEMORY_MAGIC
 #ifdef ALCS_ADAPTER_SUPPORT_MEMORY_MAGIC
-#define ALCS_ADAPTER_malloc(size) LITE_malloc(size, MEM_MAGIC, "ALCS_ADAPTER")
+    #define ALCS_ADAPTER_malloc(size) LITE_malloc(size, MEM_MAGIC, "alcs_adapter")
 #else
-#define ALCS_ADAPTER_malloc(size) LITE_malloc(size)
+    #define ALCS_ADAPTER_malloc(size) LITE_malloc(size)
 #endif
 
 #define SESSIONID_LEN 8
@@ -47,7 +47,7 @@ typedef enum {
 
 typedef struct {
     int code;
-    char* msg;//MUST call coap_free to free memory
+    char *msg;//MUST call coap_free to free memory
 } ResponseMsg;
 
 #ifdef __cplusplus
@@ -65,22 +65,20 @@ typedef struct
 } AuthResult;
 */
 
-typedef void (*AuthHandler) (CoAPContext *context, NetworkAddr* addr, void* user_data, ResponseMsg* result);
-typedef struct
-{
-    char* productKey;
-    char* deviceName;
-    char* accessKey;
-    char* accessToken;
-    void* user_data;
+typedef void (*AuthHandler)(CoAPContext *context, NetworkAddr *addr, void *user_data, ResponseMsg *result);
+typedef struct {
+    char *productKey;
+    char *deviceName;
+    char *accessKey;
+    char *accessToken;
+    void *user_data;
     AuthHandler handler;
 } AuthParam;
 
-typedef struct
-{
+typedef struct {
     NetworkAddr addr;
-    char* pk;
-    char* dn;
+    char *pk;
+    char *dn;
 } AlcsDeviceKey;
 
 /*  初始化认证模块
@@ -91,13 +89,15 @@ typedef struct
  *        2 --server
  *        3 --client&server
  */
-int alcs_auth_init(CoAPContext *context, const char* productKey, const char* deviceName, char role);
-void alcs_auth_subdev_init(CoAPContext *ctx, const char* productKey, const char* deviceName);
+int alcs_auth_init(CoAPContext *context, const char *productKey, const char *deviceName, char role);
+void alcs_auth_subdev_init(CoAPContext *ctx, const char *productKey, const char *deviceName);
 void alcs_auth_deinit(void);
 
-bool alcs_is_auth (CoAPContext *ctx, AlcsDeviceKey* devKey);
-int alcs_sendmsg_secure(CoAPContext *ctx, AlcsDeviceKey* devKey, CoAPMessage *message, char observe, CoAPSendMsgHandler handler);
-int alcs_sendrsp_secure(CoAPContext *ctx, AlcsDeviceKey* devKey, CoAPMessage *message, char observe, unsigned short msgid, CoAPLenString* token);
+bool alcs_is_auth(CoAPContext *ctx, AlcsDeviceKey *devKey);
+int alcs_sendmsg_secure(CoAPContext *ctx, AlcsDeviceKey *devKey, CoAPMessage *message, char observe,
+                        CoAPSendMsgHandler handler);
+int alcs_sendrsp_secure(CoAPContext *ctx, AlcsDeviceKey *devKey, CoAPMessage *message, char observe,
+                        unsigned short msgid, CoAPLenString *token);
 
 #ifdef ALCS_CLIENT_ENABLED
 /*  身份认证--  直接传入accesskey&accesstoken
@@ -105,7 +105,7 @@ int alcs_sendrsp_secure(CoAPContext *ctx, AlcsDeviceKey* devKey, CoAPMessage *me
  *  addr：      待连设备地址
  *  auth_param：包含待连设备的信息和回调接口
  */
-void alcs_auth_has_key (CoAPContext *ctx, NetworkAddr* addr, AuthParam* auth_param);
+void alcs_auth_has_key(CoAPContext *ctx, NetworkAddr *addr, AuthParam *auth_param);
 
 /*  身份认证--通过productkey&devicename在缓存的accesskey列表中查找合适accesskey
  *  此函数需要和alcs_add_client_key 配合使用
@@ -117,43 +117,43 @@ void alcs_auth_has_key (CoAPContext *ctx, NetworkAddr* addr, AuthParam* auth_par
  *  deviceName：待连设备的deviceName
  *  handler：   结果回调接口
  */
-void alcs_auth_nego_key (CoAPContext *ctx, AlcsDeviceKey* devKey, AuthHandler handler);
+void alcs_auth_nego_key(CoAPContext *ctx, AlcsDeviceKey *devKey, AuthHandler handler);
 /*
  *
  *
  */
-int alcs_add_client_key(CoAPContext *context, const char* accesskey, const char* accesstoken, const char* productKey, const char* deviceName);
-int alcs_remove_client_key (CoAPContext *context, const char* key, char isfullkey);
+int alcs_add_client_key(CoAPContext *context, const char *accesskey, const char *accesstoken, const char *productKey,
+                        const char *deviceName);
+int alcs_remove_client_key(CoAPContext *context, const char *key, char isfullkey);
 /*
  *
  *
  */
-bool alcs_device_online (CoAPContext *context, AlcsDeviceKey* devKey);
+bool alcs_device_online(CoAPContext *context, AlcsDeviceKey *devKey);
 
 #endif
 
 #ifdef ALCS_SERVER_ENABLED
-typedef enum
-{
+typedef enum {
     LOCALDEFAULT,
     LOCALSETUP,
     FROMCLOUDSVR
 } ServerKeyPriority;
 
-int alcs_add_svr_key (CoAPContext *context, const char* keyprefix, const char* secret, ServerKeyPriority priority);
-int alcs_remove_svr_key (CoAPContext *context, const char* keyprefix);
- /*  设置吊销列表*
- *  context：   为当前设备生成的CoAPContext对象指针
- *  seqlist：   吊销列表字符串，每个被吊销设备占用三字节
- */
-int alcs_set_revocation (CoAPContext *context, const char* seqlist);
+int alcs_add_svr_key(CoAPContext *context, const char *keyprefix, const char *secret, ServerKeyPriority priority);
+int alcs_remove_svr_key(CoAPContext *context, const char *keyprefix);
+/*  设置吊销列表*
+*  context：   为当前设备生成的CoAPContext对象指针
+*  seqlist：   吊销列表字符串，每个被吊销设备占用三字节
+*/
+int alcs_set_revocation(CoAPContext *context, const char *seqlist);
 #endif
 
-int alcs_add_ctl_group (CoAPContext *context, const char* groupid, const char* accesskey, const char* accesstoken);
-int alcs_remove_ctl_group (CoAPContext *context, const char* groupid);
+int alcs_add_ctl_group(CoAPContext *context, const char *groupid, const char *accesskey, const char *accesstoken);
+int alcs_remove_ctl_group(CoAPContext *context, const char *groupid);
 
-int alcs_add_svr_group (CoAPContext *context, const char* groupid, const char* keyprefix, const char* secret);
-int alcs_remove_svr_group (CoAPContext *context, const char* groupid);
+int alcs_add_svr_group(CoAPContext *context, const char *groupid, const char *keyprefix, const char *secret);
+int alcs_remove_svr_group(CoAPContext *context, const char *groupid);
 
 #ifdef __cplusplus
 }
