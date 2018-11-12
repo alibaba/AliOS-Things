718419690dcb39bd6b7f7e8d3411d1990f0d651d
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2018-11-12 19:26:06 +0800

    BugID:16944965: Add required fields to utility Makefiles
    Change-Id: I8e72f276c374e1b22e861e8b1267f6eccd44ed93

--- utility/cjson/cjson.mk
+++ utility/cjson/cjson.mk
@@ -1,6 +1,9 @@
 NAME := cjson
 
-$(NAME)_TYPE := share
+$(NAME)_MBINS_TYPE := share
+$(NAME)_VERSION := 0.0.1
+$(NAME)_SUMMARY :=
+
 GLOBAL_INCLUDES += include
 # don't modify to L_CFLAGS, because CONFIG_CJSON_WITHOUT_DOUBLE should enable global
 #$(NAME)_CFLAGS  += -DCONFIG_CJSON_WITHOUT_DOUBLE
