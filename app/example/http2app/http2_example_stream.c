cd7692fb9db4657dbbdf00002dd42642f9123b43
zhangtian <dongren.zt@alibaba-inc.com>
2018-11-12 14:11:17 +0800

    BugID:17646749:[h2 app] add special header for demo
    Change-Id: I8f0a346502d1344c293951b8bf10cfadf5eb3b82

--- app/example/http2app/http2_example_stream.c
+++ app/example/http2app/http2_example_stream.c
@@ -147,7 +147,7 @@ static int http2_stream_test()
 
     http2_header header[] = {
         MAKE_HEADER("test_name", "test_http2_header"),
-        MAKE_HEADER_CS("hello", "world"),
+        MAKE_HEADER("x-for-test", "hello world"),
     };
     
     header_ext_info_t my_header_info = {
