0b8bbba1ce1d8581acdb18f9ff3c30f86e03eefb
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-11-12 17:39:43 +0800

    BugID:16846667: Support process msgq test
    	modified:   example/uapp1/src/app.c
    	modified:   example/uapp1/src/entry.c
    	modified:   example/uapp1/src/ucli.c
    	modified:   example/uapp2/src/app.c
    	modified:   example/uapp2/src/entry.c
    
    Change-Id: I1af050e7574ff3b2bef65bf568c829b9d7d28a92

--- app/example/uapp1/src/app.c
+++ app/example/uapp1/src/app.c
@@ -2,9 +2,10 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-#include <k_api.h>
-#include <hal/hal.h>
+#include <u_api.h>
 #include <ucli.h>
+
+//#define ENABLE_PROC_MSG
 
 #define APP_STACK_SIZE 0x400
 
@@ -13,11 +14,13 @@ cpu_stack_t app_stack[APP_STACK_SIZE];
 
 static ktimer_t app_timer;
 
+#ifdef ENABLE_PROC_MSG
+static size_t msg_id;
+#endif
+
 static void app_run(void *arg)
 {
     int cnt = 0;
-
-    ktask_t *cur = krhino_cur_task_get();
 
     while (1) {
         printf("uapp1 app cnt 0x%x\r\n", cnt);
@@ -31,6 +34,11 @@ static void app_timer_cb(void *timer, void *arg)
     static int cnt = 0;
 
     printf("app timer cb called %d\r\n", cnt++);
+
+#ifdef ENABLE_PROC_MSG
+    char msg[20] = "hello message queue";
+    krhino_msg_snd(msg_id, msg, 20);
+#endif
 }
 
 int application_start(int argc, char *argv[])
@@ -45,6 +53,9 @@ int application_start(int argc, char *argv[])
 
     run_test_case();
 
+#ifdef ENABLE_PROC_MSG
+    msg_id = krhino_msg_get(0x1234, 0, 1024);
+#endif
     krhino_timer_create(&app_timer, "app_timer", app_timer_cb,
                         10, 100, NULL, 1);
 
