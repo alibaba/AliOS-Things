908586da2ca13b3153f77623acfa3bd53eee0890
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2018-11-12 19:26:06 +0800

    BugID:16944965: Add required fields to utility Makefiles
    Change-Id: I8e72f276c374e1b22e861e8b1267f6eccd44ed93

--- utility/hashtable/hashtable.mk
+++ utility/hashtable/hashtable.mk
@@ -1,6 +1,9 @@
 NAME := hashtable
 
-$(NAME)_TYPE := share
+$(NAME)_MBINS_TYPE := share
+$(NAME)_VERSION := 0.0.1
+$(NAME)_SUMMARY :=
+
 $(NAME)_SOURCES := hashtable.c
 $(NAME)_INCLUDES := ../../middleware/alink/protocol/os/
 GLOBAL_INCLUDES += .
