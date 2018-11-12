8ec119ef79045de98d58d00e9b3fd8a2cb4a3e00
lingjun chen <lingjun.clj@alibaba-inc.com>
2018-11-11 23:48:01 -0800

    BugID:16731946:fix u_proc_msg.c
    Change-Id: Ib363e872c6484033a632d3e8117762aad3b4a84c

--- kernel/rhino/uspace/u_proc_msg.c
+++ kernel/rhino/uspace/u_proc_msg.c
@@ -3,33 +3,78 @@
 
 #define MAX_MSG_SIZE 0xffffffffu
 
-size_t krhino_msg_get(uint32_t key, uint32_t flg, size_t size)
+static void msg_dyn_create(kbuf_queue_t **queue, const name_t *name,
+                                    size_t size, size_t max_msg, uint32_t key)
 {
+    CPSR_ALLOC();
+
+    kbuf_queue_t *queue_obj;
     klist_t      *tmp;
     klist_t      *head;
-    kbuf_queue_t *queue;
-    kstat_t       ret;
+    kbuf_queue_t *queue_tmp;
 
-    CPSR_ALLOC();
+    queue_obj = krhino_mm_alloc(sizeof(kbuf_queue_t));
+
+    if (queue_obj == NULL) {
+        *queue = 0;
+        return;
+    }
+
+    queue_obj->buf = krhino_mm_alloc(size);
+
+    if (queue_obj->buf == NULL) {
+        *queue = 0;
+        krhino_mm_free(queue_obj);
+        return;
+    }
+
+
+    /* init the queue blocked list */
+    klist_init(&queue_obj->blk_obj.blk_list);
+
+    queue_obj->buf                = queue_obj->buf;
+    queue_obj->cur_num            = 0u;
+    queue_obj->peak_num           = 0u;
+    queue_obj->max_msg_size       = max_msg;
+    queue_obj->blk_obj.name       = name;
+    queue_obj->blk_obj.blk_policy = BLK_POLICY_PRI;
+    queue_obj->mm_alloc_flag      = K_OBJ_DYN_ALLOC;
+
+    queue_obj->blk_obj.obj_type = RHINO_BUF_QUEUE_OBJ_TYPE;
+
+    ringbuf_init(&(queue_obj->ringbuf), queue_obj->buf, size, RINGBUF_TYPE_DYN, max_msg);
+    queue_obj->min_free_buf_size  = queue_obj->ringbuf.freesize;
 
     head = &g_kobj_list.buf_queue_head;
 
     RHINO_CRITICAL_ENTER();
     for (tmp = head->next; tmp != head; tmp = tmp->next) {
-        queue = krhino_list_entry(tmp, kbuf_queue_t, buf_queue_item);
-        if (queue->key == key) {
-            return (size_t)queue;
+        queue_tmp = krhino_list_entry(tmp, kbuf_queue_t, buf_queue_item);
+        if (queue_tmp->key == key) {
+            *queue = queue_tmp;
+            RHINO_CRITICAL_EXIT();
+            krhino_mm_free(queue_obj->buf);
+            krhino_mm_free(queue_obj);
+            return;
         }
     }
+    klist_insert(&(g_kobj_list.buf_queue_head), &queue_obj->buf_queue_item);
+    queue_obj->key = key;
     RHINO_CRITICAL_EXIT();
 
-    ret = krhino_buf_queue_dyn_create(&queue, "buf_queue", size, MAX_MSG_SIZE);
+   *queue = queue_obj;
+}
 
-    if (ret != RHINO_SUCCESS) {
+
+size_t krhino_msg_get(uint32_t key, uint32_t flg, size_t size)
+{
+    kbuf_queue_t *queue = 0;
+
+    msg_dyn_create(&queue, "buf_queue", size, MAX_MSG_SIZE, key);
+
+    if (queue == RHINO_SUCCESS) {
         return 0;
     }
-
-    queue->key = key;
 
     return (size_t)queue;        
 }
