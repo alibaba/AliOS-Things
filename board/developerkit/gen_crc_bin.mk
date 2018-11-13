3373702d9ed79bb79fcfac7d5979dfd3ebf14d8d
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2018-11-13 19:04:31 +0800

    BugID:17814037:move gen_crc_bin.mk l496_bootloader.bin from mcu/stm32l4xx_cube to board/developerkit
    Change-Id: I9d5e367ca557222d12e62bf6d7ed836ac94d0432

---
+++ board/developerkit/gen_crc_bin.mk
@@ -0,0 +1,4 @@
+OTA_BIN_OUTPUT_FILE := $(LINK_OUTPUT_FILE:$(LINK_OUTPUT_SUFFIX)=$(BIN_OUTPUT_SUFFIX))
+EXTRA_POST_BUILD_TARGETS += gen_crc_bin
+gen_crc_bin:
+	python $(SCRIPTS_PATH)/st_mcu_bin_pack_tool.py 0x80000 0x4000 $(SOURCE_ROOT)/board/developerkit/l496_bootloader.bin $(OTA_BIN_OUTPUT_FILE)
