3f0255e887d034bca95bb0f46e6e02e229828449
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-11-13 17:47:50 +0800

    BugID:17814530:remove unreadable text in msp432p4111launchpad.mk
    Change-Id: Ifa1789f275dcc7e6c6f7b5708f1f9c121157ca1f

--- board/msp432p4111launchpad/msp432p4111launchpad.mk
+++ board/msp432p4111launchpad/msp432p4111launchpad.mk
@@ -1,4 +1,3 @@
-# 组件名称，建议和当前目录名称保持一致
 NAME := msp432p4111launchpad
 
 #It's useless for now, just use this code for no reason
@@ -9,25 +8,18 @@ MODULE               := 1062
 # other: armhflinux/xtensa
 # csky: ck802/ck803
 # MIPS: MIPS-I
-# 可以在build目录查看不同的aos_tools_chain_*文件来查看
 HOST_ARCH            := Cortex-M4
 
-# board上MCU型号或者所属系列，必须和platform/mcu/ 目录下的对应的目录名称保持一致
 HOST_MCU_FAMILY      := msp432p4xx
 
-# 用于多bin编译使用，目前只有kernel和app两种类型，除去example部分，其他暂时全部属于kernel
 $(NAME)_TYPE := kernel
 
-# 板卡是否支持多BIN
 SUPPORT_BINS         := no
 
-# MCU名称，请和厂商的型号官方名称保持一致，会用于自动生成keil/iar工程时的芯片选择。
 HOST_MCU_NAME        := MSP432P4111
 
-# 是否使能VFP
 ENABLE_VFP           := 1
 
-# 本目录需要编译的源码
 $(NAME)_SOURCES += aos/board_partition.c \
                    aos/soc_init.c \
                    MSP_EXP432P4111.c \
@@ -44,8 +36,6 @@ GLOBAL_CFLAGS += -D__MSP432P4111__ -DDeviceFamily_MSP432P4x1xI -DSTDIO_UART=0
 # The following content is optional, which is used for the keil/iar project generate
 # Gcc compilation is supported by default
 
-# AliOS Things 支持自动生成keil/iar工程。以下部分为不同编译环境的启动文件和连接脚本
-# 该部分为可选实现，但是gcc编译环境为默认支持，即gcc的启动文件和连接脚本必须存在
 ifeq ($(COMPILER), armcc)
 $(NAME)_SOURCES += startup_msp432p4111_uvision.s    
 else ifeq ($(COMPILER), iar)
