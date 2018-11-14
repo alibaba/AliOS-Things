912c16c897a2f4a7518d535a056af1164382c465
zhangtian <dongren.zt@alibaba-inc.com>
2018-11-14 22:09:02 +0800

    BugID:17646749:[mqtt/guider]use dynamic length of conn info
    Change-Id: I992452e1be9f0eb044b7b2f1445f70de29fd60a9
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/infra/system/device.c
+++ middleware/linkkit/sdk-c/src/infra/system/device.c
@@ -9,7 +9,7 @@
 
 #include "iotx_system_internal.h"
 
-static iotx_conn_info_t     iotx_conn_info;
+static iotx_conn_info_t     iotx_conn_info = {0, NULL, NULL, NULL, NULL, NULL};
 
 int iotx_device_info_get(iotx_device_info_t *device_info)
 {
@@ -27,6 +27,31 @@ int iotx_device_info_get(iotx_device_info_t *device_info)
 
 iotx_conn_info_pt iotx_conn_info_get(void)
 {
-    memset(&iotx_conn_info, 0, sizeof(iotx_conn_info));
     return &iotx_conn_info;
 }
+
+void iotx_conn_info_release(void)
+{
+    if(iotx_conn_info.host_name != NULL) {
+        LITE_free(iotx_conn_info.host_name);
+    }
+     if(iotx_conn_info.username != NULL) {
+        LITE_free(iotx_conn_info.username);
+    }
+    if(iotx_conn_info.password != NULL) {
+        LITE_free(iotx_conn_info.password);
+    }
+    if(iotx_conn_info.client_id != NULL) {
+        LITE_free(iotx_conn_info.client_id);
+    }
+    memset(&iotx_conn_info, 0, sizeof(iotx_conn_info));
+}
+
+iotx_conn_info_pt iotx_conn_info_reload(void)
+{
+    iotx_conn_info_release();
+    if(iotx_guider_authenticate(&iotx_conn_info) < 0)
+        return NULL;
+    return &iotx_conn_info;
+}
+
