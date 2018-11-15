86e4157c8ff804de107a7fd0e4b83069f9943794
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-11-15 16:14:43 +0800

    BugID:17853093: add mk/config.in/readme for boards
    Change-Id: Id323d6fc9ad89fea5eaa6ae38b385fe54272283f

--- board/esp32devkitc/esp32devkitc.mk
+++ board/esp32devkitc/esp32devkitc.mk
@@ -1,19 +1,19 @@
 NAME := board_esp32devkitc
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
-MODULE              := 1062
-HOST_ARCH           := xtensa
-HOST_MCU_FAMILY     := esp32
-SUPPORT_BINS        := no
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := configuration for board esp32devkitc
+MODULE             := 1062
+HOST_ARCH          := xtensa
+HOST_MCU_FAMILY    := esp32
+SUPPORT_BINS       := no
 
 # todo: remove these after rhino/lwip ready
-osal               ?= rhino
+osal ?= rhino
 
 
 CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_ESP32
-CONFIG_SYSINFO_DEVICE_NAME := ESP32
+CONFIG_SYSINFO_DEVICE_NAME   := ESP32
 
 GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
 GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
---
+++ board/frdmkl27z/README.md
@@ -0,0 +1,15 @@
+## Over View
+
+## Feature of Board
+
+## Directories
+
+```sh
+frdmkl27z # configuration files for board frdmkl27z
+```
+
+## Board Hardware Resources
+
+## Update log
+
+## Reference
