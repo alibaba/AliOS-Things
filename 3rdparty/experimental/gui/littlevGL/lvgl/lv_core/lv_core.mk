53a43967971ffb8d668d33208f5f77d522171ac3
chongling <yj144830@alibaba-inc.com>
2018-11-15 21:25:24 +0800

    BugID:17711889:littlevgl component
    Change-Id: I8e72736aeb3fb8a2ba797345f0860e02b0edbcdd

--- 3rdparty/experimental/gui/littlevGL/lvgl/lv_core/lv_core.mk
+++ 3rdparty/experimental/gui/littlevGL/lvgl/lv_core/lv_core.mk
@@ -1,11 +1,9 @@
-NAME := lv_core
+$(NAME)_SOURCES     += ./lvgl/lv_core/lv_group.c
+$(NAME)_SOURCES     += ./lvgl/lv_core/lv_indev.c
+$(NAME)_SOURCES     += ./lvgl/lv_core/lv_obj.c
+$(NAME)_SOURCES     += ./lvgl/lv_core/lv_refr.c
+$(NAME)_SOURCES     += ./lvgl/lv_core/lv_style.c
+$(NAME)_SOURCES     += ./lvgl/lv_core/lv_vdb.c
 
-$(NAME)_SOURCES     += lv_group.c
-$(NAME)_SOURCES     += lv_indev.c
-$(NAME)_SOURCES     += lv_obj.c
-$(NAME)_SOURCES     += lv_refr.c
-$(NAME)_SOURCES     += lv_style.c
-$(NAME)_SOURCES     += lv_vdb.c
-
-$(NAME)_INCLUDES    += ../../
-$(NAME)_INCLUDES    += ../
+$(NAME)_INCLUDES    += .
+$(NAME)_INCLUDES    += /lvgl
--- 3rdparty/experimental/gui/littlevGL/lvgl/lv_misc/lv_misc.mk
+++ 3rdparty/experimental/gui/littlevGL/lvgl/lv_misc/lv_misc.mk
@@ -1,18 +1,16 @@
-NAME := lv_misc
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_font.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_circ.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_area.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_task.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_fs.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_anim.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_mem.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_ll.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_color.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_txt.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_ufs.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_trigo.c
+$(NAME)_SOURCES     += ./lvgl/lv_misc/lv_math.c
 
-$(NAME)_SOURCES     += lv_font.c
-$(NAME)_SOURCES     += lv_circ.c
-$(NAME)_SOURCES     += lv_area.c
-$(NAME)_SOURCES     += lv_task.c
-$(NAME)_SOURCES     += lv_fs.c
-$(NAME)_SOURCES     += lv_anim.c
-$(NAME)_SOURCES     += lv_mem.c
-$(NAME)_SOURCES     += lv_ll.c
-$(NAME)_SOURCES     += lv_color.c
-$(NAME)_SOURCES     += lv_txt.c
-$(NAME)_SOURCES     += lv_ufs.c
-$(NAME)_SOURCES     += lv_trigo.c
-$(NAME)_SOURCES     += lv_math.c
-
-$(NAME)_INCLUDES    += ../../
-$(NAME)_INCLUDES    += ../
+$(NAME)_INCLUDES    += .
+$(NAME)_INCLUDES    += /lvgl
