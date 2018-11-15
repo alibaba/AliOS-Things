ed88924abfbf1f36bd4e5acfa1c51bd460ea5bc6
andy.zwj <andy.zwj@alibaba-inc.com>
2018-11-15 18:23:35 +0800

    BugID:17115413:relocate udata init function call
    Change-Id: Ieda5eed6d2dbac5ac20cdc7a872a88729a9db6ac

--- app/example/i2c_hts221_test/i2c_hts221_test.c
+++ app/example/i2c_hts221_test/i2c_hts221_test.c
@@ -26,7 +26,7 @@
 #include <aos/yloop.h>
 #include "netmgr.h"
 #include "aos/uData.h"
-
+#include "uData_queue.h"
 #define AOS_SENSOR_HUMI_SENSIRION_HTS221
 
 #define UDATA_PRINT    printf
@@ -53,97 +53,112 @@ do{\
 }while(0);
 
 
-void uData_report_demo(input_event_t *event, void *priv_data)
+int uData_local_publish(udata_type_e type, void* pdata, uint32_t len)
 {
+
+    if(pdata == NULL){
+        return -1;
+    }
+
+    switch (type) {
+    
+         case UDATA_SERVICE_PEDOMETER: {
+             accel_data_t *acc = (accel_data_t *)pdata;
+             UDATA_SHOW_UINT_3(type, (uint32_t)acc->timestamp, acc->data[0], acc->data[1], acc->data[2]);
+             break;
+         }
+    
+    
+         case UDATA_SERVICE_MAG: {
+             mag_data_t *mag = (mag_data_t *)pdata;
+             UDATA_SHOW_UINT_3(type, (uint32_t)mag->timestamp, mag->data[0], mag->data[1], mag->data[2]);
+             break;
+         }
+    
+         case UDATA_SERVICE_GYRO: {
+             gyro_data_t *gyro = (gyro_data_t *)pdata;
+             UDATA_SHOW_UINT_3(type, (uint32_t)gyro->timestamp, gyro->data[0], gyro->data[1], gyro->data[2]);
+             break;
+         }
+    
+         case UDATA_SERVICE_ALS: {
+             als_data_t *als = (als_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, als->timestamp, als->lux);
+             break;
+         }
+    
+         case UDATA_SERVICE_PS: {
+             proximity_data_t *ps = (proximity_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, ps->timestamp, ps->present);
+             break;
+         }
+    
+         case UDATA_SERVICE_BARO: {
+             barometer_data_t *baro = (barometer_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, baro->timestamp, baro->p);
+             break;
+         }
+    
+         case UDATA_SERVICE_TEMP: {
+             temperature_data_t *temp = (temperature_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, temp->timestamp, temp->t);
+             break;
+         }
+    
+         case UDATA_SERVICE_UV: {
+             uv_data_t *uv = (uv_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, uv->timestamp, uv->uvi);
+             break;
+         }
+    
+         case UDATA_SERVICE_HUMI: {
+             humidity_data_t *humi = (humidity_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, humi->timestamp, humi->h);
+             break;
+         }
+    
+         case UDATA_SERVICE_HALL: {
+             hall_data_t *hall = (hall_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, hall->timestamp, hall->hall_level);
+             break;
+         }
+         case UDATA_SERVICE_HR: {
+             heart_rate_data_t *heart = (heart_rate_data_t *)pdata;
+             UDATA_SHOW_UINT_1(type, heart->timestamp, heart->hear_rate);
+             break;
+         }
+    
+         case UDATA_SERVICE_GPS: {
+             gps_data_t *gps = (gps_data_t *)pdata;
+             UDATA_SHOW_FLOAT_3(type, gps->timestamp, gps->lat, gps->lon, gps->elv);
+             break;
+         }
+    
+         default:
+             return -1;
+     }
+
+    return 0;
+
+}
+
+void uData_report_demo(sensor_msg_pkg_t *msg)
+{
+    int ret = 0;
     udata_pkg_t buf;
-    if ((event == NULL) || (event->type != EV_UDATA)) {
+    if ((msg == NULL)) {
         return;
     }
 
-    if (event->code == CODE_UDATA_REPORT_PUBLISH) {
-        int ret = 0;
-        ret = uData_report_publish(event, &buf);
+    if (msg->cmd == UDATA_MSG_REPORT_PUBLISH) {
+        ret = uData_report_publish(msg->value, &buf);
         if (ret != 0) {
             return;
         }
-        switch (buf.type) {
-
-            case UDATA_SERVICE_ACC: {
-                accel_data_t *acc = (accel_data_t *)buf.payload;
-                UDATA_SHOW_UINT_3(buf.type, (uint32_t)acc->timestamp, acc->data[0], acc->data[1], acc->data[2]);
-                break;
-            }
-
-
-            case UDATA_SERVICE_MAG: {
-                mag_data_t *mag = (mag_data_t *)buf.payload;
-                UDATA_SHOW_UINT_3(buf.type, (uint32_t)mag->timestamp, mag->data[0], mag->data[1], mag->data[2]);
-                break;
-            }
-
-            case UDATA_SERVICE_GYRO: {
-                gyro_data_t *gyro = (gyro_data_t *)buf.payload;
-                UDATA_SHOW_UINT_3(buf.type, (uint32_t)gyro->timestamp, gyro->data[0], gyro->data[1], gyro->data[2]);
-                break;
-            }
-
-            case UDATA_SERVICE_ALS: {
-                als_data_t *als = (als_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, als->timestamp, als->lux);
-                break;
-            }
-
-            case UDATA_SERVICE_PS: {
-                proximity_data_t *ps = (proximity_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, ps->timestamp, ps->present);
-                break;
-            }
-
-            case UDATA_SERVICE_BARO: {
-                barometer_data_t *baro = (barometer_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, baro->timestamp, baro->p);
-                break;
-            }
-
-            case UDATA_SERVICE_TEMP: {
-                temperature_data_t *temp = (temperature_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, temp->timestamp, temp->t);
-                break;
-            }
-
-            case UDATA_SERVICE_UV: {
-                uv_data_t *uv = (uv_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, uv->timestamp, uv->uvi);
-                break;
-            }
-
-            case UDATA_SERVICE_HUMI: {
-                humidity_data_t *humi = (humidity_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, humi->timestamp, humi->h);
-                break;
-            }
-
-            case UDATA_SERVICE_HALL: {
-                hall_data_t *hall = (hall_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, hall->timestamp, hall->hall_level);
-                break;
-            }
-            case UDATA_SERVICE_HR: {
-                heart_rate_data_t *heart = (heart_rate_data_t *)buf.payload;
-                UDATA_SHOW_UINT_1(buf.type, heart->timestamp, heart->hear_rate);
-                break;
-            }
-
-            case UDATA_SERVICE_GPS: {
-                gps_data_t *gps = (gps_data_t *)buf.payload;
-                UDATA_SHOW_FLOAT_3(buf.type, (uint32_t)gps->timestamp, gps->lat, gps->lon, gps->elv);
-                break;
-            }
-
-
-            default:
-                break;
-
+        
+        ret = uData_local_publish(buf.type,buf.payload,0);
+        if (ret != 0) {
+            return;
         }
 
     }
@@ -152,8 +167,11 @@ void uData_report_demo(input_event_t *event, void *priv_data)
 int udata_sample(void)
 {
     int ret = 0;
-
-    aos_register_event_filter(EV_UDATA, uData_report_demo, NULL);
+    ret = uData_register_msg_handler(uData_report_demo);
+    if (ret == -1) {
+        LOG("%s %s %s %d\n", uDATA_STR, __func__, ERROR_LINE, __LINE__);
+        return ret;
+    }
 
     ret = uData_subscribe(UDATA_SERVICE_HUMI);
     if (ret != 0) {
@@ -166,6 +184,12 @@ int udata_sample(void)
 
 int application_start(int argc, char **argv)
 {
+    int ret;
+    ret = uData_main();
+    if (unlikely(ret)) {
+        return -1;
+    }
+
     LOG("application hts221_i2c test started.");
 
     udata_sample();
