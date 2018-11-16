859be7674c7062c8cbcaf8b4e984183948548d16
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-16 10:52:27 +0800

    BugID:17712046: Add Config.in/mk/Readme for fatfs
    Change-Id: I396ac1ae86c27180933c9b9fe1ef8b7f32c50d67

--- 3rdparty/experimental/fs/fatfs/fatfs.mk
+++ 3rdparty/experimental/fs/fatfs/fatfs.mk
@@ -1,20 +1,20 @@
 NAME := fatfs
 
-$(NAME)_MBINS_TYPE        := kernel
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := Fatfs filesystem
 
-$(NAME)_SOURCES     := fatfs.c
-$(NAME)_SOURCES     += diskio.c
-$(NAME)_SOURCES     += ff/ff.c
-$(NAME)_SOURCES     += ff/ffunicode.c
+$(NAME)_SOURCES := fatfs.c
+$(NAME)_SOURCES += diskio.c
+$(NAME)_SOURCES += ff/ff.c
+$(NAME)_SOURCES += ff/ffunicode.c
 
 #default gcc
 ifeq ($(COMPILER),)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 endif
 
-GLOBAL_INCLUDES     += include ff/include
-GLOBAL_DEFINES      += AOS_FATFS
+GLOBAL_INCLUDES += include ff/include
+GLOBAL_DEFINES  += AOS_FATFS
