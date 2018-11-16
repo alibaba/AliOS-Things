b6330019bbc448a3941f6262276f9364c9e9a8f6
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-16 11:28:13 +0800

    BugID:17813138: Add Config.in/mk/Readme for cramfs
    Change-Id: I72c306e482ab7fa5abb7bd4f164faaccb0af6b79

--- 3rdparty/experimental/fs/cramfs/cramfs.mk
+++ 3rdparty/experimental/fs/cramfs/cramfs.mk
@@ -1,23 +1,23 @@
 NAME := cramfs
 
-$(NAME)_MBINS_TYPE        := kernel
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := compress ROM filesystem
 
-$(NAME)_SOURCES     +=  src/cramfs.c \
-                        src/uncompress.c \
-                        src/flashio.c \
-                        vfs_cramfs.c
+$(NAME)_SOURCES += src/cramfs.c \
+                   src/uncompress.c \
+                   src/flashio.c \
+                   vfs_cramfs.c
 
 #default gcc
 ifeq ($(COMPILER),)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 endif
 
-$(NAME)_INCLUDES    += include
-$(NAME)_COMPONENTS  += zlib
+$(NAME)_INCLUDES   += include
+$(NAME)_COMPONENTS += zlib
 
-GLOBAL_INCLUDES     += .
-GLOBAL_DEFINES      += AOS_CRAMFS
+GLOBAL_INCLUDES += .
+GLOBAL_DEFINES  += AOS_CRAMFS
