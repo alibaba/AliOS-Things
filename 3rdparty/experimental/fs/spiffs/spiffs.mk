4ebbea7e5404ab36b1a9a6aa950c32a155c36db7
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-16 11:14:32 +0800

    BugID:17712068: Add Config.in/mk/Readme for SPIFFS and fix SPIFFS compile error
    Change-Id: If3d36cb4d72aec9964d4a8c758f6e3916a6cb86b

--- 3rdparty/experimental/fs/spiffs/spiffs.mk
+++ 3rdparty/experimental/fs/spiffs/spiffs.mk
@@ -1,22 +1,22 @@
 NAME := spiffs
 
-$(NAME)_MBINS_TYPE        := kernel
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := SPIFFS filesystem
 
-$(NAME)_SOURCES     +=  spiffs_port.c \
-                        spiffs/spiffs_cache.c \
-                        spiffs/spiffs_check.c \
-                        spiffs/spiffs_gc.c \
-                        spiffs/spiffs_hydrogen.c \
-                        spiffs/spiffs_nucleus.c
+$(NAME)_SOURCES += spiffs_port.c \
+                   spiffs/spiffs_cache.c \
+                   spiffs/spiffs_check.c \
+                   spiffs/spiffs_gc.c \
+                   spiffs/spiffs_hydrogen.c \
+                   spiffs/spiffs_nucleus.c
 
 #default gcc
 ifeq ($(COMPILER),)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 endif
 
-GLOBAL_INCLUDES     += include spiffs/include
-GLOBAL_DEFINES      += AOS_SPIFFS
+GLOBAL_INCLUDES += include spiffs/include
+GLOBAL_DEFINES  += AOS_SPIFFS
