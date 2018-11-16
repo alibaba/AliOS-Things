e05d4e266b1c9260cd98280cb5c96526920f8b10
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-11-16 22:45:39 +0800

    BugID:17883722:Move sal/wifi and sal/gprs to sal/hal/
    Change-Id: Iac616b60947cccd14fe0c700176d692ec6c9b51f

--- network/sal/hal/wifi/bk7231/bk7231.mk
+++ network/sal/hal/wifi/bk7231/bk7231.mk
@@ -9,7 +9,7 @@ GLOBAL_DEFINES += DEV_SAL_BK7231
 $(NAME)_COMPONENTS += yloop
 
 ifneq (1, $(at_adapter))
-$(NAME)_COMPONENTS += sal network.sal.atparser
+$(NAME)_COMPONENTS += network.sal.atparser
 
 $(NAME)_SOURCES += bk7231.c
 endif
