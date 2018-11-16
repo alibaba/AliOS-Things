dcd6617add2f354d05f79d78005c077c377bee87
jianlong.gaojianlo <jianlong.gaojianlo@alibaba-inc.com>
2018-11-16 14:40:07 +0800

    BugID:17688849: Refine developerkit gui
    Devkit is the entrance of all simple apps for developerkit
    
    Change-Id: Ic2b1091122f5c3196d9b0e9438b9e523d46dbe66

--- app/example/devkit/dk_gui/dk_gui.mk
+++ app/example/devkit/dk_gui/dk_gui.mk
@@ -1,19 +1,25 @@
-NAME := littlevgl_developerkit
+NAME := devkit_gui
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 0.0.1
 $(NAME)_SUMMARY :=
-$(NAME)_SOURCES := littlevgl_developerkit.c AliOS_Things_logo.c sensor_display.c
-
-GLOBAL_DEFINES += LITTLEVGL_DEVELOPERKIT
+$(NAME)_SOURCES := developerkitgui.c AliOS_Things_logo.c sensor_display.c house.c weather.c
 
 $(NAME)_COMPONENTS := yloop cli
+
+ENABLE_IRDA_HAL := 1
+
+ENABLE_CAMERA_HAL := 1
 
 ifeq ($(BENCHMARKS),1)
 $(NAME)_COMPONENTS  += benchmarks
 GLOBAL_DEFINES      += CONFIG_CMD_BENCHMARKS
 endif
 
+$(NAME)_INCLUDES    += .
+
 $(NAME)_COMPONENTS += 3rdparty.experimental.gui.littlevGL
+
+GLOBAL_DEFINES += LITTLEVGL_DEVELOPERKIT
 
 GLOBAL_INCLUDES += ./
