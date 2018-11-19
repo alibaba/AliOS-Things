3300d92a9febdf885a16ed990e2a14d45b38c7a3
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-11-19 17:19:09 +0800

    BugID:17906082: add mk/config.in/readme for boards
    Change-Id: I79061f6b54dc544bfdc19569db0104e7f7ffa318

--- board/stm32f411re-nucleo/stm32f411re-nucleo.mk
+++ board/stm32f411re-nucleo/stm32f411re-nucleo.mk
@@ -1,48 +1,42 @@
 NAME := stm32f411re-nucleo
 
-
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
-MODULE               := 1062
-HOST_ARCH            := Cortex-M4
-HOST_MCU_FAMILY      := stm32f4xx_cube
-SUPPORT_BINS         := no
-HOST_MCU_NAME        := STM32F411RET
-ENABLE_VFP           := 1
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := configuration for board stm32f411re-nucleo
+MODULE             := 1062
+HOST_ARCH          := Cortex-M4
+HOST_MCU_FAMILY    := stm32f4xx_cube
+SUPPORT_MBINS      := no
+HOST_MCU_NAME      := STM32F411RET
+ENABLE_VFP         := 1
 
 $(NAME)_SOURCES += aos/board.c \
                    aos/soc_init.c
 
 $(NAME)_SOURCES += Src/stm32f4xx_hal_msp.c \
-                   Src/main.c \
-	           Src/stm32f4xx_it.c 
+                   Src/main.c              \
+                   Src/stm32f4xx_it.c
 
-AOS_NETWORK_SAL ?= y
+AOS_NETWORK_SAL    ?= y
 ifeq (y,$(AOS_NETWORK_SAL))
 $(NAME)_COMPONENTS += linkkit/sdk-c/src/services/mdal/sal
-module ?= wifi.mk3060
+module             ?= wifi.mk3060
 endif
 
-
-
-                   
 ifeq ($(COMPILER), armcc)
-$(NAME)_SOURCES += startup_stm32f411xe_keil.s    
+$(NAME)_SOURCES    += startup_stm32f411xe_keil.s
 $(NAME)_LINK_FILES := startup_stm32f411xe_keil.o
 else ifeq ($(COMPILER), iar)
-$(NAME)_SOURCES += startup_stm32f411xe_iar.s  
+$(NAME)_SOURCES    += startup_stm32f411xe_iar.s
 else
-$(NAME)_SOURCES += startup_stm32f411xe.s
+$(NAME)_SOURCES    += startup_stm32f411xe.s
 endif
 
-GLOBAL_INCLUDES += . \
+GLOBAL_INCLUDES += .    \
                    aos/ \
                    Inc/
-				   
+
 GLOBAL_CFLAGS += -DSTM32F411xE -DSRAM1_SIZE_MAX=0x20000 -DCENTRALIZE_MAPPING
-
-
 
 ifeq ($(COMPILER),armcc)
 GLOBAL_LDFLAGS += -L --scatter=board/stm32f411re-nucleo/stm32f411xe.sct
@@ -52,10 +46,8 @@ else
 GLOBAL_LDFLAGS += -T board/stm32f411re-nucleo/STM32F411RETx_FLASH.ld
 endif
 
-
-
 CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_f411-nucleo
-CONFIG_SYSINFO_DEVICE_NAME := f411-nucleo
+CONFIG_SYSINFO_DEVICE_NAME   := f411-nucleo
 
 GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
 GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
