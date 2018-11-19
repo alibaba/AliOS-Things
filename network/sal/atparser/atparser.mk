e57927ed4808f53dd6c4e16a6bf92fb5ab0f383a
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-11-19 21:27:34 +0800

    BugID:17910536:changes for atparser component
    Change-Id: I9fd25b5cbfd2e0cd607499594d9fbc6ddf03b13b

--- network/sal/atparser/atparser.mk
+++ network/sal/atparser/atparser.mk
@@ -1,27 +1,15 @@
-##
- # Copyright (C) 2015-2017 Alibaba Group Holding Limited
-##
-
 NAME := atparser
-
-with_atparser ?= 1
-
-GLOBAL_INCLUDES += ./
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 0.0.1
 $(NAME)_SUMMARY :=
 
+GLOBAL_INCLUDES += ./
+
 $(NAME)_SOURCES := atparser.c
 GLOBAL_DEFINES += AOS_ATCMD
 
-ifeq (1,$(hdlc))
+ifeq (y,$(FEATURE_HDLC_UART))
 $(NAME)_SOURCES += hdlc.c
 GLOBAL_DEFINES += HDLC_UART
-endif
-
-# Follow original modification, it seems needed to sleep a little bit
-# due to developerkit(bk7231 wifi module inside) limited resource
-ifeq ($(CONFIG_SYSINFO_DEVICE_NAME),developerkit)
-GLOBAL_DEFINES += AT_PARSER_DELAY_FLAG
 endif
