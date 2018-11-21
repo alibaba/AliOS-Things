843f4e5bb314bc7cfb911871f78e97b7f0077652
lingjun chen <lingjun.clj@alibaba-inc.com>
2018-11-20 22:39:14 -0800

    BugID:16731946:delete mm blk head
    Change-Id: I3771722eb8bd000ab8ce0827b926681433192d0c

--- kernel/rhino/core/k_stats.c
+++ kernel/rhino/core/k_stats.c
@@ -11,10 +11,6 @@ void kobj_list_init(void)
     klist_init(&(g_kobj_list.task_head));
     klist_init(&(g_kobj_list.mutex_head));
 
-#if (RHINO_CONFIG_MM_BLK > 0)
-    klist_init(&(g_kobj_list.mblkpool_head));
-#endif
-
 #if (RHINO_CONFIG_SEM > 0)
     klist_init(&(g_kobj_list.sem_head));
 #endif
