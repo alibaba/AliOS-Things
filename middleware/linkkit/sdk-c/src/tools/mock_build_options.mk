ab5cd590bb5dafe6412b29818ba2b1e0ddda65bd
Yang, Xiao <yusheng.yx@alibaba-inc.com>
2018-11-21 11:35:49 +0800

    BugID:17646749:[mingw32] fix build error when generate Windows version SDK
    Change-Id: Iac6d560cba49a36ef572f8ce1839ed7bcd29166d
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/tools/mock_build_options.mk
+++ middleware/linkkit/sdk-c/src/tools/mock_build_options.mk
@@ -1,6 +1,8 @@
 
 ifneq (,$(filter -D_PLATFORM_IS_WINDOWS_,$(CFLAGS)))
     CFLAGS := $(filter-out -DOTA_ENABLED,$(CFLAGS))
+    CFLAGS := $(filter-out -DWIFI_PROVISION_ENABLED,$(CFLAGS))
+    CFLAGS := $(filter-out -DDEV_BIND_ENABLED,$(CFLAGS))
 endif
 
 ifeq (Darwin,$(shell uname))
