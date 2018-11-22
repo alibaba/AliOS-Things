a5615a3b3720fd1348dad493f52b1c3f307e3463
east moutain <east.moutain.yang@gmail.com>
2018-11-22 16:16:58 +0800

    BugID:17712290: uspace dir component revolution
    	new file:   uspace/Config.in
    	renamed:    rhino/uspace/include/u_api.h -> uspace/include/u_api.h
    	renamed:    rhino/uspace/include/u_mm.h -> uspace/include/u_mm.h
    	renamed:    rhino/uspace/include/u_proc_msg.h -> uspace/include/u_proc_msg.h
    	renamed:    rhino/uspace/include/u_task.h -> uspace/include/u_task.h
    	renamed:    rhino/uspace/include/u_timer.h -> uspace/include/u_timer.h
    	renamed:    rhino/uspace/syscall/include/syscall.h -> uspace/syscall/include/syscall.h
    	renamed:    rhino/uspace/syscall/include/syscall_no.h -> uspace/syscall/include/syscall_no.h
    	renamed:    rhino/uspace/syscall/ksyscall/ksyscall.c -> uspace/syscall/ksyscall/ksyscall.c
    	renamed:    rhino/uspace/syscall/usyscall/usyscall.c -> uspace/syscall/usyscall/usyscall.c
    	renamed:    rhino/uspace/u_mm.c -> uspace/u_mm.c
    	renamed:    rhino/uspace/u_proc_msg.c -> uspace/u_proc_msg.c
    	renamed:    rhino/uspace/u_res_task.c -> uspace/u_res_task.c
    	renamed:    rhino/uspace/u_task.c -> uspace/u_task.c
    	renamed:    rhino/uspace/u_timer.c -> uspace/u_timer.c
    	renamed:    rhino/uspace/uspace.mk -> uspace/uspace.mk
    
    Change-Id: I33d1c78d1fd4c9f8f81ffd3295098cc57c85b1a2

--- kernel/uspace/include/u_proc_msg.h
+++ kernel/uspace/include/u_proc_msg.h
@@ -5,10 +5,21 @@
 #ifndef U_PROC_H
 #define U_PROC_H
 
-size_t  krhino_msg_get(uint32_t key, uint32_t flg, size_t size);
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+size_t krhino_msg_get(uint32_t key, uint32_t flg, size_t size);
+
 kstat_t krhino_msg_snd(size_t msq_id, void *msg, size_t msg_sz);
+
 kstat_t krhino_msg_recv(size_t msq_id, tick_t ticks, void *msg, size_t *msg_sz);
+
 kstat_t krhino_msg_del(size_t msq_id);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* U_PROC_H */
 
