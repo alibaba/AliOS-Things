a96f4a1623e07beb848156022d8bf8d4d9ee6e9e
andy.zwj <andy.zwj@alibaba-inc.com>
2018-11-22 21:22:47 +0800

    BugID: 17115413:Format adjustment for udata
    Change-Id: I5eee61dc4431aaa17da21bb656bc358fa70a9e66

--- middleware/udata/include/service_mgr.h
+++ middleware/udata/include/service_mgr.h
@@ -11,17 +11,20 @@
 #ifndef SERVICE_MANAGER_H
 #define SERVICE_MANAGER_H
 
-#include "uData_com_desc.h"
+#include "udata_com_desc.h"
 
 int          uData_service_init(void);
 int          uData_service_mgr_init(void);
 int          uData_find_service(udata_type_e type);
 int          uData_service_register(uData_service_t *service);
 int          uData_service_unregister(udata_type_e type);
-int          uData_get_report_pkg(void *buf, uint32_t index);
+int          uData_get_report_pkg(udata_type_e type, void *buf);
 int          uData_service_subscribe(udata_type_e type);
 int          uData_service_unsubscribe(udata_type_e type);
-int          uData_service_get_payload(uint32_t abs_index, void **paddr, uint32_t *plen);
+int          uData_service_get_payload(uint32_t abs_index, void **pdata, uint32_t *plen);
+int          uData_install_report_pkg(udata_type_e type, void *data, size_t len);
+int          uData_service_cb_register(udata_type_e type, SERVICE_PROCESS_CB proc,SERVICE_IOCTL_CB ioctl);
+int          uData_service_cb_unregister(udata_type_e type);
 
 
 #endif /*SERVICE_MANAGER_H*/
