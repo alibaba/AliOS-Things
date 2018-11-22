4e8737a60e3a637364180d6fadb0473bd4ee149d
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2018-11-22 19:53:43 +0800

    BugID:17958917: Add BLE Mesh commands support.
    Change-Id: I84be36c66931faeca3268812d683b492eb3d9e1c

---
+++ network/bluetooth/bt_mesh/aos.mk
@@ -0,0 +1,32 @@
+NAME := bt_mesh
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 0.0.1
+$(NAME)_SUMMARY :=
+
+$(NAME)_COMPONENTS-y += network.bluetooth.bt.bt_common network.bluetooth.bt_mesh.port
+
+$(NAME)_INCLUDES-y += ./inc/ \
+                      ./inc/api/mesh/
+
+$(NAME)_COMPONENTS-y += yloop
+
+$(NAME)_SOURCES-y := ./src/access.c \
+                     ./src/adv.c \
+                     ./src/beacon.c \
+                     ./src/crypto.c \
+                     ./src/cfg_srv.c \
+                     ./src/cfg_cli.c \
+                     ./src/health_srv.c \
+                     ./src/health_cli.c \
+                     ./src/main.c \
+                     ./src/net.c \
+                     ./src/prov.c \
+                     ./src/proxy.c \
+                     ./src/transport.c \
+                     ./src/shell.c
+
+GLOBAL_INCLUDES-y += ./inc/ \
+                     ./inc/api/
+
+GLOBAL_DEFINES-y += CONFIG_BT_MESH
