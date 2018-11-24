e59f232a570a679df996211bb210f417963efa6d
east moutain <east.moutain.yang@gmail.com>
2018-11-24 15:06:36 +0800

    BugID:16846667: enable user task deletion function
    Detail:
        - add u_res_task into app's complilation list
        - added queue syscall API
    
    Change-Id: I379f5b3db9ac5af45f4fa81eb0b8d26d5863c1ff

--- kernel/uspace/uspace.mk
+++ kernel/uspace/uspace.mk
@@ -17,6 +17,7 @@ $(NAME)_SOURCES    += u_proc_msg.c
 $(NAME)_SOURCES    += syscall/ksyscall/ksyscall.c
 else ifeq ($(MBINS),app)
 $(NAME)_SOURCES    := u_timer.c
+$(NAME)_SOURCES    += u_res_task.c
 $(NAME)_SOURCES    += u_mm.c
 $(NAME)_SOURCES    += syscall/usyscall/usyscall.c
 GLOBAL_DEFINES     += AOS_UMM
