206238666a261bb4801817bbc051712cb530cf51
李诚 lc122798 <lc122798@alibaba-inc.com>
2018-11-24 09:51:05 +0800

    Revert "BugID:17974259: imx6 mk modify; dir board"
    This reverts commit e750139bdfb9945f122973b2b646dd653a586055.
    
    Change-Id: I32cb6590a7098b0aea4b5c5dda32a3f0c0e6e24e

--- board/imx6sl/imx6sl.mk
+++ board/imx6sl/imx6sl.mk
@@ -5,8 +5,7 @@ $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := configuration for board imx6sl
 MODULE             := imx6sl
 HOST_ARCH          := Cortex-A9
-HOST_MCU_FAMILY    := imx6
-HOST_MCU_NAME      := imx6sl
+HOST_MCU_FAMILY    := imx6.imx6sl
 SUPPORT_MBINS      := no
 
 $(NAME)_SOURCES := board.c
