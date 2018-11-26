818848328bf609665d3ac5476fef363e0ebefb1c
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-26 11:27:11 +0800

    BugID:17865306: Add module enable flag check during osal building
    Change-Id: I10495f92046e54052f8b665da4f31c787834c455

--- osal/aos/include/aos/aos.h
+++ osal/aos/include/aos/aos.h
@@ -10,15 +10,31 @@ extern "C" {
 #endif
 
 #include <aos/types.h>
+
+#ifdef CONFIG_AOS_CLI
 #include <aos/cli.h>
+#endif
+
 #include <aos/debug.h>
 #include <aos/kernel.h>
+
+#ifdef AOS_KV
 #include <aos/kv.h>
+#endif
+
 #include <aos/list.h>
 #include <aos/log.h>
+
+#ifdef AOS_VFS
 #include <vfs.h>
+#endif
+
 #include <aos/version.h>
+
+#ifdef AOS_LOOP
 #include <aos/yloop.h>
+#endif
+
 #include <aos/errno.h>
 #include <aos/init.h>
 
