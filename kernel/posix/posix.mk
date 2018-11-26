f35a642a6dffad10d2a1a8793e24f9e510147753
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-11-26 15:45:11 +0800

    BugID:17988925: modify kernel component of mk/config.in/readme
    Change-Id: I838ce53441edf80d7b4d798d42d07df8c8b7b8b8

--- kernel/posix/posix.mk
+++ kernel/posix/posix.mk
@@ -1,29 +1,29 @@
 NAME := posix
 
-$(NAME)_MBINS_TYPE  := kernel
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := posix for alios things
 
-$(NAME)_SOURCES     := pthread.c
-$(NAME)_SOURCES     += pthread_attr.c
-$(NAME)_SOURCES     += pthread_cond.c
-$(NAME)_SOURCES     += pthread_mutex.c
-$(NAME)_SOURCES     += pthread_sched.c
-$(NAME)_SOURCES     += pthread_tsd.c
-$(NAME)_SOURCES     += posix_timer.c
-$(NAME)_SOURCES     += posix_semaphore.c
-$(NAME)_SOURCES     += posix_mqueue.c
-$(NAME)_SOURCES     += posix_dirent.c
-$(NAME)_SOURCES     += posix_prctl.c
+$(NAME)_SOURCES := pthread.c
+$(NAME)_SOURCES += pthread_attr.c
+$(NAME)_SOURCES += pthread_cond.c
+$(NAME)_SOURCES += pthread_mutex.c
+$(NAME)_SOURCES += pthread_sched.c
+$(NAME)_SOURCES += pthread_tsd.c
+$(NAME)_SOURCES += posix_timer.c
+$(NAME)_SOURCES += posix_semaphore.c
+$(NAME)_SOURCES += posix_mqueue.c
+$(NAME)_SOURCES += posix_dirent.c
+$(NAME)_SOURCES += posix_prctl.c
 
 #default gcc
 ifeq ($(COMPILER),)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 else ifeq ($(COMPILER),armcc)
-GLOBAL_DEFINES      += __BSD_VISIBLE
+GLOBAL_DEFINES += __BSD_VISIBLE
 endif
 
-GLOBAL_INCLUDES     += include
-GLOBAL_DEFINES      += AOS_POSIX
+GLOBAL_INCLUDES += include
+GLOBAL_DEFINES  += AOS_POSIX
