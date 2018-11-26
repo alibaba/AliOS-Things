5520a05964bd2a044050755ecf6bd5d8d4f9b1ff
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-11-26 21:20:31 +0800

    BugID:17996932:Change makefile name to aos.mk
    Change-Id: Iafdc83300883c98f5b7f4be20ce9e0da0a4c6971

--- network/sal/atparser/aos.mk
+++ network/sal/atparser/aos.mk
@@ -2,7 +2,7 @@ NAME := atparser
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := component for sending and receiving AT command over uart
 
 GLOBAL_INCLUDES += ./
 
--- network/sal/hal/wifi/mk3060/aos.mk
+++ network/sal/hal/wifi/mk3060/aos.mk
@@ -2,7 +2,7 @@ NAME := device_sal_mk3060
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := sal hal implementation for mk3060
 $(NAME)_SOURCES += wifi_atcmd.c
 GLOBAL_DEFINES += DEV_SAL_MK3060
 
