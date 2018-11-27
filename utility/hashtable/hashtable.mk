40f09e84fb15f39e41755833a2e465891f1fd0dc
chenan.xxw <chenan.xxw@alibaba-inc.com>
2018-11-27 13:44:04 +0800

    BugID:17707347: add SUMMARY in mk files
    Change-Id: I422d1bcf0f8bd21ff2a3fbc0938c4dd822c646c7

--- utility/hashtable/hashtable.mk
+++ utility/hashtable/hashtable.mk
@@ -2,7 +2,7 @@ NAME := hashtable
 
 $(NAME)_MBINS_TYPE := share
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := The hashtable provides friendly APIs to create, add, find hash table.
 
 $(NAME)_SOURCES := hashtable.c
 $(NAME)_INCLUDES := ../../middleware/alink/protocol/os/
