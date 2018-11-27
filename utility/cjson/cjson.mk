7067884b9d13fa6e307d82852973cb88e61e3388
chenan.xxw <chenan.xxw@alibaba-inc.com>
2018-11-27 13:44:04 +0800

    BugID:17707347: add SUMMARY in mk files
    Change-Id: I422d1bcf0f8bd21ff2a3fbc0938c4dd822c646c7

--- utility/cjson/cjson.mk
+++ utility/cjson/cjson.mk
@@ -2,7 +2,7 @@ NAME := cjson
 
 $(NAME)_MBINS_TYPE := share
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := An ultra-lightweight, portable, single-file, simple-as-can-be ANSI-C compliant JSON parser, under MIT license.
 
 GLOBAL_INCLUDES += include
 # don't modify to L_CFLAGS, because CONFIG_CJSON_WITHOUT_DOUBLE should enable global
