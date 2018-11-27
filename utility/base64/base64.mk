ee56e1bcc951059d1b3ca6a936b35a4d39aad1d5
chenan.xxw <chenan.xxw@alibaba-inc.com>
2018-11-27 13:44:04 +0800

    BugID:17707347: add SUMMARY in mk files
    Change-Id: I422d1bcf0f8bd21ff2a3fbc0938c4dd822c646c7

--- utility/base64/base64.mk
+++ utility/base64/base64.mk
@@ -2,7 +2,7 @@ NAME := base64
 
 $(NAME)_MBINS_TYPE := share
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := base64 encode/decode
 
 $(NAME)_SOURCES     := base64.c
 
--- utility/digest_algorithm/digest_algorithm.mk
+++ utility/digest_algorithm/digest_algorithm.mk
@@ -2,7 +2,7 @@ NAME := digest_algorithm
 
 $(NAME)_MBINS_TYPE := share
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := digest_algorithm provides many checksum and digest algorithm with the friendly APIs.
 
 $(NAME)_SOURCES := digest_algorithm.c CheckSumUtils.c crc.c crc16.c md5.c
 GLOBAL_INCLUDES += .
