072481a126f786ea6f38df7f21fa68f316712d0f
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2018-11-27 10:42:36 +0800

    BugID:17847784: Rework some makefile in bluetooth directory.
    Change-Id: I8b68f7cef17e37d2d9e69591f9d86f2f1730b6a4

---
+++ network/bluetooth/bt/aos.mk
@@ -0,0 +1,67 @@
+NAME := bt
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 0.0.1
+$(NAME)_SUMMARY :=
+GLOBAL_DEFINES-y += AOS_BT
+
+GLOBAL_INCLUDES-y += include \
+                   include/drivers
+
+$(NAME)_INCLUDES += bt_common/tinycrypt/include \
+                    ../../rhino/core/include
+
+$(NAME)_COMPONENTS-y += yloop
+
+$(NAME)_COMPONENTS-y += network.bluetooth.bt.bt_common
+
+$(NAME)_SOURCES-y := host/uuid.c \
+                     host/hci_core.c \
+                     host/conn.c \
+                     host/l2cap.c \
+                     host/att.c \
+                     host/gatt.c \
+                     host/crypto.c \
+                     host/smp.c \
+                     host/keys.c
+
+$(NAME)_SOURCES-y += host/hci_ecc.c
+
+bt_controller?=0
+ifeq ($(bt_controller), 1)
+$(NAME)_COMPONENTS-y += network.bluetooth.bt.controller
+GLOBAL_DEFINES-y += CONFIG_BT_CTLR
+endif
+
+ifeq ($(hci_h4),1)
+$(NAME)_SOURCES-y += hci_driver/h4.c
+endif
+
+ifeq ($(COMPILER),)
+$(NAME)_CFLAGS-y      += -Wall -Werror
+else ifeq ($(COMPILER),gcc)
+$(NAME)_CFLAGS-y      += -Wall -Werror
+endif
+
+GLOBAL_DEFINES-y += CONFIG_AOS_BLUETOOTH
+GLOBAL_DEFINES-y += CONFIG_BLUETOOTH
+GLOBAL_DEFINES-y += CONFIG_BT_PERIPHERAL
+GLOBAL_DEFINES-y += CONFIG_BT_SMP
+GLOBAL_DEFINES-y += CONFIG_BT_CONN
+
+GLOBAL_DEFINES-y += CONFIG_BLE_50
+
+## BLE debug log general control macro (Note: still to be affected by DEBUG)
+## Enable below macros if BLE stack debug needed
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_LOG
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG
+
+## BLE subsystem debug log control macro
+## Enable below macros if component-specific debug needed
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_L2CAP
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_CONN
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_ATT
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_GATT
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_HCI_DRIVER
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_HCI_CORE
+#GLOBAL_DEFINES += CONFIG_BT_DEBUG_CORE
