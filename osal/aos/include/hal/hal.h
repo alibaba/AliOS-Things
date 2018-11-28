9338577999596e2c67f4b25cd990d0a0f9c0e1bd
chongling <yj144830@alibaba-inc.com>
2018-11-28 13:45:47 +0800

    BugID:17784971:move kernel/hal to osal 2
    Change-Id: I61cbc1b3eacb5da9af939b6f23262881a1f08553

---
+++ osal/aos/include/hal/hal.h
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef HAL_HAL_H_
+#define HAL_HAL_H_
+
+#include <stdint.h>
+#include <stddef.h>
+
+#include "hal/base.h"
+#include "hal/wifi.h"
+
+#include "hal/soc/soc.h"
+
+#endif /* HAL_HAL_H */
