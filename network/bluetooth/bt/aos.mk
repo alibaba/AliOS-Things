4f3b9c41e416485fc6f557426400ac148ca2451c
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2018-11-28 17:29:00 +0800

    BugID:17835344: Add SUMMARY for components under network/bluetooth.
    Change-Id: I05f38d1e9c16c57a7dd06586e12d5a52d194a2f2

--- network/bluetooth/bt/aos.mk
+++ network/bluetooth/bt/aos.mk
@@ -2,7 +2,8 @@ NAME := bt
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := BLE stack.
+
 GLOBAL_DEFINES-y += AOS_BT
 
 GLOBAL_INCLUDES-y += include \
--- network/bluetooth/profile/aos.mk
+++ network/bluetooth/profile/aos.mk
@@ -1,6 +1,8 @@
 NAME := profile
 
 $(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 0.0.1
+$(NAME)_SUMMARY := BLE commonly used profiles.
 
 $(NAME)_SOURCES-y := bas.c cts.c cts.h dis.c hrs.c
 
