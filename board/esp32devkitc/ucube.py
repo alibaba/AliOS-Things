a143ee615cee42892c4f7193164d28f9c0aebbf2
Li Cheng <lc122798@alibaba-inc.com>
2018-11-28 10:27:27 +0800

    BugID:16857751:[autobuild]remove nolonger exist alinkapp from autobuild targets
    Change-Id: I51a7e4e0f1b65afaa5b38475a361d8e7e55b3a0e

--- board/esp32devkitc/ucube.py
+++ board/esp32devkitc/ucube.py
@@ -7,4 +7,4 @@ if aos_global_config.get('hci_h4', 0):
 
 
 platform_options="wifi=1"
-linux_only_targets="alinkapp bluetooth.bleadv bluetooth.bleperipheral helloworld networkapp acapp linkkitapp mqttapp coapapp linkkit_gateway comboapp hdlcapp.hdlcserver tls meshapp bluetooth.blemesh smpapp bluetooth.breezeapp bluetooth.blemesh_cli blink wifihalapp udataapp netmgrapp nano helloworld_nocli bluetooth.blemesh_srv athostapp comboapp cryptotest yts bluetooth.blemesh_tmall i2c_hts221_test otaapp kernel_test http2app devkit.dk_ld"
+linux_only_targets="bluetooth.bleadv bluetooth.bleperipheral helloworld networkapp acapp linkkitapp mqttapp coapapp linkkit_gateway comboapp hdlcapp.hdlcserver tls meshapp bluetooth.blemesh smpapp bluetooth.breezeapp bluetooth.blemesh_cli blink wifihalapp udataapp netmgrapp nano helloworld_nocli bluetooth.blemesh_srv athostapp comboapp cryptotest yts bluetooth.blemesh_tmall i2c_hts221_test otaapp kernel_test http2app devkit.dk_ld"
