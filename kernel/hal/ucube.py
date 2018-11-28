cb109003a8401126117f865812800f1d63f7543c
chongling <yj144830@alibaba-inc.com>
2018-11-28 11:23:59 +0800

    BugID:17784971:move kernel/hal to osal
    Change-Id: I2b89f55c255fcd3347eda4e2e87b059ca3e05920

--- kernel/hal/ucube.py
+++
@@ -1,8 +0,0 @@
-src     = Split('''
-        wifi.c
-        ota.c					
-''')
-component = aos_component('hal', src)
-
-component.add_global_macros('AOS_HAL')
-component.add_global_includes('include', '../rhino', '../rhino/hal/soc')
