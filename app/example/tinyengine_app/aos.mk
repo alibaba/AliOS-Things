79e5dd7e8fd14ebc4f0dcc117a678d4a2ac16b37
lijun.xlj <lijun.xlj@alibaba-inc.com>
2018-11-28 18:46:09 +0800

    BugID:18028258:rebase tinyengine from 1.3.0 to latest alios things
    Change-Id: I5ab89dd4a27eb6f96d60341144a7444808b20566

---
+++ app/example/tinyengine_app/aos.mk
@@ -0,0 +1,16 @@
+NAME := tinyengine_app
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 0.0.1
+$(NAME)_SUMMARY := an engine for javascript application development
+$(NAME)_SOURCES := tinyengine_app.c
+
+#choose JSE_TINYENGINE OR JSE_DUKTAPE here
+#export AOS_CONFIG_TINYENGINE_TYPE=JSE_DUKTAPE
+export AOS_CONFIG_TINYENGINE_TYPE=JSE_TINYENGINE
+
+$(NAME)_COMPONENTS := log yloop cli feature.linkkit-mqtt cjson netmgr middleware.common middleware.uagent.uota
+$(NAME)_COMPONENTS += 3rdparty.experimental.fs.spiffs
+$(NAME)_COMPONENTS += middleware.tinyengine
+
+GLOBAL_INCLUDES += ./
