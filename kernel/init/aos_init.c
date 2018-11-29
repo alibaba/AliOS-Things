1678fce0288f1aee3f30bd5bb3de17da2302076b
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2018-11-29 09:44:22 +0800

    BugID:18044450:Rm not used AOS_HAL_WIFI macro
    Change-Id: Ic72c08049fa3dc091e7e41d11fbf3ac4f1c72256

--- kernel/init/aos_init.c
+++ kernel/init/aos_init.c
@@ -151,7 +151,6 @@ void tcpip_cli_init(void)
 }
 #endif
 
-#ifdef AOS_HAL_WIFI
 void wifi_debug_cmd(char *buf, int len, int argc, char **argv)
 {
     hal_wifi_start_debug_mode(NULL);
@@ -210,7 +209,6 @@ void hal_wifi_cli_init(void)
 {
     aos_cli_register_commands(&wifi_cli_cmd[0],sizeof(wifi_cli_cmd) / sizeof(struct cli_command));
 }
-#endif
 
 void cli_service_init(kinit_t *kinit)
 {
@@ -224,9 +222,7 @@ void cli_service_init(kinit_t *kinit)
 #ifndef CONFIG_NO_TCPIP
         tcpip_cli_init();
 #endif
-#ifdef AOS_HAL_WIFI
         hal_wifi_cli_init();
-#endif
     }
     return;
 }
