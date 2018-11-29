2ff9cf3ccdeebda5134fd537602d6a6ea4d76893
yx170385 <yx170385@alibaba-inc.com>
2018-11-29 16:50:04 +0800

    BugID:18050250:aos_rhino api sort
    Change-Id: I38607a5ec83ea20a2d0fbb32c37c88531500f979

--- osal/aos/aos_common.c
+++ osal/aos/aos_common.c
@@ -1,2 +1,21 @@
 #include "aos_common.h"
+#include <k_config.h>
+
 const char *SYSINFO_KERNEL_VERSION="AOS-R-2.0.0";
+
+extern void hal_reboot(void);
+
+void aos_reboot(void)
+{
+    hal_reboot();
+}
+
+int aos_get_hz(void)
+{
+    return RHINO_CONFIG_TICKS_PER_SECOND;
+}
+
+const char *aos_version_get(void)
+{
+    return SYSINFO_KERNEL_VERSION;
+}
