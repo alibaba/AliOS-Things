fde0fb8a156f836c77dc784aafe6d6f9034bfccf
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2018-11-29 13:21:08 +0800

    BugID:17865306: Convert 3rdparty makefiles to aos.mk
    Change-Id: Id880c696bb1b14f0734b6e5d264b7a5db54f0d70

--- 3rdparty/experimental/micropython/aos.mk
+++ 3rdparty/experimental/micropython/aos.mk
@@ -2,7 +2,7 @@ NAME := micropython
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := a lean and efficient Python 3 implementation for MCU
 
 #default gcc
 ifeq ($(COMPILER),)
