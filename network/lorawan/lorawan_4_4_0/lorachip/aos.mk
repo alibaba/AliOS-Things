a52304e703c9a8a6874ebaabfb019fde2b4aea75
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2018-11-29 10:21:57 +0800

    BugID:16959715: add Summary for lorachip
    Change-Id: I3f306dbb81cc3c67cdb9b170e1edd42cef9e135c

--- network/lorawan/lorawan_4_4_0/lorachip/aos.mk
+++ network/lorawan/lorawan_4_4_0/lorachip/aos.mk
@@ -2,7 +2,7 @@ NAME := lorachip
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := lorachip component for LoRaWAN Protocal Stack 4.4.0
 
 ifeq ($(LORACHIP), sx1276)
 $(NAME)_SOURCES := sx1276/sx1276.c
