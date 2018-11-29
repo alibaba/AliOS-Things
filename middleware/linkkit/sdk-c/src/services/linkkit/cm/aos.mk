2c2be504195711cfccf64d8a0fb9ff8ab7dd8386
zhangtian <dongren.zt@alibaba-inc.com>
2018-11-29 17:41:44 +0800

    BugID:17646749:add Config.in for all component
    Change-Id: I294d04d2fa6a882ea97a22044b9c7362ab738351
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/services/linkkit/cm/aos.mk
+++ middleware/linkkit/sdk-c/src/services/linkkit/cm/aos.mk
@@ -21,8 +21,6 @@ endif
 
 $(NAME)_COMPONENTS := middleware/linkkit/sdk-c/src/infra/utils \
 middleware/linkkit/sdk-c/src/infra/log \
-middleware/linkkit/sdk-c/src/infra/system \
-middleware/linkkit/sdk-c/src/sdk-impl \
 
 $(NAME)_INCLUDES := ../../../../src/infra \
 ../../../../src/infra/log \
