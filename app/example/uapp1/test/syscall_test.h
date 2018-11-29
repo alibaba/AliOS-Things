7a0bbbe9dc04d8fe4e75fa3c72eb1ce6ef1079ff
east moutain <east.moutain.yang@gmail.com>
2018-11-29 22:45:15 +0800

    BugID:16846667: Added static mutex/semaphore test cases
    	renamed:    example/uapp1/test/mutex_test.c -> example/uapp1/test/dynamic_mutex_test.c
    	new file:   example/uapp1/test/dynamic_semaphore_test.c
    	new file:   example/uapp1/test/static_mutex_test.c
    	new file:   example/uapp1/test/static_semaphore_test.c
    	modified:   example/uapp1/test/syscall_test.h
    	modified:   example/uapp1/test/test.c
    	modified:   example/uapp1/uapp1.mk
    
    Change-Id: I4144076b13cb6734a49c929e4c8b68af66050fe8

--- app/example/uapp1/test/syscall_test.h
+++ app/example/uapp1/test/syscall_test.h
@@ -8,8 +8,10 @@
 int task_test(void);
 int time_test(void);
 int timer_test(void);
-int mutex_test(void);
-int semphore_test(void);
+int dynamic_mutex_test(void);
+int static_mutex_test(void);
+int dynamic_semaphore_test(void);
+int static_semaphore_test(void);
 int buf_queue_test(void);
 
 #endif /* SYSCALL_TEST_H */
