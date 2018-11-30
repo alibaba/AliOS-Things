378fb5e3cddbe9d55781ff88788112a315614486
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-11-30 14:00:36 +0800

    BugID:17688894:Move hdlcapp and salserver to test/developer
    Change-Id: I8a1f921f2a6f79cf6b3f5360b2e4d055b986d31f

--- app/example/hdlcapp/hdlcclient/ucube.py
+++
@@ -1,9 +0,0 @@
-src     = Split('''
-        hdlcclient.c
-''')
-
-component = aos_component('hdlcclient', src)
-component.add_comp_deps('kernel/yloop', 'kernel/cli', 'network/sal/atparser', 'network/netmgr')
-component.add_global_macros('DEBUG', 'AOS_ATCMD')
-
-component.add_global_includes('.')
