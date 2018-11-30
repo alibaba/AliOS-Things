ec73878615ee8f0bf2956495d4818302a138a06c
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-11-30 15:02:08 +0800

    BugID:17711858: Update the vfs reference in app/example
    Change-Id: I3052c616a24e7c687b9c6583f505e487c24589fa

--- app/example/vflashdemo/vflashdemo.mk
+++ app/example/vflashdemo/vflashdemo.mk
@@ -7,6 +7,8 @@ $(NAME)_SOURCES := main.c
 
 GLOBAL_DEFINES += AOS_NO_WIFI
 
+$(NAME)_COMPONENTS += kernel.fs.vfs
+
 ifeq ($(BENCHMARKS),1)
 $(NAME)_COMPONENTS  += benchmarks
 GLOBAL_DEFINES      += CONFIG_CMD_BENCHMARKS
