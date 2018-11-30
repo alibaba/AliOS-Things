e17995ded98404b2942c588a3cb315e681d0f386
zhangtian <dongren.zt@alibaba-inc.com>
2018-11-30 19:32:22 +0800

    BugID:17646749:[linkkit related app] improve for modularization
    Change-Id: I195bc493cef7af0624629a8157a7f193773a572f

---
+++ app/example/coapapp/aos.mk
@@ -0,0 +1,27 @@
+NAME := coapapp
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 2.3.0
+$(NAME)_SUMMARY := coap cloud example
+$(NAME)_SOURCES := app_entry.c \
+                   coap_example.c
+
+$(NAME)_COMPONENTS += feature.linkkit-coap \
+                      network/netmgr \
+                      middleware/common \
+                      utility/cjson \
+                      kernel/cli
+
+GLOBAL_DEFINES += CONFIG_AOS_CLI
+
+ifeq ($(LWIP),1)
+$(NAME)_COMPONENTS  += lwip
+no_with_lwip := 0
+endif
+
+ifeq ($(loop),1)
+$(NAME)_DEFINES      += TEST_LOOP
+endif
+
+GLOBAL_INCLUDES += ./
+
