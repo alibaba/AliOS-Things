8bad448c16c348fb82c8b046994ff10700a98713
zhangtian <dongren.zt@alibaba-inc.com>
2018-11-30 19:32:22 +0800

    BugID:17646749:[linkkit related app] improve for modularization
    Change-Id: I195bc493cef7af0624629a8157a7f193773a572f

--- app/example/http2app/aos.mk
+++ app/example/http2app/aos.mk
@@ -1,27 +1,27 @@
-NAME := coapapp
+NAME := http2app
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
-$(NAME)_SOURCES := app_entry.c \
-                   coap_example.c
+$(NAME)_SUMMARY := http2 stream examples
 
-$(NAME)_COMPONENTS += feature.linkkit-coap \
+$(NAME)_SOURCES := app_entry.c   http2_example_stream.c
+
+$(NAME)_COMPONENTS += feature.linkkit-http2 \
                       network/netmgr \
                       middleware/common \
                       utility/cjson \
                       kernel/cli
 
-GLOBAL_DEFINES += CONFIG_AOS_CLI
+
+GLOBAL_DEFINES += CONFIG_AOS_CLI ON_DAILY
+
 
 ifeq ($(LWIP),1)
 $(NAME)_COMPONENTS  += lwip
 no_with_lwip := 0
 endif
 
-ifeq ($(loop),1)
-$(NAME)_DEFINES      += TEST_LOOP
-endif
 
 GLOBAL_INCLUDES += ./
 
+GLOBAL_DEFINES += CONFIG_DM_DEVTYPE_SINGLE  HTTP2_RECV_BUFFER_LENGHT=4096
