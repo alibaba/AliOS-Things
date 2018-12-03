a729e42b785322f5bb789078c13282c14fd79e86
andy.zwj <andy.zwj@alibaba-inc.com>
2018-12-03 16:45:53 +0800

    BugID:17532990:alter sensor path
    Change-Id: Ibf1c6124b7a96154784c704a1082177a51654e1a

--- board/starterkit/ucube.py
+++ board/starterkit/ucube.py
@@ -22,7 +22,7 @@ aos_global_config.set('SUPPORT_MBINS', 'no')
 aos_global_config.set('HOST_MCU_NAME', 'STM32L433CCTx')
 
 dependencis =Split('''
-    device/sensor
+    drivers/sensor
 ''')
 for i in dependencis:
     component.add_comp_deps(i)
