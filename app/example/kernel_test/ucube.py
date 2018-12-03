dcd9217294ceffb998c777fe9cf4d33fac8b9ac1
jianlong.gaojianlo <jianlong.gaojianlo@alibaba-inc.com>
2018-12-03 12:05:26 +0800

    BugID:17669000: move test app to test/develop
    1, lark: https://yuque.antfin-inc.com/aliosthings/wiki/nseuku
    2, compile commands are same as before
    
    Change-Id: I6145e5919da360afd4f9328d3be982c97545a001

--- app/example/kernel_test/ucube.py
+++
@@ -1,7 +0,0 @@
-src     = Split('''
-        kernel_test.c
-''')
-
-component = aos_component('kernel_test', src)
-component.add_comp_deps('kernel/cli', 'kernel/rhino/test')
-component.add_global_macros('AOS_NO_WIFI')
--- app/example/modbus_test/ucube.py
+++
@@ -1,8 +0,0 @@
-src     = Split('''
-        modbus_test.c
-        modbus_demo.c
-''')
-
-component = aos_component('modbus_test', src)
-component.add_comp_deps('kernel/yloop', 'kernel/cli','kernel/rhino/bus/mbmaster')
-component.add_global_macros('AOS_NO_WIFI')
