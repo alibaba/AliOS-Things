522a8ff4a3a510dfb2dec5524b807f187260281a
lingjun chen <lingjun.clj@alibaba-inc.com>
2018-12-02 23:12:52 -0800

    BugID:16731946:fix krhino_task_dyn_del when smp
    Change-Id: Ia9c7226add5245965f5cb80a598648876d73baff

--- kernel/rhino/k_idle.c
+++ kernel/rhino/k_idle.c
@@ -3,10 +3,17 @@
  */
 
 #include <k_api.h>
+#include <stdio.h>
 
 void idle_task(void *arg)
 {
     CPSR_ALLOC();
+
+#if (RHINO_CONFIG_CPU_NUM > 1)
+    klist_t *head;
+    ktask_t *task_del;
+    head =  &task_del_head;
+#endif
 
     /* avoid warning */
     (void)arg;
@@ -16,10 +23,19 @@ void idle_task(void *arg)
 #endif
 
     while (RHINO_TRUE) {
+#if (RHINO_CONFIG_CPU_NUM > 1)
         RHINO_CPU_INTRPT_DISABLE();
-
+        if (head->next != head) {
+            task_del = krhino_list_entry(head->next, ktask_t, task_del_item);
+            if (task_del->cur_exc == 0) {
+                klist_rm(&task_del->task_del_item);
+                krhino_task_dyn_del(task_del);
+            }
+        }
+        RHINO_CPU_INTRPT_ENABLE();
+#endif
+        RHINO_CPU_INTRPT_DISABLE();
         g_idle_count[cpu_cur_get()]++;
-
         RHINO_CPU_INTRPT_ENABLE();
 
 #if (RHINO_CONFIG_USER_HOOK > 0)
