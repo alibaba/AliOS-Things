3028a5c801b7dc53d359132ea8edad5be1df79f8
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2018-12-04 15:37:15 +0800

    BugID:18111823:add stm32f429zi to keil&IAR project
    Change-Id: I03ddde99c8793191e6aa29b2dacf55b57c2d0596

--- board/stm32f429zi-nucleo/ucube.py
+++ board/stm32f429zi-nucleo/ucube.py
@@ -1,1 +1,2 @@
 linux_only_targets="helloworld netmgrapp helloworld_nocli tls hdlcapp.hdlcserver hdlcapp.hdlcclient mqttapp meshapp linkkitapp coapapp networkapp acapp wifihalapp linkkit_gateway udataapp wifimonitor athostapp blink modbus_test yts i2c_hts221_test kernel_test otaapp cryptotest http2app nano vflashdemo devkit.dk_ld benchmark.footprint_test"
+windows_only_targets="helloworld|COMPILER=armcc helloworld|COMPILER=iar"
