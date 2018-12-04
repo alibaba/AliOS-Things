21c6ee235d89972ae99967500558c76756a51862
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-12-04 14:04:36 +0800

    BugID:18109685: osal/aos/include -> include
    Change-Id: I5ec7d6fb530ab461b333cf8d80e2757532e95143

--- osal/aos.mk
+++ osal/aos.mk
@@ -1,57 +1,47 @@
 NAME := osal
 
 $(NAME)_MBINS_TYPE := share
-$(NAME)_VERSION := 0.0.1.0
-$(NAME)_SUMMARY := os interface adaptation layer
+$(NAME)_VERSION    := 0.0.1.0
+$(NAME)_SUMMARY    := os interface adaptation layer
 
-$(NAME)_COMPONENTS += middleware.common
-
-GLOBAL_INCLUDES += ./mico/include ./aos/include ./aos ./aos/include/hal ./aos/include/hal/soc
+GLOBAL_INCLUDES += ./mico/include ./aos
 
 #default gcc
 ifeq ($(COMPILER),)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 endif
 
 ifeq ($(HOST_ARCH),ARM968E-S)
 $(NAME)_CFLAGS += -marm
 endif
 
-osal ?= rhino
+osal            ?= rhino
 $(NAME)_SOURCES += aos/aos_common.c
 
 ifeq ($(osal),freertos)
-GLOBAL_DEFINES += OSAL_FREERTOS
-
-$(NAME)_SOURCES += \
-    aos/ext/aos_freertos.c
+GLOBAL_DEFINES  += OSAL_FREERTOS
+$(NAME)_SOURCES += aos/ext/aos_freertos.c
 endif
 
 ifeq ($(osal),posix)
-GLOBAL_DEFINES += OSAL_POSIX
-
-$(NAME)_SOURCES += \
-    aos/ext/aos_posix.c
+GLOBAL_DEFINES  += OSAL_POSIX
+$(NAME)_SOURCES += aos/ext/aos_posix.c
 endif
 
 ifeq ($(osal),rhino_sxr)
-GLOBAL_DEFINES += OSAL_SXR
-
-$(NAME)_SOURCES += \
-    aos/ext/aos_sxr.c
+GLOBAL_DEFINES  += OSAL_SXR
+$(NAME)_SOURCES += aos/ext/aos_sxr.c
 endif
 
 ifeq ($(osal),rhino_sxr_mutios)
-GLOBAL_DEFINES += OSAL_SXR_MUTIOS
-
-$(NAME)_SOURCES += \
-    aos/ext/aos_rhino_rda8955.c
+GLOBAL_DEFINES  += OSAL_SXR_MUTIOS
+$(NAME)_SOURCES += aos/ext/aos_rhino_rda8955.c
 endif
 
 ifeq ($(osal),rhino)
-GLOBAL_DEFINES += OSAL_RHINO AOS_HAL
+GLOBAL_DEFINES     += OSAL_RHINO AOS_HAL
 $(NAME)_COMPONENTS += rhino
 
 ifeq ($(HOST_MCU_FAMILY),esp32)
