3683d573d1e5e0c68d61d93bd8ae1839ad82d8ab
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-12-04 14:03:43 +0800

    BugID:18109685: osal/aos/include -> include
    Change-Id: I16cc9eed282d9bfb20cf9780485e64aecc020a7b
    Reviewed-on: http://yuncode.alibaba-inc.com/177787
    Tested-by: 李诚 lc122798 <lc122798@alibaba-inc.com>
    Reviewed-by: 刘自龙 zilong.liuzl <zilong.liuzl@alibaba-inc.com>

---
+++ include/aos/aos.h
@@ -0,0 +1,44 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef AOS_AOS_H
+#define AOS_AOS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <aos/types.h>
+
+#ifdef CONFIG_AOS_CLI
+#include <aos/cli.h>
+#endif
+
+#include <aos/debug.h>
+#include <aos/kernel.h>
+
+#ifdef AOS_KV
+#include <aos/kv.h>
+#endif
+
+#include <aos/list.h>
+#include <aos/log.h>
+
+#ifdef AOS_VFS
+#include <aos/vfs.h>
+#endif
+
+#ifdef AOS_LOOP
+#include <aos/yloop.h>
+#endif
+
+#include <aos/errno.h>
+#include <aos/init.h>
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AOS_AOS_H */
+
