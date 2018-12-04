bee3288da86ed4537120a987d6d0b345ee2fca80
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-12-04 16:26:25 +0800

    BugID:17982117: add mk/config.in/readme of test/kernel
    Change-Id: I500e9783d537f0f2e3d3204e4e23f78a0f56b1a9

--- test/testcase/kernel/fs/fatfs_test/fatfs_test.mk
+++ test/testcase/kernel/fs/fatfs_test/fatfs_test.mk
@@ -1,11 +1,11 @@
 NAME := fatfs_test
 
 $(NAME)_MBINS_TYPE := app
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY :=
-$(NAME)_COMPONENTS  += 3rdparty.experimental.fs.fatfs
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := testcase for fatfs
+$(NAME)_COMPONENTS += 3rdparty.experimental.fs.fatfs
 
-$(NAME)_SOURCES     += fatfs_test.c
+$(NAME)_SOURCES += fatfs_test.c
 
-$(NAME)_CFLAGS      += -Wall -Werror
+$(NAME)_CFLAGS += -Wall -Werror
 
---
+++ test/testcase/osal_test/README.md
@@ -0,0 +1,15 @@
+## Overview
+
+## Feature of Board
+
+## Directories
+
+```sh
+osal_test # test for osal
+```
+
+## Board Hardware Resources
+
+## Update log
+
+## Reference
