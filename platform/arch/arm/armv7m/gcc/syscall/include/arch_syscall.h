368c02d9a05d56cd9addfcc1c47c7eeacb185474
east moutain <east.moutain.yang@gmail.com>
2018-12-04 19:00:00 +0800

    BugID:16846667: Simplify syscall function
    	modified:   arch/arm/armv7a/gcc/syscall/include/arch_syscall.h
    	modified:   arch/arm/armv7m/gcc/syscall/include/arch_syscall.h
    
    Change-Id: Ia837383862c4728e4db68c53f998dae2ea407d21

--- platform/arch/arm/armv7m/gcc/syscall/include/arch_syscall.h
+++ platform/arch/arm/armv7m/gcc/syscall/include/arch_syscall.h
@@ -6,32 +6,36 @@
 
 #include <svc.h>
 
-#define SYSCALL(syscall_no, arg1) \
-    ({ \
-        unsigned int _syscall_no = syscall_no; \
-        unsigned int _arg1 = (unsigned int)arg1; \
-        union { \
-            unsigned long long u64; \
-            unsigned long u32[2]; \
-        } _ret; \
-        __asm__ __volatile__ ( \
-            "push   {r2,r3,r4,r8}\t\n" \
-            "mov    r0, %[_syscall_no]\t\n" \
-            "mov    r1, %[_arg1]\t\n" \
-            "svc    %[_svc_no]\t\n" \
-            "pop    {r2,r3,r4,r8}\t\n" \
-            "mov    %[_ret_0], r0\t\n" \
-            "mov    %[_ret_1], r1\t\n" \
-            : [_ret_0]"=r"(_ret.u32[0]), \
-              [_ret_1]"=r"(_ret.u32[1]) \
-            : [_syscall_no]"r"(_syscall_no), \
-              [_arg1]"r"(_arg1), \
-              [_svc_no]"i"(SVC_syscall) \
-            : "r0", "r1" \
-            ); \
-            _ret.u64; \
-    })
+static inline unsigned long long SYSCALL(int syscall_no, void *arg)
+{
+    register unsigned long long ret asm("r0");
 
+    register int reg0 asm("r0") = (int)syscall_no;
+    register int reg1 asm("r1") = (int)arg;
 
-#endif // ARCH_SYSCALL_H
+    /* r0-r3:   caller-save registers
+     * r4-r8:   callee-save registers
+     * r9:      might be callee-save register or not,
+     *          it depends on the AAPCS.
+     * r10-r11: callee-save registers
+     * r12-r15: special registers
+     */
+
+    /* use r8 to save the return address during
+     * executing system call on Cortex-M platform
+     */
+
+    __asm__ __volatile__ (
+        "push   {r2,r3,r8,r9,r12,r14}\t\n"
+        "svc    %[_svc_no]\t\n"
+        "pop    {r2,r3,r8,r9,r12,r14}\t\n"
+        : "=r"(ret)
+        : [_svc_no]"i"(SVC_syscall), "r"(reg0), "r"(reg1)
+        : "memory"
+        );
+
+    return ret;
+}
+
+#endif /* ARCH_SYSCALL_H */
 
