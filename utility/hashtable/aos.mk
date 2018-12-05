878c641b0220b1a3d55d0adf2ee521cf2a990595
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-12-05 19:43:09 +0800

    BugID:18139673: rm rm utility/hashtable
    Change-Id: I6af7e8c5328633539f90f6cdea6ff76ce05ba0ad

--- utility/hashtable/aos.mk
+++
@@ -1,9 +0,0 @@
-NAME := hashtable
-
-$(NAME)_MBINS_TYPE := share
-$(NAME)_VERSION := 0.0.1
-$(NAME)_SUMMARY := The hashtable provides friendly APIs to create, add, find hash table.
-
-$(NAME)_SOURCES := hashtable.c
-$(NAME)_INCLUDES := ../../middleware/alink/protocol/os/
-GLOBAL_INCLUDES += .
--- utility/hashtable/ucube.py
+++
@@ -1,6 +0,0 @@
-src = Split('''
-    hashtable.c
-''')
-
-component = aos_component('hashtable', src)
-component.add_includes('../../middleware/alink/protocol/os/')
