cee6131b3db6b489f53170e0eafe392ce6d41fe6
jianlong.gaojianlo <jianlong.gaojianlo@alibaba-inc.com>
2018-12-05 19:05:31 +0800

    BugID:16944728: add copyright of crc and move to dedicated audio hal
    Change-Id: I4fba0f4302e72907519eb669b004f93a77207a6e

--- utility/digest_algorithm/aos.mk
+++ utility/digest_algorithm/aos.mk
@@ -4,5 +4,5 @@ $(NAME)_MBINS_TYPE := share
 $(NAME)_VERSION := 0.0.1
 $(NAME)_SUMMARY := digest_algorithm provides many checksum and digest algorithm with the friendly APIs.
 
-$(NAME)_SOURCES := digest_algorithm.c CheckSumUtils.c crc.c crc16.c md5.c
+$(NAME)_SOURCES := digest_algorithm.c CheckSumUtils.c crc.c md5.c
 GLOBAL_INCLUDES += .
