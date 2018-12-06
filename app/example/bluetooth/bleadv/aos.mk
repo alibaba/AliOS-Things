224a98bb85b87d5f1d7be58ed628510eb380d8e0
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2018-12-06 19:11:34 +0800

    bugID:18002569:[bt] use variable to control CONFIG_BT_SMP marco.
    Change-Id: I7ddda64e9e28c8d21151666834055e675521ada4

--- app/example/bluetooth/bleadv/aos.mk
+++ app/example/bluetooth/bleadv/aos.mk
@@ -6,8 +6,9 @@ $(NAME)_SUMMARY := Ble adv example.
 $(NAME)_SOURCES     := main.c
 
 ble = 1
+en_bt_smp = 1
 
 $(NAME)_COMPONENTS  += network.bluetooth.bt yloop cli
-GLOBAL_DEFINES      += AOS_NO_WIFI CONFIG_BT_SMP
+GLOBAL_DEFINES      += AOS_NO_WIFI
 
 GLOBAL_INCLUDES += ../
