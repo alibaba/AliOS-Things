7a11da365417ae36fe787e796d7696e5f12ff30c
junping.pjp <junping.pjp@alibaba-inc.com>
2018-12-06 12:11:37 +0000

    BugID: 18138458: header file included check
    Change-Id: I2d120fa1e078a3610428352a90a79e6603210635

--- utility/digest_algorithm/aos.mk
+++ utility/digest_algorithm/aos.mk
@@ -1,7 +1,7 @@
 NAME := digest_algorithm
 
 $(NAME)_MBINS_TYPE := share
-$(NAME)_VERSION := 0.0.1
+$(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := digest_algorithm provides many checksum and digest algorithm with the friendly APIs.
 
 $(NAME)_SOURCES := digest_algorithm.c CheckSumUtils.c crc.c md5.c
