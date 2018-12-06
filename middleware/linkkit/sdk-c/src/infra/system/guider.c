44cdf372f9f9bbdd3f3f7ab107c24ace190e3142
yuhao.tyh|taoyuhao <yuhao.tyh@alibaba-inc.com>
2018-12-06 15:20:19 +0800

    BugID:17646749:reduce dependence file of mqtt
    Change-Id: I569cb524e25091a6b782f2496925314c1d4dadb2

--- middleware/linkkit/sdk-c/src/infra/system/guider.c
+++ middleware/linkkit/sdk-c/src/infra/system/guider.c
@@ -466,8 +466,10 @@ static int guider_get_iotId_iotToken(
                    iotx_ca_get()
 #endif
                   );
+#if defined(WITH_FAC_JSON_FLOW)
     sys_info("Downstream Payload:");
     iotx_facility_json_print(iotx_payload, LOG_INFO_LEVEL, '<');
+#endif
 
     pvalue = LITE_json_value_of("code", iotx_payload, MEM_MAGIC, "sys.preauth");
     if (!pvalue) {
@@ -772,4 +774,51 @@ failed:
     }
     return -1;
 
+}
+
+static iotx_conn_info_t     iotx_conn_info = {0, 0, NULL, NULL, NULL, NULL, NULL};
+
+int iotx_device_info_get(iotx_device_info_t *device_info)
+{
+    if (device_info == NULL) {
+        return -1;
+    }
+    memset(device_info, 0x0, sizeof(iotx_device_info_t));
+    HAL_GetProductKey(device_info->product_key);
+    HAL_GetDeviceName(device_info->device_name);
+    HAL_GetDeviceSecret(device_info->device_secret);
+    HAL_GetDeviceID(device_info->device_id);
+
+    return 0;
+}
+
+iotx_conn_info_pt iotx_conn_info_get(void)
+{
+    return &iotx_conn_info;
+}
+
+void iotx_conn_info_release(void)
+{
+    if (iotx_conn_info.host_name != NULL) {
+        LITE_free(iotx_conn_info.host_name);
+    }
+    if (iotx_conn_info.username != NULL) {
+        LITE_free(iotx_conn_info.username);
+    }
+    if (iotx_conn_info.password != NULL) {
+        LITE_free(iotx_conn_info.password);
+    }
+    if (iotx_conn_info.client_id != NULL) {
+        LITE_free(iotx_conn_info.client_id);
+    }
+    memset(&iotx_conn_info, 0, sizeof(iotx_conn_info));
+}
+
+iotx_conn_info_pt iotx_conn_info_reload(void)
+{
+    iotx_conn_info_release();
+    if (iotx_guider_authenticate(&iotx_conn_info) < 0) {
+        return NULL;
+    }
+    return &iotx_conn_info;
 }
--- middleware/linkkit/sdk-c/src/infra/system/report.c
+++ middleware/linkkit/sdk-c/src/infra/system/report.c
@@ -382,3 +382,30 @@ int iotx_report_mid(void *pclient)
     VERSION_DEBUG("MID Report: finished, IOT_MQTT_Publish() = %d", ret);
     return SUCCESS_RETURN;
 }
+
+#ifndef BUILD_AOS
+unsigned int aos_get_version_info(unsigned char version_num[VERSION_NUM_SIZE],
+                                  unsigned char random_num[RANDOM_NUM_SIZE], unsigned char mac_address[MAC_ADDRESS_SIZE],
+                                  unsigned char chip_code[CHIP_CODE_SIZE], unsigned char *output_buffer, unsigned int output_buffer_size)
+{
+    char *p = (char *)output_buffer;
+
+    if (output_buffer_size < AOS_ACTIVE_INFO_LEN) {
+        return 1;
+    }
+
+    memset(p, 0, output_buffer_size);
+
+    LITE_hexbuf_convert(version_num, p, VERSION_NUM_SIZE, 1);
+    p += VERSION_NUM_SIZE * 2;
+    LITE_hexbuf_convert(random_num, p, RANDOM_NUM_SIZE, 1);
+    p += RANDOM_NUM_SIZE * 2;
+    LITE_hexbuf_convert(mac_address, p, MAC_ADDRESS_SIZE, 1);
+    p += MAC_ADDRESS_SIZE * 2;
+    LITE_hexbuf_convert(chip_code, p, CHIP_CODE_SIZE, 1);
+    p += CHIP_CODE_SIZE * 2;
+    strcat(p, "1111111111222222222233333333334444444444");
+
+    return 0;
+}
+#endif
