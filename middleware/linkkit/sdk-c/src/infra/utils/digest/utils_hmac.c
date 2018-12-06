edf2f7b1fe4bdafcf5369672448b9d6be2579842
yinghao.cyh <yinghao.cyh@alibaba-inc.com>
2018-12-06 16:12:52 +0800

    BugID:17646749: clean up utils_sha1.c
    Change-Id: Id21723651a582ef34d39f1c74c3cc872d1711cba
    Signed-off-by: yinghao.cyh <yinghao.cyh@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/infra/utils/digest/utils_hmac.c
+++ middleware/linkkit/sdk-c/src/infra/utils/digest/utils_hmac.c
@@ -10,6 +10,7 @@
 #include "utils_hmac.h"
 #include "utils_base64.h"
 
+#ifdef UTILS_MD5
 void utils_hmac_md5(const char *msg, int msg_len, char *digest, const char *key, int key_len)
 {
     if ((NULL == msg) || (NULL == digest) || (NULL == key)) {
@@ -59,55 +60,9 @@ void utils_hmac_md5(const char *msg, int msg_len, char *digest, const char *key,
         digest[i * 2 + 1] = utils_hb2hex(out[i]);
     }
 }
+#endif  /* #ifdef UTILS_MD5 */
 
-#if defined(DEV_BIND_ENABLED)
-void utils_hmac_sha1_hex(const char *msg, int msg_len, char *digest, const char *key, int key_len)
-{
-    if ((NULL == msg) || (NULL == digest) || (NULL == key)) {
-        utils_err("parameter is Null,failed!");
-        return;
-    }
-
-    if (key_len > KEY_IOPAD_SIZE) {
-        utils_err("key_len > size(%d) of array", KEY_IOPAD_SIZE);
-        return;
-    }
-
-    iot_sha1_context context;
-    unsigned char k_ipad[KEY_IOPAD_SIZE];    /* inner padding - key XORd with ipad  */
-    unsigned char k_opad[KEY_IOPAD_SIZE];    /* outer padding - key XORd with opad */
-    unsigned char out[SHA1_DIGEST_SIZE];
-    int i;
-
-    /* start out by storing key in pads */
-    memset(k_ipad, 0, sizeof(k_ipad));
-    memset(k_opad, 0, sizeof(k_opad));
-    memcpy(k_ipad, key, key_len);
-    memcpy(k_opad, key, key_len);
-
-    /* XOR key with ipad and opad values */
-    for (i = 0; i < KEY_IOPAD_SIZE; i++) {
-        k_ipad[i] ^= 0x36;
-        k_opad[i] ^= 0x5c;
-    }
-
-    /* perform inner SHA */
-    utils_sha1_init(&context);                                      /* init context for 1st pass */
-    utils_sha1_starts(&context);                                    /* setup context for 1st pass */
-    utils_sha1_update(&context, k_ipad, KEY_IOPAD_SIZE);            /* start with inner pad */
-    utils_sha1_update(&context, (unsigned char *) msg, msg_len);    /* then text of datagram */
-    utils_sha1_finish(&context, out);                               /* finish up 1st pass */
-
-    /* perform outer SHA */
-    utils_sha1_init(&context);                              /* init context for 2nd pass */
-    utils_sha1_starts(&context);                            /* setup context for 2nd pass */
-    utils_sha1_update(&context, k_opad, KEY_IOPAD_SIZE);    /* start with outer pad */
-    utils_sha1_update(&context, out, SHA1_DIGEST_SIZE);     /* then results of 1st hash */
-    utils_sha1_finish(&context, out);                       /* finish up 2nd pass */
-    memcpy(digest, out, SHA1_DIGEST_SIZE);
-}
-#endif  /* #if defined(DEV_BIND_ENABLED) */
-
+#ifdef UTILS_SHA256
 void utils_hmac_sha256(const char *msg, int msg_len, char *digest, const char *key, int key_len)
 {
     if ((NULL == msg) || (NULL == digest) || (NULL == key)) {
@@ -157,7 +112,9 @@ void utils_hmac_sha256(const char *msg, int msg_len, char *digest, const char *k
         digest[i * 2 + 1] = utils_hb2hex(out[i]);
     }
 }
+#endif /* #ifdef UTILS_SHA256 */
 
+#ifdef UTILS_SHA1
 void utils_hmac_sha1(const char *msg, int msg_len, char *digest, const char *key, int key_len)
 {
     if ((NULL == msg) || (NULL == digest) || (NULL == key)) {
@@ -208,8 +165,7 @@ void utils_hmac_sha1(const char *msg, int msg_len, char *digest, const char *key
     }
 }
 
-#if defined(ALCS_ENABLED)
-void utils_hmac_sha1_raw(const char *msg, int msg_len, char *digest, const char *key, int key_len)
+void utils_hmac_sha1_hex(const char *msg, int msg_len, char *digest, const char *key, int key_len)
 {
     if ((NULL == msg) || (NULL == digest) || (NULL == key)) {
         utils_err("parameter is Null,failed!");
@@ -252,7 +208,7 @@ void utils_hmac_sha1_raw(const char *msg, int msg_len, char *digest, const char 
     utils_sha1_update(&context, k_opad, KEY_IOPAD_SIZE);    /* start with outer pad */
     utils_sha1_update(&context, out, SHA1_DIGEST_SIZE);     /* then results of 1st hash */
     utils_sha1_finish(&context, out);                       /* finish up 2nd pass */
-
     memcpy(digest, out, SHA1_DIGEST_SIZE);
 }
-#endif  /* #if defined(ALCS_ENABLED) */
+
+#endif  /* #ifdef UTILS_SHA1 */
