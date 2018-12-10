a8d3cdc1fd4b46c4c4c6eb42e00acb0048c1f4e9
zhangtian <dongren.zt@alibaba-inc.com>
2018-12-10 16:31:39 +0800

    BugID:17646749:delete comp digest_algorithm
    Change-Id: I670498451725bae977fd775656cfa2886f5f920b

--- utility/digest_algorithm/aos.mk
+++
@@ -1,8 +0,0 @@
-NAME := digest_algorithm
-
-$(NAME)_MBINS_TYPE := share
-$(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY := digest_algorithm provides many checksum and digest algorithm with the friendly APIs.
-
-$(NAME)_SOURCES := digest_algorithm.c CheckSumUtils.c crc.c md5.c
-GLOBAL_INCLUDES += .
--- utility/digest_algorithm/ucube.py
+++
@@ -1,9 +0,0 @@
-src = Split('''
-        CheckSumUtils.c
-        crc.c
-        crc16.c
-        digest_algorithm.c
-        md5.c
-''')
-
-component = aos_component('digest_algorithm', src)
