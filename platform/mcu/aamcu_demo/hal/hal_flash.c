886b4e276fb58afa216496c35ed66f7ef65b6a92
zhuqing <zhuqing.zq@alibaba-inc.com>
2018-12-11 11:49:13 +0800

    BugID:18204058:add board aa3 and mcu aa standard realization demo; dir platform
    Change-Id: I928068534f6e93760396425491531e42b5eef65b

--- platform/mcu/aamcu_demo/hal/hal_flash.c
+++ platform/mcu/aamcu_demo/hal/hal_flash.c
@@ -1,29 +1,24 @@
 /*
  * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
-#include <hal/hal.h>
-
 #ifdef HAL_FLASH_MODULE_ENABLED
+
+#include <k_api.h>
+#include "hal/soc/soc.h"
 
 hal_logic_partition_t *hal_flash_get_info(hal_partition_t pno)
 {
-
 }
 
-int32_t hal_flash_write(hal_partition_t pno, uint32_t *poff, const void *buf, uint32_t buf_size)
+int32_t hal_flash_write(hal_partition_t pno, uint32_t *poff, const void *buf , uint32_t buf_size)
 {
-
 }
 
 int32_t hal_flash_read(hal_partition_t pno, uint32_t *poff, void *buf, uint32_t buf_size)
 {
-
 }
 
 int32_t hal_flash_erase(hal_partition_t pno, uint32_t off_set, uint32_t size)
 {
-
 }
-
-#endif /* HAL_FLASH_MODULE_ENABLED */
-
+#endif
--- platform/mcu/aamcu_demo/hal/hal_timer.c
+++ platform/mcu/aamcu_demo/hal/hal_timer.c
@@ -2,29 +2,23 @@
  * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
 
-#include <hal/hal.h>
-
-#ifdef HAL_TIMER_MODULE_ENABLED
+#include <k_api.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "hal/hal.h"
 
 int32_t hal_timer_init(timer_dev_t *tim)
 {
-
 }
 
 int32_t hal_timer_start(timer_dev_t *tim)
 {
-
 }
 
 void hal_timer_stop(timer_dev_t *tim)
 {
-
 }
 
 int32_t hal_timer_finalize(timer_dev_t *tim)
 {
-
 }
-
-#endif /* HAL_TIMER_MODULE_ENABLED */
-
