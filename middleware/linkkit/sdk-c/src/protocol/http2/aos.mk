7c91c1a0a75de0c32a13303c8bff09f5ff41b945
zhangtian <dongren.zt@alibaba-inc.com>
2018-12-11 10:52:35 +0800

    BugID:17646749:[h2] move all nghttp2 file to dir nghttp2
    Change-Id: Iab46acefcd013e1cb7275755c986e967a4ae2bf6
    Signed-off-by: Yang, Xiao <yusheng.yx@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/protocol/http2/aos.mk
+++ middleware/linkkit/sdk-c/src/protocol/http2/aos.mk
@@ -4,30 +4,8 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 2.3.0
 $(NAME)_SUMMARY := Nghttp2 and its encapsulation
 
-$(NAME)_SOURCES := ./nghttp2_version.c \
-./nghttp2_hd_huffman.c \
-./nghttp2_debug.c \
-./nghttp2_http.c \
-./nghttp2_hd.c \
-./nghttp2_npn.c \
-./nghttp2_map.c \
-./nghttp2_priority_spec.c \
-./nghttp2_hd_huffman_data.c \
-./nghttp2_net.c \
-./nghttp2_queue.c \
-./nghttp2_buf.c \
-./nghttp2_option.c \
-./iotx_http2_api.c \
-./nghttp2_outbound_item.c \
-./nghttp2_frame.c \
-./nghttp2_stream.c \
-./nghttp2_helper.c \
-./nghttp2_submit.c \
-./nghttp2_callbacks.c \
-./nghttp2_pq.c \
-./nghttp2_session.c \
-./nghttp2_rcbuf.c \
-./nghttp2_mem.c \
+$(NAME)_SOURCES := ./nghttp2/*.c \
+					./iotx_http2_api.c \
  
 $(NAME)_COMPONENTS := middleware/linkkit/sdk-c/src/infra/utils \
 middleware/linkkit/sdk-c/src/infra/log \
@@ -43,5 +21,4 @@ $(NAME)_INCLUDES := ../../../src/infra/utils \
 ../../../src/protocol/http2/nghttp2 \
 ./
 
-GLOBAL_INCLUDES += ./nghttp2 
 
