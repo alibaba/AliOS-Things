011849e0d4c7cb96525667afbade693fd233d133
zhuqing <zhuqing.zq@alibaba-inc.com>
2018-12-11 11:49:13 +0800

    BugID:18204058:add board aa3 and mcu aa standard realization demo; dir platform
    Change-Id: I928068534f6e93760396425491531e42b5eef65b

--- platform/mcu/aamcu_demo/hal/hal_adc.c
+++ platform/mcu/aamcu_demo/hal/hal_adc.c
@@ -2,24 +2,23 @@
  * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
 
-#include <hal/hal.h>
+#include <k_api.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "hal/hal.h"
 
 #ifdef HAL_ADC_MODULE_ENABLED
 
 int32_t hal_adc_init(adc_dev_t *adc)
 {
-
 }
 
 int32_t hal_adc_value_get(adc_dev_t *adc, void *output, uint32_t timeout)
 {
-
 }
 
 int32_t hal_adc_finalize(adc_dev_t *adc)
 {
-
 }
-
-#endif /* HAL_ADC_MODULE_ENABLED */
+#endif
 
