3ae4004e122fae6b134035a9444aa5685af168a0
wuqi.hz <wuqi.hz@alibaba-inc.com>
2018-12-11 15:02:01 +0800

    BugID:17795330:delete RHINO_EACHCORE_SYSTEM_STACK_SIZE
    Change-Id: I81ca7b70ffc65f3117a3f8e21ebd607d2b51d2d2

--- board/imx6dq/k_config.h
+++ board/imx6dq/k_config.h
@@ -199,10 +199,5 @@
 #define RHINO_CONFIG_SYSTEM_STACK_SIZE       0x4000
 #endif
 
-/* System stack size (for one core) */
-#ifndef RHINO_EACHCORE_SYSTEM_STACK_SIZE
-#define RHINO_EACHCORE_SYSTEM_STACK_SIZE     (RHINO_CONFIG_SYSTEM_STACK_SIZE/RHINO_CONFIG_CPU_NUM)
-#endif
-
 #endif /* CONFIG_H */
 
