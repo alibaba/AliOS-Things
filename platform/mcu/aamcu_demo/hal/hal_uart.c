911fd4ad1bbdabf44c78ed4558d73a06a7179e68
Zilong Liu <zilong.liuzl@alibaba-inc.com>
2018-12-11 20:18:08 +0800

    BugID:18117168: modify demo code
    Change-Id: I406d8953afa00cdf33322ccba21c01c0da8df650

--- platform/mcu/aamcu_demo/hal/hal_uart.c
+++ platform/mcu/aamcu_demo/hal/hal_uart.c
@@ -2,28 +2,30 @@
  * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
 
-#include <k_api.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include "hal/hal.h"
+#include "aos/hal/uart.h"
 
 #ifdef HAL_UART_MODULE_ENABLED
 
 int32_t hal_uart_init(uart_dev_t *uart)
 {
+
 }
 
 int32_t hal_uart_send(uart_dev_t *uart, const void *data, uint32_t size, uint32_t timeout)
 {
+
 }
 
 int32_t hal_uart_recv_II(uart_dev_t *uart, void *data, uint32_t expect_size,
-                      uint32_t *recv_size, uint32_t timeout)
+                         uint32_t *recv_size, uint32_t timeout)
 {
+
 }
 
 int32_t hal_uart_finalize(uart_dev_t *uart)
 {
+
 }
 
-#endif
+#endif /* HAL_UART_MODULE_ENABLED */
+
