600df64c6431f4b8a40d385ff56297e0f553d5b5
congling.zm <congling.zm@alibaba-inc.com>
2018-12-12 18:45:06 +0800

    BugID:18243432: Fix code version issue, middleware/uagent
    Change-Id: I1bea6190a3641c57842fc9b1dbdae67700d28ec3

--- middleware/uagent/uota/hal/aos.mk
+++ middleware/uagent/uota/hal/aos.mk
@@ -1,7 +1,7 @@
 NAME := ota_hal
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 0.0.1
+$(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := ota porting HAL APIs
 
 #default gcc
--- middleware/uagent/uota/src/transport/aos.mk
+++ middleware/uagent/uota/src/transport/aos.mk
@@ -8,7 +8,7 @@ $(NAME)_CFLAGS      += -Wall -Werror
 endif
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 0.0.1
+$(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := ota transport message on clould.
 
 ifeq ($(TRANSPORT),coap)
