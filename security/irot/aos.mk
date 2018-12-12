4c2269bdbffeb122b326cbe7cc7f666acc8b9833
congling.zm <congling.zm@alibaba-inc.com>
2018-12-12 18:47:53 +0800

    BugID:18243432: Fix code version issue, security/
    Change-Id: Ia21bce73b36c141297a5c452a0b81a5a64f13f29
    Reviewed-on: http://yuncode.alibaba-inc.com/180136
    Tested-by: 李诚 lc122798 <lc122798@alibaba-inc.com>
    Reviewed-by: 范文宗 wenzong.fwz <wenzong.fwz@alibaba-inc.com>
    Tested-by: 范文宗 wenzong.fwz <wenzong.fwz@alibaba-inc.com>
    Reviewed-by: 王雪松 cedar.wxs <cedar.wxs@alibaba-inc.com>

--- security/irot/aos.mk
+++ security/irot/aos.mk
@@ -1,7 +1,7 @@
 NAME := irot
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 0.0.1
+$(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := root of trust, based on mcu, se or tee
 
 GLOBAL_INCLUDES += ../include/irot
