6419abecf7b1bd1eb8361881aeb9b059067b1fb5
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2018-12-13 11:54:26 +0800

    BugID:18254899: test/testcase/kernel directory rectification
    Change-Id: I90fe1145bc72f0675511db98f2a632229de086a6

--- test/testcase/kernel/rhino/aos.mk
+++ test/testcase/kernel/rhino/aos.mk
@@ -4,20 +4,20 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := test for rhino core
 
-$(NAME)_COMPONENTS  += rhino
+$(NAME)_COMPONENTS += rhino
 
 GLOBAL_INCLUDES += ./
 
 ifeq ($(COMPILER),)
-$(NAME)_CFLAGS  += -Wall -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
-$(NAME)_CFLAGS  += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized
-$(NAME)_CFLAGS  += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable
-$(NAME)_CFLAGS  += -Wno-unused-value -Wno-strict-aliasing
+$(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
+$(NAME)_CFLAGS += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized
+$(NAME)_CFLAGS += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable
+$(NAME)_CFLAGS += -Wno-unused-value -Wno-strict-aliasing
 else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS  += -Wall -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
-$(NAME)_CFLAGS  += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized
-$(NAME)_CFLAGS  += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable
-$(NAME)_CFLAGS  += -Wno-unused-value -Wno-strict-aliasing
+$(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
+$(NAME)_CFLAGS += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized
+$(NAME)_CFLAGS += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable
+$(NAME)_CFLAGS += -Wno-unused-value -Wno-strict-aliasing
 endif
 
 $(NAME)_SOURCES := test_fw.c                             \
