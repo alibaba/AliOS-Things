ca16c3fb00d2d0521540c68ba29b9818585ff449
wuqi.hz <wuqi.hz@alibaba-inc.com>
2018-12-13 19:23:45 +0800

    BugID:18268588:delete config macro: RHINO_CONFIG_TIMER_RATE/RHINO_CONFIG_GCC_RETADDR/RHINO_CONFIG_MM_LEAKCHECK, Set RHINO_CONFIG_TASK_INFO from 10 to 1
    Change-Id: I592e987406693b54c268a6e04480a6b54a5b16cc

--- kernel/rhino/include/k_mm_debug.h
+++ kernel/rhino/include/k_mm_debug.h
@@ -5,29 +5,33 @@
 #ifndef RHINO_MM_DEBUG_H
 #define RHINO_MM_DEBUG_H
 
+#include "k_mm.h"
+
 #if (RHINO_CONFIG_MM_DEBUG > 0)
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define AOS_MM_SCAN_REGION_MAX 10
-typedef struct {
-    void *start;
-    void *end;
-} mm_scan_region_t;
-
-#if (RHINO_CONFIG_GCC_RETADDR > 0u)
-#include <k_mm.h>
-#define AOS_UNSIGNED_INT_MSB (1u << (sizeof(unsigned int) * 8 - 1))
-void krhino_owner_attach(k_mm_head *mmhead, void *addr, size_t allocator);
+#if defined(__CC_ARM)
+#define RHINO_RETURN_ADDRESS()      __return_address()
+#elif defined(__ICCARM__)
+#define RHINO_RETURN_ADDRESS()      NULL
+#elif defined(__GNUC__)
+#define RHINO_RETURN_ADDRESS()      __builtin_return_address(0)
+#else
+#define RHINO_RETURN_ADDRESS()      NULL
 #endif
 
-uint32_t krhino_mm_leak_region_init(void *start, void *end);
+#define AOS_UNSIGNED_INT_MSB        (1u << (sizeof(unsigned int) * 8 - 1))
+
+void krhino_owner_attach(void *addr, size_t allocator);
+
+/* set owner to return address */
+#define krhino_owner_return_addr(addr) \
+    krhino_owner_attach(addr, (size_t)RHINO_RETURN_ADDRESS())
 
 uint32_t dumpsys_mm_info_func(uint32_t len);
-
-uint32_t dump_mmleak(void);
 
 #ifdef __cplusplus
 }
