9688b32260482e3aebf3962c3ad08ff414d36f5a
崇龄 <yj144830@alibaba-inc.com>
2018-12-14 11:05:08 +0800

    BugID:17920924:add support of chdir getcwd creat 4
    Change-Id: I35d8138c40ff90a964b31670f9e04edcba3ab920

--- test/testcase/kernel/posix/aos.mk
+++ test/testcase/kernel/posix/aos.mk
@@ -5,12 +5,13 @@ $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := test code of posix
 $(NAME)_COMPONENTS += kernel.posix
 
-$(NAME)_SOURCES := posix_cond_test.c  \
-                   posix_mutex_test.c \
-                   posix_sem_test.c   \
-                   posix_task_test.c  \
-                   posix_test.c       \
-                   posix_tsd_test.c
+$(NAME)_SOURCES := posix_cond_test.c   \
+                   posix_mutex_test.c  \
+                   posix_sem_test.c    \
+                   posix_task_test.c   \
+                   posix_test.c        \
+                   posix_tsd_test.c    \
+                   posix_dirent_test.c
 
 $(NAME)_CFLAGS += -Wall -Werror
 
