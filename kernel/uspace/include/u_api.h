3571ddf8e5958fa73fa72e8d79a38896a8359b25
yangwencheng <wencheng.ywc@alibaba-inc.com>
2018-12-16 16:55:39 +0800

    BugID:16846667: Defined uapp default priority
    	modified:   uspace/include/u_api.h
    
    Change-Id: I41699381a3435ebfc1f413ee73123858b2fb2831

--- kernel/uspace/include/u_api.h
+++ kernel/uspace/include/u_api.h
@@ -6,12 +6,12 @@
 #define U_API_H
 
 #include <k_api.h>
-
-
 #include <u_mm.h>
 #include <u_task.h>
 #include <u_timer.h>
 #include <u_proc_msg.h>
+
+#define APP_DEFAULT_PRIO    32
 
 #endif /* U_API_H */
 
