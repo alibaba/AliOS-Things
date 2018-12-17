c086f0223f7302bd4392ba57ca58972adb5bdeef
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2018-12-17 20:08:55 +0800

    BugID:18310098:move sal-hal to drivers folder
    Change-Id: Id1e0b8cb03fed476fda981878bdd5e6c84f10575

--- platform/mcu/dahua/aos.mk
+++ platform/mcu/dahua/aos.mk
@@ -73,7 +73,7 @@ $(NAME)_SOURCES += aos/aos.c                                    \
 
 ifeq ($(SAL),1)
 $(NAME)_SOURCES += hal/wifi_port.c
-GLOBAL_INCLUDES += ../../../network/nal/sal-hal/wifi/esp8266
+GLOBAL_INCLUDES += ../../../drivers/sal/wifi/esp8266
 endif
 
 ifeq ($(LWIP),1)
