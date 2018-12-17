88d7e460a21f3415aaf37fb31b95a4ae4869fb7a
崇龄 <yj144830@alibaba-inc.com>
2018-12-17 14:08:11 +0800

    BugID:17920924:move posix to osal4
    Change-Id: I254a0e6b099b9ba415b466d4e36c55bae5532205

---
+++ osal/posix/posix_init.c
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#include "posix/posix_init.h"
+
+extern int pthread_lock_init(void);
+extern int timer_lock_init(void);
+
+int posix_init(void)
+{
+    int ret = -1;
+
+#if (POSIX_PTHREAD_ENABLE > 0)
+    ret = pthread_lock_init();
+    if (ret != 0) {
+        return -1;
+    }
+#endif
+
+#if (POSIX_TIMER_ENABLE > 0)
+    ret = timer_lock_init();
+    if (ret != 0) {
+        return -1;
+    }
+#endif
+
+    return ret;
+}
