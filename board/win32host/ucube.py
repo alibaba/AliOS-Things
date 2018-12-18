ad1ed9613d8acfcea815dc18e726dfd42a91fe8c
崇龄 <yj144830@alibaba-inc.com>
2018-12-18 19:36:02 +0800

    BugID:17920924:move posix testcase 1
    Change-Id: I9e835438a08a8993bbce1aaab6a93b8f81b5726e

--- board/win32host/ucube.py
+++ board/win32host/ucube.py
@@ -4,23 +4,23 @@ component = aos_board_component('win32host', 'windows', src)
 
 # Define the default component testcase set for this board
 testcases = Split('''
-    test/testcase/basic_test 
-    test/testcase/framework/alink_test 
-    test/testcase/middleware/uagent/uota_test 
-    test/testcase/framework/mqtt_test 
-    test/testcase/network/netmgr_test 
-    test/testcase/framework/wifi_hal_test 
+    test/testcase/basic_test
+    test/testcase/framework/alink_test
+    test/testcase/middleware/uagent/uota_test
+    test/testcase/framework/mqtt_test
+    test/testcase/network/netmgr_test
+    test/testcase/framework/wifi_hal_test
     test/testcase/kernel/modules/fatfs_test
-    test/testcase/kernel/modules/kv_test 
-    test/testcase/kernel/rhino_test 
-    test/testcase/osal_test 
-    test/testcase/kernel/vfs_test 
-    test/testcase/network/protocols/umesh_test 
-    test/testcase/network/yloop_test 
-    test/testcase/security/alicrypto_test 
+    test/testcase/kernel/modules/kv_test
+    test/testcase/kernel/rhino_test
+    test/testcase/osal_test/aos/osal_aos_test
+    test/testcase/kernel/vfs_test
+    test/testcase/network/protocols/umesh_test
+    test/testcase/network/yloop_test
+    test/testcase/security/alicrypto_test
     test/testcase/security/id2_test
     test/testcase/security/tls_test
-    test/testcase/utility/cjson_test 
+    test/testcase/utility/cjson_test
 ''')
 
 component.set_global_testcases(testcases)
