e2dee2ea1a9590a3191104fb35da3fb99fc140c7
secretmikut <yuhao.tyh@alibaba-inc.com>
2018-12-19 23:56:01 +0800

    stop call thing_prop_changed after set property value using linkkit_set_value

--- framework/protocol/linkkit/dm/src/dm_impl.c
+++ framework/protocol/linkkit/dm/src/dm_impl.c
@@ -95,6 +95,7 @@ static int dm_impl_set_property_value(void *_self, const void *thing_id, const v
            (value || value_str));
 
     HAL_MutexLock(thing_manager_object->_usercall_mutex);
+    thing_manager_object->_property_identifier_set_from_cloud = 0;
     res = (*thing_manager)->set_thing_property_value(thing_manager, thing_id, identifier, value, value_str);
     HAL_MutexUnlock(thing_manager_object->_usercall_mutex);
 
