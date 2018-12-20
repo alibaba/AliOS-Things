0caacbd0ea5ba5df044951f3d3f57f4be033aea6
zhuqing <zhuqing.zq@alibaba-inc.com>
2018-12-20 15:09:43 +0800

    BugID:16944925:add board for Sstm32f103ze according to BSP standands;dir board
    Change-Id: Ia0fcbe418b2e37d15fb920e08b685da3e59d8a4a

---
+++ board/stm32f103ze/aos.mk
@@ -0,0 +1,56 @@
+NAME := board_stm32f103ze
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.0
+$(NAME)_SUMMARY    := configuration for board stm32f429zi-nucleo
+MODULE             := 1062
+HOST_ARCH          := Cortex-M3
+HOST_MCU_FAMILY    := mcu_stm32f1xx
+SUPPORT_MBINS       := no
+HOST_MCU_NAME      := STM32F103ZETx
+ENABLE_VFP         := 0
+
+$(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) osal init
+
+$(NAME)_SOURCES += config/k_config.c \
+                    config/partition_conf.c  \
+                    drivers/sram.c    \
+                    drivers/gpio.c    \
+                    drivers/stm32f1xx_hal_msp.c    \
+                    drivers/stm32f1xx_main.c    \
+                    startup/board.c   \
+                    startup/startup.c
+
+ywss_support    ?= 0
+
+GLOBAL_DEFINES += CONFIG_AOS_KV_BUFFER_SIZE=32768 #32kb
+GLOBAL_DEFINES += CONFIG_AOS_KV_BLK_BITS=14 #(1 << 14) = 16kb
+GLOBAL_DEFINES += CONFIG_NO_TCPIP
+
+#depends on sal module if select sal function via build option "AOS_NETWORK_SAL=y"
+AOS_NETWORK_SAL    ?= n
+
+ifeq ($(COMPILER), armcc)
+$(NAME)_SOURCES += startup/startup_keil.s
+$(NAME)_LINK_FILES := startup/startup_keil.o
+GLOBAL_LDFLAGS += -L --scatter=board/stm32f103ze/stm32f103ze.sct
+else ifeq ($(COMPILER), iar)
+$(NAME)_SOURCES += startup/startup_iar.s
+GLOBAL_LDFLAGS += --config board/stm32f103ze/stm32f103ze.icf
+else
+GLOBAL_CFLAGS += -DUSE_DIRECT_UART_PUTC
+$(NAME)_SOURCES += startup/startup_gcc.s
+GLOBAL_LDFLAGS += -T board/stm32f103ze/stm32f103ze.ld
+endif
+
+GLOBAL_INCLUDES += . \
+                   config/ \
+                   drivers/
+
+GLOBAL_CFLAGS += -DSTM32F103xE -DCENTRALIZE_MAPPING -DHAL_SRAM_MODULE_ENABLED
+
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_f103
+CONFIG_SYSINFO_DEVICE_NAME   := stm32f103
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
