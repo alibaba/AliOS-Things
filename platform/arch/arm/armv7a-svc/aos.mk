59b85a7e9642f16aa698a4b165273b92160a6e26
congling.zm <congling.zm@alibaba-inc.com>
2018-12-20 15:15:20 +0800

    BugID:18372741:update version from 0.0.1 to 1.0.0. plaftorm/
    Change-Id: I9d4cb29171c84ef60362385c13b8f78501c24bb4

--- platform/arch/arm/armv7a-svc/aos.mk
+++ platform/arch/arm/armv7a-svc/aos.mk
@@ -1,7 +1,7 @@
 NAME := arch_armv7a-svc
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 0.0.1.0
+$(NAME)_VERSION    := 1.0.0
 $(NAME)_SUMMARY    := arch for armv7a
 
 ifeq ($(ENABLE_USPACE),1)
