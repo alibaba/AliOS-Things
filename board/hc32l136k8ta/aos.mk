2b493a75eb3204221c3a5ddbc50ea86c7619f078
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-12-20 17:16:37 +0800

    BugID:17922164: Add ssc1667 & hc32l136k8ta board (keil support)
    Change-Id: I87a6098431c3dc98c3a1c0710c464c19e3dd48aa

--- board/hc32l136k8ta/aos.mk
+++ board/hc32l136k8ta/aos.mk
@@ -1,13 +1,13 @@
-NAME := board_hr8p296fllt
+NAME := board_hc32l136k8ta
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.0
-$(NAME)_SUMMARY    := configuration for board hr8p296fllt
+$(NAME)_SUMMARY    := configuration for board hc32l136k8ta
 MODULE             := 1062
 HOST_ARCH          := Cortex-M0
-HOST_MCU_FAMILY    := mcu_hr8p2xx
+HOST_MCU_FAMILY    := mcu_hc32l136
 SUPPORT_MBINS      := no
-HOST_MCU_NAME      := HR8P296
+HOST_MCU_NAME      := hc32l136
 ENABLE_VFP         := 0
 
 $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) osal newlib_stub
@@ -15,12 +15,12 @@ $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) osal newlib_stub
 $(NAME)_SOURCES += config/k_config.c \
                    startup/board.c   \
                    startup/startup.c \
-                   drivers/irqhandler.c
+                   drivers/interrupts_hc32l136.c
 
 ifeq ($(COMPILER), armcc)
 $(NAME)_SOURCES    += startup/startup_keil.s
 $(NAME)_LINK_FILES := startup/startup_keil.o
-GLOBAL_LDFLAGS += -L --scatter=board/hr8p296fllt/hr8p296.sct
+GLOBAL_LDFLAGS += -L --scatter=board/hc32l136k8ta/hc32l136.sct
 endif
 
 GLOBAL_INCLUDES += .    \
@@ -29,5 +29,5 @@ GLOBAL_INCLUDES += .    \
                    startup/  \
 
 # Keil project support
-$(NAME)_KEIL_VENDOR = HR
-$(NAME)_KEIL_DEVICE = HR8P296FLLT
+$(NAME)_KEIL_VENDOR = HDSC
+$(NAME)_KEIL_DEVICE = HC32L136
