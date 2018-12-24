6bd6862b2750eee87bb5ab810d2b379ead3b9653
崇龄 <yj144830@alibaba-inc.com>
2018-12-24 17:03:41 +0800

    BugID:17920924:modify macro name 2
    Change-Id: Ib5e8f5f35a953805927c0bbf0d6749ae083637be

--- include/posix/posix_default_config.h
+++ include/posix/posix_default_config.h
@@ -10,39 +10,45 @@ extern "C"
 {
 #endif
 
-/* module enable */
-#ifndef POSIX_PTHREAD_ENABLE
-#define POSIX_PTHREAD_ENABLE        1
+/* set to 1 if you want to enable pthread, set to 0 to disable pthread */
+#ifndef POSIX_CONFIG_PTHREAD_ENABLE
+#define POSIX_CONFIG_PTHREAD_ENABLE        1
 #endif
 
-#ifndef POSIX_MQUEUE_ENABLE
-#define POSIX_MQUEUE_ENABLE         1
+/* set to 1 if you want to enable mqueue, set to 0 to disable mqueue */
+#ifndef POSIX_CONFIG_MQUEUE_ENABLE
+#define POSIX_CONFIG_MQUEUE_ENABLE         1
 #endif
 
-#ifndef POSIX_SEMAPHORE_ENABLE
-#define POSIX_SEMAPHORE_ENABLE      1
+/* set to 1 if you want to enable semaphore, set to 0 to disable semaphore */
+#ifndef POSIX_CONFIG_SEMAPHORE_ENABLE
+#define POSIX_CONFIG_SEMAPHORE_ENABLE      1
 #endif
 
-#ifndef POSIX_TIMER_ENABLE
-#define POSIX_TIMER_ENABLE          1
+/* set to 1 if you want to enable timer, set to 0 to disable timer */
+#ifndef POSIX_CONFIG_TIMER_ENABLE
+#define POSIX_CONFIG_TIMER_ENABLE          1
 #endif
 
-#ifndef POSIX_DIRENT_ENABLE
-#define POSIX_DIRENT_ENABLE         1
+/* set to 1 if you want to enable dirent, set to 0 to disable dirent */
+#ifndef POSIX_CONFIG_DIRENT_ENABLE
+#define POSIX_CONFIG_DIRENT_ENABLE         1
 #endif
 
-/* POSIX_DIRENT_ENABLE must set to 1 before this enable */
+/* POSIX_CONFIG_DIRENT_ENABLE must set to 1 before this enable */
 /* ramfs must be added to the prj before this enable */
-#ifndef POSIX_DIRENT_TMPFILE_ENABLE
-#define POSIX_DIRENT_TMPFILE_ENABLE 1
+/* set to 1 if you want to enable tmpfile, set to 0 to disable tmpfile */
+#ifndef POSIX_CONFIG_DIRENT_TMPFILE_ENABLE
+#define POSIX_CONFIG_DIRENT_TMPFILE_ENABLE 1
 #endif
 
-#ifndef POSIX_PRCTL_ENABLE
-#define POSIX_PRCTL_ENABLE          1
+/* set to 1 if you want to enable prctl, set to 0 to disable prctl */
+#ifndef POSIX_CONFIG_PRCTL_ENABLE
+#define POSIX_CONFIG_PRCTL_ENABLE          1
 #endif
 
 /* the position of pthread struct in user info in TCB */
-#define PTHREAD_USER_INFO_POS 0
+#define PTHREAD_CONFIG_USER_INFO_POS 0
 
 #ifdef __cplusplus
 }
