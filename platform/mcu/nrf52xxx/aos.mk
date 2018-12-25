35c441dbffcb9ac96a8a8c09f2d6db0ab5e649f1
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2018-12-25 15:01:47 +0800

    BugID:18356074:Move bt controller macro to bt controller mk
    Change-Id: I69c4c941fdd3fa821e7536253da1093e7bb796de

--- platform/mcu/nrf52xxx/aos.mk
+++ platform/mcu/nrf52xxx/aos.mk
@@ -126,4 +126,3 @@ include $($(NAME)_LOCATION)/$(HOST_MCU_NAME).mk
 
 include $($(NAME)_LOCATION)/bt_controller/bt_controller.mk
 #$(NAME)_COMPONENTS += bt_controller
-GLOBAL_DEFINES-y += CONFIG_BT_CTLR
