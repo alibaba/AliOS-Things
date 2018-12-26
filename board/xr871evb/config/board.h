79a60c410560e442dc11083b0544baee69b78aba
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-12-26 19:14:29 +0800

    BugID:17922164: Add xr871evb & xr809 board support (from github XRADIO)
    Change-Id: Ib9a12f2c3398068c5ac793c7c55865cedb91a089

---
+++ board/xr871evb/config/board.h
@@ -0,0 +1,10 @@
+#define HARDWARE_REVISION   "V1.0"
+#define MODEL               "XR871"
+
+#ifdef BOOTLOADER
+#define STDIO_UART 0
+#define STDIO_UART_BUADRATE 115200
+#else
+#define STDIO_UART 0
+#define STDIO_UART_BUADRATE 115200
+#endif
