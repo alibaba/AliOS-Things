87c0128a20b09c32f8c985bd5273d7f5ced6a5a0
jianniu.ljg|lijianguo <jianniu.ljg@alibaba-inc.com>
2018-12-28 19:39:45 +0800

    BugID:17712167:pwr management componentization
    Change-Id: I2386f1eceb57324b6a040c4d2e3edfdd61801c39

---
+++ include/aos/pwrmgmt.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+
+#ifndef AOS_PWRMGMT_H
+#define AOS_PWRMGMT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <pwrmgmt_api.h>
+
+int aos_pwrmgmt_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* AOS_PWRMGMT_H */
