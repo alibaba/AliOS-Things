9b9e62e6933427d7795556189bf7ff2b84cb2a1c
Fan Qian <qianfan.qf@alibaba-inc.com>
2018-12-28 20:22:12 +0800

    BugID:17922164: Remove unnecessary mk for xr871 platform
    Change-Id: I02a78285e028a0f2081df3be344d01b7891ce3b6

--- platform/mcu/xr871/drivers/src/audio/audio_manager/audio_manager.mk
+++
@@ -1,8 +0,0 @@
-
-
-NAME := audio_manager
-
-$(NAME)_TYPE := kernel
-$(NAME)_SOURCES := \
-                   audio_manager.c
-
--- platform/mcu/xr871/drivers/src/net/lwip/lwip.mk
+++
@@ -1,5 +0,0 @@
-NAME := xrlwip
-
-$(NAME)_TYPE := kernel
-$(NAME)_SOURCES := memcpy.c \
-                   checksum.c
