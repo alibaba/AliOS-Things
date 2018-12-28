f5885453470d9ec3e4ab39e2dfc5d0406df0b8f6
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2018-12-28 06:45:31 +0000

    BugID:18314777: merge for lora team sdk
    Change-Id: I5a29a1148917c748473333ce7a2520cf89b978d5

--- include/network/hal/lorawan.h
+++ include/network/hal/lorawan.h
@@ -10,8 +10,7 @@ extern "C" {
 #endif
 
 #include <stdint.h>
-
-#include "lora/system/timer.h"
+#include "system/timer.h"
 
 /* the struct is for changing the device working mode */
 typedef struct {
@@ -19,33 +18,6 @@ typedef struct {
     void (*exit_stop_mode)(void);
     void (*enter_sleep_mode)(void);
 } hal_lrwan_dev_chg_mode_t;
-
-/* LoRaWan time and timer interface */
-typedef struct {
-    void     (*delay_ms)(uint32_t delay);
-    uint32_t (*set_timer_context)(void);
-    uint32_t (*get_timer_context)(void);
-    uint32_t (*get_delta_context)(uint32_t now, uint32_t old);
-    uint32_t (*get_timer_elapsed_time)(void);
-    void     (*stop_alarm)(void);
-    void     (*set_alarm)(uint32_t timeout);
-    void     (*set_uc_wakeup_time)(void);
-    void     (*set_timeout)(TimerEvent_t *obj);
-    void     (*set_timer_val)(TimerEvent_t *obj, uint32_t value);
-
-    TimerTime_t (*compute_elapsed_time)(TimerTime_t time);
-    TimerTime_t (*get_current_time)(void);
-    TimerTime_t (*get_temp_compensation)(TimerTime_t period, float temperature);
-} hal_lrwan_time_itf_t;
-
-/* the struct is for control of radio */
-typedef struct {
-    void     (*radio_reset)(void);
-    void     (*radio_reset_cfg_input)(void);
-    void     (*radio_rw_en)(void);
-    void     (*radio_rw_dis)(void);
-    uint16_t (*radio_rw)(uint16_t tx_data);
-} hal_lrwan_radio_ctrl_t;
 
 typedef struct {
     uint8_t  (*get_battery_level)(void);
