f842e575c1ecf94a70bbc4cb4330aca9a457d5f7
zhangtian <dongren.zt@alibaba-inc.com>
2019-01-02 18:35:17 +0800

    bugID:17646749:[cm] add setup retry
    Change-Id: I5fae1d0ad6a063d124e4d6ef874ed969d6048961

--- middleware/linkkit/sdk-c/src/services/linkkit/cm/iotx_cm_mqtt.c
+++ middleware/linkkit/sdk-c/src/services/linkkit/cm/iotx_cm_mqtt.c
@@ -222,21 +222,25 @@ static int  _mqtt_connect(uint32_t timeout)
     HAL_GetDeviceName(device_name);
     HAL_GetDeviceSecret(device_secret);
 
-    /* Device AUTH */
-    if (0 != IOT_SetupConnInfo(product_key, device_name, device_secret, (void **)&pconn_info)) {
-        cm_err("IOT_SetupConnInfo failed");
-        return -1;
-    }
-
-    mqtt_param->port = pconn_info->port;
-    mqtt_param->host = pconn_info->host_name;
-    mqtt_param->client_id = pconn_info->client_id;
-    mqtt_param->username = pconn_info->username;
-    mqtt_param->password = pconn_info->password;
-    mqtt_param->pub_key = pconn_info->pub_key;
+    ARGUMENT_SANITY_CHECK(strlen(device_name), FAIL_RETURN);
+    ARGUMENT_SANITY_CHECK(strlen(product_key), FAIL_RETURN);
 
     iotx_time_init(&timer);
     utils_time_countdown_ms(&timer, timeout);
+    /* Device AUTH */
+    do {
+        if (0 == IOT_SetupConnInfo(product_key, device_name, device_secret, (void **)&pconn_info)) {
+            mqtt_param->port = pconn_info->port;
+            mqtt_param->host = pconn_info->host_name;
+            mqtt_param->client_id = pconn_info->client_id;
+            mqtt_param->username = pconn_info->username;
+            mqtt_param->password = pconn_info->password;
+            mqtt_param->pub_key = pconn_info->pub_key;
+            break;
+        }
+        cm_err("IOT_SetupConnInfo failed");
+        HAL_SleepMs(500);
+    } while (!utils_time_is_expired(&timer));
 
     do {
         pclient = IOT_MQTT_Construct((iotx_mqtt_param_t *)_mqtt_conncection->open_params);
@@ -251,6 +255,7 @@ static int  _mqtt_connect(uint32_t timeout)
             }
             return 0;
         }
+        HAL_SleepMs(500);
     } while (!utils_time_is_expired(&timer));
 
     iotx_cm_event_msg_t event;
