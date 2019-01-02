08e8e964acf7674e3c26d28ac15b2a9bd16a7e47
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-01-02 14:20:03 +0800

    BugID:18268588:remove redundant architecture files
    Change-Id: I23a975200694d08b5a5ed51cf66b234ac14b750a

--- platform/arch/arm/armv6m/aos.mk
+++ platform/arch/arm/armv6m/aos.mk
@@ -4,22 +4,19 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.0
 $(NAME)_SUMMARY    := arch for armv6m
 
-$(NAME)_SOURCES := panic/panic_c.c
+$(NAME)_SOURCES := common/panic_c.c
+$(NAME)_SOURCES += common/port_c.c
+
+GLOBAL_INCLUDES += common/
 
 ifeq ($(COMPILER),armcc)
 GLOBAL_ASMFLAGS += --cpreproc
-$(NAME)_SOURCES += panic/panic_armcc.S
-$(NAME)_SOURCES += armcc/m0/port_c.c
+$(NAME)_SOURCES += common/panic_armcc.S
 $(NAME)_SOURCES += armcc/m0/port_s.S
-GLOBAL_INCLUDES += armcc/m0/
 else ifeq ($(COMPILER),iar)
-$(NAME)_SOURCES += panic/panic_iccarm.S
-$(NAME)_SOURCES += iccarm/m0/port_c.c
+$(NAME)_SOURCES += common/panic_iccarm.S
 $(NAME)_SOURCES += iccarm/m0/port_s.S
-GLOBAL_INCLUDES += iccarm/m0/
 else
-$(NAME)_SOURCES += panic/panic_gcc.S
-$(NAME)_SOURCES += gcc/m0/port_c.c
+$(NAME)_SOURCES += common/panic_gcc.S
 $(NAME)_SOURCES += gcc/m0/port_s.S
-GLOBAL_INCLUDES += gcc/m0/
 endif
--- platform/arch/arm/armv7m/ucube.py
+++ platform/arch/arm/armv7m/ucube.py
@@ -1,39 +1,32 @@
 src = []
+
+component = aos_arch_component('armv7m', src)
+component.add_sources('common/panic_c.c', 'common/port_c.c')
+component.add_global_includes('common/')
+
 if aos_global_config.compiler == 'armcc':
-    component = aos_arch_component('armv7m', src)
     component.add_global_asflags('--cpreproc')
-    component.add_sources('panic/panic_c.c', 'panic/panic_armcc.S')
+    component.add_sources('common/panic_armcc.S')
     if aos_global_config.arch == 'Cortex-M3':
-        component.add_sources('armcc/m3/port_c.c', 'armcc/m3/port_s.S')
-        component.add_global_includes('armcc/m3')
+        component.add_sources('armcc/m3/port_s.S')
     elif aos_global_config.arch == 'Cortex-M4':
-        component.add_sources('armcc/m4/port_c.c', 'armcc/m4/port_s.S')
-        component.add_global_includes('armcc/m4')
+        component.add_sources('armcc/m4/port_s.S')
     elif aos_global_config.arch == 'Cortex-M7':
-        component.add_sources('armcc/m7/port_c.c', 'armcc/m7/port_s.S')
-        component.add_global_includes('armcc/m7')
+        component.add_sources('armcc/m7/port_s.S')
 elif aos_global_config.compiler == 'iar':
-    component = aos_arch_component('armv7m', src)
-    component.add_sources('panic/panic_c.c', 'panic/panic_iccarm.S')
+    component.add_sources('common/panic_iccarm.S')
     if aos_global_config.arch == 'Cortex-M3':
-        component.add_sources('iccarm/m3/port_c.c', 'iccarm/m3/port_s.S')
-        component.add_global_includes('iccarm/m3')
+        component.add_sources('iccarm/m3/port_s.S')
     elif aos_global_config.arch == 'Cortex-M4':
-        component.add_sources('iccarm/m4/port_c.c', 'iccarm/m4/port_s.S')
-        component.add_global_includes('iccarm/m4')
+        component.add_sources('iccarm/m4/port_s.S')
     elif aos_global_config.arch == 'Cortex-M7':
-        component.add_sources('iccarm/m7/port_c.c', 'iccarm/m7/port_s.S')
-        component.add_global_includes('iccarm/m7')
+        component.add_sources('iccarm/m7/port_s.S')
 else:
-    component = aos_arch_component('armv7m', src)
-    component.add_sources('panic/panic_c.c', 'panic/panic_gcc.S')
+    component.add_sources('common/panic_gcc.S')
     if aos_global_config.arch == 'Cortex-M3':
-        component.add_sources('gcc/m3/port_c.c', 'gcc/m3/port_s.S')
-        component.add_global_includes('gcc/m3')
+        component.add_sources('gcc/m3/port_s.S')
     elif aos_global_config.arch == 'Cortex-M4':
-        component.add_sources('gcc/m4/port_c.c', 'gcc/m4/port_s.S')
-        component.add_global_includes('gcc/m4')
+        component.add_sources('gcc/m4/port_s.S')
     elif aos_global_config.arch == 'Cortex-M7':
-        component.add_sources('gcc/m7/port_c.c', 'gcc/m7/port_s.S')
-        component.add_global_includes('gcc/m7')
+        component.add_sources('gcc/m7/port_s.S')
 
