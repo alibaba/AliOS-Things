de0d518b619f6bafa57226e38fccf32a07e3c33e
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-01-02 10:02:20 +0800

    BugID:18522021:Fix otaapp download fail for with sal/atparser
    Change-Id: If359443a93f70b13a4ff67e78b4542d5a6f1b21f

---
+++ network/nal/atparser/atparser_opts.h
@@ -0,0 +1,18 @@
+#ifndef _ATPARSER_OPTS_H_
+#define _ATPARSER_OPTS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define ATPSR_OPT_ENABLED       1
+#define ATPSR_OPT_DISABLED      0
+
+#define ATPSR_SINGLE_TASK       ATPSR_OPT_DISABLED
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
+
