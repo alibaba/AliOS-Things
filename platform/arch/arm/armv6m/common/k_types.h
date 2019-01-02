f3177d9bb946e352f2b137b6aa27cfe1a0308836
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-01-02 14:20:03 +0800

    BugID:18268588:remove redundant architecture files
    Change-Id: I23a975200694d08b5a5ed51cf66b234ac14b750a

--- platform/arch/arm/armv6m/common/k_types.h
+++ platform/arch/arm/armv6m/common/k_types.h
@@ -5,8 +5,8 @@
 #ifndef K_TYPES_H
 #define K_TYPES_H
 
-#define RHINO_TASK_STACK_OVF_MAGIC   0xdeadbeafu     /* 32 bit or 64 bit stack overflow magic value */
-#define RHINO_INTRPT_STACK_OVF_MAGIC 0xdeaddeadu     /* 32 bit or 64 bit stack overflow magic value */
+#define RHINO_TASK_STACK_OVF_MAGIC   0xdeadbeafu     /* stack overflow magic value */
+#define RHINO_INTRPT_STACK_OVF_MAGIC 0xdeaddeadu     /* stack overflow magic value */
 #define RHINO_MM_CORRUPT_DYE         0xFEFEFEFE
 #define RHINO_MM_FREE_DYE            0xABABABAB
 
@@ -23,15 +23,16 @@
 #error "Unsupported compiler"
 #endif
 
+
 typedef char     name_t;
 typedef uint32_t sem_count_t;
 typedef uint32_t cpu_stack_t;
-typedef uint64_t hr_timer_t;
-typedef uint64_t lr_timer_t;
+typedef uint32_t hr_timer_t;
+typedef uint32_t lr_timer_t;
 typedef uint32_t mutex_nested_t;
 typedef uint8_t  suspend_nested_t;
 typedef uint64_t ctx_switch_t;
 typedef uint32_t cpu_cpsr_t;
 
-#endif /* TYPES_H */
+#endif /* K_TYPES_H */
 
