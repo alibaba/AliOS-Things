139c2fb1d14c655e22984ddedf6739a2f0448b78
zqyhcf <36759024+zqyhcf@users.noreply.github.com>
2019-01-02 19:11:32 +0800

    add aaboard_demo for aamcu_demo according to BSP standards (#790)
    * add aaboard_demo for aamcu_demo according to BSP standards
    * rm linux_only_targets for rv32m1_vega

---
+++ platform/mcu/aamcu_demo/hal/hw.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+#include <k_api.h>
+#include <aos/log.h>
+#include <hal/soc/soc.h>
+#include <hal/soc/timer.h>
+#include <hal/base.h>
+
+void hal_reboot(void)
+{
+}
