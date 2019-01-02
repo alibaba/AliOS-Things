ea05e7a15425d19d417c8990856729de3fb8950d
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-01-02 14:20:03 +0800

    BugID:18268588:remove redundant architecture files
    Change-Id: I23a975200694d08b5a5ed51cf66b234ac14b750a

--- platform/arch/arm/armv7a/aos.mk
+++ platform/arch/arm/armv7a/aos.mk
@@ -8,6 +8,7 @@ $(NAME)_SOURCES := common/k_vector.c
 $(NAME)_SOURCES += common/k_cache.c
 $(NAME)_SOURCES += common/k_mmu_sd.c
 $(NAME)_SOURCES += common/panic.c
+$(NAME)_SOURCES += common/port_c.c
 GLOBAL_INCLUDES += common
 
 ifeq ($(COMPILER),armcc)
@@ -18,18 +19,13 @@ $(NAME)_SOURCES += common/k_cache_armcc.S
 ifeq ($(HOST_ARCH),Cortex-A5)
 $(NAME)_SOURCES += armcc/a5/port_s.S
 $(NAME)_SOURCES += armcc/a5/vector_s.S
-$(NAME)_SOURCES += armcc/a5/port_c.c
-GLOBAL_INCLUDES += armcc/a5
 else ifeq ($(HOST_ARCH),Cortex-A7)
 $(NAME)_SOURCES += armcc/a7/port_s.S
 $(NAME)_SOURCES += armcc/a7/vector_s.S
-$(NAME)_SOURCES += armcc/a7/port_c.c
-GLOBAL_INCLUDES += armcc/a7
 else ifeq ($(HOST_ARCH),Cortex-A9)
 $(NAME)_SOURCES += armcc/a9/port_s.S
 $(NAME)_SOURCES += armcc/a9/vector_s.S
-$(NAME)_SOURCES += armcc/a9/port_c.c
-GLOBAL_INCLUDES += armcc/a9
+
 endif
 
 else ifeq ($(COMPILER),iar)
@@ -39,18 +35,12 @@ $(NAME)_SOURCES += common/k_cache_iccarm.S
 ifeq ($(HOST_ARCH),Cortex-A5)
 $(NAME)_SOURCES += iccarm/a5/port_s.S
 $(NAME)_SOURCES += iccarm/a5/vector_s.S
-$(NAME)_SOURCES += iccarm/a5/port_c.c
-GLOBAL_INCLUDES += iccarm/a5
 else ifeq ($(HOST_ARCH),Cortex-A7)
 $(NAME)_SOURCES += iccarm/a7/port_s.S
 $(NAME)_SOURCES += iccarm/a7/vector_s.S
-$(NAME)_SOURCES += iccarm/a7/port_c.c
-GLOBAL_INCLUDES += iccarm/a7
 else ifeq ($(HOST_ARCH),Cortex-A9)
 $(NAME)_SOURCES += iccarm/a9/port_s.S
 $(NAME)_SOURCES += iccarm/a9/vector_s.S
-$(NAME)_SOURCES += iccarm/a9/port_c.c
-GLOBAL_INCLUDES += iccarm/a9
 endif
 
 else
@@ -60,18 +50,12 @@ $(NAME)_SOURCES += common/k_cache_gcc.S
 ifeq ($(HOST_ARCH),Cortex-A5)
 $(NAME)_SOURCES += gcc/a5/port_s.S
 $(NAME)_SOURCES += gcc/a5/vector_s.S
-$(NAME)_SOURCES += gcc/a5/port_c.c
-GLOBAL_INCLUDES += gcc/a5
 else ifeq ($(HOST_ARCH),Cortex-A7)
 $(NAME)_SOURCES += gcc/a7/port_s.S
 $(NAME)_SOURCES += gcc/a7/vector_s.S
-$(NAME)_SOURCES += gcc/a7/port_c.c
-GLOBAL_INCLUDES += gcc/a7
 else ifeq ($(HOST_ARCH),Cortex-A9)
 $(NAME)_SOURCES += gcc/a9/port_s.S
 $(NAME)_SOURCES += gcc/a9/vector_s.S
-$(NAME)_SOURCES += gcc/a9/port_c.c
-GLOBAL_INCLUDES += gcc/a9
 endif
 
 endif
--- platform/arch/arm/armv7m/aos.mk
+++ platform/arch/arm/armv7m/aos.mk
@@ -4,70 +4,43 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.0
 $(NAME)_SUMMARY    := arch for armv7m
 
-$(NAME)_SOURCES := panic/panic_c.c
+$(NAME)_SOURCES := common/panic_c.c
+$(NAME)_SOURCES += common/port_c.c
+
+GLOBAL_INCLUDES += common/
 
 ifeq ($(COMPILER),armcc)
-$(NAME)_SOURCES += panic/panic_armcc.S
+$(NAME)_SOURCES += common/panic_armcc.S
 GLOBAL_ASMFLAGS += --cpreproc
 
 ifeq ($(HOST_ARCH),Cortex-M3)
-$(NAME)_SOURCES += armcc/m3/port_c.c
 $(NAME)_SOURCES += armcc/m3/port_s.S
-GLOBAL_INCLUDES += armcc/m3/
 else ifeq ($(HOST_ARCH),Cortex-M4)
-$(NAME)_SOURCES += armcc/m4/port_c.c
 $(NAME)_SOURCES += armcc/m4/port_s.S
-GLOBAL_INCLUDES += armcc/m4/
 else ifeq ($(HOST_ARCH),Cortex-M7)
-$(NAME)_SOURCES += armcc/m7/port_c.c
 $(NAME)_SOURCES += armcc/m7/port_s.S
-GLOBAL_INCLUDES += armcc/m7/
 endif
 
 else ifeq ($(COMPILER),iar)
-$(NAME)_SOURCES += panic/panic_iccarm.S
+$(NAME)_SOURCES += common/panic_iccarm.S
 
 ifeq ($(HOST_ARCH),Cortex-M3)
-$(NAME)_SOURCES += iccarm/m3/port_c.c
 $(NAME)_SOURCES += iccarm/m3/port_s.S
-GLOBAL_INCLUDES += iccarm/m3/
 else ifeq ($(HOST_ARCH),Cortex-M4)
-$(NAME)_SOURCES += iccarm/m4/port_c.c
 $(NAME)_SOURCES += iccarm/m4/port_s.S
-GLOBAL_INCLUDES += iccarm/m4/
 else ifeq ($(HOST_ARCH),Cortex-M7)
-$(NAME)_SOURCES += iccarm/m7/port_c.c
 $(NAME)_SOURCES += iccarm/m7/port_s.S
-GLOBAL_INCLUDES += iccarm/m7/
 endif
 
 else
-$(NAME)_SOURCES += panic/panic_gcc.S
+$(NAME)_SOURCES += common/panic_gcc.S
 
 ifeq ($(HOST_ARCH),Cortex-M3)
-$(NAME)_SOURCES += gcc/m3/port_c.c
 $(NAME)_SOURCES += gcc/m3/port_s.S
-GLOBAL_INCLUDES += gcc/m3/
 else ifeq ($(HOST_ARCH),Cortex-M4)
-ifeq ($(ENABLE_USPACE),1)
-$(NAME)_SOURCES += gcc/syscall/m4/port_c.c
-$(NAME)_SOURCES += gcc/syscall/m4/port_s.S
-$(NAME)_SOURCES += gcc/syscall/svc/svc.S
-$(NAME)_SOURCES += gcc/syscall/svc/fault.c
-$(NAME)_SOURCES += gcc/syscall/mpu/arch_mpu.c
-$(NAME)_SOURCES += gcc/syscall/mpu/mpu.c
-GLOBAL_INCLUDES += gcc/syscall/m4/
-GLOBAL_INCLUDES += gcc/syscall/svc/
-GLOBAL_INCLUDES += gcc/syscall/include/
-else
-$(NAME)_SOURCES += gcc/m4/port_c.c
 $(NAME)_SOURCES += gcc/m4/port_s.S
-GLOBAL_INCLUDES += gcc/m4/
-endif
 else ifeq ($(HOST_ARCH),Cortex-M7)
-$(NAME)_SOURCES += gcc/m7/port_c.c
 $(NAME)_SOURCES += gcc/m7/port_s.S
-GLOBAL_INCLUDES += gcc/m7/
 endif
 
 endif
