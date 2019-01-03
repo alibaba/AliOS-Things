b955cc0b25e4361c6292d0124a0a5dbe3b5bc5c6
zhuqing <zhuqing.zq@alibaba-inc.com>
2019-01-03 11:50:18 +0800

    BugID:16944925:sort rhinorun example;dir board
    Change-Id: I5c4b944f975fbcb9eaceb9ca7b4f8c61d51ccb05

--- board/stm32f769i-discovery/aos.mk
+++ board/stm32f769i-discovery/aos.mk
@@ -11,9 +11,32 @@ HOST_MCU_NAME      := STM32F769I-Discovery
 
 $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) osal init
 
-GLOBAL_INCLUDES += .
-
+$(NAME)_SOURCES += config/k_config.c \
+                    config/partition_conf.c  \
+                    drivers/stm32f7xx_hal_msp.c    \
+                    drivers/stm32f7xx_it.c    \
+		            drivers/system_stm32f7xx.c    \
+                    startup/board.c   \
+                    startup/startup.c
+ifeq ($(COMPILER), armcc)
+$(NAME)_SOURCES += startup/startup_keil.s
+$(NAME)_LINK_FILES += startup/startup_keil.o
+$(NAME)_LINK_FILES += drivers/stm32f7xx_it.o
+$(NAME)_LINK_FILES += drivers/stm32f7xx_hal_msp.o
+GLOBAL_LDFLAGS += -L --scatter=board/stm32f769i-discovery/stm32f769i-discovery.sct
+else ifeq ($(COMPILER), iar)
+$(NAME)_SOURCES += startup/startup_iar.s
+GLOBAL_LDFLAGS += --config board/stm32f769i-discovery/stm32f769i-discovery.icf
+else
+GLOBAL_CFLAGS += -DUSE_DIRECT_UART_PUTC
+$(NAME)_SOURCES += startup/startup_gcc.s
+GLOBAL_LDFLAGS += -T board/stm32f769i-discovery/stm32f769i-discovery.ld
+endif
+GLOBAL_INCLUDES += . \
+                   config/ \
+                   drivers/
 GLOBAL_DEFINES += STDIO_UART=0 CONFIG_NO_TCPIP
+GLOBAL_CFLAGS += -DSTM32F769xx
 
 CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_F769-nucleo
 CONFIG_SYSINFO_DEVICE_NAME   := f769-nucleo
