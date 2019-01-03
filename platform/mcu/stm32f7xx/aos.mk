7ee2c6bb069dc664b9c67f8838674dba19ae1e34
zhuqing <zhuqing.zq@alibaba-inc.com>
2019-01-03 11:50:52 +0800

    BugID:16944925:sort rhinorun example;dir platform
    Change-Id: Icaace23a1f098600c3e4cef432bbfb6d5277777a

--- platform/mcu/stm32f7xx/aos.mk
+++ platform/mcu/stm32f7xx/aos.mk
@@ -16,8 +16,6 @@ GLOBAL_INCLUDES += $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Inc        
                    $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Inc/Legacy   \
                    $(RELATIVE_PATH_MCU)/Drivers/CMSIS/Include                     \
                    $(RELATIVE_PATH_MCU)/Drivers/CMSIS/Device/ST/STM32F7xx/Include \
-                   $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun         \
-                   $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery                  \
                    $(RELATIVE_PATH_MCU)/hal
 
 $(NAME)_SOURCES := $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal.c          \
@@ -37,31 +35,13 @@ $(NAME)_SOURCES := $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7
                    $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_ll_tim.c       \
                    $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_ll_pwr.c       \
                    $(RELATIVE_PATH_MCU)/Drivers/STM32F7xx_HAL_Driver/Src/stm32f7xx_hal_cortex.c   \
-                   $(RELATIVE_PATH_MCU)/hal/cache.c                                               \
+                   $(RELATIVE_PATH_MCU)/hal/hal_cache.c                                               \
                    $(RELATIVE_PATH_MCU)/hal/hw.c                                                  \
                    $(RELATIVE_PATH_MCU)/hal/hal_uart_stm32f7.c
 
-$(NAME)_SOURCES += $(RELATIVE_PATH_MCU)/aos/soc_impl.c \
 
 GLOBAL_CFLAGS += -DSTM32F769xx
 
-ifeq ($(COMPILER), armcc)
-$(NAME)_SOURCES += $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/startup_stm32f769xx_keil.s
-
-else ifeq ($(COMPILER), iar)
-$(NAME)_SOURCES += $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/startup_stm32f769xx_iar.s
-
-else
-$(NAME)_SOURCES += $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/startup_stm32f769xx.s
-
-endif
-
-ifeq ($(HOST_MCU_NAME), STM32F769I-Discovery)
-$(NAME)_SOURCES += $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun/soc_init.c          \
-                   $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun/stm32f7xx_hal_msp.c \
-                   $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun/stm32f7xx_it.c      \
-                   $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun/system_stm32f7xx.c
-endif
 
 ifeq ($(COMPILER),armcc)
 GLOBAL_CFLAGS   += --c99 --cpu=7E-M -D__MICROLIB -g --apcs=interwork --split_sections
@@ -104,18 +84,3 @@ GLOBAL_LDFLAGS += -mcpu=cortex-m7           \
                   --specs=nosys.specs       \
                   $(CLIB_LDFLAGS_NANO_FLOAT)
 endif
-
-ifeq ($(COMPILER),armcc)
-GLOBAL_LDFLAGS += -L --scatter=platform/mcu/stm32f7xx/src/STM32F769I-Discovery/STM32F769I_DISCOVERY.sct
-else ifeq ($(COMPILER),iar)
-GLOBAL_LDFLAGS += --config platform/mcu/stm32f7xx/src/STM32F769I-Discovery/stm32f769xx.icf
-else
-GLOBAL_LDFLAGS += -T platform/mcu/stm32f7xx/src/STM32F769I-Discovery/STM32F769NIHx_FLASH.ld
-endif
-
-ifeq ($(COMPILER),armcc)
-$(NAME)_LINK_FILES := $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/startup_stm32f769xx_keil.o
-$(NAME)_LINK_FILES += $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun/stm32f7xx_it.o
-$(NAME)_LINK_FILES += $(RELATIVE_PATH_MCU)/src/STM32F769I-Discovery/rhinorun/stm32f7xx_hal_msp.o
-endif
-
