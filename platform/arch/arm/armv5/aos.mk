4bc948319e26d9bc80458a70ac14ba1d401a7f6e
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-01-03 14:14:31 +0800

    BugID:18268588:Add k_compiler.h for all arch
    Change-Id: I2c1c793390d5b07b63b277835cdef79547a269a4

--- platform/arch/arm/armv5/aos.mk
+++ platform/arch/arm/armv5/aos.mk
@@ -4,12 +4,12 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.0
 $(NAME)_SUMMARY    := arch for armv5
 
-GLOBAL_INCLUDES += ./gcc
+GLOBAL_INCLUDES += common
 
 $(NAME)_CFLAGS := -marm -mthumb-interwork
 
 $(NAME)_SOURCES += gcc/port_c.c    \
                    gcc/port_s.S    \
-                   panic/panic_c.c \
-                   panic/panic_gcc.S
+                   common/panic_c.c \
+                   common/panic_gcc.S
 
--- platform/arch/linux/port.h
+++ platform/arch/linux/port.h
@@ -2,40 +2,39 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-#ifndef K_CPU_H
-#define K_CPU_H
+#ifndef PORT_H
+#define PORT_H
 
 #include <signal.h>
 
-#define cpu_bitmap_clz(val)    __builtin_clz((val))
-
-sigset_t cpu_intrpt_save(void);
-void   cpu_intrpt_restore(sigset_t cpsr);
+cpu_cpsr_t cpu_intrpt_save(void);
+void   cpu_intrpt_restore(cpu_cpsr_t cpsr);
 void   cpu_intrpt_switch(void);
 void   cpu_first_task_start(void);
 void  *cpu_task_stack_init(cpu_stack_t *base, size_t size, void *arg, task_entry_t entry);
 void   cpu_task_switch(void);
 
-#define CPSR_ALLOC()    sigset_t cpsr
-
 #if (RHINO_CONFIG_CPU_NUM > 1)
-#define RHINO_CPU_INTRPT_DISABLE() { cpu_intrpt_save(); krhino_spin_lock(&g_sys_lock);}
-#define RHINO_CPU_INTRPT_ENABLE()  { krhino_spin_unlock(&g_sys_lock);cpu_intrpt_restore(cpsr);}
-
+#define CPSR_ALLOC()                cpu_cpsr_t cpsr
+#define RHINO_CPU_INTRPT_DISABLE()  \
+        do{cpsr = cpu_intrpt_save(); krhino_spin_lock(&g_sys_lock);}while(0)
+#define RHINO_CPU_INTRPT_ENABLE()   \
+        do{krhino_spin_unlock(&g_sys_lock);cpu_intrpt_restore(cpsr);}while(0)
 
 static inline void osPortCompareSet(volatile uint32_t *addr, uint32_t compare, uint32_t *set)
 {
 	return ;
 }
 
-
 void cpu_signal(uint8_t cpu_num);
 uint8_t cpu_cur_get(void);
 void cpu_spin_lock(kspinlock_t *lock);
 void cpu_spin_unlock(kspinlock_t *lock);
+
 #else
-#define RHINO_CPU_INTRPT_DISABLE() { cpu_intrpt_save();}
-#define RHINO_CPU_INTRPT_ENABLE()  { cpu_intrpt_restore(cpsr);}
+#define CPSR_ALLOC()                cpu_cpsr_t cpsr
+#define RHINO_CPU_INTRPT_DISABLE()  do{cpsr = cpu_intrpt_save();}while(0)
+#define RHINO_CPU_INTRPT_ENABLE()   do{cpu_intrpt_restore(cpsr);}while(0)
 
 RHINO_INLINE uint8_t cpu_cur_get(void)
 {
@@ -49,5 +48,5 @@ void   cpu_start_hook(void);
 void   cpu_task_create_hook(ktask_t *tcb);
 void   cpu_task_del_hook(ktask_t *tcb, res_free_t *arg);
 
-#endif /* K_CPU_H */
+#endif /* PORT_H */
 
