9cdfb5e893ca6e530e759f50d409fc616b54f23c
yx170385 <yx170385@alibaba-inc.com>
2019-01-03 17:32:44 +0800

    BugID:17967016:add user cli feature
    Change-Id: I81f63d6ffdec0b9180ef5a6ed11ce62061ffeaf3

---
+++ kernel/uspace/u_cli_task.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#include <k_api.h>
+#include <u_task.h>
+#include <mm.h>
+#include <cli_api.h>
+#include <errno.h>
+
+#define UCLI_TASK_USTACK 512u
+#define UCLI_TASK_KSTACK 512u
+
+ktask_t *ucli_task_obj;
+
+void ucli_task(void *arg)
+{
+    void *res;
+    void *msg;
+
+    krhino_uprocess_res_get(2, &res);
+
+    while (1) {
+        krhino_queue_recv(res, RHINO_WAIT_FOREVER, &msg);
+        if (msg != 0) {
+            printf("user cli cmd : %s\n", ((struct cli_command_st *)msg)->name);
+        }
+    }
+}
+
+kstat_t ucli_task_start(void)
+{
+    kstat_t ret;
+
+    cpu_stack_t *ustack_buf;
+
+    ustack_buf = malloc(UCLI_TASK_USTACK * sizeof(cpu_stack_t));
+    if (NULL == ustack_buf) {
+        return -ENOMEM;
+    }
+
+    ret = krhino_utask_create(&ucli_task_obj, "ucli_task", 0, 25 ,50 , ustack_buf,
+                               UCLI_TASK_USTACK, UCLI_TASK_KSTACK, ucli_task, 1);
+
+    if (ret != RHINO_SUCCESS) {
+        krhino_utask_del(ucli_task_obj);
+        return ret;
+    }
+
+    return ret;
+}
+
