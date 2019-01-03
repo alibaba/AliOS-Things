1063e8c2a37d4580e84b0113341b70780f1c9dc1
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-01-03 14:23:56 +0800

    BugID:18268588:Add k_compiler.h for all arch
    Change-Id: I5941b51fd47e1de3a50355b76a66e5e6bb2af5f9

--- kernel/rhino/include/k_mm_debug.h
+++ kernel/rhino/include/k_mm_debug.h
@@ -13,23 +13,13 @@
 extern "C" {
 #endif
 
-#if defined(__CC_ARM)
-#define RHINO_RETURN_ADDRESS()      __return_address()
-#elif defined(__ICCARM__)
-#define RHINO_RETURN_ADDRESS()      NULL
-#elif defined(__GNUC__)
-#define RHINO_RETURN_ADDRESS()      __builtin_return_address(0)
-#else
-#define RHINO_RETURN_ADDRESS()      NULL
-#endif
-
 #define AOS_UNSIGNED_INT_MSB        (1u << (sizeof(unsigned int) * 8 - 1))
 
 void krhino_owner_attach(void *addr, size_t allocator);
 
 /* set owner to return address */
 #define krhino_owner_return_addr(addr) \
-    krhino_owner_attach(addr, (size_t)RHINO_RETURN_ADDRESS())
+    krhino_owner_attach(addr, (size_t)RHINO_GET_RA())
 
 uint32_t dumpsys_mm_info_func(uint32_t len);
 
