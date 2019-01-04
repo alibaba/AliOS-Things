a32dd1455ce9fefc034c1217d99fa9273948a476
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-04 12:28:53 +0000

    BugID:17865306: update path to name for depentent component in aos.mk
    Change-Id: I10e1143beb2e9ffc2fd53fde49a894425f3778c4

--- test/testcase/kernel/fs/fatfs/aos.mk
+++ test/testcase/kernel/fs/fatfs/aos.mk
@@ -3,7 +3,7 @@ NAME := fatfs_test
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := testcase for fatfs
-$(NAME)_COMPONENTS += 3rdparty.experimental.fs.fatfs
+$(NAME)_COMPONENTS += fatfs
 
 $(NAME)_SOURCES += fatfs_test.c
 
