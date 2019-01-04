8ec5ce8eb91c8d05c890a89703fcae0854bbac48
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-04 12:28:53 +0000

    BugID:17865306: update path to name for depentent component in aos.mk
    Change-Id: I10e1143beb2e9ffc2fd53fde49a894425f3778c4

--- test/testcase/kernel/vfs/aos.mk
+++ test/testcase/kernel/vfs/aos.mk
@@ -3,7 +3,7 @@ NAME := vfs_test
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := testcase for vfs
-$(NAME)_COMPONENTS += kernel.fs.vfs
+$(NAME)_COMPONENTS += vfs
 
 $(NAME)_SOURCES += vfs_test.c
 
