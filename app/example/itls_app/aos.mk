efd4e381c07b67b8940a20c22932eb245021af69
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-04 12:28:52 +0000

    BugID:17865306: update path to name for depentent component in aos.mk
    Change-Id: I3e181421aaf05e04bfbbf65d05d30de7345c0199

--- app/example/itls_app/aos.mk
+++ app/example/itls_app/aos.mk
@@ -9,7 +9,7 @@ $(NAME)_SOURCES     := itls_client.c
 $(NAME)_COMPONENTS  := itls id2 alicrypto netmgr cli
 
 ifeq ($(LWIP),1)
-$(NAME)_COMPONENTS  += network.lwip
+$(NAME)_COMPONENTS  += lwip
 no_with_lwip := 0
 endif
 
