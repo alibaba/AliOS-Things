e112f420e2f2c019b62b9e1b3e5bd96f2646e70f
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-04 12:28:53 +0000

    BugID:17865306: update path to name for depentent component in aos.mk
    Change-Id: I700ae9fcf676f180cd7148f31dc4a934a9c61557
    Reviewed-on: http://yuncode.alibaba-inc.com/185717
    Tested-by: 李诚 lc122798 <lc122798@alibaba-inc.com>
    Reviewed-by: 庞军平 junping.pjp <junping.pjp@alibaba-inc.com>

--- security/irot/km/aos.mk
+++ security/irot/km/aos.mk
@@ -4,6 +4,6 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 2.0.1
 $(NAME)_SUMMARY := key management for mcu
 
-$(NAME)_COMPONENTS := irot.km.platform alicrypto
+$(NAME)_COMPONENTS := libplat_gen alicrypto
 
 $(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/libkm.a
