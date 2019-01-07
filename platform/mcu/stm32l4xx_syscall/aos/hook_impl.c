30a947b25b3d1e96c9ee2bf8e17a3a1f982f3eb7
yx170385 <yx170385@alibaba-inc.com>
2019-01-07 16:10:24 +0800

    BugID:17967016:move cli_q to user hook
    Change-Id: If94b9f6d8697f4d3a0e86a40bd5c6ff2094c1997

--- platform/mcu/stm32l4xx_syscall/aos/hook_impl.c
+++ platform/mcu/stm32l4xx_syscall/aos/hook_impl.c
@@ -4,6 +4,8 @@
 
 #include <k_api.h>
 
+#define PROC_MSG_NUM 100
+
 #if (RHINO_CONFIG_USER_HOOK > 0)
 void krhino_idle_hook(void)
 {
@@ -25,7 +27,26 @@ void krhino_start_hook(void)
 
 void krhino_task_create_hook(ktask_t *task)
 {
-    (void)task;
+    CPSR_ALLOC();
+
+    kqueue_t *queue;
+    kstat_t   ret;
+
+    if (task == NULL)
+        return;
+
+    if (task->is_proc == 1) {
+        ret = krhino_queue_dyn_create(&queue, "cli_queue", PROC_MSG_NUM);
+        if (ret != RHINO_SUCCESS) {
+            return;
+        }
+
+        task->cli_q = queue;
+
+        RHINO_CRITICAL_ENTER();
+        klist_insert(&(task->kobj_list.queue_head), &queue->blk_obj.obj_list);
+        RHINO_CRITICAL_EXIT();
+    }
 }
 
 void krhino_task_del_hook(ktask_t *task, res_free_t *arg)
