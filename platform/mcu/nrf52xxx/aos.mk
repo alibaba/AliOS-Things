f0376054fc1554665f053cc3cfa4ee9bb0027e0a
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-01-07 21:13:56 +0800

    BugID:18557727:open fpu hardware for pca board
    Change-Id: I12b8904411cbbeb5cf4ca3dcf216582d2c6fcbca

--- platform/mcu/nrf52xxx/aos.mk
+++ platform/mcu/nrf52xxx/aos.mk
@@ -81,44 +81,52 @@ $(NAME)_SOURCES  += hal/ble_port.c
 endif
 
 ifeq ($(COMPILER),armcc)
-GLOBAL_CFLAGS   += --c99 --cpu=7E-M -D__MICROLIB -g --apcs=interwork --split_sections
+GLOBAL_CFLAGS += --c99 --cpu=Cortex-M4 --apcs=/hardfp --fpu=vfpv4_sp_d16 -D__MICROLIB -g --split_sections
 else ifeq ($(COMPILER),iar)
-GLOBAL_CFLAGS += --cpu=Cortex-M4 \
+GLOBAL_CFLAGS += --cpu=Cortex-M4  \
                  --cpu_mode=thumb \
                  --endian=little
 else
-GLOBAL_CFLAGS += -mcpu=cortex-m4 \
-                 -mlittle-endian \
+GLOBAL_CFLAGS += -mcpu=cortex-m4           \
+                 -mlittle-endian           \
                  -mthumb -mthumb-interwork \
+                 -mfloat-abi=hard          \
+                 -mfpu=fpv4-sp-d16         \
                  -w
 endif
 
 ifeq ($(COMPILER),armcc)
-GLOBAL_ASMFLAGS += --cpu=7E-M -g --apcs=interwork --library_type=microlib --pd "__MICROLIB SETA 1"
+GLOBAL_ASMFLAGS += --cpu=Cortex-M4 -g --apcs=/hardfp --fpu=vfpv4_sp_d16 --library_type=microlib --pd "__MICROLIB SETA 1"
 else ifeq ($(COMPILER),iar)
-GLOBAL_ASMFLAGS += --cpu Cortex-M4 \
+GLOBAL_ASMFLAGS += --cpu Cortex-M4  \
                    --cpu_mode thumb \
                    --endian little
 else
-GLOBAL_ASMFLAGS += -mcpu=cortex-m4 \
-                   -mlittle-endian \
-                   -mthumb -mthumb-interwork \
+GLOBAL_ASMFLAGS += -mcpu=cortex-m4   \
+                   -mlittle-endian   \
+                   -mthumb           \
+                   -mfloat-abi=hard  \
+                   -mfpu=fpv4-sp-d16 \
                    -w
 endif
 
 ifeq ($(COMPILER),armcc)
-GLOBAL_LDFLAGS += -L --cpu=7E-M   \
-                  -L --strict \
+GLOBAL_LDFLAGS += -L --cpu=Cortex-M4                    \
+                  -L --fpu=vfpv4_sp_d16                 \
+                  -L --apcs=/hardfp                     \
+                  -L --strict                           \
                   -L --xref -L --callgraph -L --symbols \
                   -L --info=sizes -L --info=totals -L --info=unused -L --info=veneers -L --info=summarysizes
 else ifeq ($(COMPILER),iar)
 GLOBAL_LDFLAGS += --silent --cpu=Cortex-M4.vfp
 
 else
-GLOBAL_LDFLAGS += -mcpu=cortex-m4  \
-                  -mlittle-endian  \
+GLOBAL_LDFLAGS += -mcpu=cortex-m4           \
+                  -mlittle-endian           \
                   -mthumb -mthumb-interwork \
-                  --specs=nosys.specs \
+                  --specs=nosys.specs       \
+                  -mfloat-abi=hard          \
+                  -mfpu=fpv4-sp-d16         \
                   $(CLIB_LDFLAGS_NANO_FLOAT)
 endif
 
@@ -126,3 +134,4 @@ include $($(NAME)_LOCATION)/$(HOST_MCU_NAME).mk
 
 include $($(NAME)_LOCATION)/bt_controller/bt_controller.mk
 #$(NAME)_COMPONENTS += bt_controller
+
