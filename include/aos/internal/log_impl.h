e9b9d86f55926f7902e943ab9a6c13138a573f0b
zhangtian <dongren.zt@alibaba-inc.com>
2019-01-09 14:06:55 +0800

    BUGID:18619817:[log]improve compatibility
    Change-Id: Ic05e76b919aa8192531316ca7ef2368e082b0afb

--- include/aos/internal/log_impl.h
+++ include/aos/internal/log_impl.h
@@ -72,7 +72,9 @@ extern int csp_printf(const char *fmt, ...);
 #define log_print(CON, MOD, COLOR, LVL, FMT, ...) \
     do { \
         if (CON) { \
-            csp_printf("[%06d]<" LVL "> "FMT"\r\n", (unsigned)aos_now_ms(), ##__VA_ARGS__); \
+            csp_printf("[%06d]<" LVL "> ", (unsigned)aos_now_ms()); \
+            csp_printf(FMT, ##__VA_ARGS__); \
+            csp_printf("\r\n"); \
         } \
     } while (0)
 #endif
