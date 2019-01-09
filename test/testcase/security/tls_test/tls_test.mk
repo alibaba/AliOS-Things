69f77f3b72cfe0bb99952c7ea612cba0e95c582a
Jinliang Li <ljl150821@alibaba-inc.com>
2019-01-09 11:45:10 +0800

    BugID:18621523: add mbedtls_test for utility/mbedtls
    - add mbedtls selftest for cipher and x509 module.
    - add mbedtls tls_client test for tls protocol.
    
    Change-Id: Iab10f9efd19ca4474a6b4e6e4a1f153caf23b538

--- test/testcase/security/tls_test/tls_test.mk
+++ test/testcase/security/tls_test/tls_test.mk
@@ -3,7 +3,7 @@ NAME := tls_test
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 0.0.1
 $(NAME)_SUMMARY :=
-$(NAME)_COMPONENTS  += mbedtls 
+$(NAME)_COMPONENTS  += security/mbedtls
 
 $(NAME)_SOURCES     += tls_test.c
 
