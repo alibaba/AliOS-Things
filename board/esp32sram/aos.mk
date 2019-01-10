f966148a41135b977d81b529c1f0ace8c1256245
zhuqing <zhuqing.zq@alibaba-inc.com>
2019-01-10 11:05:57 +0800

    BugID:18631099:add esp32sram for 2 core;esp32devkitc no sram;dir board2
    Change-Id: I276e7b1d0a0538305cb358caa2d5894f44120d88

--- board/esp32sram/aos.mk
+++ board/esp32sram/aos.mk
@@ -1,30 +1,37 @@
-NAME := board_esp32devkitc
+NAME := board_esp32sram
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.0
-$(NAME)_SUMMARY    := configuration for board esp32devkitc
-MODULE             := 1062
-HOST_ARCH          := xtensa
+$(NAME)_SUMMARY    := configuration for board esp32sram
+MODULE              := 1062
+HOST_ARCH           := xtensa
 HOST_MCU_FAMILY    := mcu_esp32
 SUPPORT_MBINS      := no
 
 # todo: remove these after rhino/lwip ready
-osal ?= rhino
+osal               ?= rhino
 
 $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) osal init
 
+define get-os-version
+"AOS-R"-$(CURRENT_TIME)
+endef
+
+CONFIG_SYSINFO_OS_VERSION := $(call get-os-version)
 CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_ESP32
-CONFIG_SYSINFO_DEVICE_NAME   := ESP32
+CONFIG_SYSINFO_DEVICE_NAME := ESP32
 
 GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
 GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"  -DCONFIG_AOS_FATFS_SUPPORT_RAW_FLASH
-#Support 1 Core
-GLOBAL_DEFINES += LOG_LOCAL_LEVEL=4 RHINO_CONFIG_CPU_NUM=1
+#Support 2 Core
+GLOBAL_DEFINES += LOG_LOCAL_LEVEL=4 RHINO_CONFIG_CPU_NUM=2
 
-GLOBAL_ASMFLAGS += -DRHINO_CONFIG_CPU_NUM=1  
+GLOBAL_ASMFLAGS += -DRHINO_CONFIG_CPU_NUM=2  
 
-#OS_MUTICORE_NUM ?= 2
-#Support 1 Core end
+OS_MUTICORE_NUM := 2
+
+#support sram
+esp32_sram ?= 1
 
 GLOBAL_INCLUDES += .
 $(NAME)_SOURCES := board.c
