c09a94418fba32d7efa2538defa127bf6ae6c190
Li Cheng <lc122798@alibaba-inc.com>
2019-01-10 18:10:07 +0000

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Ibc7a7905a73c87de4a7be7585eb4b56bccfeec68

--- board/esp32sram/ucube.py
+++ board/esp32sram/ucube.py
@@ -7,5 +7,5 @@ if aos_global_config.get('hci_h4', 0):
 
 
 platform_options="wifi=1"
-supported_targets="alinkapp bluetooth.bleadv bluetooth.bleperipheral helloworld networkapp acapp linkkitapp mqttapp coapapp linkkit_gateway linkkit_sched comboapp"
-linux_only_targets="hdlcapp.hdlcserver tls meshapp smpapp linuxapp blink wifihalapp uDataapp netmgrapp nano helloworld_nocli athostapp comboapp"
+supported_targets="bluetooth.bleadv bluetooth.bleperipheral coapapp helloworld linkkit_gateway linkkitapp mqttapp"
+linux_only_targets="athostapp blink http2app meshapp otaapp tinyengine_app tls udataapp yts"
