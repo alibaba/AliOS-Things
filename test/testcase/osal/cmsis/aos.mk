f0df4f82b06c81b942e0714262b82961c578da0d
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2019-01-10 19:26:17 +0800

    BugID:18651714: cmsis -> osal_cmsis posix -> osal_posix
    Change-Id: I8b999d75c62b5d73ef06f04fa47583762b1d4235

--- test/testcase/osal/cmsis/aos.mk
+++ test/testcase/osal/cmsis/aos.mk
@@ -3,7 +3,7 @@ NAME := cmsis_test
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := test code of cmsis
-$(NAME)_COMPONENTS += cmsis
+$(NAME)_COMPONENTS += osal_cmsis
 
 $(NAME)_SOURCES := cmsis_test_main.c     \
                    thread_test.c     \
