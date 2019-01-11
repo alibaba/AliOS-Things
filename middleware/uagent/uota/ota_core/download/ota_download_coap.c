fbf4541cf820324eea50a46e53517ec3cc7f32a8
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2019-01-11 17:01:43 +0800

    BugID:18621764:OTA clean code according the latest doc
    Change-Id: Ifac8712b5869cd7f0492e84d611476f4479cb888

--- middleware/uagent/uota/ota_core/download/ota_download_coap.c
+++ middleware/uagent/uota/ota_core/download/ota_download_coap.c
@@ -20,10 +20,10 @@
 
 #define DOWNLOAD_PATH "/topic/ota/device/download/%s/%s"
 
-static int block_cur_num       = 0;
-static int block_more          = 1;
-static int block_size          = OTA_COAP_BLOCK_SIZE;
-static int total_size          = 0;
+static int block_cur_num = 0;
+static int block_more = 1;
+static int block_size = OTA_COAP_BLOCK_SIZE;
+static int total_size = 0;
 static int coap_client_running = 0;
 static int retry_cnt = 5;
 
@@ -32,9 +32,9 @@ static void  iotx_response_block_handler(void *arg, void *p_response);
 
 static int ota_download_start(void *pctx)
 {
-    int    ret        = 0;
-    int    breakpoint = 0;
-    char   path[128] = {0};
+    int ret = 0;
+    int breakpoint = 0;
+    char path[128] = {0};
     ota_service_t* ctx = pctx;
     if (!ctx || !(ctx->h_ch)) {
         return -1;
@@ -57,11 +57,11 @@ static int ota_download_start(void *pctx)
     sem_send  = ota_semaphore_create();
     while (coap_client_running && block_more) {
         iotx_message_t message;
-        message.p_payload     = NULL;
-        message.payload_len   = 0;
+        message.p_payload = NULL;
+        message.payload_len = 0;
         message.resp_callback = iotx_response_block_handler;
-        message.msg_type      = COAP_MESSAGE_CON;
-        message.content_type  = COAP_CONTENT_TYPE_JSON;
+        message.msg_type = COAP_MESSAGE_CON;
+        message.content_type = COAP_CONTENT_TYPE_JSON;
         ota_coap_send_block(ctx->h_ch, path, &message, COAP_OPTION_BLOCK2, block_cur_num, block_more, block_size);
         ret = ota_semaphore_wait(&sem_send, 5000);
         if(ret < 0) {
@@ -81,12 +81,12 @@ static int ota_download_start(void *pctx)
 
 static void iotx_response_block_handler(void *arg, void *p_response)
 {
-    int    len = 0;
-    int    cur_num = 0;
-    int    more = 0;
-    int    size = 0;
-    int    breakpoint = 0;
-    char   *p_payload = NULL;
+    int len = 0;
+    int cur_num = 0;
+    int more = 0;
+    int size = 0;
+    int breakpoint = 0;
+    char *p_payload = NULL;
     iotx_coap_resp_code_t resp_code;
     ota_coap_get_code(p_response, &resp_code);
     if (resp_code == 0x45) {
@@ -103,7 +103,7 @@ static void iotx_response_block_handler(void *arg, void *p_response)
                 total_size += len;
                 // OTA_LOG_I("size:%d, n:%d", size, nbytes);
                 int ret = 0;
-                ret = ota_hal_write(&breakpoint, p_payload,len);
+                ret = ota_hal_write(&breakpoint, p_payload, len);
                 if (ret < 0) {
                       OTA_LOG_E("write err:%d\n", ret);
                       return;
