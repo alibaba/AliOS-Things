70695d93aab8f0f59746755f2fd315669e427e9d
Zilong Liu <zilong.liuzl@alibaba-inc.com>
2019-01-14 11:54:30 +0800

    BugID:18679366:  modify code style
    Change-Id: I371399154c48fd3f3d35c1b7ffff98827e889522

--- test/testcase/framework/udata_test/aos.mk
+++ test/testcase/framework/udata_test/aos.mk
@@ -1,11 +1,11 @@
-NAME := uData_test
+NAME := udata_test
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY :=
 $(NAME)_COMPONENTS  += udata
 
-$(NAME)_SOURCES     += uData_test.c
+$(NAME)_SOURCES     += udata_test.c
 
 $(NAME)_CFLAGS      += -Wall -Werror
 
