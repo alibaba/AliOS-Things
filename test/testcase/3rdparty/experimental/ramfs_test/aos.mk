8ef2edcef9f46d3159a32a946d5cba18b9c55f24
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-15 12:30:38 +0000

    BugID:17865306: update path to name for depentent component in aos.mk
    Change-Id: Ie82d142a2297a215d6c83486fb2215592e426138
    
    BugID:17865306: update path to name for depentent component in aos.mk
    
    Change-Id: Ia702b2d8aab36e4f674c97cf678d1d409b1e55b6
    
    BugID:17865306: rename component makefile into aos.mk
    
    Change-Id: Ia516f4ce44f77b1a85d68e4e6430a112d34dd177

--- test/testcase/3rdparty/experimental/ramfs_test/aos.mk
+++ test/testcase/3rdparty/experimental/ramfs_test/aos.mk
@@ -1,10 +1,10 @@
 NAME := ramfs_test
 
 $(NAME)_MBINS_TYPE := app
-$(NAME)_VERSION    := 0.0.1
+$(NAME)_VERSION    := 1.0.0
 $(NAME)_SUMMARY    := testcase for ramfs
 
-$(NAME)_COMPONENTS += kernel.fs.ramfs
+$(NAME)_COMPONENTS += ramfs
 
 $(NAME)_SOURCES += ramfs_test.c
 
