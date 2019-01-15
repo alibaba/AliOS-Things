cf2598bd494f7cf9cb44c0aa57701779f0582cbc
magao.xwj <magao.xwj@alibaba-inc.com>
2019-01-15 16:58:52 +0800

    BugID:18471767:solve win cmd build mkdir.exe run error
    Change-Id: I0d6993551f08d6a3a98951fab748f513afe39ae9

---
+++ build/cmd/win32/mkdir_p.py
@@ -0,0 +1,14 @@
+import os, errno, sys
+
+def mkdir_p(path):
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5 (except OSError, exc: for Python <2.5)
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
+        else: raise
+
+if len(sys.argv) != 2:
+    print "usage:python "+sys.argv[0]+" dirname"
+else:
+    mkdir_p(sys.argv[1])
