2fa6ba0fee9a73c9b69c13f42684973ab03f8476
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-01-18 15:22:57 +0800

    bugID:18002625:[breeze] 1. Disable secure adv 2. Add MODEL encry controle.
    Change-Id: I274d5d7ebb1681c9dbac0d42bc739c3f63c801d4

--- app/example/bluetooth/breezeapp/aos.mk
+++ app/example/bluetooth/breezeapp/aos.mk
@@ -13,7 +13,6 @@ $(NAME)_COMPONENTS := breeze cli
 
 GLOBAL_DEFINES += DEBUG
 GLOBAL_DEFINES += CONFIG_BLE_LINK_PARAMETERS
-
 GLOBAL_DEFINES += BUILD_AOS
 
 BREEZEAPP_CONFIG_EN_OTA ?= 0
@@ -26,13 +25,11 @@ GLOBAL_DEFINES += CONFIG_AIS_OTA
 $(NAME)_COMPONENTS += ota_ble
 endif
 
-ifeq ($(continue_ble_adv),1)
+bz_secure_adv ?= 0
+ifeq ($(bz_secure_adv),1)
 GLOBAL_DEFINES += CONTINUE_BEL_ADV
 endif
 
-ifeq ($(append_user_adv),1)
-GLOBAL_DEFINES += APPEND_USER_ADV
-endif
-
+bz_model_encry ?= 0
 
 GLOBAL_INCLUDES += ../
