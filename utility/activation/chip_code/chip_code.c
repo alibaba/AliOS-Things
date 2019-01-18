9f6dba24b2925f63df7bdceab1f690bc07e196d2
magao.xwj <magao.xwj@alibaba-inc.com>
2019-01-18 09:23:41 +0000

    BugID:18747037:[utility]remove activation.a for save ram and fix linuxhost mcu type name
    Change-Id: Id6f0c6c568041c0534af7f74a8fe0d5f3508223c

--- utility/activation/chip_code/chip_code.c
+++ utility/activation/chip_code/chip_code.c
@@ -13,7 +13,7 @@ chip_code_st g_chip_codes[] = {
     {ESP_CHIP_VENDOR, esp32, "mcu_esp32"}, //0x00050008
     {ESP_CHIP_VENDOR, esp8266, "mcu_esp8266"}, //0x00050009
     {GD_CHIP_VENDOR, gd32f4xx, "mcu_gd32f4xx"}, //0x0006000a
-    {LINUX_VENDOR, linuxhost, "mcu_linux"}, //0x0007000b
+    {LINUX_VENDOR, linuxhost, "mcu_linuximpl"}, //0x0007000b
     {NXP_VENDOR, lpc54102, "mcu_lpc54102impl"}, //0x0008000c
     {NXP_VENDOR, mkl27z644, "mcu_mkl27z644impl"}, //0x0008000d
     {MICO_CHIP_VENDOR, moc108, "mcu_moc108"}, //0x0009000e
