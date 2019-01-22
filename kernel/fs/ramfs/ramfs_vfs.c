26fcc2b3a777140336a8e7959b47429acd418db7
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-01-22 16:14:05 +0800

    BugID:17918368: Optimize the vfs interface usage for RAMFS
    Change-Id: I13569fc26c2c0604e96a76a47b18fd31a34f54ce

--- kernel/fs/ramfs/ramfs_vfs.c
+++ kernel/fs/ramfs/ramfs_vfs.c
@@ -2,32 +2,28 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-#include <stdint.h>
-#include <stdio.h>
-#include <string.h>
 #include <fcntl.h>
+#include <string.h>
 
-#include "aos/errno.h"
+#include "vfs_api.h"
 
-#include "aos/vfs.h"
-
-#include "ramfs_types.h"
 #include "ramfs_api.h"
 #include "ramfs_adapt.h"
+#include "ramfs_types.h"
 
 #define MAX_RAMFS_FILE_NAME_BYTES 32
 
 #define RAMFS_DEFAULT_MOUNT_PATH "/RAMFS"
 
 typedef struct {
-    aos_dir_t    aos_dir;
+    vfs_dir_t    vfs_dir;
     ramfs_dir_t  ramfs_dir;
-    aos_dirent_t aos_dirent;
-} vfs_ramfs_dir_t;
+    vfs_dirent_t vfs_dirent;
+} ramfs_vfs_dir_t;
 
-static int32_t vfs_ramfs_open(file_t *fp, const char *path, int32_t flags)
+static int32_t ramfs_vfs_open(vfs_file_t *fp, const char *path, int32_t flags)
 {
-    int32_t ret         = -EPERM;
+    int32_t ret         = RAMFS_ERR_NOT_IMP;
     int32_t ramfs_flags = 0;
 
     ramfs_file_t ramfs_file;
@@ -53,11 +49,11 @@ static int32_t vfs_ramfs_open(file_t *fp, const char *path, int32_t flags)
     return ret;
 }
 
-static int32_t vfs_ramfs_close(file_t *fp)
+static int32_t ramfs_vfs_close(vfs_file_t *fp)
 {
     ramfs_file_t ramfs_file;
 
-    int32_t ret = -EPERM;
+    int32_t ret = RAMFS_ERR_NOT_IMP;
 
     memset(&ramfs_file, 0, sizeof(ramfs_file));
 
@@ -68,11 +64,11 @@ static int32_t vfs_ramfs_close(file_t *fp)
     return ret;
 }
 
-static int32_t vfs_ramfs_read(file_t *fp, char *buf, uint32_t len)
+static int32_t ramfs_vfs_read(vfs_file_t *fp, char *buf, uint32_t len)
 {
     ramfs_file_t ramfs_file;
 
-    int32_t ret = -EPERM;
+    int32_t ret = RAMFS_ERR_NOT_IMP;
     int32_t res = -1;
 
     uint32_t read_bytes = 0;
@@ -94,11 +90,11 @@ static int32_t vfs_ramfs_read(file_t *fp, char *buf, uint32_t len)
     return res;
 }
 
-static int32_t vfs_ramfs_write(file_t *fp, const char *buf, uint32_t len)
+static int32_t ramfs_vfs_write(vfs_file_t *fp, const char *buf, uint32_t len)
 {
     ramfs_file_t ramfs_file;
 
-    int32_t ret = -EPERM;
+    int32_t ret = RAMFS_ERR_NOT_IMP;
     int32_t res = -1;
 
     uint32_t write_bytes = 0;
@@ -120,9 +116,9 @@ static int32_t vfs_ramfs_write(file_t *fp, const char *buf, uint32_t len)
     return res;
 }
 
-static int32_t vfs_ramfs_access(file_t *fp, const char *path, int32_t amode)
+static int32_t ramfs_vfs_access(vfs_file_t *fp, const char *path, int32_t amode)
 {
-    int32_t ret = -EPERM;
+    int32_t ret = RAMFS_ERR_NOT_IMP;
 
     if ((amode == R_OK) || (amode == W_OK) || (amode == X_OK)) {
         return 0;
@@ -137,7 +133,7 @@ static int32_t vfs_ramfs_access(file_t *fp, const char *path, int32_t amode)
     return ret;
 }
 
-static off_t vfs_ramfs_lseek(file_t *fp, off_t off, int32_t whence)
+static uint32_t ramfs_vfs_lseek(vfs_file_t *fp, uint32_t off, int32_t whence)
 {
     ramfs_file_t ramfs_file;
 
@@ -164,33 +160,33 @@ static off_t vfs_ramfs_lseek(file_t *fp, off_t off, int32_t whence)
     return fp->offset;
 }
 
-static int32_t vfs_ramfs_link(file_t *fp, const char *path1, const char *path2)
+static int32_t ramfs_vfs_link(vfs_file_t *fp, const char *path1, const char *path2)
 {
     return ramfs_link(path1, path2);
 }
 
-static int32_t vfs_ramfs_unlink(file_t *fp, const char *path)
+static int32_t ramfs_vfs_unlink(vfs_file_t *fp, const char *path)
 {
     return ramfs_unlink(path);
 }
 
-static int32_t vfs_ramfs_remove(file_t *fp, const char *path)
+static int32_t ramfs_vfs_remove(vfs_file_t *fp, const char *path)
 {
     return ramfs_remove(path);
 }
 
-static int32_t vfs_ramfs_rename(file_t *fp, const char *old, const char *new)
+static int32_t ramfs_vfs_rename(vfs_file_t *fp, const char *old, const char *new)
 {
     return ramfs_rename(old, new);
 }
 
-static aos_dir_t *vfs_ramfs_opendir(file_t *fp, const char *path)
+static vfs_dir_t *ramfs_vfs_opendir(vfs_file_t *fp, const char *path)
 {
-    vfs_ramfs_dir_t *dp  = NULL;
-    aos_dir_t       *ret = NULL;
+    ramfs_vfs_dir_t *dp  = NULL;
+    vfs_dir_t       *ret = NULL;
     int32_t          res = -1;
 
-    dp = ramfs_mm_alloc(sizeof(vfs_ramfs_dir_t) + MAX_RAMFS_FILE_NAME_BYTES);
+    dp = ramfs_mm_alloc(sizeof(ramfs_vfs_dir_t) + MAX_RAMFS_FILE_NAME_BYTES);
 
     if (dp != NULL) {
         fp->f_arg = dp;
@@ -198,14 +194,14 @@ static aos_dir_t *vfs_ramfs_opendir(file_t *fp, const char *path)
         res = ramfs_opendir(&dp->ramfs_dir, path);
 
         if (res == 0) {
-            ret = &(dp->aos_dir);
+            ret = &(dp->vfs_dir);
         }
     }
 
     return ret;
 }
 
-static int32_t vfs_ramfs_closedir(file_t *fp, aos_dir_t *dir)
+static int32_t ramfs_vfs_closedir(vfs_file_t *fp, vfs_dir_t *dir)
 {
     if (fp->f_arg != NULL) {
         ramfs_mm_free(fp->f_arg);
@@ -214,7 +210,7 @@ static int32_t vfs_ramfs_closedir(file_t *fp, aos_dir_t *dir)
     return 0;
 }
 
-static int32_t vfs_ramfs_stat(file_t *fp, const char *path, struct aos_stat *st)
+static int32_t ramfs_vfs_stat(vfs_file_t *fp, const char *path, vfs_stat_t *st)
 {
     ramfs_stat_t ramfs_st;
 
@@ -253,7 +249,7 @@ static int32_t vfs_ramfs_stat(file_t *fp, const char *path, struct aos_stat *st)
     return ret;
 }
 
-static int32_t vfs_ramfs_fstat(file_t *fp, struct aos_stat *st)
+static int32_t ramfs_vfs_fstat(vfs_file_t *fp, vfs_stat_t *st)
 {
     ramfs_stat_t ramfs_st;
     ramfs_file_t ramfs_file;
@@ -296,7 +292,7 @@ static int32_t vfs_ramfs_fstat(file_t *fp, struct aos_stat *st)
     return ret;
 }
 
-static int32_t vfs_ramfs_statfs(file_t *fp, const char *path, struct aos_statfs *buf)
+static int32_t ramfs_vfs_statfs(vfs_file_t *fp, const char *path, vfs_statfs_t *buf)
 {
     if (buf == NULL) {
         return -1;
@@ -315,87 +311,89 @@ static int32_t vfs_ramfs_statfs(file_t *fp, const char *path, struct aos_statfs 
     return 0;
 }
 
-static int32_t vfs_ramfs_mkdir(file_t *fp, const char *path)
+static int32_t ramfs_vfs_mkdir(vfs_file_t *fp, const char *path)
 {
     return ramfs_mkdir(path);
 }
 
-static aos_dirent_t *vfs_ramfs_readdir(file_t *fp, aos_dir_t *dir)
+static vfs_dirent_t *ramfs_vfs_readdir(vfs_file_t *fp, vfs_dir_t *dir)
 {
-    vfs_ramfs_dir_t *dp  = NULL;
-    aos_dirent_t    *ret = NULL;
+    ramfs_vfs_dir_t *dp  = NULL;
+    vfs_dirent_t    *ret = NULL;
     int32_t          res = -1;
 
     dp = fp->f_arg;
 
-    if (dp->aos_dirent.d_name != NULL) {
-        res = ramfs_readdir(&(dp->ramfs_dir), dp->aos_dirent.d_name);
+    if (dp->vfs_dirent.d_name != NULL) {
+        res = ramfs_readdir(&(dp->ramfs_dir), dp->vfs_dirent.d_name);
 
         if (res == 0) {
-            ret = &dp->aos_dirent;
+            ret = &dp->vfs_dirent;
         }
     }
 
     return ret;
 }
 
-static int32_t vfs_ramfs_pathconf(file_t *fp, const char *path, int32_t name)
+static int32_t ramfs_vfs_pathconf(vfs_file_t *fp, const char *path, int32_t name)
 {
     return ramfs_pathconf(name);
 }
 
-static int32_t vfs_ramfs_fpathconf(file_t *fp, int32_t name)
+static int32_t ramfs_vfs_fpathconf(vfs_file_t *fp, int32_t name)
 {
     return ramfs_pathconf(name);
 }
 
-static int32_t vfs_ramfs_utime(file_t *fp, const char *path, const struct aos_utimbuf *times)
+static int32_t ramfs_vfs_utime(vfs_file_t *fp, const char *path, const vfs_utimbuf_t *times)
 {
-    return -1;
+    return RAMFS_ERR_NOT_IMP;
 }
 
-static void vfs_ramfs_rewinddir(file_t *fp, aos_dir_t *dir)
+static void ramfs_vfs_rewinddir(vfs_file_t *fp, vfs_dir_t *dir)
 {
-    vfs_ramfs_dir_t *dp  = NULL;
+    ramfs_vfs_dir_t *dp  = NULL;
 
     dp = fp->f_arg;
     dp->ramfs_dir.last_entry = NULL;
 }
 
-static int vfs_ramfs_rmdir(file_t *fp, const char *path)
+static int ramfs_vfs_rmdir(vfs_file_t *fp, const char *path)
 {
     return ramfs_rmdir(path);
 }
 
-fs_ops_t fs_ops_ramfs = { .open      = &vfs_ramfs_open,
-                          .close     = &vfs_ramfs_close,
-                          .read      = &vfs_ramfs_read,
-                          .write     = &vfs_ramfs_write,
-                          .access    = &vfs_ramfs_access,
-                          .lseek     = &vfs_ramfs_lseek,
-                          .sync      = NULL,
-                          .stat      = &vfs_ramfs_stat,
-                          .fstat     = &vfs_ramfs_fstat,
-                          .statfs    = &vfs_ramfs_statfs,
-                          .link      = &vfs_ramfs_link,
-                          .unlink    = &vfs_ramfs_unlink,
-                          .remove    = &vfs_ramfs_remove,
-                          .rename    = &vfs_ramfs_rename,
-                          .opendir   = &vfs_ramfs_opendir,
-                          .readdir   = &vfs_ramfs_readdir,
-                          .closedir  = &vfs_ramfs_closedir,
-                          .mkdir     = &vfs_ramfs_mkdir,
-                          .seekdir   = NULL,
-                          .ioctl     = NULL,
-                          .pathconf  = &vfs_ramfs_pathconf,
-                          .fpathconf = &vfs_ramfs_fpathconf,
-                          .utime     = &vfs_ramfs_utime,
-                          .rewinddir = &vfs_ramfs_rewinddir,
-                          .rmdir     = vfs_ramfs_rmdir };
+vfs_filesystem_ops_t ramfs_ops = { 
+    .open      = &ramfs_vfs_open,
+    .close     = &ramfs_vfs_close,
+    .read      = &ramfs_vfs_read,
+    .write     = &ramfs_vfs_write,
+    .access    = &ramfs_vfs_access,
+    .lseek     = &ramfs_vfs_lseek,
+    .sync      = NULL,
+    .stat      = &ramfs_vfs_stat,
+    .fstat     = &ramfs_vfs_fstat,
+    .statfs    = &ramfs_vfs_statfs,
+    .link      = &ramfs_vfs_link,
+    .unlink    = &ramfs_vfs_unlink,
+    .remove    = &ramfs_vfs_remove,
+    .rename    = &ramfs_vfs_rename,
+    .opendir   = &ramfs_vfs_opendir,
+    .readdir   = &ramfs_vfs_readdir,
+    .closedir  = &ramfs_vfs_closedir,
+    .mkdir     = &ramfs_vfs_mkdir,
+    .seekdir   = NULL,
+    .ioctl     = NULL,
+    .pathconf  = &ramfs_vfs_pathconf,
+    .fpathconf = &ramfs_vfs_fpathconf,
+    .utime     = &ramfs_vfs_utime,
+    .rewinddir = &ramfs_vfs_rewinddir,
+    .rmdir     = &ramfs_vfs_rmdir 
+};
 
 int32_t ramfs_register(const char *mount_path)
 {
-    int32_t ret = -1;
+    int32_t ret = RAMFS_ERR_NOT_IMP;
 
     ramfs_init();
 
@@ -403,16 +401,15 @@ int32_t ramfs_register(const char *mount_path)
         ret = ramfs_mkdir(mount_path);
 
         if (ret == 0) {
-            return aos_register_fs(mount_path, &fs_ops_ramfs, NULL);
+            return vfs_register_fs(mount_path, &ramfs_ops, NULL);
         }
     } else {
         ret = ramfs_mkdir(RAMFS_DEFAULT_MOUNT_PATH);
 
         if (ret == 0) {
-            return aos_register_fs(RAMFS_DEFAULT_MOUNT_PATH, &fs_ops_ramfs, NULL);
+            return vfs_register_fs(RAMFS_DEFAULT_MOUNT_PATH, &ramfs_ops, NULL);
         }
     }
 
     return ret;
 }
-
