0075b9e85b10eab7110d6fde683ff895d2f40885
yx170385 <yx170385@alibaba-inc.com>
2019-01-22 17:48:11 +0800

    BugID:18712794:RHINO_CONFIG_TASK_STACK_CUR_CHECK sort
    Change-Id: I7a604866efec34d55be416a431f6ff1c5bee6290

--- kernel/rhino/k_err.c
+++ kernel/rhino/k_err.c
@@ -2,13 +2,14 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-#include <k_api.h>
+#include "k_api.h"
+#include "debug_api.h"
 #include <stdio.h>
 
 void k_err_proc_debug(kstat_t err, char *file, int line)
 {
 #if (DEBUG_CONFIG_ERRDUMP > 0)
-    debug_fatal_error(kstat_t err, char *file, int line);
+    debug_fatal_error(err, file, line);
 #endif
 
     if (g_err_proc != NULL) {
