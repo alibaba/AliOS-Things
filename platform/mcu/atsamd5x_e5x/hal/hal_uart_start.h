fdbab1329f294761a9f0771d6bf72e817f74b68a
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-01-23 15:18:14 +0800

    BugID:17922164: Merge github master back for atsamd5x_e5x (5eb763c44 & 893cf61 from NashZhangMCHP)
    Change-Id: I7d03792ef351bf5e6123e92e80896904337e5dd5

--- platform/mcu/atsamd5x_e5x/hal/hal_uart_start.h
+++ platform/mcu/atsamd5x_e5x/hal/hal_uart_start.h
@@ -1,5 +1,30 @@
-/*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+/**
+ * \file
+ *
+ * \brief Flash HAL related functionality declaration.
+ *
+ * Copyright (c) 2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * \page License
+ *
+ * Subject to your compliance with these terms, you may use Microchip
+ * software and any derivatives exclusively with Microchip products.
+ * It is your responsibility to comply with third party license terms applicable
+ * to your use of third party software (including open source software) that
+ * may accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+ * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+ * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+ * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+ * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+ * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
  */
 
 #ifndef __HAL_UART_START_H
@@ -9,7 +34,7 @@
  extern "C" {
 #endif
 
-#include <atmel_start.h>  
+#include <atmel_start.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -100,4 +125,4 @@
 }
 #endif
 
-#endif /* __HAL_UART_STM32L4_H */
+#endif /* __HAL_UART_START_H */
