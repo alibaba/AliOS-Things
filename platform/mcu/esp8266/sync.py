30b3e7116e58722b7cfc95f7b2a2407d6a272ac9
Li Cheng <lc122798@alibaba-inc.com>
2019-01-23 14:54:51 +0800

    BugID:18802304:remove esptool_py from mcu/esp8266
    Change-Id: I2fa4849e39cfb682a6298067ce9554d6b7b4f2eb

--- platform/mcu/esp8266/sync.py
+++
@@ -1,9 +0,0 @@
-#define if this module need to be synchronized
-sync = True
-
-#file operations
-#['operation', 'source'[, 'destination']]
-fileopts = [
-    ['copy', '*', '.'],
-    ['delete', 'esptool_py'],
-]
