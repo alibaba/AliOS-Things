ed26e9da06df176a0734784685c2ec2c14f1a926
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-01-23 15:18:14 +0800

    BugID:17922164: Merge github master back for atsamd5x_e5x (5eb763c44 & 893cf61 from NashZhangMCHP)
    Change-Id: I7d03792ef351bf5e6123e92e80896904337e5dd5

--- platform/mcu/atsamd5x_e5x/hal/hal_uart_start.c
+++ platform/mcu/atsamd5x_e5x/hal/hal_uart_start.c
@@ -1,3 +1,32 @@
+/**
+ * \file
+ *
+ * \brief Timer HAL related functionality implementation.
+ *
+ * Copyright (c) 2018 Microchip Technology Inc. and its subsidiaries.
+ *
+ * \page License
+ *
+ * Subject to your compliance with these terms, you may use Microchip
+ * software and any derivatives exclusively with Microchip products.
+ * It is your responsibility to comply with third party license terms applicable
+ * to your use of third party software (including open source software) that
+ * may accompany Microchip software.
+ *
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES,
+ * WHETHER EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE,
+ * INCLUDING ANY IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY,
+ * AND FITNESS FOR A PARTICULAR PURPOSE. IN NO EVENT WILL MICROCHIP BE
+ * LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, INCIDENTAL OR CONSEQUENTIAL
+ * LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND WHATSOEVER RELATED TO THE
+ * SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS BEEN ADVISED OF THE
+ * POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE FULLEST EXTENT
+ * ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN ANY WAY
+ * RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+ * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ */
+
 #include <atmel_start.h>
 
 #include <string.h>
@@ -37,7 +66,8 @@ int32_t hal_uart_send(uart_dev_t *uart, const void *data, uint32_t size, uint32_
     if ((uart == NULL) || (data == NULL)) {
         return -1;
     }
-    if (uart->port == STDIO_UART) {
+
+    if (uart->port == NULL) { /* USART_2 is used for STDIO */
         uart->priv = &USART_2;
     }
 
@@ -54,7 +84,7 @@ int32_t hal_uart_recv_II(uart_dev_t *uart, void *data, uint32_t expect_size,
     if ((uart == NULL) || (data == NULL)) {
         return -1;
     }
-    if (uart->port == STDIO_UART) {
+    if (uart->port == NULL) { /* USART_2 is used for STDIO */
         uart->priv = &USART_2;
     }
 
