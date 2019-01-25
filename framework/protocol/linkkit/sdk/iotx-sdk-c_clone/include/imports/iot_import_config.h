b3f7f945771a5ee01860f6aa09cb43cc122ce8db
zhangtian <dongren.zt@alibaba-inc.com>
2019-01-25 15:35:04 +0800

    bugID:18445354:[linkkit log] enable debug level print for test
    Change-Id: Ie77468100c509712fd8254de344f0b818f670e6a

--- framework/protocol/linkkit/sdk/iotx-sdk-c_clone/include/imports/iot_import_config.h
+++ framework/protocol/linkkit/sdk/iotx-sdk-c_clone/include/imports/iot_import_config.h
@@ -44,7 +44,7 @@
 #endif
 
 #ifndef CONFIG_BLDTIME_MUTE_DBGLOG
-    #define CONFIG_BLDTIME_MUTE_DBGLOG      (1)
+    #define CONFIG_BLDTIME_MUTE_DBGLOG      (0)
 #endif
 
 #ifndef CONFIG_DISPATCH_QUEUE_MAXLEN
