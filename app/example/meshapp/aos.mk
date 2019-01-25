41c09a99e490d1296ba4ccce5d28bfff28c102b7
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-25 17:09:09 +0800

    BugID:18335647:comp Config.in optimization: lwip
    Change-Id: I011211c2ab190d82e98da133d3f45e3c4e6b098e
    
    BugID:18335647:comp Config.in optimization: lwip
    
    Change-Id: Icb0ab9d7bcee27100e2e9dc97d24eb79377903cd
    
    BugID:18335647:comp Config.in optimization: lwip
    
    Change-Id: I49f65ffcf3e416ad9c641c349d30a2209368d49f

--- app/example/meshapp/aos.mk
+++ app/example/meshapp/aos.mk
@@ -9,7 +9,10 @@ $(NAME)_SOURCES     := main.c
 $(NAME)_COMPONENTS  += umesh cli netmgr
 GLOBAL_DEFINES      += TAPIF_DEFAULT_OFF DEBUG
 
-ifeq ($(AOS_CONFIG_MESHAUTH), 1)
+ipv6 ?= IPV6
+mesh_hal_test ?= MESHAPP_CONFIG_HAL_TEST
+
+ifeq ($(MESHAUTH), 1)
 $(NAME)_COMPONENTS  += alicrypto id2
 endif
 
@@ -18,11 +21,15 @@ ifeq ($(LWIP), 1)
 $(NAME)_COMPONENTS  += lwip
 endif
 
-ifeq ($(AOS_CONFIG_IPV6),0)
+ifeq ($(ipv6),0)
 GLOBAL_DEFINES += LWIP_IPV6=0
 endif
 
-ifneq (,$(filter linuxhost armhflinux,$(COMPONENTS)))
+ifneq (,$(filter linuxhost,$(COMPONENTS)))
+DDA ?= 1
+endif
+
+ifneq (,$(filter armhflinux,$(COMPONENTS)))
 DDA ?= 1
 endif
 
@@ -35,7 +42,7 @@ ifneq (,$(filter EMW3060,$(MODULE)))
 GLOBAL_DEFINES += MESHAPP_LIGHT_ENABLED
 endif
 
-ifeq ($(MESHAPP_CONFIG_HAL_TEST),1)
+ifeq ($(mesh_hal_test),1)
 GLOBAL_DEFINES += MESH_HAL_TEST_ENABLED
 endif
 
