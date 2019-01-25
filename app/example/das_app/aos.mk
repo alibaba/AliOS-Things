73beadb1652c3a578c1cae9397b0cf0518a14e09
cedar.wxs <cedar.wxs@alibaba-inc.com>
2019-01-24 22:54:39 -0800

    BugID: 18833204: Link Security SOC DAS Example
    Change-Id: I7e8813a73c6cfd45708ac85a29fd1ccefe5e7c46
    Signed-off-by: cedar.wxs <cedar.wxs@alibaba-inc.com>

---
+++ app/example/das_app/aos.mk
@@ -0,0 +1,24 @@
+NAME := das_client
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := Link Security Device Attestation example
+
+$(NAME)_SOURCES := app_entry.c
+$(NAME)_SOURCES += mqtt_das_example.c
+$(NAME)_COMPONENTS := feature.linkkit-mqtt netmgr cjson cli das
+
+MQTTAPP_CONFIG_TEST_LOOP ?= 1
+loop ?= $(MQTTAPP_CONFIG_TEST_LOOP)
+
+ifeq ($(LWIP),1)
+$(NAME)_COMPONENTS  += lwip
+no_with_lwip := 0
+endif
+
+ifneq ($(loop),0)
+$(NAME)_DEFINES      += TEST_LOOP
+endif
+
+#GLOBAL_CFLAGS += -DON_DAILY
+
---
+++ app/example/das_app/k_app_config.h
@@ -0,0 +1,7 @@
+
+
+/* user space */
+#ifndef RHINO_CONFIG_USER_SPACE
+#define RHINO_CONFIG_USER_SPACE              0
+#endif
+
