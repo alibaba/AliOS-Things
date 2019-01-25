99108ab5b49f4890981c648b76136578f7c88f3a
junping.pjp <junping.pjp@alibaba-inc.com>
2019-01-25 15:16:20 +0800

    BugID:18335647:comp Config.in optimization: fix typo issue
    Change-Id: I877acfe9d1de427674eb96a389a658a35d7f057b

--- app/example/otaapp/aos.mk
+++ app/example/otaapp/aos.mk
@@ -4,15 +4,17 @@ $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := OTA demo app for developers
 
+OTAAPP_CONFIG_TEST_LOOP ?= 0
+loop ?= $(OTAAPP_CONFIG_TEST_LOOP)
+
 $(NAME)_SOURCES := otaapp.c 
 $(NAME)_COMPONENTS := feature.linkkit-mqtt netmgr ota cjson cli
-
 ifeq ($(LWIP),1)
 $(NAME)_COMPONENTS  += lwip
 no_with_lwip := 0
 endif
 
-ifeq ($(OTAAPP_CONFIG_TEST_LOOP),1)
+ifeq ($(loop),1)
 $(NAME)_DEFINES     += TEST_LOOP
 endif
 
