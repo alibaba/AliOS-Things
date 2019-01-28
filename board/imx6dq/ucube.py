ee8a9803771260c2c640bc24e3246b63be7b8038
cedar.wxs <cedar.wxs@alibaba-inc.com>
2019-01-27 17:55:57 -0800

    BugID: 18840700: disable id2/prov/itls app in auto test by default
    Change-Id: I39c4f40ac279fabfed89ef8e6d94de462e86f7f6
    Signed-off-by: cedar.wxs <cedar.wxs@alibaba-inc.com>

--- board/imx6dq/ucube.py
+++ board/imx6dq/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="blink bluetooth.bleadv bluetooth.blemesh_tmall bluetooth.bleperipheral helloworld id2_app modbus_demo udataapp yts"
+linux_only_targets="blink bluetooth.bleadv bluetooth.blemesh_tmall bluetooth.bleperipheral helloworld modbus_demo udataapp yts"
