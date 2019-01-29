26077c615ce746c527a57a6c851814eecdf03261
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-01-29 12:42:18 +0800

    BugID:17918302: Add aos_kv_secure_* interface for kv encrypt/decrypt feature
    Change-Id: Ifee68050f78c53d436ee4f9d265c7b7c31f7be30

--- include/aos/kv.h
+++ include/aos/kv.h
@@ -69,6 +69,33 @@ int aos_kv_del(const char *key);
  */
 int aos_kv_del_by_prefix(const char *prefix);
 
+/**
+ * Add a new KV pair with encryption.
+ *
+ * @param[in]  key    the key of the KV pair.
+ * @param[in]  value  the value of the KV pair.
+ * @param[in]  len    the length of the value.
+ * @param[in]  sync   save the KV pair to flash right now (should always be 1).
+ *
+ * @return  0 on success, negative error on failure.
+ */
+int aos_kv_secure_set(const char *key, const void *value, int len, int sync);
+
+/**
+ * Get the KV pair's value stored in buffer by its key with decrypt.
+ *
+ * @note: the buffer_len should be larger than the real length of the value,
+ *        otherwise buffer would be NULL.
+ *
+ * @param[in]      key         the key of the KV pair to get.
+ * @param[out]     buffer      the memory to store the value.
+ * @param[in-out]  buffer_len  in: the length of the input buffer.
+ *                             out: the real length of the value.
+ *
+ * @return  0 on success, negative error on failure.
+ */
+int aos_kv_secure_get(const char *key, void *buffer, int *buffer_len);
+
 #ifdef __cplusplus
 }
 #endif
