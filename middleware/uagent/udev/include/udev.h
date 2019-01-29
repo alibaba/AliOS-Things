da443b9d81d5f90468a2b67247a6f47acff6dc45
chenan.xxw <chenan.xxw@alibaba-inc.com>
2019-01-29 15:17:07 +0800

    BugID: 18795674: format coding style
    Change-Id: If8bbe178ca9d12bc01ac7c49d6bcf9160c7eccf4

--- middleware/uagent/udev/include/udev.h
+++ middleware/uagent/udev/include/udev.h
@@ -2,23 +2,29 @@
  * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
-#ifndef __UDEV_H_
-#define __UDEV_H_
+#ifndef UDEV_H
+#define UDEV_H
 
 #ifdef __cplusplus
 extern "C" {
-#endif
+#endif /* __cplusplus */
 
 #define UDEV_BROKER_HOST     "116.62.245.240"
 #define UDEV_BROKER_PORT     1883
 
-/* mqtt broker keepalive (ms) */
-#define UDEV_KEEPALIVE      10000
+#define UDEV_KEEPALIVE      10000   /* mqtt broker keepalive (ms) */
 
+/**
+* @brief udev component init
+* @param pk     product key
+* @param dn     device name
+*
+* @return -1: fail  0: succeed
+*/
 int udev_init(const char *pk, const char *dn);
 
 #ifdef __cplusplus
 }
-#endif
+#endif /* __cplusplus */
 
-#endif /* __UDEV_H_ */
+#endif /* UDEV_H */
