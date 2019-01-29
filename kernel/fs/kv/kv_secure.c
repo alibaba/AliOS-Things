9bdb17813899c74b1df54abc78a15458508ae6b4
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-01-29 12:43:12 +0800

    BugID:17918302: Optimize the kv secure feature implement
    Change-Id: I5bfc44e98efde9e46166c4a27bb6af3686b214b6

--- kernel/fs/kv/kv_secure.c
+++ kernel/fs/kv/kv_secure.c
@@ -16,30 +16,26 @@
 
 #include "mbedtls/aes.h"
 
+#define OFB_KEY_LEN 32
+#define OFB_IV_LEN  16
+
 static mbedtls_aes_context aes_context;
 
-static uint8_t aes_key[32] = {
-    0x86, 0xf6, 0xd2, 0xbe, 0x45, 0xb5, 0xab, 0x9c,
-    0xc7, 0xd5, 0x96, 0xf7, 0xaf, 0x45, 0xfa, 0xf7,
-    0xbe, 0x6a, 0x5d, 0xb0, 0x04, 0xc4, 0xde, 0xb5,
-    0xf5, 0x0c, 0x4f, 0xc3, 0x71, 0x19, 0x3e, 0xe8
-};
-
-static uint8_t aes_iv[16] = {
-    0xef, 0x80, 0x18, 0xdc, 0xa3, 0x72, 0x72, 0x31,
-    0x99, 0x2e, 0x3a, 0xba, 0x60, 0xf5, 0x0b, 0xd4
-};
-
-static int32_t _ofb_encrypt_wrap(uint8_t *input, uint8_t *output, uint32_t len)
+static int32_t _ofb_encrypt_wrap(uint8_t *ofb_key, uint8_t *ofb_iv,
+                                 uint8_t *input, uint8_t *output, uint32_t len)
 {
     int32_t ret;
 
     uint32_t offset = 0;
-    uint8_t  iv[16] = {0};
+    uint8_t  iv[OFB_IV_LEN] = {0};
 
-    memcpy(iv, aes_iv, sizeof(aes_iv));
+    if ((input == NULL) || (output == NULL)) {
+        return KV_ERR_INVALID_PARAM;
+    }
 
-    ret = mbedtls_aes_setkey_enc(&aes_context, aes_key, sizeof(aes_key) * 8);
+    memcpy(iv, ofb_iv, sizeof(iv));
+
+    ret = mbedtls_aes_setkey_enc(&aes_context, ofb_key, OFB_KEY_LEN * 8);
     if (ret != KV_OK) {
         return ret;
     }
@@ -54,7 +50,17 @@ static int32_t _ofb_encrypt_wrap(uint8_t *input, uint8_t *output, uint32_t len)
 
 int32_t kv_secure_encrypt(uint8_t *input, uint8_t *output, uint32_t input_len)
 {
-    if (_ofb_encrypt_wrap(input, output, input_len) != KV_OK) {
+    uint8_t *ofb_key = NULL;
+    uint8_t *ofb_iv  = NULL;
+
+    ofb_key = kv_secure_getkey(OFB_KEY_LEN);
+    ofb_iv  = kv_secure_getiv(OFB_IV_LEN);
+
+    if ((ofb_key == NULL) || (ofb_iv == NULL)) {
+        return KV_ERR_ENCRYPT;
+    }
+
+    if (_ofb_encrypt_wrap(ofb_key, ofb_iv, input, output, input_len) != KV_OK) {
         return KV_ERR_ENCRYPT;
     }
 
@@ -63,6 +69,16 @@ int32_t kv_secure_encrypt(uint8_t *input, uint8_t *output, uint32_t input_len)
 
 int32_t kv_secure_decrypt(uint8_t *input, uint8_t *output, uint32_t input_len)
 {
+    uint8_t *ofb_key = NULL;
+    uint8_t *ofb_iv  = NULL;
+
+    ofb_key = kv_secure_getkey(OFB_KEY_LEN);
+    ofb_iv  = kv_secure_getiv(OFB_IV_LEN);
+
+    if ((ofb_key == NULL) || (ofb_iv == NULL)) {
+        return KV_ERR_ENCRYPT;
+    }
+
     if (_ofb_encrypt_wrap(input, output, input_len) != KV_OK) {
         return KV_ERR_ENCRYPT;
     }
