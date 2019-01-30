49853e122cfa859130c42d8d5f99d778a4667b5f
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-01-30 14:54:03 +0800

    bugID:18638526:[esp32] BT host restream, enable breezeapp in AutoBuild
    Change-Id: I6d330442c878b0e5cef7cc8eafad6529066b727a

--- board/esp32sram/ucube.py
+++ board/esp32sram/ucube.py
@@ -6,6 +6,5 @@ if aos_global_config.get('hci_h4', 0):
     component.add_global_macros('CONFIG_BLE_HCI_H4_UART_PORT=1')
 
 
-platform_options="wifi=1"
-supported_targets="bluetooth.bleadv bluetooth.bleperipheral coapapp helloworld linkkit_gateway linkkitapp mqttapp"
+supported_targets="bluetooth.bleadv bluetooth.bleperipheral bluetooth.breezeapp coapapp helloworld linkkit_gateway linkkitapp mqttapp"
 linux_only_targets="athostapp blink http2app meshapp otaapp tls udataapp yts"
