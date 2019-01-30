47de68ce582f71f57ede0d9a3ba22d7f13f1603a
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-01-30 15:26:26 +0800

    BugID:17787848: Configure a certificate config for Prevalidate
    Change-Id: Ibacf53ab34f4950c5df41b42859fbc5d741291e9

--- test/testcase/certificate_test/aos.mk
+++ test/testcase/certificate_test/aos.mk
@@ -7,4 +7,12 @@ $(NAME)_SOURCES    := aos_test.c cutest/cut.c
 $(NAME)_COMPONENTS := rhino
 $(NAME)_COMPONENTS += kv
 
+#Prevalidate Mode
+ifeq ($(certificate_level), pv)
+$(NAME)_DEFINES += TEST_CONFIG_MALLOC_FREE_TIMES=1000
+$(NAME)_DEFINES += TEST_CONFIG_CREATE_TASK_TIMES=100
+$(NAME)_DEFINES += TEST_CONFIG_SYNC_TIMES=1000
+$(NAME)_DEFINES += TEST_CONFIG_KV_ENABLED=0
+endif
+
 $(NAME)_CFLAGS += -Wall -Werror -Wno-unused-varible
