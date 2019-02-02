8e1ef5f919be03bd803ba6b2ad0c4afb798759c2
gonghui.hui <gonghui.hui@alibaba-inc.com>
2019-02-02 23:18:31 +0800

    BugID:17158311:update ulog focus on the pop out log via file system
    Change-Id: Id7c9e14b822b9d01cab22dd8bc3eaf1ab514009d

--- middleware/uagent/ulog/ulog.c
+++ middleware/uagent/ulog/ulog.c
@@ -1,17 +1,23 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
 #include <time.h>
 #include "ulog/ulog.h"
+#include "ulog_api.h"
+#include "uring_fifo.h"
 #include "aos/kernel.h"
 #include "k_config.h"
 
-typedef void(* on_ulog_man_service)(const uint32_t, const uint32_t);
+typedef void(*on_ulog_man_service)(const uint32_t, const uint32_t);
 
 typedef uint8_t session_filter_para_t;
 
-typedef struct{
+typedef struct {
     char*                func_mark;
     char*                sub_func_mark[2];
     on_ulog_man_service  handler;
@@ -19,58 +25,63 @@ typedef struct{
 
 #define CHECK_PASS_POP_OUT(SESSION, LEVEL) (stop_filter_level[SESSION]>LEVEL)
 
-/* used for indicates message has been filled into fifo */
-static aos_sem_t sem_log_push;
-
 static aos_task_t ulog_routine;
 static char g_host_name[10];
 
 #define TAG_FILE_NAME_MAX_LEN 12
 static char* UNKNOWN_FILE_NAME = "unknown";
 
+static char module_name[MOD_SIZE][4] = { "UN","OS","NT","CD","AP" };
+const char months[][4] = { "Jan", "Feb", "Mar", "Apr", "May",
+                       "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
 
-const char months[][4] = {"Jan", "Feb", "Mar", "Apr", "May",
-                       "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
 
-static aos_mutex_t ulog_mutex;
-
-uint8_t push_stop_filter_level = LOG_EMERG;
+static uint8_t push_stop_filter_level = LOG_EMERG;
 
 /* stop filter, any level <= stop_filter_level(value >= this value) will be abonded */
 session_filter_para_t stop_filter_level[SESSION_CNT] =
 {
-    STOP_FILTER_DEFAULT, STOP_FILTER_UDP
-};
+    STOP_FILTER_DEFAULT,
+ #if ULOG_POP_UDP_ENABLE
+    STOP_FILTER_UDP,
+ #endif
 
-extern void on_update_syslog_watcher_addr(const uint32_t ip_d, const uint32_t port );
-extern void on_tcpip_service_on(const uint32_t on, const uint32_t off);
+ #if ULOG_POP_FS_ENABLE
+    STOP_FILTER_FS
+ #endif
+};
 
 const ulog_man_handler_service_t ulog_man_handler_service[] =
 {
+#if ULOG_POP_UDP_ENABLE
     { "listen", {"ip","port"}, on_update_syslog_watcher_addr },
     { "tcpip",  {"on","off" }, on_tcpip_service_on           },
+#endif
+#if ULOG_POP_FS_ENABLE
+    { "fspause", {"on","off"}, on_fs_record_pause },
+#endif
 };
 
 static void ulog_man_handler(const char* raw_str)
 {
-    if(raw_str!=NULL){
-        const uint8_t man_handler_service_size = sizeof(ulog_man_handler_service)/sizeof(ulog_man_handler_service_t);
-        uint8_t i = 0;
-        for(; i<man_handler_service_size; i++){
-            if(0==strncmp(ulog_man_handler_service[i].func_mark, raw_str, strlen(ulog_man_handler_service[i].func_mark))){
-                char* p = &raw_str[strlen(ulog_man_handler_service[i].func_mark)];
-                if(p[0] == ' '){
-                    uint32_t param[2] = {0, 0};
+    if (raw_str != NULL) {
+        const int8_t man_handler_service_size = sizeof(ulog_man_handler_service) / sizeof(ulog_man_handler_service_t);
+        int8_t i = 0;
+        for (; i < man_handler_service_size; i++) {
+            if (0 == strncmp(ulog_man_handler_service[i].func_mark, raw_str, strlen(ulog_man_handler_service[i].func_mark))) {
+                const char* p = &raw_str[strlen(ulog_man_handler_service[i].func_mark)];
+                if (p[0] == ' ') {
+                    uint32_t param[2] = { 0, 0 };
                     uint8_t j = 0;
 
-                    for(; j<2; j++){
+                    for (; j < 2; j++) {
                         char * q = NULL;
-                        if( NULL != (q=strstr(&p[1], ulog_man_handler_service[i].sub_func_mark[j])) ){
-                            char* val_str = &q[strlen(ulog_man_handler_service[i].sub_func_mark[j])+1];
+                        if (NULL != (q = strstr(&p[1], ulog_man_handler_service[i].sub_func_mark[j]))) {
+                            char* val_str = &q[strlen(ulog_man_handler_service[i].sub_func_mark[j]) + 1];
                             param[j] = strtoul(val_str, NULL, 10);
                         }
                     }
-                    if( (param[0]!=0||param[1]!=0) && (ulog_man_handler_service[i].handler != NULL) ){
+                    if ((param[0] != 0 || param[1] != 0) && (ulog_man_handler_service[i].handler != NULL)) {
                         ulog_man_handler_service[i].handler(param[0], param[1]);
                     }
                 }
@@ -80,27 +91,7 @@ static void ulog_man_handler(const char* raw_str)
     }
 }
 
-static void* wrapper_mutex_init()
-{
-    void* rc = NULL;
-
-    if(0==aos_mutex_new(&ulog_mutex)){
-        rc = (void*)&ulog_mutex;
-    }
-    return rc;
-}
-
-int32_t wrapper_mutex_lock(void* param)
-{
-    return aos_mutex_lock((aos_mutex_t*)param,AOS_WAIT_FOREVER);
-}
-
-int32_t wrapper_mutex_unlock(void* param)
-{
-    return aos_mutex_unlock((aos_mutex_t*)param);
-}
-
-//Result is like "Nov 28 15:19:20.122 "
+/* Result is like "Nov 28 15:19:20.122 " */
 char *syslog_format_time(char *buffer, const int len)
 {
     const long long ms = aos_now_ms();
@@ -108,12 +99,12 @@ char *syslog_format_time(char *buffer, const int len)
         time_t rawtime;
         time(&rawtime);
         struct tm *tm = localtime(&rawtime);
-        //%b format of strftime() is platform-dependent, so we realized it by-self
-        snprintf(buffer,len,"%s ",months[tm->tm_mon<12?tm->tm_mon:0]);
-        strftime(&buffer[4], len-4, "%d %H:%M:%S", tm);
-        const int32_t milli    = ms % 1000;
+        /* %b format of strftime() is platform-dependent, so we realized it by-self */
+        snprintf(buffer, len, "%s ", months[tm->tm_mon < 12 ? tm->tm_mon : 0]);
+        strftime(&buffer[4], len - 4, "%d %H:%M:%S", tm);
+        const int32_t milli = ms % 1000;
         char      ms_str[8] = "";
-	    memset(ms_str, 0, sizeof(ms_str));
+        memset(ms_str, 0, sizeof(ms_str));
         snprintf(ms_str, sizeof(ms_str), ".%03d ", milli);
         strncat(buffer, ms_str, len - strlen(buffer) - 1);
     }
@@ -121,73 +112,133 @@ char *syslog_format_time(char *buffer, const int len)
     return buffer;
 }
 
-static void ulog_push_cb(void* para)
-{
-    aos_sem_signal(&sem_log_push);
-}
-
 static char* trim_file_path(const char* path)
 {
-	char* filename = (char*)path;
-    if(path!=NULL) {
-        if( (filename=strrchr(path, '/')) != NULL ) {
+    char* filename = (char*)path;
+    if (path != NULL) {
+        if ((filename = strrchr(path, '/')) != NULL) {
             filename++;
         }
     }
 
-    if(filename!=NULL) {
-        const size_t filename_len=strlen(filename);
-        if( filename_len>0 ) {
-            if(filename_len>TAG_FILE_NAME_MAX_LEN) { //trig it using last 32 string
-                filename += filename_len-TAG_FILE_NAME_MAX_LEN;
+    if (filename != NULL) {
+        const size_t filename_len = strlen(filename);
+        if (filename_len > 0) {
+            if (filename_len > TAG_FILE_NAME_MAX_LEN) { /* trig it using last 32 string */
+                filename += filename_len - TAG_FILE_NAME_MAX_LEN;
             }
-        } else {//cannot get file name if input path is like "/d/timer/"
+        } else { /* cannot get file name if input path is like "/d/timer/" */
             filename = UNKNOWN_FILE_NAME;
         }
-    } else { //using UNKNOWN_FILE_NAME
+    } else { /* using UNKNOWN_FILE_NAME */
         filename = UNKNOWN_FILE_NAME;
     }
 
-	return filename;
+    return filename;
 }
 
-osi_uring_fifo os_related =
+/**
+ * Function prototype for log text
+ *
+ * REMARK: Recommed use below brief API instead of this
+ *
+ * @param  m    Module type, reference @MOD_TYPE
+ * @param  s    Serverity of Log, choose from above value,
+ * @param  f    Usually File name
+ * @param  l    Usually Line number of comment
+ * @param  fmt, ...  Variable Parameter, support format print to log
+ * @return  0: success. use type int is align with aos_*
+ */
+int ulog(const MOD_TYPE m, const unsigned char s, const char* f, const unsigned long l, const char *fmt, ...)
 {
-    &g_host_name[0],
-    wrapper_mutex_init,
-    wrapper_mutex_lock,
-    wrapper_mutex_unlock,
-    syslog_format_time,
-    ulog_push_cb,
-    trim_file_path
-};
+    int rc = -1;
+    if (log_init && s < push_stop_filter_level) {
+        /* using type char instead of int8_t which align with the prototype of string operation function */
+        char tmp_buf[SYSLOG_SIZE];
+        char* p = &tmp_buf[0];
+        uint16_t ulog_len = 0;
+        /*ZERO the buffer is not necessary as null terminated is considering
+        when using snprintf strncat vsnprintf */
+#if EXTREAM_LOG_TEXT == 0
+        /* PRI */
+        snprintf(p, SYSLOG_SIZE, "<%d>", SYSLOG_FACILITY + s);
+
+        /* HEADER */
+        syslog_format_time(&p[strlen(p)], SYSLOG_SIZE - strlen(p));/* -->Total 23~24 Byte */
+        strncat(p, g_host_name, SYSLOG_SIZE - strlen(p) - 1);
+        strncat(p, " ", SYSLOG_SIZE - strlen(p) - 1);
+        /* -->Total 23~33 Byte */
+
+        /* MSG */
+        ulog_len = strlen(p);/* MUST < 36 */
+        p += ulog_len;
+        int16_t empty_room_size = SYSLOG_SIZE - ulog_len;
+
+        uint16_t len = snprintf(p, empty_room_size, "%s_%s[%d]: ", m < MOD_SIZE ? module_name[m] : module_name[MOD_UNKNOWN],
+            trim_file_path(f), l);
+        ulog_len += (len < empty_room_size) ? len : (empty_room_size - 1);
+        p += len;
+
+        empty_room_size = SYSLOG_SIZE - ulog_len;
+        if (empty_room_size > 1) {
+            va_list argptr;
+            va_start(argptr, fmt);
+            len = vsnprintf(p, empty_room_size, fmt, argptr);
+            ulog_len += (len < empty_room_size) ? len : (empty_room_size - 1);
+            va_end(argptr);
+        }
+#else
+        /* severity */
+        snprintf(p, SYSLOG_SIZE, "<%d> ", s);
+        ulog_len = strlen(p);
+
+        va_list argptr;
+        va_start(argptr, fmt);
+        const int len = vsnprintf(&p[ulog_len], SYSLOG_SIZE - ulog_len, fmt, argptr);
+        ulog_len += (len < (SYSLOG_SIZE - ulog_len)) ? len : (SYSLOG_SIZE - ulog_len - 1);
+        va_end(argptr);
+#endif /*EXTREAM_LOG_TEXT == 0*/
+        /* null-terminated also be calculated when saved in message queue */
+        ulog_len++;
+        rc = uring_fifo_push_s(tmp_buf, ulog_len);
+
+    }
+    return rc;
+}
 
 static void ulog_handler(void* para, const void* log_text, const uint16_t log_len)
 {
-    if( (log_text!=NULL) && log_len>0 ) {
+    if ((log_text != NULL) && log_len > 0) {
         char* str = (char*)log_text;
-#if EXTREAM_LOG_TEXT==0
-        if( (str[0]=='<') && (strchr(str, '>')!=NULL) ) {//syslog format
-            //pri = facility*8+level
+        if ((str[0] == '<') && (strchr(str, '>') != NULL)) {
+            /* pri = facility*8+level */
             const uint32_t pri = strtoul(&str[1], NULL, 10);
-            const uint8_t level = (pri&0x7);
-            if(CHECK_PASS_POP_OUT(SESSION_DIRECTION,level)){
-                if(log_get_mutex()){
+            const uint8_t severity = (pri & 0x7);
+            if (CHECK_PASS_POP_OUT(SESSION_DIRECTION, severity)) {
+                if (log_get_mutex()) {
                     puts(log_text);
                     log_release_mutex();
                 }
             }
 
-            if(CHECK_PASS_POP_OUT(SESSION_UDP,level)){
-                pop_out_on_udp(log_text,log_len);
+#if ULOG_POP_UDP_ENABLE
+            if (CHECK_PASS_POP_OUT(SESSION_UDP, severity)) {
+                pop_out_on_udp(log_text, log_len);
             }
-        }else
 #endif
-        if(0==strncmp(str,ULOG_CMD_PREFIX,strlen(ULOG_CMD_PREFIX))){
+
+#if ULOG_POP_FS_ENABLE
+            if (CHECK_PASS_POP_OUT(SESSION_FILE, severity)) {
+                pop_out_on_fs(log_text, log_len);
+            }
+#endif
+
+        } else if (0 == strncmp(str, ULOG_CMD_PREFIX, strlen(ULOG_CMD_PREFIX))) {
             /* syslog management */
             ulog_man_handler(&str[strlen(ULOG_CMD_PREFIX)]);
-        }else{
-            if(log_get_mutex()){
+        } else {
+            if (log_get_mutex()) {
+                /* unknown format */
                 puts(log_text);
                 log_release_mutex();
             }
@@ -197,54 +248,56 @@ static void ulog_handler(void* para, const void* log_text, const uint16_t log_le
 
 static void log_routine(void* para)
 {
-    while(1)
+    while (1)
     {
-        //PRI      HEADER            MSG
-        //<130>Oct 9 22:33:20.111 soc OS_kernel.c[111]: The audit daemon is exiting.
-        if(aos_sem_wait(&sem_log_push, AOS_WAIT_FOREVER)==0)
-        {
-            uring_fifo_pop_cb(ulog_handler, NULL);
-        }
+        /* PRI      HEADER            MSG */
+        /* <130>Oct 9 22:33:20.111 soc OS_kernel.c[111]: The audit daemon is exiting. */
+        uring_fifo_pop_cb(ulog_handler, NULL);
     }
 }
 
-void ulog_async_init(const uint8_t host_name[8])
+void ulog_async_init(const char host_name[8])
 {
-    uring_fifo_init(DEFAULT_ASYNC_SYSLOG_DEPTH);
-    if( 0==aos_sem_new(&sem_log_push, 0) ) {
-        if( 0==aos_task_new_ext(&ulog_routine,
-            "ulog",
-            log_routine,
-            NULL,
-            LOG_ROUTINE_TASK_STACK_DEPTH,
-            RHINO_CONFIG_USER_PRI_MAX) ) {
-            on_filter_level_change(SESSION_CNT,LOG_NONE);
-            if(NULL!=host_name) {
-                strncpy(g_host_name, host_name, sizeof(g_host_name));
-                g_host_name[sizeof(g_host_name)-1] = '\0';
-            }
+    /* start log on fs feature if both vfs && spiffs support */
+#if ULOG_POP_FS_ENABLE
+    ulog_fs_init();
+#endif
+
+    uring_fifo_init();
+
+    if (0 == aos_task_new_ext(&ulog_routine,
+        "ulog",
+        log_routine,
+        NULL,
+        LOG_ROUTINE_TASK_STACK_DEPTH,
+        RHINO_CONFIG_USER_PRI_MAX)) {
+        on_filter_level_change(SESSION_CNT, LOG_NONE);
+        if (NULL != host_name) {
+            strncpy(g_host_name, host_name, sizeof(g_host_name));
+            g_host_name[sizeof(g_host_name) - 1] = '\0';
         }
     }
+
 }
 
 void on_filter_level_change(const SESSION_TYPE session, const uint8_t level)
 {
     bool next_handle = true;
-    if(session==SESSION_CNT){//only happen on init, get the initial push level
+    if (session == SESSION_CNT) {/* only happen on init, get the initial push level */
         push_stop_filter_level = LOG_EMERG;
-    }else if(session<SESSION_CNT && level<=LOG_NONE){
+    } else if (session < SESSION_CNT && level <= LOG_NONE) {
         stop_filter_level[session] = level;
 
-        //suppose we use update session sf(stop filter) as push filter, this value will be updated below
+        /* suppose we use update session sf(stop filter) as push filter, this value will be updated below */
         push_stop_filter_level = stop_filter_level[session];
-    }else{
+    } else {
         next_handle = false;
     }
-    if(next_handle){
+    if (next_handle) {
         uint8_t i = 0;
-        for(; i<SESSION_CNT;i++)
+        for (; i < SESSION_CNT; i++)
         {
-            if(push_stop_filter_level<stop_filter_level[i]){
+            if (push_stop_filter_level < stop_filter_level[i]) {
                 push_stop_filter_level = stop_filter_level[i];
             }
         }
@@ -253,8 +306,8 @@ void on_filter_level_change(const SESSION_TYPE session, const uint8_t level)
 
 void ulog_man(const char* cmd_str)
 {
-    if(cmd_str!=NULL && log_init){
-        const uint16_t cmd_str_size = strlen(cmd_str)+strlen(ULOG_CMD_PREFIX)+1;
+    if (cmd_str != NULL && log_init) {
+        const uint16_t cmd_str_size = strlen(cmd_str) + strlen(ULOG_CMD_PREFIX) + 1;
         char* tmpbuf = (char*)aos_malloc(cmd_str_size);
         strncpy(tmpbuf, ULOG_CMD_PREFIX, cmd_str_size);
         strncat(tmpbuf, cmd_str, cmd_str_size - strlen(ULOG_CMD_PREFIX) - 1);
--- middleware/uagent/ulog/uring_fifo.c
+++ middleware/uagent/ulog/uring_fifo.c
@@ -1,176 +1,61 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#include "uring_fifo.h"
 #include <string.h>
 #include <stdio.h>
 #include <stdarg.h>
-#include "uring_fifo.h"
+#include "aos/kernel.h"
 
-//syslog protocol, facility local use 0
-const uint8_t SYSLOG_FACILITY = 128U;
-
-/* log struct */
-typedef struct{
-    uint8_t      fifo_len;
-    char         fifo_text[SYSLOG_SIZE];
-}fifo_t;
-
-extern osi_uring_fifo os_related;
-
-static uint16_t msg_in_ptr  = 0;    //input point of buffer
-static uint16_t msg_out_ptr = 0;    //output point of buffer
-//static uint16_t peak_used   = 0;    //peak used of buffer
-
-static fifo_t* log_buffer = NULL;
-//comes from customer
-static void* fifo_entry_mutex;
-
-static uint16_t syslog_depth = MIN_SYSLOG_DEPTH;
-
-static char module_name[MOD_SIZE][4] = {"UN","OS","NT","CD","AP"};
+static aos_queue_t ulog_queue;
+static uint8_t*    ulog_buffer;
 
 
-bool uring_fifo_init(const uint16_t queue_depth)
+/**
+* This function will create a ring fifo for ulog.
+*
+*
+* @return  0: success.
+*/
+int uring_fifo_init()
 {
-    bool rc = false;
-    //the depth too small is not allowed
-    syslog_depth = queue_depth>MIN_SYSLOG_DEPTH?queue_depth:MIN_SYSLOG_DEPTH;
-    log_buffer = (fifo_t*)aos_malloc(syslog_depth*sizeof(fifo_t));
-    if(log_buffer != NULL){
-        fifo_entry_mutex = os_related._mutex_init();
-        if(fifo_entry_mutex==NULL){
-            aos_free(log_buffer);
-            log_buffer = NULL;
-        }else{
-            rc = true;
+    int rc = -1;
+    if (ulog_buffer == NULL) {
+        ulog_buffer = aos_malloc(DEFAULT_ASYNC_BUF_SIZE);
+        if (ulog_buffer != NULL) {
+            rc = aos_queue_new(&ulog_queue, ulog_buffer, DEFAULT_ASYNC_BUF_SIZE, SYSLOG_SIZE);
+            if (0 != rc) {
+                aos_free(ulog_buffer);
+                ulog_buffer = NULL;
+            }
         }
     }
 
     return rc;
 }
 
-//Thread Safe to operate this routine.
-bool uring_fifo_push_s(const void* buf, const uint16_t len)
+/**
+* Thread Safe to put the msg into ring - fifo.
+*
+* @param[in]  queue  pointer to the queue.
+* @param[in]  msg    msg to send.
+* @param[in]  size   size of the msg.
+*
+* @return  0: success.
+*/
+int uring_fifo_push_s(const void* buf, const uint16_t len)
 {
-	bool rc = false;
-	if(buf!=NULL && len!=0 && len<=SYSLOG_SIZE)	{
-		if(0==os_related._mutex_lock(fifo_entry_mutex)) {
-    		if ( (msg_in_ptr+1)%syslog_depth==msg_out_ptr )//record buffer is overflow
-    		{
-    			rc = false;
-    		}
-    		else
-    		{
-    			memcpy(log_buffer[msg_in_ptr].fifo_text,buf,len);
-    			log_buffer[msg_in_ptr].fifo_len=len;
-
-    			msg_in_ptr = (msg_in_ptr+1)%syslog_depth;
-                if(NULL != os_related._log_push_cb)
-                {
-                    os_related._log_push_cb(NULL);
-                }
-
-    			rc = true;
-    		}
-            os_related._mutex_unlock(fifo_entry_mutex);
-		}
-	}
-
-	return rc;
+    return aos_queue_send(&ulog_queue, (void*)buf, len);
 }
 
-//Thread Safe to operate this routine.
-bool post_log(const MOD_TYPE m, const uint8_t s, const char* f, const uint32_t l, const char *fmt, ...)
+int uring_fifo_pop_cb(pop_callback cb, void* cb_arg)
 {
-	bool rc = false;
-    if(0==os_related._mutex_lock(fifo_entry_mutex)) {
-        if ( (msg_in_ptr+1)%syslog_depth==msg_out_ptr )//record buffer is overflow
-        {
-            rc = false;
-        }
-        else
-        {
-            log_buffer[msg_in_ptr].fifo_len = 0;
-			//using type char instead of int8_t which align with the prototype of string operation function
-            char* p = (char*)log_buffer[msg_in_ptr].fifo_text;
-			//ZERO the buffer is not necessary as null terminated is considering
-			//when using snprintf strncat vsnprintf
-
-#if EXTREAM_LOG_TEXT == 0
-
-            //PRI
-            snprintf(p, SYSLOG_SIZE,"<%d>",SYSLOG_FACILITY+s);
-
-            //HEADER
-            os_related._syslog_time(&p[strlen(p)], SYSLOG_SIZE-strlen(p));//-->Total 23~24 Byte
-            if(os_related._host_name!=NULL){
-                strncat(p, os_related._host_name, SYSLOG_SIZE - strlen(p) - 1);
-                strncat(p," ", SYSLOG_SIZE - strlen(p) - 1);
-            }                                                             //-->Total 23~33 Byte
-
-            //MSG
-            log_buffer[msg_in_ptr].fifo_len = strlen(p);//MUST < 36
-            p += log_buffer[msg_in_ptr].fifo_len;
-            int32_t empty_room_size = SYSLOG_SIZE-log_buffer[msg_in_ptr].fifo_len;
-
-            int32_t len = snprintf(p,empty_room_size,"%s_%s[%d]: ",m<MOD_SIZE?module_name[m]:module_name[MOD_UNKNOWN],
-                os_related._trim_file_path(f),l);
-            log_buffer[msg_in_ptr].fifo_len += len<empty_room_size?len:empty_room_size-1;
-            p += len;
-
-            empty_room_size = SYSLOG_SIZE-log_buffer[msg_in_ptr].fifo_len;
-            if(empty_room_size>1){
-                va_list argptr;
-                va_start(argptr, fmt);
-                len = vsnprintf(p, empty_room_size, fmt, argptr);
-                log_buffer[msg_in_ptr].fifo_len += len<empty_room_size?len:empty_room_size-1;
-                va_end(argptr);
-            }
-#else
-            va_list argptr;
-			va_start(argptr, fmt);
-			const int len = vsnprintf(p, SYSLOG_SIZE, fmt, argptr);
-            log_buffer[msg_in_ptr].fifo_len +=  len<SYSLOG_SIZE?len:SYSLOG_SIZE-1;
-			va_end(argptr);
-#endif /*EXTREAM_LOG_TEXT == 0*/
-            p[SYSLOG_SIZE-1] = '\0';
-
-            msg_in_ptr = (msg_in_ptr+1)%syslog_depth;
-            if(NULL != os_related._log_push_cb)
-            {
-                os_related._log_push_cb(NULL);
-            }else{
-                //TBD, error happen, shall report to system
-            }
-
-            rc = true;
-        }
-        os_related._mutex_unlock(fifo_entry_mutex);
+    char tmp_buf[SYSLOG_SIZE];
+    unsigned int rcv_size = 0;
+    int rc = aos_queue_recv(&ulog_queue, AOS_WAIT_FOREVER, tmp_buf, &rcv_size);
+    if ((0 == rc) && (cb != NULL)) {
+        cb(cb_arg, tmp_buf, (uint16_t)rcv_size);
     }
-
-	return rc;
-}
-
-int32_t uring_fifo_pop( void* const buf, const uint16_t _len)
-{
-	int32_t rc = -1;
-	if( (buf!=NULL) && (_len!=0) ) {
-        if (msg_in_ptr!=msg_out_ptr){
-            rc = log_buffer[msg_out_ptr].fifo_len<_len?log_buffer[msg_out_ptr].fifo_len:_len;
-            memcpy(buf,log_buffer[msg_out_ptr].fifo_text,rc);
-            msg_out_ptr = (msg_out_ptr+1)%syslog_depth;
-        } else {
-            rc = 0;
-        }
-	}
     return rc;
 }
-
-int32_t uring_fifo_pop_cb(pop_callback cb, void* cb_arg)
-{
-	int32_t rc = -1;
-	if( (cb!=NULL)&&(msg_in_ptr!=msg_out_ptr) ) {
-        cb(cb_arg, log_buffer[msg_out_ptr].fifo_text, log_buffer[msg_out_ptr].fifo_len);
-        msg_out_ptr = (msg_out_ptr+1)%syslog_depth;
-	}
-    return rc;
-}
-
-
