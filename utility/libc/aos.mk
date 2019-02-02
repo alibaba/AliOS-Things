d6e1fe8694328825718dbd7e77a097820c6c9d1f
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-02-02 16:44:01 +0800

    BugID:16846667: remove armcc_libc_uspace.c and newlib_stub_uspace.c
    Change-Id: Ib4807cdd94ee3430e4ed6d5a30655ba3706e1883

--- utility/libc/aos.mk
+++ utility/libc/aos.mk
@@ -7,15 +7,7 @@ $(NAME)_MBINS_TYPE := share
 $(NAME)_COMPONENTS += vfs
 
 ifeq ($(COMPILER),armcc)
-ifeq ($(MBINS),app)
-ifeq ($(ENABLE_USPACE),1)
-$(NAME)_SOURCES := compilers/armlibc/armcc_libc_uspace.c
-else
 $(NAME)_SOURCES := compilers/armlibc/armcc_libc.c
-endif
-else
-$(NAME)_SOURCES := compilers/armlibc/armcc_libc.c
-endif
 GLOBAL_INCLUDES += compilers/armlibc
 else ifeq ($(COMPILER),rvct)
 $(NAME)_SOURCES := compilers/armlibc/armcc_libc.c
@@ -28,15 +20,7 @@ $(NAME)_SOURCES := mips_rda_stub.c
 else ifeq ($(HOST_MCU_FAMILY),mcu_e310)
 $(NAME)_SOURCES := newlib_riscv_stub.c
 else ifneq ($(HOST_MCU_FAMILY),mcu_linuximpl)
-ifeq ($(MBINS),app)
-ifeq ($(ENABLE_USPACE),1)
-$(NAME)_SOURCES := newlib_stub_uspace.c
-else
 $(NAME)_SOURCES := newlib_stub.c
-endif
-else
-$(NAME)_SOURCES := newlib_stub.c
-endif
 endif
 
 ifeq ($(IDE),keil)
