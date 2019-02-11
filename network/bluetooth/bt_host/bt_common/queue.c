4a99b0207b9ad96d39c4069c1cb226780284fdae
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-02-11 18:16:44 +0800

    BugID:18934873:Opt ble tx sync logic and poll uses local sem
    Change-Id: I19c8eb15aadf300b518e74429ce96b0e74ddebcb

--- network/bluetooth/bt_host/bt_common/queue.c
+++ network/bluetooth/bt_host/bt_common/queue.c
@@ -7,31 +7,20 @@
 #include <misc/dlist.h>
 #include "queue.h"
 
-// TODO: rm this tmp code
-#include "net/buf.h"
-struct cmd_data {
-    u8_t type;
-    u8_t status;
-    u16_t opcode;
-    u8_t sync;
-};
-#define cmd(buf) ((struct cmd_data *)net_buf_user_data(buf))
-extern struct k_sem g_poll_sem;
-
 void k_queue_init(struct k_queue *queue)
 {
     sys_slist_init(&queue->data_q);
     sys_dlist_init(&queue->poll_events);
 }
 
-static inline void handle_poll_events(struct k_queue *queue, u32_t state, uint8_t sync)
+static inline void handle_poll_events(struct k_queue *queue, u32_t state)
 {
-    _handle_obj_poll_events(&queue->poll_events, state, sync);
+    _handle_obj_poll_events(&queue->poll_events, state);
 }
 
 void k_queue_cancel_wait(struct k_queue *queue)
 {
-    handle_poll_events(queue, K_POLL_STATE_NOT_READY, 0);
+    handle_poll_events(queue, K_POLL_STATE_NOT_READY);
 }
 
 static void queue_insert(struct k_queue *queue, void *prev, void *data)
@@ -40,7 +29,7 @@ static void queue_insert(struct k_queue *queue, void *prev, void *data)
 
     node->next = NULL;
     sys_slist_insert(&queue->data_q, prev, data);
-    handle_poll_events(queue, K_POLL_STATE_DATA_AVAILABLE, cmd(data)->sync);
+    handle_poll_events(queue, K_POLL_STATE_DATA_AVAILABLE);
 }
 
 void k_queue_insert(struct k_queue *queue, void *prev, void *data)
@@ -60,34 +49,13 @@ void k_queue_prepend(struct k_queue *queue, void *data)
 
 void k_queue_append_list(struct k_queue *queue, void *head, void *tail)
 {
-    uint8_t sync = cmd(head)->sync;
-
-    if (sync == 0) {
-        sync = 2;
-    }
     sys_slist_append_list(&queue->data_q, head, tail);
-    handle_poll_events(queue, K_POLL_STATE_DATA_AVAILABLE, sync);
-    if (sync == 2) {
-        k_sem_give(&g_poll_sem);
-    }
+    handle_poll_events(queue, K_POLL_STATE_DATA_AVAILABLE);
 }
 
-extern void wait_for_event_done(int timeout);
 void *k_queue_get(struct k_queue *queue, s32_t timeout)
 {
-    void *buf = sys_slist_get(&queue->data_q);
-    uint32_t start_ms = k_uptime_get_32();
-
-    if (timeout == 0) {
-        return buf;
-    }
-
-    while (buf == NULL &&
-           ((timeout > 0 && (timeout >= (k_uptime_get_32() - start_ms))) || (timeout == K_FOREVER))) {
-        wait_for_event_done(timeout);
-        buf = sys_slist_get(&queue->data_q);
-    }
-    return buf;
+    return sys_slist_get(&queue->data_q);
 }
 
 int k_queue_is_empty(struct k_queue *queue)
