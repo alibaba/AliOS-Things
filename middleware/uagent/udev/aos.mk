0268948bf7cdb80ee30ae712f57afb084ca2c868
jiangang.fjg <jiangang.fjg@alibaba-inc.com>
2019-02-13 19:10:41 +0800

    BugID:18955931: fix multi-bin compilation of developerkit board
    Change-Id: Ibe649459218cc9e4b7c37d91b17827c5f6e3d08d

--- middleware/uagent/udev/aos.mk
+++ middleware/uagent/udev/aos.mk
@@ -2,7 +2,7 @@ NAME := udev
 
 $(NAME)_VERSION := 0.1.0
 $(NAME)_SUMMARY := developerment over the air
-$(NAME)_MBINS_TYPE := app
+$(NAME)_MBINS_TYPE := kernel
 
 $(NAME)_SOURCES     := udev.c udev_net.c udev_mqtt_client.c
 
