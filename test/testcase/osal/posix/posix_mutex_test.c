7bfb33935c30c7086d23ab15d47e4496f8ecfa9a
崇龄 <yj144830@alibaba-inc.com>
2019-02-14 11:02:03 +0800

    BugID:17920924:add pthread attr support and testcase 3
    Change-Id: I10b1b9a503035df085f7059847bc17bbf09f6dd6

--- test/testcase/osal/posix/posix_mutex_test.c
+++ test/testcase/osal/posix/posix_mutex_test.c
@@ -7,6 +7,7 @@
 
 #include "k_api.h"
 #include "posix/pthread.h"
+#include "ulog/ulog.h"
 
 static pthread_mutex_t mutex;
 
@@ -19,11 +20,22 @@ static void *init_task(void *arg)
 
 static void *demo_task1(void *arg)
 {
-    int count = 0;
+    int   count  = 0;
+    void *status = NULL;
 
     while (1) {
         pthread_mutex_lock(&mutex);
-        printf("demo_task1 get sem %d\n", count++);
+
+        LOGI(TAG, "demo_task1 get mutex %d\n", count);
+        krhino_task_sleep(RHINO_CONFIG_TICKS_PER_SECOND * 5);
+
+        pthread_mutex_unlock(&mutex);
+
+        if (count == 5) {
+            pthread_exit(status);
+        }
+
+        count++;
     };
 
     return NULL;
@@ -31,22 +43,85 @@ static void *demo_task1(void *arg)
 
 static void *demo_task2(void *arg)
 {
-    int count = 0;
+    int   count  = 0;
+    void *status = NULL;
 
     while (1) {
-        pthread_mutex_unlock(&mutex);
-        printf("demo_task2 release sem %d\n", count++);
+        pthread_mutex_lock(&mutex);
+
+        LOGI(TAG, "demo_task2 get mutex %d\n", count);
         krhino_task_sleep(RHINO_CONFIG_TICKS_PER_SECOND);
+
+        pthread_mutex_unlock(&mutex);
+
+        if (count == 10) {
+            pthread_exit(status);
+        }
+
+        count++;
     };
 
     return NULL;
 }
 
+/*
+The correct serial port print data when executing this test case is as follows：
+
+posix_mutex_case start !
+demo_task1 get mutex 0
+(interval 5s)
+demo_task2 get mutex 0
+(interval 1s)
+demo_task1 get mutex 1
+(interval 5s)
+demo_task2 get mutex 1
+(interval 1s)
+demo_task1 get mutex 2
+(interval 5s)
+demo_task2 get mutex 2
+(interval 1s)
+demo_task1 get mutex 3
+(interval 5s)
+demo_task2 get mutex 3
+(interval 1s)
+demo_task1 get mutex 4
+(interval 5s)
+demo_task2 get mutex 4
+(interval 1s)
+demo_task2 get mutex 5
+(interval 1s)
+demo_task2 get mutex 6
+(interval 1s)
+demo_task2 get mutex 7
+(interval 1s)
+demo_task2 get mutex 8
+(interval 1s)
+demo_task2 get mutex 9
+(interval 1s)
+posix_mutex_case end !
+*/
 void posix_mutex_case(void)
 {
-    pthread_t thread;
+    int   ret    = -1;
+    void *status = NULL;
 
-    pthread_create(&thread, NULL, init_task, NULL);
-    pthread_create(&thread, NULL, demo_task1, NULL);
-    pthread_create(&thread, NULL, demo_task2, NULL);
+    pthread_t thread1;
+    pthread_t thread2;
+    pthread_t thread3;
+
+    LOGI(TAG, "posix_mutex_case start !\n");
+
+    ret = pthread_create(&thread1, NULL, init_task, NULL);
+    ret |= pthread_create(&thread2, NULL, demo_task1, NULL);
+    ret |= pthread_create(&thread3, NULL, demo_task2, NULL);
+
+    if (ret == 0) {
+        ret = pthread_join(thread1, &status);
+        ret = pthread_join(thread2, &status);
+        ret = pthread_join(thread3, &status);
+    }
+
+    pthread_mutex_destroy(&mutex);
+
+    LOGI(TAG, "posix_mutex_case end !\n");
 }
--- test/testcase/osal/posix/posix_test.c
+++ test/testcase/osal/posix/posix_test.c
@@ -2,12 +2,36 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-extern void posix_task_case1(void);
-extern void posix_task_case2(void);
-extern void posix_task_case3(void);
-extern void posix_task_case4(void);
-extern void posix_task_case5(void);
-extern void posix_task_case6(void);
+extern void posix_pthread_create_case(void);
+extern void posix_pthread_join_case(void);
+extern void posix_pthread_cleanup_case1(void);
+extern void posix_pthread_cleanup_case2(void);
+extern void posix_pthread_detach_case1(void);
+extern void posix_pthread_detach_case2(void);
+extern void posix_pthread_once_case(void);
+extern void posix_pthread_setschedparam_case1(void);
+extern void posix_pthread_setschedparam_case2(void);
+extern void posix_pthread_setschedprio_case(void);
+
+extern void posix_pthread_attr_init_case(void);
+extern void posix_pthread_attr_destroy_case(void);
+extern void posix_pthread_attr_setdetachstate_case(void);
+extern void posix_pthread_attr_getdetachstate_case(void);
+extern void posix_pthread_attr_setschedpolicy_case(void);
+extern void posix_pthread_attr_getschedpolicy_case(void);
+extern void posix_pthread_attr_setschedparam_case(void);
+extern void posix_pthread_attr_getschedparam_case(void);
+extern void posix_pthread_attr_setstacksize_case(void);
+extern void posix_pthread_attr_getstacksize_case(void);
+extern void posix_pthread_attr_setstackaddr_case(void);
+extern void posix_pthread_attr_getstackaddr_case(void);
+extern void posix_pthread_attr_setstack_case(void);
+extern void posix_pthread_attr_getstack_case(void);
+extern void posix_pthread_attr_setinheritsched_case(void);
+extern void posix_pthread_attr_getinheritsched_case(void);
+extern void posix_pthread_attr_setscope_case(void);
+extern void posix_pthread_attr_getscope_case(void);
+
 extern void posix_sem_case1(void);
 extern void posix_sem_case2(void);
 extern void posix_mutex_case(void);
@@ -16,10 +40,11 @@ extern void posix_tsd_case(void);
 
 void posix_test_entry(void)
 {
-    posix_task_case1();
-    posix_task_case2();
-    posix_task_case3();
-    posix_task_case4();
-    posix_sem_case1();
-    posix_sem_case2();
+    posix_pthread_create_case();
+    posix_pthread_join_case();
+    posix_pthread_cleanup_case1();
+    posix_pthread_cleanup_case2();
+    posix_pthread_detach_case1();
+    posix_pthread_detach_case2();
+    posix_pthread_once_case();
 }
