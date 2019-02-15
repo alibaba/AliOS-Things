bbc7a44de87ed72643da97beabc612e9a33abbcf
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-02-15 08:54:01 +0800

    BugID:18961962:Add ble host config to each board
    Change-Id: Ic87f24cbe1174a913c6801bc5b419ff717565bef

---
+++ board/pca10040e/ble_config.h
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef BLE_CONFIG_H
+#define BLE_CONFIG_H
+
+/**
+ * CONFIG_BT: Tx thread stack size
+ */
+
+#ifndef CONFIG_BT_HCI_TX_STACK_SIZE
+#define CONFIG_BT_HCI_TX_STACK_SIZE 290
+#endif
+
+#ifndef CONFIG_BT_L2CAP_TX_BUF_COUNT
+#define CONFIG_BT_L2CAP_TX_BUF_COUNT 2
+#endif
+
+#ifndef CONFIG_BT_RX_BUF_COUNT
+#define CONFIG_BT_RX_BUF_COUNT 2
+#endif
+
+#ifndef CONFIG_BT_MAX_CONN
+#define CONFIG_BT_MAX_CONN 1
+#endif
+
+#define CONFIG_BT_RECV_IS_RX_THREAD
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* BLE_DEF_CONFIG_H */
---
+++ board/pca10056/ble_config.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef BLE_CONFIG_H
+#define BLE_CONFIG_H
+
+/**
+ * CONFIG_BT: Tx thread stack size
+ */
+
+#ifndef CONFIG_BT_HCI_TX_STACK_SIZE
+#define CONFIG_BT_HCI_TX_STACK_SIZE 290
+#endif
+
+#define CONFIG_BT_RECV_IS_RX_THREAD
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* BLE_DEF_CONFIG_H */
