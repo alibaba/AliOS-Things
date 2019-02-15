0e13a3e21aef9aab929145e42b328010e4e09b66
崇龄 <yj144830@alibaba-inc.com>
2019-02-15 17:31:28 +0800

    BugID:17920924:add support of mutex cond attr 3
    Change-Id: I6246524ef72aeddc3e2cbd9946f83ba746c13bf3

--- test/testcase/osal/posix/posix_timer_test.c
+++ test/testcase/osal/posix/posix_timer_test.c
@@ -4,6 +4,7 @@
 
 #include "posix/timer.h"
 #include "posix/semaphore.h"
+#include "ulog/ulog.h"
 
 timer_t timer1_id;
 sem_t   sem;
@@ -20,23 +21,23 @@ void timer1_thread(void)
 
     static int count = 0;
 
-    printf("timer1_thread is running !\n");
+    LOGI(TAG, "timer1_thread is running !\n");
 
-    ret = clock_gettime(CLOCK_REALTIME, &timer_spec_get_cur);
+    ret = clock_gettime(CLOCK_MONOTONIC, &timer_spec_get_cur);
     if (ret != 0) {
         test_failed_flag = 1;
-        printf("clock_gettime error !\n");
+        LOGI(TAG, "clock_gettime error !\n");
     }
 
     if (count == 0) {
         if ((timer_spec_get_cur.tv_sec - timer_spec_get_origin.tv_sec) != 3) {
             test_failed_flag = 1;
-            printf("timer it_value error !\n");
+            LOGI(TAG, "timer it_value error !\n");
         }
     } else {
         if ((timer_spec_get_cur.tv_sec - timer_spec_get_pre.tv_sec) != 2) {
             test_failed_flag = 1;
-            printf("timer it_interval error !\n");
+            LOGI(TAG, "timer it_interval error !\n");
         }
     }
 
@@ -44,13 +45,13 @@ void timer1_thread(void)
         ret = timer_delete(timer1_id);
         if (ret != 0) {
             test_failed_flag = 1;
-            printf("timer_delete error !\n");
+            LOGI(TAG, "timer_delete error !\n");
         }
 
         ret = sem_post(&sem);
         if (ret != 0) {
             test_failed_flag = 1;
-            printf("sem_init error !\n");
+            LOGI(TAG, "sem_init error !\n");
         }
     }
 
@@ -68,12 +69,12 @@ void posix_timer_case1(void)
     struct sigevent   timer1_sig;
     struct timespec   timer_spec_buf;
 
-    printf("*********** posix_timer_case1 start ***********\n");
+    LOGI(TAG, "*********** posix_timer_case1 start ***********\n");
 
     ret = sem_init(&sem, 0, 0);
     if (ret != 0) {
         test_failed_flag = 1;
-        printf("sem_init error !\n");
+        LOGI(TAG, "sem_init error !\n");
     }
 
     memset(&timer_spec_get_origin, 0, sizeof(struct timespec));
@@ -83,10 +84,10 @@ void posix_timer_case1(void)
     timer1_sig.sigev_notify_function = timer1_thread;
 
     /* create the timer */
-    ret = timer_create(CLOCK_REALTIME, &timer1_sig, &timer1_id);
+    ret = timer_create(CLOCK_MONOTONIC, &timer1_sig, &timer1_id);
     if (ret != 0) {
         test_failed_flag = 1;
-        printf("timer_settime error !\n");
+        LOGI(TAG, "timer_settime error !\n");
     }
 
     /* run for the first time after 3 seconds, then run every 2 seconds */
@@ -99,7 +100,7 @@ void posix_timer_case1(void)
     ret = timer_settime(timer1_id, 0, &it, NULL);
     if (ret != 0) {
         test_failed_flag = 1;
-        printf("timer_settime error !\n");
+        LOGI(TAG, "timer_settime error !\n");
     }
 
     /* get the timer value */
@@ -107,27 +108,27 @@ void posix_timer_case1(void)
     if (ret == 0) {
         if ((timer_spec_buf.tv_sec != 2) || (timer_spec_buf.tv_nsec != 0)) {
             test_failed_flag = 1;
-            printf("timer_gettime error !\n");
+            LOGI(TAG, "timer_gettime error !\n");
         }
     } else {
         test_failed_flag = 1;
-        printf("timer_gettime error !\n");
+        LOGI(TAG, "timer_gettime error !\n");
     }
 
-    ret = clock_gettime(CLOCK_REALTIME, &timer_spec_get_origin);
+    ret = clock_gettime(CLOCK_MONOTONIC, &timer_spec_get_origin);
     if (ret != 0) {
         test_failed_flag = 1;
-        printf("clock_gettime error !\n");
+        LOGI(TAG, "clock_gettime error !\n");
     }
 
     sem_wait(&sem);
     sem_destroy(&sem);
 
     if (test_failed_flag == 0) {
-        printf("posix_timer_case1 test OK !\n");
+        LOGI(TAG, "posix_timer_case1 test OK !\n");
     }
 
-    printf("*********** posix_timer_case1 end ***********\n\n");
+    LOGI(TAG, "*********** posix_timer_case1 end ***********\n\n");
 }
 
 /* testcase for clock_gettime/clock_nanosleep/nanosleep/sleep */
@@ -140,41 +141,41 @@ void posix_timer_case2(void)
     struct timespec timer_spec_get2;
     struct timespec timer_spec_set;
 
-    printf("*********** posix_timer_case2 start ***********\n");
+    LOGI(TAG, "*********** posix_timer_case2 start ***********\n");
 
-    ret = clock_gettime(CLOCK_REALTIME, &timer_spec_get1);
+    ret = clock_gettime(CLOCK_MONOTONIC, &timer_spec_get1);
     if (ret != 0) {
-        printf("clock_gettime error !\n");
+        LOGI(TAG, "clock_gettime error !\n");
         return;
     }
 
     timer_spec_set.tv_sec = 2;
     timer_spec_set.tv_nsec = 0;
-    clock_nanosleep(CLOCK_REALTIME, 0, &timer_spec_set, NULL);
+    clock_nanosleep(CLOCK_MONOTONIC, 0, &timer_spec_set, NULL);
 
-    ret = clock_gettime(CLOCK_REALTIME, &timer_spec_get2);
+    ret = clock_gettime(CLOCK_MONOTONIC, &timer_spec_get2);
     if (ret != 0) {
-        printf("clock_gettime error !\n");
+        LOGI(TAG, "clock_gettime error !\n");
         return;
     }
 
     seconds = timer_spec_get2.tv_sec - timer_spec_get1.tv_sec;
     if (seconds != 2) {
-        printf("clock_nanosleep error !\n");
+        LOGI(TAG, "clock_nanosleep error !\n");
         return;
     }
 
     sleep(3);
 
-    ret = clock_gettime(CLOCK_REALTIME, &timer_spec_get2);
+    ret = clock_gettime(CLOCK_MONOTONIC, &timer_spec_get2);
     if (ret != 0) {
-        printf("clock_gettime error !\n");
+        LOGI(TAG, "clock_gettime error !\n");
         return;
     }
 
     seconds = timer_spec_get2.tv_sec - timer_spec_get1.tv_sec;
     if (seconds != 5) {
-        printf("clock_nanosleep error !\n");
+        LOGI(TAG, "clock_nanosleep error !\n");
         return;
     }
 
@@ -182,21 +183,21 @@ void posix_timer_case2(void)
     timer_spec_set.tv_nsec = 0;
     nanosleep(&timer_spec_set, NULL);
 
-    ret = clock_gettime(CLOCK_REALTIME, &timer_spec_get2);
+    ret = clock_gettime(CLOCK_MONOTONIC, &timer_spec_get2);
     if (ret != 0) {
-        printf("clock_gettime error !\n");
+        LOGI(TAG, "clock_gettime error !\n");
         return;
     }
 
     seconds = timer_spec_get2.tv_sec - timer_spec_get1.tv_sec;
     if (seconds != 7) {
-        printf("nanosleep error !\n");
+        LOGI(TAG, "nanosleep error !\n");
         return;
     }
 
-    printf("posix_timer_case2 test OK !\n");
+    LOGI(TAG, "posix_timer_case2 test OK !\n");
 
-    printf("*********** posix_timer_case2 end ***********\n\n");
+    LOGI(TAG, "*********** posix_timer_case2 end ***********\n\n");
 }
 
 void posix_timer_test(void)
