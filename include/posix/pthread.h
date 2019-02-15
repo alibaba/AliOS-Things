315efd74be8b123773fe3177585918692432f8a0
崇龄 <yj144830@alibaba-inc.com>
2019-02-15 17:30:23 +0800

    BugID:17920924:add support of mutex cond attr 1
    Change-Id: Ifc69c1ff90515c907931f7219d8ac0e24b696bb3

--- include/posix/pthread.h
+++ include/posix/pthread.h
@@ -16,9 +16,6 @@ extern "C"
 #include "k_api.h"
 #include "posix_config.h"
 
-typedef ktask_t *pthread_t;
-typedef int      pthread_once_t;
-
 /* the value of contentionscope in struct pthread_attr_t */
 #define PTHREAD_SCOPE_PROCESS   0
 #define PTHREAD_SCOPE_SYSTEM    1
@@ -48,6 +45,9 @@ typedef int      pthread_once_t;
 #define DEFAULT_THREAD_PRIORITY   30
 #define DEFAULT_THREAD_SLICE      10
 
+#define PTHREAD_INITIALIZED_OBJ 0xABCDEFEF
+#define PTHREAD_ONCE_INIT       0
+
 enum {
     PTHREAD_CANCEL_ASYNCHRONOUS,
     PTHREAD_CANCEL_ENABLE,
@@ -56,7 +56,9 @@ enum {
     PTHREAD_CANCELED
 };
 
-typedef int pid_t;
+typedef ktask_t *pthread_t;
+typedef int      pthread_once_t;
+typedef int      pid_t;
 
 struct sched_param {
     int    sched_priority; /* process execution scheduling priority */
@@ -143,6 +145,8 @@ RHINO_INLINE pthread_t _pid_to_pthread(pid_t pid)
     return (pthread_t)pid;
 }
 
+/* function in pthread.c */
+int       pthread_atfork(void (*prepare)(void), void (*parent)(void), void (*child)(void));
 int       pthread_create(pthread_t *thread, const pthread_attr_t *attr,
                          void *(*start_routine)(void *), void *arg);
 void      pthread_exit(void *value_ptr);
@@ -158,12 +162,19 @@ int       pthread_setschedparam(pthread_t thread, int policy, const struct sched
 void      pthread_cleanup_pop(int execute);
 void      pthread_cleanup_push(void (*routine)(void *), void *arg);
 pthread_t pthread_self(void);
+int       pthread_getcpuclockid(pthread_t thread_id, clockid_t *clock_id);
+int       pthread_setconcurrency(int new_level);
+int       pthread_getconcurrency(void);
+int       pthread_setschedprio(pthread_t thread, int prio);
 
+/* function in pthread_sched.c */
 int sched_yield(void);
+int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param);
 int sched_get_priority_min(int policy);
 int sched_get_priority_max(int policy);
-int sched_setscheduler(pid_t pid, int policy, const struct sched_param *param);
+int sched_rr_get_interval(pid_t pid, struct timespec *interval);
 
+/* function in pthread_attr.c */
 int pthread_attr_init(pthread_attr_t *attr);
 int pthread_attr_destroy(pthread_attr_t *attr);
 int pthread_attr_setdetachstate(pthread_attr_t *attr, int detachstate);
@@ -178,38 +189,85 @@ int pthread_attr_setstackaddr(pthread_attr_t *attr, void *stackaddr);
 int pthread_attr_getstackaddr(const pthread_attr_t *attr, void **stackaddr);
 int pthread_attr_setstack(pthread_attr_t *attr, void *stackaddr, size_t stacksize);
 int pthread_attr_getstack(const pthread_attr_t *attr, void **stackaddr, size_t *stacksize);
+int pthread_attr_setinheritsched(pthread_attr_t *attr, int inheritsched);
+int pthread_attr_getinheritsched(const pthread_attr_t *restrict attr, int *restrict inheritsched);
 int pthread_attr_setguardsize(pthread_attr_t *attr, size_t guardsize);
 int pthread_attr_getguardsize(const pthread_attr_t *attr, size_t *guardsize);
 int pthread_attr_setscope(pthread_attr_t *attr, int scope);
 int pthread_attr_getscope(const pthread_attr_t *attr, int *scope);
 
+/* definetion for pthread mutex */
+#define PTHREAD_UNUSED_YET_OBJ -1
+#define PTHREAD_MUTEX_INITIALIZER { NULL, PTHREAD_UNUSED_YET_OBJ }
+
+/*
+ * This type of mutex does not detect deadlock. A thread attempting to
+ * relock this mutex without first unlocking it shall deadlock. Attempting
+ * to unlock a mutex locked by a different thread results in undefined
+ * behavior.  Attempting to unlock an unlocked mutex results in undefined
+ * behavior.
+ */
+#define PTHREAD_MUTEX_NORMAL 0
+
+/*
+ * A thread attempting to relock this mutex without first unlocking
+ * it shall succeed in locking the mutex.  The relocking deadlock which
+ * can occur with mutexes of type PTHREAD_MUTEX_NORMAL cannot occur with
+ * this type of mutex.  Multiple locks of this mutex shall require the
+ * same number of unlocks to release the mutex before another thread can
+ * acquire the mutex. A thread attempting to unlock a mutex which another
+ * thread has locked shall return with an error.  A thread attempting to
+ * unlock an unlocked mutex shall return with an error.
+ */
+#define PTHREAD_MUTEX_RECURSIVE 1
+
+/*
+ * This type of mutex provides error checking. A thread attempting
+ * to relock this mutex without first unlocking it shall return with an
+ * error. A thread attempting to unlock a mutex which another thread has
+ * locked shall return with an error. A thread attempting to unlock an
+ * unlocked mutex shall return with an error.
+ */
+#define PTHREAD_MUTEX_ERRORCHECK 2
+
+/*
+ * Attempting to recursively lock a mutex of this type results
+ * in undefined behavior. Attempting to unlock a mutex of this type
+ * which was not locked by the calling thread results in undefined
+ * behavior. Attempting to unlock a mutex of this type which is not locked
+ * results in undefined behavior. An implementation may map this mutex to
+ * one of the other mutex types.
+ */
+#define PTHREAD_MUTEX_DEFAULT 3
+
+#define PTHREAD_PROCESS_PRIVATE 0 /* visible within only the creating process */
+#define PTHREAD_PROCESS_SHARED  1 /* visible too all processes with access to
+                                     the memory where the resource is located */
+
+#define PTHREAD_PRIO_NONE    0
+#define PTHREAD_PRIO_INHERIT 1
+#define PTHREAD_PRIO_PROTECT 2
+
+#define DEFAULT_MUTEX_TYPE         PTHREAD_MUTEX_DEFAULT
+#define DEFAULT_MUTEX_PROCOCOL     PTHREAD_PRIO_INHERIT
+#define DEFAULT_MUTEX_PRIOCEILING  POSIX_HIGH_PRI
+#define DEFAULT_MUTEX_PSHARED      PTHREAD_PROCESS_PRIVATE
+
 typedef struct pthread_mutexattr {
     int is_initialized;
-    int recursive;
+    int type;
+    int protocol;
+    int prioceiling;
+    int pshared;
 } pthread_mutexattr_t;
 
 typedef struct pthread_mutex {
-    kmutex_t *mutex;
-    int       initted;
+    int                  initted;
+    kmutex_t            *mutex;
+    pthread_mutexattr_t *attr;
 } pthread_mutex_t;
 
-#define PTHREAD_INITIALIZED_OBJ 0xABCDEFEF
-#define PTHREAD_UNUSED_YET_OBJ  -1
-#define PTHREAD_ONCE_INIT       0
-
-#define PTHREAD_MUTEX_INITIALIZER { NULL, PTHREAD_UNUSED_YET_OBJ }
-
-/* Mutex types */
-enum {
-    PTHREAD_MUTEX_NORMAL     = 0, /* Normal mutex        */
-    PTHREAD_MUTEX_RECURSIVE  = 1, /* Recursive mutex     */
-    PTHREAD_MUTEX_ERRORCHECK = 2, /* Error checking mutex*/
-
-    PTHREAD_MUTEX_ERRORCHECK_NP = PTHREAD_MUTEX_ERRORCHECK, /* Error checking mutex*/
-    PTHREAD_MUTEX_RECURSIVE_NP  = PTHREAD_MUTEX_RECURSIVE,  /* Recursive mutex */
-    PTHREAD_MUTEX_DEFAULT       = PTHREAD_MUTEX_NORMAL      /* Default mutex */
-};
-
+/* function in pthread_mutex.c */
 int pthread_mutex_init(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
 int pthread_mutex_destroy(pthread_mutex_t *mutex);
 int pthread_mutex_lock(pthread_mutex_t *mutex);
@@ -223,18 +281,25 @@ int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int type);
 int pthread_mutexattr_setpshared(pthread_mutexattr_t *attr, int pshared);
 int pthread_mutexattr_getpshared(pthread_mutexattr_t *attr, int *pshared);
 
-typedef struct pthread_cond {
-    kmutex_t *lock;
-    int       waiting;
-    int       signals;
-    ksem_t   *wait_sem;
-    ksem_t   *wait_done;
-} pthread_cond_t;
+#define DEFAULT_COND_CLOCK  CLOCK_REALTIME
+#define DEFAULT_COND_SHARED PTHREAD_PROCESS_PRIVATE
 
 typedef struct pthread_condattr {
-    int __dummy;
+  int     is_initialized;
+  clock_t clock;   /* specifiy clock for timeouts */
+  int     pshared; /* allow this to be shared amongst processes */
 } pthread_condattr_t;
 
+typedef struct pthread_cond {
+    kmutex_t          *lock;
+    int                waiting;
+    int                signals;
+    ksem_t             *wait_sem;
+    ksem_t             *wait_done;
+    pthread_condattr_t *attr;
+} pthread_cond_t;
+
+/* function in pthread_cond.c */
 int pthread_condattr_init(pthread_condattr_t *attr);
 int pthread_condattr_destroy(pthread_condattr_t *attr);
 int pthread_cond_init(pthread_cond_t *cond, const pthread_condattr_t *attr);
@@ -271,6 +336,7 @@ typedef struct pthread_key_list_s {
     struct pthread_key_list_s *next;
 } pthread_key_list_t;
 
+/* function in pthread_tsd.c */
 int   pthread_key_create(pthread_key_t *key, void (*destructor)(void*));
 int   pthread_setspecific(pthread_key_t key, const void *value);
 void *pthread_getspecific(pthread_key_t key);
