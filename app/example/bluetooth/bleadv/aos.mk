2ac0bd88e9e2bf192dde07e362bfa861d9e35779
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-02-15 08:59:35 +0800

    BugID:18961962:Rm bleadv yloop dependency
    Change-Id: I86ffe6e5cb15156b5496fea351750f5bbb18390e

--- app/example/bluetooth/bleadv/aos.mk
+++ app/example/bluetooth/bleadv/aos.mk
@@ -8,7 +8,7 @@ $(NAME)_SOURCES     := main.c
 ble = 1
 en_bt_smp = 1
 
-$(NAME)_COMPONENTS  += bt_host yloop cli
+$(NAME)_COMPONENTS  += bt_host cli
 GLOBAL_DEFINES      += AOS_NO_WIFI
 
 GLOBAL_INCLUDES += ../
