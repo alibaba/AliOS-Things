5cd4f465e7aeff69caba03943669e441a41e0d6c
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-02-19 13:55:10 +0800

    bugID:18002625:refine Breezeapp menuconfig, enable OTA in default settings.
    Change-Id: Ic60e1e8549f3f3b8a0bbb69b47cce78b00800761

--- app/example/bluetooth/breezeapp/aos.mk
+++ app/example/bluetooth/breezeapp/aos.mk
@@ -5,32 +5,25 @@ $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := breezeapp
 $(NAME)_SOURCES := breezeapp.c
 
-ble = 1
-bz_en_auth = 1
-bz_en_awss = 0
 
 $(NAME)_COMPONENTS := breeze cli
 
 GLOBAL_DEFINES += DEBUG
 GLOBAL_DEFINES += BUILD_AOS
 
-BREEZEAPP_CONFIG_EN_OTA ?= 0
-bz_en_ota ?= $(BREEZEAPP_CONFIG_EN_OTA)
-ifeq ($(bz_en_ota),1)
-ifeq ($(bz_en_auth), 0)
-$(error OTA need authentication, please set "bz_en_auth = 1")
+ifeq ($(CONFIG_COMP_BZ_OTA), y)
+ifneq ($(CONFIG_COMP_BZ_EN_AUTH), y)
+$(error OTA need authentication, please set "CONFIG_COMP_BZ_EN_AUTH = y")
 endif
 GLOBAL_DEFINES += CONFIG_AIS_OTA
 $(NAME)_COMPONENTS += ota_ble
 endif
 
-bz_secure_adv ?= 0
-ifeq ($(bz_secure_adv),1)
+ifeq ($(CONFIG_COMP_BZ_SECURE_ADV), y)
 GLOBAL_DEFINES += CONTINUE_BEL_ADV
 endif
 
-bz_model_encry ?= 0
-ifeq ($(bz_model_encry), 1)
+ifeq ($(CONFIG_COMP_BZ_MODEL_SEC), y)
 GLOBAL_DEFINES += CONFIG_MODEL_SECURITY
 endif
 
