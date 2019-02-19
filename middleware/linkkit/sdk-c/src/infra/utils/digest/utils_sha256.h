98353ff5cebfc6181fe910a757938c27d65f9815
zhangtian <dongren.zt@alibaba-inc.com>
2019-02-19 11:08:11 +0800

    bugID:18992508:[utils_sha256] fix sha256 do not work in rtos issue
    Change-Id: Ib507d09f2a61bbe074b30e0853ec183a3dae6078

--- middleware/linkkit/sdk-c/src/infra/utils/digest/utils_sha256.h
+++ middleware/linkkit/sdk-c/src/infra/utils/digest/utils_sha256.h
@@ -10,16 +10,23 @@
 
 #include "iot_import.h"
 
-#define SHA256_BLOCK_LENGTH     64
-#define SHA256_DIGEST_LENGTH    32
-#define SHA256_DIGEST_STRING_LENGTH (SHA256_DIGEST_LENGTH * 2 + 1)
+#include <stdint.h>
 
-#define SHA256_SHORT_BLOCK_LENGTH   (SHA256_BLOCK_LENGTH - 8)
+#define SHA256_DIGEST_LENGTH            (32)
+#define SHA256_BLOCK_LENGTH             (64)
+#define SHA256_SHORT_BLOCK_LENGTH       (SHA256_BLOCK_LENGTH - 8)
+#define SHA256_DIGEST_STRING_LENGTH     (SHA256_DIGEST_LENGTH * 2 + 1)
+
+/**
+ * \brief          SHA-256 context structure
+ */
 typedef struct {
-    uint32_t state[8];
-    uint64_t bitcount;
-    unsigned char buffer[SHA256_BLOCK_LENGTH];
+    uint32_t total[2];          /*!< number of bytes processed  */
+    uint32_t state[8];          /*!< intermediate digest state  */
+    unsigned char buffer[64];   /*!< data block being processed */
+    int is224;                  /*!< 0 => SHA-256, else SHA-224 */
 } iot_sha256_context;
+
 typedef union {
     char sptr[8];
     uint64_t lint;
@@ -62,7 +69,7 @@ void utils_sha256_starts(iot_sha256_context *ctx);
  * \param input    buffer holding the  data
  * \param ilen     length of the input data
  */
-void utils_sha256_update(iot_sha256_context *ctx, const unsigned char *input, size_t ilen);
+void utils_sha256_update(iot_sha256_context *ctx, const unsigned char *input, uint32_t ilen);
 
 /**
  * \brief          SHA-256 final digest
@@ -70,10 +77,10 @@ void utils_sha256_update(iot_sha256_context *ctx, const unsigned char *input, si
  * \param ctx      SHA-256 context
  * \param output   SHA-256 checksum result
  */
-void utils_sha256_finish(iot_sha256_context *ctx, unsigned char output[32]);
+void utils_sha256_finish(iot_sha256_context *ctx, uint8_t output[32]);
 
 /* Internal use */
-void utils_sha256_process(iot_sha256_context *ctx, const uint32_t *data);
+void utils_sha256_process(iot_sha256_context *ctx, const unsigned char data[64]);
 
 /**
  * \brief          Output = SHA-256( input buffer )
@@ -82,6 +89,7 @@ void utils_sha256_process(iot_sha256_context *ctx, const uint32_t *data);
  * \param ilen     length of the input data
  * \param output   SHA-256 checksum result
  */
-void utils_sha256(const unsigned char *input, size_t ilen, unsigned char output[32]);
+void utils_sha256(const uint8_t *input, uint32_t ilen, uint8_t output[32]);
+
 
 #endif
