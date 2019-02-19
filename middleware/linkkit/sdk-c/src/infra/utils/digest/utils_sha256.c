8704007e7b261971fdf193246de7bd6a5a988dd6
zhangtian <dongren.zt@alibaba-inc.com>
2019-02-19 11:08:11 +0800

    bugID:18992508:[utils_sha256] fix sha256 do not work in rtos issue
    Change-Id: Ib507d09f2a61bbe074b30e0853ec183a3dae6078

--- middleware/linkkit/sdk-c/src/infra/utils/digest/utils_sha256.c
+++ middleware/linkkit/sdk-c/src/infra/utils/digest/utils_sha256.c
@@ -2,113 +2,39 @@
  * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
 #ifdef UTILS_SHA256
-
 #include <stdlib.h>
 #include <string.h>
-#include "iot_import.h"
-#include "iotx_log.h"
+#include <stdint.h>
 #include "utils_sha256.h"
 
-/* Shift-right (used in SHA-256, SHA-384, and SHA-512): */
-#define R(b,x)      ((x) >> (b))
-/* 32-bit Rotate-right (used in SHA-256): */
-#define _S32(b,x)   (((x) >> (b)) | ((x) << (32 - (b))))
+#define SHA256_KEY_IOPAD_SIZE (64)
+#define SHA256_DIGEST_SIZE    (32)
 
-/* Two of six logical functions used in SHA-256, SHA-384, and SHA-512: */
-#define Ch(x,y,z)   (((x) & (y)) ^ ((~(x)) & (z)))
-#define Maj(x,y,z)  (((x) & (y)) ^ ((x) & (z)) ^ ((y) & (z)))
+/*
+ * 32-bit integer manipulation macros (big endian)
+ */
+#ifndef GET_UINT32_BE
+#define GET_UINT32_BE(n,b,i)                            \
+    do {                                                    \
+        (n) = ( (uint32_t) (b)[(i)    ] << 24 )             \
+              | ( (uint32_t) (b)[(i) + 1] << 16 )             \
+              | ( (uint32_t) (b)[(i) + 2] <<  8 )             \
+              | ( (uint32_t) (b)[(i) + 3]       );            \
+    } while( 0 )
+#endif
 
-/* Four of six logical functions used in SHA-256: */
-#define Sigma0_256(x)   (_S32(2,  (x)) ^ _S32(13, (x)) ^ _S32(22, (x)))
-#define Sigma1_256(x)   (_S32(6,  (x)) ^ _S32(11, (x)) ^ _S32(25, (x)))
-#define sigma0_256(x)   (_S32(7,  (x)) ^ _S32(18, (x)) ^ R(3 ,   (x)))
-#define sigma1_256(x)   (_S32(17, (x)) ^ _S32(19, (x)) ^ R(10,   (x)))
+#ifndef PUT_UINT32_BE
+#define PUT_UINT32_BE(n,b,i)                            \
+    do {                                                    \
+        (b)[(i)    ] = (unsigned char) ( (n) >> 24 );       \
+        (b)[(i) + 1] = (unsigned char) ( (n) >> 16 );       \
+        (b)[(i) + 2] = (unsigned char) ( (n) >>  8 );       \
+        (b)[(i) + 3] = (unsigned char) ( (n)       );       \
+    } while( 0 )
+#endif
 
-/* Hash constant words K for SHA-256: */
-const static uint32_t K256[64] = {
-    0x428a2f98UL, 0x71374491UL, 0xb5c0fbcfUL, 0xe9b5dba5UL,
-    0x3956c25bUL, 0x59f111f1UL, 0x923f82a4UL, 0xab1c5ed5UL,
-    0xd807aa98UL, 0x12835b01UL, 0x243185beUL, 0x550c7dc3UL,
-    0x72be5d74UL, 0x80deb1feUL, 0x9bdc06a7UL, 0xc19bf174UL,
-    0xe49b69c1UL, 0xefbe4786UL, 0x0fc19dc6UL, 0x240ca1ccUL,
-    0x2de92c6fUL, 0x4a7484aaUL, 0x5cb0a9dcUL, 0x76f988daUL,
-    0x983e5152UL, 0xa831c66dUL, 0xb00327c8UL, 0xbf597fc7UL,
-    0xc6e00bf3UL, 0xd5a79147UL, 0x06ca6351UL, 0x14292967UL,
-    0x27b70a85UL, 0x2e1b2138UL, 0x4d2c6dfcUL, 0x53380d13UL,
-    0x650a7354UL, 0x766a0abbUL, 0x81c2c92eUL, 0x92722c85UL,
-    0xa2bfe8a1UL, 0xa81a664bUL, 0xc24b8b70UL, 0xc76c51a3UL,
-    0xd192e819UL, 0xd6990624UL, 0xf40e3585UL, 0x106aa070UL,
-    0x19a4c116UL, 0x1e376c08UL, 0x2748774cUL, 0x34b0bcb5UL,
-    0x391c0cb3UL, 0x4ed8aa4aUL, 0x5b9cca4fUL, 0x682e6ff3UL,
-    0x748f82eeUL, 0x78a5636fUL, 0x84c87814UL, 0x8cc70208UL,
-    0x90befffaUL, 0xa4506cebUL, 0xbef9a3f7UL, 0xc67178f2UL
-};
-/* Initial hash value H for SHA-256: */
-const static uint32_t sha256_initial_hash_value[8] = {
-    0x6a09e667UL,
-    0xbb67ae85UL,
-    0x3c6ef372UL,
-    0xa54ff53aUL,
-    0x510e527fUL,
-    0x9b05688cUL,
-    0x1f83d9abUL,
-    0x5be0cd19UL
-};
 
-extern uint32_t os_htobe32(uint32_t data);
-extern uint64_t os_htobe64(uint64_t data);
-extern uint32_t os_be32toh(uint32_t data);
-
-static uint8_t is_little_endian()
-{
-    static uint32_t _endian_x_ = 1;
-    return ((const uint8_t *)(& _endian_x_))[0];
-}
-
-static uint8_t  is_big_endian()
-{
-    return !is_little_endian();
-}
-/* reverse byte order */
-static  uint32_t reverse_32bit(uint32_t data)
-{
-    data = (data >> 16) | (data << 16);
-    return ((data & 0xff00ff00UL) >> 8) | ((data & 0x00ff00ffUL) << 8);
-}
-
-/* host byte order to big endian */
-uint32_t os_htobe32(uint32_t data)
-{
-    if (is_big_endian()) {
-        return data;
-    }
-    return reverse_32bit(data);
-}
-/* big endian to host byte order */
-uint32_t os_be32toh(uint32_t data)
-{
-    return os_htobe32(data);
-}
-
-static __inline uint64_t reverse_64bit(uint64_t data)
-{
-    data = (data >> 32) | (data << 32);
-    data = ((data & 0xff00ff00ff00ff00ULL) >> 8) | ((data & 0x00ff00ff00ff00ffULL) << 8);
-
-    return ((data & 0xffff0000ffff0000ULL) >> 16) | ((data & 0x0000ffff0000ffffULL) << 16);
-}
-
-/* host to big endian */
-uint64_t os_htobe64(uint64_t data)
-{
-    if (is_big_endian()) {
-        return data;
-    }
-
-    return reverse_64bit(data);
-}
-
-static void utils_sha256_zeroize(void *v, size_t n)
+static void utils_sha256_zeroize(void *v, uint32_t n)
 {
     volatile unsigned char *p = v;
     while (n--) {
@@ -134,201 +60,209 @@ void utils_sha256_clone(iot_sha256_context *dst,
 }
 void utils_sha256_starts(iot_sha256_context *ctx)
 {
-    if (NULL == ctx) {
-        return;
+    int is224 = 0;
+    ctx->total[0] = 0;
+    ctx->total[1] = 0;
+
+    if (is224 == 0) {
+        /* SHA-256 */
+        ctx->state[0] = 0x6A09E667;
+        ctx->state[1] = 0xBB67AE85;
+        ctx->state[2] = 0x3C6EF372;
+        ctx->state[3] = 0xA54FF53A;
+        ctx->state[4] = 0x510E527F;
+        ctx->state[5] = 0x9B05688C;
+        ctx->state[6] = 0x1F83D9AB;
+        ctx->state[7] = 0x5BE0CD19;
     }
-    memcpy(ctx->state, sha256_initial_hash_value, SHA256_DIGEST_LENGTH);
-    memset(ctx->buffer, 0, SHA256_BLOCK_LENGTH);
-    ctx->bitcount = 0;
+
+    ctx->is224 = is224;
 }
-void utils_sha256_process(iot_sha256_context *ctx, const uint32_t *data)
+
+static const uint32_t K[] = {
+    0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5,
+    0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5,
+    0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3,
+    0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174,
+    0xE49B69C1, 0xEFBE4786, 0x0FC19DC6, 0x240CA1CC,
+    0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA,
+    0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7,
+    0xC6E00BF3, 0xD5A79147, 0x06CA6351, 0x14292967,
+    0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13,
+    0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85,
+    0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3,
+    0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070,
+    0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5,
+    0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3,
+    0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208,
+    0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2,
+};
+
+#define  SHR(x,n) ((x & 0xFFFFFFFF) >> n)
+#define ROTR(x,n) (SHR(x,n) | (x << (32 - n)))
+
+#define S0(x) (ROTR(x, 7) ^ ROTR(x,18) ^  SHR(x, 3))
+#define S1(x) (ROTR(x,17) ^ ROTR(x,19) ^  SHR(x,10))
+
+#define S2(x) (ROTR(x, 2) ^ ROTR(x,13) ^ ROTR(x,22))
+#define S3(x) (ROTR(x, 6) ^ ROTR(x,11) ^ ROTR(x,25))
+
+#define F0(x,y,z) ((x & y) | (z & (x | y)))
+#define F1(x,y,z) (z ^ (x & (y ^ z)))
+
+#define R(t)                                    \
+    (                                               \
+            W[t] = S1(W[t -  2]) + W[t -  7] +          \
+                   S0(W[t - 15]) + W[t - 16]            \
+    )
+
+#define P(a,b,c,d,e,f,g,h,x,K)                  \
+    {                                               \
+        temp1 = h + S3(e) + F1(e,f,g) + K + x;      \
+        temp2 = S2(a) + F0(a,b,c);                  \
+        d += temp1; h = temp1 + temp2;              \
+    }
+
+void utils_sha256_process(iot_sha256_context *ctx, const unsigned char data[64])
 {
-    uint32_t a, b, c, d, e, f, g, h, s0, s1;
-    uint32_t T1, T2, *W256;
-    int j;
+    uint32_t temp1, temp2, W[64];
+    uint32_t A[8];
+    unsigned int i;
 
-    W256 = (uint32_t *) ctx->buffer;
+    for (i = 0; i < 8; i++) {
+        A[i] = ctx->state[i];
+    }
 
-    /* Initialize registers with the prev. intermediate value */
-    a = ctx->state[0];
-    b = ctx->state[1];
-    c = ctx->state[2];
-    d = ctx->state[3];
-    e = ctx->state[4];
-    f = ctx->state[5];
-    g = ctx->state[6];
-    h = ctx->state[7];
+#if defined(INFRA_SHA256_SMALLER)
+    for (i = 0; i < 64; i++) {
+        if (i < 16) {
+            GET_UINT32_BE(W[i], data, 4 * i);
+        } else {
+            R(i);
+        }
 
-    j = 0;
+        P(A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], W[i], K[i]);
 
-    do {
-        /* Copy data while converting to host byte order */
-        W256[j] = os_htobe32(*data++);
+        temp1 = A[7];
+        A[7] = A[6];
+        A[6] = A[5];
+        A[5] = A[4];
+        A[4] = A[3];
+        A[3] = A[2];
+        A[2] = A[1];
+        A[1] = A[0];
+        A[0] = temp1;
+    }
+#else /* INFRA_SHA256_SMALLER */
+    for (i = 0; i < 16; i++) {
+        GET_UINT32_BE(W[i], data, 4 * i);
+    }
 
-        /* Apply the SHA-256 compression function to update a..h */
-        T1 = h + Sigma1_256(e) + Ch(e, f, g) + K256[j] + W256[j];
+    for (i = 0; i < 16; i += 8) {
+        P(A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], W[i + 0], K[i + 0]);
+        P(A[7], A[0], A[1], A[2], A[3], A[4], A[5], A[6], W[i + 1], K[i + 1]);
+        P(A[6], A[7], A[0], A[1], A[2], A[3], A[4], A[5], W[i + 2], K[i + 2]);
+        P(A[5], A[6], A[7], A[0], A[1], A[2], A[3], A[4], W[i + 3], K[i + 3]);
+        P(A[4], A[5], A[6], A[7], A[0], A[1], A[2], A[3], W[i + 4], K[i + 4]);
+        P(A[3], A[4], A[5], A[6], A[7], A[0], A[1], A[2], W[i + 5], K[i + 5]);
+        P(A[2], A[3], A[4], A[5], A[6], A[7], A[0], A[1], W[i + 6], K[i + 6]);
+        P(A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[0], W[i + 7], K[i + 7]);
+    }
 
-        T2 = Sigma0_256(a) + Maj(a, b, c);
-        h = g;
-        g = f;
-        f = e;
-        e = d + T1;
-        d = c;
-        c = b;
-        b = a;
-        a = T1 + T2;
+    for (i = 16; i < 64; i += 8) {
+        P(A[0], A[1], A[2], A[3], A[4], A[5], A[6], A[7], R(i + 0), K[i + 0]);
+        P(A[7], A[0], A[1], A[2], A[3], A[4], A[5], A[6], R(i + 1), K[i + 1]);
+        P(A[6], A[7], A[0], A[1], A[2], A[3], A[4], A[5], R(i + 2), K[i + 2]);
+        P(A[5], A[6], A[7], A[0], A[1], A[2], A[3], A[4], R(i + 3), K[i + 3]);
+        P(A[4], A[5], A[6], A[7], A[0], A[1], A[2], A[3], R(i + 4), K[i + 4]);
+        P(A[3], A[4], A[5], A[6], A[7], A[0], A[1], A[2], R(i + 5), K[i + 5]);
+        P(A[2], A[3], A[4], A[5], A[6], A[7], A[0], A[1], R(i + 6), K[i + 6]);
+        P(A[1], A[2], A[3], A[4], A[5], A[6], A[7], A[0], R(i + 7), K[i + 7]);
+    }
+#endif /* INFRA_SHA256_SMALLER */
 
-        j++;
-    } while (j < 16);
-
-    do {
-        /* Part of the message block expansion: */
-        s0 = W256[(j + 1) & 0x0f];
-        s0 = sigma0_256(s0);
-        s1 = W256[(j + 14) & 0x0f];
-        s1 = sigma1_256(s1);
-
-        /* Apply the SHA-256 compression function to update a..h */
-        T1 = h + Sigma1_256(e) + Ch(e, f, g) + K256[j] + (W256[j & 0x0f] += s1 + W256[(j + 9) & 0x0f] + s0);
-        T2 = Sigma0_256(a) + Maj(a, b, c);
-        h = g;
-        g = f;
-        f = e;
-        e = d + T1;
-        d = c;
-        c = b;
-        b = a;
-        a = T1 + T2;
-
-        j++;
-    } while (j < 64);
-
-    /* Compute the current intermediate hash value */
-    ctx->state[0] += a;
-    ctx->state[1] += b;
-    ctx->state[2] += c;
-    ctx->state[3] += d;
-    ctx->state[4] += e;
-    ctx->state[5] += f;
-    ctx->state[6] += g;
-    ctx->state[7] += h;
-
-    /* Clean up */
-    a = b = c = d = e = f = g = h = T1 = T2 = 0;
+    for (i = 0; i < 8; i++) {
+        ctx->state[i] += A[i];
+    }
 }
-void utils_sha256_update(iot_sha256_context *ctx, const unsigned char *input, size_t ilen)
+void utils_sha256_update(iot_sha256_context *ctx, const unsigned char *input, uint32_t ilen)
 {
-    unsigned int freespace, usedspace;
+    size_t fill;
+    uint32_t left;
 
     if (ilen == 0) {
-        /* Calling with no data is valid - we do nothing */
         return;
     }
 
-    /* Sanity check: */
-    if (ctx == (iot_sha256_context *) 0 || input == (unsigned char *) 0) {
-        return;
+    left = ctx->total[0] & 0x3F;
+    fill = 64 - left;
+
+    ctx->total[0] += (uint32_t) ilen;
+    ctx->total[0] &= 0xFFFFFFFF;
+
+    if (ctx->total[0] < (uint32_t) ilen) {
+        ctx->total[1]++;
     }
 
-    usedspace = (ctx->bitcount >> 3) % SHA256_BLOCK_LENGTH;
-    if (usedspace > 0) {
-        /* Calculate how much free space is available in the buffer */
-        freespace = SHA256_BLOCK_LENGTH - usedspace;
+    if (left && ilen >= fill) {
+        memcpy((void *)(ctx->buffer + left), input, fill);
+        utils_sha256_process(ctx, ctx->buffer);
+        input += fill;
+        ilen  -= fill;
+        left = 0;
+    }
 
-        if (ilen >= freespace) {
-            /* Fill the buffer completely and process it */
-            memcpy(&ctx->buffer[usedspace], input, freespace);
-            ctx->bitcount += freespace << 3;
-            ilen -= freespace;
-            input += freespace;
-            utils_sha256_process(ctx, (uint32_t *) ctx->buffer);
-        } else {
-            /* The buffer is not yet full */
-            memcpy(&ctx->buffer[usedspace], input, ilen);
-            ctx->bitcount += ilen << 3;
-            /* Clean up: */
-            usedspace = freespace = 0;
-            return;
-        }
+    while (ilen >= 64) {
+        utils_sha256_process(ctx, input);
+        input += 64;
+        ilen  -= 64;
     }
-    while (ilen >= SHA256_BLOCK_LENGTH) {
-        /* Process as many complete blocks as we can */
-        utils_sha256_process(ctx, (uint32_t *) input);
-        ctx->bitcount += SHA256_BLOCK_LENGTH << 3;
-        ilen -= SHA256_BLOCK_LENGTH;
-        input += SHA256_BLOCK_LENGTH;
-    }
+
     if (ilen > 0) {
-        /* There's left-overs, so save 'em */
-        memcpy(ctx->buffer, input, ilen);
-        ctx->bitcount += ilen << 3;
+        memcpy((void *)(ctx->buffer + left), input, ilen);
     }
-    /* Clean up: */
-    usedspace = freespace = 0;
 }
-void utils_sha256_finish(iot_sha256_context *ctx, unsigned char output[32])
+
+static const unsigned char sha256_padding[64] = {
+    0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+};
+
+void utils_sha256_finish(iot_sha256_context *ctx, uint8_t output[32])
 {
-    /* int icount = 0; */
-    uint32_t *d = (uint32_t *) output;
-    unsigned int usedspace;
+    uint32_t last, padn;
+    uint32_t high, low;
+    unsigned char msglen[8];
 
-    /* Sanity check: */
-    if (ctx == (iot_sha256_context *) 0) {
-        return;
+    high = (ctx->total[0] >> 29)
+           | (ctx->total[1] <<  3);
+    low  = (ctx->total[0] <<  3);
+
+    PUT_UINT32_BE(high, msglen, 0);
+    PUT_UINT32_BE(low,  msglen, 4);
+
+    last = ctx->total[0] & 0x3F;
+    padn = (last < 56) ? (56 - last) : (120 - last);
+
+    utils_sha256_update(ctx, sha256_padding, padn);
+    utils_sha256_update(ctx, msglen, 8);
+
+    PUT_UINT32_BE(ctx->state[0], output,  0);
+    PUT_UINT32_BE(ctx->state[1], output,  4);
+    PUT_UINT32_BE(ctx->state[2], output,  8);
+    PUT_UINT32_BE(ctx->state[3], output, 12);
+    PUT_UINT32_BE(ctx->state[4], output, 16);
+    PUT_UINT32_BE(ctx->state[5], output, 20);
+    PUT_UINT32_BE(ctx->state[6], output, 24);
+
+    if (ctx->is224 == 0) {
+        PUT_UINT32_BE(ctx->state[7], output, 28);
     }
-
-    /* If no digest buffer is passed, we don't bother doing this: */
-    if (output != (unsigned char *) 0) {
-        usedspace = (ctx->bitcount >> 3) % SHA256_BLOCK_LENGTH;
-        ctx->bitcount = os_htobe64(ctx->bitcount);
-        if (usedspace > 0) {
-            /* Begin padding with a 1 bit: */
-            ctx->buffer[usedspace++] = 0x80;
-
-            if (usedspace <= SHA256_SHORT_BLOCK_LENGTH) {
-                /* Set-up for the last transform: */
-                memset(&ctx->buffer[usedspace], 0, SHA256_SHORT_BLOCK_LENGTH - usedspace);
-            } else {
-                if (usedspace < SHA256_BLOCK_LENGTH) {
-                    memset(&ctx->buffer[usedspace], 0, SHA256_BLOCK_LENGTH - usedspace);
-                }
-                /* Do second-to-last transform: */
-                utils_sha256_process(ctx, (uint32_t *) ctx->buffer);
-
-                /* And set-up for the last transform: */
-                memset(ctx->buffer, 0, SHA256_SHORT_BLOCK_LENGTH);
-            }
-        } else {
-            /* Set-up for the last transform: */
-            memset(ctx->buffer, 0, SHA256_SHORT_BLOCK_LENGTH);
-
-            /* Begin padding with a 1 bit: */
-            *ctx->buffer = 0x80;
-        }
-        /* Set the bit count: */
-        u_retLen tmp;
-        tmp.lint = ctx->bitcount;
-        memcpy(&ctx->buffer[SHA256_SHORT_BLOCK_LENGTH], tmp.sptr, 8);
-
-
-        /* Final transform: */
-        utils_sha256_process(ctx, (uint32_t *) ctx->buffer);
-
-        {
-            /* Convert TO host byte order */
-            int j;
-            for (j = 0; j < 8; j++) {
-                ctx->state[j] = os_be32toh(ctx->state[j]);
-                *d++ = ctx->state[j];
-            }
-        }
-    }
-
-    /* Clean up state data: */
-    memset(ctx, 0, sizeof(iot_sha256_context));
-    usedspace = 0;
 }
-void utils_sha256(const unsigned char *input, size_t ilen, unsigned char output[32])
+
+void utils_sha256(const uint8_t *input, uint32_t ilen, uint8_t output[32])
 {
     iot_sha256_context ctx;
 
@@ -338,5 +272,4 @@ void utils_sha256(const unsigned char *input, size_t ilen, unsigned char output[
     utils_sha256_finish(&ctx, output);
     utils_sha256_free(&ctx);
 }
-
 #endif
