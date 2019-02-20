8225c98efbc730b85eedc02d0cc278d8ac35b622
zilong.liuzl <zilong.liuzl@alibaba-inc.com>
2019-02-20 10:12:21 +0800

    BugID:19029333: modify code style
    Change-Id: I4e7fb98239e82d90bf8ad2f6c318846c96786525

--- kernel/rhino/k_err.c
+++ kernel/rhino/k_err.c
@@ -1,10 +1,9 @@
 /*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
-#include "k_api.h"
 #include "debug_api.h"
-#include <stdio.h>
+#include "k_api.h"
 
 void k_err_proc_debug(kstat_t err, char *file, int line)
 {
@@ -16,3 +15,4 @@ void k_err_proc_debug(kstat_t err, char *file, int line)
         g_err_proc(err);
     }
 }
+
