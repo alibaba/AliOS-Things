94ab8c27912b4652d6c2d51492a35fc80759dc5f
zixun.lyq <zixun.lyq@alibaba-inc.com>
2019-02-21 17:34:49 +0800

    BugID:19055439:[OTA] optimize component options
    Change-Id: I00a15065c54d3a15e47e524b0a33d5b081fe2a2c

--- middleware/uagent/ota/aos.mk
+++ middleware/uagent/ota/aos.mk
@@ -11,5 +11,25 @@ else ifeq ($(COMPILER),gcc)
 $(NAME)_CFLAGS      += -Wall -Werror
 endif
 
-$(NAME)_COMPONENTS += ota_core ota_hal
+$(NAME)_SOURCES := ota_core/ota_service.c \
+                   ota_core/download/ota_download_http.c \
+                   ota_core/transport/ota_transport_mqtt.c \
+                   ota_core/verify/ota_hash.c \
+                   ota_core/verify/ota_sign.c
+
+$(NAME)_SOURCES += hal/ota_hal_module.c \
+                   hal/ota_hal_os.c
+
+ifneq (,$(filter mcu_linuximpl,$(HOST_MCU_FAMILY)))
+$(NAME)_SOURCES += hal/ota_hal_host.c
+else
+ifneq (,$(filter mcu_esp32,$(HOST_MCU_FAMILY)))
+$(NAME)_SOURCES += hal/ota_hal_esp32.c
+else
+$(NAME)_SOURCES += hal/ota_hal_plat.c
+endif
+endif
+
+GLOBAL_INCLUDES += include hal ota_core/verify 2nd_boot
+
 GLOBAL_DEFINES += OTA_SIGNAL_CHANNEL=1
