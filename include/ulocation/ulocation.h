7237e49c65200f06ecb1fa3d4aaaa6f17ed18936
zxl <chenqi.zxl@alibaba-inc.com>
2019-02-22 13:46:51 +0800

    BugID:19050431: ulocation support gaode locating service
    Change-Id: Ie260930cd86844200a79c804f2e56f9462ab25c4

---
+++ include/ulocation/ulocation.h
@@ -0,0 +1,132 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifndef _U_LOCATION_H_
+#define _U_LOCATION_H_
+
+typedef enum
+{
+    ULOC_SCENARIO_OUTDOOR = 0,
+    ULOC_SCENARIO_INDOOR  = 1,
+    ULOC_SCENARIO_COUNT
+} uloc_scenario_e;
+
+typedef struct 
+{
+    float longitude;
+    float latitude;
+    float altitude;
+} outdoor_location_t;
+
+typedef struct 
+{
+    float x;
+    float y;
+    float z;
+} indoor_location_t;
+
+typedef union
+{
+    indoor_location_t  indoor;
+    outdoor_location_t outdoor;
+    struct 
+    {
+        float x;
+        float y;
+        float z;
+    } coordinate;
+} location_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*******************************************************************
+*  initial location service
+*
+*  @return: 
+*******************************************************************/
+int uloc_init(uloc_scenario_e scen, int update_inv);
+
+/*******************************************************************
+*  terminate locating service
+*
+*  @return: 
+*******************************************************************/
+int uloc_deinit();
+
+/* OUTDOOR */
+/*******************************************************************
+*  get latitude
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_latitude(float * latitude);
+
+/*******************************************************************
+*  get altitude
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_altitude(float * altitude);
+
+/*******************************************************************
+*  get longitude
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_longitude(float *longitude);
+
+/* INDOOR */
+/*******************************************************************
+*  get x
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_x(float * x);
+
+/*******************************************************************
+*  get y
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_y(float * y);
+
+/*******************************************************************
+*  get z
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_z(float * z);
+
+/*******************************************************************
+*  get location
+*
+*  @return: 
+*******************************************************************/
+int uloc_get_location(location_t * rlt);
+
+/*******************************************************************
+*  fetch new location value, this method should be invoked by user 
+*  when there is no individual self task.
+*
+*  @return: 
+*******************************************************************/
+int uloc_update_locationinfo(location_t ** gps, 
+                             char ** mmac, char ** macs,   /* wifi       */
+                             char ** cdma, char ** bts,  char ** nearbts);
+
+/*******************************************************************
+*  uloc_update_gpsinfo
+*
+*  @return: 
+*******************************************************************/
+int uloc_update_gpsinfo(location_t * lo);
+
+#ifdef __cplusplus
+}
+#endif /* extern c */
+
+#endif /* _U_LOCATION_H_ */
+
