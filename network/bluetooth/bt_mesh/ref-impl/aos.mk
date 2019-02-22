87c5098a3cad69fd86edbffb28b5717742a7a3f9
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-02-22 22:08:38 +0800

    BugId:19028262: Fix bt_mesh comps
    Change-Id: I2f1f32d8dcaa5ce71e2792fe1500aae29a700478

--- network/bluetooth/bt_mesh/ref-impl/aos.mk
+++ network/bluetooth/bt_mesh/ref-impl/aos.mk
@@ -1,6 +1,8 @@
 NAME := mesh_hal_ref_impl
 
-$(NAME)_COMPONENTS := 
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := BLE Mesh HAL reference implementation
 
 $(NAME)_SOURCES := ./mesh_hal_ble.c \
                    ./mesh_hal_sec.c
@@ -11,7 +13,7 @@ GLOBAL_INCLUDES += .
 endif
 
 ifneq ($(bt_mesh_standalone_deploy),1)
-$(NAME)_COMPONENTS += bluetooth.bt_host
+$(NAME)_COMPONENTS += bt_host
 GLOBAL_INCLUDES += ../bt_host/host/ \
                    ../bt_host/include/
 endif
