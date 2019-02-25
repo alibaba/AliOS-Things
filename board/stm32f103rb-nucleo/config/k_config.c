fc88a2b283e36a4b47b8fa87a08616d58779c4db
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-02-25 16:33:21 +0800

    BugID:19092717: Remove unused stm32f103ze, Add stm32f103rb-nucleo support
    Change-Id: Ib92dac3f2eaa1284bafeb8fc28cbadf411a90dd9

--- board/stm32f103rb-nucleo/config/k_config.c
+++ board/stm32f103rb-nucleo/config/k_config.c
@@ -1,6 +1,16 @@
-/*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
- */
+/**
+  ******************************************************************************
+  * @file    k_config.c
+  * @author  MCU China FAE team
+  * @version 1.0
+  * @date    05/01/2019
+  * @brief   aos porting layer
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2019 STMicroelectronics
+  *
+  ******************************************************************************
+  */
 
 #include <k_api.h>
 #include <assert.h>
@@ -48,16 +58,17 @@ void aos_heap_set()
 }
 
 #elif defined (__ICCARM__)/* IAR */
-#define HEAP_BUFFER_SIZE 1024*20
 int           g_region_num = 1;
-uint8_t g_heap_buf[HEAP_BUFFER_SIZE];
-k_mm_region_t g_mm_region[] = {{g_heap_buf, HEAP_BUFFER_SIZE}};
+k_mm_region_t g_mm_region[1];
 void aos_heap_set()
 {
-    g_mm_region[0].start = g_heap_buf;
-    g_mm_region[0].len   = HEAP_BUFFER_SIZE;
+    #pragma section="HEAP"
+    uint8_t* heap_start= (uint8_t*)__section_begin("HEAP");
+    uint8_t* heap_end=(uint8_t*)__section_end("HEAP");
+    /* heap_start and heap_len is set by linkscript(*.ld) */
+    g_mm_region[0].start = heap_start;
+    g_mm_region[0].len = heap_end - heap_start;
 }
-
 #else /* GCC */
 extern void         *_estack;
 extern void         *__bss_end__;
@@ -74,10 +85,47 @@ void aos_heap_set()
 #endif
 #endif
 
+#if (RHINO_CONFIG_TASK_STACK_CUR_CHECK > 0)
+size_t soc_get_cur_sp()
+{
+    size_t sp = 0;
+#if defined (__GNUC__)&&!defined(__CC_ARM)
+    asm volatile(
+        "mov %0,sp\n"
+        :"=r"(sp));
+#endif
+    return sp;
+}
+static void soc_print_stack()
+{
+    void    *cur, *end;
+    int      i=0;
+    int     *p;
+
+    end   = krhino_cur_task_get()->task_stack_base + krhino_cur_task_get()->stack_size;
+    cur = (void *)soc_get_cur_sp();
+    p = (int*)cur;
+    while(p < (int*)end) {
+        if(i%4==0) {
+            printf("\r\n%08x:",(uint32_t)p);
+        }
+        printf("%08x ", *p);
+        i++;
+        p++;
+    }
+    printf("\r\n");
+    return;
+}
+#endif
+
 void soc_err_proc(kstat_t err)
 {
     (void)err;
     printf("soc_err_proc %d \r\n", err);
+    //krhino_backtrace_now();
+    #if (RHINO_CONFIG_TASK_STACK_CUR_CHECK > 0)
+    //soc_print_stack();
+    #endif
     while(1);
 }
 
