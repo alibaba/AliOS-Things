d826ac1179cf881263646a75d945ba95c2a74e93
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2019-02-26 21:37:52 +0800

    BugID:19084538: Fix the issue that friendship clear cfm msg may be filtered.
    Change-Id: Icd33f67ade000219a959365736eb6ab5d1b06e7d

--- network/bluetooth/bt_mesh/src/transport.c
+++ network/bluetooth/bt_mesh/src/transport.c
@@ -1339,7 +1339,7 @@ int bt_mesh_trans_recv(struct net_buf_simple *buf, struct bt_mesh_net_rx *rx)
 	 */
 	if (IS_ENABLED(CONFIG_BT_MESH_LOW_POWER) &&
 	    bt_mesh_lpn_established() && rx->net_if == BT_MESH_NET_IF_ADV &&
-	    (!bt_mesh_lpn_waiting_update() || !rx->friend_cred)) {
+	    ((!bt_mesh_lpn_waiting_update() || !rx->friend_cred) && bt_mesh_lpn_clear_ongoing())) {
 		BT_WARN("Ignoring unexpected message in Low Power mode");
 		return -EAGAIN;
 	}
