a4dcea8b5f1a5316fbda9fad6015705ccd3472f4
xinweng.ljh | lujihua <xinweng.ljh@alibaba-inc.com>
2019-02-26 14:41:39 +0800

    BugID:18888885:optimized the gateway app to gerrit main line
    Change-Id: Ib9415d0378017ab0b620b14c67a2d1a1edbed814

--- app/profile/gatewayapp/ucube.py
+++ app/profile/gatewayapp/ucube.py
@@ -1,47 +1,24 @@
-src =Split(''' 
-    gateway.c
-    gateway_app.c
-    linkkit_export.c
-    lite_queue.c
-    gateway_tsl.c
-    linkedge_ws.c
+src =Split('''
+    udata_example.c
 ''')
-component =aos_component('gatewayapp', src)
-	
-dependencis =Split(''' 
-    framework/connectivity/mqtt
-    framework/fota
-    framework/netmgr
-    framework/common
-    framework/protocol/linkkit/cm
-    framework/protocol/linkkit/dm
-    framework/protocol/linkkit/alcs
-    framework/protocol/linkkit/iotkit
-    framework/ywss4linkkit
+
+component =aos_component('udataapp', src)
+global_includes =Split('''
+    .
+    ../../../drivers/sensor/include
+''')
+
+dependencis =Split('''
     kernel/cli
-    drivers/sensor 
-    framework/uData
-    network/websockets
+    network/netmgr
+    drivers/sensor
+    middleware/udata
 ''')
 for i in dependencis:
     component.add_comp_deps(i)
 
-global_macros =Split(''' 
-    MQTT_DIRECT
-    ALIOT_DEBUG
-    IOTX_DEBUG
-    USE_LPTHREAD
-    FOTA_RAM_LIMIT_MODE
-    COAP_WITH_YLOOP
-    TEST_ALCS
-    ON_PRE2=1
-    DATA_TO_CLOUD
-    PT_SCANNER=1
+global_macros =Split('''
 ''')
 for i in global_macros:
     component.add_global_macros(i)
-
-if aos_global_config.get('LWIP') == 1:
-    component.add_comp_deps("kernel/protocols/net")
-    aos_global_config.set('no_with_lwip', 0)
 
