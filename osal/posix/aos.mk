2d1fbdbdcffe8347c909f443979706c954a61ecd
congling.zm <congling.zm@alibaba-inc.com>
2019-02-28 11:50:12 +0800

    BugID:19113176: add RPM_INCLUDE_DIR to indicate where the header file folder is located example 1  RPM_INCLUDE_DIR := cmsis  component name: osal_cmsis  component path: osal/cmsis/  include path: (root directory of source code)/include/cmsis
    example 2
     RPM_INCLUDE_DIR := ../hal
     component name: ls_hal
     component path: security/ls_hal
     include path: (root directory of source code)/security/include/hal
    Change-Id: I59b966a9f069afcc63b0243d54836ddc557fe474

--- osal/posix/aos.mk
+++ osal/posix/aos.mk
@@ -3,6 +3,8 @@ NAME := osal_posix
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.0
 $(NAME)_SUMMARY    := posix for alios things
+#if component's header files under another directory, add RPM_INCLUDE_DIR to indicate where the header file folder is located
+RPM_INCLUDE_DIR    := posix
 
 $(NAME)_SOURCES := pthread.c
 $(NAME)_SOURCES += pthread_attr.c
