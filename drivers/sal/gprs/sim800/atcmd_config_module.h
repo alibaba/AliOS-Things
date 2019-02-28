4b53de39b1c8707d2a28c1db093cc18dc5005606
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-02-28 15:57:56 +0800

    BugID:19125898: refine bk7231 sal hal impl and add configurable send delay for sim800
    Change-Id: Ib62d26f3ca213280dbb7b91e92bec934c57cd71b

--- drivers/sal/gprs/sim800/atcmd_config_module.h
+++ drivers/sal/gprs/sim800/atcmd_config_module.h
@@ -29,6 +29,9 @@
 #define AT_RECV_FAIL_POSTFIX "ERROR\r\n"
 #define AT_SEND_DELIMITER "\r"
 
+// send delay
+#define AT_SEND_DATA_DELAY_MS 50
+
 // AT event
 #define AT_EVENT_ENET_DATA "+ENETEVENT:"
 
