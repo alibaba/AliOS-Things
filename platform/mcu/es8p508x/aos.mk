87142332dc38d8c4c6bea5780a1b0651459c8287
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2019-02-28 10:10:13 +0800

    BugID:17952514: fix some keil project build errors
    Change-Id: I73cbc4a74b034a3a2207b3c3906430ac510e9c46

--- platform/mcu/es8p508x/aos.mk
+++ platform/mcu/es8p508x/aos.mk
@@ -13,7 +13,8 @@ GLOBAL_INCLUDES += drivers/        \
                    drivers/library/Include \
                    drivers/cmsis
 
-$(NAME)_SOURCES += hal/hw.c
+$(NAME)_SOURCES += hal/hw.c \
+	           hal/hal_uart.c
 
 # The library is built by Keil4 (source file is under driver directory)
 $(NAME)_PREBUILT_LIBRARY := drivers/ES8P508x.a
