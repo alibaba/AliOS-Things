76c7e4d8ba063a6859ef4bbd1481cef3c0f9312b
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2019-03-01 18:57:47 +0800

    BugID:19174429: add pca10040 keil project
    Change-Id: Ice48927535e9c766480c466b49979e65d5311638

--- network/bluetooth/bt_host/host/hci_core.c
+++ network/bluetooth/bt_host/host/hci_core.c
@@ -14,7 +14,6 @@
 #include <zephyr.h>
 #include <string.h>
 #include <stdio.h>
-#include <errno.h>
 #include <atomic.h>
 #include <misc/util.h>
 #include <misc/slist.h>
@@ -29,6 +28,7 @@
 #include <bluetooth/hci_vs.h>
 #include <bluetooth/hci_driver.h>
 #include <bluetooth/storage.h>
+#include <bluetooth/errno.h>
 
 #define BT_DBG_ENABLED IS_ENABLED(CONFIG_BT_DEBUG_HCI_CORE)
 #include "common/log.h"
