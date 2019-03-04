962621c0f9e5a78a27497b9365435d683c78f8a4
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-03-04 21:50:13 +0800

    bugID:19183337:[breeze] Misc change for secret per product.
    Change-Id: Ib0ec3641304eab073ea5c26b30ccbed7c3ef6f49

--- network/bluetooth/breeze/core/include/utils.h
+++ network/bluetooth/breeze/core/include/utils.h
@@ -17,6 +17,13 @@
     *((uint8_t *)(data) + 3) = (uint8_t)((val >> 24) & 0xFF); \
 }
 
+#define SET_U32_BE(data, val) {                               \
+    *(uint8_t *)(data) =  (uint8_t)((val >> 24) & 0xFF);      \
+    *((uint8_t *)(data) + 1) = (uint8_t)((val >> 16) & 0xFF); \
+    *((uint8_t *)(data) + 2) = (uint8_t)((val >> 8) & 0xFF);  \
+    *((uint8_t *)(data) + 3) = (uint8_t)(val & 0xFF);         \
+}
+
 #ifndef MIN
 #define MIN(a, b) ((a) < (b) ? (a) : (b))
 #endif
