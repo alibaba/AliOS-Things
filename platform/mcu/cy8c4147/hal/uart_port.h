f180919570332d55babfa152b7053a5dcc7e8bb8
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-03-06 19:53:04 +0800

    BugID:19223833: Fix GPL License conflict
    Change-Id: I232feaac14f9f14311e7d4d80dcabe8ae93eb23c

--- platform/mcu/cy8c4147/hal/uart_port.h
+++ platform/mcu/cy8c4147/hal/uart_port.h
@@ -57,34 +57,6 @@ int default_UART_Init(void);
 void uart_put_char(int c);
 int32_t aos_uart_recv(void *data, uint32_t expect_size, uint32_t *recv_size, uint32_t timeout);
 int32_t aos_uart_send(void *data, uint32_t size, uint32_t timeout);
-struct circ_buf {
-    char *buf;
-    int head;
-    int tail;
-};
-
-/* Return count in buffer.  */
-#define CIRC_CNT(head,tail,size) (((head) - (tail)) & ((size)-1))
-
-/* Return space available, 0..size-1.  We always leave one free char
-   as a completely full buffer has head == tail, which is the same as
-   empty.  */
-#define CIRC_SPACE(head,tail,size) CIRC_CNT((tail),((head)+1),(size))
-
-/* Return count up to the end of the buffer.  Carefully avoid
-   accessing head and tail more than once, so they can change
-   underneath us without returning inconsistent results.  */
-#define CIRC_CNT_TO_END(head,tail,size) \
-    ({int end = (size) - (tail); \
-      int n = ((head) + end) & ((size)-1); \
-      n < end ? n : end;})
-
-/* Return space available up to the end of the buffer.  */
-#define CIRC_SPACE_TO_END(head,tail,size) \
-    ({int end = (size) - 1 - (head); \
-      int n = (end + (tail)) & ((size)-1); \
-      n <= end ? n : end+1;})
-//the size should be 2^
 #define UART_CONSOLE_SIZE 256
 #ifdef __cplusplus
 }
