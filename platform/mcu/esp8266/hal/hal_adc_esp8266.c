188044d3b69c531c8d83d7cfd054fc943d221ad8
崇龄 <yj144830@alibaba-inc.com>
2019-03-08 15:53:24 +0800

    BugID:19272307:add support of esp8266 i2c adc timer
    Change-Id: I7eddc8a71f7845565767f0fd7ac18fb293d6a155

---
+++ platform/mcu/esp8266/hal/hal_adc_esp8266.c
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#include "aos/hal/adc.h"
+#include "esp8266/ets_sys.h"
+
+extern uint16_t test_tout();
+
+int32_t hal_adc_init(adc_dev_t *adc)
+{
+    return 0;
+}
+
+int32_t hal_adc_value_get(adc_dev_t *adc, void *output, uint32_t timeout)
+{
+    uint16_t val = 0;
+    uint16_t *data = (uint16_t *)output;
+
+    if ((adc == NULL) || (output == NULL)) {
+    	return -1;
+    }
+
+    val = test_tout(0);
+    if (val != 0xFFFF) {
+        /* The working voltage of ADC is designed according to 1.1v. Later, the actual working
+           voltage of ADC is increased to 1.2v, so this scale is added. */
+        val = val * 12 / 11;
+
+        if (val > 1023) {
+            val = 1023; /* 10-bit precision ADC */
+        }
+    }
+
+    *data = val;
+
+    return 0;
+}
+
+int32_t hal_adc_finalize(adc_dev_t *adc)
+{
+    return 0;
+}
