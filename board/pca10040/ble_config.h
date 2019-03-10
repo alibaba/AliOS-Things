c8f97352e6b355dc71b507f0d931a9b99e86d6a1
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-03-10 13:55:51 +0800

    BugID:19278468:Add config options for link params
    Change-Id: Iacc71d28f663ecc961387c6ede7f68378e78f2e3

--- board/pca10040/ble_config.h
+++ board/pca10040/ble_config.h
@@ -5,16 +5,15 @@
 #ifndef BLE_CONFIG_H
 #define BLE_CONFIG_H
 
-/**
- * CONFIG_BT: Tx thread stack size
- */
-
-#ifndef CONFIG_BT_HCI_TX_STACK_SIZE
 #define CONFIG_BT_HCI_TX_STACK_SIZE 512
-#endif
 
-#ifndef CONFIG_BT_HCI_CMD_COUNT
 #define CONFIG_BT_HCI_CMD_COUNT 16
+
+#ifdef CONFIG_BLE_LINK_PARAMETERS
+#define  CONFIG_SUP_TO_LIMIT 400
+#define  CONFIG_CONN_SUP_TIMEOUT 400
+#define  CONFIG_CONN_INTERVAL_MIN 24
+#define  CONFIG_CONN_INTERVAL_MAX 40
 #endif
 
 #if defined(__cplusplus)
