ee0528ed6241a0f0ece6607915f87381ea0dc1f1
Li Cheng <lc122798@alibaba-inc.com>
2019-03-11 14:38:53 +0800

    BugID:19084924:synchonize changes in app folder

--- app/example/yts_nocli/yts_nocli.mk
+++
@@ -1,9 +0,0 @@
-NAME := yts_nocli
-
-#$(NAME)_SOURCES := main.c
-#$(NAME)_COMPONENTS := testcase testcase.certificate_test rhino.test log rhino.vfs yloop hal
-
-$(NAME)_SOURCES := main.c
-$(NAME)_COMPONENTS := testcase testcase.certificate_test rhino.test log rhino.vfs yloop hal
-
-$(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable
