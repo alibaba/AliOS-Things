3d274aedf0d1a0e278baa309a7ec90c546a5fb2c
Li Cheng <lc122798@alibaba-inc.com>
2019-03-11 14:20:50 +0800

    BugID:19084924:synchonize changes in network/umesh folder

--- network/umesh/aos.mk
+++ network/umesh/aos.mk
@@ -1,6 +1,8 @@
 NAME := umesh
 
 $(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := uMesh provides the ability to form local area mesh network.
 GLOBAL_INCLUDES += include
 GLOBAL_DEFINES += CONFIG_AOS_MESH
 
@@ -10,6 +12,8 @@ else ifeq ($(COMPILER),iar)
 $(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/iar/umesh.a
 else ifeq ($(ENABLE_VFP),1)
 $(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/VFP/gcc/umesh.a
+else ifeq ($(ipv6),1)
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/IPv6/gcc/umesh.a
 else
 $(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/gcc/umesh.a
 endif
