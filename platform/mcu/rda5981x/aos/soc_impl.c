e2c4dc1071b5612f324b3c948a273582660e61e3
yx170385 <yx170385@alibaba-inc.com>
2019-03-11 16:06:22 +0800

    BugID:18712794:reboot after fatal error
    Change-Id: I5dca510e0a5341762cf39c4bd641e185f1eba9aa

--- platform/mcu/rda5981x/aos/soc_impl.c
+++ platform/mcu/rda5981x/aos/soc_impl.c
@@ -5,6 +5,8 @@
 #include <k_api.h>
 #include <assert.h>
 
+extern void hal_reboot(void);
+
 #if (RHINO_CONFIG_HW_COUNT > 0)
 void soc_hw_timer_init(void)
 {
@@ -40,43 +42,11 @@ k_mm_region_t g_mm_region[] =
     {{(uint8_t*)&heap_start,(size_t)&heap_len},{(uint8_t*)&__SmemLeft,(size_t)&__SmemLeftLen},{(uint8_t*)&__IramLeft,(size_t)&__IramLeftLen}};
 int           g_region_num  = sizeof(g_mm_region)/sizeof(k_mm_region_t);
 
-size_t soc_get_cur_sp()
-{
-    size_t sp = 0;
-    asm volatile(
-        "mov %0,sp\n"
-        :"=r"(sp));
-    return sp;
-}
 
-static void soc_print_stack()
-{
-
-    uint32_t offset = 0;
-    kstat_t  rst    = RHINO_SUCCESS;
-    void    *cur, *end;
-    int      i=0;
-    int     *p;
-
-    end   = krhino_cur_task_get()->task_stack_base + krhino_cur_task_get()->stack_size;
-    cur = soc_get_cur_sp();
-    p = (int*)cur;
-    while(p < (int*)end) {
-        if(i%4==0) {
-            printf("\r\n%08x:",(uint32_t)p);
-        }
-        printf("%08x ", *p);
-        i++;
-        p++;
-    }
-    printf("\r\n");
-    return;
-}
 void soc_err_proc(kstat_t err)
 {
     (void)err;
-    soc_print_stack();
-    assert(0);
+    hal_reboot();
 }
 
 krhino_err_proc_t g_err_proc = soc_err_proc;
