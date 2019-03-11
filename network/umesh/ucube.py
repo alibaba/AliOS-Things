6952225dbfa4ff9cac3b999d7b8b39a77836fe0c
Li Cheng <lc122798@alibaba-inc.com>
2019-03-11 14:20:50 +0800

    BugID:19084924:synchonize changes in network/umesh folder

--- network/umesh/ucube.py
+++
@@ -1,8 +0,0 @@
-src = []
-component = aos_component('mesh', src)
-component.add_global_includes('include')
-component.add_global_macros('CONFIG_AOS_MESH')
-
-libpath = '/'.join(['lib', component.get_arch(), component.get_compiler, 'mesh.a'])
-component.add_prebuilt_libs(libpath)
-
