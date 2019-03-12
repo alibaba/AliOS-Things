30eb0e464e65dc4548dad9703b49a8ba12af2b7a
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-03-12 16:15:14 +0800

    BugID:19120134:Add logics for assembling a http request
    Change-Id: I7c83d4317e97cc229f6cd6e93bc79a42e8502103

--- network/httpc/http_client.h
+++ network/httpc/http_client.h
@@ -7,11 +7,33 @@
 #ifndef HTTP_CLIENT_H
 #define HTTP_CLIENT_H
 
+#define HTTP_CLIENT_FLAG_KEEP_ALIVE 0x00000001
+#define HTTP_CLIENT_FLAG_NO_CACHE 0x00000004
+
+#define HTTP_CLIENT_FLAG_SECURE 0x00000010
+#define HTTP_CLIENT_FLAG_PORT 0x00000020
+
 /* http client connection handle */
 typedef struct httpc_s {
     int socket;
+    uint8_t index;
+    uint32_t flags;
+    char server_name[CONFIG_HTTPC_SERVER_NAME_SIZE + 2];
     httpc_recv_fun recv_fn;
     uint16_t rx_state;
+    uint16_t port;
+
+    char header[CONFIG_HTTPC_HEADER_SIZE + 1];
+    uint16_t header_len;
 } httpc_t;
+
+#define HTTPC_VERSION "HTTP/1.1"
+
+#define HTTPC_CRLF "\r\n"
+
+typedef struct httpc_param_s {
+    char *param;
+    uint16_t len;
+} httpc_param_t;
 
 #endif
