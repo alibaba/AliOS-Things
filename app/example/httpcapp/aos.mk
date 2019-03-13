a09f64e4ecd4ac9daca141a0fba595069a2b9fc2
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-03-13 18:55:52 +0800

    BugID:19120134:Add httpc app sample
    Change-Id: I799fb44fbeb13bcf60dac36a190dcc4fb043c02d

---
+++ app/example/httpcapp/aos.mk
@@ -0,0 +1,10 @@
+NAME := httpcapp
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := HTTP Client app
+
+$(NAME)_SOURCES := main.c
+$(NAME)_COMPONENTS := httpc netmgr lwip cli
+
+GLOBAL_DEFINES += BUILD_AOS
