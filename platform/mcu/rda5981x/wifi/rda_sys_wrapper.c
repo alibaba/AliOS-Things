2bb4945be93407069bac80da0b5dd117f90aaa08
shilei.yu <shilei.yu@unisoc.com>
2019-03-13 21:18:10 +0800

    update wifi driver

--- platform/mcu/rda5981x/wifi/rda_sys_wrapper.c
+++ platform/mcu/rda5981x/wifi/rda_sys_wrapper.c
@@ -83,7 +83,8 @@ r_s32 rda_alarm_start(r_void *handle, r_u32 timeout_ms)
 {
     r_s32 ret;
     if(NULL != handle) {
-        ret = aos_timer_change((aos_timer_t *)handle, timeout_ms);
+        //ret = aos_timer_change((aos_timer_t *)handle, timeout_ms);
+        ret = krhino_timer_change(((aos_timer_t *)handle)->hdl, krhino_ms_to_ticks(timeout_ms), 0);
         if(ret != 0)
             return ERR;
         return aos_timer_start((aos_timer_t *)handle);
@@ -213,7 +214,7 @@ r_s32 rda_queue_recv(r_void *handler, r_u32 msg, r_u32 millisec)
 {
     rda_queue_handle *r_handler = (rda_queue_handle*)handler;
     r_s32 size;
-    if(aos_queue_recv((aos_queue_t *)(r_handler->queue), millisec, (r_void*)msg, &size) == 0)
+    if(aos_queue_recv((aos_queue_t *)(r_handler->queue), millisec, (r_void*)msg, &size) != 0)
         return ERR;
     else
         return NO_ERR;
