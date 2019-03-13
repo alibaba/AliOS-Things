36cde9be9227cfcfeace07cd82ae21a772ec2042
Li Cheng <lc122798@alibaba-inc.com>
2019-03-13 12:21:51 +0800

    fixed travis CI build problem
    Change-Id: I279d86d67125871e6a419fdc0a5de8708c24fd23

--- board/mk3080/ucube.py
+++ board/mk3080/ucube.py
@@ -1,3 +1,3 @@
 
-supported_targets="alinkapp helloworld meshapp networkapp acapp linkkitapp mqttapp coapapp linkkit_gateway linkkit_sched"
-linux_only_targets="id2_app wifimonitor hdlcapp.hdlcserver sst_app nano helloworld_nocli tls linuxapp uDataapp blink vflashdemo netmgrapp itls_app wifihalapp prov_app athostapp"
+supported_targets="helloworld meshapp networkapp acapp linkkitapp mqttapp coapapp linkkit_gateway"
+linux_only_targets="id2_app wifimonitor hdlcapp.hdlcserver nano helloworld_nocli tls linuxapp uDataapp blink vflashdemo netmgrapp itls_app wifihalapp prov_app athostapp"
--- board/stm32f429zi-nucleo/ucube.py
+++ board/stm32f429zi-nucleo/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="helloworld netmgrapp helloworld_nocli tls hdlcapp.hdlcserver hdlcapp.hdlcclient mqttapp meshapp linkkitapp coapapp networkapp acapp wifihalapp linkkit_gateway alinkapp linuxapp linkkit_sched uDataapp wifimonitor athostapp"
+linux_only_targets="helloworld netmgrapp helloworld_nocli tls hdlcapp.hdlcserver hdlcapp.hdlcclient mqttapp meshapp linkkitapp coapapp networkapp acapp wifihalapp linkkit_gateway linuxapp uDataapp wifimonitor athostapp"
