414ebc60f3269bfa005698d0e978bbf3ba80ce1c
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-03-15 08:48:31 +0800

    BugID:19120134:Rename httpcapp to httpapp
    Change-Id: I40ebfbf182f6ce47787f1c92fb5c6aedd64b9d05

--- app/example/httpapp/aos.mk
+++ app/example/httpapp/aos.mk
@@ -1,10 +1,10 @@
-NAME := httpcapp
+NAME := httpapp
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := HTTP Client app
 
 $(NAME)_SOURCES := main.c
-$(NAME)_COMPONENTS := httpc netmgr cli
+$(NAME)_COMPONENTS := http netmgr cli
 
 GLOBAL_DEFINES += BUILD_AOS
