9cad875b606e99a5910daded584582ea286e9619
Li Cheng <lc122798@alibaba-inc.com>
2019-03-18 02:12:56 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: I792ad667469f3bf46565e190cf57485c23ce94be

--- board/numaker-pfm-m487/ucube.py
+++ board/numaker-pfm-m487/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="athostapp blink coapapp helloworld http2app linkkit_gateway linkkitapp modbus_demo mqttapp otaapp udataapp udevapp ulocationapp yts"
+linux_only_targets="athostapp blink coapapp helloworld http2app httpapp linkkit_gateway linkkitapp meshapp modbus_demo mqttapp otaapp udataapp udevapp ulocationapp yts"
