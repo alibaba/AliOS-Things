c00664e8938d290c7b427070b1295522b845bce9
Cheng Li <vivid8710@gmail.com>
2019-03-21 20:05:44 +0800

    cherry-pick changes 198812:198813 to master branch (#919)
    * BugID:19092717: Add WM600 Network/Wifi bugfix
    
    Change-Id: If9dda01d75b432e24c90211acb562e32f112e379
    
    * BugID:19092717: Add WM600 bugfix patch
    
    Change-Id: I39b4dc5403b84ab15a570bd7eb5dc8b47b8b87f2

--- board/wm_w600_kit/ucube.py
+++ board/wm_w600_kit/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="athostapp coapapp helloworld http2app linkkit_gateway linkkitapp modbus_demo mqttapp otaapp tls udataapp udevapp ulocationapp yts"
+linux_only_targets="coapapp helloworld http2app linkkit_gateway linkkitapp modbus_demo mqttapp otaapp tls udataapp udevapp ulocationapp yts"
