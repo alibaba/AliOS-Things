699e76e159e5f45a62b3b1b182ce3c0de710e8d8
jianlong.gaojianlo|高建龙 <jianlong.gaojianlo@alibaba-inc.com>
2019-03-25 17:24:17 +0800

    BugID:19524562: fix dk.dk_audio build error issue
    Change-Id: I0092c60070f8a1f366cad13f7d2a95bdf252e995
    (cherry picked from commit 0831989d7c05896259353efb5bb7ffa5da94d3f4)
    
    BugID:19524562:fix dk.dk_audio build error issue
    
    The issue is fixed after using new head files
    
    Change-Id: I31936b640e55b22426cd487556853f5825a7998b
    (cherry picked from commit 77d63b429fab3882edfec028e7fcb750471431c0)
    
    fix merge issue: new file was put in wrong dir

--- app/example/dk/dk_audio/aos.mk
+++ app/example/dk/dk_audio/aos.mk
@@ -4,7 +4,7 @@ $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := Developer Kit audio example
 ifeq ($(CONFIG_SYSINFO_DEVICE_NAME),developerkit)
-$(NAME)_SOURCES := developerkitaudio.c
+$(NAME)_SOURCES := audioapp.c
 endif
 
 GLOBAL_DEFINES += AOS_NO_WIFI
