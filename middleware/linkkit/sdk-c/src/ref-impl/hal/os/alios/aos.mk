1d0c22819cc7aea228ddf84550febce904f145a2
zhangtian <dongren.zt@alibaba-inc.com>
2019-03-25 16:02:02 +0800

    bugID:19620416:[hal] Compatible with open source mbedlts
    Change-Id: Ie0ff45cc7e82f8043a6ddecc758487b0c4fee1d7

--- middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/aos.mk
+++ middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/aos.mk
@@ -17,6 +17,7 @@ $(NAME)_COMPONENTS += ulog activation chip_code netmgr
 
 ifeq (y,$(strip $(DEV_BIND_ENABLED)))
 $(NAME)_SOURCES += HAL_AWSS_rhino.c
+
 endif
 
 ifeq (y,$(strip $(HAL_CRYPTO)))
@@ -25,7 +26,8 @@ ifeq (y,$(strip $(SUPPORT_ITLS)))
 $(NAME)_COMPONENTS +=  itls
 else
 ifeq (y,$(strip $(SUPPORT_TLS)))
-$(NAME)_COMPONENTS +=  imbedtls
+$(NAME)_DEFINES += USE_PUBLIC_MBEDTLS
+$(NAME)_COMPONENTS +=  mbedtls
 else 
 $(NAME)_DEFINES += USE_PUBLIC_MBEDTLS
 $(NAME)_COMPONENTS +=  mbedtls
@@ -36,16 +38,20 @@ endif
 ifeq (y,$(strip $(SUPPORT_ITLS)))
 $(info SUPPORT_ITLS = y, so using iTLS)
 $(NAME)_SOURCES += HAL_TLS_itls.c    
+$(NAME)_SOURCES += HAL_Crypt_rhino.c 
 $(NAME)_COMPONENTS += itls
 else
 ifeq (y,$(strip $(SUPPORT_TLS)))
 $(info SUPPORT_TLS = y, so using normal TLS)
 $(NAME)_SOURCES += HAL_TLS_mbedtls.c 
-$(NAME)_COMPONENTS += imbedtls
+$(NAME)_SOURCES += HAL_Crypt_rhino.c 
+$(NAME)_COMPONENTS += mbedtls
+
 
 ifeq (y,$(strip $(COAP_SUPPORT_DTLS)))
 $(info COAP_SUPPORT_DTLS = y, so compile DTLS)
 $(NAME)_SOURCES += HAL_DTLS_mbedtls.c
+$(NAME)_COMPONENTS += mbedtls
 GLOBAL_DEFINES += COAP_DTLS_SUPPORT
 endif
 endif
