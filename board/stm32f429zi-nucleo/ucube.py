95e99c74679cd6ddeec9f1dd2c1cbd7acdf760a1
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2019-03-26 18:24:49 +0800

    BugID:17952514: add linkkit keil project to stm32f429zi
    Change-Id: I4bb552ad1a49c89719538c21ed5df0eb1bacb12c

--- board/stm32f429zi-nucleo/ucube.py
+++ board/stm32f429zi-nucleo/ucube.py
@@ -1,2 +1,2 @@
 linux_only_targets="athostapp blink coapapp helloworld http2app httpapp linkkit_gateway linkkitapp meshapp modbus_demo mqttapp otaapp tls udataapp udevapp ulocationapp yts"
-windows_only_targets="helloworld|COMPILER=armcc helloworld|COMPILER=iar"
+windows_only_targets="helloworld|COMPILER=armcc linkkitapp|COMPILER=armcc helloworld|COMPILER=iar"
