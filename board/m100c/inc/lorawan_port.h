738c1d237053fd2d8788a66c37903a7d11aacdb6
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-03-27 11:50:03 +0800

    BugID:19665272: include "k_err.h" is not allowed
    Change-Id: Iee69a7d8b79bf545f0a5018f83081d4fa81405f8

--- board/m100c/inc/lorawan_port.h
+++ board/m100c/inc/lorawan_port.h
@@ -12,50 +12,20 @@ extern "C"
 {
 #endif
 
-/* Includes ------------------------------------------------------------------*/
-#include <string.h>
-#include <stdio.h>
-#include <stddef.h>
-#include <stdint.h>
-
 #include "hal/lorawan.h"
 #include "utilities.h"
 
-// rhino header files
-#include <k_config.h>
-#include <k_default_config.h>
-#include <k_types.h>
-#include <k_err.h>
-#include <k_critical.h>
-#include <k_spin_lock.h>
-#include <k_sys.h>
-#include <k_list.h>
-#include <k_obj.h>
-#include <k_sched.h>
-#include <k_task.h>
-#include <k_ringbuf.h>
-#include <k_queue.h>
-#include <k_buf_queue.h>
-#include <k_sem.h>
-#include <k_task_sem.h>
-#include <k_mutex.h>
-#include <k_event.h>
-#include <k_stats.h>
-#include <k_mm_debug.h>
-#include <k_mm_blk.h>
-#include <k_mm_region.h>
-#include <k_mm.h>
-#include <k_workqueue.h>
-#include <k_internal.h>
-#include <k_trace.h>
-#include <k_soc.h>
-#include <k_hook.h>
-#include <port.h>
-#include <k_bitmap.h>
+/* NOTICE:
+   "TIMER_CMD_START" conflict in "k_timer.h" and "efm32g_timer.h",
+   so "K_TIMER_H" is defined for exclude "k_timer.h" in "k_api.h" */
+#define K_TIMER_H
+/* rhino header files */
+#include "k_api.h"
+#undef  K_TIMER_H
 
-    extern hal_lrwan_dev_chg_mode_t aos_lrwan_chg_mode;
-    extern hal_lrwan_sys_t          aos_lrwan_sys;
-    extern hal_manufactory_itf_t    aos_mft_itf;
+extern hal_lrwan_dev_chg_mode_t aos_lrwan_chg_mode;
+extern hal_lrwan_sys_t          aos_lrwan_sys;
+extern hal_manufactory_itf_t    aos_mft_itf;
 
 #ifdef __cplusplus
 }
