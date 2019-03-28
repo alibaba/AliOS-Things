569b328fd9aa7d9d0414ee1c814b1c0bd46e7501
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-03-28 19:15:55 +0800

    BugID:19092717: Update bk7231U code from BK(github 859 pull request)
    Change-Id: I58ab202b1f741a1ea90a31096cb4bd91110a842b

--- board/bk7231udevkitc/aos.mk
+++ board/bk7231udevkitc/aos.mk
@@ -30,7 +30,7 @@ GLOBAL_CFLAGS += -DAOS_OTA_BANK_SINGLE
 GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
 GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
 
-GLOBAL_LDS_INCLUDES += $($(NAME)_LOCATION)/bk7231udevkitc.ld.S
+GLOBAL_LDFLAGS += -L $(SOURCE_ROOT)/board/bk7231udevkitc
 
 # Extra build target include bootloader, and copy output file to eclipse debug file (copy_output_for_eclipse)
 EXTRA_TARGET_MAKEFILES +=  $($(HOST_MCU_FAMILY)_LOCATION)/gen_crc_bin.mk
---
+++ board/bk7231udevkitc/board.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#define HARDWARE_REVISION   "V1.0"
+#define MODEL               "BK7231U"
+
+#define STDIO_UART 0
+#define STDIO_UART_BUADRATE 115200
+
+#define BK_UART  1   ///first uart
+#define BK_UART_BUADRATE 115200  ///bps
+
