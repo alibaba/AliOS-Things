a608230999500b692384fc76f1a9d45d586ee46e
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-03-29 00:10:48 +0800

    BugID:19711268:AT parser multiple dev support
    Change-Id: I59d4c66fa2071203e750dc49326a8231dac24b54

--- drivers/sal/wifi/athost/athost.c
+++ drivers/sal/wifi/athost/athost.c
@@ -10,6 +10,9 @@
 #include "aos/kernel"
 #include "ulog/ulog.h"
 
+#include <atcmd_config_platform.h>
+#include <atcmd_config_module.h>
+
 #include <atparser.h>
 #include <sal_import.h>
 
@@ -31,6 +34,7 @@
 #define STOP_AUTOCONN_CMD_LEN (sizeof(STOP_AUTOCONN_CMD)+1+1+5+1)
 
 #define AT_RESET_CMD "AT"
+#define AT_CMD_EHCO_OFF "AT+UARTE=OFF"
 
 typedef int (*at_data_check_cb_t)(char data);
 
@@ -54,12 +58,14 @@ static netconn_client_status_notify_t g_netconn_client_status_cb;
 static int socket_data_info_get(char *buf, uint32_t buflen, at_data_check_cb_t valuecheck);
 static int socket_ip_info_check(char data);
 static int socket_data_len_check(char data);
+static uart_dev_t uart_dev;
+int at_dev_fd = -1;
 
 static void handle_tcp_udp_client_conn_state(uint8_t link_id)
 {
     char s[32] = {0};
 
-    at_read(s, 6);
+    at_read(at_dev_fd, s, 6);
     if (strstr(s, "CLOSED") != NULL) {
         LOGI(TAG, "Server closed event.");
         if (aos_sem_is_valid(&g_link[link_id].sem_close)) {
@@ -69,14 +75,14 @@ static void handle_tcp_udp_client_conn_state(uint8_t link_id)
         LOGI(TAG, "Server conn (%d) closed.", link_id);
     } else if (strstr(s, "CONNEC") != NULL) {
         LOGI(TAG, "Server conn (%d) successful.", link_id);
-        at_read(s, 3);
+        at_read(at_dev_fd, s, 3);
         if (aos_sem_is_valid(&g_link[link_id].sem_start)) {
             LOGD(TAG, "sem is going to be waked up: 0x%x", &g_link[link_id].sem_start);
             aos_sem_signal(&g_link[link_id].sem_start); // wakeup send task
         }
     } else if (strstr(s, "DISCON") != NULL) {
         LOGI(TAG, "Server conn (%d) disconnected.", link_id);
-        at_read(s, 6);
+        at_read(at_dev_fd, s, 6);
     } else {
         LOGW(TAG, "No one handle this unkown event!!!");
     }
@@ -108,7 +114,7 @@ static void handle_remote_client_conn_state()
     char ipaddr[16] = {0};
 
     /* Eat the "LIENT," */
-    at_read(reader, 6);
+    at_read(at_dev_fd, reader, 6);
     if (memcmp(reader, "LIENT,", strlen("LIENT,")) != 0) {
         LOGE(TAG, "0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x invalid event format!!!\r\n",
              reader[0], reader[1], reader[2], reader[3], reader[4], reader[5]);
@@ -203,7 +209,7 @@ static int socket_data_info_get(char *buf, uint32_t buflen, at_data_check_cb_t v
     }
 
     do {
-        at_read(&buf[i], 1);
+        at_read(at_dev_fd, &buf[i], 1);
         if (buf[i] == ','
 #ifdef SAL_SERVER
             || buf[i] == '\r'
@@ -241,7 +247,7 @@ static void handle_socket_data()
     char single;
 
     /* Eat the "OCKET," */
-    at_read(reader, 6);
+    at_read(at_dev_fd, reader, 6);
     if (memcmp(reader, "OCKET,", strlen("OCKET,")) != 0) {
         LOGE(TAG, "0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x invalid event format!!!\r\n",
              reader[0], reader[1], reader[2], reader[3], reader[4], reader[5]);
@@ -294,13 +300,13 @@ static void handle_socket_data()
         return;
     }
 
-    ret = at_read(recvdata, len);
+    ret = at_read(at_dev_fd, recvdata, len);
     if (ret != len) {
         LOGE(TAG, "at read error recv %d want %d!\n", ret, len);
         goto err;
     }
 
-    at_read(&single, 1);
+    at_read(at_dev_fd, &single, 1);
     if (single != '\r') {
         LOGE(TAG, "at fail to read delimiter %d after data %d!\n", '\r', single);
         goto err;
@@ -336,7 +342,7 @@ static void handle_udp_broadcast_data()
     char *recvdata = NULL;
 
     /* Eat the "DP_BROADCAST," */
-    at_read(reader, 13);
+    at_read(at_dev_fd, reader, 13);
     if (memcmp(reader, "DP_BROADCAST,", strlen("DP_BROADCAST,")) != 0) {
         LOGE(TAG, "%s invalid event format!!!\r\n",
              reader[0], reader[1], reader[2], reader[3], reader[4], reader[5]);
@@ -391,7 +397,7 @@ static void handle_udp_broadcast_data()
         return;
     }
 
-    at_read(recvdata, len);
+    at_read(at_dev_fd, recvdata, len);
     recvdata[len] = '\0';
 
     hal_wifi_ip_stat_t ip_stat;
@@ -427,19 +433,19 @@ static void net_event_handler(void *arg, char *buf, int buflen)
     char s[32] = {0};
     LOGD(TAG, "%s entry.", __func__);
 
-    at_read(&c, 1);
+    at_read(at_dev_fd, &c, 1);
     if (c >= '0' && c < ('0' + LINK_ID_MAX)) {
         int link_id = c - '0';
-        at_read(&c, 1);
+        at_read(at_dev_fd, &c, 1);
         if (c != ',') {
             LOGE(TAG, "!!!Error: wrong CIPEVENT string. 0x%02x\r\n", c);
             return;
         }
-        at_read(&c, 1);
+        at_read(at_dev_fd, &c, 1);
         if (c == 'S') {
             LOGD(TAG, "%s server conn state event, linkid: %d.", __func__, link_id);
             /* Eat the "ERVER," */
-            at_read(s, 6);
+            at_read(at_dev_fd, s, 6);
             if (memcmp(s, "ERVER,", strlen("ERVER,")) != 0) {
                 LOGE(TAG, "invalid event format 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x 0x%02x",
                      s[0], s[1], s[2], s[3], s[4], s[5]);
@@ -449,7 +455,7 @@ static void net_event_handler(void *arg, char *buf, int buflen)
         } else if (c == 'U') {
             LOGD(TAG, "%s UDP conn state event.", __func__);
             /* Eat the "DP," */
-            at_read(s, 3);
+            at_read(at_dev_fd, s, 3);
             if (memcmp(s, "DP,", strlen("DP,")) != 0) {
                 LOGE(TAG, "%s invalid event format 0x%02x 0x%02x 0x%02x \r\n", __FUNCTION__, s[0], s[1], s[2]);
                 return;
@@ -481,7 +487,7 @@ static void athost_uart_echo_off()
 {
     char out[64] = {0};
 
-    at_send_wait_reply(AT_CMD_EHCO_OFF, strlen(AT_CMD_EHCO_OFF), true,
+    at_send_wait_reply(at_dev_fd, AT_CMD_EHCO_OFF, strlen(AT_CMD_EHCO_OFF), true,
                        NULL, 0, out, sizeof(out), NULL);
     LOGD(TAG, "The AT response is: %s", out);
     if (strstr(out, AT_RSP_FAIL) != NULL) {
@@ -519,7 +525,7 @@ int HAL_SAL_Init(void)
         snprintf(cmd, STOP_CMD_LEN - 1, "%s=%d", STOP_CMD, link);
         LOGD(TAG, "%s %d - AT cmd to run: %s", __func__, __LINE__, cmd);
 
-        at_send_wait_reply(cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
+        at_send_wait_reply(at_dev_fd, cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
         LOGD(TAG, "The AT response is: %s", out);
         if (strstr(out, AT_RSP_FAIL) != NULL) {
             LOGD(TAG, "%s %d failed", __func__, __LINE__);
@@ -532,7 +538,7 @@ int HAL_SAL_Init(void)
         snprintf(cmd, STOP_AUTOCONN_CMD_LEN - 1, "%s=%d,0", STOP_AUTOCONN_CMD, link);
         LOGD(TAG, "%s %d - AT cmd to run: %s", __func__, __LINE__, cmd);
 
-        at_send_wait_reply(cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
+        at_send_wait_reply(at_dev_fd, cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
         LOGD(TAG, "The AT response is: %s", out);
         if (strstr(out, AT_RSP_FAIL) != NULL) {
             LOGE(TAG, "%s %d failed", __func__, __LINE__);
@@ -541,7 +547,7 @@ int HAL_SAL_Init(void)
         memset(cmd, 0, sizeof(cmd));
     }
 
-    at_register_callback(NET_OOB_PREFIX, NULL, 0, net_event_handler, NULL);
+    at_register_callback(NET_OOB_PREFIX, NULL, NULL, 0, net_event_handler, NULL);
     inited = 1;
 
     return 0;
@@ -641,7 +647,7 @@ int HAL_SAL_Start(sal_conn_t *c)
 
     LOGD(TAG, "\r\n%s %d - AT cmd to run: %s \r\n", __func__, __LINE__, cmd);
 
-    at_send_wait_reply(cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
+    at_send_wait_reply(at_dev_fd, cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
     LOGD(TAG, "The AT response is: %s", out);
     if (strstr(out, AT_RSP_FAIL) != NULL) {
         LOGE(TAG, "%s %d failed", __func__, __LINE__);
@@ -739,7 +745,7 @@ int HAL_SAL_Send(int fd,
 
     LOGD(TAG, "\r\n%s %d - AT cmd to run: %s\r\n", __func__, __LINE__, cmd);
 
-    at_send_wait_reply((const char *)cmd, strlen(cmd), true, (const char *)data, len,
+    at_send_wait_reply(at_dev_fd, (const char *)cmd, strlen(cmd), true, (const char *)data, len,
                         out, sizeof(out), NULL);
     LOGD(TAG, "\r\nThe AT response is: %s\r\n", out);
 
@@ -763,7 +769,7 @@ int HAL_SAL_DomainToIp(char *domain,
     snprintf(cmd, DOMAIN_CMD_LEN - 1, "%s=%s", DOMAIN_CMD, domain);
     LOGD(TAG, "%s %d - AT cmd to run: %s", __func__, __LINE__, cmd);
 
-    at_send_wait_reply(cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
+    at_send_wait_reply(at_dev_fd, cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
     LOGD(TAG, "The AT response is: %s", out);
     if (strstr(out, AT_RECV_SUCCESS_POSTFIX) == NULL) {
         LOGE(TAG, "%s %d failed", __func__, __LINE__);
@@ -833,7 +839,7 @@ int HAL_SAL_Close(int fd,
     snprintf(cmd, STOP_CMD_LEN - 1, "%s=%d", STOP_CMD, link_id);
     LOGD(TAG, "%s %d - AT cmd to run: %s", __func__, __LINE__, cmd);
 
-    at_send_wait_reply(cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
+    at_send_wait_reply(at_dev_fd, cmd, strlen(cmd), true, NULL, 0, out, sizeof(out), NULL);
     LOGD(TAG, "The AT response is: %s", out);
     if (strstr(out, AT_RSP_FAIL) != NULL) {
         LOGE(TAG, "%s %d failed", __func__, __LINE__);
@@ -885,7 +891,33 @@ static int athost_client_status_notify(netconn_client_status_notify_t cb)
 
 int sal_device_init(void)
 {
+    at_config_t at_config = { 0 };
+
     at_init();
+
+    /* uart_dev should be maintained in whole life cycle */
+    uart_dev.port                = AT_UART_PORT;
+    uart_dev.config.baud_rate    = AT_UART_BAUDRATE;
+    uart_dev.config.data_width   = AT_UART_DATA_WIDTH;
+    uart_dev.config.parity       = AT_UART_PARITY;
+    uart_dev.config.stop_bits    = AT_UART_STOP_BITS;
+    uart_dev.config.flow_control = AT_UART_FLOW_CONTROL;
+    uart_dev.config.mode         = AT_UART_MODE;
+
+    /* configure and add one uart dev */
+    at_config.type                             = AT_DEV_UART;
+    at_config.port                             = AT_UART_PORT;
+    at_config.dev_cfg                          = &uart_dev;
+    at_config.send_delimiter                   = AT_SEND_DELIMITER;
+    at_config.reply_cfg.reply_prefix           = AT_RECV_PREFIX;
+    at_config.reply_cfg.reply_success_postfix  = AT_RECV_SUCCESS_POSTFIX;
+    at_config.reply_cfg.reply_fail_postfix     = AT_RECV_FAIL_POSTFIX;
+
+   if ((at_dev_fd = at_add_dev(&at_config)) < 0) {
+        LOGE(TAG, "AT parser device add failed!\n");
+        return -1;
+    }
+
     return 0;
 }
 
