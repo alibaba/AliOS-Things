5f7df0c1f79eab647089af6add04231cc499a313
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2019-03-29 10:47:10 +0800

    BugID:19718144:modified sv6266 xtal from 25 to 40
    Change-Id: Ie017efcdb4809713d55e7d7b2a8f211fbf39f2c1

--- platform/mcu/sv6266/hal/ota_port.c
+++ platform/mcu/sv6266/hal/ota_port.c
@@ -42,7 +42,7 @@ const char* bootflag = "bootflag";
 int hal_ota_switch_to_new_fw()
 {
 #define M_OTA_HEADER_CHECK      (6)
-	uint32_t reserved_addr = (uint32_t)(&__lds_reserved_start) - (uint32_t)(&FLASH_BEGIN);
+    uint32_t reserved_addr = (uint32_t)(&__lds_reserved_start) - (uint32_t)(&FLASH_BEGIN);
     uint32_t ota_data_check[M_OTA_HEADER_CHECK];
     OS_DeclareCritical();
 
@@ -65,7 +65,7 @@ int hal_ota_switch_to_new_fw()
     if (ota_data_check[5] != SETTING_PSRAM_HEAP_BASE) {
         return -7;
     }
-	OS_EnterCritical();
+    OS_EnterCritical();
 #if defined(CONFIG_ENABLE_WDT)
     drv_wdt_kick(SYS_WDT);
 #endif
@@ -73,107 +73,33 @@ int hal_ota_switch_to_new_fw()
 #if defined(CONFIG_ENABLE_WDT)
     drv_wdt_kick(SYS_WDT);
 #endif
-	flash_page_program(reserved_addr, strlen(bootflag), bootflag);
+    flash_page_program(reserved_addr, strlen(bootflag), bootflag);
 #if defined(CONFIG_ENABLE_WDT)
     drv_wdt_kick(SYS_WDT);
 #endif
-	OS_ExitCritical();
-	REG32(0xc0000000) = 0x00200000;
+    OS_ExitCritical();
+    REG32(0xc0000000) = 0x00200000;
     return 0;
 }
 
 
 unsigned int _off_set = 0;
-static int sv6266_ota_init(hal_ota_module_t *m, void *something)
-{
-    hal_partition_t pno = HAL_PARTITION_OTA_TEMP;
-    hal_logic_partition_t *partition_info = hal_flash_get_info( pno );
-
-    printf("ota module init\n");
-    _off_set = *(uint32_t*)something;
-    
-    if(_off_set==0) {
-        if (hal_flash_erase(pno, 0 ,partition_info->partition_length) != 0) {
-            return 1;
-        }
-        //CRC16_Init( &contex );
-        memset(&ota_info, 0 , sizeof ota_info);
-    } else {
-        //contex.crc=hal_ota_get_crc16();
-        ota_info.ota_len=_off_set;
-        //printf("--------get crc16 context.crc=%d!--------\n",contex.crc);
-    }
-
-    return 0;
-}
-
-
-static int sv6266_ota_write(hal_ota_module_t *m, volatile uint32_t* off_set, uint8_t* in_buf ,uint32_t in_buf_len)
-{
-    hal_partition_t pno = HAL_PARTITION_OTA_TEMP;
-    int ret;
-    
-    //printf("[%s] offset=%Xh, len=%d\n", __func__, *off_set, in_buf_len);
-    printf("!");
-    //CRC16_Update( &contex, in_buf, in_buf_len);
-
-    ret = hal_flash_write(pno, &_off_set, in_buf, in_buf_len);
-    ota_info.ota_len += in_buf_len;
-    if (ret != 0) {
-        return 1;
-    }
-    return ret;
-}
-
-static int sv6266_ota_read(hal_ota_module_t *m,  volatile uint32_t* off_set, uint8_t* out_buf, uint32_t out_buf_len)
-{
-    hal_partition_t pno = HAL_PARTITION_OTA_TEMP;
-    LOG(">>> OffSet %d, len %d",*off_set,out_buf_len);
-    hal_flash_read(pno, (uint32_t*)off_set, out_buf, out_buf_len);
-    //printf("[%s]\n", __func__);
-   
-    return 0;
-}
 
 static int sv6266_ota_set_boot(hal_ota_module_t *m, void *something)
 {
     ota_finish_param_t *param = (ota_finish_param_t *)something;
 
-    if (param==NULL){
+    if (param == NULL) {
         return kGeneralErr;
     }
-    if (param->result_type==OTA_FINISH)
-    {
+    if (param->result_type == OTA_FINISH) {
         //CRC16_Final( &contex, (uint16_t *)&ota_info.ota_crc );
         printf("switch to new fw\n");
         memset(&ota_info, 0 , sizeof ota_info);
         if (hal_ota_switch_to_new_fw() != 0) {
             return kGeneralErr;
         }
-    } else if (param->result_type==OTA_BREAKPOINT) {
-        printf("OTA package is incomplete!\n");
-        //hal_ota_save_crc16(contex.crc);
     }
     return kNoErr;
 }
-
-//static uint16_t hal_ota_get_crc16(void)
-//{
-//    int len = 2;
-//    uint16_t crc16=0;
-//    aos_kv_get(KV_HAL_OTA_CRC16, &crc16, &len);
-//    return crc16;
-//}
-//
-//static void  hal_ota_save_crc16(uint16_t crc16)
-//{
-//    aos_kv_set(KV_HAL_OTA_CRC16, &crc16, 2, 1);
-//}
-
-struct hal_ota_module_s sv6266_ota_module = {
-    .init = sv6266_ota_init,
-    .ota_write = sv6266_ota_write,
-    .ota_read = sv6266_ota_read,
-    .ota_set_boot = sv6266_ota_set_boot,
-};
 
