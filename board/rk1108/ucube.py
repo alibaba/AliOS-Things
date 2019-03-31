367871c4333a54346da4678d8a63654ef12ef92d
Li Cheng <lc122798@alibaba-inc.com>
2019-04-01 02:12:52 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Iae50e4790887c0cd54bd76e64c78c8dedf87deca

--- board/rk1108/ucube.py
+++ board/rk1108/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="athostapp coapapp helloworld http2app linkkit_gateway linkkitapp mqttapp otaapp tls udevapp ulocationapp yts"
+linux_only_targets="athostapp coapapp helloworld http2app httpapp linkkit_gateway linkkitapp meshapp mqttapp otaapp tls udevapp ulocationapp yts"
