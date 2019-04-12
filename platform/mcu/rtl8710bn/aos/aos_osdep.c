f313fa481eeaf02bc2a997c669f8c8e2043459d7
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-04-12 19:34:58 +0800

    BugID:19864048:Update rtl8710 wifi driver for fixing coredump and compatability issue
    Change-Id: I4a81c8aaecc29cb6c35b83904d6045938ee31078
    
    SCM: remove useless merge files
    
    Change-Id: I7d6b986023640cdaf012870a04a6d753c7886338

--- platform/mcu/rtl8710bn/aos/aos_osdep.c
+++ platform/mcu/rtl8710bn/aos/aos_osdep.c
@@ -644,7 +644,7 @@ int _aos_timer_change_no_repeat(aos_timer_t *timer, int ms)
     if (ret != RHINO_SUCCESS) {
         return ret;
     }
-    
+
     ret = krhino_timer_change(timer->hdl, MS2TICK(ms), 0);
     if (ret == RHINO_SUCCESS) {
         return 0;
@@ -701,7 +701,8 @@ u32  _aos_timerChangePeriodFromISR( _timerHandle xTimer,
     if(xNewPeriod == 0)
         xNewPeriod += 1;
 
-    (u32)aos_timer_stop(&xTimer->timer);
+    (u32)aos_timer_stop(&xTimer->timer);  
+
     return !aos_timer_change(&xTimer->timer, xNewPeriod);
 }
 
