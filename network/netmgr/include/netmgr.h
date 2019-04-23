24f9b1fa77e50bdd029bed0921c3d19003baff4f
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-04-23 22:05:32 +0800

    BugID:20072103:Add netmgr connect api
    Change-Id: Ief0894238c43e9cb87d648722573f2b365841dc8

--- network/netmgr/include/netmgr.h
+++ network/netmgr/include/netmgr.h
@@ -20,6 +20,20 @@ int netmgr_init(void);
 void netmgr_deinit(void);
 int netmgr_start(bool autoconfig);
 
+#ifdef NET_WITH_wIFI
+/**
+ *
+ * connect to specified SSID and password
+ *
+ * @param[in] ssid     WiFi SSID
+ * @param[in] password WiFi password
+ *
+ * @return 0   begin to connect success
+ * @return < 0 begin to connect fail
+ */
+int32_t netmgr_connect(const char *ssid, const uint8_t *password);
+#endif
+
 #if defined(__cplusplus)
 }
 #endif
