c4fc8a98ff4096773a47898e0fb30c9ff443997f
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-04-23 15:25:48 +0800

    bugID:20105742:[breeze] Device activation report function.
    Change-Id: I3a26001258cb42dbc56316e8d9363b6fc2ffe1eb

--- network/bluetooth/breeze/include/breeze_hal_os.h
+++ network/bluetooth/breeze/include/breeze_hal_os.h
@@ -114,4 +114,14 @@ int os_kv_del(const char *key);
  */
 int os_rand(void);
 
+/**
+ * get platform's os info(AliOS Things or non-AliOS Things), and vendor's info.
+ * @param[in-out]     info       Device's OS info and chip's info with the format".
+ * @param[in-out]     p_len      Length of the OS info and vendor's info.
+ *
+ * @return  0 on success, negative error on failure.
+ */
+
+int os_get_version_chip_info(char* info, uint8_t* p_len);
+
 #endif
