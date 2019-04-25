009ea247b6e29f736c46f2fbf38a1f392f2eecf0
zhangtian <dongren.zt@alibaba-inc.com>
2019-04-25 20:47:14 +0800

    bugID:20246360:[coap] delete 4 useless hal api and fix coap psk mode Change-Id: I86034d2316fc0adfac9e168cf58ef6caa2163aa5

--- middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/HAL_UDP_rhino.c
+++ middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/HAL_UDP_rhino.c
@@ -38,51 +38,49 @@
  */
 intptr_t HAL_UDP_create(_IN_ char *host, _IN_ unsigned short port)
 {
-    int                flag      = 1;
-    int                ret       = -1;
-    int                socket_id = -1;
-    struct sockaddr_in local_addr; /*local addr*/
+    int                     rc = -1;
+    long                    socket_id = -1;
+    char                    port_ptr[6] = {0};
+    struct addrinfo         hints;
+    char                    addr[NETWORK_ADDR_LEN] = {0};
+    struct addrinfo        *res, *ainfo;
+    struct sockaddr_in     *sa = NULL;
 
-    if ((socket_id = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        fprintf(stderr, "socket create failed\r\n");
-        return -1;
+    if (NULL == host) {
+        return (-1);
     }
 
-    ret = setsockopt(socket_id, SOL_SOCKET, SO_REUSEADDR, &flag, sizeof(flag));
-    if (ret < 0) {
-        close(socket_id);
-        fprintf(stderr, "setsockopt SO_REUSEADDR failed");
-        return -1;
+    sprintf(port_ptr, "%u", port);
+    memset((char *)&hints, 0x00, sizeof(hints));
+    hints.ai_socktype = SOCK_DGRAM;
+    hints.ai_family = AF_INET;
+    hints.ai_protocol = IPPROTO_UDP;
+
+    rc = getaddrinfo(host, port_ptr, &hints, &res);
+    if (0 != rc) {
+        platform_err("getaddrinfo error: %d", rc);
+        return (-1);
     }
 
-    flag = 1;
-#ifdef IP_RECVPKTINFO
-    if ((ret = setsockopt(socket_id, IPPROTO_IP, IP_RECVPKTINFO, &flag,
-                          sizeof(flag))) < 0)
-#else  /* IP_RECVPKTINFO */
-    if ((ret = setsockopt(socket_id, IPPROTO_IP, IP_PKTINFO, &flag,
-                          sizeof(flag))) < 0)
-#endif /* IP_RECVPKTINFO */
-        if (ret < 0) {
+    for (ainfo = res; ainfo != NULL; ainfo = ainfo->ai_next) {
+        if (AF_INET == ainfo->ai_family) {
+            sa = (struct sockaddr_in *)ainfo->ai_addr;
+            inet_ntop(AF_INET, &sa->sin_addr, addr, NETWORK_ADDR_LEN);
+
+            socket_id = socket(ainfo->ai_family, ainfo->ai_socktype, ainfo->ai_protocol);
+            if (socket_id < 0) {
+                platform_err("create socket error");
+                continue;
+            }
+            if (0 == connect(socket_id, ainfo->ai_addr, ainfo->ai_addrlen)) {
+                break;
+            }
+
             close(socket_id);
-            fprintf(stderr, "setsockopt IP_PKTINFO failed\r\n");
-            return -1;
         }
-
-
-    memset(&local_addr, 0x00, sizeof(local_addr));
-    local_addr.sin_family = AF_INET;
-    if (NULL != host) {
-        inet_aton(host, &local_addr.sin_addr);
-    } else {
-        local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     }
-    local_addr.sin_port = htons(port);
-    ret = bind(socket_id, (struct sockaddr *)&local_addr, sizeof(local_addr));
+    freeaddrinfo(res);
 
-    // fprintf(stderr,"\r\n[%s LINE #%d]  Create socket port %d fd %d ret
-    // %d\r\n",
-    //                    __FILE__, __LINE__, port, socket_id, ret);
     return socket_id;
 }
 
@@ -141,11 +139,6 @@ int HAL_UDP_close_without_connect(_IN_ intptr_t sockfd)
     return close((int)sockfd);
 }
 
-int HAL_UDP_connect(_IN_ intptr_t sockfd, _IN_ const char *host,
-                    _IN_ unsigned short port)
-{
-    return 0;
-}
 
 int HAL_UDP_recvfrom(intptr_t sockfd, NetworkAddr *p_remote,
                      unsigned char *p_data, unsigned int datalen,
@@ -200,12 +193,6 @@ int HAL_UDP_recvfrom(intptr_t sockfd, NetworkAddr *p_remote,
     return count;
 }
 
-int HAL_UDP_recv(intptr_t sockfd, unsigned char *p_data, unsigned int datalen,
-                 unsigned int timeout_ms)
-{
-    return 0;
-}
-
 
 int HAL_UDP_sendto(intptr_t sockfd, const NetworkAddr *p_remote,
                    const unsigned char *p_data, unsigned int datalen,
@@ -235,51 +222,6 @@ int HAL_UDP_sendto(intptr_t sockfd, const NetworkAddr *p_remote,
     return rc;
 }
 
-int HAL_UDP_send(_IN_ intptr_t sockfd, _IN_ const unsigned char *p_data,
-                 _IN_ unsigned int datalen, _IN_ unsigned int timeout_ms)
-{
-    int            ret;
-    fd_set         write_fds;
-    struct timeval timeout = { timeout_ms / 1000, (timeout_ms % 1000) * 1000 };
-
-    FD_ZERO(&write_fds);
-    FD_SET(sockfd, &write_fds);
-
-    ret = select(sockfd + 1, NULL, &write_fds, NULL, &timeout);
-    if (ret == 0) {
-        return 0; /* write timeout */
-    }
-
-    if (ret < 0) {
-        if (errno == EINTR) {
-            return -3; /* want write */
-        }
-        return -4; /* write failed */
-    }
-
-    ret = send(sockfd, (char *)p_data, (int)datalen, 0);
-
-    if (ret < 0) {
-        platform_err("send");
-    }
-
-    return ret;
-}
-
-int HAL_UDP_read(intptr_t p_socket, unsigned char *p_data, unsigned int datalen)
-{
-    long socket_id = -1;
-    int  count     = -1;
-
-    if (NULL == p_data || 0 == p_socket) {
-        return -1;
-    }
-
-    socket_id = (long)p_socket;
-    count     = (int)read(socket_id, p_data, datalen);
-
-    return count;
-}
 
 int HAL_UDP_joinmulticast(intptr_t sockfd, char *p_group)
 {
@@ -338,6 +280,7 @@ int HAL_UDP_write(intptr_t p_socket, const unsigned char *p_data,
     return rc;
 }
 
+
 int HAL_UDP_readTimeout(intptr_t p_socket, unsigned char *p_data,
                         unsigned int datalen, unsigned int timeout)
 {
@@ -378,5 +321,6 @@ int HAL_UDP_readTimeout(intptr_t p_socket, unsigned char *p_data,
     }
 
     /* This call will not block */
-    return HAL_UDP_read(p_socket, p_data, datalen);
+    return  read((long)p_socket, p_data, datalen);
+
 }
