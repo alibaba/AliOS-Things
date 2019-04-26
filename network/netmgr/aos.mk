8659667f1a2bf87c9181f07974f78d5fc8349ef1
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-04-26 10:13:00 +0800

    BugID:20072103:Refactor netmgr code structure and API specs
    Change-Id: I95a35b2281deae377262996ad4bdf54f9693fae6

--- network/netmgr/aos.mk
+++ network/netmgr/aos.mk
@@ -3,7 +3,7 @@ NAME := netmgr
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := network manager manages different types of RF.
-$(NAME)_SOURCES-y := netmgr.c
+$(NAME)_SOURCES-y := src/netmgr.c
 
 #default gcc
 ifeq ($(COMPILER),)
@@ -15,20 +15,20 @@ endif
 ifeq (y,$(AOS_NET_WITH_CELLULAR))
 GLOBAL_DEFINES-y += NET_WITH_CELLULAR
 $(NAME)_SOURCES-y += hal/cellular.c
-$(NAME)_SOURCES-y += interfaces/netmgr_cellular.c
+$(NAME)_SOURCES-y += src/netmgr_cellular.c
 GLOBAL_INCLUDES-y += ../include/hal/
 endif
 
 AOS_NET_WITH_WIFI ?= y
 ifeq (y,$(AOS_NET_WITH_WIFI))
-$(NAME)_SOURCES-y += interfaces/netmgr_wifi.c
+$(NAME)_SOURCES-y += src/netmgr_wifi.c
 GLOBAL_DEFINES-y += NET_WITH_WIFI
 $(NAME)_SOURCES-y += hal/wifi.c
 endif
 
 ifeq (y,$(AOS_NET_WITH_BLANK))
 $(NAME)_SOURCES-y += hal/net.c
-$(NAME)_SOURCES-y += interfaces/netmgr_net.c
+$(NAME)_SOURCES-y += src/netmgr_net.c
 GLOBAL_INCLUDES-y += ../include/hal/
 endif
 
