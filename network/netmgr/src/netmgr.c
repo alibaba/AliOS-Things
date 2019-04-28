5c9f2dd38e6b6c67a92563ba8d9d42bbd549da23
wanglu_luwang <wanglu.luwang@alibaba-inc.com>
2019-04-28 11:22:00 +0800

    BugID:18962832:Refine netmgr_connect API with timeout param
    Change-Id: I4410eed0b315ce9ce13c9e64dfc1b2d5b805f5ec

--- network/netmgr/src/netmgr.c
+++ network/netmgr/src/netmgr.c
@@ -3,6 +3,8 @@
  */
 
 #include <stdlib.h>
+#include <string.h>
+#include "aos/kernel.h"
 #include "netmgr.h"
 
 int netmgr_init(void)
@@ -32,10 +34,22 @@ int netmgr_start(bool autoconfig)
 #endif
 }
 
+void netmgr_stats(int32_t interface, netmgr_stats_t *stats)
+{
+    if (stats == NULL) {
+        return;
+    }
+
+    memset(stats, 0, sizeof(netmgr_stats_t));
+    stats->ip_available = netmgr_get_ip_state();
+    netmgr_wifi_get_ip(stats->ip);
+}
+
 #ifdef NET_WITH_WIFI
-int32_t netmgr_connect(const char *ssid, const uint8_t *password)
+int32_t netmgr_connect(const char *ssid, const uint8_t *password, uint32_t timeout)
 {
     netmgr_ap_config_t config;
+    uint32_t time_elapsed = 0;
 
     memset(&config, 0, sizeof(config));
     if (ssid == NULL || strlen(ssid) > MAX_SSID_SIZE ||
@@ -46,8 +60,17 @@ int32_t netmgr_connect(const char *ssid, const uint8_t *password)
     strncpy(config.ssid, ssid, strlen(ssid));
     strncpy(config.pwd, password, strlen(password));
     netmgr_set_ap_config(&config);
-    netmgr_wifi_start(false);
-    return 0;
-}
+    netmgr_reconnect_wifi();
 
+    while (time_elapsed < timeout) {
+        if (netmgr_get_ip_state() == false) {
+            aos_msleep(500);
+            time_elapsed += 500;
+        } else {
+            return 0;
+        }
+    }
+
+    return -2;
+}
 #endif
