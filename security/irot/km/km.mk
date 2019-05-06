80b889913b73b936896e49b1f5c163d5a90949e5
cedar.wxs <cedar.wxs@alibaba-inc.com>
2019-05-06 10:41:08 +0800

    BugID:18838784: Release Security Library With Host ARCH
    Change-Id: Idd8147a1acc51a0d4c3375d358ed5155a41950d5
    Signed-off-by: cedar.wxs <cedar.wxs@alibaba-inc.com>

--- security/irot/km/km.mk
+++ security/irot/km/km.mk
@@ -2,10 +2,4 @@ NAME := libkm
 
 $(NAME)_COMPONENTS := irot.km.platform alicrypto
 
-ifeq ($(HOST_ARCH), linux)
-HOST_NAME := $(HOST_ARCH)
-else
-HOST_NAME := $(shell echo $(CONFIG_SYSINFO_DEVICE_NAME)|tr A-Z a-z)
-endif
-
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_NAME)/libkm.a
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/libkm.a
--- security/itls/itls.mk
+++ security/itls/itls.mk
@@ -10,17 +10,11 @@ ifneq ($(HOST_ARCH), ck802)
 $(NAME)_DEFINES     += CONFIG_KEY_OTP_ENABLED
 endif
 
-ifeq ($(HOST_ARCH), linux)
-HOST_NAME := $(HOST_ARCH)
-else
-HOST_NAME := $(shell echo $(CONFIG_SYSINFO_DEVICE_NAME)|tr A-Z a-z)
-endif
-
 ifeq ($(DEBUG), no)
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_NAME)/libitls.a
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/libitls.a
 else
 GLOBAL_DEFINES           += CONFIG_SSL_DEBUG
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_NAME)/libitls.a.dbg
+$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/libitls.a.dbg
 endif
 
 $(NAME)_COMPONENTS  := alicrypto
