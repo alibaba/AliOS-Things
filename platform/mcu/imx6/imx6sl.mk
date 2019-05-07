357023bd26687dfac1bd3e2fbc38def963979d99
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-05-07 22:16:53 +0800

    BugID:20099139: Fix issue that source is not existing
    Change-Id: I4947457303cb169bc6daf653f5927ead0e8c9234

--- platform/mcu/imx6/imx6sl.mk
+++ platform/mcu/imx6/imx6sl.mk
@@ -1,8 +1,8 @@
 GLOBAL_INCLUDES +=  ./imx6_platform_sdk/board/mx6sl/evk  \
-                    ./imx6_platform_sdk/sdk/include/mx6sl  \
+                    ./imx6_platform_sdk/sdk/include/mx6sl
 
 $(NAME)_SOURCES +=  ./imx6_platform_sdk/board/mx6sl/evk/hardware_modules.c   \
                     ./imx6_platform_sdk/board/mx6sl/evk/board_io_expanders.c   \
                     ./imx6_platform_sdk/board/mx6sl/evk/iomux/uart_iomux_config.c   \
                     ./imx6_platform_sdk/board/mx6sl/evk/iomux/uart1_iomux_config.c   \
-                    ./imx6_platform_sdk/board/mx6sl/evk/iomux/iomux_config.c   \
+                    ./imx6_platform_sdk/board/mx6sl/evk/iomux/iomux_config.c
