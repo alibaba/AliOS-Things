cd9f1d291883ace5e541e8efd164be369b7d828f
yj144830 <yj144830@alibaba-inc.com>
2019-05-14 21:03:49 +0800

    BugID:20250821: add bk7231s support
    Change-Id: I144a98114afb2cd4734b9f81e47e998484e4dd7f

---
+++ board/bk7231sdevkitc/board.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#define HARDWARE_REVISION   "V1.0"
+#define MODEL               "BK7231S"
+
+#define STDIO_UART 0
+#define STDIO_UART_BUADRATE 115200
+
+#define BK_UART  1
+#define BK_UART_BUADRATE 115200
+
