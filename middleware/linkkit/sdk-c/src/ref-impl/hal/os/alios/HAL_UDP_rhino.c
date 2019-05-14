77f2fd34c9af670c9fe756cf0e528fb6b965a91e
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2019-05-14 10:42:09 +0800

    BugID:17952514: fix the build error in keil project
    Change-Id: I213df04c66c9635dfa8c61dcee12e8b6765af141

--- middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/HAL_UDP_rhino.c
+++ middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/HAL_UDP_rhino.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <aos/errno.h>
+#include <errno.h>
 #include "ulog/ulog.h"
 #include "aos/kernel.h"
 #include <network/network.h>
