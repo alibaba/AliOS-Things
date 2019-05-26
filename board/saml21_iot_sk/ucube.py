a5c680867d58b9efc2178f03c332e0b228af5614
Li Cheng <lc122798@alibaba-inc.com>
2019-05-27 02:15:23 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Ic9a3990079831e3325744fa2d1866aaff8aa9e65

--- board/saml21_iot_sk/ucube.py
+++ board/saml21_iot_sk/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="coapapp helloworld modbus_demo mqttapp otaapp udataapp udevapp yts"
+linux_only_targets="coapapp helloworld modbus_demo mqttapp otaapp udata_demo.sensor_local_demo udata_demo.udata_local_demo udataapp udevapp yts"
