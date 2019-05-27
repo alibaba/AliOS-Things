474e113be44b5910f7dd453da526276e3a862d3f
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-05-27 12:02:00 +0800

    [PATCH] bugID:20751849:[breeze]Awss, misc change for non ali ble stack.
    Change-Id: I430dcf361c45cb18551491f08f0e990ca5400682

--- board/bk7231udevkitc/board.c
+++ board/bk7231udevkitc/board.c
@@ -3,7 +3,7 @@
  */
 
 #include "aos/hal/flash.h"
-#include <aos/kernel.h>
+#include "aos/kernel.h"
 
 /* Logic partition on flash devices */
 const hal_logic_partition_t hal_partitions[] =
@@ -13,34 +13,33 @@ const hal_logic_partition_t hal_partitions[] =
 	    .partition_owner            = HAL_FLASH_EMBEDDED,
 	    .partition_description      = "Bootloader",
 	    .partition_start_addr       = 0x000000,
-        .partition_length           = 0x8000, //32k bytes
+        .partition_length           = 0x10000, //64k bytes
 	    .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
-	},
-	/*reserved 2k*/
-	[HAL_PARTITION_APPLICATION] =
-	{
-	    .partition_owner            = HAL_FLASH_EMBEDDED,
-	    .partition_description      = "Application",
-	    .partition_start_addr       = 0x008800,//cpu is 0x8000
-        .partition_length           = 0xF0000, //960k bytes
-	    .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
-	},
-	/*reserved 26k*/
-	[HAL_PARTITION_OTA_TEMP] =
-	{
-		.partition_owner		   = HAL_FLASH_EMBEDDED,
-		.partition_description	   = "OTA Storage",
-		.partition_start_addr	   = 0x0FF000,//cpu is 0xF0000
-		.partition_length		   = 0xF0000, //960k bytes
-		.partition_options		   = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
 	},
 	[HAL_PARTITION_PARAMETER_1] =
 	{
 		.partition_owner			= HAL_FLASH_EMBEDDED,
 		.partition_description		= "PARAMETER1",
-		.partition_start_addr		= 0x1EF000,// boot information need protect
+		.partition_start_addr		= 0x010000,// boot information need protect
 		.partition_length			= 0x1000, // 4k bytes
 		.partition_options			= PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
+	},
+	[HAL_PARTITION_APPLICATION] =
+	{
+	    .partition_owner            = HAL_FLASH_EMBEDDED,
+	    .partition_description      = "Application",
+	    .partition_start_addr       = 0x011000,//cpu is 0x10000
+        .partition_length           = 0xE1000, //900k bytes
+	    .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
+	},
+	/*reserved 52k*/
+	[HAL_PARTITION_OTA_TEMP] =
+	{
+		.partition_owner		   = HAL_FLASH_EMBEDDED,
+		.partition_description	   = "OTA Storage",
+		.partition_start_addr	   = 0x0FF000,//cpu is 0xF0000
+		.partition_length		   = 0xE1000, //900k bytes
+	    .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
 	},
 	/*protected flash size is 0~1984k. below are not protected.*/
     [HAL_PARTITION_PARAMETER_2] =
@@ -48,25 +47,25 @@ const hal_logic_partition_t hal_partitions[] =
         .partition_owner            = HAL_FLASH_EMBEDDED,
         .partition_description      = "PARAMETER2",
         .partition_start_addr       = 0x1F0000,
-        .partition_length           = 0x2000, // 4k bytes
+        .partition_length           = 0x2000, // 8k bytes
         .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
 	[HAL_PARTITION_PARAMETER_4] =
-	{
-		.partition_owner			= HAL_FLASH_EMBEDDED,
+    {
+        .partition_owner           = HAL_FLASH_EMBEDDED,
 		.partition_description		= "PARAMETER4",
 		.partition_start_addr		= 0x1F2000,
 		.partition_length			= 0x1000, // 4k bytes
-		.partition_options			= PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
-	},
+        .partition_options         = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
+    },
 	[HAL_PARTITION_RF_FIRMWARE] =
-	{
-		.partition_owner			= HAL_FLASH_EMBEDDED,
+    {
+        .partition_owner            = HAL_FLASH_EMBEDDED,
 		.partition_description		= "RF parameter",
 		.partition_start_addr		= 0x1F3000,
-		.partition_length			= 0x1000,  // 4k bytes
-		.partition_options			= PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
-	},
+        .partition_length           = 0x1000, // 4k bytes
+        .partition_options          = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
+    },
 };
 
 void board_init(void)
