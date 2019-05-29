629b35eb3a6f1f547c6d2046c6d8af2030877966
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2019-05-29 16:25:14 +0800

    BugID:19174429:add asr to aos make
    Change-Id: Iae12f77c307e2f30f22ca35009c46093279b1f5c

--- board/asr6501/aos.mk
+++ board/asr6501/aos.mk
@@ -1,6 +1,86 @@
 NAME := board_asr6501
 
-$(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY := configuration for board asr6501
+JTAG := jlink
 
-AOS_BUILD_COMPATIBLE := no
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.0
+$(NAME)_SUMMARY    := configuration for board asr6501
+MODULE             := 1062
+HOST_ARCH          := Cortex-M0
+HOST_MCU_FAMILY    := mcu_cy8c4147
+SUPPORT_MBINS      := no
+
+$(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) kernel_init
+
+$(NAME)_SOURCES := board.c \
+	               src/asr_board.c \
+				   src/delay.c \
+				   src/gpio-board.c \
+				   src/gpio.c \
+				   src/gpio_irq.c \
+				   src/low_power.c \
+				   src/spi-board.c \
+				   src/systime.c \
+				   src/timeServer.c \
+				   src/utilities.c 
+
+
+GLOBAL_INCLUDES += . \
+				   inc 
+
+AOS_NETWORK_SAL ?= n
+ifeq (y,$(AOS_NETWORK_SAL))
+$(NAME)_COMPONENTS += sal netmgr
+module             ?= wifi.mk3060
+else
+GLOBAL_DEFINES += CONFIG_NO_TCPIP
+endif
+
+lorawanback      ?= 0
+ifeq ($(lorawanback), 0)
+	$(NAME)_COMPONENTS += lorawan_4_4_2
+else ifeq ($(lorawanback), 1)
+	$(NAME)_COMPONENTS += lorawan_4_4_0
+endif
+
+GLOBAL_DEFINES += CY8C4147
+
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_CY8C4147
+CONFIG_SYSINFO_DEVICE_NAME   := CY8C4147
+
+CONFIG_MANUFACTURER = ASR
+CONFIG_DEVICE_MODEL = 6501
+CONFIG_VERSION = 4.0
+
+LORACHIP = sx1262
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
+
+GLOBAL_CFLAGS += -DSYSINFO_MANUFACTURER=\"$(CONFIG_MANUFACTURER)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_MODEL=\"$(CONFIG_DEVICE_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_VERSION=\"$(CONFIG_VERSION)\"
+
+ifeq ($(COMPILER),armcc)
+else ifeq ($(COMPILER),iar)
+else
+GLOBAL_LDFLAGS += -L $($(NAME)_LOCATION)/asr6501
+endif
+
+# Global defines
+# HSE_VALUE = STM32 crystal frequency = 26MHz (needed to make UART work correctly)
+GLOBAL_DEFINES += $$(if $$(NO_CRLF_STDIO_REPLACEMENT),,CRLF_STDIO_REPLACEMENT)
+
+WIFI_FIRMWARE_SECTOR_START    := 2      #0x2000
+FILESYSTEM_IMAGE_SECTOR_START := 256    #0x100000
+
+# Extra build target include bootloader, and copy output file to eclipse debug file (copy_output_for_eclipse)
+EXTRA_TARGET_MAKEFILES +=
+
+# Define default component testcase set
+ifeq (, $(findstring yts, $(BUILD_STRING)))
+GLOBAL_DEFINES  += RHINO_CONFIG_WORKQUEUE=1
+TEST_COMPONENTS += basic_test wifi_hal_test rhino_test aos_test kv_test cjson_test
+else
+GLOBAL_DEFINES  += RHINO_CONFIG_WORKQUEUE=0
+endif
