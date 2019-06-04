70a7eaab04290531b33a8cb18e11fdf25ef484b4
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-06-04 17:23:17 +0800

    BugID:16846667: added board stm32f429zi-mk for micro kernel demo
    Change-Id: Ia53d5db5f28496058b959f9948f8116807397410

---
+++ board/stm32f429zi-mk/src/gpio.c
@@ -0,0 +1,18 @@
+
+#include "aos/hal/gpio.h"
+#include "stm32f4xx_hal.h"
+
+void MX_GPIO_Init(void)
+{
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+}
+
