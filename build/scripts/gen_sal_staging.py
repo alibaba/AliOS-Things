96bd291170aa1cb731864d99ff82e66c02e3427b
yajun.syj <yajun.syj@alibaba-inc.com>
2019-06-17 11:32:29 +0800

    BugID:20175528: Add gen_sal_staging.py and templates
    Change-Id: Ie97778bed01d1b47349c9d90642bbb346fe3ffbb

---
+++ build/scripts/gen_sal_staging.py
@@ -0,0 +1,105 @@
+import os
+import sys
+import click
+
+reload(sys)
+sys.setdefaultencoding('UTF8')
+
+scriptdir = os.path.dirname(os.path.abspath(__file__))
+templates = {
+    "gprs": ["templates/sal_gprs_template", "drivers/sal/gprs"],
+    "wifi": ["templates/sal_wifi_template", "drivers/sal/wifi"],
+}
+
+
+def copy_template(tempfile, templatedir, destdir, drivername):
+    """ Copy template files to destdir """
+
+    contents = []
+    # Replace drivername from file contents
+
+    with open(os.path.join(templatedir, tempfile), "r") as f:
+        for line in f.readlines():
+            if "@drivername@" in line:
+                line = line.replace("@drivername@", drivername)
+            elif "@DRIVERNAME@" in line:
+                line = line.replace("@DRIVERNAME@", drivername.upper())
+            else:
+                pass
+
+            contents += [line]
+
+    # Replace drivername from filename
+    if tempfile == "aos.mk.temp":
+        destfile = "aos.mk"
+    elif "drivername" in tempfile:
+        destfile = tempfile.replace("drivername", drivername)
+    else:
+        destfile = tempfile
+
+    # Write to destfile
+    if contents:
+        with open(os.path.join(destdir, destfile), "w") as f:
+            for line in contents:
+                f.write(line)
+
+
+def update_configin(configin_file, devicetype, drivername):
+    """ Update Config.in in paraent directory """
+    contents = []
+    source_found = "no"
+    default_found = "no"
+    with open(configin_file, "r") as f:
+        for line in f.readlines():
+            if "default" in line:
+                default_found = "yes"
+
+            if default_found == "yes" and "default" not in line:
+                contents += ['    default AOS_COMP_DEVICE_SAL_%s if BSP_EXTERNAL_%s_MODULE = "%s.%s"\n' %
+                             (drivername.upper(), devicetype.upper(), devicetype, drivername)]
+                default_found = "no"
+
+            if "source" in line:
+                source_found = "yes"
+
+            if source_found == "yes" and "source" not in line:
+                contents += ['source "drivers/sal/%s/%s/Config.in"\n' % (devicetype, drivername)]
+                source_found = "no"
+
+            contents += [line]
+
+    with open(configin_file, "w") as f:
+        for line in contents:
+            f.write(line)
+
+
+@click.command()
+@click.argument("drivername", metavar="[DRIVERNAME]")
+@click.option("-m", "--mfname", help="The manufacturer of device")
+@click.option("-t", "--devicetype", required=True, type=click.Choice(["gprs", "wifi"]), help="The type of device")
+@click.option("-a", "--author", help="The author of driver")
+def cli(drivername, mfname, devicetype, author):
+    """ Create sal driver staging from template """
+    templatedir = os.path.join(scriptdir, templates[devicetype][0])
+    destdir = os.path.join(scriptdir, "../../", templates[devicetype][1], drivername)
+    destdir = os.path.abspath(destdir)
+
+    if os.path.isdir(destdir):
+        click.echo("[Error] The driver directory is existing!\n%s" % destdir)
+        return 1
+    else:
+        os.makedirs(destdir)
+
+    sources = os.listdir(templatedir)
+    for tempfile in sources:
+        copy_template(tempfile, templatedir, destdir, drivername)
+
+    configin_file = os.path.join(scriptdir, "../../drivers/sal/%s/Config.in" % devicetype)
+    if os.path.isfile(configin_file):
+        update_configin(configin_file, devicetype, drivername)
+
+    click.echo("[Info] Drivers Initialized at: %s" % destdir)
+
+
+if __name__ == "__main__":
+    cli()
