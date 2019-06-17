ec93b66897c6bf0154e48926657a617674d84dca
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-06-17 19:40:33 +0800

    [PATCH] BugID:21078277:[ble] fix breezeapp build err.
    Change-Id: Ia5c00e89a7ce3a97c43df6b5fdcd537a80955ecf

--- network/bluetooth/breeze/ref-impl/aos.mk
+++ network/bluetooth/breeze/ref-impl/aos.mk
@@ -7,17 +7,16 @@ $(NAME)_SUMMARY := This is an AliOS Things based implementation of Breeze HAP AP
 $(NAME)_SOURCES-y := breeze_hal_ble.c breeze_hal_os.c breeze_hal_sec.c  aes.c
 
 ifeq ($(CONFIG_COMP_BZ_HAL_BLESTACK), y)
-$(NAME)_COMPONENTS-y := bt_host 
+$(NAME)_COMPONENTS-y := bt_host
 endif
 
 $(NAME)_INCLUDES += include/mbedtls
 ifeq ($(CONFIG_COMP_BZ_HAL_CRYPTO_MEBDTLS), y)
-$(NAME)_COMPONENTS-y += mbedtls 
+$(NAME)_COMPONENTS-y += mbedtls
 GLOBAL_DEFINES-y += USE_EXTERNAL_MEBDTLS
 else
 $(NAME)_SOURCES-y += sha256.c
 $(NAME)_SOURCES-y += aes_mbed.c
 endif
 
-$(NAME)_COMPONENTS-y += ulog 
-ble = 1
+$(NAME)_COMPONENTS-y += ulog
