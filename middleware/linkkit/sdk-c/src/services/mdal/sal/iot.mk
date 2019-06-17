fab0b7842e2fcfecff4af46a80ad94481a971267
yajun.syj <yajun.syj@alibaba-inc.com>
2019-06-17 11:00:45 +0800

    BugID:20175528: Move sal from middleware to network folder
    Change-Id: I043a97f91fcfd678c44d939b7e4fff4910efab46

--- middleware/linkkit/sdk-c/src/services/mdal/sal/iot.mk
+++
@@ -1,9 +0,0 @@
-LIBA_TARGET         := libiot_sal.a
-
-LIB_SRCS_PATTERN    := ./src/sal_sockets.c \
-                       ./src/sal_err.c     \
-                       ./src/ip4_addr.c    \
-                       ./src/sal_arch.c    \
-                       ./src/sal_util.c
-
-HDR_REFS            := src/infra
