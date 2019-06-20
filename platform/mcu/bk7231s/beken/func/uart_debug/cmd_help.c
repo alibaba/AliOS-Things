1de30c5f74cc127ddb64c1e0ff97a66d1b29fa8e
yj144830 <yj144830@alibaba-inc.com>
2019-06-20 21:03:39 +0800

    BugID:20250821:fix copyright comflict
    Change-Id: I36ea4e180defa577cdd97ccf5f38e0427712c2d3

--- platform/mcu/bk7231s/beken/func/uart_debug/cmd_help.c
+++ platform/mcu/bk7231s/beken/func/uart_debug/cmd_help.c
@@ -8,60 +8,53 @@
 int _do_help(cmd_tbl_t *cmd_start, int cmd_items, cmd_tbl_t *cmdtp, int flag,
              int argc, char *const argv[])
 {
-    int i;
     int rcode = 0;
+    int ret;
 
-    if (argc == 1)  	/* show list of commands */
+    cmd_tbl_t *cmd_array[COMMAND_MAX_COUNT];
+    int i, j, swaps;
+    cmd_tbl_t *temp;
+    const char *usage = NULL;
+
+    if (argc == 1)
     {
-        cmd_tbl_t *cmd_array[COMMAND_MAX_COUNT];
-        int i, j, swaps;
-
-        /* Make array of commands from .uboot_cmd section */
         cmdtp = cmd_start;
         for (i = 0; i < cmd_items; i++)
         {
             cmd_array[i] = cmdtp++;
         }
 
-        /* Sort command list (trivial bubble sort) */
-        for (i = cmd_items - 1; i > 0; --i)
+        for (i = cmd_items - 1; i > 0; i--)
         {
             swaps = 0;
-            for (j = 0; j < i; ++j)
+            for (j = 0; j < i; j++)
             {
-                if (os_strcmp(cmd_array[j]->name,
-                              cmd_array[j + 1]->name) > 0)
+                ret = os_strcmp(cmd_array[j]->name, cmd_array[j + 1]->name);
+                if (ret > 0)
                 {
-                    cmd_tbl_t *tmp;
-                    tmp = cmd_array[j];
+                    temp = cmd_array[j];
                     cmd_array[j] = cmd_array[j + 1];
-                    cmd_array[j + 1] = tmp;
-                    ++swaps;
+                    cmd_array[j + 1] = temp;
+                    swaps++;
                 }
             }
             if (!swaps)
                 break;
         }
 
-        /* print short help (usage) */
         for (i = 0; i < cmd_items; i++)
         {
-            const char *usage = cmd_array[i]->usage;
-
-            /* allow user abort */
+            usage = cmd_array[i]->usage;
 
             if (usage == NULL)
                 continue;
 
-            os_printf("%-*s- %s\n", CONFIG_SYS_HELP_CMD_WIDTH,
+            os_printf("msg %-*s- %s\n", CONFIG_SYS_HELP_CMD_WIDTH,
                       cmd_array[i]->name, usage);
         }
         return 0;
     }
 
-    /*
-     * command help (long version)
-     */
     for (i = 1; i < argc; ++i)
     {
         cmdtp = cmd_find_tbl(argv[i], cmd_start, cmd_items);
@@ -71,24 +64,23 @@ int _do_help(cmd_tbl_t *cmd_start, int cmd_items, cmd_tbl_t *cmdtp, int flag,
         }
         else
         {
-            os_printf("Unknown command '%s' - try 'help' without arguments for list of all known commands\n\n",
+            os_printf("Unknown command '%s' - try 'help' to list all of known commands\n\n",
                       argv[i]);
             rcode = 1;
         }
     }
+
     return rcode;
 }
 
-
-/*
- * Use puts() instead of os_printf() to avoid os_printf buffer overflow
- * for long help messages
- */
 int do_help(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 {
 #if CFG_UART_DEBUG
-    cmd_tbl_t *start = entry_get_start();
-    const int len = entry_get_count();
+    cmd_tbl_t *start = NULL;
+    const int len = 0;
+
+    start = entry_get_start();
+    len = entry_get_count();
 
     return _do_help(start, len, cmdtp, flag, argc, argv);
 #else
