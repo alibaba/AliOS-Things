8a65a1e51e5b71466864d94a960e39c7fa621a94
yajun.syj <yajun.syj@alibaba-inc.com>
2019-06-27 15:48:21 +0800

    BugID:19162543: Remove lwm2m examples
    Change-Id: I4691b3e99aa7dc02e7cd05edef9fe0d482b053c8

--- network/lwm2m/aos.mk
+++ network/lwm2m/aos.mk
@@ -7,9 +7,7 @@ $(NAME)_SUMMARY := lwm2m component
 GLOBAL_DEFINES-y += LWM2M_LITTLE_ENDIAN \
                   LWM2M_CLIENT_MODE
 
-GLOBAL_INCLUDES += ./wakaama-1.0/core \
-                   ./wakaama-1.0/examples/shared \
-                   ./wakaama-1.0/examples/client
+GLOBAL_INCLUDES += ./wakaama-1.0/core
 
 LWM2M_SRC_PATH := ./wakaama-1.0/core
 
@@ -37,31 +35,3 @@ $(NAME)_SOURCES := $(LWM2M_SRC_PATH)/block1.c \
 
 $(NAME)_COMPONENTS += libcoap
 GLOBAL_DEFINES  += LWM2M_LIBCOAP_ENABLED
-
-#example client
-LWM2M_EXAMPLE_PATH := ./wakaama-1.0/examples
-
-$(NAME)_SOURCES += $(LWM2M_EXAMPLE_PATH)/client/object_access_control.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_device.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_security.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/lwm2mclient.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_connectivity_moni.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_firmware.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_server.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_connectivity_stat.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/object_location.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/system_api.c \
-                   $(LWM2M_EXAMPLE_PATH)/client/test_object.c \
-                   $(LWM2M_EXAMPLE_PATH)/shared/platform.c \
-                   $(LWM2M_EXAMPLE_PATH)/shared/commandline.c \
-                   $(LWM2M_EXAMPLE_PATH)/shared/memtrace.c
-
-ifeq (y,$(LWM2M_WITH_DTLS))
-$(NAME)_COMPONENTS += mbedtls
-GLOBAL_DEFINES-y += NDEBUG USE_EXTERNAL_MEBDTLS WITH_MBEDTLS LWM2M_WITH_MBEDTLS
-$(NAME)_SOURCES += $(LWM2M_EXAMPLE_PATH)/shared/mbedconnection.c
-else
-$(NAME)_SOURCES += $(LWM2M_EXAMPLE_PATH)/shared/connection.c
-endif
-
-#end example client
