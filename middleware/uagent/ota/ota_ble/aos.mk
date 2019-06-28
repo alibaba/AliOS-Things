b1cacab53d9f54c34b4e04218a78c7a25822bff7
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2019-06-28 16:53:02 +0800

    bugid:20235446:uniform code folder
    Change-Id: I68306356acbdffa2c345acc37a0bc63262159f0b

--- middleware/uagent/ota/ota_ble/aos.mk
+++ middleware/uagent/ota/ota_ble/aos.mk
@@ -4,14 +4,12 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := ble device upgrade
 
-$(NAME)_SOURCES += \
-    src/ota_breeze.c \
-    src/ota_breeze_service.c \
-    src/ota_breeze_transport.c \
-    src/ota_breeze_plat.c      \
-    src/ota_breeze_os.c
+$(NAME)_SOURCES +=           \
+      ota_breeze.c           \
+      ota_breeze_service.c   \
+      ota_breeze_transport.c \
+      ota_breeze_plat.c      \
+      ota_breeze_os.c
 
-GLOBAL_INCLUDES += . \
-                   inc
-
+GLOBAL_INCLUDES += .
 GLOBAL_DEFINES += AOS_COMP_OTA_BLE
