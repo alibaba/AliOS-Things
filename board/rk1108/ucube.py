424d772c6abd3cc5205991ae380e9cd529043a6c
jianzhou <jianzhou.cjz@alibaba-inc.com>
2019-07-02 05:18:12 -0700

    BugID:21361939: restore ci for athostapp
    Change-Id: I61047382fb1519f1b896f7aa1a41d5477d462c90

--- board/rk1108/ucube.py
+++ board/rk1108/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets=" coapapp helloworld http2app httpapp linkkit_gateway linkkitapp meshapp mqttapp otaapp tls udata_demo.sensor_local_demo udevapp ulocationapp yts"
+linux_only_targets="athostapp coapapp helloworld http2app httpapp linkkit_gateway linkkitapp meshapp mqttapp otaapp tls udata_demo.sensor_local_demo udevapp ulocationapp yts"
