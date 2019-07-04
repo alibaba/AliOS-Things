a7e137524d8db65249faed5747e0f5ff3c6f458d
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-07-04 16:23:30 +0800

    BugID:20072916: Support for creating/building App
    Change-Id: I44696f9f51ec5150addc7c18dd748e5c56dafac7

--- build/scripts/auto_component.py
+++ build/scripts/auto_component.py
@@ -6,5 +6,5 @@ if len(sys.argv) == 2:
    file_dir = os.path.join(os.getcwd(), sys.argv[1])
    if not os.path.exists(file_dir):
        os.makedirs(file_dir)
-   with open(os.path.join(file_dir, "auto_component.mk"), "w") as f:
+   with open(os.path.join(file_dir, "aos.mk"), "w") as f:
        f.write(component_mk_str)
---
+++ build/scripts/templates/new_project_template/app_main.c
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+
+#include <aos/kernel.h>
+
+/**********************user code*************************/
+int application_start(int argc, char *argv[])
+{
+    /*add your code*/
+  
+    while(1) {
+        /*add your code*/
+        /*printf("helloworld\r\n")*/
+      
+        aos_msleep(1000);
+    };
+}
