797696d888b5bfe9599544c93af0fd4dbb38aad0
yajun.syj <yajun.syj@alibaba-inc.com>
2019-07-05 16:12:51 +0800

    BugID:19162543: Add coap/lwm2m api and optimize lwm2m source code
    Change-Id: Id4ec3365a179b9272b93a4fa86b5fe5e5f64fa97

--- network/lwm2m/aos.mk
+++ network/lwm2m/aos.mk
@@ -1,15 +1,15 @@
 NAME := lwm2m
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.0
+$(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := lwm2m component
 
 GLOBAL_DEFINES-y += LWM2M_LITTLE_ENDIAN \
-                  LWM2M_CLIENT_MODE
+                    LWM2M_CLIENT_MODE
 
-GLOBAL_INCLUDES += ./wakaama-1.0/core \
+GLOBAL_INCLUDES += ./include \
 
-LWM2M_SRC_PATH := ./wakaama-1.0/core
+LWM2M_SRC_PATH := ./src/wakaama-1.0
 
 ifeq (y,$(LWM2M_BOOTSTRAP_CLIENT_SUPPORT))
 GLOBAL_DEFINES += LWM2M_BOOTSTRAP
