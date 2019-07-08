664a9576ab26800b01910556cd701ff9a7498734
zixun.lyq <zixun.lyq@alibaba-inc.com>
2019-07-08 17:25:50 +0800

    BugID:21449050: Reconnect Retry Code optimize
    Change-Id: I229a0707f255074610b9c491ba7dcb839b9742f6

--- middleware/uagent/ota/ota_core/download/ota_download_http.c
+++ middleware/uagent/ota/ota_core/download/ota_download_http.c
@@ -57,22 +57,21 @@ static const char *ca_cert = \
 int ota_download_start(char *url)
 {
     char hdr[512] = {0};
-    int ret = 0;
+    int ret = OTA_DOWNLOAD_INIT_FAIL;
     unsigned int offset = 0;
     http_rsp_info_t rsp_info = {0};
     char *content = NULL;
     char *host_name = NULL;
     char *host_uri = NULL;
     int fd = 0;
-    int retry = OTA_DOWNLOAD_RETRY_CNT;
-    int i = 0;
+    int i = 0, j = 0;
     int ota_rx_size = 0;
     int ota_file_size = 0;
     unsigned char ota_header_found = false;
     httpc_connection_t settings = {0};
     httpc_handle_t httpc_handle = 0;
     int percent                 = 0;
-    int divisor                 = 10; 
+    int divisor                 = 10;
 #if defined OTA_CONFIG_ITLS
     char pkps[128] = {0};
 #endif
@@ -87,178 +86,172 @@ int ota_download_start(char *url)
         return ret;
     }
 
-RETRY:
-    fd = socket(AF_INET, SOCK_STREAM, 0);
-    if (fd < 0) {
-        ret = OTA_DOWNLOAD_INIT_FAIL;
-        goto EXIT;
-    }
-    memset(&settings, 0, sizeof(settings));
-    settings.socket = fd;
-    settings.server_name = host_name;
+    for(j = OTA_DOWNLOAD_RETRY_CNT; (j > 0)&&(ret < 0); j--) {
+        fd = socket(AF_INET, SOCK_STREAM, 0);
+        if (fd < 0) {
+            ret = OTA_DOWNLOAD_INIT_FAIL;
+            goto EXIT;
+        }
+        memset(&settings, 0, sizeof(settings));
+        settings.socket = fd;
+        settings.server_name = host_name;
 #if defined BOARD_ESP8266
-    settings.flags = 0x80;
+        settings.flags = 0x80;
 #elif defined OTA_CONFIG_ITLS
-    if(ctx != NULL) {
-        strncpy(pkps, ctx->pk, strlen(ctx->pk));
-        strncpy(pkps + strlen(ctx->pk) + 1, ctx->ps, strlen(ctx->ps));
-    }
-    settings.ca_cert = pkps;
+        if(ctx != NULL) {
+            strncpy(pkps, ctx->pk, strlen(ctx->pk));
+            strncpy(pkps + strlen(ctx->pk) + 1, ctx->ps, strlen(ctx->ps));
+        }
+        settings.ca_cert = pkps;
 #elif defined OTA_CONFIG_SECURE_DL_MODE
-    settings.ca_cert = ca_cert;
+       settings.ca_cert = ca_cert;
 #else
-    settings.flags = 0x80;
+       settings.flags = 0x80;
 #endif
-    settings.req_buf = dl_buf;
-    settings.req_buf_size = OTA_DOWNLOAD_BLOCK_SIZE;
-    httpc_handle = httpc_init(&settings);
-    if (httpc_handle == 0) {
-        ret = OTA_DOWNLOAD_INIT_FAIL;
-        goto EXIT;
-    }
+       settings.req_buf = dl_buf;
+       settings.req_buf_size = OTA_DOWNLOAD_BLOCK_SIZE;
+       httpc_handle = httpc_init(&settings);
+       if (httpc_handle == 0) {
+           ret = OTA_DOWNLOAD_INIT_FAIL;
+           goto EXIT;
+       }
 
-    if (retry >= OTA_DOWNLOAD_RETRY_CNT) {
-        ret = httpc_construct_header(hdr, 512, "Accept", "*/*");
-        if (ret < 0) {
-            ret = OTA_DOWNLOAD_CON_FAIL;
-            goto EXIT;
-        }
-    } else {
-        ota_msleep(6000);
-        char resume[64] = {0};
-        ota_snprintf(resume, 64, "bytes=%d-", offset);
-        ret = httpc_construct_header(hdr, 512, "Range", resume);
-        if (ret < 0) {
-            ret = OTA_DOWNLOAD_CON_FAIL;
-            goto EXIT;
-        }
-    }
-    memset(dl_buf, 0, OTA_DOWNLOAD_BLOCK_SIZE);
-    for(i = 0; i < OTA_DOWNLOAD_RETRY_CNT; i++){
-        ret = httpc_send_request(httpc_handle, HTTP_GET, host_uri, hdr, NULL, NULL, 0);
-        if(ret >= 0){
-            break;
-        }
-        else {
-            ota_msleep(1500);
-            OTA_LOG_E("send cnt;%d ret:%d \n",i ,ret);
-        }
-    }
-    if (ret < 0) {
-        ret = OTA_DOWNLOAD_REQ_FAIL;
-        goto EXIT;
-    }
-    ota_upgrading = 1;
-    memset(dl_buf, 0, OTA_DOWNLOAD_BLOCK_SIZE);
-    while (ota_file_size == 0 || ota_rx_size < ota_file_size) {
-        for(i = 0; i < OTA_DOWNLOAD_RETRY_CNT; i++){
-            ret = httpc_recv_response(httpc_handle, dl_buf, OTA_DOWNLOAD_BLOCK_SIZE, &rsp_info, OTA_DOWNLOAD_TIMEOUT);
-            if ((ret == HTTP_ERECV) || (ret == HTTP_ETIMEOUT)) {
-               ota_msleep(1500);
-               OTA_LOG_E("recv retry:%d ret:%d \n", i, ret);
-               continue;
-            } else {
-               break;
-            }
-        }
-        if (ota_upgrading == 0) {
-            OTA_LOG_E("download stop.\n");
-            ret = OTA_DOWNLOAD_RECV_FAIL;
-            break;
-        }
-        else if (ret < 0) {
-            ret = OTA_DOWNLOAD_RECV_FAIL;
-            break;
-        } else {
-            if (rsp_info.body_present || rsp_info.message_complete) {
-                if (ota_header_found == false) {
-                    if (ota_file_size == 0) {
-                        content = strstr((const char *)dl_buf, "Content-Length");
-                        if (content) {
-                            ret = sscanf(content, "%*[^ ]%d", &ota_file_size);
-                            if (ret < 0) {
-                                OTA_LOG_I("header fail\n");
-                                break;
-                            }
-                            ota_header_found = true;
-                            OTA_LOG_I("header file size %d\r\n", ota_file_size);
-                        }
-                        else {
-                            continue;
-                        }
-                    }
-                    content = strstr((const char *)dl_buf, "\r\n\r\n");
-                    if (content) {
-                        content += 4;
-                        ota_rx_size = rsp_info.rsp_len - ((unsigned char *)content - dl_buf);
-                        if((ctx != NULL)&&(ctx->on_data != NULL)) {
-                            ret = ctx->on_data(content, ota_rx_size);
-                        } else {
-                            ret = ota_write(&offset, content, ota_rx_size);
-                        }
-                        if (ret < 0) {
-                            ret = OTA_UPGRADE_WRITE_FAIL;
-                            goto EXIT;
-                        }
-                    }
+       if (j >= OTA_DOWNLOAD_RETRY_CNT) {
+           ret = httpc_construct_header(hdr, 512, "Accept", "*/*");
+           if (ret < 0) {
+                ret = OTA_DOWNLOAD_CON_FAIL;
+                goto EXIT;
+           }
+       } else {
+           ota_msleep(6000);
+           OTA_LOG_E("reconnect retry:%d ret:%d \n", j, ret);
+           char resume[64] = {0};
+           ota_snprintf(resume, 64, "bytes=%d-", offset);
+           ret = httpc_construct_header(hdr, 512, "Range", resume);
+           if (ret < 0) {
+                ret = OTA_DOWNLOAD_CON_FAIL;
+                goto EXIT;
+           }
+       }
+       memset(dl_buf, 0, OTA_DOWNLOAD_BLOCK_SIZE);
+       for(i = 0; i < OTA_DOWNLOAD_RETRY_CNT; i++){
+           ret = httpc_send_request(httpc_handle, HTTP_GET, host_uri, hdr, NULL, NULL, 0);
+           if(ret >= 0){
+                break;
+           }
+           else {
+                ota_msleep(1500);
+                OTA_LOG_E("send cnt;%d ret:%d \n",i ,ret);
+           }
+       }
+       if (ret < 0) {
+           ret = OTA_DOWNLOAD_REQ_FAIL;
+           goto EXIT;
+       }
+       ota_upgrading = 1;
+       memset(dl_buf, 0, OTA_DOWNLOAD_BLOCK_SIZE);
+       while (ota_file_size == 0 || ota_rx_size < ota_file_size) {
+           for(i = 0; i < OTA_DOWNLOAD_RETRY_CNT; i++){
+                ret = httpc_recv_response(httpc_handle, dl_buf, OTA_DOWNLOAD_BLOCK_SIZE, &rsp_info, OTA_DOWNLOAD_TIMEOUT);
+                if ((ret == HTTP_ERECV) || (ret == HTTP_ETIMEOUT)) {
+                    ota_msleep(1500);
+                    OTA_LOG_E("recv retry:%d ret:%d \n", i, ret);
+                    continue;
+                } else {
+                    break;
                 }
-                else {
-                    if((ctx != NULL)&&(ctx->on_data != NULL)) {
-                        ret = ctx->on_data((char *)dl_buf, rsp_info.rsp_len);
-                    } else {
-                        ret = ota_write(&offset, (char *)dl_buf, rsp_info.rsp_len);
-                    }
-                    if (ret < 0) {
-                         ret = OTA_UPGRADE_WRITE_FAIL;
-                         goto EXIT;
-                    }
-                    ota_rx_size += rsp_info.rsp_len;
-                }
-                //OTA_LOG_E("ota (%d/%d) recv len:%d \r\n", ota_rx_size, ota_file_size, rsp_info.rsp_len);
-                if(ota_file_size) {
-                    percent = (ota_rx_size * 100) /ota_file_size;
-                    if(percent / divisor) {
-                         divisor += 10;
-                         if((ctx != NULL)&&(ctx->on_percent != NULL)) {
-                             ctx->on_percent(percent);
-                         } else {
-#if !defined BOARD_ESP8266 && !defined OTA_CONFIG_SECURE_DL_MODE
-                             if(ctx != NULL)
-                             ota_transport_status(ctx->pk, ctx->dn, percent);
-#endif
+           }
+           if (ota_upgrading == 0) {
+                OTA_LOG_E("download stop.\n");
+                ret = OTA_DOWNLOAD_RECV_FAIL;
+                break;
+           } else if (ret < 0) {
+                ret = OTA_DOWNLOAD_RECV_FAIL;
+                break;
+           } else {
+                if (rsp_info.body_present || rsp_info.message_complete) {
+                    if (ota_header_found == false) {
+                        if (ota_file_size == 0) {
+                             content = strstr((const char *)dl_buf, "Content-Length");
+                             if (content) {
+                                 ret = sscanf(content, "%*[^ ]%d", &ota_file_size);
+                                 if (ret < 0) {
+                                     OTA_LOG_I("header fail\n");
+                                     break;
+                                 }
+                                 ota_header_found = true;
+                                 OTA_LOG_I("header file size %d\r\n", ota_file_size);
+                             } else {
+                                 continue;
+                             }
                          }
-                         OTA_LOG_I("ota recv data(%d/%d)\r\n", ota_rx_size, ota_file_size);
+                         content = strstr((const char *)dl_buf, "\r\n\r\n");
+                         if (content) {
+                             content += 4;
+                             ota_rx_size = rsp_info.rsp_len - ((unsigned char *)content - dl_buf);
+                             if((ctx != NULL)&&(ctx->on_data != NULL)) {
+                                 ret = ctx->on_data(content, ota_rx_size);
+                             } else {
+                                 ret = ota_write(&offset, content, ota_rx_size);
+                             }
+                             if (ret < 0) {
+                                 ret = OTA_UPGRADE_WRITE_FAIL;
+                                 goto EXIT;
+                             }
+                         }
+                    } else {
+                         if((ctx != NULL)&&(ctx->on_data != NULL)) {
+                             ret = ctx->on_data((char *)dl_buf, rsp_info.rsp_len);
+                         } else {
+                             ret = ota_write(&offset, (char *)dl_buf, rsp_info.rsp_len);
+                         }
+                         if (ret < 0) {
+                             ret = OTA_UPGRADE_WRITE_FAIL;
+                             goto EXIT;
+                         }
+                         ota_rx_size += rsp_info.rsp_len;
                     }
-                }
-            }
-        }
-    }
+                    //OTA_LOG_E("ota (%d/%d) recv len:%d \r\n", ota_rx_size, ota_file_size, rsp_info.rsp_len);
+                    if(ota_file_size) {
+                         percent = (ota_rx_size * 100) /ota_file_size;
+                         if(percent / divisor) {
+                             divisor += 10;
+                             if((ctx != NULL)&&(ctx->on_percent != NULL)) {
+                                 ctx->on_percent(percent);
+                             } else {
+#if !defined BOARD_ESP8266 && !defined OTA_CONFIG_SECURE_DL_MODE
+                                 if(ctx != NULL)
+                                 ota_transport_status(ctx->pk, ctx->dn, percent);
+#endif
+                             }
+                             OTA_LOG_I("ota recv data(%d/%d)\r\n", ota_rx_size, ota_file_size);
+                         }
+                     }
+                 }
+             }
+         }
 
 EXIT:
-    ota_upgrading = 0;
-    if(settings.socket > 0) {
-        close(settings.socket);
-        settings.socket = 0;
+         ota_upgrading = 0;
+         if(settings.socket > 0) {
+             close(settings.socket);
+             settings.socket = 0;
+         }
+         if(httpc_handle > 0) {
+             httpc_deinit(httpc_handle);
+             httpc_handle = 0;
+         }
+         httpc_handle = 0;
+         ota_header_found = false;
+         ota_file_size = 0;
+         ota_rx_size = 0;
     }
-    if(httpc_handle > 0) {
-        httpc_deinit(httpc_handle);
-        httpc_handle = 0;
-    }
-    httpc_handle = 0;
-    ota_header_found = false;
-    ota_file_size = 0;
-    ota_rx_size = 0;
-    if(ret < 0 && retry > 0){
-        retry--;
-        OTA_LOG_E("retry ret:%d  count:%d \n", ret, retry);
-        goto RETRY;
-    }
-    OTA_LOG_E("compelte:%d \n", ret);
+    OTA_LOG_E("download compelte:%d \n", ret);
     return ret;
 }
 
 /**
- * ota_download_init  OTA download init
+ * ota_download_stop  OTA download stop
  *
  * @param[in] void
  *
@@ -266,7 +259,7 @@ EXIT:
  * @return OTA_DOWNLOAD_INIT_FAIL  OTA download init failed.
  * @return OTA_DOWNLOAD_CON_FAIL   OTA download connect failed.
  * @return OTA_DOWNLOAD_REQ_FAIL   OTA download request failed.
- * @return OTA_DOWNLOAD_RECV_FAIL  OTA download receive failed. 
+ * @return OTA_DOWNLOAD_RECV_FAIL  OTA download receive failed.
  */
 int ota_download_stop(void)
 {
