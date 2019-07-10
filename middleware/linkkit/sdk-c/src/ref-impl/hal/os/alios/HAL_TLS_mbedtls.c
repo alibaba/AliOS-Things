adc0e86000f6f26ea9c5d7c094744598e4cf4dc5
zhangtian <dongren.zt@alibaba-inc.com>
2019-07-10 11:48:57 +0800

    BugID:21294927:[hal tls]improve random for tls;enable yloop timer
    Change-Id: I0cc43fc680487cecfe3d463cc5888a66f9adb544
    Reviewed-on: http://yuncode.alibaba-inc.com/217429
    Reviewed-by: 庞军平 junping.pjp <junping.pjp@alibaba-inc.com>
    Tested-by: 庞军平 junping.pjp <junping.pjp@alibaba-inc.com>

--- middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/HAL_TLS_mbedtls.c
+++ middleware/linkkit/sdk-c/src/ref-impl/hal/os/alios/HAL_TLS_mbedtls.c
@@ -134,6 +134,26 @@ static int _ssl_random(void *p_rng, unsigned char *output, size_t output_len)
     return 0;
 }
 
+static void _aos_srand(unsigned int seed)
+{
+#define SEED_MAGIC 0x123
+    int           ret        = 0;
+    int           seed_len   = 0;
+    unsigned int  seed_val   = 0;
+    static char  *g_seed_key = "seed_key";
+
+    seed_len = sizeof(seed_val);
+    ret = aos_kv_get(g_seed_key, &seed_val, &seed_len);
+    if (ret) {
+        seed_val = SEED_MAGIC;
+    }
+    seed_val += seed;
+    srand(seed_val);
+
+    seed_val = rand();
+    aos_kv_set(g_seed_key, &seed_val, sizeof(seed_val), 1);
+}
+
 static void _ssl_debug(void *ctx, int level, const char *file, int line,
                        const char *str)
 {
@@ -197,7 +217,7 @@ static int _ssl_client_init(mbedtls_ssl_context *ssl,
     mbedtls_ssl_init(ssl);
     mbedtls_ssl_config_init(conf);
     mbedtls_x509_crt_init(crt509_ca);
-
+    _aos_srand(aos_now_ms());
     /*verify_source->trusted_ca_crt==NULL
      * 0. Initialize certificates
      */
