700a6028c7ec1ca763e7c33e354918478b25a1fe
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-07-11 23:35:16 +0800

    BugID:21512518: refine atparser allow user to config prompt timeout and whether to  wait response for data sending
    Change-Id: I7ac0cc06ccb01b72cb26a610d2ecf61cb8161654

--- network/nal/atparser/atparser_opts.h
+++ network/nal/atparser/atparser_opts.h
@@ -22,6 +22,7 @@ extern "C" {
 #define AT_RECV_DEFAULT_FAIL_POSTFIX        "ERROR\r\n"
 #define AT_SEND_DEFAULT_DELIMITER           "\r"
 #define AT_SEND_DEFAULT_DATA_PROMPT         ">"
+#define AT_SEND_DEFAULT_PROMPT_TIMEOUT_MS   200
 
 #define AT_WORKER_DEFAULT_STACK_SIZE        1024
 #define AT_WORKER_DEFAULT_PRIORITY          32  /* Default AOS_DEFAULT_APP_PR */
