5a322230d91cbf008075d7ad82f23fccf22ef728
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2019-07-18 07:08:14 +0800

    bugid:20235446:add ble image generate python tool
    Change-Id: Iaf5ee6bb0c64f59942cc3501db1d2b11f9de2a58

--- platform/mcu/nrf52xxx/gen_image_bin.mk
+++ platform/mcu/nrf52xxx/gen_image_bin.mk
@@ -15,6 +15,7 @@ BOOT_OFFSET := 0x00
 APP_OFFSET  := 0x10000
 
 GEN_BIN_OUTPUT_FILE:= $(SCRIPTS_PATH)/gen_common_bin_output_file.py
+GEN_BIN_HEADER_FILE:= $(SOURCE_ROOT)/platform/mcu/nrf52xxx/ota_gen_image_info.py
 
 gen_standard_images:
 	$(QUIET)$(ECHO) Generate ALL Standard Images:
@@ -23,6 +24,7 @@ gen_standard_images:
 	$(PYTHON) $(SCRIPTS_PATH)/gen_common_bin_output_file.py -o $(ALL_BIN_OUTPUT_FILE) -f $(BOOT_OFFSET) $(BOOT_BIN_FILE)
 	$(PYTHON) $(SCRIPTS_PATH)/gen_common_bin_output_file.py -o $(ALL_BIN_OUTPUT_FILE) -f $(APP_OFFSET) $(BIN_OUTPUT_FILE)
 	$(PYTHON) $(SCRIPTS_PATH)/ota_gen_md5_bin.py $(OTA_BIN_OUTPUT_FILE) -m $(IMAGE_MAGIC)
+	$(PYTHON) $(GEN_BIN_HEADER_FILE) -o $(OTA_BIN_INFO_FILE) -m $(IMAGE_MAGIC) $(OTA_BIN_OUTPUT_FILE)
 	$(PYTHON) $(SCRIPTS_PATH)/gen_common_bin_output_file.py -o $(OTA_BIN_INFO_FILE) -f $(OTA_IMAGE_OFFSET) $(OTA_BIN_OUTPUT_FILE)
 	$(QUIET) $(CP) $(OTA_BIN_INFO_FILE) $(OTA_BIN_OUTPUT_FILE)
 	$(QUIET) $(RM) $(OTA_BIN_INFO_FILE)
