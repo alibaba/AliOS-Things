836376212dca2629ab6f01e93ac3a9bcc0038ebe
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2019-07-18 21:17:33 +0800

    BugID:19871291: Add uND module(default-disable) in network.
    Change-Id: I228b71ffb18cf8f155ac8754e333d7c9469b8d76

---
+++ network/und/include/und_platform.h
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef UND_PLATFORM_H
+#define UND_PLATFORM_H
+
+#include <stdio.h>
+#include <string.h>
+
+#include "wrappers.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#define und_platform_malloc(size)     HAL_Malloc(size)
+#define und_platform_free(ptr)        HAL_Free(ptr)
+#define und_platform_memset           memset
+#define und_platform_strlen           strlen
+#define und_platform_snprintf         snprintf
+#define und_platform_mutex_lock       HAL_MutexLock
+#define und_platform_mutex_unlock     HAL_MutexUnlock
+#define und_platform_mutex_create     HAL_MutexCreate
+#define und_platform_mutex_destroy    HAL_MutexDestroy
+#define und_platform_timer_create     HAL_Timer_Create
+#define und_platform_timer_start      HAL_Timer_Start
+#define und_platform_timer_stop       HAL_Timer_Stop
+#define und_platform_timer_delete     HAL_Timer_Delete
+#define und_platform_kv_set           HAL_Kv_Set
+#define und_platform_kv_get           HAL_Kv_Get
+#define und_platform_get_product_key  HAL_GetProductKey
+#define und_platform_get_device_name  HAL_GetDeviceName
+#define und_platform_uptime_ms        HAL_UptimeMs
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* UND_PLATFORM_H */
