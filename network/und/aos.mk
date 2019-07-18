cfa9603feece1c07164350d8851c2dba10be3fa9
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2019-07-18 21:17:33 +0800

    BugID:19871291: Add uND module(default-disable) in network.
    Change-Id: I228b71ffb18cf8f155ac8754e333d7c9469b8d76

---
+++ network/und/aos.mk
@@ -0,0 +1,45 @@
+NAME := und
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := network diagnosis provide ablity to report failure/error of network and memeory usage to cloud.
+$(NAME)_SOURCES-y := src/und.c src/und_adapter.c src/und_manage.c src/und_packet.c src/und_sched.c src/und_report.c
+#default gcc
+ifeq ($(COMPILER),)
+$(NAME)_CFLAGS-y      += -Wall -Werror
+else ifeq ($(COMPILER),gcc)
+$(NAME)_CFLAGS-y      += -Wall -Werror
+endif
+
+ifeq (y,$(AOS_COMP_UND))
+AOS_UND_WITH_WIFI ?= y
+endif
+
+ifeq (y,$(AOS_UND_USE_MQTT))
+GLOBAL_DEFINES-y += FEATURE_UND_USE_MQTT
+endif
+
+ifeq (y,$(AOS_UND_USE_UAGENT))
+GLOBAL_DEFINES-y += FEATURE_UND_USE_UAGENT
+endif
+
+ifeq (y,$(AOS_UND_WITH_WIFI))
+GLOBAL_DEFINES-y += FEATURE_UND_WITH_WIFI
+$(NAME)_SOURCES-y += hal/und_wifi.c
+endif
+
+ifeq (y,$(AOS_UND_WITH_BLE))
+GLOBAL_DEFINES-y += FEATURE_UND_WITH_BLE
+$(NAME)_SOURCES-y += hal/und_ble.c
+endif
+
+ifeq (y,$(pvtest))
+GLOBAL_DEFINES-y += PREVALIDATE_TEST
+endif
+
+$(NAME)_COMPONENTS-y += kv osal_aos yloop linkkit_sdk_c
+
+GLOBAL_INCLUDES-y += include middleware/linkkit/sdk-c/include
+
+GLOBAL_DEFINES-y += FEATURE_UND_SUPPORT
+GLOBAL_DEFINES-y += AOS_UND
