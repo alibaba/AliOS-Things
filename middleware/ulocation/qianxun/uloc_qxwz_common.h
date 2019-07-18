4bdafcef1d8a7123d7e129bb6fdbd302af8c957f
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-07-18 18:36:01 +0800

    BugID:20525630: optimize the ulocation code and rename some file
    Change-Id: If9fcbd27bdbc0336c2fbd8e80bb1e48e70b575c3
    Reviewed-on: http://yuncode.alibaba-inc.com/218847
    Tested-by: 李诚 lc122798 <lc122798@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

--- middleware/ulocation/qianxun/uloc_qxwz_common.h
+++ middleware/ulocation/qianxun/uloc_qxwz_common.h
@@ -4,11 +4,11 @@
 
 #ifdef QXWZ_ENABLED
 #include "qxwz_types.h"
+#include <aos/kernel.h>
 
 #define REPORT_GGA_DATA_SIZE 128
 #define GPS_RAW_DATA_SIZE    1024
 #define QXWZ_PRINT_BUFFER    1024
-#define WAIT_FOREVER         0xFFFFFFFFU
 #define SOCKET_TIMEOUT       1000
 #define SOCKET_BUFFER        2048
 #define GGA_DATA_PERIOD      1000
@@ -26,5 +26,14 @@ typedef enum  {
     END_CR,
     END_NL
 } gga_data_filter_index;
+
+typedef struct {
+    char status;       /* 状态值 */
+    double longitude;  /* 经度 */
+    double latitude;   /* 纬度 */
+    char nors;         /* 南北纬 */
+    char wore;         /* 东西经 */
+    double altitude;   /* 高度 */
+} uloc_gga_info;
 
 #endif
