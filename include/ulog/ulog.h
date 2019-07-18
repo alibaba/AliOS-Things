c71c76ed419c77627e6ad4c1271d2bc5676b143b
gonghui.hui <gonghui.hui@alibaba-inc.com>
2019-07-18 18:33:52 +0800

    BugID:21203770: Unify API Interface of uLog with LOGX, advanced function of uLog can be used via configuration
    Change-Id: I7475a5e3dc61e8d512806b42d1aa818a8392bea3

--- include/ulog/ulog.h
+++ include/ulog/ulog.h
@@ -7,8 +7,18 @@
 
 #include "ulog_config.h"
 
-#ifdef __cplusplus
-extern "C" {
+/**
+ * Important!!! Switch on NDEBUG will make no log produced(except use API ULOG),
+ * it doesn't work even you try to increase log level.
+ */
+#ifdef NDEBUG
+#define CONFIG_LOGMACRO_SILENT
+#endif
+
+#if SYNC_LOG_DETAILS
+#define ULOG_TAG __FILE__, __LINE__
+#else
+#define ULOG_TAG "", 0
 #endif
 
 #define LOG_EMERG   0 /* system is unusable */
@@ -22,7 +32,7 @@ extern "C" {
 #define LOG_NONE    8 /* used in stop filter, all log will pop out */
 
 typedef enum {
-    AOS_LL_NONE  = LOG_ALERT,   /* disable log */
+    AOS_LL_NONE  = LOG_EMERG,   /* disable log */
     AOS_LL_FATAL = LOG_CRIT,    /* fatal log will output */
     AOS_LL_ERROR = LOG_ERR,     /* fatal + error log will output */
     AOS_LL_WARN  = LOG_WARNING, /* fatal + warn + error log will output(default level) */
@@ -30,25 +40,12 @@ typedef enum {
     AOS_LL_DEBUG = LOG_DEBUG,   /* debug + info + warn + error + fatal log will output */
 } aos_log_level_t;
 
-/**
- * Module Type, used for distinguish funcion module of whole system.
- *
- */
-typedef enum{
-    MOD_KERNEL   = 0,  /* Log produced in kernel */
-    MOD_SECURITY = 4,  /* Log produced in security and authority  */
-    MOD_SYSLOG   = 5,  /* Log produced in syslog self */
-    MOD_NET      = 7,  /* Log produced in Net, inlude lwip, bluetooth, CANopen, etc  */
-    MOD_MID      = 16, /* Log produced in middleware, include uagent, udata, linkkit */
-    MOD_USR1         , /* Log produced in user app */
-    MOD_USR2         , /* Log produced in user app */
-}MOD_TYPE;
-
-#if SYNC_LOG_DETAILS
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 /*
- * Function prototype for syncronized log text, Recommed use below brief API instead of this
- *
+ * Function prototype for syncronized log text, Recommed use below brief API LOGX
  *
  * @param  s         Serverity of Log
  * @param  mod       Module name
@@ -57,42 +54,15 @@ typedef enum{
  * @param  fmt, ...  Variable Parameter, support format print to log
  * @return  0: success; other: fail.
  */
-int rt_log(const unsigned char s, const char* mod, const char* f, const unsigned long l, const char *fmt, ...);
+int ulog(const unsigned char s, const char *mod, const char *f,
+         const unsigned long l, const char *fmt, ...);
 
 /*
  * Log at the level set by aos_set_log_level().
  *
- * @param[in]  fmt  same as printf() usage.
+ * @param[in]  ...  same as printf() usage.
  */
-#define LOG(...) rt_log(LOG_EMERG, "", __FILE__, __LINE__, __VA_ARGS__)
-
-#else
-/*
- * Function prototype for syncronized log text, Recommed use below brief API instead of this
- *
- * @param  s         Serverity of Log
- * @param  mod       module name, shall not longer than MOD_MAX_LEN, or else be trimmed
- * @param  fmt, ...  Variable Parameter, support format print to log
- * @return  0: success; other: fail.
- */
-#if SYNC_LOG_MOD
-int rt_log(const unsigned char s, const char* mod, const char *fmt, ...);
-#else
-int rt_log(const unsigned char s, const char *fmt, ...);
-#endif
-
-/*
- * Log at the level set by aos_set_log_level().
- *
- * @param[in]  fmt  same as printf() usage.
- */
-#if SYNC_LOG_MOD
-#define LOG(...) rt_log(LOG_EMERG, "", __VA_ARGS__)
-#else
-#define LOG(...) rt_log(LOG_EMERG, __VA_ARGS__)
-#endif
-
-#endif
+#define LOG(...) ulog(LOG_ALERT, "AOS", ULOG_TAG, __VA_ARGS__)
 
 #ifdef CONFIG_LOGMACRO_SILENT
 #define LOGF(mod, ...)
@@ -100,181 +70,99 @@ int rt_log(const unsigned char s, const char *fmt, ...);
 #define LOGW(mod, ...)
 #define LOGI(mod, ...)
 #define LOGD(mod, ...)
-#else
 
-#if SYNC_LOG_DETAILS
-
+#else /* !CONFIG_LOGMACRO_SILENT */
 /*
  * Log at fatal level.
  *
  * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
+ * @param[in]  ...  same as printf() usage.
  */
-#define LOGF(mod, ...) rt_log(LOG_CRIT, mod, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGF(mod, ...) ulog(LOG_CRIT, mod, ULOG_TAG, __VA_ARGS__)
 
 /*
  * Log at error level.
  *
  * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
+ * @param[in]  ...  same as printf() usage.
  */
-#define LOGE(mod, ...) rt_log(LOG_ERR, mod, __FILE__, __LINE__, __VA_ARGS__)
+#define LOGE(mod, ...) ulog(LOG_ERR, mod, ULOG_TAG, __VA_ARGS__)
 
 /*
  * Log at warning level.
  *
  * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
+ * @param[in]  ...  same as printf() usage.
  */
-#define LOGW(mod, ...) rt_log(LOG_WARNING, mod, __FILE__, __LINE__,  __VA_ARGS__)
+#define LOGW(mod, ...) ulog(LOG_WARNING, mod, ULOG_TAG, __VA_ARGS__)
 
 /*
  * Log at info level.
  *
  * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
+ * @param[in]  ...  same as printf() usage.
  */
-#define LOGI(mod, ...) rt_log(LOG_INFO, mod, __FILE__, __LINE__,  __VA_ARGS__)
+#define LOGI(mod, ...) ulog(LOG_INFO, mod, ULOG_TAG, __VA_ARGS__)
 
 /*
  * Log at debug level.
  *
  * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
+ * @param[in]  ...  same as printf() usage.
  */
 #ifdef DEBUG
-#define LOGD(mod, ...) rt_log(LOG_DEBUG, mod, __FILE__, __LINE__,  __VA_ARGS__)
-#else
-#define LOGD(mod, ...)
-#endif
-#else
-
-/*
- * Log at fatal level.
- *
- * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
- */
-#if SYNC_LOG_MOD
-#define LOGF(mod, ...) rt_log(LOG_CRIT, mod, __VA_ARGS__)
-#else
-#define LOGF(mod, ...) rt_log(LOG_CRIT, __VA_ARGS__)
-#endif
-
-/*
- * Log at error level.
- *
- * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
- */
-#if SYNC_LOG_MOD
-#define LOGE(mod, ...) rt_log(LOG_ERR, mod, __VA_ARGS__)
-#else
-#define LOGE(mod, ...) rt_log(LOG_ERR, __VA_ARGS__)
-#endif
-
-/*
- * Log at warning level.
- *
- * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
- */
-#if SYNC_LOG_MOD
-#define LOGW(mod, ...) rt_log(LOG_WARNING, mod, __VA_ARGS__)
-#else
-#define LOGW(mod, ...) rt_log(LOG_WARNING, __VA_ARGS__)
-#endif
-
-/*
- * Log at info level.
- *
- * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
- */
-#if SYNC_LOG_MOD
-#define LOGI(mod, ...) rt_log(LOG_INFO, mod, __VA_ARGS__)
-#else
-#define LOGI(mod, ...) rt_log(LOG_INFO, __VA_ARGS__)
-#endif
-
-/*
- * Log at debug level.
- *
- * @param[in]  mod  string description of module.
- * @param[in]  fmt  same as printf() usage.
- */
-#ifdef DEBUG
-#if SYNC_LOG_MOD
-#define LOGD(mod, ...) rt_log(LOG_DEBUG, mod, __VA_ARGS__)
-#else
-#define LOGD(mod, ...) rt_log(LOG_DEBUG, __VA_ARGS__)
-#endif
-
+#define LOGD(mod, ...) ulog(LOG_DEBUG, mod, ULOG_TAG,  __VA_ARGS__)
 #else
 #define LOGD(mod, ...)
 #endif
 
-#endif /* CONFIG_LOGMACRO_DETAILS */
 #endif /* CONFIG_LOGMACRO_SILENT */
 
 /**
  * Set the log level.
  *
- * @param[in]  log_level  level to be set,must be one of AOS_LL_NONE,AOS_LL_FATAL,AOS_LL_ERROR,AOS_LL_WARN,AOS_LL_INFO or AOS_LL_DEBUG.
+ * @param[in]  log_level  level to be set,must be one of AOS_LL_NONE,AOS_LL_FATAL,
+ * AOS_LL_ERROR,AOS_LL_WARN,AOS_LL_INFO or AOS_LL_DEBUG.
  */
-void aos_set_log_level(aos_log_level_t log_level);
-
-/**
- * Function prototype for log text, Recommed use below brief API instead of this
- *
- * REMARK: call this function will cost at least 64+ULOG_CONFIG_SYSLOG_SIZE*2 in syslog mode
- *          64 as normal local variable cost, temp buffer of size ULOG_CONFIG_SYSLOG_SIZE
- *          is used to format syslog message, another ULOG_CONFIG_SYSLOG_SIZE cost because
- *          aos message queue used. In future a customized queue will be designed to reduce the cost.
- *          cost is less if select EXTREAM_LOG_TEXT in the case of same SYSLOG_SIZE as normal local variable cost reduce
- *
- *
- * @param  m    Module type, reference @MOD_TYPE
- * @param  s    Serverity of Log, choose from above value,
- * @param  f    Usually File name
- * @param  l    Usually Line number of comment
- * @param  fmt, ...  Variable Parameter, support format print to log
- * @return  0: success. use type int is align with aos_*
- */
-int ulog (const MOD_TYPE m, const unsigned char s, const char* f, const unsigned long l, const char *fmt, ...);
-
-/**
- * Function prototype for ayncronized ulog
- *
- *
- * @param MOD, only choose from enum MOD_TYPE
- * @param  S    Serverity of Log, choose from above value,
- * @param  ...  Variable Parameter, Log text try to log
- */
-#define ULOGA(MOD, S, ...) ulog(MOD, S, __FILE__, __LINE__, __VA_ARGS__)
+int aos_set_log_level(aos_log_level_t log_level);
 
 /**
  * Function prototype for log init
  *
  * @param host_name host name, like "SOC", character shall no more than 8 to save resouce
  */
-extern void ulog_init(const char host_name[8]);
+void ulog_init(void);
 
 /**
  * Function prototype for ulog management
  *
  * @param cmd_str command string, user fill the command string to manage ulog, format
  *        shall as below:
- *        "tcpip on=1" or "tcpip off=1" to notice the tcpip feature is install or not, which
- *        have impact on the ulog pop out via udp session;
- *        "listen ip=XXX.XXX.XXX.XXX port=XXX" to notice the syslog listener's address
+ *        "tcpip on=1" or "tcpip off=1" to notice the tcpip feature is install or not,
+ *        which have impact on the ulog pop out via udp session;
+ *        "listen ip=XXX.XXX.XXX.XXX" to notice the syslog listener's address
  *        and/or port, only passing "listen ip=XXX.XXX.XXX.XXX" if you keep the port.
  *        For more command string, you can refer as ulog_man_handler_service in ulog.c
+ * @return 0: Success, -EINVAL: invalid parameter, -EPERM: ulog module not initied yet,
+ *        -EACCES: this function not work as there is no ASYNC method using. Other fail
+ *        reason may come from kernel message queue.
  */
-extern void ulog_man(const char *cmd_str);
+int ulog_man(const char *cmd_str);
+
+/**
+ * Function prototype for get ulog list recorded in file system.
+ * Precondition of use this function is switch on ULOG_POP_FS_ENABLE
+ *
+ * @param buf  buffer received the context of file list
+ * @param len  length of buffer
+ * @return  0 sucessfully, -EINVAL param illegal, -ENOMEM not enough room to receive file
+ *          list, -EIO system error.
+ */
+int aos_get_ulog_list(char *buf, const unsigned short len);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /* ULOG_H */
+
