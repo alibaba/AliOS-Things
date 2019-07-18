94394e3926661d9bcb475d485582715f810c39a0
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-07-18 18:36:01 +0800

    BugID:20525630: optimize the ulocation code and rename some file
    Change-Id: If9fcbd27bdbc0336c2fbd8e80bb1e48e70b575c3
    Reviewed-on: http://yuncode.alibaba-inc.com/218847
    Tested-by: 李诚 lc122798 <lc122798@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

--- middleware/ulocation/qianxun/uloc_qxwz_utils.c
+++ middleware/ulocation/qianxun/uloc_qxwz_utils.c
@@ -6,7 +6,7 @@
 #include "aos/hal/uart.h"
 #include "ulog/ulog.h"
 #include "qxwz_types.h"
-#include "qxwz_common.h"
+#include "uloc_qxwz_common.h"
 
 static uart_dev_t uart_dev;
 static uint8_t report_ggadata[REPORT_GGA_DATA_SIZE] = {0};
@@ -88,13 +88,77 @@ static int32_t qxwz_gga_filter(uint8_t gps_rawdata[], int len)
                 if('\n' == data){
                     cur_state = NONE;
                     report_ggadata[j] = data;
-                    j=0;
+                    j = 0;
                     ret = 0;
                 }
                 break;
         }
     }
     return ret;
+}
+
+
+static uint8_t uloc_getcomma(uint8_t num, char *str)
+{
+    uint8_t i,j = 0;
+    uint8_t len = 0;
+
+    if (num <= 0 || str == NULL){
+        return -1;
+    }
+
+    len = strlen(str);
+    for(i = 0; i < len; i++){
+        if(str[i] == ','){
+            j++;
+        }
+        if(j == num){
+            return (i+1); /* 返回当前找到的逗号位置的下一个位置 */
+        }
+    }
+
+    return 0;
+}
+
+static double uloc_convert_atof(char *s)
+{
+    char buf[128];
+    uint8_t i = 0;
+    double rev;
+
+    i = uloc_getcomma(1, s); /* 得到数据长度 */
+    strncpy(buf, s, i);
+    buf[i] = 0;        /* 加字符串结束标志 */
+    rev=atof(buf);     /* 字符串转float */
+
+    return rev;
+}
+
+int32_t uloc_ggainfo_parse(char *gga_data, uloc_gga_info *location)
+{
+    int32_t ret = -1;
+    char *buf;
+
+    if (gga_data == NULL || location == NULL){
+        return ret;
+    }
+    buf = gga_data;
+    location->status = buf[uloc_getcomma(6, buf)];
+    location->longitude = uloc_convert_atof(&buf[uloc_getcomma(4, buf)]);
+    location->latitude = uloc_convert_atof(&buf[uloc_getcomma(2, buf)]);
+    location->nors = buf[uloc_getcomma(3, buf)];
+    location->wore = buf[uloc_getcomma(5, buf)];
+    location->altitude = uloc_convert_atof(&buf[uloc_getcomma(9, buf)]);
+
+    return 0;
+}
+
+void show_ggainfo(uloc_gga_info *location)
+{
+    LOGI("uLocation-qxwz", "status : %c\n", location->status);
+    LOGI("uLocation-qxwz", "longitude : %10.4f %c\n", location->longitude, location->wore);
+    LOGI("uLocation-qxwz", "latitude : %10.4f %c\n", location->latitude, location->nors);
+    LOGI("uLocation-qxwz", "altitude : %10.4f\n", location->altitude);
 }
 
 int32_t qxwz_gga_init(uint8_t port, uint32_t baud)
@@ -125,7 +189,7 @@ int32_t qxwz_rtcm_send(qxwz_void_t *rtcm, qxwz_u32_t len)
         LOGE("uLocation-qxwz", "qxwz rtcm is null\n");
         return ret;
     }
-    ret = hal_uart_send(&uart_dev, (uint8_t*)rtcm, len, WAIT_FOREVER);
+    ret = hal_uart_send(&uart_dev, (uint8_t*)rtcm, len, AOS_WAIT_FOREVER);
     if (ret != 0){
         LOGE("uLocation-qxwz", "qxwz rtcm data send failed!\n");
         return ret;
@@ -145,7 +209,7 @@ int32_t qxwz_gga_get(uint8_t data[], uint32_t data_len)
         return ret;
     }
 
-    ret = hal_uart_recv_II(&uart_dev, &gps_raw_data, GPS_RAW_DATA_SIZE, recv_size, WAIT_FOREVER);
+    ret = hal_uart_recv_II(&uart_dev, &gps_raw_data, GPS_RAW_DATA_SIZE, recv_size, AOS_WAIT_FOREVER);
     if (ret != 0){
         LOGE("uLocation-qxwz", "qxwz gps data recv failed!\n");
         return ret;
