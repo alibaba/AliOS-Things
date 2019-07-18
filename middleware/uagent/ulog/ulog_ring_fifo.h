5f715994daba08646298ea131defd4fb5e7e9005
gonghui.hui <gonghui.hui@alibaba-inc.com>
2019-07-18 18:40:08 +0800

    BugID:21203770: Unify API Interface of uLog with LOGX, advanced function of uLog can be used via configuration
    Change-Id: I2d16735cd244c245672e5e2bd1682c8f1745e711

--- middleware/uagent/ulog/ulog_ring_fifo.h
+++ middleware/uagent/ulog/ulog_ring_fifo.h
@@ -2,16 +2,14 @@
  * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
-#ifndef U_RING_FIFO_H_
-#define U_RING_FIFO_H_
-
-#include "ulog_config.h"
-
+#ifndef ULOG_RING_FIFO_H_
+#define ULOG_RING_FIFO_H_
+#include <stdint.h>
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef void (*pop_callback)(void*, const void* src, const uint16_t len);
+typedef void (*pop_callback)(void*, void* src, const uint16_t len);
 
 extern int uring_fifo_push_s(const void* buf, const uint16_t len);
 
@@ -22,4 +20,4 @@ extern int uring_fifo_init(void);
 }
 #endif
 
-#endif /*U_RING_FIFO_H_*/
+#endif /*ULOG_RING_FIFO_H_*/
--- middleware/uagent/ulog/ulog_utility.c
+++ middleware/uagent/ulog/ulog_utility.c
@@ -10,15 +10,51 @@
 #include "ulog/ulog.h"
 #include "ulog_api.h"
 #include "aos/kernel.h"
+#include "aos/errno.h"
+#include "ulog_ring_fifo.h"
 
+#if SYSLOG_TIME_FORMAT
 static const char months[][4] = { "Jan", "Feb", "Mar", "Apr", "May",
                        "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+#endif
 
-/* Result is like "Nov 28 15:19:20.122 " */
-char *syslog_format_time(char *buffer, const int len)
+static aos_mutex_t log_mutex;
+
+/* log init mutex */
+void log_init_mutex(void)
 {
-    const long long ms = aos_now_ms();
-    if (buffer != NULL && len > 4) {
+    aos_mutex_new(&log_mutex);
+}
+
+bool log_get_mutex()
+{
+    return 0 == aos_mutex_lock(&log_mutex, AOS_WAIT_FOREVER);
+}
+
+void log_release_mutex()
+{
+    aos_mutex_unlock(&log_mutex);
+}
+
+char* replace_char(char *str, char find, char replace)
+{
+    if (NULL != str) {
+        char *current_pos = strchr(str, find);
+        while (NULL != current_pos) {
+            *current_pos = replace;
+            current_pos = strchr(current_pos, find);
+        }
+    }
+    return str;
+}
+
+/* result like 99.356 ,i.e. s.ms */
+/* Result is like "Nov 28 15:19:20.122" */
+char *ulog_format_time(char *buffer, const int len)
+{
+    if(NULL!=buffer && len>4){
+        long long ms = aos_now_ms();
+#if SYSLOG_TIME_FORMAT
         time_t rawtime;
         time(&rawtime);
         struct tm *tm = localtime(&rawtime);
@@ -28,15 +64,20 @@ char *syslog_format_time(char *buffer, const int len)
         const int32_t milli = ms % 1000;
         char      ms_str[8] = "";
         memset(ms_str, 0, sizeof(ms_str));
-        snprintf(ms_str, sizeof(ms_str), ".%03d ", milli);
+        snprintf(ms_str, sizeof(ms_str), ".%03d", milli);
         strncat(buffer, ms_str, len - strlen(buffer) - 1);
+#else /* !SYSLOG_TIME_FORMAT */
+        snprintf(buffer, len, "%4d.%03d", (int)(ms / 1000), (int)(ms % 1000));
+#endif /* SYSLOG_TIME_FORMAT */
     }
-
     return buffer;
 }
 
 char* trim_file_path(const char* path)
 {
+#if SYNC_ABS_PATH
+    return path;
+#else
     char* filename = (char*)path;
     if (path != NULL) {
         if ((filename = strrchr(path, '/')) != NULL) {
@@ -58,5 +99,27 @@ char* trim_file_path(const char* path)
     }
 
     return filename;
+#endif
+}
+
+int ulog_man(const char* cmd_str)
+{
+#if ULOG_CONFIG_ASYNC
+    int rc = -EINVAL;
+    if (NULL != cmd_str && strlen(cmd_str) > 0) {
+        rc = -EPERM;
+        if (log_init) {
+            const uint16_t cmd_str_size = strlen(cmd_str) + strlen(ULOG_CMD_PREFIX) + 1;
+            char* tmpbuf = (char*)aos_malloc(cmd_str_size);
+            strncpy(tmpbuf, ULOG_CMD_PREFIX, cmd_str_size);
+            strncat(tmpbuf, cmd_str, cmd_str_size - strlen(ULOG_CMD_PREFIX) - 1);
+            rc = uring_fifo_push_s(tmpbuf, cmd_str_size);
+            aos_free(tmpbuf);
+        }
+    }
+#else /*!ULOG_CONFIG_ASYNC */
+    int rc = -EACCES;
+#endif
+    return rc;
 }
 
