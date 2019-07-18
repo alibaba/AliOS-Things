85c832afcd388e5c9d1f4e800aea451ff93d672f
gonghui.hui <gonghui.hui@alibaba-inc.com>
2019-07-18 18:40:08 +0800

    BugID:21203770: Unify API Interface of uLog with LOGX, advanced function of uLog can be used via configuration
    Change-Id: I2d16735cd244c245672e5e2bd1682c8f1745e711

--- middleware/uagent/ulog/ulog_api.h
+++ middleware/uagent/ulog/ulog_api.h
@@ -4,44 +4,33 @@
 
 #ifndef U_LOG_API__H
 #define U_LOG_API__H
-
+#include <stdint.h>
+#include <stdbool.h>
 #include "ulog_config.h"
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-typedef enum{
-    SESSION_DIRECTION,  /* default out direction, usually uart for rtos, termial for Linux */
-#if ULOG_POP_UDP_ENABLE
-    SESSION_UDP,        /* syslog via udp, port 514 is default for syslog watcher */
-#endif
-
-#if ULOG_POP_FS_ENABLE
-    SESSION_FILE,       /* log on local file system */
-#endif
-
-    SESSION_CNT
-}SESSION_TYPE;
-
-
 extern bool log_init;
 
-int log_get_mutex(void);
+bool log_get_mutex(void);
 
 void log_release_mutex(void);
 
 void log_init_mutex(void);
 
-void on_sync_filter_level_change(const char level);
-char get_sync_stop_level(void);
+void on_sync_filter_level_change(const ulog_session_type_t session, const char level);
+void on_filter_change(const ulog_session_type_t session, const char level);
 
+char* get_sync_stop_level();
 
 int32_t ulog_fs_init(void);
 
-void on_filter_level_change(const SESSION_TYPE session, const uint8_t level);
+void on_filter_level_changes(const ulog_session_type_t session, const uint8_t level);
 
-void ulog_async_init(const char host_name[8]);
+bool check_pass_pop_out(const ulog_session_type_t session, const uint8_t level);
+
+void ulog_async_init();
 
 int32_t pop_out_on_udp(const char* data, const uint16_t len);
 
@@ -49,21 +38,25 @@ int32_t pop_out_on_fs(const char* data, const uint16_t len);
 
 void on_show_ulog_file(void);
 
-
 void update_net_cli(const char cmd, const char* param);
+
 void fs_control_cli(const char cmd, const char* param);
 
-
+char* replace_char(char *str, char find, char replace);
 
 void on_update_syslog_watcher_addr(const uint32_t ip_nl, const uint32_t port);
 void on_tcpip_service_on(const uint32_t on, const uint32_t off);
 void on_fs_record_pause(const uint32_t on, const uint32_t off);
 
 char* trim_file_path(const char* path);
-char *syslog_format_time(char *buffer, const int len);
+char *ulog_format_time(char *buffer, const int len);
+
+int http_start(const char *url, const unsigned short idx);
+void on_fs_upload(const uint32_t idx, const uint32_t start);
 
 #ifdef __cplusplus
 }
 #endif
 
 #endif /*U_LOG_API__H*/
+
--- middleware/uagent/ulog/ulog_ring_fifo.c
+++ middleware/uagent/ulog/ulog_ring_fifo.c
@@ -2,11 +2,11 @@
  * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
-#include "uring_fifo.h"
+#include "ulog_ring_fifo.h"
 #include <string.h>
 #include <stdio.h>
-#include <stdarg.h>
 #include "aos/kernel.h"
+#include "ulog_config.h"
 
 static aos_queue_t ulog_queue;
 static uint8_t*    ulog_buffer;
@@ -24,7 +24,7 @@ int uring_fifo_init()
     if (ulog_buffer == NULL) {
         ulog_buffer = aos_malloc(DEFAULT_ASYNC_BUF_SIZE);
         if (ulog_buffer != NULL) {
-            rc = aos_queue_new(&ulog_queue, ulog_buffer, DEFAULT_ASYNC_BUF_SIZE, SYSLOG_SIZE);
+            rc = aos_queue_new(&ulog_queue, ulog_buffer, DEFAULT_ASYNC_BUF_SIZE, ULOG_SIZE);
             if (0 != rc) {
                 aos_free(ulog_buffer);
                 ulog_buffer = NULL;
@@ -51,7 +51,7 @@ int uring_fifo_push_s(const void* buf, const uint16_t len)
 
 int uring_fifo_pop_cb(pop_callback cb, void* cb_arg)
 {
-    char tmp_buf[SYSLOG_SIZE];
+    char tmp_buf[ULOG_SIZE];
     unsigned int rcv_size = 0;
     int rc = aos_queue_recv(&ulog_queue, AOS_WAIT_FOREVER, tmp_buf, &rcv_size);
     if ((0 == rc) && (cb != NULL)) {
