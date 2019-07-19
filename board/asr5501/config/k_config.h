973a1b93da0775f53fa00af65d62ffa851655b4b
jianniu.ljg|lijianguo <jianniu.ljg@alibaba-inc.com>
2019-07-19 11:10:29 +0800

    [PATCH] BugID:21612384:update to SDKV2.8.0 to fix memory crash and connect error
    Change-Id: Ic5cdce9bea7011487b17202739dc015ea7ac3563

--- board/asr5501/config/k_config.h
+++ board/asr5501/config/k_config.h
@@ -73,7 +73,7 @@
 
 /*must reserve enough stack size for timer cb will consume*/
 #ifndef RHINO_CONFIG_TIMER_TASK_STACK_SIZE
-#define RHINO_CONFIG_TIMER_TASK_STACK_SIZE   1024
+#define RHINO_CONFIG_TIMER_TASK_STACK_SIZE   256
 #endif
 #ifndef RHINO_CONFIG_TIMER_TASK_PRI
 #define RHINO_CONFIG_TIMER_TASK_PRI          5
