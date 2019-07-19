736cfd34f625de814e3849fb61bb489bc69cbc25
chenan.xxw <chenan.xxw@alibaba-inc.com>
2019-07-19 10:17:55 +0800

    BugID: 21435991: update jsengine_app
    Change-Id: I1f030fd1d15c45a97c5232ecc342677e6a041666

--- app/example/jsengine_app/jsengine_app.c
+++ app/example/jsengine_app/jsengine_app.c
@@ -1,16 +1,23 @@
 /*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
 #include "aos/kernel.h"
+#include "ulog/ulog.h"
+#include "aos/yloop.h"
+#include "netmgr.h"
 
-extern int tiny_engine_start();
+extern int tiny_engine_start(void);
 
-/*the entrance for tinyengine on AliOS-things*/
+/* the entrance for JSEngine on AliOS-things */
 int application_start(int argc, char *argv[])
 {
+    aos_set_log_level(AOS_LL_DEBUG);
+
     tiny_engine_start();
+
     aos_loop_run();
+
     return 0;
 }
 
