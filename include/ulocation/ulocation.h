babfcea8a60a9730e33b9cdffdfc8ab5c74be2e5
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-07-22 17:58:08 +0800

    BugID:18200497: modified the ulocation code specification
    Change-Id: Ibe745da54e30f3965171f9bbf9fcb650948b7eee

--- include/ulocation/ulocation.h
+++ include/ulocation/ulocation.h
@@ -9,7 +9,7 @@ typedef enum {
     ULOC_SCENARIO_OUTDOOR = 0,
     ULOC_SCENARIO_INDOOR  = 1,
     ULOC_SCENARIO_COUNT
-} uloc_scenario_e;
+} ulocation_scenario_e;
 
 typedef struct {
     float longitude;
@@ -40,59 +40,59 @@ extern "C" {
 /**
  * Initial location service
  */
-int uloc_init(uloc_scenario_e scen, int update_inv);
+int ulocation_init(ulocation_scenario_e scen, int update_inv);
 
 /**
  * Terminate locating service
  */
-int uloc_deinit(void);
+int ulocation_deinit(void);
 
 /**
  * Get latitude
  */
-int uloc_get_latitude(float *latitude);
+int ulocation_get_latitude(float *latitude);
 
 /**
  * Get altitude
  */
-int uloc_get_altitude(float *altitude);
+int ulocation_get_altitude(float *altitude);
 
 /**
  * Get longitude
  */
-int uloc_get_longitude(float *longitude);
+int ulocation_get_longitude(float *longitude);
 
 /**
  * Get x
  */
-int uloc_get_x(float *x);
+int ulocation_get_x(float *x);
 
 /**
  * Get y
  */
-int uloc_get_y(float *y);
+int ulocation_get_y(float *y);
 
 /**
  * Get z
  */
-int uloc_get_z(float *z);
+int ulocation_get_z(float *z);
 
 /**
  * Get location
  */
-int uloc_get_location(location_t *rlt);
+int ulocation_get_location(location_t *rlt);
 
 /**
  * Fetch new location value, this method should be invoked by user
  * when there is no individual self task.
  */
-int uloc_update_locationinfo(location_t **gps, char **mmac, char **macs, /* wifi */
+int ulocation_update_locationinfo(location_t **gps, char **mmac, char **macs, /* wifi */
                              char **cdma, char **bts, char **nearbts);
 
 /**
  * uloc_update_gpsinfo
  */
-int uloc_update_gpsinfo(location_t *lo);
+int ulocation_update_gpsinfo(location_t *lo);
 
 #ifdef __cplusplus
 }
