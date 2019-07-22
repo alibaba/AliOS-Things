296f258fd40d1f5e4517f2809758cd0119e64736
Zilong Liu <zilong.liuzl@alibaba-inc.com>
2019-07-22 10:18:51 +0800

    BugID:21666951: modify code style
    Change-Id: I305db0f117003a9d569f3775b9d62e548c10e316

--- include/network/coap.h
+++ include/network/coap.h
@@ -2,8 +2,8 @@
  * Copyright (C) 2015-2018 Alibaba Group Holding Limited
  */
 
-#ifndef __COAP_API_H__
-#define __COAP_API_H__
+#ifndef COAP_API_H
+#define COAP_API_H
 
 #if defined(__cplusplus)
 extern "C" {
@@ -13,16 +13,16 @@ extern "C" {
 
 /*iotx return code definition*/
 typedef enum {
-    IOTX_ERR_RECV_MSG_TIMEOUT = -9,   /*Receive message timeout */
-    IOTX_ERR_SEND_MSG_FAILED =  -8,   /* Send message failed*/
-    IOTX_ERR_MSG_TOO_LOOG    =  -7,   /* The payload too loog */
-    IOTX_ERR_URI_TOO_LOOG    =  -6,   /* URI length too long */
-    IOTX_ERR_NOT_AUTHED      =  -5,   /* Client isn't authed */
-    IOTX_ERR_AUTH_FAILED     =  -4,   /* Client authed failed */
-    IOTX_ERR_BUFF_TOO_SHORT  =  -3,   /* Buffer too short */
-    IOTX_ERR_NO_MEM          =  -2,   /* Malloc failed */
-    IOTX_ERR_INVALID_PARAM   =  -1,   /* Invalid parameter */
-    IOTX_SUCCESS             =   0,   /* Success */
+    IOTX_ERR_RECV_MSG_TIMEOUT = -9, /*Receive message timeout */
+    IOTX_ERR_SEND_MSG_FAILED  = -8, /* Send message failed*/
+    IOTX_ERR_MSG_TOO_LOOG     = -7, /* The payload too loog */
+    IOTX_ERR_URI_TOO_LOOG     = -6, /* URI length too long */
+    IOTX_ERR_NOT_AUTHED       = -5, /* Client isn't authed */
+    IOTX_ERR_AUTH_FAILED      = -4, /* Client authed failed */
+    IOTX_ERR_BUFF_TOO_SHORT   = -3, /* Buffer too short */
+    IOTX_ERR_NO_MEM           = -2, /* Malloc failed */
+    IOTX_ERR_INVALID_PARAM    = -1, /* Invalid parameter */
+    IOTX_SUCCESS              =  0, /* Success */
 } iotx_ret_code_t;
 
 /* The message payload encode format */
@@ -33,10 +33,10 @@ typedef enum {
 
 /* The message type */
 typedef enum {
-    IOTX_MESSAGE_CON  = 0,       /* confirmable message */
-    IOTX_MESSAGE_NON  = 1,       /* non-confirmable message */
-    IOTX_MESSAGE_ACK  = 2,       /* acknowledgement message */
-    IOTX_MESSAGE_RST  = 3,       /* reset message */
+    IOTX_MESSAGE_CON = 0, /* confirmable message */
+    IOTX_MESSAGE_NON = 1, /* non-confirmable message */
+    IOTX_MESSAGE_ACK = 2, /* acknowledgement message */
+    IOTX_MESSAGE_RST = 3, /* reset message */
 } iotx_msg_type_t;
 
 /* IoTx events to notify application */
@@ -47,31 +47,30 @@ typedef enum {
 } iotx_coap_event_t;
 
 typedef enum {
-    IOTX_COAP_RESP_CODE_CONTENT        = 0x45,       /* Mapping to 2.05, Content*/
-    IOTX_COAP_RESP_CODE_BAD_REQUEST    = 0x80,       /* Mapping to 4.00, Bad Request*/
-    IOTX_COAP_RESP_CODE_UNAUTHORIZED   = 0x81,       /* Mapping to 4.01, Token is invalid or expire*/
-    IOTX_COAP_RESP_CODE_NOT_FOUND      = 0x84,       /* Mapping to 4.04, Path or uri is not found*/
-    IOTX_COAP_RESP_CODE_URL_TOO_LONG   = 0x8E,       /* Mapping to 4.14, The request url is too long*/
-    IOTX_COAP_RESP_CODE_INTERNAL_SERVER_ERROR = 0xA0,/* Mapping to 5.00, Internal server error*/
-
+    IOTX_COAP_RESP_CODE_CONTENT               = 0x45, /* Mapping to 2.05, Content*/
+    IOTX_COAP_RESP_CODE_BAD_REQUEST           = 0x80, /* Mapping to 4.00, Bad Request*/
+    IOTX_COAP_RESP_CODE_UNAUTHORIZED          = 0x81, /* Mapping to 4.01, Token is invalid or expire*/
+    IOTX_COAP_RESP_CODE_NOT_FOUND             = 0x84, /* Mapping to 4.04, Path or uri is not found*/
+    IOTX_COAP_RESP_CODE_URL_TOO_LONG          = 0x8E, /* Mapping to 4.14, The request url is too long*/
+    IOTX_COAP_RESP_CODE_INTERNAL_SERVER_ERROR = 0xA0, /* Mapping to 5.00, Internal server error*/
 } iotx_coap_resp_code_t;
 
 /* Callback function to notify the application events.*/
 typedef void (*iotx_event_handle_t)(void *context, iotx_coap_event_t event, void *p_data);
 
 typedef struct {
-    char        product_key[IOTX_PRODUCT_KEY_LEN + 1];
-    char        device_name[IOTX_DEVICE_NAME_LEN + 1];
-    char        device_id[IOTX_DEVICE_ID_LEN + 1];
-    char        device_secret[IOTX_DEVICE_SECRET_LEN + 1];
+    char product_key[IOTX_PRODUCT_KEY_LEN + 1];
+    char device_name[IOTX_DEVICE_NAME_LEN + 1];
+    char device_id[IOTX_DEVICE_ID_LEN + 1];
+    char device_secret[IOTX_DEVICE_SECRET_LEN + 1];
 } iotx_coap_device_info_t;
 
 /* IoTx initializa parameters */
 typedef struct {
-    char                      *p_url;        /*Can be NULL*/
-    int                       wait_time_ms; /*unit is micro second*/
-    iotx_coap_device_info_t   *p_devinfo;    /*Device info*/
-    iotx_event_handle_t       event_handle; /*TODO, not supported now*/
+    char                    *p_url;        /*Can be NULL*/
+    int                      wait_time_ms; /*unit is micro second*/
+    iotx_coap_device_info_t *p_devinfo;    /*Device info*/
+    iotx_event_handle_t      event_handle; /*TODO, not supported now*/
 } iotx_coap_config_t;
 
 /* Callback function to handle the response message.*/
@@ -79,18 +78,16 @@ typedef void (*iotx_response_callback_t)(void *p_arg, void *p_message);
 
 /* IoTx message definition */
 typedef struct {
-    unsigned char           *p_payload;
-    unsigned short           payload_len;
-    iotx_content_type_t      content_type;
-    iotx_msg_type_t          msg_type;
-    void                    *user_data;
-    iotx_response_callback_t resp_callback;
+    unsigned char            *p_payload;
+    unsigned short            payload_len;
+    iotx_content_type_t       content_type;
+    iotx_msg_type_t           msg_type;
+    void                     *user_data;
+    iotx_response_callback_t  resp_callback;
 } iotx_message_t;
-
 
 /*iotx coap context definition*/
 typedef void iotx_coap_context_t;
-
 
 /** @defgroup group_api api
  *  @{
@@ -125,7 +122,6 @@ iotx_coap_context_t *IOT_CoAP_Init(iotx_coap_config_t *p_config);
  */
 void IOT_CoAP_Deinit(iotx_coap_context_t **p_context);
 
-
 /**
  * @brief   Handle device name authentication with remote server.
  *
@@ -136,8 +132,7 @@ void IOT_CoAP_Deinit(iotx_coap_context_t **p_context);
  * @retval IOTX_ERR_AUTH_FAILED     : Authenticate failed or timeout.
  * @see iotx_ret_code_t.
  */
-int  IOT_CoAP_DeviceNameAuth(iotx_coap_context_t *p_context);
-
+int IOT_CoAP_DeviceNameAuth(iotx_coap_context_t *p_context);
 
 /**
  * @brief   Handle CoAP response packet from remote server,
@@ -148,8 +143,7 @@ int  IOT_CoAP_DeviceNameAuth(iotx_coap_context_t *p_context);
  * @return status.
  * @see iotx_ret_code_t.
  */
-int  IOT_CoAP_Yield(iotx_coap_context_t *p_context);
-
+int IOT_CoAP_Yield(iotx_coap_context_t *p_context);
 
 /**
  * @brief   Send a message with specific path to server.
@@ -164,7 +158,7 @@ int  IOT_CoAP_Yield(iotx_coap_context_t *p_context);
  * @retval IOTX_ERR_NOT_AUTHED      : The client hasn't authenticated with server
  * @see iotx_ret_code_t.
  */
-int  IOT_CoAP_SendMessage(iotx_coap_context_t *p_context,   char *p_path, iotx_message_t *p_message);
+int IOT_CoAP_SendMessage(iotx_coap_context_t *p_context, char *p_path, iotx_message_t *p_message);
 
 /**
 * @brief Retrieves the length and payload pointer of specified message.
@@ -177,7 +171,7 @@ int  IOT_CoAP_SendMessage(iotx_coap_context_t *p_context,   char *p_path, iotx_m
 * @retval IOTX_ERR_INVALID_PARAM    : Can't get the payload due to invalid parameter.
 * @see iotx_ret_code_t.
 **/
-int  IOT_CoAP_GetMessagePayload(void *p_message, unsigned char **pp_payload, int *p_len);
+int IOT_CoAP_GetMessagePayload(void *p_message, unsigned char **pp_payload, int *p_len);
 
 /**
 * @brief Get the response code from a CoAP message.
@@ -190,7 +184,7 @@ int  IOT_CoAP_GetMessagePayload(void *p_message, unsigned char **pp_payload, int
 * @retval  IOTX_ERR_INVALID_PARAM   : Pointer to the message is NULL.
 * @see iotx_ret_code_t.
 **/
-int  IOT_CoAP_GetMessageCode(void *p_message, iotx_coap_resp_code_t *p_resp_code);
+int IOT_CoAP_GetMessageCode(void *p_message, iotx_coap_resp_code_t *p_resp_code);
 
 /** @} */ /* end of api_coap */
 /** @} */ /* end of api */
@@ -199,3 +193,4 @@ int  IOT_CoAP_GetMessageCode(void *p_message, iotx_coap_resp_code_t *p_resp_code
 }
 #endif
 #endif
+
