ac322b5d55c1dffe9515109596c58e078723fd54
Zilong Liu <zilong.liuzl@alibaba-inc.com>
2019-07-22 14:24:51 +0800

    BugID:21666951: modify code style
    Change-Id: Ie1c1a57fb27232d25ef0fb9532be74101e466bf4

--- kernel/mk/kspace/fs_fd.c
+++ kernel/mk/kspace/fs_fd.c
@@ -1,11 +1,13 @@
 /*
  * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
+
 #include "k_config.h"
 
 #if (RHINO_CONFIG_UFS > 0)
 
 #include <stdio.h>
+
 #include "task_group.h"
 
 typedef struct {
@@ -19,9 +21,9 @@ typedef struct {
 int fs_fd_close(int pid)
 {
     task_group_t *group;
-    klist_t *head;
-    klist_t *iter;
-    fs_fd_t *fd_obj;
+    klist_t      *head;
+    klist_t      *iter;
+    fs_fd_t      *fd_obj;
 
     group = task_group_get_by_pid(pid);
     if (NULL == group) {
@@ -47,13 +49,13 @@ int fs_fd_close(int pid)
     return 0;
 }
 
-// called in fs close
+/* called in fs close */
 int fs_fd_dec_ref(int pid, int fd)
 {
     task_group_t *group;
-    klist_t *head;
-    klist_t *iter;
-    fs_fd_t *fd_obj;
+    klist_t      *head;
+    klist_t      *iter;
+    fs_fd_t      *fd_obj;
 
     group = task_group_get_by_pid(pid);
     if (NULL == group) {
@@ -85,15 +87,16 @@ int fs_fd_dec_ref(int pid, int fd)
     return 0;
 }
 
-// called in fs open
+/* called in fs open */
 int fs_fd_inc_ref(int pid, int fd)
 {
     task_group_t *group;
-    klist_t *head;
-    klist_t *iter;
-    fs_fd_t *fd_obj;
-    int      found;
-    int      ret = 0;
+    klist_t      *head;
+    klist_t      *iter;
+    fs_fd_t      *fd_obj;
+
+    int found;
+    int ret = 0;
 
     group = task_group_get_by_pid(pid);
     if (NULL == group) {
@@ -118,8 +121,9 @@ int fs_fd_inc_ref(int pid, int fd)
     if (found == 0) {
         fd_obj = (fs_fd_t*)krhino_mm_alloc(sizeof(fs_fd_t));
         if (NULL != fd_obj) {
-            fd_obj->fd = fd;
+            fd_obj->fd      = fd;
             fd_obj->ref_cnt = 1;
+
             klist_init(&fd_obj->node);
             klist_insert(head, &fd_obj->node);
         } else {
