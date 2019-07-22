0ec035c3c7ea5e7dcd8b788e3e75ea2ef02488b3
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-07-22 17:58:37 +0800

    BugID:18200497: modified the ulocation code specification
    Change-Id: I7fcac5dad0bb757ed86d0b40241b7e05b56117e3

--- app/example/ulocation/qianxunapp/aos.mk
+++ app/example/ulocation/qianxunapp/aos.mk
@@ -4,7 +4,7 @@ $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 0.0.1
 $(NAME)_SUMMARY := ulocation integrated qianxun SDK app for demo
 
-$(NAME)_SOURCES := qianxun.c
+$(NAME)_SOURCES := qxwz_example.c
 
 GLOBAL_DEFINES += AOS_NO_WIFI
 
