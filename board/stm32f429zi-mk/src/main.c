bc779c975bd504bfd65162983fd8cc6074e8830e
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-07-24 15:25:26 +0800

    BugID:16846667: normalize microkernel startup process
    Change-Id: Ic74d4536580c8ee2a442c7866389caaa3516d704

--- board/stm32f429zi-mk/src/main.c
+++ board/stm32f429zi-mk/src/main.c
@@ -2,17 +2,17 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-#include "aos/kernel.h"
-#include <k_api.h>
-#include <aos/kernel.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "aos/kernel.h"
+#include "k_api.h"
 
 #include "aos/hal/uart.h"
 #include "hal_uart_stm32f4.h"
 #include "board.h"
 
-#include <aos/cli.h>
+#include "aos/cli.h"
+#include "aos/init.h"
 #include "ulog/ulog.h"
 
 #define AOS_START_STACK 1536
@@ -20,29 +20,38 @@
 static ktask_t demo_task_obj;
 cpu_stack_t demo_task_buf[AOS_START_STACK];
 
-extern int application_start(int argc, char **argv);
-
-extern void board_init(void);
-
-extern void hw_start_hal(void);
+static kinit_t kinit;
 
 extern int lwip_tcpip_init(void);
+extern int application_start(int argc, char **argv);
+
+int g_proc_var; /**< for test purpose */
+
+static void var_init()
+{
+    kinit.argc = 0;
+    kinit.argv = NULL;
+    kinit.cli_enable = 1;
+}
 
 static void sys_init(void)
 {
     stm32_soc_init();
 
-    /* hw_start_hal(); */
-
     aos_cli_init();
 
-    ulog_init();
+    var_init();
+
+    aos_components_init(&kinit);
 
     LOG("strart lwip_tcpip_init\r\n");
 
-    //lwip_tcpip_init();
+    lwip_tcpip_init();
 
-    aos_run_app();
+    g_proc_var = 0x5a5a;
+    LOG("kernel set g_proc_var to 0x%x\r\n", g_proc_var);
+
+    application_start(0, NULL);
 }
 
 
