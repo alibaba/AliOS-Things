3ae3fb7bc507ba96b30470ca421df4ae1704c329
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-07-25 16:51:18 +0800

    BugID:21737422: remove unnecessary GLOBAL_INCLUDES in osal
    Change-Id: Ieee862b4deac6ff4df9d46cab89f0e60984c00ea

--- osal/cmsis/aos.mk
+++ osal/cmsis/aos.mk
@@ -6,8 +6,6 @@ $(NAME)_SUMMARY    := cmsis for alios things
 #if component's header files under another directory, add RPM_INCLUDE_DIR to indicate where the header file folder is located
 RPM_INCLUDE_DIR    := cmsis
 
-GLOBAL_INCLUDES += .
-
 $(NAME)_SOURCES := cmsis_os.c
 
 GLOBAL_DEFINES  += AOS_COMP_OSAL_CMSIS
