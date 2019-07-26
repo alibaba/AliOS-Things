9950698784858344772e9b04b362c05bf61f5592
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-07-26 11:16:06 +0800

    BugID:21737422: Add fatfs & spiffs & yts global includes
    Change-Id: I895b661c874cbd1413e9afee70f22327ce01ad3b

---
+++ include/spiffs/aos_spiffs.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef FS_SPIFFS_H
+#define FS_SPIFFS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int32_t vfs_spiffs_register(void);
+int32_t vfs_spiffs_unregister(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+ 
+#endif
