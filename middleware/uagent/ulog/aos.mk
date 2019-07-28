bf0f452526f4ddbb291ac26a11cca58342ff5b4f
gonghui.hui <gonghui.hui@alibaba-inc.com>
2019-07-28 22:56:19 +0800

    BugID:21203770: Update ulog for menuconfig
    Change-Id: Ia3a7224976824fdfe6747d7c1c829cdd838e211f

--- middleware/uagent/ulog/aos.mk
+++ middleware/uagent/ulog/aos.mk
@@ -12,15 +12,37 @@ $(NAME)_SOURCES     := ulog.c ulog_init.c ulog_utility.c
 
 $(NAME)_CFLAGS      += -Wall -Werror
 $(NAME)_CFLAGS      += -DMOD_VER=\"$($(NAME)_VERSION)\"
+
+#ulog support fs record
+ifeq ($(ULOG_CONFIG_POP_FS),y)
+$(NAME)_SOURCES     += ulog_session_file.c
+$(NAME)_SOURCES     += ulog_fs_cfg.c
+
+ifeq ($(ULOG_CONFIG_FS_SPIFFS),y)
+$(NAME)_COMPONENTS += spiffs
+$(info >>> BE Reserved flash for spiffs in board/flashpartion under spiffs select)
+
+else ifeq ($(ULOG_CONFIG_FS_FATFS_SD),y)
+$(NAME)_COMPONENTS  += fatfs
+GLOBAL_DEFINES      += CONFIG_AOS_FATFS_SUPPORT_MMC
+
+else ifeq ($(ULOG_CONFIG_FS_FATFS_FLASH),y)
+$(NAME)_COMPONENTS  += fatfs
+GLOBAL_DEFINES      += CONFIG_AOS_FATFS_SUPPORT_RAW_FLASH
+
+endif
+endif
+
+#ulog support syslog udp mode
+ifeq ($(ULOG_CONFIG_POP_UDP), y)
+$(NAME)_SOURCES     += ulog_session_udp.c
+endif
+
+#ulog support aync mode
 ifeq ($(ULOG_CONFIG_ASYNC),y)
 GLOBAL_DEFINES += ULOG_CONFIG_ASYNC
 $(NAME)_SOURCES     += ulog_async.c
 $(NAME)_SOURCES     += ulog_ring_fifo.c
-ifeq ($(ULOG_CONFIG_POP_FS),y)
-$(NAME)_SOURCES     += ulog_session_file.c
-$(NAME)_SOURCES     += ulog_fs_cfg.c
-endif
-$(NAME)_SOURCES     += ulog_session_udp.c
 endif
 
 $(NAME)_INCLUDES += include
