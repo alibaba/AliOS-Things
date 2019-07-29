1316ea4ae62efea7540f6e34496adcc0d4711ae5
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-07-29 19:31:19 +0800

    BugID:21737422: Clean unnecessary global include
    Change-Id: I364313d928ef004a6b55bab5f16c7b699f11d7bf

--- 3rdparty/experimental/fs/jffs2/jffs2.h
+++ 3rdparty/experimental/fs/jffs2/jffs2.h
@@ -2,19 +2,19 @@
  * Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
-#ifndef FS_UFFS_H
-#define FS_UFFS_H
+#ifndef FS_JFFS2_H
+#define FS_JFFS2_H
 
 #ifdef __cplusplus
 extern "C" {
 #endif
- 
-int32_t vfs_uffs_register(void);
-int32_t vfs_uffs_unregister(void);
- 
+
+int32_t jffs2_register(uint32_t start_addr, uint32_t length);
+int32_t jffs2_unregister(void);
+
 #ifdef __cplusplus
 }
 #endif
 
- 
 #endif
+
