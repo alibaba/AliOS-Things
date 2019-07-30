d115fe7f44f6bc34c7dc81fe8c69dc1648017cdf
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-07-30 20:07:41 +0800

    BugID:21805011: modified the code specification
    Change-Id: I4cb699d78b7ebad129ec4f16dfbd4e9544036f39

---
+++ kernel/bus/canopen/warp/canopen_warp.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifdef AOS_CANOPEN
+
+#include "aos/errno.h"
+#include "board.h"
+#include "canfestival.h"
+
+#define MAX_NODE_ID  127
+#define MAX_INDEX    0xFFFF
+#define MAX_SUBINDEX 0xFF
+#define CO_BAUD_125K 125000
+#define CO_BAUD_250K 250000
+#define CO_BAUD_500K 500000
+#define CO_BAUD_1M   1000000
+
+typedef enum canopen_status {
+    CO_SUCCESS = 0u,
+    CO_ODENTRY_REGISTER_ERROR,
+    CO_DRIVER_INIT_FAILED,
+} co_status_t;
+
+typedef uint32_t (*co_objectdict_cb)(CO_Data *d, const indextable *table, uint8_t i);
+
+typedef struct od_subindex {
+    uint8_t          access_type;
+    uint8_t          data_type;
+    uint32_t         size;
+    void*            object;
+    co_objectdict_cb callback;
+} od_subindex_t;
+
+/** Struct for creating entries in the communictaion profile
+ */
+typedef struct indextable {
+    od_subindex_t  *sub_index;
+    uint8_t        sub_count;
+    uint16_t       index;
+} indextable_t;
+
+typedef void (*co_heartbeatError_t)(CO_Data *d, uint8_t id);
+typedef void (*co_nodeguardError_t)(CO_Data *d, uint8_t id);
+typedef void (*co_initialisation_t)(CO_Data *d);
+typedef void (*co_preOperational_t)(CO_Data *d);
+typedef void (*co_operational_t)(CO_Data *d);
+typedef void (*co_stopped_t)(CO_Data *d);
+typedef void (*co_post_sync_t)(CO_Data *d);
+typedef void (*co_post_TPDO_t)(CO_Data *d);
+typedef void (*co_post_SlaveBootup_t)(CO_Data *d, uint8_t id);
+typedef void (*co_post_emcy_t)(CO_Data *d, uint8_t id, uint16_t err_code,
+                               uint8_t err_reg, const uint8_t err_spec);
+typedef uint32_t (* co_storeODSubIndex_t)(CO_Data *d, uint16_t index, uint8_t sub_index);
+
+typedef struct co_register_function_callback
+{
+    co_initialisation_t   initialisation;
+    co_preOperational_t   preOperational;
+    co_operational_t      operational;
+    co_stopped_t          stopped;
+    co_post_sync_t        post_sync;
+    co_post_TPDO_t        post_TPDO;
+    co_post_SlaveBootup_t post_Slave_Bootup;
+    co_post_emcy_t        post_emcy;
+    co_heartbeatError_t   heartbeatError;
+    co_nodeguardError_t   nodeGuardError;
+    co_storeODSubIndex_t  store_OD_SubIndex;
+}co_register_function_callback_t;
+
+int canopen_init(PORT_CAN_TYPE can_port, PORT_TIMER_TYPE timer_port, uint32_t baud, CO_Data *d);
+int canopen_od_init(CO_Data *d, co_register_function_callback_t *fun_callback);
+int canopen_od_handler(CO_Data *d, uint16_t index, uint8_t sub_index, co_objectdict_cb *od_callback);
+int init_nodes(CO_Data *d, uint8_t id);
+
+#endif    /* AOS_CANOPEN */
