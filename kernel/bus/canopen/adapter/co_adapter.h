b8bd0c20089b1b7ccc0300d85085786732d0ba6c
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-07-30 20:07:41 +0800

    BugID:21805011: modified the code specification
    Change-Id: I4cb699d78b7ebad129ec4f16dfbd4e9544036f39

---
+++ kernel/bus/canopen/adapter/co_adapter.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifndef __CO_ADAPTER_H__
+#define __CO_ADAPTER_H__
+
+#include "ulog/ulog.h"
+#include "aos/hal/can.h"
+#include "aos/hal/timer.h"
+#include "canfestival.h"
+
+#define WAIT_FOREVER 0xFFFFFFFFU
+#define MAX_COUNTER_VALUE 0xFFFF
+
+extern uint32_t hal_timer_getcounter(timer_dev_t *tim);
+extern void hal_timer_setcounter(timer_dev_t *tim, uint32_t counter);
+extern void can_dispatch();
+
+int32_t timer_init(uint8_t port);
+int32_t can_init(uint8_t port, uint32_t baud, CO_Data *d);
+
+#endif    /* __ADAPTER_H__ */
+
