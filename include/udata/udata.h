05cffcfee0c7f855e93b97d37283c517c3354800
andy.zwj <andy.zwj@alibaba-inc.com>
2019-07-31 10:08:15 +0800

    BugID:21810812:standardize udata include files
    Change-Id: I1e1b3a6bc6a717ecfc1d8e4372082904fc5fbff1

--- include/udata/udata.h
+++ include/udata/udata.h
@@ -9,6 +9,24 @@
 extern "C" {
 #endif
 
+#include <stdbool.h>
+
+#define MAX_DATA_SIZE     64
+#define UATA_PAYLOAD_SIZE ((MAX_DATA_SIZE + sizeof(uint64_t) - 1) / sizeof(uint64_t))
+
+#define UDATA_MSG_DEV_READ           1
+#define UDATA_MSG_DEV_IOCTL          2
+#define UDATA_MSG_DEV_OPEN           3
+#define UDATA_MSG_DEV_CLOSE          4
+#define UDATA_MSG_DEV_ENABLE         5
+#define UDATA_MSG_DEV_DISABLE        6
+#define UDATA_MSG_SERVICE_SUBSRIBE   7
+#define UDATA_MSG_SERVICE_UNSUBSRIBE 8
+#define UDATA_MSG_SERVICE_PROCESS    9
+#define UDATA_MSG_SERVICE_IOCTL      10
+#define UDATA_MSG_REPORT_PUBLISH     11
+#define UDATA_MSG_DATA_TO_CLOUD      12
+
 typedef enum {
     UDATA_SERVICE_ACC = 0,  /* Accelerometer */
     UDATA_SERVICE_MAG,      /* Magnetometer */
@@ -47,6 +65,20 @@ typedef enum {
 
     UDATA_MAX_CNT,
 } udata_type_e;
+
+typedef struct _sensor_msg_pkg_t {
+    unsigned int cmd;
+    unsigned int index;
+    unsigned int value;
+} sensor_msg_pkg_t;
+
+typedef struct _udata_pkg_t {
+    bool         valid;
+    udata_type_e type;
+    uint64_t     payload[UATA_PAYLOAD_SIZE];
+} udata_pkg_t;
+
+int udata_register_msg_handler(void *func);
 
 int udata_report_publish(udata_type_e type, void *pdata);
 
