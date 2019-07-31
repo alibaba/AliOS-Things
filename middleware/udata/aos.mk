137b7129dccf70bf29d6dfeff23ad5f643aa044c
andy.zwj <andy.zwj@alibaba-inc.com>
2019-07-31 10:09:15 +0800

    BugID:21810812:standardize udata include files
    Change-Id: I2518b2f82ed3dd1faeeda3baf5bc1720880a946a

--- middleware/udata/aos.mk
+++ middleware/udata/aos.mk
@@ -29,7 +29,7 @@ EXTRA_TARGET_MAKEFILES +=  $(SOURCE_ROOT)/middleware/udata/gen_cjson_data.mk
 GLOBAL_DEFINES += UDATA_CJSON_SUPPORTED
 endif
 
-GLOBAL_INCLUDES += ./include
+$(NAME)_INCLUDES += ./include
 
 ifeq ($(COMPILER),)
 $(NAME)_CFLAGS      += -Wall -Werror
