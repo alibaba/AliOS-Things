ffdb3c02edf29f4b6b9b9773e680e58567e383b8
andy.zwj <andy.zwj@alibaba-inc.com>
2019-07-31 10:09:15 +0800

    BugID:21810812:standardize udata include files
    Change-Id: I2518b2f82ed3dd1faeeda3baf5bc1720880a946a

--- middleware/udata/include/udata_queue.h
+++ middleware/udata/include/udata_queue.h
@@ -4,8 +4,7 @@
 
 int udata_post_msg(sensor_msg_pkg_t msg);
 int udata_unregister_msg_handler(int index);
-int udata_register_msg_handler(void *func);
-int udata_start();
+int udata_start(void);
 int udata_new_servicetask(const char *name, void (*fn)(void *),void *arg,int stack_size, int prio);
 int udata_msg_recv(int task_id, unsigned int ms, void *msg,unsigned int *size);
 int udata_observe_servicetask_tag(int taskid,sensor_tag_e tag, uint8_t instance);
