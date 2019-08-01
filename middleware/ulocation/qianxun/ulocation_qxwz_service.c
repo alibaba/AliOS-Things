3bc11ad7117a95d8b42d056f23ae93efbf2f3225
薪翁 <xinweng.ljh@alibaba-inc.com>
2019-08-01 14:01:24 +0800

    BugID:21838817: remove the ulocation golbal include file and code style
    Change-Id: If9cef64a2bb55bb8adfdd4bbdfd275eefa73b96d
    Reviewed-on: http://yuncode.alibaba-inc.com/221839
    Tested-by: 李诚 lc122798 <lc122798@alibaba-inc.com>
    Reviewed-by: 刘自龙 zilong.liuzl <zilong.liuzl@alibaba-inc.com>

--- middleware/ulocation/qianxun/ulocation_qxwz_service.c
+++ middleware/ulocation/qianxun/ulocation_qxwz_service.c
@@ -3,35 +3,40 @@
  */
 
 #ifdef QXWZ_ENABLED
+
 #include "ulog/ulog.h"
 #include "qxwz_types.h"
 #include "qxwz_status.h"
 #include "qxwz_sdk.h"
-#include "ulocation_qxwz_common.h"
 #include "board.h"
+#include "ulocation_qxwz_common.h"
+#include "ulocation_qxwz_service.h"
 
-static uint8_t auth_flag = 0;
+static uint8_t auth_flag                  = 0;
 static uint8_t s_gga[REPORT_GGA_DATA_SIZE]={0};
+
 static ulocation_gga_info location;
 
 static int32_t receive_iprtcm(qxwz_void_t *rtcm, qxwz_u32_t len, qxwz_data_type_e type)
 {
     int32_t ret = -1;
-    LOGD("uLocation-qxwz", "got ip rtcm!\n");
-    if (rtcm == NULL){
-        LOGD("uLocation-qxwz", "rtcm is null!\n");
+
+    LOGD("uLocation-qxwz", "got ip rtcm!");
+    if (rtcm == NULL) {
+        LOGD("uLocation-qxwz", "rtcm is null!");
         return ret;
     }
-    if (type == RTCM_TYPE_RAW){
+    if (type == RTCM_TYPE_RAW) {
         ret = qxwz_rtcm_send(rtcm, len);
     }
+
     return ret;
 }
 
 static void receive_status(qxwz_s32_t status)
 {
-    LOGD("uLocation-qxwz", "got rtcm status = %d!\n", status);
-    if (status == QXWZ_STATUS_NTRIP_USER_IDENTIFY_SUCCESS){
+    LOGD("uLocation-qxwz", "got rtcm status = %d!", status);
+    if (status == QXWZ_STATUS_NTRIP_USER_IDENTIFY_SUCCESS) {
         auth_flag = 1;
     }
 }
@@ -45,46 +50,54 @@ qxwz_status_response_t status_res = {
     receive_status
 };
 
-int32_t uloc_qianxun_service(qxwz_usr_config_t *s_config)
+int ulocation_qianxun_service(ulocation_qxwz_usr_config_t *usr_config)
 {
-    static int s_current_time = 0;
-    static uint32_t i = 0;
-    int32_t ret = 0;
+    int s_current_time = 0;
+    uint32_t i         = 0;
+    int ret            = 0;
+
     s_current_time = time(NULL);
 
-    ret = qxwz_gga_init(GGA_DATA_PORT, GGA_DATA_BAUD);  /* need modified board logic port name */
-    if (ret != 0){
-        LOGE("uLocation-qxwz", "ulocation qianxun app gga init failed!\n");
+    ret = ulocation_qxwz_init(GGA_DATA_PORT, GGA_DATA_BAUD);  /* need modified board logic port name */
+    if (ret != 0) {
+        LOGE("uLocation-qxwz", "ulocation qianxun init failed!");
         return ret;
     }
 
-    qxwz_setting(s_config, QXWZ_FALSE);
+    ret = qxwz_setting(usr_config, QXWZ_FALSE);
+    if (ret != 0) {
+        LOGE("uLocation-qxwz", "ulocation qianxun user config failed!");
+        return ret;
+    }
+
     ret = qxwz_start(&data_res, &status_res);
-    if(0 != ret){
+    if (0 != ret) {
+        LOGE("uLocation-qxwz", "ulocation qianxun service init failed!");
         return ret;
     }
 
-    while(1){
+    while(1) {
         ret = qxwz_tick(s_current_time);
-        if (ret < 0){
-            LOGE("uLocation-qxwz", "qxwz tick is %d!\n", ret);
+        if (ret < 0) {
+            LOGE("uLocation-qxwz", "qxwz tick is %d!", ret);
             return ret;
         }
         aos_msleep(GGA_DATA_PERIOD);
         s_current_time += 1;
         qxwz_soc_recv_handler();
 
-        if(auth_flag == 1 ){
-            ret = qxwz_gga_get(s_gga, REPORT_GGA_DATA_SIZE);
-            if (ret < 0){
-                LOGE("uLocation-qxwz", "GPS data get error!\n");
+        if (auth_flag == 1 ) {
+            LOGD("uLocation-qxwz", "qxwz auth success!");
+            ret = ulocation_qxwz_getgga(s_gga);
+            if (ret < 0) {
+                LOGE("uLocation-qxwz", "gga data get error!");
             }
-
             qxwz_send_data(s_gga, strlen(s_gga) + 1, UDATA_GGA);
-            ret = uloc_ggainfo_parse(s_gga, &location);
-            if (ret != 0){
+            ret = ulocation_ggainfo_parse(s_gga, &location);
+            if (ret != 0) {
                 return ret;
             }
+            show_ggainfo(&location);
         }
     }
 
@@ -92,3 +105,4 @@ int32_t uloc_qianxun_service(qxwz_usr_config_t *s_config)
 }
 
 #endif
+
