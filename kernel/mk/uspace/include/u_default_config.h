7ddd2f93df5e04bcb124c1eaa4c05786f773fc8c
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-08-01 21:23:07 +0800

    BugID:16846667: adjusted header file includ path
    Change-Id: Ice5bdb6cf7533fab1506de5b775950a96faf4135

--- kernel/mk/uspace/include/u_default_config.h
+++ kernel/mk/uspace/include/u_default_config.h
@@ -2,11 +2,22 @@
  * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
-#ifndef U_APP_DEFAULT_CONFIG_H
-#define U_APP_DEFAULT_CONFIG_H
+#ifndef U_DEFAULT_CONFIG_H
+#define U_DEFAULT_CONFIG_H
 
-/* user space */
-#if (RHINO_CONFIG_USER_SPACE > 0)
+/**
+ * config user space app name, it's also entry task name
+ */
+#ifndef RHINO_CONFIG_APP_NAME
+#define RHINO_CONFIG_APP_NAME "uapp"
+#endif
+
+/**
+ * config user space app version
+ */
+#ifndef RHINO_CONFIG_APP_VERSION
+#define RHINO_CONFIG_APP_VERSION 0x00000001
+#endif
 
 /**
  * config user space entry task priority
@@ -35,19 +46,7 @@
 #define RHINO_CONFIG_APP_ENTRY_TASK_USTACK_SIZE 256
 #endif
 
-/**
- * config user space app name, it's also entry task name
- */
-#ifndef RHINO_CONFIG_APP_NAME
-#define RHINO_CONFIG_APP_NAME "uapp"
-#endif
 
-/**
- * config user space app version
- */
-#ifndef RHINO_CONFIG_APP_VERSION
-#define RHINO_CONFIG_APP_VERSION 0x00000001
-#endif
 
 /********************** URES **************************/
 #if (RHINO_CONFIG_URES > 0)
@@ -73,20 +72,20 @@
  * config user space resource task's kstack size
  * the stack actual size in bytes =
  *   RHINO_CONFIG_URES_TASK_KSTACK_SIZE * sizeof (cpu_stack_t)
- * On default set to 512 bytes.
+ * On default set to 1K bytes.
  */
 #ifndef RHINO_CONFIG_URES_TASK_KSTACK_SIZE
-#define RHINO_CONFIG_URES_TASK_KSTACK_SIZE 128
+#define RHINO_CONFIG_URES_TASK_KSTACK_SIZE 256
 #endif
 
 /**
  * config user space resource task's ustack size
  * the stack actual size in bytes =
  *   RHINO_CONFIG_URES_TASK_USTACK_SIZE * sizeof (cpu_stack_t)
- * On default set to 512 bytes.
+ * On default set to 1K bytes.
  */
 #ifndef RHINO_CONFIG_URES_TASK_USTACK_SIZE
-#define RHINO_CONFIG_URES_TASK_USTACK_SIZE 128
+#define RHINO_CONFIG_URES_TASK_USTACK_SIZE 256
 #endif /* RHINO_CONFIG_URES_TASK_USTACK_SIZE */
 
 #elif (RHINO_CONFIG_URES_SUPPORT > 0)
@@ -116,14 +115,14 @@
  * config user space cli task's kstack size
  */
 #ifndef RHINO_CONFIG_UCLI_TASK_KSTACK_SIZE
-#define RHINO_CONFIG_UCLI_TASK_KSTACK_SIZE 128
+#define RHINO_CONFIG_UCLI_TASK_KSTACK_SIZE 256
 #endif
 
 /**
  * config user space cli task's ustack size
  */
 #ifndef RHINO_CONFIG_UCLI_TASK_USTACK_SIZE
-#define RHINO_CONFIG_UCLI_TASK_USTACK_SIZE 128
+#define RHINO_CONFIG_UCLI_TASK_USTACK_SIZE 256
 #endif
 
 #elif (RHINO_CONFIG_UCLI_SUPPORT > 0)
@@ -150,7 +149,7 @@
  * config user space callback task kstack size
  */
 #ifndef RHINO_CONFIG_UCALLBACK_TASK_KSTACK_SIZE
-#define RHINO_CONFIG_UCALLBACK_TASK_KSTACK_SIZE 128
+#define RHINO_CONFIG_UCALLBACK_TASK_KSTACK_SIZE 256
 #endif
 
 /**
@@ -184,7 +183,7 @@
  * config user space timer task's kstack size
  */
 #ifndef RHINO_CONFIG_UTIMER_TASK_KSTACK_SIZE
-#define RHINO_CONFIG_UTIMER_TASK_KSTACK_SIZE 128
+#define RHINO_CONFIG_UTIMER_TASK_KSTACK_SIZE 256
 #endif
 
 /**
@@ -201,7 +200,6 @@
 #define RHINO_CONFIG_UTIMER_MSG_NUM 4
 #endif
 
-#endif /* RHINO_CONFIG_USER_SPACE */
 
-#endif /* U_APP_DEFAULT_CONFIG_H */
+#endif /* U_DEFAULT_CONFIG_H */
 
