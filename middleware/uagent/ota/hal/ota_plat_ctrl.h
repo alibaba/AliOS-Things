17554003038b446fa1aee46c27b79b398b32544b
zixun.lyq <zixun.lyq@alibaba-inc.com>
2019-08-05 16:11:15 +0800

    BugID:21853979: Bugfixes for 2.1.1
    1. receive two times upgrade message
    2. compile error for iTLS
    3. clear parameter and data
    4. rename log module from uota to ota
    5. retry for downloading
    
    Change-Id: I7ae1c92336b6b4c6798a0eff843bea9b76880078

--- middleware/uagent/ota/hal/ota_plat_ctrl.h
+++ middleware/uagent/ota/hal/ota_plat_ctrl.h
@@ -15,6 +15,7 @@ int ota_rollback(void);
 int hal_reboot_bank(void);
 int ota_is_download_mode(void);
 int ota_int(ota_boot_param_t *param);
+int ota_clear(ota_boot_param_t *param);
 int ota_set_boot(ota_boot_param_t *param);
 int ota_write(unsigned int *off, char *in_buf, unsigned int in_buf_len);
 int ota_read(unsigned int *off, char *out_buf, unsigned int out_buf_len);
