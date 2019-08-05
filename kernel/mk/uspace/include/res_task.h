5dc83b9383cf9929ccb06d671eb816db7dbc8f04
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-08-05 20:19:38 +0800

    BugID:16846667: added extern "C" to uspace header files to support C++
    Change-Id: I76e4a6ac2280a05b499b76cb5e0a0aa07ceddb8e

--- kernel/mk/uspace/include/res_task.h
+++ kernel/mk/uspace/include/res_task.h
@@ -5,6 +5,14 @@
 #ifndef RES_TASK_H
 #define RES_TASK_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int res_task_start(size_t kstack_size, size_t ustack_size, uint8_t prio);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* RES_TASK_H */
