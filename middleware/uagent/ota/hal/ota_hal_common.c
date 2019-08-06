da2d7e717cb3dfc868b2781594a5ba1ddec816a9
zixun.lyq <zixun.lyq@alibaba-inc.com>
2019-08-06 09:13:31 +0800

    BugID:21835226:8266 default to secure download mode
    1. 8266 to secure download mode
    2. Add base64 for itls
    
    Change-Id: I0bc8b883e875df8672b21e618bbe7a67c1c9232b

--- middleware/uagent/ota/hal/ota_hal_common.c
+++ middleware/uagent/ota/hal/ota_hal_common.c
@@ -21,7 +21,6 @@
 #if !defined (AOS_COMP_OTA_BLE)
 #include "mbedtls/sha256.h"
 #include "mbedtls/md5.h"
-#include "mbedtls/base64.h"
 #include "mbedtls/bignum.h"
 #include "mbedtls/rsa.h"
 #endif
@@ -383,18 +382,86 @@ OTA_RSA_PUBKEY_VERIFY_OVER:
 #endif
     return ret;
 }
+#endif
 
 /*base64*/
+static const unsigned char base64_dec_map[128] =
+{
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127, 127, 127, 127, 127, 127, 127, 127,
+    127, 127, 127,  62, 127, 127, 127,  63,  52,  53,
+     54,  55,  56,  57,  58,  59,  60,  61, 127, 127,
+    127,  64, 127, 127, 127,   0,   1,   2,   3,   4,
+      5,   6,   7,   8,   9,  10,  11,  12,  13,  14,
+     15,  16,  17,  18,  19,  20,  21,  22,  23,  24,
+     25, 127, 127, 127, 127, 127, 127,  26,  27,  28,
+     29,  30,  31,  32,  33,  34,  35,  36,  37,  38,
+     39,  40,  41,  42,  43,  44,  45,  46,  47,  48,
+     49,  50,  51, 127, 127, 127, 127, 127
+};
+
 int ota_base64_decode(const unsigned char *src, unsigned int slen, unsigned char *dst, unsigned int *dlen)
 {
-    unsigned int len = 0;
-    if(mbedtls_base64_decode(dst, *dlen, &len, src, slen) != 0) {
-        return -1;
+    unsigned int i, n;
+    unsigned int j, x;
+    unsigned char *p;
+
+    for( i = n = j = 0; i < slen; i++ )
+    {
+        if( ( slen - i ) >= 2 &&
+            src[i] == '\r' && src[i + 1] == '\n' )
+            continue;
+
+        if( src[i] == '\n' )
+            continue;
+
+        if( src[i] == '=' && ++j > 2 )
+            return -1;
+
+        if( src[i] > 127 || base64_dec_map[src[i]] == 127 )
+            return -1;
+
+        if( base64_dec_map[src[i]] < 64 && j != 0 )
+            return -1;
+
+        n++;
     }
-    *dlen = len;
+
+    if( n == 0 )
+        return 0;
+
+    n = ( ( n * 6 ) + 7 ) >> 3;
+    n -= j;
+
+    if( dst == 0 || *dlen < n )
+    {
+        *dlen = n;
+        return -2;
+    }
+
+   for( j = 3, n = x = 0, p = dst; i > 0; i--, src++ )
+   {
+        if( *src == '\r' || *src == '\n' )
+            continue;
+
+        j -= ( base64_dec_map[*src] == 64 );
+        x  = ( x << 6 ) | ( base64_dec_map[*src] & 0x3F );
+
+        if( ++n == 4 )
+        {
+            n = 0;
+            if( j > 0 ) *p++ = (unsigned char)( x >> 16 );
+            if( j > 1 ) *p++ = (unsigned char)( x >>  8 );
+            if( j > 2 ) *p++ = (unsigned char)( x       );
+        }
+    }
+
+    *dlen = p - dst;
+
     return 0;
 }
-#endif
 
 /*CRC16*/
 static unsigned short update_crc16(unsigned short crcIn, unsigned char byte)
@@ -461,7 +528,7 @@ int ota_hal_mqtt_publish(char *topic, int qos, void *data, int len)
 int ota_hal_mqtt_subscribe(char *topic, void *cb, void *ctx)
 {
 #if (OTA_SIGNAL_CHANNEL) == 1
-    return IOT_MQTT_Subscribe_Sync(NULL, topic, 0, cb, ctx, 1000);
+    return IOT_MQTT_Subscribe(NULL, topic, 0, cb, ctx);
 #else
     return 0;
 #endif
