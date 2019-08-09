9293f861f1b7ecc3c01171ce21036e3d63cbd75a
Xicai Chen <xicai.cxc@alibaba-inc.com>
2019-08-09 14:38:59 +0800

    BugID:21970143: fix alcs compile error
    Change-Id: Id9fb18daa9d4a43033c8d608093f126708f3f830

--- middleware/linkkit/dev_model/alcs/alcs_mqtt.c
+++ middleware/linkkit/dev_model/alcs/alcs_mqtt.c
@@ -9,11 +9,9 @@
 #include "alcs_internal.h"
 #include "alcs_api_internal.h"
 #include "CoAPExport.h"
-#include "alcs_api.h"
 #include "alcs_adapter.h"
 #include "alcs_mqtt.h"
 #include "alcs_adapter.h"
-#include "CoAPPlatform.h"
 
 static alcs_mqtt_ctx_t g_alcs_mqtt_ctx;
 
