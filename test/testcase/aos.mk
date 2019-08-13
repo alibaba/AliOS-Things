63340fd533fe8162ba2cf1d2f22a6cdf880b760d
Fan Qian <qianfan.qf@alibaba-inc.com>
2019-08-13 16:39:59 +0800

    BugID:22016604: rename yts framework module to yunit
    Change-Id: I050870d02f852686971c9fbcf43fb02c4740eae8

--- test/testcase/aos.mk
+++ test/testcase/aos.mk
@@ -4,9 +4,7 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := testcase framework
 
-$(NAME)_COMPONENTS := auto_component
-
-$(NAME)_SOURCES := yunit.c yts_main.c
+$(NAME)_COMPONENTS := auto_component yunit
 
 $(NAME)_CFLAGS += -Wall -Werror
 
