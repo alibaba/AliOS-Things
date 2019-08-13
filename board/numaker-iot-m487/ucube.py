6171b64617a9f256a6a1e9371f96be3349f9ab6a
jinzhou.zjz <jinzhou.zjz@alibaba-inc.com>
2019-08-13 14:30:55 +0800

    BugID:20502926: add keil project of numaker-iot-m487
    Change-Id: I066c689fcdb0978392d0214b99b2c815ac04dc9e

--- board/numaker-iot-m487/ucube.py
+++ board/numaker-iot-m487/ucube.py
@@ -1,1 +1,3 @@
 linux_only_targets="athostapp blink coapapp helloworld http2app httpapp linkkit_gateway linkkitapp lwm2mapp meshapp modbus_demo mqttapp otaapp udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp yts"
+windows_only_targets="helloworld|COMPILER=armcc linkkitapp|COMPILER=armcc"
+
