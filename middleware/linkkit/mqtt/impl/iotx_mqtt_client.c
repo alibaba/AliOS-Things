d5258d91080dc9aa08bc49883920aeddcedf9301
zhangtian <dongren.zt@alibaba-inc.com>
2019-08-14 11:37:22 +0800

    BugID:21918609:fix itls connect
    Change-Id: If1035ba1fb317405d709cfcffa43bdc30b265465

--- middleware/linkkit/mqtt/impl/iotx_mqtt_client.c
+++ middleware/linkkit/mqtt/impl/iotx_mqtt_client.c
@@ -211,13 +211,6 @@ static int iotx_mc_init(iotx_mc_client_t *pClient, iotx_mqtt_param_t *pInitParam
 
     memset(&pClient->ipstack, 0, sizeof(utils_network_t));
 
-#ifdef SUPPORT_TLS
-    {
-        extern const char *iotx_ca_crt;
-        pInitParams->pub_key = iotx_ca_crt;
-    }
-#endif
-
     rc = iotx_net_init(&pClient->ipstack, pInitParams->host, pInitParams->port, pInitParams->pub_key);
 
     if (SUCCESS_RETURN != rc) {
