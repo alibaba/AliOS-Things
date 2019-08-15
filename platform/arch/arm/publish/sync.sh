73e2da691c2ab808f7f5e021aa9fefded1903020
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-08-15 19:45:25 +0800

    BugID:16846667: Support to close microkernel key source files
    Change-Id: Id9acaca6d122a4dabb7eb9fcfe66b19762cc4252

---
+++ platform/arch/arm/publish/sync.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp -rf armv6m-mk_lib ../armv6m-mk/lib
+cp armv6m-mk_aos.mk ../armv6m-mk/aos.mk
+
+cp -rf armv7m-mk_lib ../armv7m-mk/lib
+cp armv7m-mk_aos.mk ../armv7m-mk/aos.mk
+
+cp -rf armv7a-mk_lib ../armv7a-mk/lib
+cp armv7a-mk_aos.mk ../armv7a-mk/aos.mk
