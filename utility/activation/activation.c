4f1c51a76fc16ec8cf34fab56ff9df257e0dbb24
magao.xwj <magao.xwj@alibaba-inc.com>
2019-08-15 20:16:25 +0800

    BugID:21999418:move thing.deviceinfo.update SYS_ALIOS_ACTIVATION message send function to activation.c
    Change-Id: I14e7d803ec7d0b2dcdc97eff25dcefc78e0e685a

--- utility/activation/activation.c
+++ utility/activation/activation.c
@@ -4,6 +4,10 @@
 #include <stdio.h>
 #include <string.h>
 #include "activation.h"
+#include "linkkit/infra/infra_report.h"
+#include "linkkit/wrappers/wrappers_defs.h"
+#include "linkkit/wrappers/wrappers_os.h"
+#include "ulog/ulog.h"
 
 #ifndef NULL
 #define NULL    ((void *)0)
@@ -14,6 +18,7 @@
 
 #define PLAIN_FLAG "PLAIN"
 
+#define TAG "activation"
 /* Don't do anything in release builds */
 #define DEBUG_PRINTF(fmt, ...)
 //#define DEBUG_PRINTF printf 
@@ -78,4 +83,106 @@ UINT32 aos_get_version_info (UINT8 version_num[VERSION_NUM_SIZE], UINT8 random_n
     return ENCYPT_SUCCESS;    
 }
 
+static const char *DEVICE_INFO_UPDATE_FMT = "{\"id\":\"%d\",\"version\":\"1.0\",\"params\":["
+                         "{\"attrKey\":\"SYS_ALIOS_ACTIVATION\",\"attrValue\":\"%s\",\"domain\":\"SYSTEM\"}"
+                         "],\"method\":\"thing.deviceinfo.update\"}";
+                                     
+int iotx_report_ext_msg(info_report_func_pt info_report_func, void * handle)
+{
+    int ret = 0;
+    int i;
+    char mac[MAC_ADDRESS_SIZE] = {0};
+    char version[VERSION_NUM_SIZE] = {0};
+    char random_num[RANDOM_NUM_SIZE];
+    char chip_code[CHIP_CODE_SIZE] = {0};
+    char output[AOS_ACTIVE_INFO_LEN] = {0};
+    char topic_name[IOTX_URI_MAX_LEN + 1] = {0};
+    char *msg = NULL;
+    int  msg_len = 0;
+    char _product_key[IOTX_PRODUCT_KEY_LEN + 1] = {0};
+    char _device_name[IOTX_DEVICE_NAME_LEN + 1] = {0};
 
+    if (info_report_func == NULL) {
+        LOGE(TAG,"report func not register!");
+        return -1;
+    }
+    HAL_GetProductKey(_product_key);
+    HAL_GetDeviceName(_device_name);
+
+    LOGD(TAG,"devinfo report");
+
+    /* Construct aos activation data */
+    aos_get_version_hex((unsigned char *)version);
+    aos_get_mac_hex((unsigned char *)mac);
+    mac[6] = ACTIVE_SINGLE_GW;
+#ifdef BUILD_AOS
+    mac[7] = ACTIVE_LINKKIT_AOS;
+#else
+    mac[7] = ACTIVE_LINKKIT_ONLY;
+#endif
+
+    aos_srand(HAL_UptimeMs());
+    for (i = 0; i < 4; i ++) {
+        random_num[i] = (char)(aos_rand()&0xFF);
+    }
+    aos_get_chip_code((unsigned char *)chip_code);
+    /*
+    input: version 4byte + random 4 byte + mac 4byte + chip_code 4byte
+    output: output_buffer store the version info process. length at least OUTPUT_SPACE_SIZE
+    return: 0 success, 1 failed
+    */
+    ret = aos_get_version_info((unsigned char *)version, (unsigned char *)random_num, (unsigned char *)mac,
+                               (unsigned char *)chip_code, (unsigned char *)output, AOS_ACTIVE_INFO_LEN);
+    if (ret) {
+        LOGE(TAG,"aos_get_version_info failed");
+        return FAIL_RETURN;
+    }
+    LOGD(TAG,"get aos avtive info: %s", output);
+
+    /* devinfo update topic name */
+    ret = snprintf(topic_name,
+                       IOTX_URI_MAX_LEN,
+                       "/sys/%s/%s/thing/deviceinfo/update",
+                       _product_key,
+                       _device_name);
+    if (ret <= 0) {
+        LOGE(TAG,"topic generate err");
+        return FAIL_RETURN;
+    }
+    LOGD(TAG,"devinfo report topic: %s", topic_name);
+
+    msg_len = strlen(DEVICE_INFO_UPDATE_FMT) + 10 + AOS_ACTIVE_INFO_LEN + 1;
+    msg = (char *)aos_malloc(msg_len);
+    if (msg == NULL) {
+        LOGE(TAG,"malloc err");
+        return FAIL_RETURN;
+    }
+    memset(msg, 0, msg_len);
+
+    /* devinfo update message */
+    ret = snprintf(msg,
+                       msg_len,
+                       DEVICE_INFO_UPDATE_FMT,
+                       iotx_report_id(),
+                       output
+                      );
+    if (ret <= 0) {
+        LOGE(TAG,"topic msg generate err");
+        aos_free(msg);
+        return FAIL_RETURN;
+    }
+    LOGD(TAG,"devinfo report data: %s", msg);
+
+    if (info_report_func != NULL) {
+        info_report_func(handle, topic_name, 1, msg, strlen(msg));
+    }
+
+    aos_free(msg);
+    if (ret < 0) {
+        LOGE(TAG,"publish failed, ret = %d", ret);
+        return FAIL_RETURN;
+    }
+    LOGD(TAG,"devinfo report succeed");
+
+    return SUCCESS_RETURN;
+}                                     
