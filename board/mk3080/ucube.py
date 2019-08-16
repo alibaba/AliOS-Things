ed2c29ffb692067dff75d72b9ca49b7384e433ed
Li Cheng <lc122798@alibaba-inc.com>
2019-08-16 19:20:38 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: I976ccdbd1bf7d0757cf2ceab231d894786942572

--- board/mk3080/ucube.py
+++ board/mk3080/ucube.py
@@ -1,2 +1,2 @@
 
-linux_only_targets="athostapp blink coapapp das_app helloworld http2app httpapp id2_app itls_app linkkit_gateway linkkitapp lwm2mapp meshapp modbus_demo mqttapp otaapp prov_app tls udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp yts"
+linux_only_targets="athostapp blink coapapp das_app helloworld http2app httpapp id2_app itls_app linkkit_gateway linkkitapp lwm2mapp meshapp modbus_demo mqttapp otaapp prov_app tls udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp yts"
