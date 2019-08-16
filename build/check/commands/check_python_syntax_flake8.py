4beda71cad14f0c32237ad7b25f9e4c4d7449954
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-08-16 20:12:47 +0800

    BugID:19675668: Fix return code for check scripts
    Change-Id: I580e590a72c254a61e0b9ef421fee5f96b1f47ab

--- build/check/commands/check_python_syntax_flake8.py
+++ build/check/commands/check_python_syntax_flake8.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from util import info, warn
+from util import info, warn, error
 
 name = "flake8"
 short_help = "Check python syntax with flake8"
@@ -12,8 +12,8 @@ Usage: aos check %s [python files ...]
 Check python syntax with Flake8.
 """ % name
 
-## Flake8 check configs
-MAX_LINE_LEN = 100
+# Flake8 check configs
+MAX_LINE_LEN = 120
 
 
 def run():
@@ -22,12 +22,17 @@ def run():
         warn(help)
         return 0
 
-    cmd = "flake8 --max-line-length=%s %s" % (MAX_LINE_LEN, " ".join(sys.argv[1:]))
+    cmd = "flake8 --max-line-length=%s %s > %s.log 2>&1" % (MAX_LINE_LEN, " ".join(sys.argv[1:]), name)
     info("Running cmd: %s ..." % cmd)
     ret = os.system(cmd)
     if not ret:
         info("Check Python Syntax Pass!")
+    else:
+        os.system("cat %s.log" % name)
+        error("Check Python Syntax Failed")
+
+    return ret
 
 
 if __name__ == "__main__":
-    run()
+    sys.exit(run())
