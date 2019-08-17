86eaea565eb1fcece99f2244d89df7004eeb7edf
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-08-17 14:06:06 +0800

    BugID:19675668: Add check for aos.mk
    Change-Id: Ie66623e91b7d954c5be50e6e92eda8f69f66fea6

--- build/check/commands/util.py
+++ build/check/commands/util.py
@@ -2,6 +2,7 @@ import click
 import platform
 import os
 import sys
+import subprocess
 
 
 def info(message):
@@ -37,3 +38,37 @@ def which(program):
             if is_exe(exe_file):
                 return exe_file
     return None
+
+
+def check_win_format(filename):
+    """ Check if the file has Windows CRLF line terminators """
+    ret = False
+    try:
+        command = "file %s" % filename
+        output = subprocess.check_output(command, shell=True)
+        if "with CRLF line terminators" in output:
+            ret = True
+    except Exception as e:
+        print(e)
+        pass
+
+    return ret
+
+
+def check_invalid_chars(filename, matchlist):
+    ret = []
+    command = 'grep -w -E -i -r -n "%s" %s' % ("|".join(matchlist), filename)
+    try:
+        output = subprocess.check_output(command, shell=True)
+        for line in output.splitlines():
+            ret.append(line.split(":")[0])
+    except Exception:
+        pass
+
+    return ret
+
+
+def check_extra_blank(filename):
+    """ Check if there's extra blank spaces in file """
+    matchlist = ["^\\s+$", "\\s+$"]
+    return check_invalid_chars(filename, matchlist)
