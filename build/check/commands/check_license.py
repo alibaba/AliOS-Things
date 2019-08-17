f33c5ec5d82e2d7bd236e56f23a0a02285a4a024
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-08-17 14:06:06 +0800

    BugID:19675668: Add check for aos.mk
    Change-Id: Ie66623e91b7d954c5be50e6e92eda8f69f66fea6

--- build/check/commands/check_license.py
+++ build/check/commands/check_license.py
@@ -5,6 +5,7 @@ from util import info, warn, error
 
 name = "license"
 short_help = "Check for 3rd Licenses"
+log_file = "check_%s.log" % name
 
 help = """
 Usage: aos check %s [source files ...]
@@ -37,14 +38,18 @@ def run():
     if len(sys.argv) > 1:
         check_files = sys.argv[1:]
 
-    cmd = 'grep -w -E -i -r --color "%s" %s > %s.log 2>&1' % ("|".join(black_list), " ".join(check_files), name)
+    cmd = 'grep -w -E -i -r --color "%s" %s > %s 2>&1' % ("|".join(black_list), " ".join(check_files), log_file)
     info("Running cmd: %s ..." % cmd)
     ret = os.system(cmd)
+
     if ret == 0:
-        os.system("cat %s.log" % name)
+        os.system("cat %s" % log_file)
         error("Check 3rd License Failed!")
-    else:
+    elif ret == 256:
         info("Check 3rd License Passed!")
+    elif ret == 512:
+        os.system("cat %s" % log_file)
+        error("Run cmd error: %s" % cmd)
 
 
 if __name__ == '__main__':
