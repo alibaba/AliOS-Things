03e443e2c2e02b17f43ff44b5d4a2040a63db74d
lc122798 <lc122798@alibaba-inc.com>
2019-08-19 16:33:25 +0800

    BugID:22083675:update component version for component release
    Change-Id: I6e034800a57f7ad5e7f11e671a89985d468a84fc

--- drivers/sal/wifi/bk7231/aos.mk
+++ drivers/sal/wifi/bk7231/aos.mk
@@ -1,7 +1,7 @@
 NAME := device_sal_bk7231
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.0
+$(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := sal hal implemenation for bk7231
 $(NAME)_SOURCES += wifi_atcmd_bk7231.c
 GLOBAL_DEFINES += DEV_SAL_BK7231
