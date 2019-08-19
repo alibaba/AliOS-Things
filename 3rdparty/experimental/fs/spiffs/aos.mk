a6f1e263fa81968a46e4a2ce91bdfa18e37bfad1
lc122798 <lc122798@alibaba-inc.com>
2019-08-19 16:33:24 +0800

    BugID:22083675:update component version for component release
    Change-Id: Ie3ab5e2b7bed53b2828d7c0193a8c669c181032a

--- 3rdparty/experimental/fs/spiffs/aos.mk
+++ 3rdparty/experimental/fs/spiffs/aos.mk
@@ -1,7 +1,7 @@
 NAME := spiffs
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.0
+$(NAME)_VERSION    := 1.0.1
 $(NAME)_SUMMARY    := SPIFFS filesystem
 
 $(NAME)_SOURCES += spiffs/spiffs_cache.c \
