9394cbaab4b7b536998f1439dce7ff4052e83ae4
lc122798 <lc122798@alibaba-inc.com>
2019-08-19 16:33:25 +0800

    BugID:22083675:update component version for component release
    Change-Id: I6e034800a57f7ad5e7f11e671a89985d468a84fc

--- drivers/sal/wifi/gt202/aos.mk
+++ drivers/sal/wifi/gt202/aos.mk
@@ -1,7 +1,7 @@
 NAME := device_sal_gt202
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.0
+$(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := sal hal implementation for gt202
 
 $(NAME)_SOURCES += gt202_sal.c
--- drivers/sal/wifi/mk3060/aos.mk
+++ drivers/sal/wifi/mk3060/aos.mk
@@ -1,7 +1,7 @@
 NAME := device_sal_mk3060
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.0
+$(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := sal hal implementation for mk3060
 $(NAME)_SOURCES += wifi_atcmd.c
 GLOBAL_DEFINES += DEV_SAL_MK3060
