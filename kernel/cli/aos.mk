6f7b1beea88f493e8e9cbc82567fe0bef65bef22
jianniu.ljg|lijianguo <jianniu.ljg@alibaba-inc.com>
2019-08-22 19:16:59 +0800

    BugID:22190178:optimize the accuracy of cpu utilization statistics
    Change-Id: I59dbf25f9bd64fa461c3621322edb86eaaf10fac

--- kernel/cli/aos.mk
+++ kernel/cli/aos.mk
@@ -4,7 +4,7 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION    := 1.0.1
 $(NAME)_SUMMARY    := Command Line Interface
 
-$(NAME)_SOURCES := cli.c cli_adapt.c cli_default_command.c cli_dumpsys.c
+$(NAME)_SOURCES := cli.c cli_adapt.c cli_default_command.c cli_dumpsys.c cli_cpuusage.c
 
 ifeq ($(COMPILER),armcc)
 else ifeq ($(COMPILER),rvct)
