b1a671f378cdb76e68242f5321a93b1d1b27a83c
ht166124 <ht166124@alibaba-inc.com>
2019-08-22 21:09:55 +0800

    BugID: 20306260: Update security alicrypto/id2/irot/isst for LK SDK v2.0
    Change-Id: Ia7f1e1ecd95c601f3b38bf822993cdc29a2184b3
    Reviewed-on: http://yuncode.alibaba-inc.com/226477
    Tested-by: <iot-scm@alibaba-inc.com>
    Reviewed-by: 王雪松 cedar.wxs <cedar.wxs@alibaba-inc.com>

--- security/irot/se/aos.mk
+++ security/irot/se/aos.mk
@@ -1,12 +1,12 @@
 NAME := libkm_se
-SE_CHIP_NAME := template
+CHIP_NAME := template
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 2.0.0
 $(NAME)_SUMMARY := key management for se
 
 $(NAME)_INCLUDES  := ./inc
-$(NAME)_INCLUDES  += ./chipset/$(SE_CHIP_NAME)
+$(NAME)_INCLUDES  += ./chipset/$(CHIP_NAME)
 $(NAME)_INCLUDES  += ./../../include/irot
 $(NAME)_INCLUDES  += ./../../include/irot/se
 
@@ -14,7 +14,7 @@ $(NAME)_SOURCES   := src/core/km_to_irot.c
 $(NAME)_SOURCES   += src/core/mtk_se_adapter.c
 $(NAME)_SOURCES   += src/core/std_se_adapter.c
 $(NAME)_SOURCES   += src/log/chip_log.c
-$(NAME)_SOURCES   += chipset/$(SE_CHIP_NAME)/se_driver/se_driver.c
+$(NAME)_SOURCES   += chipset/$(CHIP_NAME)/se_driver/se_driver.c
 
 $(NAME)_COMPONENTS  := ls_osa
 
