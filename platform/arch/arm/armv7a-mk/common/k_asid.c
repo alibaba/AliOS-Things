24e914905aba492413d234ac92e3e8f97a7c8909
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-08-23 16:33:56 +0800

    BugID:16846667: Adjust files for microkernel close source
    Change-Id: I03b5694c14be4c15e1d425dbd385414a21260f20

--- platform/arch/arm/armv7a-mk/common/k_asid.c
+++ platform/arch/arm/armv7a-mk/common/k_asid.c
@@ -7,16 +7,75 @@ static uint32_t g_asid[ASID_WORD] = {0};
 
 int32_t k_asid_reserve(int32_t asid)
 {
+    int     word;
+    int     bit;
+
+    CPSR_ALLOC();
+
+    if (asid >= ARM_MAX_ASIDS) {
+        return -1;
+    }
+
+    RHINO_CRITICAL_ENTER();
+
+    word = asid / 32;
+    bit  = asid % 32;
+    g_asid[word] |= bit << 1;
+
+    RHINO_CRITICAL_EXIT();
+
     return 0;
 }
 
 int32_t k_asid_alloc(void)
 {
-    return 0;
+    int     word;
+    int     bit;
+    int     asid;
+
+    CPSR_ALLOC();
+
+    RHINO_CRITICAL_ENTER();
+    for (word = 0; word < ASID_WORD; word++) {
+        for (bit = 0; bit < 32; bit++) {
+            if (g_asid[word] & (1<<bit) == 0) {
+                asid = word*32+bit;
+                if (asid >= ARM_MAX_ASIDS) {
+                    asid = -1;
+                } else {
+                    g_asid[word] |= (1<<bit);
+                }
+                goto out;
+            }
+        }
+    }
+
+out:
+    RHINO_CRITICAL_EXIT();
+
+    return asid;
 }
 
 int32_t k_asid_dealloc(int32_t asid)
 {
+    int     word;
+    int     bit;
+
+    CPSR_ALLOC();
+
+    if (asid >= ARM_MAX_ASIDS) {
+        return -1;
+    }
+
+    word = asid / 32;
+    bit = asid % 32;
+
+    RHINO_CRITICAL_ENTER();
+
+    g_asid[word] &= ~(bit<<1);
+
+    RHINO_CRITICAL_EXIT();
+
     return 0;
 }
 
--- platform/arch/arm/armv7a-mk/include/k_arch.h
+++ platform/arch/arm/armv7a-mk/include/k_arch.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
 #ifndef K_ARCH_H
@@ -12,6 +12,10 @@
 #define FPU_AVL ((defined(__CC_ARM) && defined(__TARGET_FPU_VFP)) \
                 || (defined(__ICCARM__) && defined(__ARMVFP__))   \
                 || (defined(__GNUC__) && defined(__VFP_FP__) && !defined(__SOFTFP__)))
+
+#define NEON_AVL ((defined(__CC_ARM) && defined(__ARM_NEON__)) \
+                || (defined(__ICCARM__) && defined(__ARM_NEON))   \
+                || (defined(__GNUC__) && defined(__ARM_NEON)))
 
 /* ARM exception IDs */
 #define ARM_EXCEPT_RESET                    0x00u       /* Reset */
@@ -44,8 +48,11 @@
 #define _INS_(x) #x
 #define STR(x) _INS_(x)
 
+
 /* Read from coprocessor 15 register */
 /* Instruction Synchronization Barrier */
+
+#if defined(__CC_ARM)
 #define OS_ISB() \
         __asm volatile ("isb")
 
@@ -58,33 +65,58 @@
         __asm volatile ("dsb")
 
 /* Get coprocessor 15 ( System control functionality ) */
-#define OS_GET_CP15(op1, Rt, CRn, CRm, op2) \
-    do { \
-        __asm {                                            \
-            mrc p15, op1, Rt, CRn, CRm, op2            \
-        }                                                  \
-    } while(0)
-
+#define OS_GET_CP15(opc1, Rt, CRn, CRm, opc2) \
+    __asm ("mrc p15, " # opc1 ", " # Rt ", c" # CRn ", c" # CRm ", " # opc2)
 
 /* Set coprocessor 15 ( System control functionality ) */
-#define OS_SET_CP15(op1, Rt, CRn, CRm, op2) \
-    do {                                                       \
-        __asm {                                            \
-            mcr p15, op1, Rt, CRn, CRm, op2            \
-        }                                                  \
-    } while(0)
+#define OS_SET_CP15(opc1, Rt, CRn, CRm, opc2) \
+    __asm ("mcr p15, " # opc1 ", " # Rt ", c" # CRn ", c" # CRm ", " # opc2)
+
+#elif defined(__GNUC__)
+/* Instruction Synchronization Barrier */
+#define OS_ISB() \
+    __asm volatile ("isb sy":::"memory")
+
+/* Data Memory Barrier */
+#define OS_DMB() \
+    __asm volatile ("dmb sy":::"memory")
+
+/* Data Synchronization Barrier */
+#define OS_DSB() \
+    __asm volatile ("dsb sy":::"memory")
+
+/* Get coprocessor 15 ( System control functionality ) */
+#define OS_GET_CP15(opc1, Rt, CRn, CRm, opc2) \
+    __asm volatile("mrc p15, " # opc1 ", %0, c" # CRn ", c" # CRm ", " # opc2 : "=r" (Rt) : : "memory" )
+
+/* Set coprocessor 15 ( System control functionality ) */
+#define OS_SET_CP15(opc1, Rt, CRn, CRm, opc2) \
+    __asm volatile("mcr p15, " # opc1 ", %0, c" # CRn ", c" # CRm ", " # opc2 : : "r" (Rt) : "memory" )
+
+#else
+#error "Unknown Compiler"
+#endif
+
 
 /********** CP15 c0 register summary, identification registers **********/
+/* CCSIDR, Cache Size ID Registers */
+RHINO_INLINE uint32_t os_get_CCSIDR(void)
+{
+    uint32_t reg;
+    OS_GET_CP15(1, reg, 0, 0, 0);
+    return reg;
+}
+
 /* CSSELR, Cache Size Selection Register */
 RHINO_INLINE uint32_t os_get_CSSELR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(2, reg, c0, c0, 0);
+    OS_GET_CP15(2, reg, 0, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_CSSELR(uint32_t reg)
 {
-    OS_SET_CP15(2, reg, c0, c0, 0);
+    OS_SET_CP15(2, reg, 0, 0, 0);
 }
 
 /********** CP15 c1 register summary, system control registers **********/
@@ -92,36 +124,36 @@ RHINO_INLINE void os_set_CSSELR(uint32_t reg)
 RHINO_INLINE uint32_t os_get_SCTLR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c1, c0, 0);
+    OS_GET_CP15(0, reg, 1, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_SCTLR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c1, c0, 0);
+    OS_SET_CP15(0, reg, 1, 0, 0);
 }
 
 /* ACTLR, Auxiliary Control Register, IMPLEMENTATION DEFINED */
 RHINO_INLINE uint32_t os_get_ACTLR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c1, c0, 1);
+    OS_GET_CP15(0, reg, 1, 0, 1);
     return reg;
 }
 RHINO_INLINE void os_set_ACTLR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c1, c0, 1);
+    OS_SET_CP15(0, reg, 1, 0, 1);
 }
 
 /* CPACR, Coprocessor Access Control Register */
 RHINO_INLINE uint32_t os_get_CPACR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c1, c0, 2);
+    OS_GET_CP15(0, reg, 1, 0, 2);
     return reg;
 }
 RHINO_INLINE void os_set_CPACR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c1, c0, 2);
+    OS_SET_CP15(0, reg, 1, 0, 2);
 }
 
 /********** CP15 c2 and c3 register summary, Memory protection and control registers **********/
@@ -129,36 +161,36 @@ RHINO_INLINE void os_set_CPACR(uint32_t reg)
 RHINO_INLINE uint32_t os_get_TTBR0(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c2, c0, 0);
+    OS_GET_CP15(0, reg, 2, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_TTBR0(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c2, c0, 0);
+    OS_SET_CP15(0, reg, 2, 0, 0);
 }
 
 /* TTBCR, Translation Table Base Control Register */
-RHINO_INLINE void *os_get_TTBCR(void)
+RHINO_INLINE uint32_t os_get_TTBCR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c2, c0, 2);
-    return (void*)reg;
+    OS_GET_CP15(0, reg, 2, 0, 2);
+    return reg;
 }
 RHINO_INLINE void os_set_TTBCR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c2, c0, 2);
+    OS_SET_CP15(0, reg, 2, 0, 2);
 }
 
 /* DACR, Domain Access Control Register */
 RHINO_INLINE uint32_t os_get_DACR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c3, c0, 0);
+    OS_GET_CP15(0, reg, 3, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_DACR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c3, c0, 0);
+    OS_SET_CP15(0, reg, 3, 0, 0);
 }
 
 /********** CP15 c5 and c6 register summary, Memory system fault registers **********/
@@ -166,122 +198,136 @@ RHINO_INLINE void os_set_DACR(uint32_t reg)
 RHINO_INLINE uint32_t os_get_DFSR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c5, c0, 0);
+    OS_GET_CP15(0, reg, 5, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_DFSR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c5, c0, 0);
+    OS_SET_CP15(0, reg, 5, 0, 0);
 }
 
 /* IFSR, Instruction Fault Status Register */
 RHINO_INLINE uint32_t os_get_IFSR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c5, c0, 1);
+    OS_GET_CP15(0, reg, 5, 0, 1);
     return reg;
 }
 RHINO_INLINE void os_set_IFSR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c5, c0, 1);
+    OS_SET_CP15(0, reg, 5, 0, 1);
 }
 
 /* DFAR, Data Fault Address Register */
 RHINO_INLINE uint32_t os_get_DFAR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c6, c0, 0);
+    OS_GET_CP15(0, reg, 6, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_DFAR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c6, c0, 0);
+    OS_SET_CP15(0, reg, 6, 0, 0);
 }
 
 /* IFAR, Instruction Fault Address Register */
 RHINO_INLINE uint32_t os_get_IFAR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c6, c0, 2);
+    OS_GET_CP15(0, reg, 6, 0, 2);
     return reg;
 }
 RHINO_INLINE void os_set_IFAR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c6, c0, 2);
+    OS_SET_CP15(0, reg, 6, 0, 2);
 }
 
+/********** CP15 c7 register summary, Cache maintenance, address translation, and other functions **********/
+/* PAR, Physical Address Register */
 RHINO_INLINE uint32_t os_get_PAR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c7, c4, 0);
+    OS_GET_CP15(0, reg, 7, 4, 0);
     return reg;
 }
-/********** CP15 c7 register summary, Cache maintenance, address translation, and other functions **********/
+
 /* ICIALLU, Invalidate all instruction caches to PoU */
 RHINO_INLINE void os_set_ICIALLU(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c5, 0);
+    OS_SET_CP15(0, reg, 7, 5, 0);
+}
+
+/* ICIMVAU, Invalidate instruction caches by MVA to PoU */
+RHINO_INLINE void os_set_ICIMVAU(uint32_t reg)
+{
+    OS_SET_CP15(0, reg, 7, 5, 1);
 }
 
 /* BPIALL, Invalidate all branch predictors */
 RHINO_INLINE void os_set_BPIALL(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c5, 6);
+    OS_SET_CP15(0, reg, 7, 5, 6);
+}
+
+/* BPIMVA, Invalidate MVA from branch predictors */
+RHINO_INLINE void os_set_BPIMVA(uint32_t reg)
+{
+    OS_SET_CP15(0, reg, 7, 5, 7);
 }
 
 /* DCIMVAC, Invalidate data cache line by MVA to PoC */
 RHINO_INLINE void os_set_DCIMVAC(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c6, 1);
+    OS_SET_CP15(0, reg, 7, 6, 1);
 }
 
 /* DCISW, Invalidate data cache line by set/way */
 RHINO_INLINE void os_set_DCISW(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c6, 2);
-}
-
-/* DCCMVAC, Clean data cache line by MVA to PoC */
-RHINO_INLINE void os_set_DCCMVAC(uint32_t reg)
-{
-    OS_SET_CP15(0, reg, c7, c10, 1);
-}
-
-/* DCCSW, Clean data cache line by set/way */
-RHINO_INLINE void os_set_DCCSW(uint32_t reg)
-{
-    OS_SET_CP15(0, reg, c7, c10, 2);
+    OS_SET_CP15(0, reg, 7, 6, 2);
 }
 
 /* ATS1CPR, PL1 read translation */
 RHINO_INLINE void os_set_ATS1CPR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c8, 0);
+    OS_SET_CP15(0, reg, 7, 8, 0);
 }
 
 /* ATS1CPW, PL1 write translation */
 RHINO_INLINE void os_set_ATS1CPW(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c8, 1);
+    OS_SET_CP15(0, reg, 7, 8, 1);
+}
+
+/* DCCMVAC, Clean data cache line by MVA to PoC */
+RHINO_INLINE void os_set_DCCMVAC(uint32_t reg)
+{
+    OS_SET_CP15(0, reg, 7, 10, 1);
+}
+
+/* DCCSW, Clean data cache line by set/way */
+RHINO_INLINE void os_set_DCCSW(uint32_t reg)
+{
+    OS_SET_CP15(0, reg, 7, 10, 2);
 }
 
 /* DCCIMVAC, Clean and invalidate data cache line by MVA to PoC */
 RHINO_INLINE void os_set_DCCIMVAC(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c14, 1);
+    OS_SET_CP15(0, reg, 7, 14, 1);
 }
 
 /* DCCISW, Clean and invalidate data cache line by set/way */
 RHINO_INLINE void os_set_DCCISW(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c7, c14, 2);
+    OS_SET_CP15(0, reg, 7, 14, 2);
 }
 
 /********** CP15 c8 register summary, TLB maintenance operations **********/
 /* TLBIALL, invalidate unified TLB */
 RHINO_INLINE void os_set_TLBIALL(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c8, c7, 0);
+    OS_SET_CP15(0, reg, 8, 7, 0);
 }
 
 /********** CP15 c12 register summary, Security Extensions registers **********/
@@ -289,32 +335,36 @@ RHINO_INLINE void os_set_TLBIALL(uint32_t reg)
 RHINO_INLINE uint32_t os_get_VBAR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c12, c0, 0);
+    OS_GET_CP15(0, reg, 12, 0, 0);
     return reg;
 }
 RHINO_INLINE void os_set_VBAR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c12, c0, 0);
+    OS_SET_CP15(0, reg, 12, 0, 0);
 }
 
-/********** CP15 c13 register summary, Process, context and thread ID registers **********//* CONTEXTIDR, Context ID Register */
+/********** CP15 c13 register summary, Process, context and thread ID registers **********/
+/* CONTEXTIDR, Context ID Register */
 RHINO_INLINE uint32_t os_get_CONTEXTIDR(void)
 {
     uint32_t reg;
-    OS_GET_CP15(0, reg, c13, c0, 1);
+    OS_GET_CP15(0, reg, 13, 0, 1);
     return reg;
 }
 RHINO_INLINE void os_set_CONTEXTIDR(uint32_t reg)
 {
-    OS_SET_CP15(0, reg, c13, c0, 1);
+    OS_SET_CP15(0, reg, 13, 0, 1);
 }
 
 typedef struct {
-#ifdef FPU_AVL
+#if (FPU_AVL)
     long FPEXC;
-    /* The Cortex-A5 FPU is a VFPv4-D16 implementation of the ARMv7 floating-point architecture */
-    long FPU[32];
     long FPSCR;
+#if (NEON_AVL)
+    long FPU[64];
+#else
+    long FPU[32];
+#endif
 #endif
     long CPSR;
     long R0;
