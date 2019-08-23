5c3226002eefa4be8da48d6538c591103309ca4a
yangwencheng <wencheng.ywc@alibaba-inc.com>
2019-08-23 15:01:18 +0800

    BugID:16846667: open source syscall
    Change-Id: I713e93b5bfebfab6a5d74cbffce13c98b1ebb1f2

---
+++ kernel/mk/syscall/ksyscall/dyn_syscall.c
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#include "k_config.h"
+
+#if (RHINO_CONFIG_DYN_SYSCALL_TBL > 0)
+
+#include <stddef.h>
+#include "k_config.h"
+#include "syscall_no.h"
+
+extern void *syscall_tbl[];
+
+int register_syscall(int syscall_no, void *func)
+{
+    /* invalid syscall number */
+    if (syscall_no < SYS_USER_DEFINE_BASE
+        || syscall_no > SYS_USER_DEFINE_END) {
+        return -1;
+    }
+
+    /* syscall is already registered */
+    if (NULL != syscall_tbl[syscall_no]) {
+        return -2;
+    }
+
+    syscall_tbl[syscall_no] = func;
+
+    return 0;
+}
+
+int unregister_syscall(int syscall_no)
+{
+    /* invalid syscall number */
+    if (syscall_no < SYS_USER_DEFINE_BASE
+        || syscall_no > SYS_USER_DEFINE_END) {
+        return -1;
+    }
+
+    syscall_tbl[syscall_no] = NULL;
+
+    return 0;
+}
+
+#endif /* RHINO_CONFIG_DYN_SYSCALL_TBL */
+
