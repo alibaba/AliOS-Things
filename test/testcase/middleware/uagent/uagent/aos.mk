bd39389c55662b6ba1a2142d9f938f0799d40868
gonghui.hui <gonghui.hui@alibaba-inc.com>
2019-08-27 15:10:01 +0800

    BugID:22225023: Sync between aos.mk and Config.in.
    Change-Id: Ifac0579e68743f2bf5221e226623a860ad85eb29

--- test/testcase/middleware/uagent/uagent/aos.mk
+++ test/testcase/middleware/uagent/uagent/aos.mk
@@ -3,7 +3,7 @@ NAME := uagent_test
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION    := 0.0.1
 $(NAME)_SUMMARY    := testcase for uagent
-$(NAME)_COMPONENTS += uagent cjson linkkit_sdk_c
+$(NAME)_COMPONENTS += uagent netmgr cli cjson libiot_mqtt
 
 $(NAME)_SOURCES += uagent_test.c
 
