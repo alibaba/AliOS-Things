60995761b31eb675d0c3e4289092ebd717ac8d78
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2019-08-27 23:28:00 +0800

    bugid:20235446:add mcu type for qingke mk3072
    Change-Id: I007b0e0f9124befe578fac94c15b4283046c0869

---
+++ platform/mcu/asr5501mk/aos.mk
@@ -0,0 +1,84 @@
+NAME := mcu_asr5501mk
+
+HOST_OPENOCD := asr5501mk
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.0
+$(NAME)_SUMMARY    := driver & sdk for platform/mcu asr5501mk
+
+LWIP := 1
+
+ifeq ($(LWIP),1)
+$(NAME)_COMPONENTS += lwip
+no_with_lwip       := 0
+GLOBAL_DEFINES     += WITH_LWIP
+endif
+
+$(NAME)_COMPONENTS += arch_armv7m
+
+$(NAME)_CFLAGS += -DLEGA_CM4 -DALIOS_SUPPORT -DWIFI_DEVICE -D_SPI_FLASH_ENABLE_ -DPS_CLOSE_APLL -DDCDC_PFMMODE_CLOSE -D_SPI_FLASH_120MHz_ -DCFG_MIMO_UF
+$(NAME)_CFLAGS += -DCFG_BATX=1 -DCFG_BARX=1  -DCFG_REORD_BUF=4  -DCFG_SPC=4  -DCFG_TXDESC0=4 -DCFG_TXDESC1=4 -DCFG_TXDESC2=4 -DCFG_TXDESC3=4 -DCFG_TXDESC4=4 -DCFG_CMON -DCFG_MDM_VER_V21 -DCFG_SOFTAP_SUPPORT -DCFG_SNIFFER_SUPPORT -DCFG_DBG=2 -D__FPU_PRESENT=1 -DDX_CC_TEE -DHASH_SHA_512_SUPPORTED -DCC_HW_VERSION=0xF0 -DDLLI_MAX_BUFF_SIZE=0x10000 -DSSI_CONFIG_TRNG_MODE=0
+#$(NAME)_CFLAGS += -DXO_FREQ_BIAS_CONFIG
+
+#default a0v2 config
+ifeq ($(buildsoc),a0v1)
+$(NAME)_CFLAGS += -DLEGA_A0V1
+else
+$(NAME)_CFLAGS += -DLEGA_A0V2
+endif
+
+$(NAME)_CFLAGS += -Wall -Wno-error -Wno-unused-variable -Wno-unused-parameter -Wno-implicit-function-declaration
+$(NAME)_CFLAGS += -Wno-type-limits -Wno-sign-compare -Wno-pointer-sign -Wno-uninitialized
+$(NAME)_CFLAGS += -Wno-return-type -Wno-unused-function -Wno-unused-but-set-variable
+$(NAME)_CFLAGS += -Wno-unused-value -Wno-strict-aliasing
+
+BINS ?=
+
+GLOBAL_CFLAGS   += -mcpu=cortex-m4 -mthumb -mthumb-interwork -mfloat-abi=softfp -mfpu=fpv4-sp-d16 -w -std=gnu99 -ffunction-sections -fdata-sections
+GLOBAL_ASMFLAGS += -mcpu=cortex-m4 -mthumb -mfloat-abi=softfp -mfpu=fpv4-sp-d16
+
+GLOBAL_DEFINES += CONFIG_AOS_KV_MULTIPTN_MODE
+GLOBAL_DEFINES += CONFIG_AOS_KV_PTN=6
+GLOBAL_DEFINES += CONFIG_AOS_KV_SECOND_PTN=7
+GLOBAL_DEFINES += CONFIG_AOS_KV_PTN_SIZE=4096
+GLOBAL_DEFINES += KV_CONFIG_TOTAL_SIZE=8192
+GLOBAL_DEFINES += ALI_ALGO_AES_ROM_TABLES
+
+GLOBAL_LDFLAGS += -mcpu=cortex-m4 -mthumb -Wl,-gc-sections
+
+GLOBAL_INCLUDES += drivers/driver/inc \
+            drivers/platform/CMSIS/Include \
+            drivers/common \
+            drivers/platform/misc \
+            drivers/platform/system/include \
+            hal/inc \
+
+#soc
+$(NAME)_SOURCES += drivers/platform/system/systick_delay.c \
+            drivers/platform/system/system_cm4.c \
+            drivers/platform/misc/misc.c \
+            drivers/common/lega_mac_addr.c
+
+#hal
+$(NAME)_SOURCES += hal/src/hw.c \
+            hal/src/gpio.c \
+            hal/src/uart.c \
+            hal/src/flash.c \
+            hal/src/wdg.c   \
+            hal/src/wifi_port.c \
+            hal/src/ota.c \
+            hal/src/pwrmgmt_hal/board_cpu_pwr.c \
+            hal/src/pwrmgmt_hal/board_cpu_pwr_rtc.c \
+            hal/src/pwrmgmt_hal/board_cpu_pwr_systick.c
+
+#driver
+$(NAME)_SOURCES += drivers/driver/src/lega_flash_alg.c \
+            drivers/driver/src/lega_efuse.c \
+            drivers/driver/src/lega_board.c \
+            drivers/driver/src/lega_flash.c \
+            drivers/driver/src/lega_gpio.c \
+            drivers/driver/src/lega_uart.c \
+            drivers/driver/src/lega_wdg.c \
+
+$(NAME)_PREBUILT_LIBRARY := drivers/libs/$(HOST_ARCH)/libasr_wifi.a
+
---
+++ platform/mcu/asr5501mk/hal/src/pwrmgmt_hal/board_cpu_pwr_rtc.c
@@ -0,0 +1,72 @@
+/*
+ * Copyright (C) 2018 Alibaba Group Holding Limited
+ */
+
+/*
+ * This file supplied RTC one-shot start/stop services for CPU tickless
+ * module, verifyied on STM32L496-DISCOVERY with C3/C4 mode.
+ * C3: stop mode.
+ * C4: standby mode.
+ */
+#include <k_api.h>
+
+#if (AOS_COMP_PWRMGMT > 0)
+#include <stdint.h>
+#include <stdbool.h>
+
+#include <cpu_tickless.h>
+
+#include <pmu.h>
+
+#define RTC_FREQ      (32768)
+
+static pwr_status_t rtc_init(void);
+static uint32_t     rtc_one_shot_max_seconds(void);
+static pwr_status_t rtc_one_shot_start(uint64_t planUs);
+static pwr_status_t rtc_one_shot_stop(uint64_t *pPassedUs);
+
+one_shot_timer_t rtc_one_shot = {
+    rtc_init,
+    rtc_one_shot_max_seconds,
+    rtc_one_shot_start,
+    rtc_one_shot_stop,
+};
+
+static pwr_status_t rtc_init(void)
+{
+    return PWR_OK;
+}
+
+static pwr_status_t rtc_one_shot_start(uint64_t planUs)
+{
+    uint32_t status = 0;
+    uint32_t cc_counter = planUs * RTC_FREQ / 1000000;
+
+    if ((planUs / 1000) < 4) {
+        return PWR_ERR;
+    }
+
+    status = lega_drv_rtc_cc_set(cc_counter, PMU_STATE_MODEMSLEEP);
+    if (status == 0) {
+        lega_drv_rtc_enable();
+        return PWR_OK;
+    } else {
+        return PWR_ERR;
+    }
+}
+
+static pwr_status_t rtc_one_shot_stop(uint64_t *pPassedUs)
+{
+    uint32_t counter = 0;
+    counter = lega_drv_rtc_counter_get();
+    *pPassedUs = (uint64_t)counter * 1000000 / RTC_FREQ;
+    lega_drv_goto_active();
+    return PWR_OK;
+}
+
+static uint32_t rtc_one_shot_max_seconds(void)
+{
+    return (lega_drv_rtc_max_ticks_get() / RTC_FREQ);
+}
+
+#endif /* AOS_COMP_PWRMGMT */
