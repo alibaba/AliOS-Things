08719a7e718488f94eaf822219984be8c33b2a8f
leizhe.wzl <leizhe.wzl@alibaba-inc.com>
2019-08-28 17:35:14 +0800

    bugid:20235446:add linux ota bin generate tools
    Change-Id: Ic1f8cc239639a6c163f11218db425cedc9e9ae3c

--- platform/mcu/asr5501mk/gen_ota_bin.mk
+++ platform/mcu/asr5501mk/gen_ota_bin.mk
@@ -1,5 +1,5 @@
 EXTRA_POST_BUILD_TARGETS += gen_standard_images
 CUSTOM_OTA := 1
 gen_standard_images:
-	$(PYTHON) platform/mcu/asr5501mk/ota.py $(BIN_OUTPUT_FILE)
+	$(PYTHON) platform/mcu/asr5501mk/ota.py $(BIN_OUTPUT_FILE) $(HOST_OS)
 	$(PYTHON) $(SCRIPTS_PATH)/ota_gen_md5_bin.py $(OTA_BIN_OUTPUT_FILE) -m $(IMAGE_MAGIC)
