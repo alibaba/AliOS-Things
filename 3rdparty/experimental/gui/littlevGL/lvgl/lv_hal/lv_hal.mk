3198c8f6c47ef248485f6513b56253cbf4a03d62
崇龄 <yj144830@alibaba-inc.com>
2019-08-29 16:11:37 +0800

    BugID:22305500:update littlevGL to version 6.0
    Change-Id: Ia3b127b155635bcd2ce43716e7c59503ef4d4134

--- 3rdparty/experimental/gui/littlevGL/lvgl/lv_hal/lv_hal.mk
+++
@@ -1,6 +0,0 @@
-$(NAME)_SOURCES     += ./lvgl/lv_hal/lv_hal_disp.c
-$(NAME)_SOURCES     += ./lvgl/lv_hal/lv_hal_indev.c
-$(NAME)_SOURCES     += ./lvgl/lv_hal/lv_hal_tick.c
-
-$(NAME)_INCLUDES    += .
-$(NAME)_INCLUDES    += /lvgl
---
+++ 3rdparty/experimental/gui/littlevGL/src/lv_hal/lv_hal.mk
@@ -0,0 +1,6 @@
+$(NAME)_SOURCES     += ./src/lv_hal/lv_hal_disp.c
+$(NAME)_SOURCES     += ./src/lv_hal/lv_hal_indev.c
+$(NAME)_SOURCES     += ./src/lv_hal/lv_hal_tick.c
+
+$(NAME)_INCLUDES    += .
+$(NAME)_INCLUDES    += /src
