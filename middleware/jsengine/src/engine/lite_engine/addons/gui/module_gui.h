cd88d11ad5034bb0bbc08b4b05b952bf58a8e970
chenan.xxw <chenan.xxw@alibaba-inc.com>
2019-08-29 13:51:17 +0800

    BugID: 21044381: Add support for littlevGL. fix http module bug
    Change-Id: Id2e90d4f7e7b7c63da9bc5b33f57722be294f26c

---
+++ middleware/jsengine/src/engine/lite_engine/addons/gui/module_gui.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifndef MODULE_GUI_H
+#define MODULE_GUI_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * register the add-on of GUI module
+ *
+ * @return  void
+ */
+
+void module_gui_register(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* MODULE_GUI_H */
