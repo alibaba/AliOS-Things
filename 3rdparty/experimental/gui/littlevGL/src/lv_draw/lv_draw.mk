d013520cd09afdbd5c1d56625814dd0aac06fb70
崇龄 <yj144830@alibaba-inc.com>
2019-08-29 16:11:37 +0800

    BugID:22305500:update littlevGL to version 6.0
    Change-Id: Ia3b127b155635bcd2ce43716e7c59503ef4d4134

---
+++ 3rdparty/experimental/gui/littlevGL/src/lv_draw/lv_draw.mk
@@ -0,0 +1,13 @@
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_arc.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_basic.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_img.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_label.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_line.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_rect.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_draw_triangle.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_img_cache.c
+$(NAME)_SOURCES     += ./src/lv_draw/lv_img_decoder.c
+
+$(NAME)_INCLUDES    += .
+$(NAME)_INCLUDES    += /src
