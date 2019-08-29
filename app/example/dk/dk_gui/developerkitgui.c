ff111065b2dbf86c2d69386ab25aeef1cbb71de3
崇龄 <yj144830@alibaba-inc.com>
2019-08-29 15:55:37 +0800

    BugID:22305500:update littlevGL to version 6.0
    Change-Id: Ibff3c9d6c00b61bac451b006e13e6eb11de266e7

--- app/example/dk/dk_gui/developerkitgui.c
+++ app/example/dk/dk_gui/developerkitgui.c
@@ -3,7 +3,7 @@
  */
 
 #include <k_api.h>
-#include "lvgl/lvgl.h"
+#include "lvgl.h"
 #include "aos/kernel.h"
 #include "soc_init.h"
 #include "sensor_display.h"
@@ -13,14 +13,13 @@
 
 static void gui_init(void);
 static void littlevgl_refresh_task(void *arg);
-static void lvgl_drv_register(void);
-static void lvgl_drv_register(void);
-static void my_disp_flush(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const lv_color_t *color_p);
-static void my_disp_fill(int32_t x1, int32_t y1, int32_t x2, int32_t y2, lv_color_t color);
-static void my_disp_map(int32_t x1, int32_t y1, int32_t x2, int32_t y2, const lv_color_t *color_p);
+static void lvgl_disp_drv_init(void);
+static void my_disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p);
+
+extern int freetype_init(void);
 
 /* display driver */
-lv_disp_drv_t dis_drv;
+lv_disp_drv_t disp_drv;
 
 int application_start(int argc, char *argv[])
 {
@@ -36,16 +35,20 @@ void gui_init(void)
     /* init littlevGL */
     lv_init();
 
+#if FREETYPE_DISPLAY == 1
+    freetype_init();
+#endif
+
     /* init LCD */
     st7789_init();
 
     /* register driver for littlevGL */
-    lvgl_drv_register();
+    lvgl_disp_drv_init();
 
     /* create a task to refresh the LCD */
     aos_task_new("littlevgl_refresh_task", littlevgl_refresh_task, NULL, 2048);
 
-    /* int app */
+    /* start app */
 #if FREETYPE_DISPLAY == 0
     sensor_display();
 #else
@@ -56,6 +59,9 @@ void gui_init(void)
 static void littlevgl_refresh_task(void *arg)
 {
     while (1) {
+
+        printf("littlevgl_refresh_task !\n");
+
         /* this function is used to refresh the LCD */
         lv_task_handler();
 
@@ -63,52 +69,44 @@ static void littlevgl_refresh_task(void *arg)
     }
 }
 
-
-void lvgl_drv_register(void)
+void lvgl_disp_drv_init(void)
 {
-    lv_disp_drv_init(&dis_drv);
+    static lv_disp_buf_t disp_buf_2;
+    static lv_color_t buf2_1[LV_HOR_RES_MAX * 10];                        /*A buffer for 10 rows*/
+    static lv_color_t buf2_2[LV_HOR_RES_MAX * 10];                        /*An other buffer for 10 rows*/
+    lv_disp_buf_init(&disp_buf_2, buf2_1, buf2_2, LV_HOR_RES_MAX * 10);   /*Initialize the display buffer*/
 
-    dis_drv.disp_flush = my_disp_flush;
-    dis_drv.disp_fill  = my_disp_fill;
-    dis_drv.disp_map   = my_disp_map;
-    lv_disp_drv_register(&dis_drv);
+    /*-----------------------------------
+     * Register the display in LittlevGL
+     *----------------------------------*/
+
+    lv_disp_drv_init(&disp_drv);                    /*Basic initialization*/
+
+    /*Set up the functions to access to your display*/
+
+    /*Set the resolution of the display*/
+    disp_drv.hor_res = 240;
+    disp_drv.ver_res = 240;
+
+    /*Used to copy the buffer's content to the display*/
+    disp_drv.flush_cb = my_disp_flush;
+
+    /*Set a display buffer*/
+    disp_drv.buffer = &disp_buf_2;
+
+    /*Finally register the driver*/
+    lv_disp_drv_register(&disp_drv);
 }
 
-void my_disp_flush(int32_t x1, int32_t y1, int32_t x2, int32_t y2,
-                   const lv_color_t *color_p)
+void my_disp_flush(lv_disp_drv_t * disp_drv, const lv_area_t * area, lv_color_t * color_p)
 {
     int32_t x = 0;
     int32_t y = 0;
 
-    for (y = y1; y <= y2; y++) {
-        ST7789H2_WriteLine(x1, y, (uint8_t *)color_p, (x2 - x1 + 1));
-        color_p += (x2 - x1 + 1);
+    for (y = area->y1; y <= area->y2; y++) {
+        ST7789H2_WriteLine(area->x1, y, (uint8_t *)color_p, (area->x2 - area->x1 + 1));
+        color_p += (area->x2 - area->x1 + 1);
     }
 
-    lv_flush_ready();
-}
-
-void my_disp_fill(int32_t x1, int32_t y1, int32_t x2, int32_t y2,
-                  lv_color_t color)
-{
-    int32_t i = 0;
-    int32_t j = 0;
-
-    for (i = x1; i <= x2; i++) {
-        for (j = y1; j <= y2; j++) {
-            ST7789H2_WritePixel(i, j, color.full);
-        }
-    }
-}
-
-void my_disp_map(int32_t x1, int32_t y1, int32_t x2, int32_t y2,
-                 const lv_color_t *color_p)
-{
-    int32_t x = 0;
-    int32_t y = 0;
-
-    for (y = y1; y <= y2; y++) {
-        ST7789H2_WriteLine(x1, y, (int16_t *)color_p, (x2 - x1 + 1));
-        color_p += (x2 - x1 + 1);
-    }
+    lv_disp_flush_ready(&disp_drv);
 }
