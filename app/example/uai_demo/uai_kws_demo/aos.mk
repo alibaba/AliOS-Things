2fbf9cee5507c8455962c0e816786226ef1cae65
william.wgj <william.wgj@alibaba-inc.com>
2019-08-29 12:44:40 +0800

    BugID:22299846: uAI code merge into master
    Change-Id: I9412a24ad3baade5e5507e1496d7608d9aa9b39e

---
+++ app/example/uai_demo/uai_kws_demo/aos.mk
@@ -0,0 +1,18 @@
+NAME := uai_kws_demo
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := uAI KWS DEMO
+$(NAME)_SOURCES := uai_kws_demo.c \
+					dnn_weights.c \
+					wav_data.c    \
+					mfcc.c
+
+$(NAME)_COMPONENTS += osal_aos cli uai 
+
+GLOBAL_DEFINES += AOS_NO_WIFI
+
+GLOBAL_INCLUDES += ./
+
+cmsis_nn_enable ?= 1
+dnn_enable      ?= 1
