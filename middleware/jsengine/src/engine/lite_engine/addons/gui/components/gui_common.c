980b081657b36e139240fcbf5666235a75ed5dc6
chenan.xxw <chenan.xxw@alibaba-inc.com>
2019-08-29 13:51:17 +0800

    BugID: 21044381: Add support for littlevGL. fix http module bug
    Change-Id: Id2e90d4f7e7b7c63da9bc5b33f57722be294f26c

---
+++ middleware/jsengine/src/engine/lite_engine/addons/gui/components/gui_common.c
@@ -0,0 +1,15 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "be_module.h"
+#include "jse_common.h"
+
+#include <k_api.h>
+#include "lvgl/lvgl.h"
+
+be_jse_symbol_t *gui_common_handle_cb(be_jse_vm_ctx_t *execInfo,
+                                      be_jse_symbol_t *var, const char *name)
+{
+    return NULL;
+}
