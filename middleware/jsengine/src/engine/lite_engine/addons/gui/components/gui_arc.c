1d70db636d5a158664b526f9918a213c44c49f50
chenan.xxw <chenan.xxw@alibaba-inc.com>
2019-08-29 13:51:17 +0800

    BugID: 21044381: Add support for littlevGL. fix http module bug
    Change-Id: Id2e90d4f7e7b7c63da9bc5b33f57722be294f26c

---
+++ middleware/jsengine/src/engine/lite_engine/addons/gui/components/gui_arc.c
@@ -0,0 +1,24 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "be_module.h"
+#include "jse_common.h"
+
+#include <k_api.h>
+#include "lvgl/lvgl.h"
+
+static be_jse_symbol_t *gui_scr_act(void)
+{
+    be_jse_handle_function(0, NULL, NULL, NULL, NULL);
+    return new_int_symbol((int)lv_scr_act());
+}
+
+be_jse_symbol_t *gui_arc_handle_cb(be_jse_vm_ctx_t *execInfo,
+                                   be_jse_symbol_t *var, const char *name)
+{
+    if (0 == strcmp(name, "scrAct")) {
+        return gui_scr_act();
+    }
+    return NULL;
+}
