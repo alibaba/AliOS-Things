f8382fd9cfab69075d7b6c1788fd35eccf05f08a
william.wgj <william.wgj@alibaba-inc.com>
2019-08-30 19:12:28 +0800

    BugID:22299846: uAI code merge into master, fix read file bug
    Change-Id: I017ecd3c6599deba1b3966584c02d55c124cd68d
    Reviewed-on: http://yuncode.alibaba-inc.com/228092
    Tested-by: 范文宗 wenzong.fwz <wenzong.fwz@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

---
+++ middleware/uai/function/include/uai_function.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifndef UAI_FUNCTION_H
+#define UAI_FUNCTION_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+void uai_relu(int8_t * data, uint16_t size, uint16_t out_width);
+void uai_relu6(int8_t * data, uint16_t size, uint16_t out_width);
+void uai_sigmod(int8_t * data, uint16_t size, uint16_t out_width);
+void uai_tanh(int8_t * data, uint16_t size, uint16_t out_width);
+int
+uai_fully_connected(const int8_t * pV,
+                    const int8_t * pM,
+                    const uint16_t dim_vec,
+                    const uint16_t num_of_rows,
+                    const uint16_t bias_shift,
+                    const uint16_t out_shift, const int8_t * bias, int8_t * pOut, int16_t * vec_buffer);
+int
+uai_fully_connected_opt(const int8_t * pV,
+                        const int8_t * pM,
+                        const uint16_t dim_vec,
+                        const uint16_t num_of_rows,
+                        const uint16_t bias_shift,
+                        const uint16_t out_shift, const int8_t * bias, int8_t * pOut, int16_t * vec_buffer);
+                        
+void uai_softmax(const int8_t *in_vec, const uint16_t dim_vec, int8_t *out_vec);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  /* end of UAI_FUNCTION_H */
