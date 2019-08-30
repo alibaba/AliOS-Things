63d4c6ef96ae300b2ec7abf5346a179a9e7ce42f
chenan.xxw <chenan.xxw@alibaba-inc.com>
2019-08-30 16:31:05 +0800

    BugID: 21044381: add support for littlevGL 6.0, add support for LCD module
    Change-Id: Ia2f2ab8053ed36018507eb461ec9546bb099149c

--- middleware/jsengine/src/engine/lite_engine/addons/gui/components/gui_common.c
+++ middleware/jsengine/src/engine/lite_engine/addons/gui/components/gui_common.c
@@ -6,7 +6,7 @@
 #include "jse_common.h"
 
 #include <k_api.h>
-#include "lvgl/lvgl.h"
+#include "lvgl.h"
 
 be_jse_symbol_t *gui_common_handle_cb(be_jse_vm_ctx_t *execInfo,
                                       be_jse_symbol_t *var, const char *name)
