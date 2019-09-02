96a97a5cfa751c07593a728f173000ccefe47bce
william.wgj <william.wgj@alibaba-inc.com>
2019-09-02 19:52:32 +0800

    BugID:22360499: uAI API uai_fully_connected mismatch fix
    Change-Id: Iff536b5643b0a05f0158a0239b28a92cdc48fa09
    Reviewed-on: http://yuncode.alibaba-inc.com/228423
    Tested-by: 范文宗 wenzong.fwz <wenzong.fwz@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

--- middleware/uai/function/include/uai_function.h
+++ middleware/uai/function/include/uai_function.h
@@ -9,25 +9,31 @@
 extern "C" {
 #endif
 
-void uai_relu(int8_t * data, uint16_t size, uint16_t out_width);
-void uai_relu6(int8_t * data, uint16_t size, uint16_t out_width);
-void uai_sigmod(int8_t * data, uint16_t size, uint16_t out_width);
-void uai_tanh(int8_t * data, uint16_t size, uint16_t out_width);
-int
-uai_fully_connected(const int8_t * pV,
-                    const int8_t * pM,
-                    const uint16_t dim_vec,
-                    const uint16_t num_of_rows,
-                    const uint16_t bias_shift,
-                    const uint16_t out_shift, const int8_t * bias, int8_t * pOut, int16_t * vec_buffer);
-int
-uai_fully_connected_opt(const int8_t * pV,
-                        const int8_t * pM,
-                        const uint16_t dim_vec,
-                        const uint16_t num_of_rows,
+void uai_relu(int8_t *data, uint16_t size, uint16_t out_width);
+void uai_relu6(int8_t *data, uint16_t size, uint16_t out_width);
+void uai_sigmod(int8_t *data, uint16_t size, uint16_t out_width);
+void uai_tanh(int8_t *data, uint16_t size, uint16_t out_width);
+
+int uai_fully_connected(const int8_t  *input_buffer,
+                        const int8_t  *weights,
+                        const uint16_t input_size,
+                        const uint16_t weight_rows,
                         const uint16_t bias_shift,
-                        const uint16_t out_shift, const int8_t * bias, int8_t * pOut, int16_t * vec_buffer);
-                        
+                        const uint16_t out_shift,
+                        const int8_t  *bias,
+                        int8_t        *out_buffer,
+                        int16_t       *vec_buffer);
+
+int uai_fully_connected_opt(const int8_t  *input_buffer,
+                            const int8_t  *weights,
+                            const uint16_t input_size,
+                            const uint16_t weight_rows,
+                            const uint16_t bias_shift,
+                            const uint16_t out_shift,
+                            const int8_t   *bias,
+                            int8_t         *out_buffer,
+                            int16_t        *vec_buffer);
+
 void uai_softmax(const int8_t *in_vec, const uint16_t dim_vec, int8_t *out_vec);
 
 #ifdef __cplusplus
