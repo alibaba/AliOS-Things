3b94fed83a79fd0a9f94101f2d0ee6aa81232ffd
william.wgj <william.wgj@alibaba-inc.com>
2019-09-02 19:52:32 +0800

    BugID:22360499: uAI API uai_fully_connected mismatch fix
    Change-Id: Iff536b5643b0a05f0158a0239b28a92cdc48fa09
    Reviewed-on: http://yuncode.alibaba-inc.com/228423
    Tested-by: 范文宗 wenzong.fwz <wenzong.fwz@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

--- middleware/uai/function/connected/uai_fully_connected.c
+++ middleware/uai/function/connected/uai_fully_connected.c
@@ -6,42 +6,42 @@
 #include "arm_nnfunctions.h"
 #endif
 
-int uai_fully_connected(const int8_t  *inut_buffer,
+int uai_fully_connected(const int8_t  *input_buffer,
                         const int8_t  *weights,
                         const uint16_t input_size,
                         const uint16_t weight_rows,
                         const uint16_t bias_shift,
                         const uint16_t out_shift,
                         const int8_t  *bias,
-                        int8_t        *out_buffer, 
+                        int8_t        *out_buffer,
                         int16_t       *vec_buffer)
 {
 #if defined(UAI_USE_CMSIS_NN)
-    return arm_fully_connected_q7(inut_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
+    return arm_fully_connected_q7(input_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
 #elif defined(UAI_USE_HARDWARE_NN)
-    return uai_fully_connected_hal(inut_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
+    return uai_fully_connected_hal(input_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
 #else
     /* TODO: This version does not support */
-    return uai_fully_connected_software(inut_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
+    return uai_fully_connected_software(input_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
 #endif
 }
 
-int uai_fully_connected_opt(const int8_t  *inut_buffer,
+int uai_fully_connected_opt(const int8_t  *input_buffer,
                             const int8_t  *weights,
                             const uint16_t input_size,
                             const uint16_t weight_rows,
                             const uint16_t bias_shift,
                             const uint16_t out_shift,
                             const int8_t   *bias,
-                            int8_t         *out_buffer, 
+                            int8_t         *out_buffer,
                             int16_t        *vec_buffer)
 {
 #if defined(UAI_USE_CMSIS_NN)
-    return arm_fully_connected_q7_opt(inut_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
+    return arm_fully_connected_q7_opt(input_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
 #elif defined(UAI_USE_HARDWARE_NN)
-    return uai_fully_connected_opt_hal(inut_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
+    return uai_fully_connected_opt_hal(input_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
 #else
     /* TODO: This version does not support */
-    return uai_fully_connected_opt_software(inut_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
+    return uai_fully_connected_opt_software(input_buffer, weights, input_size, weight_rows, bias_shift, out_shift, bias, out_buffer, vec_buffer);
 #endif
 }
