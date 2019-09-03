f93fca02a5f42d8f7bbd50249cbd326f8520654f
zhuqing <zhuqing.zq@alibaba-inc.com>
2019-09-03 14:43:11 +0800

    BugID:18744724:lowpower modify krhino_spin_lock_irq_save(lock, flags);kernel3
    Change-Id: I351e37a41a2dd595af7d78e356bc4bf07462e6d9

--- kernel/rhino/include/k_spin_lock.h
+++ kernel/rhino/include/k_spin_lock.h
@@ -17,45 +17,45 @@ typedef struct {
 extern void k_wait_allcores(void);
 
 
-#define KRHINO_SPINLOCK_FREE_VAL             0xB33FFFFFu
+#define KRHINO_SPINLOCK_FREE_VAL                        0xB33FFFFFu
 
 /* SMP spin lock */
-#define krhino_spin_lock(lock)               do {                                       \
-                                                 cpu_spin_lock((lock));                 \
-                                             } while (0)                                \
+#define krhino_spin_lock(lock)                          do {                                            \
+                                                            cpu_spin_lock((lock));                      \
+                                                        } while (0)                                     \
 
-#define krhino_spin_unlock(lock)             do {                                       \
-                                                 cpu_spin_unlock((lock));               \
-                                             } while (0)
+#define krhino_spin_unlock(lock)                        do {                                            \
+                                                            cpu_spin_unlock((lock));                    \
+                                                        } while (0)
 
-#define krhino_spin_lock_irq_save(lock, flags)      do {                                \
-                                                 flags        = cpu_intrpt_save();      \
-                                                 cpu_spin_lock((lock));                 \
-                                             } while (0)
+#define krhino_spin_lock_irq_save(lock, flags)          do {                                            \
+                                                            flags          = cpu_intrpt_save();         \
+                                                            cpu_spin_lock((lock));                      \
+                                                        } while (0)
 
-#define krhino_spin_unlock_irq_restore(lock, flags) do {                                \
-                                                 cpu_spin_unlock((lock));        \
-                                                 cpu_intrpt_restore(flags);     \
-                                             } while (0)
+#define krhino_spin_unlock_irq_restore(lock, flags)     do {                                            \
+                                                            cpu_spin_unlock((lock));                    \
+                                                            cpu_intrpt_restore(flags);                  \
+                                                        } while (0)
 
-#define krhino_spin_lock_init(lock)          do {                                       \
-                                                 kspinlock_t *s = (kspinlock_t *)(lock);\
-                                                 s->owner       = KRHINO_SPINLOCK_FREE_VAL;\
-                                             } while(0)
+#define krhino_spin_lock_init(lock)                     do {                                            \
+                                                            kspinlock_t *s = (kspinlock_t *)(lock);     \
+                                                            s->owner       = KRHINO_SPINLOCK_FREE_VAL;  \
+                                                        } while(0)
 #else
 /* UP spin lock */
-#define krhino_spin_lock(lock)              krhino_sched_disable();
-#define krhino_spin_unlock(lock)            krhino_sched_enable();
+#define krhino_spin_lock(lock)                          krhino_sched_disable();
+#define krhino_spin_unlock(lock)                        krhino_sched_enable();
 
-#define krhino_spin_lock_irq_save(lock, flags)      do {                                \
-                                                (void)lock;                             \
-                                                 flags        = cpu_intrpt_save();      \
-                                             } while (0)
+#define krhino_spin_lock_irq_save(lock, flags)          do {                                            \
+                                                            (void)lock;                                 \
+                                                            flags          = cpu_intrpt_save();         \
+                                                        } while (0)
 
-#define krhino_spin_unlock_irq_restore(lock, flags) do {                                \
-                                                (void)lock;                             \
-                                                 cpu_intrpt_restore(flags);             \
-                                             } while (0)
+#define krhino_spin_unlock_irq_restore(lock, flags)     do {                                            \
+                                                            (void)lock;                                 \
+                                                            cpu_intrpt_restore(flags);                  \
+                                                        } while (0)
 
 #define krhino_spin_lock_init(lock)
 #endif  /* RHINO_CONFIG_CPU_NUM > 1 */
