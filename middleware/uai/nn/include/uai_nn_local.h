8a79573aa8bca7c265f84223b308054a1d2ed49d
william.wgj <william.wgj@alibaba-inc.com>
2019-09-04 09:13:45 +0800

    BugID:22387823: uAI fully connected function interface optimize
    Change-Id: I27b0122d5b33d180fd6618dc84c5acea5b9e465a
    Reviewed-on: http://yuncode.alibaba-inc.com/228734
    Tested-by: 范文宗 wenzong.fwz <wenzong.fwz@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

--- middleware/uai/nn/include/uai_nn_local.h
+++ middleware/uai/nn/include/uai_nn_local.h
@@ -5,22 +5,16 @@
 #ifndef UAI_NN_LOCAL_H
 #define UAI_NN_LOCAL_H
 
+#include "uai_function.h"
+
 #ifdef __cplusplus
 extern "C" {
 #endif
 
 
-typedef int (*uai_fconn_func)(const int8_t  *inut_buffer,
-                                const int8_t  *weights,
-                                const uint16_t input_size,
-                                const uint16_t weight_rows,
-                                const uint16_t bias_shift,
-                                const uint16_t out_shift,
-                                const int8_t   *bias,
-                                int8_t         *out_buffer, 
-                                int16_t        *vec_buffer);
+typedef int (*uai_fconn_func)(uai_input_s *input, uai_weight_s *weight, uai_bias_s *bias, uai_output_s *output, int16_t *vec_buffer);
 
-typedef void (*uai_act_func)(int8_t * data, uint16_t size, uint16_t int_width);
+typedef void (*uai_act_func)(int8_t *data, uint16_t size, uint16_t int_width);
 
 typedef void (*uai_softmax_func)(const int8_t *in_vec, const uint16_t dim_vec, int8_t *out_vec);
 
