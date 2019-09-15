a1d3da17d996c68be76d1d545df2a40c76143f7d
Li Cheng <lc122798@alibaba-inc.com>
2019-09-16 02:14:01 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: I53b56d639bda739311d4b0a9d60d7ca80531c8aa

--- board/stm32l476rg-nucleo/ucube.py
+++ board/stm32l476rg-nucleo/ucube.py
@@ -22,5 +22,5 @@ if aos_global_config.get('sal', 1) == 1:
 
 component.set_enable_vfp()
 
-linux_only_targets="blink coapapp helloworld http2app linkkit_gateway linkkitapp modbus_demo mqttapp otaapp tls udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp yts"
+linux_only_targets="blink coapapp helloworld http2app jsengine_app linkkit_gateway linkkitapp modbus_demo mqttapp otaapp tls udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp yts"
 windows_only_targets="blink|COMPILER=armcc blink|COMPILER=iar helloworld|COMPILER=armcc helloworld|COMPILER=iar linkkitapp|COMPILER=armcc"
