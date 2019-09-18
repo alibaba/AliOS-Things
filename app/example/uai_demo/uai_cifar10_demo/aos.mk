6949e238833bdcf47ea845965282d3d9ce42d8fa
william.wgj <william.wgj@alibaba-inc.com>
2019-09-18 15:59:59 +0800

    BugID:22497022: support cnn model and cifar10 example
    Change-Id: I2bdd27ff4d8d097ce71c0044a2101d4be11da9b9

---
+++ app/example/uai_demo/uai_cifar10_demo/aos.mk
@@ -0,0 +1,15 @@
+NAME := uai_cifar10_demo
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 1.0.0
+$(NAME)_SUMMARY := uAI CIFAR10 DEMO
+$(NAME)_SOURCES := uai_cifar10_demo.c cnn_weights.c img_data.c
+
+$(NAME)_COMPONENTS += osal_aos cli uai
+
+GLOBAL_DEFINES += AOS_NO_WIFI
+
+GLOBAL_INCLUDES += ./
+
+cmsis_nn_enable ?= 1
+cnn_enable      ?= 1
