3341caee7b480ad52b206ab9a1b1462e775a6e32
william.wgj <william.wgj@alibaba-inc.com>
2019-09-19 10:17:09 +0800

    BugID:22497022: support cnn model and cifar10 example
    Change-Id: I7c3e1ce34002747fc171cac96b4734b4ea02ab44
    Reviewed-on: http://yuncode.alibaba-inc.com/231286
    Tested-by: <iot-scm@alibaba-inc.com>
    Reviewed-by: 万国建 william.wgj <william.wgj@alibaba-inc.com>

--- middleware/uai/3rdparty/cmsis_nn/get_cmsis_nn.sh
+++ middleware/uai/3rdparty/cmsis_nn/get_cmsis_nn.sh
@@ -1,4 +1,4 @@
-#git clone https://github.com/ARM-software/CMSIS_5.git
+git clone https://github.com/ARM-software/CMSIS_5.git
 if [ ! -f "CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC_nonsquare.c" ]; then
     cp arm_pool_q7_HWC_nonsquare.c CMSIS_5/CMSIS/NN/Source/PoolingFunctions/arm_pool_q7_HWC_nonsquare.c
 fi
