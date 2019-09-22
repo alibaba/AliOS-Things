4888c47c59f47454078bbc1e44990299f7444234
wenzong.fwz <wenzong.fwz@alibaba-inc.com>
2019-09-22 16:08:19 +0800

    BugID:19675668: Support for App dev framework
    * Generate aos comp index
    * Create App from builtin examples
    * Support menuconfig for depended comps
    * Update dependencies according to includes
    
    Change-Id: I65dd87b2729fc82dcdd53738c29284505271e3dc

--- build/scripts/app_update_aosmk.py
+++ build/scripts/app_update_aosmk.py
@@ -5,104 +5,53 @@ import sys
 import hashlib
 import re
 import json
+from lib.code import get_md5sum, write_md5sum, compute_header_md5sum, get_depends_from_source
+from lib.config import get_project_config
 
 CONFIG_FILE = ".aos"
-COMP_INCLUDES = "aos_comp_includes.json"
+COMP_INDEX = "aos_comp_index.json"
 
-def get_comp_depends(include_list):
-    includes = []
-    depends = []
-    with open(COMP_INCLUDES, "r") as f:
-        includes = json.load(f)
-
-    for include_file in include_list:
-        found = False
-        for key in includes:
-            dirs = includes[key]["include_dirs"]
-            files = includes[key]["include_files"]
-            for dir in dirs:
-                if os.path.abspath(os.path.join(dir, include_file)) in files:
-                    depends.append(key)
-                    found = True
-                    break
-            if found:
-                break
-
-    return depends
-
-def get_md5sum(config_file):
-    ret = None
-    with open(config_file, "r") as f:
-        for line in f.readlines():
-            line = line.strip()
-            if "MD5SUM_HEADER" in line:
-                ret = line.replace("MD5SUM_HEADER=", "")
-
-    return ret
-
-def write_md5sum(config_file, md5sum_value):
-    content = []
-    with open(config_file, "r") as f:
-        for line in f.readlines():
-            if "MD5SUM_HEADER" in line:
-                continue
-            else:
-                content.append(line)
-
-    content.append("MD5SUM_HEADER=%s\n" % md5sum_value)
-    with open(config_file, "w") as f:
-        for line in content:
-            f.write(line)
-
-def md5sum(string):
-    return hashlib.md5(string).hexdigest()
-
-def get_include_file(code_file):
-    include_list = []
-
-    patten = re.compile(r'#include\s+(<|")(.*)(>|")')
-    with open(code_file, "r") as f:
-        for line in f.readlines():
-            if line.startswith("#include"):
-                line = line.strip()
-                match = patten.match(line)
-                if match:
-                    include_list.append(match.group(2))
-
-    return include_list
 
 def update_aosmk(dirname):
-    code_list = []
+    """ Update App's aos.mk """
     aosmk = os.path.join(dirname, "aos.mk")
     config_file = os.path.join(dirname, CONFIG_FILE)
 
     old_md5sum = get_md5sum(config_file)
-    for root, dirs, files in os.walk(dirname):
-        for file in files:
-            if file.endswith(".c") or file.endswith(".h") or file.endswith(".cpp"):
-                code_list.append(os.path.join(root, file))
-
-    include_list = []
-    for code_file in code_list:
-        include_list += get_include_file(code_file)
-
-    sorted(include_list)
-    new_md5sum = md5sum(" ".join(include_list))
+    (new_md5sum, include_list) = compute_header_md5sum(dirname)
 
     if old_md5sum == new_md5sum:
         return True
 
+    comp_info = {}
+    with open(COMP_INDEX, "r") as f:
+        comp_info = json.load(f)
+
+    source_depends = []
+    if comp_info:
+        source_depends = get_depends_from_source(comp_info, include_list)
+
+    origin_depends = []
+    tmp = get_project_config(config_file, "DEPENDENCIES")
+    if tmp:
+        origin_depends = tmp.split()
+
+    depends = list(set(source_depends).difference(set(origin_depends)))
+
     content = []
+    found_c_deps = False
+    add_line = "$(NAME)_COMPONENTS_CUSTOMIZED := %s\n" % " ".join(depends)
     with open(aosmk, "r") as f:
         for line in f.readlines():
-            if "$(NAME)_COMPONENTS" in line:
-                continue
+            if line.startswith("$(NAME)_COMPONENTS_CUSTOMIZED"):
+                line = add_line
+                found_c_deps = True
 
             content.append(line)
 
-    depends = get_comp_depends(include_list)
-    for dep in depends:
-        content.append("$(NAME)_COMPONENTS += %s\n" % dep)
+    if not found_c_deps:
+        content.append(add_line)
+        content.append("$(NAME)_COMPONENTS += $($(NAME)_COMPONENTS_CUSTOMIZED)\n")
 
     with open(aosmk, "w") as f:
         for line in content:
