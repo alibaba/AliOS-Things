54c991c061784814003ae6dfa493332d06a59b9b
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-09-24 13:59:38 +0800

    BugID:22693037:[ble] Autotest for ble, update YTS case and func test.
    Change-Id: I2e2d80fd370d2bfa5cb947d2d14d1247226370f6

---
+++ test/testcase/network/ble/aos.mk
@@ -0,0 +1,10 @@
+NAME := ble_test
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION    := 0.0.1
+$(NAME)_SUMMARY    := testcase for ble 
+
+$(NAME)_SOURCES += ble_test.c
+
+$(NAME)_CFLAGS += -Wall -Werror
+
