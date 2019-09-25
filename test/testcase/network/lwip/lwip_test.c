a638cd893ab6e52aa4416266b3fa976d74c54fa6
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2019-09-25 17:38:28 +0800

    BugID:22724304: Add more API test cases for lwip.
    Change-Id: Iad4fc93c5f114d2852e21419024a8716b3fc6aa2

--- test/testcase/network/lwip/lwip_test.c
+++ test/testcase/network/lwip/lwip_test.c
@@ -14,6 +14,8 @@
 
 #define TCP_SERVER_IP "134.102.218.18"
 #define TCP_SERVER_PORT 80
+#define UDP_SERVER_IP TCP_SERVER_IP
+#define UDP_SERVER_PORT 5683
 
 #define RECV_BUFFER_LEN 1512
 
@@ -102,6 +104,140 @@ err:
     if (recv_buf) aos_free(recv_buf);
 }
 
+static void test_lwip_socket_adv(void)
+{
+    int fd, ret, stype = -1;
+    struct sockaddr_in addr;
+    struct sockaddr addr2;
+    socklen_t slen = sizeof(addr2);
+
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    YUNIT_ASSERT(fd >= 0);
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(TCP_SERVER_PORT);
+    addr.sin_addr.s_addr = inet_addr(TCP_SERVER_IP);
+    ret = connect(fd, (struct sockadd *)&addr, sizeof(addr));
+    YUNIT_ASSERT(ret == 0);
+    if (ret != 0) return;
+
+    memset(&addr2, 0, sizeof(addr2));
+    ret = getpeername(fd, &addr2, &slen);
+    YUNIT_ASSERT(ret == 0);
+
+    slen = sizeof(addr2);
+    memset(&addr2, 0, sizeof(addr2));
+    ret = getsockname(fd, &addr2, &slen);
+    YUNIT_ASSERT(ret == 0);
+
+    ret = getsockopt(fd, SOL_SOCKET, SO_TYPE, &stype, sizeof(stype));
+    YUNIT_ASSERT(ret == 0);
+
+    uint32_t mode = 1;
+    ret = ioctl(fd, FIONBIO, &mode);
+    YUNIT_ASSERT(ret != -1);
+
+    ret = -1;
+    ret = fcntl(fd, F_GETFL, 0);
+    YUNIT_ASSERT(ret >= 0);
+
+    ret = shutdown(fd, SHUT_RDWR);
+    YUNIT_ASSERT(ret == 0);
+
+    ret = eventfd(0, 0);
+    YUNIT_ASSERT(ret != -1);
+    if (ret != -1) close(ret);
+}
+
+#define UDP_TEST_STR "hello world"
+static void test_lwip_socket_udp(void)
+{
+    int fd, ret;
+    struct sockaddr_in addr;
+
+    fd = socket(AF_INET, SOCK_DGRAM, 0);
+    YUNIT_ASSERT(fd >= 0);
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_port = htons((short)atoi(UDP_SERVER_PORT));
+    addr.sin_addr.s_addr = inet_addr(UDP_SERVER_IP);
+    addr.sin_family = AF_INET;
+
+    ret = sendto(fd, UDP_TEST_STR, strlen(UDP_TEST_STR), 0, 
+                 (struct sockaddr*)&addr, sizeof(struct sockaddr_in));
+    YUNIT_ASSERT(ret != -1);
+
+    fd_set read_fds;
+    struct timeval tv;
+    FD_ZERO(&read_fds);
+    FD_SET(fd, &read_fds);
+    tv.tv_sec  = 1;
+    tv.tv_usec = 0;
+    ret = select(fd + 1, &read_fds, NULL, NULL, &tv);
+    YUNIT_ASSERT(ret != -1);
+
+    uint8_t buf[32];
+    struct sockaddr fromaddr;
+    socklen_t slen;
+    if (ret > 0) {
+        slen = sizeof(fromaddr);
+        ret = recvfrom(fd, buf, sizeof(buf), MSG_DONTWAIT, &fromaddr, &slen);
+        YUNIT_ASSERT(ret != -1);
+    }
+
+    struct msghdr msg;
+    struct iovec iov;
+    iov.iov_base = buf;
+    iov.iov_len = sizeof(buf);
+    msg.msg_name = &addr;
+    msg.msg_namelen = sizeof(addr);
+    msg.msg_iovlen = 1;
+    msg.msg_iov = &iov;
+    msg.msg_control = NULL;
+    msg.msg_controllen = 0;
+    ret = sendmsg(fd, &msg, 0);
+    YUNIT_ASSERT(ret != -1);
+
+    ret = close(fd);
+    YUNIT_ASSERT(ret == 0);
+}
+
+#define TCP_SERVER_PORT 32000
+static void test_lwip_socket_server(void)
+{
+    int fd, ret;
+    struct sockaddr_in daddr;
+
+    fd = socket(AF_INET, SOCK_STREAM, IPPROTO_IP);
+    YUNIT_ASSERT(fd >= 0);
+
+    daddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    daddr.sin_family = AF_INET;
+    daddr.sin_port = htons(TCP_SERVER_PORT);
+
+    ret = bind(fd, (struct sockaddr *)&daddr, sizeof(daddr));
+    YUNIT_ASSERT(ret == 0);
+
+    ret = listen(fd, 1);
+    YUNIT_ASSERT(ret == 0);
+
+    struct timeval timeout;
+    timeout.tv_sec = 30;
+    timeout.tv_usec = 0;
+    ret = setsockopt (fd, SOL_SOCKET, SO_RCVTIMEO, (char *)&timeout,
+                      sizeof(timeout));
+    YUNIT_ASSERT(ret == 0);
+
+    struct sockaddr saddr;
+    socklen_t slen = sizeof(saddr);
+    ret = accept(fd, &saddr, &slen);
+    YUNIT_ASSERT((ret != -1) || ((ret == -1) && (errno == EAGAIN)));
+
+    ret = close(fd);
+    YUNIT_ASSERT(ret == 0);
+}
+
 static int init(void)
 {
     return 0;
@@ -123,7 +259,10 @@ static void teardown(void)
 }
 
 static yunit_test_case_t aos_lwip_testcases[] = {
-    { "socket", test_lwip_socket },
+    { "basic", test_lwip_socket },
+    { "advanced", test_lwip_socket_adv },
+    { "udp", test_lwip_socket_udp },
+    { "server", test_lwip_socket_server },
     YUNIT_TEST_CASE_NULL
 };
 
