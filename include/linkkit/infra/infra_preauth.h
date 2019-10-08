0e3f036f02ba9b8b2f899091193d5d5b19447fb5
zhangtian <dongren.zt@alibaba-inc.com>
2019-10-08 16:44:27 +0800

    BugID:22829358:[pre_auth]delete iotx_pre_auth_output_t,using iotx_sign_mqtt_t uniformly
    Change-Id: I99c045cf21842cbc6e85893c72dd3c2c8266151a

--- include/linkkit/infra/infra_preauth.h
+++ include/linkkit/infra/infra_preauth.h
@@ -19,20 +19,7 @@
     #define preauth_debug(...)
 #endif
 
-#define PRE_AUTH_HOSTNAME_MAXLEN  (64)
-#define PRE_AUTH_CLIENT_ID_MAXLEN (200)
-#define PRE_AUTH_USERNAME_MAXLEN  (64)
-#define PRE_AUTH_PASSWORD_MAXLEN  (65)
-
-typedef struct {
-    char hostname[PRE_AUTH_HOSTNAME_MAXLEN];
-    uint16_t port;
-    char clientid[PRE_AUTH_CLIENT_ID_MAXLEN];
-    char username[PRE_AUTH_USERNAME_MAXLEN];
-    char password[PRE_AUTH_PASSWORD_MAXLEN];
-} iotx_pre_auth_output_t;
-
 int preauth_get_connection_info(iotx_mqtt_region_types_t region, iotx_dev_meta_info_t *dev_meta,
-                                const char *sign, const char *device_id, iotx_pre_auth_output_t *preauth_output);
+                                const char *sign, const char *device_id, iotx_sign_mqtt_t *preauth_output);
 
 #endif /* #ifndef __INFRA_PREAUTH__ */
