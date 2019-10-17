2e6267c5ec3e4992bc798a2bd2bcbdac3cd5fc0b
alixiyuan <wencheng.ywc@alibaba-inc.com>
2019-10-17 13:37:23 +0800

    BugID:16846667: remove process logic from k_dyn_mem_proc.c
    Change-Id: I630c6e0589b416bb0e3edb650d47b22844cda30c

--- core/rhino/k_dyn_mem_proc.c
+++ core/rhino/k_dyn_mem_proc.c
@@ -7,103 +7,7 @@
 #if (RHINO_CONFIG_KOBJ_DYN_ALLOC > 0)
 
 #if (RHINO_CONFIG_USER_SPACE > 0)
-
-#include "task_group.h"
-#include "res.h"
-#include "k_proc.h"
-
-static void free_user_res(task_group_t *group)
-{
-    kmutex_t     *mutex;
-    klist_t      *head, *iter;
-
-    /*mutex res exit*/
-    head = &(group->kobj_list.mutex_head);
-    iter = head->next;
-
-    while (iter != head) {
-        mutex = krhino_list_entry(iter, kmutex_t, blk_obj.obj_list);
-        iter = iter->next;
-        krhino_mutex_dyn_del(mutex);
-    }
-
-    /*sem res exit*/
-#if (RHINO_CONFIG_SEM > 0)
-    ksem_t       *sem;
-    head = &(group->kobj_list.sem_head);
-    iter = head->next;
-
-    while (iter != head) {
-        sem = krhino_list_entry(iter, ksem_t, blk_obj.obj_list);
-        iter = iter->next;
-        krhino_sem_dyn_del(sem);
-    }
-#endif
-
-    /*queue res exit*/
-#if (RHINO_CONFIG_QUEUE > 0)
-    kqueue_t     *queue;
-    head = &(group->kobj_list.queue_head);
-    iter = head->next;
-
-    while (iter != head) {
-        queue = krhino_list_entry(iter, kqueue_t, blk_obj.obj_list);
-        iter = iter->next;
-        krhino_queue_dyn_del(queue);
-    }
-#endif
-
-    /*buf queue res exit*/
-#if (RHINO_CONFIG_BUF_QUEUE > 0)
-    kbuf_queue_t *buf_queue;
-    head = &(group->kobj_list.buf_queue_head);
-    iter = head->next;
-
-    while (iter != head) {
-        buf_queue = krhino_list_entry(iter, kbuf_queue_t, blk_obj.obj_list);
-        iter = iter->next;
-        krhino_buf_queue_dyn_del(buf_queue);
-    }
-#endif
-
-    /* event res exit*/
-#if (RHINO_CONFIG_EVENT_FLAG > 0)
-    kevent_t     *event;
-    head = &(group->kobj_list.event_head);
-    iter = head->next;
-
-    while (iter != head) {
-        event = krhino_list_entry(iter, kevent_t, blk_obj.obj_list);
-        iter = iter->next;
-        krhino_event_dyn_del(event);
-    }
-#endif
-}
-
-static void proc_free(ktask_t *task)
-{
-    task_group_t *group;
-    int           pid;
-
-    group = task->task_group;
-    if (NULL == group) {
-        return;
-    }
-
-    task_group_remove_res_obj(group, RES_OBJ_TASK, task);
-
-    if (group->task_cnt > 0) {
-        if (group->state == TGS_NORMAL) {
-            res_free(group->pid, task->task_ustack_base);
-        }
-    } else {
-        pid = group->pid;
-        free_user_res(group);
-        task_group_release(group);
-        task_group_destory(group);
-        k_proc_unload(pid);
-    }
-}
+void krhino_utask_free_res(ktask_t *task);
 #endif
 
 void dyn_mem_proc_task(void *arg)
@@ -130,7 +34,7 @@ void dyn_mem_proc_task(void *arg)
                 klist_rm(&res_free->res_list);
                 RHINO_CRITICAL_EXIT();
 #if (RHINO_CONFIG_USER_SPACE > 0)
-                proc_free((ktask_t *)(res_free->res[1]));
+                krhino_utask_free_res((ktask_t *)(res_free->res[1]));
 #endif
                 memcpy(&tmp, res_free, sizeof(res_free_t));
                 for (i = 0; i < tmp.cnt; i++) {
