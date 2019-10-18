9b80031c314665e8793b2ae3af1aaeef004a88ea
yuezhen.xyz <yuezhen.xyz@alibaba-inc.com>
2019-10-18 10:55:32 +0800

    BugID:23044503: Modify format of uND packet & reporting scheme of uND.
    chipsets in following list support uND,
    
    ESP8266/ESP32/RDA5981/ASR5501/RTL8710BN/SV6266/XR871/BK7231/
    BK7231U/BK7231S/WM_W600/lpc54102/lpcxpresso54102/dh5021a
    
    Change-Id: Ia0058262fab8a57ab657f8d933e6281c52056445
    Reviewed-on: http://yuncode.alibaba-inc.com/235414
    Tested-by: <iot-scm@alibaba-inc.com>
    Reviewed-by: 庞军平 junping.pjp <junping.pjp@alibaba-inc.com>

--- board/asr5501/drivers/include/lega_wlan_api.h
+++ board/asr5501/drivers/include/lega_wlan_api.h
@@ -22,46 +22,41 @@ typedef enum {
   STA,      /*Act as a station which can connect to an access point*/
 } lega_wlan_type_e;
 
-enum {
-    WLAN_DHCP_DISABLE = 0,
+typedef enum {
+    WLAN_DHCP_DISABLE = 0,  //use static ip address in station mode
     WLAN_DHCP_CLIENT,
     WLAN_DHCP_SERVER,
-};
+} lega_wlan_dhcp_mode_e;
 
 typedef enum {
-    EVENT_STATION_UP = 1,  /*used in station mode,
-                            indicate station associated in open mode or 4-way-handshake done in WPA/WPA2*/
-    EVENT_STATION_DOWN,    /*used in station mode, indicate station deauthed*/
-    EVENT_AP_UP,           /*used in softap mode, indicate softap enabled*/
-    EVENT_AP_DOWN,         /*used in softap mode, indicate softap disabled*/
+    EVENT_STATION_UP = 1,   /* used in station mode,indicate station associated
+                              in open mode or 4-way-handshake done in WPA/WPA2 */
+    EVENT_STATION_DOWN,     /* used in station mode, indicate station deauthed */
+    EVENT_AP_UP,            /* used in softap mode, indicate softap enabled    */
+    EVENT_AP_DOWN,          /* used in softap mode, indicate softap disabled   */
 } lega_wifi_event_e;
-typedef enum {
-    CONNECT_SUCC,
-    CONNECT_SCAN_FAIL,
-    CONNECT_CONN_FAIL,
-} lega_start_adv_results_e;
 
 /**
  *  @brief  Scan result using normal scan.
  */
 typedef struct {
   uint8_t is_scan_adv;
-  char ap_num;       /**< The number of access points found in scanning. */
+  char ap_num;              /* number of access points found in scanning. */
   struct {
-    char    ssid[32+1];  /*ssid max len:32. +1 is for '\0'. when ssidlen is 32  */
-    char    ap_power;   /**< Signal strength, min:0, max:100. */
-    char    bssid[6];     /* The BSSID of an access point. */
-    char    channel;      /* The RF frequency, 1-13 */
-    uint8_t security;     /* Security type, @ref wlan_sec_type_t */
+    char    ssid[32+1];     /* ssid max len:32. +1 is for '\0'. when ssidlen is 32  */
+    char    ap_power;       /* Signal strength, min:0, max:100. */
+    char    bssid[6];       /* BSSID of an access point. */
+    char    channel;        /* RF frequency, 1-13 */
+    uint8_t security;       /* Security type, @ref wlan_sec_type_t */
   } * ap_list;
 } lega_wlan_scan_result_t;
 
 typedef enum {
-    WLAN_SECURITY_OPEN,                   //NONE
-    WLAN_SECURITY_WEP,                    //WEP
-    WLAN_SECURITY_WPA,                    //WPA
-    WLAN_SECURITY_WPA2,                   //WPA2
-    WLAN_SECURITY_AUTO,                   //WPA or WPA2
+    WLAN_SECURITY_OPEN,     //NONE
+    WLAN_SECURITY_WEP,      //WEP
+    WLAN_SECURITY_WPA,      //WPA
+    WLAN_SECURITY_WPA2,     //WPA2
+    WLAN_SECURITY_AUTO,     //WPA or WPA2
     WLAN_SECURITY_MAX,
 } lega_wlan_security_e;
 
@@ -73,34 +68,36 @@ typedef struct {
     char    bssid[6];       /* BSSID of the wlan needs to be connected.*/
     char    ssid_len;       /*ssid length*/
     char    pwd_len;        /*password length*/
-    char    channel;       /* wifi channel 0-13.*/
-    char    security;      /*refer to lega_wlan_security_e*/
+    char    channel;        /* wifi channel 0-13.*/
+    char    security;       /*refer to lega_wlan_security_e*/
 } lega_wlan_ap_info_adv_t;
 
-/*only used in station mode*/
+/*used in station and softap mode*/
 typedef struct {
-    char    dhcp;             /* no use currently */
-    char    macaddr[16];      /* mac address on the target wlan interface, ASCII*/
-    char    ip[16];           /* Local IP address on the target wlan interface, ASCII*/
-    char    gate[16];         /* Router IP address on the target wlan interface, ASCII */
-    char    mask[16];         /* Netmask on the target wlan interface, ASCII*/
+    char    start_ip[16];     /* start ip addr of dhcp pool in softap mode */
+    char    end_ip[16];       /* end ip addr of dhcp pool in softap mode*/
+    char    dhcp;             /* refer to lega_wlan_dhcp_mode_e */
+    char    macaddr[16];      /* mac address on the target wlan interface, e.g. "AA1122334455"*/
+    char    ip[16];           /* Local IP address on the target wlan interface, e.g. "192.168.1.100" */
+    char    gate[16];         /* Router IP address on the target wlan interface, "192.168.1.1" */
+    char    mask[16];         /* Netmask on the target wlan interface, "255.255.255.0" */
     char    dns[16];          /* no use currently , ASCII*/
     char    broadcastip[16];  /* no use currently , ASCII*/
 } lega_wlan_ip_stat_t;
 
 /*only used in station mode*/
 typedef struct {
-    int     is_connected;  /* The link to wlan is established or not, 0: disconnected, 1: connected. */
-    int     wifi_strength; /* Signal strength of the current connected AP */
+    int     is_connected;    /* The link to wlan is established or not, 0: disconnected, 1: connected. */
+    int     wifi_strength;   /* Signal strength of the current connected AP */
     char    ssid[32+1];      /* ssid max len:32. +1 is for '\0'. when ssidlen is 32  */
-    char    bssid[6];      /* BSSID of the current connected wlan */
-    int     channel;       /* Channel of the current connected wlan */
+    char    bssid[6];        /* BSSID of the current connected wlan */
+    int     channel;         /* Channel of the current connected wlan */
 } lega_wlan_link_stat_t;
 
 /*used in open cmd of hal_wifi_module_t*/
 typedef struct {
     char    wifi_mode;              /* refer to hal_wifi_type_t*/
-    char    security;               /* security mode */
+    char    security;               /* security mode, refer to lega_wlan_security_e */
     char    wifi_ssid[32];          /* in station mode, indicate SSID of the wlan needs to be connected.
                                        in softap mode, indicate softap SSID*/
     char    wifi_key[64];           /* in station mode, indicate Security key of the wlan needs to be connected,
@@ -114,9 +111,34 @@ typedef struct {
     char    mac_addr[6];            /* connect bssid in sta mode*/
     char    reserved[32];           /* no use currently */
     int     wifi_retry_interval;    /* no use currently */
+    int     wifi_retry_times;       /* used in station mode to config reconnecting times after disconnected*/
     int     interval;               /* used in softap mode to config beacon listen interval */
     int     hide;                   /* used in softap mode to config hidden SSID */
 } lega_wlan_init_type_t;
+
+typedef struct {
+    int  rssi;                     /* rssi of connected ap for station mode */
+    int  snr;                      /* snr of connected ap for station mode */
+    int  per;                      /* per of tx data failed for station mode(ppm) */
+    unsigned long long tx_total;   /* statistics total transmit pkt for station mode */
+    unsigned long long tx_fail;    /* statistics total transmit pkt for station mode */
+} lega_wireless_info_t;
+
+/**
+ * @brief sta ip and mac address used in softap mode
+ * sta_ip_addr: e.g. when ip addr==192.168.1.1<-->sta_ip_addr == 0x0101A8C0
+ */
+typedef struct{
+    uint32_t sta_ip_addr;                           /* station ip addr  */
+    uint8_t  sta_mac_addr[6];                       /* station mac addr */
+}lega_wlan_client_addr_info_t;
+
+/*store linked station info*/
+typedef struct{
+    int client_num;                                 /* linked station number */
+    lega_wlan_client_addr_info_t sta[4];            /* linked station entry, max client number is 4 */
+}lega_wlan_ap_client_info_t;
+
 
 /** @brief  wifi init functin, user should call it before use any wifi cmd
  *  @return    0       : on success.
@@ -151,12 +173,6 @@ int lega_wlan_close(void);
  *  @return    other   : error occurred
  */
 int lega_wlan_start_scan(void);
-
-/** @brief  used in station mode, scan cmd
- *  @return    0       : on success.
- *  @return    other   : error occurred
- */
-int lega_wlan_start_scan_adv(void);
 
 /** @brief  used in station mode, scan cmd
  *
@@ -251,9 +267,6 @@ int lega_wlan_set_ps_mode(uint8_t ps_on);
 /*when use monitor mode, user should register this type of callback function to get the received MPDU*/
 typedef void (*monitor_cb_t)(uint8_t*data, int len, int rssi);
 
-/*when use monitor-ap mode, user should register this type of callback function */
-typedef void (*monitor_ap_cb_t)();
-
 /** @brief  used in sniffer mode, callback function to get received MPDU, should register before start_monitor
  *
  * @param fn    : refer to monitor_data_cb_t
@@ -262,28 +275,6 @@ typedef void (*monitor_ap_cb_t)();
  *  @return    other   : error occurred
  */
 int lega_wlan_register_monitor_cb(monitor_cb_t fn);
-
-/** @brief  used in sniffer-ap mode, callback function for application
- *
- * @param fn    : refer to monitor_ap_cb_t
- *
- *  @return    0       : on success.
- *  @return    other   : error occurred
- */
-int lega_wlan_register_monitor_ap_cb(monitor_ap_cb_t fn);
-
-/* start adv callback function, notify the connect results*/
-typedef void (*start_adv_cb_t)(lega_start_adv_results_e status);
-
-/** @brief  used in sta mode, callback function to notify the connecting results
- *
- * @param fn    : refer to start_adv_cb_t
- *
- *  @return    0       : on success.
- *  @return    other   : error occurred
- */
-int lega_wlan_register_start_adv_cb(start_adv_cb_t fn);
-
 
 /** @brief  used in station mode or sniffer mode, call this cmd to send a MPDU constructed by user
  *
@@ -369,34 +360,21 @@ void lega_drv_rco_cal(void);
  */
 void lega_drv_close_dcdc_pfm(void);
 
-/** @brief  config to support smartconfig in MIMO scenario
- *
- */
-void lega_wlan_smartconfig_mimo_enable(void);
-
 /** @brief  set country code to update country code, different country may have different channel list
- *   called after hal_wifi_init
+ *          support channel 1-13 by default
+ *          called after lega_wlan_init
+ *
+ * @param country  : country code string, currently support: "CN" "EU" "JP" "US"
  */
 void lega_wlan_set_country_code(char *country);
 
-/** @brief  start monitor and ap coexist mode
- *
- * @param init_info    : refer to lega_wlan_init_type_t
- *
- * @return    0       : on success.
- * @return    other   : error occurred
+/** @brief  get linked station ip and address in ap mode
+ *  @param            : input param to get sta ip and mac addr
  */
-int lega_wlan_start_monitor_ap(lega_wlan_init_type_t* init_info);
-
-/** @brief  stop monitor and ap coexist mode
- *
- * @return    0       : on success.
- * @return    other   : error occurred
- */
-int lega_wlan_stop_monitor_ap();
+void lega_get_client_ip_mac(lega_wlan_ap_client_info_t* sta_addr);
 
 /** @brief  get current temperature (C degree)
- *  called after hal_wifi_init
+ *  called after lega_wlan_init
  *
  * @param p_temp    : input param to get temperature, memory managed by caller
  *
@@ -409,7 +387,7 @@ int16_t  lega_rf_get_temperature(int16_t *p_temp);
 typedef void (*temperature_get_cb_t)(int16_t temperature);
 
 /** @brief  set the timer to get temperature (in second)
- *  called after hal_wifi_init
+ *  called after lega_wlan_init
  *
  * @param timer_in_sec    : the timer in second
  *
@@ -419,7 +397,7 @@ typedef void (*temperature_get_cb_t)(int16_t temperature);
 int lega_wlan_set_temperature_get_timer(uint64_t timer_in_sec);
 
 /** @brief  register the temperature get callback function
- *  called after hal_wifi_init
+ *  called after lega_wlan_init
  *
  * @param func    : the function will called to notify the temperature.
  *
@@ -427,5 +405,17 @@ int lega_wlan_set_temperature_get_timer(uint64_t timer_in_sec);
  * @return    other   : error occurred
  */
 int lega_wlan_register_temperature_get_cb(temperature_get_cb_t func);
+
+
+/** @brief  get network wireless info
+ *  called after lega_wlan_init
+ *
+ * @param wireless_info    : collect wireless info from HW
+ *
+ * @return    0       : on success.
+ * @return    other   : error occurred
+ */
+int lega_get_wireless_network_info(lega_wireless_info_t *wireless_info);
+
 
 #endif  //_LEGA_WIFI_API_H_
