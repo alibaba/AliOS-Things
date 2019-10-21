df29756030a4231e5c6a78feb2f02268fa2b6ec4
jianzhou <jianzhou.cjz@alibaba-inc.com>
2019-10-21 17:42:20 +0800

    BugID:23063498: add aos hal for websoc and remove unnecessary file
    Change-Id: If602bcc760b8c5551a360329d16df6d388d78564

--- components/network/websoc/aos.mk
+++ components/network/websoc/aos.mk
@@ -6,18 +6,16 @@ $(NAME)_SUMMARY := websocket component
 
 $(NAME)_INCLUDES += src
 
-ifeq (y,$(WEBSOCKET_WITH_TLS))
+ifeq (y,$(WEBSOC_WITH_TLS))
  GLOBAL_DEFINES += WEBSOCKET_SSL_ENABLE
 $(NAME)_COMPONENTS += mbedtls
 endif
 
-$(NAME)_SOURCES := src/rws_common.c \
-                   src/rws_error.c \
+$(NAME)_SOURCES := src/rws_error.c \
                    src/rws_frame.c \
-                   src/librws.c \
                    src/rws_list.c \
                    src/rws_memory.c \
                    src/rws_socketpriv.c \
                    src/rws_socketpub.c \
                    src/rws_string.c \
-                   src/rws_thread.c
+                   src/rws_thread_aos.c
