4611bbac80ba8a0e1d2e36af9fb201395335658c
jianzhou <jianzhou.cjz@alibaba-inc.com>
2019-10-21 17:42:20 +0800

    BugID:23063498: add aos hal for websoc and remove unnecessary file
    Change-Id: If602bcc760b8c5551a360329d16df6d388d78564

--- components/network/websoc/src/rws_thread_aos.c
+++ components/network/websoc/src/rws_thread_aos.c
@@ -3,19 +3,17 @@
  */
 
 #include "websoc/librws.h"
-#include "rws_thread.h"
 #include "rws_memory.h"
-#include "rws_common.h"
 
 #include <assert.h>
-
-#include <posix/pthread.h>
 #include <unistd.h>
+
+#include "aos/kernel.h"
 
 struct rws_thread_struct {
 	rws_thread_funct thread_function;
 	void * user_object;
-	pthread_t thread;
+	void * task;
 };
 
 typedef struct _rws_threads_joiner_struct {
@@ -32,8 +30,6 @@ static void rws_threads_joiner_clean(void) { // private
 		return;
 	}
 	_threads_joiner->thread = NULL;
-	pthread_join(t->thread, &r);
-	assert(r == NULL);
 
 	rws_free(t);
 }
@@ -61,75 +57,63 @@ static void * rws_thread_func_priv(void * some_pointer) {
 	return NULL;
 }
 
-rws_thread rws_thread_create(rws_thread_funct thread_function, void * user_object) {
+#define DEFAULT_WEBSOC_THREAD_STACKSIZE 16 * 1024
+#define DEFAULT_WEBSOC_THREAD_NAME      "websoc"
+#define DEFAULT_WEBSOC_THREAD_PRIO      32
+rws_thread rws_thread_create(rws_thread_funct thread_function, void * user_object)
+{
 	rws_thread t = NULL;
 	int res = -1;
-	pthread_attr_t attr;
+	int stack_size, prio;
+	char *name;
 
 	if (!thread_function) {
 		return NULL;
 	}
+
 	rws_threads_joiner_create_ifneed();
 	t = (rws_thread)rws_malloc_zero(sizeof(struct rws_thread_struct));
 	t->user_object = user_object;
 	t->thread_function = thread_function;
-	if (pthread_attr_init(&attr) == 0) {
-        pthread_attr_setstacksize(&attr, 16 * 1024);
-        struct sched_param sched;
-        sched.sched_priority = 32;
-        pthread_attr_setschedparam(&attr, &sched);
-        //if (pthread_attr_setscope(&attr, PTHREAD_SCOPE_SYSTEM) == 0) {
-   		    if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE) == 0) {
-				res = pthread_create(&t->thread, &attr, &rws_thread_func_priv, (void *)t);
-				pthread_setname_np(t->thread, "GnWebsoc");
-			}
-		//}
-		pthread_attr_destroy(&attr);
-	}
+    
+    stack_size = DEFAULT_WEBSOC_THREAD_STACKSIZE;
+    name = DEFAULT_WEBSOC_THREAD_NAME;
+    prio = DEFAULT_WEBSOC_THREAD_PRIO;
+
+    res = aos_task_new_ext((aos_task_t *)&t->task, name, &rws_thread_func_priv, (void *)t, stack_size,
+                            prio);
 	assert(res == 0);
+
 	return t;
 }
 
 void rws_thread_sleep(const unsigned int millisec) {
-	usleep(millisec * 1000); // 1s = 1'000'000 microsec.
+	aos_msleep(millisec);
 }
 
 rws_mutex rws_mutex_create_recursive(void) {
-	pthread_mutex_t * mutex = (pthread_mutex_t *)rws_malloc_zero(sizeof(pthread_mutex_t));
-	int res = -1;
-	pthread_mutexattr_t attr;
+	aos_mutex_t mutex;
+    if (0 != aos_mutex_new(&mutex)) {
+        return NULL;
+    }
 
-	printf("%s %d\n", __func__, __LINE__);
-	if (pthread_mutexattr_init(&attr) == 0) {
-		printf("%s %d\n", __func__, __LINE__);
-		if (pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_RECURSIVE) == 0) {
-			printf("%s %d\n", __func__, __LINE__);
-			res = pthread_mutex_init(mutex, &attr);
-		}
-		printf("%s %d\n", __func__, __LINE__);
-		pthread_mutexattr_destroy(&attr);
-	}
-	printf("%s %d %d\n", __func__, __LINE__, res);
-	assert(res == 0);
-	return mutex;
+    return mutex.hdl;
 }
 
 void rws_mutex_lock(rws_mutex mutex) {
-	if (mutex) {
-		pthread_mutex_lock((pthread_mutex_t *)mutex);
-	}
+	if (NULL != mutex) {
+        aos_mutex_lock((aos_mutex_t *)&mutex, AOS_WAIT_FOREVER);
+    }
 }
 
 void rws_mutex_unlock(rws_mutex mutex) {
-	if (mutex) {
-		pthread_mutex_unlock((pthread_mutex_t *)mutex);
-	}
+	if (NULL != mutex) {
+        aos_mutex_unlock((aos_mutex_t *)&mutex);
+    }
 }
 
 void rws_mutex_delete(rws_mutex mutex) {
-	if (mutex) {
-		pthread_mutex_destroy((pthread_mutex_t *)mutex);
-		rws_free(mutex);
-	}
+    if (NULL != mutex) {
+        aos_mutex_free((aos_mutex_t *)&mutex);
+    }
 }
-
