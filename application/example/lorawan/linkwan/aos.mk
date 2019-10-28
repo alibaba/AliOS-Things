eac1b318ceadd4d6fa9d33e87f6d5a0b9bbd326e
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2019-10-28 10:43:26 +0800

    BugID:23180581: optimize for lorawan config and makefile
    Change-Id: I5cb8fc54be68eb35314418467f62dd86296c0907

--- application/example/lorawan/linkwan/aos.mk
+++ application/example/lorawan/linkwan/aos.mk
@@ -7,12 +7,3 @@ $(NAME)_SUMMARY := linkwan
 $(NAME)_SOURCES := main.c
 
 linkwan := 1
-
-# lorawanback defined at board/***/aos.mk
-ifeq ($(lorawanback), 0)
-$(NAME)_COMPONENTS := lorawan_4_4_2
-else ifeq ($(lorawanback), 1)
-$(NAME)_COMPONENTS := lorawan_4_4_0
-endif
-
-$(NAME)_COMPONENTS += kv
