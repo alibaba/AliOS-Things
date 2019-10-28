6d71e8099ecb136fdab65dbd49dbee6e734c74a0
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2019-10-28 10:45:02 +0800

    BugID:23180581: optimize for lorawan config and makefile
    Change-Id: Ie8f87ed8630475d573da61b14030434809279566

--- components/network/lorawan/lorawan_4_4_0/aos.mk
+++ components/network/lorawan/lorawan_4_4_0/aos.mk
@@ -6,16 +6,18 @@ $(NAME)_SUMMARY := LoRaWAN Protocal Stack Version 4.4.0
 
 $(NAME)_COMPONENTS += osal_aos
 
-$(NAME)_SOURCES := system/crypto/aes.c       \
-                   system/crypto/cmac.c      \
-                   system/timer.c            \
-                   system/delay.c            \
-                   system/gpio.c             \
-                   system/spi.c              \
-                   mac/region/Region.c       \
+$(NAME)_SOURCES  = mac/region/Region.c       \
                    mac/region/RegionCommon.c \
                    mac/LoRaMac.c             \
-                   mac/LoRaMacCrypto.c 
+                   mac/LoRaMacCrypto.c       \
+                   system/crypto/aes.c       \
+                   system/crypto/cmac.c      \
+
+ifneq ($(manufactory_lora_driver), 1)
+$(NAME)_SOURCES += system/timer.c            \
+                   system/delay.c            \
+                   system/gpio.c
+endif
 
 ifeq ($(LORACHIP), sx1276)
 $(NAME)_SOURCES += radio/sx1276/sx1276.c
@@ -29,7 +31,7 @@ $(NAME)_INCLUDES +=  .             \
                     mac/region    \
                     system
 
-linkwan?=0
+linkwan ?= 1
 ifeq ($(linkwan), 1)
 GLOBAL_DEFINES += CONFIG_LINKWAN CONFIG_DEBUG_LINKWAN     \
                   CONFIG_AOS_DISABLE_TICK CONFIG_NO_TCPIP \
@@ -48,14 +50,14 @@ $(NAME)_SOURCES += linkwan/linkwan_ica_at.c
 #$(NAME)_SOURCES += linkwan/linkwan_at.c
 endif
 else
-$(NAME)_SOURCES += lora/mac/region/RegionAS923.c    \
-                   lora/mac/region/RegionAU915.c    \
-                   lora/mac/region/RegionCN470.c    \
-                   lora/mac/region/RegionCN779.c    \
-                   lora/mac/region/RegionEU433.c    \
-                   lora/mac/region/RegionEU868.c    \
-                   lora/mac/region/RegionIN865.c    \
-                   lora/mac/region/RegionKR920.c    \
-                   lora/mac/region/RegionUS915.c    \
-                   lora/mac/region/RegionUS915-Hybrid.c
+$(NAME)_SOURCES += mac/region/RegionAS923.c    \
+                   mac/region/RegionAU915.c    \
+                   mac/region/RegionCN470.c    \
+                   mac/region/RegionCN779.c    \
+                   mac/region/RegionEU433.c    \
+                   mac/region/RegionEU868.c    \
+                   mac/region/RegionIN865.c    \
+                   mac/region/RegionKR920.c    \
+                   mac/region/RegionUS915.c    \
+                   mac/region/RegionUS915-Hybrid.c
 endif
