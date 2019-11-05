4afa0a4006c8b9f7ff988ec62ae193a6d71b5b8c
zhangtian <dongren.zt@alibaba-inc.com>
2019-11-05 17:08:35 +0800

    BugID:23325808:[coap] fix memory may be out of bounds issue & folder adjust
    Change-Id: I722c805e13705bc1abfe6821160570f7db22ae5a

---
+++ components/linkkit/wrappers/aos.mk
@@ -0,0 +1,54 @@
+NAME := libiot_wrappers
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 3.0.1
+$(NAME)_SUMMARY := HAL for AliOS Things
+
+$(NAME)_COMPONENTS += libiot_infra
+
+$(NAME)_SOURCES := os/HAL_OS_rhino.c \
+                   os/HAL_TCP_rhino.c \
+                   os/HAL_PRODUCT_rhino.c \
+
+$(NAME)_DEFINES +=  USE_YLOOP
+
+$(NAME)_SOURCES-$(FS_ENABLED) += os/HAL_FS_rhino.c
+
+$(NAME)_COMPONENTS += ulog activation chip_code netmgr
+
+GLOBAL_DEFINES += BUILD_AOS
+$(NAME)_DEFINES += USE_YLOOP
+
+ifeq (y,$(strip $(DEV_BIND_ENABLED)))
+$(NAME)_SOURCES += os/HAL_AWSS_rhino.c
+endif
+
+ifeq (y,$(strip $(COAP_SERVER)))
+$(NAME)_SOURCES += os/HAL_UDP_rhino.c
+endif
+
+ifeq (y,$(strip $(SUPPORT_TLS)))
+    ifeq (y,$(strip $(USE_ITLS)))
+        $(NAME)_COMPONENTS +=  itls id2 alicrypto  libiot_certs 
+        $(NAME)_SOURCES += os/HAL_TLS_itls.c
+    else
+        $(NAME)_DEFINES += USE_PUBLIC_MBEDTLS
+        $(NAME)_COMPONENTS +=  mbedtls libiot_certs
+        $(NAME)_SOURCES += os/HAL_TLS_mbedtls.c  os/dns.c
+    endif
+endif
+
+ifeq (y,$(strip $(COAP_COMM_ENABLED)))
+ifeq (y,$(strip $(COAP_DTLS_SUPPORT)))
+$(info COAP_SUPPORT_DTLS = y, so compile DTLS)
+$(NAME)_SOURCES += os/HAL_DTLS_mbedtls.c
+$(NAME)_COMPONENTS += mbedtls libiot_certs
+$(NAME)_DEFINES += USE_PUBLIC_MBEDTLS
+endif
+$(NAME)_SOURCES += os/HAL_UDP_rhino.c
+endif 
+
+ifeq (y,$(strip $(TLS_SAVE_TICKET)))
+$(NAME)_DEFINES += os/TLS_SAVE_TICKET
+endif
+
