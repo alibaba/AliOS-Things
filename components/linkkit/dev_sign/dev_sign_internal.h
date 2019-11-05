cc5a799c9365b796f70eba3dfb9ae1130566381f
zhangtian <dongren.zt@alibaba-inc.com>
2019-11-05 17:08:35 +0800

    BugID:23325808:[coap] fix memory may be out of bounds issue & folder adjust
    Change-Id: I722c805e13705bc1abfe6821160570f7db22ae5a

---
+++ components/linkkit/dev_sign/dev_sign_internal.h
@@ -0,0 +1,12 @@
+#ifndef _DEV_SIGN_INTERNAL_H_
+#define _DEV_SIGN_INTERNAL_H_
+
+#include <stdio.h>
+#include <string.h>
+#include "linkkit/infra/infra_config.h"
+#include "linkkit/infra/infra_defs.h"
+#include "linkkit/dev_sign_api.h"
+#include "linkkit/wrappers/wrappers.h"
+
+#endif
+
---
+++ components/linkkit/mqtt/mqtt_wrapper.h
@@ -0,0 +1,28 @@
+#include "linkkit/infra/infra_types.h"
+#include "linkkit/infra/infra_defs.h"
+#include "linkkit/wrappers/wrappers_defs.h"
+#include "linkkit/wrappers/wrappers.h"
+#include "linkkit/mqtt_api.h"
+
+/* mqtt protocol wrapper */
+void *wrapper_mqtt_init(iotx_mqtt_param_t *mqtt_params);
+int wrapper_mqtt_connect(void *client);
+int wrapper_mqtt_yield(void *client, int timeout_ms);
+int wrapper_mqtt_check_state(void *client);
+int wrapper_mqtt_subscribe(void *client,
+                           const char *topicFilter,
+                           iotx_mqtt_qos_t qos,
+                           iotx_mqtt_event_handle_func_fpt topic_handle_func,
+                           void *pcontext);
+int wrapper_mqtt_subscribe_sync(void *client,
+                                const char *topic_filter,
+                                iotx_mqtt_qos_t qos,
+                                iotx_mqtt_event_handle_func_fpt topic_handle_func,
+                                void *pcontext,
+                                int timeout_ms);
+int wrapper_mqtt_unsubscribe(void *client, const char *topicFilter);
+int wrapper_mqtt_publish(void *client, const char *topicName, iotx_mqtt_topic_info_pt topic_msg);
+int wrapper_mqtt_release(void **pclient);
+int wrapper_mqtt_nwk_event_handler(void *client, iotx_mqtt_nwk_event_t event, iotx_mqtt_nwk_param_t *param);
+
+
