e9285123bb5bcb6ff48bea255d279bf5063d5f04
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-11-13 16:52:42 +0800

    BugID:23028157:[blemesh]YTS test cases
    Change-Id: I4eb23e9c3776d57aa9a9f3d35a3067ca6890ffc5

--- test/testcase/network/ble/host/aos.mk
+++ test/testcase/network/ble/host/aos.mk
@@ -2,6 +2,6 @@ NAME := ble_test
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION    := 0.0.1
-$(NAME)_SUMMARY    := testcase for ble 
+$(NAME)_SUMMARY    := testcase for ble host
 
 $(NAME)_SOURCES += ble_test.c
