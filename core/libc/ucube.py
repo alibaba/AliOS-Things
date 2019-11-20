85d81617cf7e4b849c57f174fddc7aa55d2aca52
junping.pjp <junping.pjp@alibaba-inc.com>
2019-11-20 15:31:58 +0800

    BugID:23266431: AOS folder adjustment
    Change-Id: I8f828b523e1935c8938fee1c6dad099eed9cef9e

---
+++ core/libc/ucube.py
@@ -0,0 +1,32 @@
+src = []
+include_tmp = []
+
+if aos_global_config.compiler == 'armcc':
+    src = Split('''
+        compilers/armlibc/armcc_libc.c
+    ''')
+    include_tmp = Split('''
+        compilers/armlibc
+    ''')       
+elif aos_global_config.compiler == 'rvct':
+    src = Split('''
+        compilers/armlibc/armcc_libc.c
+    ''')
+    include_tmp = Split('''
+        compilers/armlibc
+    ''')
+elif aos_global_config.compiler == 'iar':
+    src = Split('''
+        compilers/iar/iar_libc.c
+    ''')
+    include_tmp = Split('''
+        compilers/iar
+    ''')  
+elif aos_global_config.board != 'linuxhost':
+    src = Split('''
+        newlib_stub.c
+    ''')
+
+component = aos_component('newlib_stub', src)
+for i in include_tmp:
+    component.add_global_includes(i)
