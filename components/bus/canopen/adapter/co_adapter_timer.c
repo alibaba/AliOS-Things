9b78be68a0a02a94c042f9cae7724c13a93d5a4d
junping.pjp <junping.pjp@alibaba-inc.com>
2019-11-20 15:31:29 +0800

    BugID:23266431: AOS folder adjustment
    Change-Id: Iecd57ebf9e43d5d9fc0e7a25886d0c8d71a91b33

---
+++ components/bus/canopen/adapter/co_adapter_timer.c
@@ -0,0 +1,73 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#ifdef AOS_CANOPEN
+
+#include "co_adapter.h"
+
+static timer_dev_t timer_canopen;
+
+void timer_dispatch(void);
+
+TIMEVAL last_counter_val = 0;
+TIMEVAL elapsed_time     = 0;
+
+int32_t timer_init(uint8_t port)
+{
+  int32_t ret = -1;
+
+  timer_canopen.port               = port;
+  timer_canopen.config.reload_mode = TIMER_RELOAD_AUTO;
+  timer_canopen.config.period      = MAX_COUNTER_VALUE;
+  timer_canopen.config.cb          = timer_dispatch;
+
+  ret = hal_timer_init(&timer_canopen);
+  if (ret != 0) {
+      LOGD("canopen", "timer init failed!\n");
+      return -1;
+  }
+
+  ret = hal_timer_start(&timer_canopen);
+  if (ret != 0) {
+      LOGD("canopen", "timer start failed!\n");
+      return -1;
+  }
+  return ret;
+}
+
+/* setTimer is a protocol library interface implement */
+void setTimer(TIMEVAL value)
+{
+    uint32_t timer = hal_timer_getcounter(&timer_canopen);
+
+    elapsed_time += timer - last_counter_val;
+
+    last_counter_val = MAX_COUNTER_VALUE-value;
+
+    hal_timer_setcounter(&timer_canopen, MAX_COUNTER_VALUE-value);
+}
+
+/* getElapsedTime is a protocol library interface implement */
+TIMEVAL getElapsedTime(void)
+{
+    uint32_t timer = hal_timer_getcounter(&timer_canopen);
+    if (timer < last_counter_val) {
+        timer += MAX_COUNTER_VALUE;
+    }
+    TIMEVAL elapsed = timer - last_counter_val + elapsed_time;
+
+    return elapsed;
+}
+
+void timer_dispatch(void)
+{
+    last_counter_val = 0;
+
+    elapsed_time = 0;
+
+    TimeDispatch();
+}
+
+#endif /* AOS_CANOPEN */
+
---
+++ components/bus/usb/usb_host/core/include/usb_host_types.h
@@ -0,0 +1,138 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef __USB_HOST_TYPES_H
+#define __USB_HOST_TYPES_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "usb_host_conf.h"
+#include "usb_host_os.h"
+#include "usb_host_debug.h"
+#include "usb_host_spec.h"
+#include "aos/hal/usbh.h"
+
+/*
+ * Return Value Assert
+ */
+#define RETURN_ASSERT(res, msg)            \
+    do {                                   \
+        if (res != USBH_OK) {              \
+            usb_err("%s : %d", msg, res);  \
+            return res;                    \
+        }                                  \
+    } while (0);
+
+#define MIN(a, b) ((a > b) ? b : a)
+#define LE16(addr) (((uint16_t)(*((uint8_t *)(addr)))) \
+                    + (((uint16_t)(*(((uint8_t *)(addr)) + 1))) << 8))
+
+/*
+ * USB Host Status
+ */
+#define USBH_OK     0
+#define USBH_FAIL   1
+
+/*
+ * USB URB Transfer Status
+ */
+#define USB_URB_IDLE        0
+#define USB_URB_DONE        1
+#define USB_URB_NOTREADY    2
+#define USB_URB_NYET        3
+#define USB_URB_ERROR       4
+#define USB_URB_STALL       5
+
+/*
+ * USB Transmition Token
+ */
+#define USBH_TOKEN_SETUP   0
+#define USBH_TOKEN_DATA    1
+
+#define USB_HOST_INUSE 1
+#define USB_HOST_UNUSE 0
+
+/* 
+ * Default Device Address
+ */
+#define USB_HOST_DEVICE_ADDRESS     1
+
+#define USB_HOST_WAIT_FOREVER ((uint32_t)-1)
+
+/*
+ * USB Host Pipe Managment Structure (Endpoint)
+ */
+typedef struct usb_host_pipe {
+    uint8_t inuse;
+    uint8_t index;
+    uint8_t ep_addr;
+    uint8_t dev_addr;
+    uint8_t ep_type; 
+    uint8_t speed;
+    uint16_t mps;
+} usbh_pipe_t;
+
+/*
+ * USB Interface Management Structure
+ */
+typedef struct usb_host_interface {
+    uint8_t inuse;
+    uint8_t intf_class;
+    uint8_t intf_subclass;
+    uint8_t intf_protocol;
+    usbh_pipe_t *pipe[USBH_MAX_PIPE_PER_INTERFACE];
+} usbh_intf_t;
+
+/*
+ * USB Device Management Structure
+ */
+typedef struct usb_host_device {
+    uint8_t attached;
+    uint8_t enumerated;
+    uint8_t nb_intf;
+    uint8_t dev_class;
+    uint8_t dev_subclass;
+    uint8_t dev_protocol;
+    uint8_t speed;
+    uint8_t address;
+    uint16_t vid;
+    uint16_t pid;
+    uint8_t data[USBH_MAX_DATA_BUFFER];
+    usbh_intf_t intf[USBH_MAX_NUM_INTERFACE];
+    void (*disconnect_cb)(void);
+} usbh_device_t;
+
+/*
+ * USB Host Management Handler
+ */
+typedef struct usb_host_handle {
+    uint8_t         inited;
+    usbh_device_t   device;
+    usbh_pipe_t     pipes[USBH_MAX_PIPE_NUM];
+    usbh_os_queue_t queue;
+    usbh_os_task_t  task;
+    usbh_os_lock_t  lock;
+    usbh_pipe_t*    control;
+    void*           hcd;
+} usb_host_t;
+
+/*
+ * Enumerate Helper Function Handler
+ */
+typedef struct enumerator_helper {
+    void (*set_vid_pid) (uint16_t vid, uint16_t pid);
+
+    int  (*parse_interface) (uint8_t intf_nb, uint8_t intf_class, 
+                             uint8_t intf_subclass, uint8_t intf_protocol);
+
+    int  (*parse_endpoint) (uint8_t intf_nb, uint8_t ep_type, uint8_t ep_addr);
+} enum_helper_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __USB_HOST_TYPES_H */
