f19d6a7ef717f1a8ef44cd427ee79730a79d294f
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2019-11-27 16:53:08 +0800

    BugID:23548285:add fun 04 and 06 supporting in modbus
    Change-Id: Icdb500594fa3c1c69c1871aa4de088662b3296ec

--- application/example/modbus_demo/modbus_demo.c
+++ application/example/modbus_demo/modbus_demo.c
@@ -15,7 +15,7 @@ void mb_main()
     uint8_t  len;
     int      status;
     uint16_t simulator1 = 0, simulator2 = 0;
-    uint32_t data_valid = 0;
+    uint16_t data_write = 0, data_resp = 0;
 
     mb_handler_t *mb_handler;
 
@@ -28,15 +28,42 @@ void mb_main()
     while (1) {
         memset(buf, 0, RECV_LEN_MAX);
 
-        status = aos_mbmaster_read_holding_reginster(mb_handler, DEVICE1_SLAVE_ADDR,
-                                                     DEVICE1_REG1_ADDR, 2, buf, &len);
+        status = aos_mbmaster_read_holding_register(mb_handler, DEVICE1_SLAVE_ADDR,
+                                                    DEVICE1_REG1_ADDR, 2, buf, &len);
         if (status == 0) {
             simulator1 = (uint16_t)((buf[0] << 8) | buf[1]);
             simulator2 = (uint16_t)((buf[2] << 8) | buf[3]);
-            printf("simulator1: %d,simulator2: %d\n", simulator1, simulator2);
+            printf("read holding register simulator1: %d,simulator2: %d\n", simulator1, simulator2);
         } else {
             printf("read holding register error\n");
         }
+
+        aos_msleep(1000);
+
+        status = aos_mbmaster_read_input_register(mb_handler, DEVICE1_SLAVE_ADDR,
+                                                  DEVICE1_REG1_ADDR, 2, buf, &len);
+        if (status == 0) {
+            simulator1 = (uint16_t)((buf[0] << 8) | buf[1]);
+            simulator2 = (uint16_t)((buf[2] << 8) | buf[3]);
+            printf("read input register simulator1: %d,simulator2: %d\n", simulator1, simulator2);
+        } else {
+            printf("read input register error\n");
+        }
+
+        aos_msleep(1000);
+
+        status = aos_mbmaster_write_single_register(mb_handler, DEVICE1_SLAVE_ADDR,
+                                                    DEVICE1_REG1_ADDR, data_write, NULL, &data_resp, NULL);
+        if (status == 0) {
+            if (data_write != data_resp) {
+                printf("write single register error\r\n");
+            } else {
+                printf("write single register ok\r\n");
+            }
+        } else {
+            printf("write single register error\n");
+        }
+        data_write++;
 
         aos_msleep(1000);
     }
