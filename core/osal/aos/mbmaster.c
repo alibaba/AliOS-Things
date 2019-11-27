d2f1d2277022b8eba93a0629625276c9aea5f1ce
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2019-11-27 16:53:40 +0800

    BugID:23548285:add fun 04 and 06 supporting in modbus
    Change-Id: Id131ef4ec453f7059e6f8bd3236a0588a5066768

--- core/osal/aos/mbmaster.c
+++ core/osal/aos/mbmaster.c
@@ -38,10 +38,9 @@ int aos_mbmaster_rtu_uninit(mb_handler_t *req_handler)
 }
 #endif /* MBMASTER_CONFIG_RTU_ENABLED */
 
-#if (MBMASTER_CONFIG_READ_HOLDING > 0)
-int aos_mbmaster_read_holding_reginster(mb_handler_t *req_handler, uint8_t slave_addr,
-                                        uint16_t start_addr, uint16_t quantity, uint8_t *respond_buf,
-                                        uint8_t *respond_count)
+int aos_mbmaster_read_holding_register(mb_handler_t *req_handler, uint8_t slave_addr,
+                                       uint16_t start_addr, uint16_t quantity, uint8_t *respond_buf,
+                                       uint8_t *respond_count)
 {
     mb_status_t status;
 
@@ -49,7 +48,7 @@ int aos_mbmaster_read_holding_reginster(mb_handler_t *req_handler, uint8_t slave
         return -1;
     }
 
-    status = mbmaster_read_holding_reginster(req_handler, slave_addr, start_addr,
+    status = mbmaster_read_holding_register(req_handler, slave_addr, start_addr,
                                                  quantity, respond_buf, respond_count);
     if (status != MB_SUCCESS) {
         printf("%s\n", status_to_string(status));
@@ -58,4 +57,44 @@ int aos_mbmaster_read_holding_reginster(mb_handler_t *req_handler, uint8_t slave
 
     return 0;
 }
-#endif /* MBMASTER_CONFIG_READ_HOLDING */
+
+int aos_mbmaster_read_input_register(mb_handler_t *req_handler, uint8_t slave_addr,
+                                     uint16_t start_addr, uint16_t quantity, uint8_t *respond_buf,
+                                     uint8_t *respond_count)
+{
+    mb_status_t status;
+
+    if (req_handler == NULL) {
+        return -1;
+    }
+
+    status = mbmaster_read_input_register(req_handler, slave_addr, start_addr,
+                                          quantity, respond_buf, respond_count);
+    if (status != MB_SUCCESS) {
+        printf("%s\n", status_to_string(status));
+        return -1;
+    }
+
+    return 0;
+}
+
+int aos_mbmaster_write_single_register(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t register_addr,
+                                       uint16_t register_value, uint16_t *resp_addr, uint16_t *resp_value,
+                                       uint8_t *exception_code)
+{
+    mb_status_t status;
+
+    if (req_handler == NULL) {
+        return -1;
+    }
+
+    status = mbmaster_write_single_register(req_handler, slave_addr, register_addr, register_value,
+                                            resp_addr, resp_value, exception_code);
+    if (status != MB_SUCCESS) {
+        printf("%s\n", status_to_string(status));
+        return -1;
+    }
+
+    return 0;
+}
+
