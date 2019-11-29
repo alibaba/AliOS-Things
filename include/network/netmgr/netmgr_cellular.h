dcf5a2084f3c40c00eefac12b32235be6d1d93bd
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-11-29 12:20:44 +0800

    BugID:23266691: refine netmgr header file
    Change-Id: Ifa8e198ed27da5974bd0ad6c8fcb82f60da3480e

--- include/network/netmgr/netmgr_cellular.h
+++ include/network/netmgr/netmgr_cellular.h
@@ -13,10 +13,40 @@ extern "C"
 {
 #endif
 
+/**
+ *
+ * @brief Initialize netmgr cellular module
+ *
+ * @return 0   initialize success
+ * @return < 0 initialize fail
+ */
 int netmgr_cellular_init(void);
+
+/**
+ *
+ * @brief Deinitialize netmgr cellular module
+ */
 void netmgr_cellular_deinit(void);
+
+/**
+ *
+ * @brief Start netmgr cellular
+ *
+ * @param[in] autoconfig start cellular provision or not
+ *
+ * @return 0   start network interface success
+ * @return < 0 start network interface fail
+ */
 int netmgr_cellular_start(bool autoconfig);
 
+/**
+ *
+ * @brief Get netmgr ip stats
+ *
+ * @return true   ip ready
+ * @return false  ip not ready yet
+ *
+ */
 bool netmgr_get_ip_state(void);
 
 #if defined(__cplusplus)
