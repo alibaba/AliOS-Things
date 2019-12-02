962e0c766334b43af1cc03bdf85e1129a56f4540
wanglu.luwang <wanglu.luwang@alibaba-inc.com>
2019-12-02 17:06:03 +0800

    BugID:23722452:Remove yts' dependency on dda
    Change-Id: Ib649de7150c6b0d4624a2275150baa22716b4fe8

--- application/example/meshapp/ucube.py
+++ application/example/meshapp/ucube.py
@@ -1,10 +1,10 @@
-src =Split(''' 
+src =Split('''
     main.c
 ''')
 component =aos_component('meshapp', src)
-aos_global_config.set('no_with_lwip', 0) 
+aos_global_config.set('no_with_lwip', 0)
 
-dependencis =Split(''' 
+dependencis =Split('''
     network/umesh
     kernel/cli
     network/netmgr
@@ -13,7 +13,7 @@ for i in dependencis:
     component.add_comp_deps(i)
 
 
-global_macros =Split(''' 
+global_macros =Split('''
     TAPIF_DEFAULT_OFF
     DEBUG
 ''')
@@ -21,35 +21,25 @@ for i in global_macros:
     component.add_global_macros(i)
 
 if aos_global_config.get('MESHAUTH') == 1:
-    dependencis=Split(''' 
+    dependencis=Split('''
         utility/base64
         security/alicrypto
     ''')
-    component.add_comp_deps(*dependencis) 
+    component.add_comp_deps(*dependencis)
 
 if aos_global_config.get("ipv6") == 0:
     component.add_global_macros("LWIP_IPV6=0")
 
 @post_config
 def meshapp_post_config(component):
-    
+
     comp_names = [comp.name for comp in aos_global_config.components]
-    if 'linuxhost' in comp_names:
-        if aos_global_config.get("DDA") == None:
-            aos_global_config.set("DDA",1)
-    
-    if 'armhflinux' in comp_names:
-        if aos_global_config.get("DDA") == None:
-            aos_global_config.set("DDA",1)
-            
-    if aos_global_config.get("DDA") == 1:        
-        component.add_comp_deps("tools/dda")
-    
+
     if "EMW3060" == aos_global_config.get("MODULE"):
-        component.add_global_macros("MESHAPP_LIGHT_ENABLED") 
-    
+        component.add_global_macros("MESHAPP_LIGHT_ENABLED")
+
     if aos_global_config.get("mesh_hal_test") == 1:
-        component.add_global_macros("MESH_HAL_TEST_ENABLED") 
+        component.add_global_macros("MESH_HAL_TEST_ENABLED")
 
 meshapp_post_config(component)
 
