bcfb8bf16124ebb46efd9a85e9e15b949f220810
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2019-12-02 14:34:59 +0800

    BugID:23702582:output information with ulog
    Change-Id: I4963e1dffd51b6ddc52f194867fe503fba394590

--- components/bus/mbmaster/include/mbmaster_api.h
+++ components/bus/mbmaster/include/mbmaster_api.h
@@ -81,14 +81,17 @@ mb_status_t mbmaster_rtu_init(mb_handler_t **handler, uint8_t port, uint32_t bau
 
 mb_status_t mbmaster_rtu_uninit(mb_handler_t *req_handler);
 
+/* function code 0x03 */
 mb_status_t mbmaster_read_holding_register(mb_handler_t *req_handler, uint8_t slave_addr,
                                            uint16_t start_addr, uint16_t quantity,
                                            uint8_t *respond_buf, uint8_t *respond_count);
 
+/* function code 0x04 */
 mb_status_t mbmaster_read_input_register(mb_handler_t *req_handler, uint8_t slave_addr,
                                          uint16_t start_addr, uint16_t quantity,
                                          uint8_t *respond_buf, uint8_t *respond_count);
 
+/* function code 0x06 */
 mb_status_t mbmaster_write_single_register(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t register_addr,
                                            uint16_t register_value, uint16_t *resp_addr, uint16_t *resp_value, uint8_t *exception_code);
 
