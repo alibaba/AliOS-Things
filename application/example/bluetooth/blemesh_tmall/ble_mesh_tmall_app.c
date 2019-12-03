b017500f273bc7cf57138fc04c4bc02da33ba755
xuguang.lxg <xuguang.lxg@alibaba-inc.com>
2019-12-03 17:14:49 +0800

    BugID:23028157:tmall example default configures.
    Change-Id: Ibdfd12412677a8839938fb4a1d9784a494d079b5

--- application/example/bluetooth/blemesh_tmall/ble_mesh_tmall_app.c
+++ application/example/bluetooth/blemesh_tmall/ble_mesh_tmall_app.c
@@ -13,7 +13,7 @@
 #include <aos/hal/gpio.h>
 #include "tmall_model.h"
 
-// use LED1 on nrf52832 pca10040 dev board for demo
+/* use LED1 on nrf52832 pca10040 dev board for demo */
 #define LED1_PIN 17
 
 static gpio_dev_t gpio_led;
@@ -51,8 +51,9 @@ static const uint8_t pid[4] = { 0x00, 0x00, 0x00, 0x02 };
 static const uint8_t mac_addr[6] = { 0x78, 0xda, 0x07, 0x11, 0x27, 0xff };
 static const char secret[] = "9c98d67f6bd3be48d36bd8ed29a95bab";
 #endif
-static const char pkey[] = "b1HaDlKgwoV"; // product key
 
+/* product key */
+static const char pkey[] = "b1HaDlKgwoV"; 
 static uint8_t dev_uuid[16] = { 0x00 };
 static uint8_t digest[32] = { 0x00 };
 
@@ -62,34 +63,34 @@ static void blemesh_tmall_profile(void)
 
     printk("Initializing tmall ble mesh profile...\n");
 
-    // calculate digest of static oob info
+    /* calculate digest of static oob info */
     bt_mesh_profile_calculate_digest(digest, pid, mac_addr, secret);
     printk("digest: %s\n", bt_hex(digest, 16));
 
-    // construct uuid broadcasted in unprovisioned beacon
+    /* construct uuid broadcasted in unprovisioned beacon */
     bt_mesh_profile_construct_uuid(dev_uuid, pid, mac_addr);
     printk("dev_uuid: %s\n", bt_hex(dev_uuid, 16));
 
-    // configure mesh profile prov
+    /* configure mesh profile prov */
     ret = bt_mesh_profile_prov_init(dev_uuid, digest, 16,
                                     prov_complete, prov_reset);
     if (ret != 0) {
         printk("fail to initialize mesh profile provisioning capability\n");
     }
 
-    // configure mesh profile composition data page 0
+    /* configure mesh profile composition data page 0 */
     ret = bt_mesh_profile_composition_data_init();
     if (ret != 0) {
         printk("fail to configure composistion data\n");
     }
 
-    // configure mesh profile supported elements and models
+    /* configure mesh profile supported elements and models */
     ret = bt_mesh_profile_model_init();
     if (ret != 0) {
         printk("fail to initialize mesh profile supported elements and models\n");
     }
 
-    // start mesh profile
+    /* start mesh profile */
     ret = bt_mesh_profile_start();
     if (ret != 0) {
         printk("fail to initialize mesh profile\n");
@@ -98,13 +99,13 @@ static void blemesh_tmall_profile(void)
 
 static void app_delayed_action(void *arg)
 {
-    // int LED1
+    /* int LED1 */
     gpio_led.port = LED1_PIN;
     gpio_led.config = OUTPUT_PUSH_PULL;
     hal_gpio_init(&gpio_led);
     hal_gpio_output_high(&gpio_led);
 
-    // init tmall ble mesh profile
+    /* init tmall ble mesh profile */
     blemesh_tmall_profile();
 }
 
@@ -151,11 +152,8 @@ static void handle_set_mac(char *pwbuf, int blen, int argc, char **argv)
             printf("Invalid format, MAC not set!!!\r\n");
             return;
         }
-
         mac[i / 3] = ((char2u8(p) & 0x0f) << 4) | (char2u8(p+1) & 0x0f);
     }
-
-    //ais_set_mac(mac);
 }
 #endif
 
