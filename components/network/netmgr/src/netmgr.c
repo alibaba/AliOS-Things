45d567fe19bb9189dd753152229789e85a29eca5
wanglu.luwang <wanglu.luwang@alibaba-inc.com>
2019-12-05 16:55:28 +0800

    BugID:23548227:Add multiple network interfaces support
    Change-Id: Iccef92841a05e3893d71a9653de4095be2cb4ae0

--- components/network/netmgr/src/netmgr.c
+++ components/network/netmgr/src/netmgr.c
@@ -5,44 +5,116 @@
 #include <stdlib.h>
 #include <string.h>
 #include <netmgr.h>
+#include "hal/net.h"
 #include "aos/kernel.h"
+#include "netmgr_priv.h"
+
+static net_interface_t g_net_interfaces[INTERFACE_INDEX_MAX];
 
 int netmgr_init(void)
 {
+    int ret = -1;
+
 #ifdef NET_WITH_WIFI
-    return netmgr_wifi_init();
-#elif defined(NET_WITH_CELLULAR)
-    return netmgr_cellular_init();
+    ret = netmgr_wifi_init();
+    if (ret < 0) {
+        return ret;
+    }
 #endif
+
+#ifdef NET_WITH_CELLULAR
+    ret = netmgr_cellular_init();
+    if (ret < 0) {
+        return ret;
+    }
+#endif
+
+    return ret;
 }
 
 void netmgr_deinit(void)
 {
 #ifdef NET_WITH_WIFI
     netmgr_wifi_deinit();
-#elif defined(NET_WITH_CELLULAR)
+#endif
+
+#ifdef NET_WITH_CELLULAR
     netmgr_cellular_deinit();
 #endif
 }
 
 int netmgr_start(bool autoconfig)
 {
+    int ret = -1;
+
 #ifdef NET_WITH_WIFI
-    return netmgr_wifi_start(autoconfig);
-#elif defined(NET_WITH_CELLULAR)
-    return netmgr_cellular_start(autoconfig);
+    ret = netmgr_wifi_start(autoconfig);
+    if (ret < 0) {
+        return ret;
+    }
 #endif
+
+#ifdef NET_WITH_CELLULAR
+    ret = netmgr_cellular_start();
+    if (ret < 0) {
+        return ret;
+    }
+#endif
+
+    return ret;
 }
 
-void netmgr_stats(int32_t interface, netmgr_stats_t *stats)
+int netmgr_stats(int32_t interface, netmgr_stats_t *stats)
 {
     if (stats == NULL) {
-        return;
+        return -1;
+    }
+
+    if (interface < 0 || interface >= INTERFACE_INDEX_MAX) {
+        return -1;
     }
 
     memset(stats, 0, sizeof(netmgr_stats_t));
-    stats->ip_available = netmgr_get_ip_state();
-    netmgr_wifi_get_ip(stats->ip);
+    switch (g_net_interfaces[interface].interface_type) {
+        case INTERFACE_WIFI:
+#ifdef NET_WITH_WIFI
+            stats->ip_available = netmgr_wifi_get_ip_state();
+            netmgr_wifi_get_ip(stats->ip);
+#endif
+            break;
+        case INTERFACE_CELLULAR:
+#ifdef NET_WITH_CELLULAR
+            stats->ip_available = netmgr_cellular_get_ip_state();
+            netmgr_cellular_get_ip(stats->ip);
+#endif
+            break;
+        default:
+            return -1;
+    }
+
+    return 0;
+}
+
+net_interface_t *netmgr_get_net_interface(uint8_t interface_type)
+{
+    net_interface_t *interface = NULL;
+
+    switch (interface_type) {
+        case INTERFACE_WIFI:
+#ifdef NET_WITH_WIFI
+            interface = &g_net_interfaces[INTERFACE_WIFI];
+#endif
+            break;
+        case INTERFACE_CELLULAR:
+#ifdef NET_WITH_CELLULAR
+            interface = &g_net_interfaces[INTERFACE_CELLULAR];
+#endif
+            break;
+        default:
+            break;
+    }
+
+    return interface;
 }
 
 #ifdef NET_WITH_WIFI
@@ -63,7 +135,7 @@ int32_t netmgr_connect(const char *ssid, const uint8_t *password, uint32_t timeo
     netmgr_reconnect_wifi();
 
     while (time_elapsed < timeout) {
-        if (netmgr_get_ip_state() == false) {
+        if (netmgr_wifi_get_ip_state() == false) {
             aos_msleep(500);
             time_elapsed += 500;
         } else {
