310b1a2c16f44a912c6fbecfde4e4dc0433585b0
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-12-09 15:49:15 +0800

    BugID:23881073: add httpdns main body
    Change-Id: I06cabb7eb83945dc591d29c2dbe3854e1f07dc38

---
+++ components/network/httpdns/include/httpdns_opts.h
@@ -0,0 +1,21 @@
+#ifndef _HTTP_DNS_OPT_H_
+#define _HTTP_DNS_OPT_H_
+
+#ifndef HTTP_DNS_DISABLED
+#define HTTP_DNS_DISABLED 0
+#endif
+
+#ifndef HTTP_DNS_USER_ID
+#define HTTP_DNS_USER_ID "189323"
+#endif
+
+const char server_list[ ][16] = {
+    "203.107.1.1",
+    "203.107.1.33",
+    "203.107.1.34",
+    "203.107.1.35",
+    "203.107.1.65",
+    "203.107.1.66"
+};
+
+#endif
