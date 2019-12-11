d7269f9c04fa03889cf47bf2fdf28b5eac11068b
zhuqing.zq <zhuqing.zq@alibaba-inc.com>
2019-12-11 11:08:50 +0800

    BugID:23956577: board xr809 and xr871 flash partition conflict;board
    Change-Id: Ie257daf66c376c6c9ee90f669d6abb2376ab5816

--- platform/board/xr871evb/config/partition_conf.c
+++ platform/board/xr871evb/config/partition_conf.c
@@ -30,7 +30,7 @@ const hal_logic_partition_t hal_partitions[] =
     {
         .partition_owner          = HAL_FLASH_EMBEDDED,
         .partition_description    = "PARAMETER2",
-        .partition_start_addr     = SPI_FLASH_CAPACITY - 2 * 0x1000,
+        .partition_start_addr     = SPI_FLASH_CAPACITY - 3 * 0x1000,
         .partition_length         = 0x2000, //8k bytes
         .partition_options        = PAR_OPT_READ_EN | PAR_OPT_WRITE_EN,
     },
