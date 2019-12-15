46d516b29110d08cdead9beb9602ab1c9bea5435
wanglu.luwang <wanglu.luwang@alibaba-inc.com>
2019-12-15 17:59:05 +0800

    BugID:23856156:Add umesh APIs' comments
    Change-Id: Ifabcf8e4b2052bec04b4dddb037ba9422b36982e

--- include/network/umesh/umesh_config.h
+++ include/network/umesh/umesh_config.h
@@ -1,36 +1,47 @@
-/*
- * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+/*!
+ * @filename umesh_config.h
+ * This is uMesh config header file
+ *
+ * @copyright  Copyright (C) 2015-2017 Alibaba Group Holding Limited
  */
 
 #ifndef UMESH_CONFIG_H
 #define UMESH_CONFIG_H
 
-#define MAX_NEIGHBORS_NUM 32
+/** @addtogroup aos_umesh uMesh
+ *  AliOS Things mesh networks
+ *
+ *  @{
+ */
 
-#define ATTACH_REQUEST_RETRY_TIMES 2
-#define ATTACH_REQUEST_INTERVAL 1000
+#define MAX_NEIGHBORS_NUM 32      /** < max number of neighbors */
+
+#define ATTACH_REQUEST_RETRY_TIMES 2  /** < attach request retry times */
+#define ATTACH_REQUEST_INTERVAL 1000  /** < attach request interval */
 
 #ifdef CONFIG_AOS_MESH_BCAST
-#define KEEP_ALIVE_COUNT 3               /* maximum retransmission count of keep alive msg */
-#define KEEP_ALIVE_SHORT_TIMEOUT 3000    /* keep alive short interval in case that leader exists */
-#define KEEP_ALIVE_LONG_TIMEOUT 10000    /* keep alive long interval during leader recover */
+#define KEEP_ALIVE_COUNT 3               /** < maximum retransmission count of keep alive msg */
+#define KEEP_ALIVE_SHORT_TIMEOUT 3000    /** < keep alive short interval in case that leader exists */
+#define KEEP_ALIVE_LONG_TIMEOUT 10000    /** < keep alive long interval during leader recover */
 
-#define LEADER_RECOVER_COUNT 5           /* double check if leader has recovered really*/
-#define LEADER_RECOVER_TIMEOUT 2000      /* leader recover timer timeout */
+#define LEADER_RECOVER_COUNT 5           /** < double check if leader has recovered really*/
+#define LEADER_RECOVER_TIMEOUT 2000      /** < leader recover timer timeout */
 #endif
 
 #ifdef CONFIG_AOS_MESH_LOWPOWER
-#define SCHEDULE_SLOTS_SIZE 3
-#define SCHEDULE_SLOT_INTERVAL 6000
+#define SCHEDULE_SLOTS_SIZE 3           /** < lowpower's schedule slots size */
+#define SCHEDULE_SLOT_INTERVAL 6000     /** < lowpower's schedule slot time interval */
 #endif
 
 #ifdef CONFIG_AOS_MESH_AUTH
-#define AUTH_REQUEST_RETRY_TIMES 2
-#define AUTH_RELAY_RETRY_TIMES 2
+#define AUTH_REQUEST_RETRY_TIMES 2      /** < auth request retry times */
+#define AUTH_RELAY_RETRY_TIMES 2        /** < auth relay retry times */
 
-#define ID2_LISTEN_PORT 1234
-#define ID2_SERVER_PORT 2345
-#define ID2_SERVER_ADDR "192.168.16.188" /* testbed sp server ip */
+#define ID2_LISTEN_PORT 1234              /** < ID2 listen port number */
+#define ID2_SERVER_PORT 2345              /** < ID2 server port number */
+#define ID2_SERVER_ADDR "192.168.16.188"  /** < testbed sp server ip */
 #endif
+
+/** @} */
 
 #endif
