6f9762e76122ebf98917aaafefacc0e9d6f00b41
wanglu.luwang <wanglu.luwang@alibaba-inc.com>
2019-12-16 17:33:35 +0800

    BugID:24039335:Clean up mesh hal test from meshapp
    Change-Id: I5ee0bb6a9318cde8a4ee354bc160eb393855eadd

--- application/example/meshapp/main.c
+++ application/example/meshapp/main.c
@@ -18,9 +18,6 @@
 #endif
 
 #include <umesh.h>
-#ifdef MESHAPP_CONFIG_HAL_TEST
-#include "umesh_hal.h"
-#endif
 
 #ifdef MESHAPP_LIGHT_ENABLED
 static gpio_dev_t gpio_led;
@@ -82,61 +79,11 @@ static void subscribed_event_cb(input_event_t *eventinfo, void *priv_data)
 }
 #endif
 
-#ifdef MESH_HAL_TEST_ENABLED
-static uint16_t g_interval = 1000;  /* ms */
-static mac_address_t g_dest = {.addr = {0xff, 0xff, 0xff, 0xff, 0xff, 0xff}, .len = 0};
-//static mac_address_t g_dest = {.addr = {0xfc, 0xee, 0xe6, 0x05, 0x72, 0xbd}, .len = 8};
-
-static void handle_received_frame(void *context, frame_t *frame,
-                                  frame_info_t *frame_info,
-                                  int error)
-{
-    mac_address_t *addr = &frame_info->peer;
-
-    printf("Rx data from: %02x:%02x:%02x:%02x:%02x:%02x\r\n",
-           addr->addr[0], addr->addr[1], addr->addr[2], addr->addr[3], addr->addr[4], addr->addr[5]);
-}
-
-static void handle_sent(void *context, frame_t *frame, int error);
-static void send_data_task(void *arg)
-{
-    frame_t frame;
-    frame.len = 0;
-
-    if (g_dest.len == 8) {
-        hal_umesh_send_ucast_request(NULL, &frame, &g_dest, handle_sent, NULL);
-    } else {
-        hal_umesh_send_bcast_request(NULL, &frame, handle_sent, NULL);
-    }
-}
-
-static void handle_sent(void *context, frame_t *frame, int error)
-{
-    if (g_dest.len == 8) {
-        printf("Tx to: %02x:%02x:%02x:%02x:%02x:%02x %s\r\n",
-               g_dest.addr[0], g_dest.addr[1], g_dest.addr[2], \
-               g_dest.addr[3], g_dest.addr[4], g_dest.addr[5], \
-               error == 0? "success": "fail");
-    } else {
-        printf("Tx to: 0xff:0xff:0xff:0xff:0xff:0xff %s\r\n", error == 0? "success": "fail");
-    }
-    aos_post_delayed_action(g_interval, send_data_task, NULL);
-}
-#endif
-
 static void app_delayed_action(void *arg)
 {
-#ifndef MESH_HAL_TEST_ENABLED
 #ifdef AOS_NETMGR
     netmgr_init();
     netmgr_start(false);
-#endif
-#else
-    hal_umesh_init();
-    hal_umesh_enable(NULL);
-    hal_umesh_register_receiver(NULL, handle_received_frame, NULL);
-    hal_umesh_set_channel(NULL, 6);
-    aos_post_delayed_action(g_interval, send_data_task, NULL);
 #endif
 }
 
