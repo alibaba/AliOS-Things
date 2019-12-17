dd5fc68dd3699d3abfa1f7c958f97d91cb40e6c2
wanglu.luwang <wanglu.luwang@alibaba-inc.com>
2019-12-17 11:59:35 +0800

    BugID:24039335:Move internal used uMesh APIs to components folder
    Change-Id: I9fea3dc6ed91abf2ecf2262214eb0b39690d46a1

--- include/network/umesh/umesh.h
+++ include/network/umesh/umesh.h
@@ -12,8 +12,9 @@
 extern "C" {
 #endif
 
+#include <stdint.h>
+#include <stdbool.h>
 #include "umesh_config.h"
-#include "umesh_types.h"
 
 /** @addtogroup aos_umesh uMesh
  *  AliOS Things mesh networks
@@ -22,28 +23,36 @@ extern "C" {
  */
 
 /**
- * Tx data via using sid as dest address
- *
- * @param[in] buf   pointer to tx buffer
- * @param[in] netid network id
- * @param[in] sid   dest sid address
- *
- * @return success or not
+ * mesh error number
  */
-ur_error_t umesh_output_sid(struct pbuf *buf, uint16_t netid, uint16_t sid);
+typedef enum ur_error_s {
+    UR_ERROR_NONE          = 0,   /** < success */
+    UR_ERROR_FAIL          = 1,   /** < fail */
+    UR_ERROR_BUSY          = 2,   /** < error busy */
+    UR_ERROR_DROP          = 3,   /** < error drop */
+    UR_ERROR_MEM           = 4,   /** < error mem */
+    UR_ERROR_ROUTE         = 5,   /** < error route */
+    UR_ERROR_PARSE         = 6,   /** < error parse */
+    UR_ERROR_ADDRESS_QUERY = 7,   /** < error address query */
+    UR_ERROR_BUFFER        = 8,   /** < error buffer */
+    UR_ERROR_INVALID_PARM  = 9,   /** < error invalid paramater */
+} ur_error_t;
 
 /**
- * Tx data via using uuid as dest address
- *
- * @param[in] buf  pointer to tx buffer
- * @param[in] uuid dest uuid address
- *
- * @return success or not
+ * node modes
  */
-ur_error_t umesh_output_uuid(struct pbuf *buf, uint8_t *uuid);
+typedef enum node_mode_s {
+    MODE_NONE     = 0x00,   /** < this is only for testing */
+    MODE_MOBILE   = 0x01,   /** < mobile node mode */
+    MODE_LOW_MASK = 0x0f,   /** < mode low mask */
+    MODE_RX_ON    = 0x10,   /** < normal node mode */
+    MODE_SUPER    = 0x20,   /** < super node mode */
+    MODE_LEADER   = 0x40,   /** < leader node mode */
+    MODE_HI_MASK  = 0xf0,   /** < mode high mask */
+} node_mode_t;
 
 /**
- * initialize uMesh
+ * initialize umesh
  *
  * @param[in] mode mesh node mode
  *
@@ -82,48 +91,6 @@ bool umesh_is_initialized(void);
 ur_error_t umesh_stop(void);
 
 /**
- * Register uMesh state callbacks
- *
- * @param[in] callback uMesh state callbacks
- *
- * @return success or not
- */
-ur_error_t umesh_register_callback(ur_adapter_callback_t *callback);
-
-#ifdef CONFIG_AOS_MESH_BCAST
-/* provide broadcast transmit capability within the scope of overall
- * mesh network in the case that leader has been offline.
- */
-typedef void (* umesh_bcast_recv_t)(uint8_t *buf, uint32_t len);
-
-/**
- * Register uMesh broadcast callbacks
- *
- * @param[in] callback uMesh broadcast callbacks
- *
- * @return success or not
- */
-ur_error_t umesh_register_bcast_callback(umesh_bcast_recv_t callback);
-
-/**
- * Register uMesh broadcast callbacks
- *
- * @param[in] buf pointer to tx buf
- * @param[in] len buffer length
- *
- * @return success or not
- */
-ur_error_t umesh_bcast_send(uint8_t *buf, uint32_t len);
-#endif
-
-/**
- * Get device state
- *
- * @return device statet
- */
-uint8_t umesh_get_device_state(void);
-
-/**
  * Get device mode
  *
  * @return device mode
@@ -137,86 +104,20 @@ uint8_t umesh_get_mode(void);
  *
  * @return success or not
  */
-ur_error_t umesh_set_mode(uint8_t mode);
+ur_error_t umesh_set_mode(node_mode_t mode);
 
 /**
- * Get mac address
- *
- * @param[in] type network media type
- *
- * @return device MAC address
+ * mesh events code
  */
-const mac_address_t *umesh_get_mac_address(media_type_t type);
-
-/**
- * Get meshnet ID
- *
- * @return meshnet ID
- */
-uint16_t umesh_get_meshnetid(void);
-
-/**
- * Get sid address
- *
- * @return sid address
- */
-uint16_t umesh_get_sid(void);
-
-/**
- * get mesh network size
- *
- * @return mesh network size
- */
-uint16_t umesh_get_meshnetsize(void);
-
-/**
- * get mesh network neighbors
- *
- * @param[in] type network media type
- *
- * @return mesh network neighbors
- */
-slist_t *umesh_get_nbrs(media_type_t type);
-
-/**
- * Check whether is the multicast address subscribed
- *
- * @param[in] addr multicast address
- *
- * @return true indicates mcast addr subscribed, false indicates not subscribed
- *
- */
-bool umesh_is_mcast_subscribed(const ur_ip6_addr_t *addr);
-
-/**
- * Check whether is the multicast address subscribed
- *
- * @param[in] addr multicast address
- *
- */
-void umesh_get_extnetid(umesh_extnetid_t *extnetid);
-
-/**
- * Set extension network id
- *
- * @param[in] extnetid extension network id
- *
- * @return success or not
- */
-ur_error_t umesh_set_extnetid(const umesh_extnetid_t *extnetid);
-
-typedef void (*cmd_cb_t)(char *buf, int len, void *priv);
-
-/**
- * uMesh CLI command proc
- *
- * @param[in] buf   pointer to command buffer
- * @param[in] len   buffer len
- * @param[in] cb    cmd related callback
- * @param[in] priv  pointer to private data
- *
- */
-void umesh_cli_cmd(char *buf, int len, cmd_cb_t cb, void *priv);
+#define CODE_MESH_STARTED        1    /** < mesh started event */
+#define CODE_MESH_ATTACHED       2    /** < mesh attached event */
+#define CODE_MESH_DETACHED       3    /** < mesh detached event */
+#define CODE_MESH_CONNECTED      4    /** < mesh connected event */
+#define CODE_MESH_DISCONNECTED   5    /** < mesh disconnected event */
+#define CODE_MESH_DATA_RECV      6    /** < mesh data recv event */
+#define CODE_MESH_PSCHED_UP      7    /** < mesh lowpower parent schedule up event */
+#define CODE_MESH_ASCHED_UP      8    /** < mesh lowpower schedule up event */
+#define CODE_MESH_SCHED_DOWN     9    /** < mesh lowpower schedule down event */
 
 /** @} */
 
