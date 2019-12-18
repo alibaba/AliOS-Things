1be0a31fdcdfc27658adfb87270ff2e0f496db7e
yajun.syj <yajun.syj@alibaba-inc.com>
2019-12-18 15:44:10 +0800

    BugID:23251492: Add httpapp command
    Change-Id: I102802155709e6a33634050afff1803338f8f32b

--- application/example/httpapp/aos.mk
+++ application/example/httpapp/aos.mk
@@ -2,9 +2,11 @@ NAME := httpapp
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.1
-$(NAME)_SUMMARY := HTTP Client app
-$(NAME)_SOURCES := httpapp.c
+$(NAME)_SUMMARY := HTTP client app
 
-$(NAME)_COMPONENTS := yloop cli netmgr http mbedtls
+$(NAME)_SOURCES := httpapp.c
+$(NAME)_COMPONENTS := http netmgr cli
 
 GLOBAL_DEFINES += BUILD_AOS
+
+CONFIG_HTTP_SECURE=1
