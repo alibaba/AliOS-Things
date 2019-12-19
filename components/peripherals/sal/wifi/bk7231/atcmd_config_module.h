cf5b86b9b115d8594841efb9001e23f3bbe69a9d
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2019-12-19 17:38:37 +0800

    BugID:23618353: Refine the UDP socket support & folder adjust
    Change-Id: I702cf9c44fdbd8ad20778a6af20699ec3c88af12

--- components/peripherals/sal/wifi/bk7231/atcmd_config_module.h
+++ components/peripherals/sal/wifi/bk7231/atcmd_config_module.h
@@ -13,14 +13,6 @@
 #define AT_RECV_FAIL_POSTFIX "ERROR\r\n"
 #define AT_SEND_DELIMITER "\r"
 
-/* uart config */
-#define AT_UART_BAUDRATE 115200
-#define AT_UART_DATA_WIDTH DATA_WIDTH_8BIT
-#define AT_UART_PARITY NO_PARITY
-#define AT_UART_STOP_BITS STOP_BITS_1
-#define AT_UART_FLOW_CONTROL FLOW_CONTROL_DISABLED
-#define AT_UART_MODE MODE_TX_RX
-
 typedef struct {
    uart_dev_t            uart_dev;
 } sal_device_config_t;
