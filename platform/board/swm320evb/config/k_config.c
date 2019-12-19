840f603d80caca8dc3787776280605cbf185f054
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-12-19 10:18:51 +0800

    bugid:24098725:remove abandoned config macros
    Change-Id: Ifa91f7896b935f51e7289c70d2a7e3660a4c1a93

--- platform/board/swm320evb/config/k_config.c
+++ platform/board/swm320evb/config/k_config.c
@@ -167,17 +167,6 @@ void aos_heap_set(void)
 #endif
 #endif
 
-#if (RHINO_CONFIG_MM_LEAKCHECK > 0 )
-extern int __bss_start__, __bss_end__, _sdata, _edata;
-void aos_mm_leak_region_init(void)
-{
-#if (RHINO_CONFIG_MM_DEBUG > 0)
-    krhino_mm_leak_region_init(&__bss_start__, &__bss_end__);
-    krhino_mm_leak_region_init(&_sdata, &_edata);
-#endif
-}
-#endif
-
 #if (RHINO_CONFIG_TASK_STACK_CUR_CHECK > 0)
 size_t soc_get_cur_sp(void)
 {
