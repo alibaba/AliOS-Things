26686e2522308eb6037b7dda40f1183261ca5f36
Wenjun Chen <wenjunchen.cwj@alibaba-inc.com>
2019-12-19 17:38:37 +0800

    BugID:23618353: Refine the UDP socket support & folder adjust
    Change-Id: I702cf9c44fdbd8ad20778a6af20699ec3c88af12

--- components/peripherals/sal/wifi/bk7231/aos.mk
+++ components/peripherals/sal/wifi/bk7231/aos.mk
@@ -5,8 +5,9 @@ $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := sal hal implemenation for bk7231
 $(NAME)_SOURCES += wifi_atcmd_bk7231.c
 GLOBAL_DEFINES += DEV_SAL_BK7231
+GLOBAL_CFLAGS += -D__SAL_DEV_NAME=bk7231
 
-$(NAME)_COMPONENTS += yloop
+$(NAME)_COMPONENTS += yloop netmgr
 
 ifneq (1, $(at_adapter))
 $(NAME)_COMPONENTS += atparser
@@ -14,4 +15,4 @@ $(NAME)_COMPONENTS += atparser
 $(NAME)_SOURCES += bk7231.c
 endif
 
-$(NAME)_INCLUDES += ./
+GLOBAL_INCLUDES += ./
--- components/peripherals/sal/wifi/esp8266/aos.mk
+++ components/peripherals/sal/wifi/esp8266/aos.mk
@@ -4,9 +4,11 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := sal hal implementation for esp8266
 GLOBAL_DEFINES += DEV_SAL_ESP8266
+GLOBAL_CFLAGS += -D__SAL_DEV_NAME=esp8266
 
 $(NAME)_COMPONENTS += yloop
 
 $(NAME)_SOURCES += esp8266.c
 $(NAME)_SOURCES += esp8266_at.c
-$(NAME)_INCLUDES += ./
+
+GLOBAL_INCLUDES += ./
