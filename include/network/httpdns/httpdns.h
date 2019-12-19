2a7de20138aef15858f1bc465452416553be6ff5
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2019-12-19 13:49:47 +0800

    BugID:23266668: update websock, httpdns, lwm2m header files
    Change-Id: Idfa9d326c228e19895c01048a4b8a20648a34aa6

--- include/network/httpdns/httpdns.h
+++ include/network/httpdns/httpdns.h
@@ -1,5 +1,20 @@
+/**
+ * @file httpdns.h
+ * httpdns API header file.
+ *
+ * @version   V1.0
+ * @date      2019-11-08
+ * @copyright Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
 #ifndef HTTPDNS_H
 #define HTTPDNS_H
+
+/** @addtogroup aos_httpdns httpdns
+ *  Interfaces of httpdns
+ *
+ *  @{
+ */
 
 #include <network/network.h>
 
@@ -22,80 +37,85 @@ typedef struct dns_cache dns_cache_t;
 
 /**
  *
- * @brief check httpdns is disabled
+ * Check httpdns is disabled
  *
- * @return 0 available
- * @return 1 disabled
+ * @return 0 on available, 1 on disabled
  */
 int httpdns_is_disabled(void);
 
 /**
  *
- * @brief Invoke host resolving action
+ * Invoke host resolving action
+ *
  * @param[in] host_name host domain
  * @param[in] asyn 1: asynchronized 0: synchronized
  *
+ * @return none
  */
 void httpdns_resolv_host(char *host_name, int asyn);
 
 /**
  *
- * @brief Fetch DNS result from cache
+ * Fetch DNS result from cache
+ *
  * @param[in]  host_name host domain
  * @param[out] cache DNS result
  *
- * @return 1  pretch success
- * @return 0  pretch failed
+ * @return 1 on success, 0 on failure
  */
 int httpdns_prefetch(char * host_name, dns_cache_t ** cache);
 
 /**
  *
- * @brief Fetch DNS result from cache with timeout
+ * Fetch DNS result from cache with timeout
+ *
  * @param[in]  host_name host domain
  * @param[out] cache DNS result
  * @param[in]  timeout fetch timeout in millisecond
  *
- * @return 1  pretch success
- * @return 0  pretch failed
+ * @return 1 on success, 0 on failure
  */
 int httpdns_prefetch_timeout(char * host_name, dns_cache_t ** cache, int ms);
 
 
-/***** httpdns dns operation *****/
+/******************* dns cache operation **********************/
 
 /**
  *
- * @brief Get the first ip address from cache
+ * Get the first ip address from cache
+ *
  * @param[in]  cache DNS result
  * @param[out] ipaddr ip address
  *
- * @return 1  success
- * @return 0  failed
+ * @return 1 on success, 0 on failure
  */
 int httpdns_get_ipaddr_from_cache(dns_cache_t * cache, char * ipaddr);
 
 /**
  *
- * @brief Free cache
+ * Free cache
+ *
  * @param[in] cache DNS result
  *
+ * @return none
  */
 void httpdns_free_cache(dns_cache_t * cache);
 
 /**
  *
- * @brief Rotate ip list in cache
+ * Rotate ip list in cache
+ *
  * @param[in] cache DNS result
  *
- * @return 1  success
- * @return 0  failed
+ * @return 1 on success, 0 on failure
  */
 int httpdns_rotate_cache(dns_cache_t * dns);
 
 #ifdef __cplusplus
 }
 #endif
+
+/** @} */
 
 #endif
 
