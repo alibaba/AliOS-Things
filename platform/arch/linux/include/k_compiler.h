9a3265105658128559ba6598f41cf0569eb24d06
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-12-20 13:45:18 +0800

    bugid:23646919:macros of k_compiler be optimized
    Change-Id: I420130ad662a5ecdbec6241dd6ad5d8b43414482

--- platform/arch/linux/include/k_compiler.h
+++ platform/arch/linux/include/k_compiler.h
@@ -6,9 +6,11 @@
 #define K_COMPILER_H
 
 #define RHINO_INLINE                static inline
+
 /* get the return address of the current function
    void * __builtin_return_address (unsigned int level) */
 #define RHINO_GET_RA()              __builtin_return_address(0)
+
 /* get the return address of the current function */
 __attribute__((always_inline)) RHINO_INLINE void *RHINO_GET_SP(void)
 {
@@ -20,9 +22,22 @@ __attribute__((always_inline)) RHINO_INLINE void *RHINO_GET_SP(void)
     return NULL;
 #endif
 }
-/* get the number of leading 0-bits in x
-   int __builtin_clz (unsigned int x) */
+
+/* Returns the number of leading 0-bits in x,
+   starting at the most signifi cant bit position. */
 #define RHINO_BIT_CLZ(x)            __builtin_clz(x)
+
+/* Returns the number of trailing 0-bits in x,
+   starting at the least signifi cant bit position. */
+#define RHINO_BIT_CTZ(x)            __builtin_ctz(x)
+
+#ifndef RHINO_WEAK
+#define RHINO_WEAK                  __attribute__((weak))
+#endif
+
+#ifndef RHINO_ASM
+#define RHINO_ASM                   __asm__
+#endif
 
 #endif /* K_COMPILER_H */
 
