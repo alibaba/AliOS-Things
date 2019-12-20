b95263adbafb10b4b013372ea97b9f85e12fb51f
wuqi.hz <wuqi.hz@alibaba-inc.com>
2019-12-20 13:45:18 +0800

    bugid:23646919:macros of k_compiler be optimized
    Change-Id: I420130ad662a5ecdbec6241dd6ad5d8b43414482

--- platform/arch/arm/armv5/include/k_compiler.h
+++ platform/arch/arm/armv5/include/k_compiler.h
@@ -6,46 +6,90 @@
 #define K_COMPILER_H
 
 #if defined(__CC_ARM)
+
 #define RHINO_INLINE                static __inline
+
 /* get the return address of the current function
    unsigned int __return_address(void) */
 #define RHINO_GET_RA()              (void *)__return_address()
+
 /* get the  the value of the stack pointer
    unsigned int __current_sp(void) */
 #define RHINO_GET_SP()              (void *)__current_sp()
 
-#ifndef __WEAK
-#define __WEAK                      __weak
+/* Returns the number of leading 0-bits in x,
+   starting at the most signifi cant bit position. */
+#define RHINO_BIT_CLZ(x)            __builtin_clz(x)
+
+/* Returns the number of trailing 0-bits in x,
+   starting at the least signifi cant bit position. */
+#define RHINO_BIT_CTZ(x)            __builtin_ctz(x)
+
+#ifndef RHINO_WEAK
+#define RHINO_WEAK                  __weak
+#endif
+
+#ifndef RHINO_ASM
+#define RHINO_ASM                   __asm
 #endif
 
 #elif defined(__ICCARM__)
+
 #define RHINO_INLINE                static inline
+
 /* get the return address of the current function
    unsigned int __get_LR(void) */
 #define RHINO_GET_RA()              (void *)__get_LR()
+
 /* get the  the value of the stack pointer
    unsigned int __get_SP(void) */
 #define RHINO_GET_SP()              (void *)__get_SP()
 
-#ifndef __WEAK
-#define __WEAK                      __weak
+/* Returns the number of leading 0-bits in x,
+   starting at the most signifi cant bit position. */
+#define RHINO_BIT_CLZ(x)            __CLZ(x)
+
+//#define RHINO_BIT_CTZ(x)
+
+#ifndef RHINO_WEAK
+#define RHINO_WEAK                  __weak
 #endif
 
+#ifndef RHINO_ASM
+#define RHINO_ASM                   asm
+#endif
+
+
 #elif defined(__GNUC__)
+
 #define RHINO_INLINE                static inline
+
 /* get the return address of the current function
    void * __builtin_return_address (unsigned int level) */
 #define RHINO_GET_RA()              __builtin_return_address(0)
+
 /* get the return address of the current function */
 __attribute__((always_inline)) RHINO_INLINE void *RHINO_GET_SP(void)
 {
     void *sp;
-    asm volatile("mov %0, SP\n" : "=r" (sp));
+    asm volatile("mov %0, SP\n":"=r"(sp));
     return sp;
 }
 
-#ifndef __WEAK
-#define __WEAK                      __attribute__((weak))
+/* Returns the number of leading 0-bits in x,
+   starting at the most signifi cant bit position. */
+#define RHINO_BIT_CLZ(x)            __builtin_clz(x)
+
+/* Returns the number of trailing 0-bits in x,
+   starting at the least signifi cant bit position. */
+#define RHINO_BIT_CTZ(x)            __builtin_ctz(x)
+
+#ifndef RHINO_WEAK
+#define RHINO_WEAK                  __attribute__((weak))
+#endif
+
+#ifndef RHINO_ASM
+#define RHINO_ASM                   __asm__
 #endif
 
 #else
