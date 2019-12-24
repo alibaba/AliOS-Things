1a70bfecb13ff18f33a9a1c3d320f5bf3be6f638
yajun.syj <yajun.syj@alibaba-inc.com>
2019-12-24 14:54:42 +0800

    BugID:23266661: Optimize export http header
    Change-Id: I704890d3fa5d0160dacbd952997e4c3d0a4cf72d

--- include/network/http/http_def_config.h
+++ include/network/http/http_def_config.h
@@ -1,37 +1,46 @@
-/*
+/**
+ * @file http_def_config.h
+ * http default config header file.
+ *
+ * @version   V1.0
+ * @date      2019-12-24
+
  * Copyright (C) 2015-2019 Alibaba Group Holding Limited
  */
 
 #ifndef HTTP_DEF_CONFIG_H
 #define HTTP_DEF_CONFIG_H
 
+/** @defgroup aos_http_parser http_parser
+  * @{
+  */
 #ifdef CONFIG_DEFINE_HTTP_CONFIG
 #include "http_config.h"
 #endif
 
 #ifndef CONFIG_HTTPC_SESSION_NUM
-#define CONFIG_HTTPC_SESSION_NUM 1
+#define CONFIG_HTTPC_SESSION_NUM 1  /**< the max number of the http session */
 #endif
 
 #ifndef CONFIG_HTTPC_SERVER_NAME_SIZE
-#define CONFIG_HTTPC_SERVER_NAME_SIZE 64
+#define CONFIG_HTTPC_SERVER_NAME_SIZE 64  /**< server name size */
 #endif
 
 #ifndef CONFIG_HTTPC_DEFAULT_CLIENT
-#define CONFIG_HTTPC_DEFAULT_CLIENT "AliOS-HTTP-Client/2.1"
+#define CONFIG_HTTPC_DEFAULT_CLIENT "AliOS-HTTP-Client/2.1"  /**< http client default user agent */
 #endif
 
 #ifndef CONFIG_HTTP_STATUS_SIZE
-#define CONFIG_HTTP_STATUS_SIZE 32
+#define CONFIG_HTTP_STATUS_SIZE 32   /**< http status size */
 #endif
 
 #ifndef CONFIG_HTTP_SECURE
 #if AOS_COMP_ITLS
-#define CONFIG_HTTP_SECURE 1
+#define CONFIG_HTTP_SECURE 1  /**< http secure support */
 #elif AOS_COMP_MBEDTLS
 #define CONFIG_HTTP_SECURE 1
 #else
-#define CONFIG_HTTP_SECURE 0
+#define CONFIG_HTTP_SECURE 0  /**< http no secure support */
 #endif
 #endif
 
@@ -48,5 +57,5 @@
 #endif
 
 #define CONFIG_HTTP_DEBUG
-
+/** @} */
 #endif
