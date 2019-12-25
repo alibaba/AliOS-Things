c7cd45a1c2b049de5d18003069a2c0a53f1502f5
yx170385 <yx170385@alibaba-inc.com>
2019-12-25 13:52:28 +0800

    BugID:23793801:[debug] module optimization : debug_api.h
    Change-Id: I9705e614134932466dc980ba7494a0f615f49368

--- include/debug/debug_api.h
+++ include/debug/debug_api.h
@@ -30,35 +30,6 @@ extern "C" {
 #define DEBUG_REBOOT_REASON_FATAL_ERR 0x04 /**< System fatal error */
 
 /**
- * Get task handle by its name
- *
- * @param[in]  name  the task name which is need to be got.
- *
- * @return task handle when found, otherwise return NULL.
- */
-ktask_t *debug_task_find(char *name);
-
-/**
- * Get task running status by task handle
- *
- * @param[in]  task  the task handle which is need to be check
- *
- * @return  1 when task is running, otherwise return 0;
- */
-int debug_task_is_running(ktask_t *task);
-
-/**
- * Get the task stack base
- *
- * @note: get the current active task stack base if param[in] is NULL
- *
- * @param[in]  task  the task handle which is need to be got
- *
- * @return  task stack base
- */
-void *debug_task_stack_bottom(ktask_t *task);
-
-/**
  * Show backtrace when called by printf
  *
  * @note: printf should be check when used in interrupt routing
@@ -136,25 +107,12 @@ void debug_overview(void);
  * Get system reboot reason
  *
  * @return reboot reason:
- * 0x01 for watchdog reset;
- * 0x02 for system panic
- * 0x03 for system repower
- * 0x04 for system fatal error
+ * DEBUG_REBOOT_REASON_WD_RST
+ * DEBUG_REBOOT_REASON_PANIC
+ * DEBUG_REBOOT_REASON_REPOWER
+ * DEBUG_REBOOT_REASON_FATAL_ERR
  */
 unsigned int debug_reboot_reason_get(void);
-
-/**
- * convert int to ascii(HEX)
- * @note:  while using format % in libc, malloc/free is involved.
- * this function avoid using malloc/free. so it works when heap corrupt.
- *
- * @param[in]   num      number
- * @param[in]   str      fix 8 character str
- *
- * @return  str
- */
-char *k_int2str(int num, char *str);
-
 
 /** @} */
 
