1ea59581891156b126baaa434aedb4aeeeac395b
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2019-12-26 22:40:07 +0800

    BugID:23702582:support function 0xf and 0x10
    Change-Id: I453f2bf6c9f34991be354ec5e35522522801e773

--- components/bus/mbmaster/physical/serial.h
+++ components/bus/mbmaster/physical/serial.h
@@ -12,7 +12,7 @@ extern "C" {
 #include "aos/hal/uart.h"
 
 mb_status_t mb_serial_init(mb_handler_t *handler, uint8_t port, uint32_t baud_rate, uint8_t data_width, mb_parity_t parity);
-mb_status_t mb_serial_frame_send(mb_handler_t *handler);
+mb_status_t mb_serial_frame_send(mb_handler_t *handler, uint32_t timeout);
 mb_status_t mb_serial_frame_recv(mb_handler_t *handler);
 mb_status_t mb_serial_finalize(mb_handler_t *handler);
 
