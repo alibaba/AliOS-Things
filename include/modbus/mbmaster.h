c30f46305afac2622896396f4e1376b54f834b57
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2019-12-26 22:40:54 +0800

    BugID:23702582:support function 0xf and 0x10
    Change-Id: I2de800e0edd0a6ca77b226380cd3e2749f82054c

--- include/modbus/mbmaster.h
+++ include/modbus/mbmaster.h
@@ -15,7 +15,7 @@ extern "C" {
 
 #include "k_api.h"
 
-//#include "mbmaster_default_config.h"
+#include "mbmaster_default_config.h"
 #include "adu/adu.h"
 #include "auxiliary/auxiliary.h"
 
@@ -62,10 +62,10 @@ typedef enum {
 typedef struct mb_handler mb_handler_t;
 
 /* Define the callback function type */
-typedef mb_status_t (*frame_send_func_t)(mb_handler_t *);
-typedef mb_status_t (*frame_recv_func_t)(mb_handler_t *);
-typedef mb_status_t (*adu_assemble_func_t)(mb_handler_t *);
-typedef mb_status_t (*adu_disassemble_func_t)(mb_handler_t *);
+typedef mb_status_t (*frame_send_func_t)(mb_handler_t *handler, uint32_t timeout);
+typedef mb_status_t (*frame_recv_func_t)(mb_handler_t *handler);
+typedef mb_status_t (*adu_assemble_func_t)(mb_handler_t *handler);
+typedef mb_status_t (*adu_disassemble_func_t)(mb_handler_t *handler);
 
 typedef struct mb_handler {
     uint8_t used;       /**< The value is 1 when used */
@@ -114,11 +114,13 @@ mb_status_t mbmaster_rtu_uninit(mb_handler_t *req_handler);
  * @param[in]   quantity       quantity of coils
  * @param[out]  respond_buf    data receive from remote device, length must >= (quantity / 8 + 1) bytes
  * @param[out]  respond_count  the number of bytes of data received
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
  * @return  the operation status, MB_SUCCESS is OK, other is error
  * @note  when MB_RESPOND_EXCEPTION is returned, the first byte of respond_buf holds the exception code
  */
 mb_status_t mbmaster_read_coils(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t start_addr,
-                                uint16_t quantity, uint8_t *respond_buf, uint8_t *respond_count);
+                                uint16_t quantity, uint8_t *respond_buf, uint8_t *respond_count,
+                                uint32_t timeout);
 
 /**
  * This function will initiate a read discrete inputs request(function code 0x02)
@@ -128,11 +130,13 @@ mb_status_t mbmaster_read_coils(mb_handler_t *req_handler, uint8_t slave_addr, u
  * @param[in]   quantity       quantity of inputs
  * @param[out]  respond_buf    data receive from remote device, length must >= (quantity / 8 + 1) bytes
  * @param[out]  respond_count  the number of bytes of data received
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
  * @return  the operation status, MB_SUCCESS is OK, other is error
  * @note  when MB_RESPOND_EXCEPTION is returned, the first byte of respond_buf holds the exception code
  */
 mb_status_t mbmaster_read_discrete_inputs(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t start_addr,
-                                          uint16_t quantity, uint8_t *respond_buf, uint8_t *respond_count);
+                                          uint16_t quantity, uint8_t *respond_buf, uint8_t *respond_count,
+                                          uint32_t timeout);
 
 /**
  * This function will initiate a read holding registers request(function code 0x03)
@@ -142,12 +146,13 @@ mb_status_t mbmaster_read_discrete_inputs(mb_handler_t *req_handler, uint8_t sla
  * @param[in]   quantity       quantity of registers
  * @param[out]  respond_buf    data receive from remote device, length must >= (quantity * 2) bytes
  * @param[out]  respond_count  the number of bytes of data received
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
  * @return  the operation status, MB_SUCCESS is OK, other is error
  * @note    when MB_RESPOND_EXCEPTION is returned, the first byte of respond_buf holds the exception code
  */
 mb_status_t mbmaster_read_holding_registers(mb_handler_t *req_handler, uint8_t slave_addr,
                                            uint16_t start_addr, uint16_t quantity,
-                                           uint8_t *respond_buf, uint8_t *respond_count);
+                                           uint8_t *respond_buf, uint8_t *respond_count, uint32_t timeout);
 
 /**
  * This function will initiate a read input registers request(function code 0x04)
@@ -157,12 +162,13 @@ mb_status_t mbmaster_read_holding_registers(mb_handler_t *req_handler, uint8_t s
  * @param[in]   quantity       quantity of input registers
  * @param[out]  respond_buf    data receive from remote device, length must >= (quantity * 2) bytes
  * @param[out]  respond_count  the number of bytes of data received
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
  * @return  the operation status, MB_SUCCESS is OK, other is error
  * @note    when MB_RESPOND_EXCEPTION is returned, the first byte of respond_buf holds the exception code
  */
 mb_status_t mbmaster_read_input_registers(mb_handler_t *req_handler, uint8_t slave_addr,
                                          uint16_t start_addr, uint16_t quantity,
-                                         uint8_t *respond_buf, uint8_t *respond_count);
+                                         uint8_t *respond_buf, uint8_t *respond_count, uint32_t timeout);
 
 /**
  * This function will initiate a write single coil request(function code 0x05)
@@ -173,25 +179,61 @@ mb_status_t mbmaster_read_input_registers(mb_handler_t *req_handler, uint8_t sla
  * @param[out]  resp_addr       coil address responded from remote device
  * @param[out]  resp_value      coil value responded from remote device
  * @param[out]  excepiton_code  exception code, when MB_RESPOND_EXCEPTION is returned
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
  * @return  the operation status, MB_SUCCESS is OK, other is error
  */
 mb_status_t mbmaster_write_single_coil(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t coil_addr,
-                                       uint16_t coil_value, uint16_t *resp_addr, uint16_t *resp_value, uint8_t *exception_code);
+                                       uint16_t coil_value, uint16_t *resp_addr, uint16_t *resp_value,
+                                       uint8_t *exception_code, uint32_t timeout);
 
 /**
  * This function will initiate a write single register request(function code 0x06)
- * @param[in]  req_handler     the handler pointer returned from mbmaster_rtu_init
+ * @param[in]   req_handler     the handler pointer returned from mbmaster_rtu_init
  * @param[in]   slave_addr      slave address of the remote device
  * @param[in]   register_addr   register address
  * @param[in]   register_value  regitter value
  * @param[out]  resp_addr       register address responded from remote device
  * @param[out]  resp_value      register value responded from remote device
  * @param[out]  excepiton_code  exception code, when MB_RESPOND_EXCEPTION is returned
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
  * @return  the operation status, MB_SUCCESS is OK, other is error
  */
 mb_status_t mbmaster_write_single_register(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t register_addr,
-                                           uint16_t register_value, uint16_t *resp_addr, uint16_t *resp_value, uint8_t *exception_code);
+                                           uint16_t register_value, uint16_t *resp_addr, uint16_t *resp_value,
+                                           uint8_t *exception_code, uint32_t timeout);
 
+/**
+ * This function will initiate a write mutiple coils request(function code 0x0F)
+ * @param[in]   req_handler     the handler pointer returned from mbmaster_rtu_init
+ * @param[in]   slave_addr      slave address of the remote device
+ * @param[in]   start_addr      start address
+ * @param[in]   quantity        quantity of outputs
+ * @param[in]   outputs_buf     outputs value(bit array, 0 to be OFF. 1 to be ON)
+ * @param[out]  resp_addr       start address responded from remote device
+ * @param[out]  resp_quantity   quantity responded from remote device
+ * @param[out]  excepiton_code  exception code, when MB_RESPOND_EXCEPTION is returned
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
+ * @return  the operation status, MB_SUCCESS is OK, other is error
+ */
+mb_status_t mbmaster_write_multiple_coils(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t start_addr,
+                                          uint16_t quantity, uint8_t *outputs_buf, uint16_t *resp_addr,
+                                          uint16_t *resp_quantity, uint8_t *exception_code, uint32_t timeout);
+/**
+ * This function will initiate a write mutiple registers request(function code 0x10)
+ * @param[in]   req_handler     the handler pointer returned from mbmaster_rtu_init
+ * @param[in]   slave_addr      slave address of the remote device
+ * @param[in]   start_addr      start address
+ * @param[in]   quantity        quantity of registers
+ * @param[in]   outputs_buf     registers value
+ * @param[out]  resp_addr       start address responded from remote device
+ * @param[out]  resp_quantity   registers quantity responded from remote device
+ * @param[out]  excepiton_code  exception code, when MB_RESPOND_EXCEPTION is returned
+ * @param[in]   timeout        the timeout time(ms) for sending the request, 0xFFFFFFFF means waiting for ever
+ * @return  the operation status, MB_SUCCESS is OK, other is error
+ */
+mb_status_t mbmaster_write_multiple_registers(mb_handler_t *req_handler, uint8_t slave_addr, uint16_t start_addr,
+                                              uint16_t quantity, uint8_t *outputs_buf, uint16_t *resp_addr,
+                                              uint16_t *resp_quantity, uint8_t *exception_code, uint32_t timeout);
 /** @} */
 
 #ifdef __cplusplus
