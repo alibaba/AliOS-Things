8aa8560ed4a66fb15fd650e57f9f4d863c489c09
hongye.jhy <hongye.jhy@alibaba-inc.com>
2019-12-27 14:59:43 +0800

    BugID:23266431:AOS code update for folder adjustment
    Change-Id: Ic387bedf7b4336c1483f7362f9fc2d323bc59d05

---
+++ components/gui/littlevGL/aos.mk
@@ -0,0 +1,19 @@
+NAME := littlevGL
+
+$(NAME)_MBINS_TYPE := framework
+$(NAME)_VERSION    := 1.0.1
+$(NAME)_SUMMARY    := a lightweight GUI lib
+
+#default gcc
+ifeq ($(COMPILER),)
+$(NAME)_CFLAGS      += -Wall -Werror
+else ifeq ($(COMPILER),gcc)
+$(NAME)_CFLAGS      += -Wall -Werror
+else ifeq ($(COMPILER),armcc)
+GLOBAL_DEFINES      += __BSD_VISIBLE
+endif
+
+GLOBAL_INCLUDES     += .
+GLOBAL_DEFINES      += AOS_COMP_LITTLEVGL
+
+include ./components/gui/littlevGL/src/src.mk
