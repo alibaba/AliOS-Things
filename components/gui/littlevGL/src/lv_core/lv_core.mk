0096550ce1706c3017391bc725ae7da3619f1f7b
hongye.jhy <hongye.jhy@alibaba-inc.com>
2019-12-27 14:59:43 +0800

    BugID:23266431:AOS code update for folder adjustment
    Change-Id: Ic387bedf7b4336c1483f7362f9fc2d323bc59d05

---
+++ components/gui/littlevGL/src/lv_core/lv_core.mk
@@ -0,0 +1,9 @@
+$(NAME)_SOURCES     += ./src/lv_core/lv_disp.c
+$(NAME)_SOURCES     += ./src/lv_core/lv_group.c
+$(NAME)_SOURCES     += ./src/lv_core/lv_indev.c
+$(NAME)_SOURCES     += ./src/lv_core/lv_obj.c
+$(NAME)_SOURCES     += ./src/lv_core/lv_refr.c
+$(NAME)_SOURCES     += ./src/lv_core/lv_style.c
+
+$(NAME)_INCLUDES    += .
+$(NAME)_INCLUDES    += /src
