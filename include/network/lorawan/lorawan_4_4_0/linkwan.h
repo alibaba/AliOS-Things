4ed1592017d818e0459ddd1f910da8df63f73b6f
jiangjiang.xjj <jiangjiang.xjj@alibaba-inc.com>
2019-12-29 23:25:45 +0800

    BugID:23266646: improve header file of lorawan
    Change-Id: Icf24af82c8e209ef7f07181a4d7736571552ed58

--- include/network/lorawan/lorawan_4_4_0/linkwan.h
+++ include/network/lorawan/lorawan_4_4_0/linkwan.h
@@ -11,65 +11,120 @@
 
 #include "linkwan_config.h"
 
+/**
+ * Join Request max retry times
+ */
 #define JOINREQ_NBTRIALS 3
 
+/**
+ * LoRa Device Work Mode Enumeration
+ */
 typedef enum node_mode_s
 {
-    NODE_MODE_NORMAL     = 0,
-    NODE_MODE_REPEATER   = 1, // Alibaba Node Repeater
-    NODE_MODE_AUTOSWITCH = 2, // switch between normal and repeater
+    NODE_MODE_NORMAL     = 0,   /* LoRa Device work in Normal Mode */
+    NODE_MODE_REPEATER   = 1,   /* LoRa Device work in Repeater Mode */
+    NODE_MODE_AUTOSWITCH = 2,   /* LoRa Device switch between normal and repeater */
 } node_mode_t;
 
+/**
+ * LoRa Device Freq Mode Enumeration
+ */
 typedef enum node_freq_mode_s
 {
-    FREQ_MODE_INTRA = 1, // uplink and downlink use same frequencies
-    FREQ_MODE_INTER = 2, // uplink and downlink use different frequencies
+    FREQ_MODE_INTRA = 1,        /* uplink and downlink use same frequencies */
+    FREQ_MODE_INTER = 2,        /* uplink and downlink use different frequencies */
 } node_freq_mode_t;
 
+/**
+ * LoRa Device Configuration Valid Enumeration
+ */
 typedef enum
 {
-    VALID_LORA_CONFIG   = 0xbeef,
-    INVALID_LORA_CONFIG = 0xfffe,
+    VALID_LORA_CONFIG   = 0xbeef,   /* LoRa Device Config Info is Valid */
+    INVALID_LORA_CONFIG = 0xfffe,   /* LoRa Device Config Info is Invalid */
 };
 
-
-typedef struct lora_dev_s
-{
-    uint8_t dev_eui[8];
-    uint8_t app_eui[8];
-    uint8_t app_key[16];
-    int8_t  freqband;
-    int8_t  datarate;
-    int8_t class;
-    uint8_t  mode; // normal or repeater
-    uint16_t mask;
-    uint16_t flag;
-    uint16_t crc;
-} __attribute__((packed)) lora_dev_t;
-
-
-typedef struct lora_abp_id_s
-{
-    uint8_t  devaddr[4];
-    uint8_t  appskey[16];
-    uint8_t  nwkskey[16];
-    uint16_t flag;
-} lora_abp_id_t;
-
+/**
+ * LoRa Device Join Method Enumeration
+ */
 typedef enum join_method_s
 {
-    STORED_JOIN_METHOD = 0,
-    DEF_JOIN_METHOD    = 1,
-    SCAN_JOIN_METHOD   = 2,
+    STORED_JOIN_METHOD = 0,     /* Join use Stored Info Method */
+    DEF_JOIN_METHOD    = 1,     /* Join use Default Configuration */
+    SCAN_JOIN_METHOD   = 2,     /* Join use Scan Freq Method */
     JOIN_METHOD_NUM
 } join_method_t;
 
+/**
+ * LoRa Device CLASS Type Enumeration
+ */
 typedef enum app_class_type_s
 {
-    LORA_APP_CLASS_A = 0,
-    LORA_APP_CLASS_B,
-    LORA_APP_CLASS_C,
+    LORA_APP_CLASS_A = 0,       /* LoRa Type of Class A */
+    LORA_APP_CLASS_B,           /* LoRa Type of Class B */
+    LORA_APP_CLASS_C,           /* LoRa Type of Class C */
 } app_class_type_t;
+
+/**
+ * LoRa Device Join Mode Enumeration
+ */
+typedef enum eJoinModeType
+{
+    JOIN_MODE_OTAA,         /* Join in OTA Mode*/
+    JOIN_MODE_ABP           /* Join in ABP Mode*/
+} JoinMode_t;
+
+/**
+ * LoRa Device Send Data Trigger Event Type
+ */
+typedef enum eTxEventType
+{
+    TX_ON_TIMER,            /* Send Data Triggered by Timer */
+    TX_ON_EVENT,            /* Send Data Triggered by Event */
+    TX_ON_NONE
+} TxEventType_t;
+
+/**
+ * LoRa Device State
+ */
+typedef enum eDevicState
+{
+    DEVICE_STATE_INIT,                  /* Device is initialized */
+    DEVICE_STATE_JOIN,                  /* Device is joining in Network */
+    DEVICE_STATE_JOINED,                /* Device is joined */
+    DEVICE_STATE_SEND,                  /* Device is Sending Data */
+    DEVICE_STATE_SEND_MAC,              /* Device is Sending Mac Data */
+    DEVICE_STATE_CYCLE,                 /* Device is Sending Data in Cycle */
+    DEVICE_STATE_SLEEP                  /* Device is Sleeping */
+} DeviceState_t;
+
+/**
+ * LoRa Device Info Struct
+ */
+typedef struct lora_dev_s
+{
+    uint8_t dev_eui[8];     /* LoRa Device DEV EUI */
+    uint8_t app_eui[8];     /* LoRa Device APP EUI */
+    uint8_t app_key[16];    /* LoRa Device APP KEY */
+    int8_t  freqband;       /* LoRa Device Freqband Info */
+    int8_t  datarate;       /* LoRa Device Data Rate */
+    int8_t class;           /* Class Type of LoRa Device used */
+    uint8_t  mode;          /* LoRa Mode: normal or repeater */
+    uint16_t mask;          /* Band mask of Device */
+    uint16_t flag;          /* Valid Flag, valid or invalid */
+    uint16_t crc;           /* Crc check of Device Info */
+} __attribute__((packed)) lora_dev_t;
+
+/**
+ * LoRa Device Info when in APB Mode
+ */
+typedef struct lora_abp_id_s
+{
+    uint8_t  devaddr[4];    /* Device Address */
+    uint8_t  appskey[16];   /* APPSKEY of LoRa Device */
+    uint8_t  nwkskey[16];   /* NETWORK SKEY of LoRa Device */
+    uint16_t flag;          /* Valid Flag of APB Info */
+} lora_abp_id_t;
 
 #ifdef CONFIG_DEBUG_LINKWAN
 #include "debug.h"
@@ -78,109 +133,415 @@ typedef enum app_class_type_s
 #define DBG_LINKWAN(...)
 #endif
 
-typedef enum eJoinModeType
-{
-    JOIN_MODE_OTAA,
-    JOIN_MODE_ABP
-} JoinMode_t;
-
-typedef enum eTxEventType
-{
-    TX_ON_TIMER,
-    TX_ON_EVENT,
-    TX_ON_NONE
-} TxEventType_t;
-
+/**
+ * LoRa Device Send App Data Struct
+ */
 typedef struct
 {
-    uint8_t *Buff;
-    uint8_t  BuffSize;
-    uint8_t  Port;
+    uint8_t *Buff;          /* Data Buffer To Send */
+    uint8_t  BuffSize;      /* Data Size To Send */
+    uint8_t  Port;          /* Tx Port For this LoRa Application */
 } __attribute__((packed)) lora_AppData_t;
 
+/**
+ * Callbacks for LoRa Application
+ */
 typedef struct sLoRaMainCallback
 {
-    uint8_t (*BoardGetBatteryLevel)(void);
-    void (*BoardGetUniqueId)(uint8_t *id);
-    uint32_t (*BoardGetRandomSeed)(void);
-    void (*LoraTxData)(lora_AppData_t *AppData);
-    void (*LoraRxData)(lora_AppData_t *AppData);
+    uint8_t (*BoardGetBatteryLevel)(void);          /* Get Battery Level Callback */
+    void (*BoardGetUniqueId)(uint8_t *id);          /* Get UniqueID Callback */
+    uint32_t (*BoardGetRandomSeed)(void);           /* Get Random Seed Callback */
+    void (*LoraTxData)(lora_AppData_t *AppData);    /* Send Data Complete Callback */
+    void (*LoraRxData)(lora_AppData_t *AppData);    /* Receive Data Callback */
 } __attribute__((packed)) LoRaMainCallback_t;
 
+/**
+ * LoRaWAN Network Paramters
+ */
 typedef struct sLoRaParam
 {
-    TxEventType_t TxEvent;
-    uint32_t      TxDutyCycleTime;
-    bool          AdrEnable;
-    int8_t        TxDatarate;
-    bool          EnablePublicNetwork;
-    uint8_t       NbTrials;
-    JoinMode_t    JoinMode;
+    TxEventType_t TxEvent;                  /* TX Trigger Type: Timer or Event */
+    uint32_t      TxDutyCycleTime;          /* TX Dutycycle Time: valid when TX is triggered by timer */
+    bool          AdrEnable;                /* Adaptive Datarate Enable Flag */
+    int8_t        TxDatarate;               /* TX Datarate */
+    bool          EnablePublicNetwork;      /* Public Network is Enabled Flag */
+    uint8_t       NbTrials;                 /* TX Retry Times when Send failed */
+    JoinMode_t    JoinMode;                 /* Join Mode: OTAA or ABP */
 } __attribute__((packed)) LoRaParam_t;
 
-typedef enum eDevicState
-{
-    DEVICE_STATE_INIT,
-    DEVICE_STATE_JOIN,
-    DEVICE_STATE_JOINED,
-    DEVICE_STATE_SEND,
-    DEVICE_STATE_SEND_MAC,
-    DEVICE_STATE_CYCLE,
-    DEVICE_STATE_SLEEP
-} DeviceState_t;
+/**
+ * set LoRa Device Freqband Mask
+ *
+ * @param[in]  mask         freqband mask info
+ *
+ * @return true on success
+ */
+bool set_lora_freqband_mask(uint16_t mask);
 
-bool     set_lora_freqband_mask(uint16_t mask);
+/**
+ * get LoRa Device Freqband Mask
+ *
+ * @param none
+ *
+ * @return freqband mask value
+ */
 uint16_t get_lora_freqband_mask(void);
 
+/**
+ * set LoRa Device EUI
+ *
+ * @param[in]  eui      LoRa Device EUI
+ *
+ * @return true when success
+ */
 bool set_lora_dev_eui(uint8_t *eui);
+
+/**
+ * get LoRa Device EUI
+ *
+ * @param[out]  eui      LoRa Device EUI
+ *
+ * @return  none
+ */
+void get_lora_dev_eui(uint8_t *eui);
+
+/**
+ * set LoRa App EUI
+ *
+ * @param[in]  eui      LoRa App EUI
+ *
+ * @return true when success
+ */
 bool set_lora_app_eui(uint8_t *eui);
+
+/**
+ * get LoRa App EUI
+ *
+ * @param[out]  eui      LoRa App EUI
+ *
+ * @return  none
+ */
+void get_lora_app_eui(uint8_t *eui);
+
+/**
+ * set LoRa App KEY
+ *
+ * @param[in]  key      LoRa App KEY
+ *
+ * @return true when success
+ */
 bool set_lora_app_key(uint8_t *key);
 
+/**
+ * get LoRa App KEY
+ *
+ * @param[out]  key      LoRa App KEY
+ *
+ * @return  none
+ */
+void get_lora_app_key(uint8_t *key);
+
+/**
+ * set LoRa Freq Band
+ *
+ * @param[in]  freqband      LoRa Device Freqband
+ *
+ * @return true when success
+ */
 bool set_lora_freq_band(int8_t freqband);
+
+/**
+ * get LoRa Freq Band
+ *
+ * @param[out]  freqband      LoRa Device Freqband
+ *
+ * @return  none
+ */
 void get_lora_freq_band(int8_t *freqband);
 
+/**
+ * set LoRa Freq Mode, Inter Freq or Intra Freq
+ *
+ * @param[in]  Freq Mode
+ *
+ * @return true when success, false when failed
+ */
+bool set_lora_freq_mode(node_freq_mode_t mode);
+
+/**
+ * get LoRa Freq Mode, Inter Freq or Intra Freq
+ *
+ * @param  none
+ *
+ * @return  FREQ_MODE_INTRA: Intra Freq Mode
+ *          FREQ_MODE_INTER: Inter Freq Mode
+ */
 node_freq_mode_t get_lora_freq_mode(void);
-bool             set_lora_freq_mode(node_freq_mode_t mode);
-bool             set_lora_tx_datarate(int8_t datarate);
-int8_t           get_lora_tx_datarate(void);
 
+/**
+ * set LoRa Device TX Datarate
+ *
+ * @param[in]  datarate    LoRa Device TX Datarate
+ *
+ * @return true when success
+ */
+bool set_lora_tx_datarate(int8_t datarate);
+
+/**
+ * get LoRa Device TX Datarate
+ *
+ * @param none
+ *
+ * @return LoRa Device TX Datarate
+ */
+int8_t get_lora_tx_datarate(void);
+
+/**
+ * Enable or Disable LoRa Device Adaptive Datarate
+ *
+ * @param[in]  state   Enable or Disable ADR
+ *
+ * @return true when success
+ */
 bool set_lora_adr(int state);
-int  get_lora_adr(void);
 
-bool   set_lora_class(int8_t class);
+/**
+ * get LoRa Device Adaptive Datarate State
+ *
+ * @param[in]  none
+ *
+ * @return 1 when ADR is enabled, 0 when ADR is disabled
+ */
+int get_lora_adr(void);
+
+/**
+ * set LoRa Device Adaptive Datarate State
+ *
+ * @param[in]  class   Class Type of LoRa Device
+ *
+ * @return true when success
+ */
+bool set_lora_class(int8_t class);
+
+/**
+ * get LoRa Device Class Type
+ *
+ * @param[in]  none
+ *
+ * @return class type of LoRa Device
+ */
 int8_t get_lora_class(void);
 
+/**
+ * set LoRa TX Confirmed Data Type Flag
+ *
+ * @param[in] confirmed   Confirmed Data Type Flag
+ *
+ * @return true when success
+ */
 bool set_lora_tx_cfm_flag(int confirmed);
-int  get_lora_tx_cfm_flag(void);
 
-bool    set_lora_tx_cfm_trials(uint8_t trials);
+/**
+ * get LoRa TX Confirmed Data Type Flag
+ *
+ * @param[in] none
+ *
+ * @return 1 when Confirmed Flag is set
+ */
+int get_lora_tx_cfm_flag(void);
+
+/**
+ * set LoRa TX Retry Times when Send Failed in Confirmed Mode
+ *
+ * @param[in] trials   Retry Times of Sendind Confirmed Data
+ *
+ * @return true when success
+ */
+bool set_lora_tx_cfm_trials(uint8_t trials);
+
+/**
+ * get LoRa TX Retry Times when Send Failed in Confirmed Mode
+ *
+ * @param[in] none
+ *
+ * @return retry time of sending confirmed data
+ */
 uint8_t get_lora_tx_cfm_trials(void);
 
-bool set_lora_state(DeviceState_t state);
+/**
+ * set LoRa Device State
+ *
+ * @param[in] state   LoRa Device State
+ *
+ * @return true when success
+ */
+bool set_device_state(DeviceState_t state);
 
-bool     set_lora_tx_dutycycle(uint32_t dutycycle);
+/**
+ * get LoRa Device State
+ *
+ * @param[in] none
+ *
+ * @return LoRa Device Status
+ */
+int get_device_state(void);
+
+/**
+ * set LoRa Device TX Dutycycle
+ *
+ * @param[in] dutycycle  the dutycycle of sending data in cycle mode
+ *
+ * @return true when success
+ */
+bool set_lora_tx_dutycycle(uint32_t dutycycle);
+
+/**
+ * get LoRa Device TX Dutycycle
+ *
+ * @param[in] none
+ *
+ * @return the dutycycle of sending data in cycle mode
+ */
 uint32_t get_lora_tx_dutycycle(void);
 
+/**
+ * get LoRa Device TX Data Info, including data buffer, data length, application port
+ *
+ * @param[in] none
+ *
+ * @return LoRa Device TX Data Info
+ */
 lora_AppData_t *get_lora_tx_data(void);
-bool            tx_lora_data(void);
-lora_AppData_t *get_lora_rx_data(void);
-void            tx_lora_mac_req(void);
 
-int    get_device_status(void);
-void   get_lora_rssi(uint8_t band, int16_t *channel_rssi);
-bool   get_lora_report_mode(void);
-bool   set_lora_report_mode(int8_t mode);
+/**
+ * trigger LoRa Device to Send Data in Buffer
+ *
+ * @param[in] none
+ *
+ * @return true when success
+ */
+bool tx_lora_data(void);
+
+/**
+ * get LoRa Device RX Data Info, including data buffer, data length, application port
+ *
+ * @param[in] none
+ *
+ * @return LoRa Device RX Data Info
+ */
+lora_AppData_t *get_lora_rx_data(void);
+
+/**
+ * trigger LoRa Device to Send Mac Data in Buffer
+ *
+ * @param[in] none
+ *
+ * @return none
+ */
+void tx_lora_mac_req(void);
+
+/**
+ * get RSSI of LoRa Device
+ *
+ * @param[in]   band           the Band Info of LoRa Device
+ *  param[out]  channel_rssi   RSSI of LoRa Device
+ *
+ * @return none
+ */
+void get_lora_rssi(uint8_t band, int16_t *channel_rssi);
+
+/**
+ * get Report Mode of LoRa Device
+ *
+ * @param[in] none
+ *
+ * @return true when success
+ */
+bool get_lora_report_mode(void);
+
+/**
+ * set Report Mode of LoRa Device
+ *
+ * @param[in] mode  Report Mode
+ *
+ * @return true when success
+ */
+bool set_lora_report_mode(int8_t mode);
+
+/**
+ * get TX Power of LoRa Device
+ *
+ * @param[in] none
+ *
+ * @return TX Power of LoRa Device
+ */
 int8_t get_lora_tx_power();
-bool   set_lora_tx_power(int8_t pwr);
-bool   set_lora_mac_rx1_delay(long rx1delay);
-// for linkWAN test
-bool    set_lora_tx_len(uint16_t len);
+
+/**
+ * set TX Power of LoRa Device
+ *
+ * @param[in] pwr TX Power of LoRa Device
+ *
+ * @return true when success
+ */
+bool set_lora_tx_power(int8_t pwr);
+
+/**
+ * set rx1 window delay of LoRa Device
+ *
+ * @param[in] rx1delay    rx1 window delay of LoRa Device
+ *
+ * @return true when success
+ */
+bool set_lora_mac_rx1_delay(long rx1delay);
+
+/**
+ * set TX Data Length
+ *
+ * @param[in] len   Data Length to be Sent
+ *
+ * @return true when success
+ */
+bool set_lora_tx_len(uint16_t len);
+
+/**
+ * get TX Data Length
+ *
+ * @param[in] len   Data Length to be Sent
+ *
+ * @return Data Length to be Sent
+ */
 uint8_t get_lora_tx_len(void);
 
+/**
+ * Trigger to send LoRa Link Check Request to Network Server
+ *
+ * @param[in] none
+ *
+ * @return true when success
+ */
 bool send_lora_link_check(void);
 
+/**
+ * Set LoRa TX Payload when need to Send Data
+ *
+ * @param[in] confirm   whether data is confirmed or unconfirmed
+ *  param[in] Nbtrials  Retry times of TX
+ *  param[in] payload   data buffer to be sent
+ *  param[in] len       data length to be sent
+ *
+ * @return true when success
+ */
 bool lora_tx_data_payload(uint8_t confirm, uint8_t Nbtrials, uint8_t *payload,
                           uint8_t len);
+
+/**
+ * Set LoRa Application Port
+ *
+ * @param[in] port  LoRa Application Port
+ *
+ * @return true when success
+ */
 bool set_lora_app_port(uint8_t port);
 
 #endif /* LINKWAN_H */
