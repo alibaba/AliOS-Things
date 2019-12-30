1d56bb00df2ea748e31c2366e5b81c8253e3e765
hongye.jhy <hongye.jhy@alibaba-inc.com>
2019-12-30 19:42:03 +0800

    BugID:23266431:AOS code update for folder adjustment
    Change-Id: Iacd5be1de6385ab4d4fbc9c0de93df2503b43665

---
+++ components/utility/cjson/aos.mk
@@ -0,0 +1,11 @@
+NAME := cjson
+
+$(NAME)_MBINS_TYPE := share
+$(NAME)_VERSION := 1.0.1
+$(NAME)_SUMMARY := An ultra-lightweight, portable, single-file, simple-as-can-be ANSI-C compliant JSON parser, under MIT license.
+
+GLOBAL_INCLUDES += include
+# don't modify to L_CFLAGS, because CONFIG_CJSON_WITHOUT_DOUBLE should enable global
+#$(NAME)_CFLAGS  += -DCONFIG_CJSON_WITHOUT_DOUBLE
+
+$(NAME)_SOURCES := cJSON.c cJSON_Utils.c
