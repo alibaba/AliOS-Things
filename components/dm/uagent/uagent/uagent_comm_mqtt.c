26176dbb77d3c27758cf597a0c9fa687d830221f
hongye.jhy <hongye.jhy@alibaba-inc.com>
2019-12-30 19:41:00 +0800

    BugID:23266431:AOS code update for folder adjustment
    Change-Id: Ia2cf6b835cd545f05aa7606b85f9b520aabbcf9e

---
+++ components/dm/uagent/uagent/uagent_comm_mqtt.c
@@ -0,0 +1,108 @@
+/*
+ * Copyright (C) 2015-2019 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "linkkit/mqtt_api.h"
+#include "uagent_config.h"
+#include "uagent_inner_api.h"
+#include "aos/kernel.h"
+#include "aos/errno.h"
+
+static char mqtt_topic_up[UAGENT_TOPIC_MAX_LEN];
+static char mqtt_topic_dn[UAGENT_TOPIC_MAX_LEN];
+static on_recv_handler recv_handler = NULL;
+static char mqtt_comm_init = 0;
+static void *mqtt_client = NULL;
+
+static void uagent_handler(void *pcontext, void *pclient, iotx_mqtt_event_msg_pt msg)
+{
+    iotx_mqtt_topic_info_pt     ptopic_info = (iotx_mqtt_topic_info_pt)msg->msg;
+    UAGENT_DEBUG("---- UAGENT INFO RCV SUB TOPIC -----");
+    switch (msg->event_type) {
+    case IOTX_MQTT_EVENT_PUBLISH_RECEIVED:
+        /* print topic name and topic message */
+        UAGENT_DEBUG( "Topic: '%.*s' (Length: %d)",
+            ptopic_info->topic_len,
+            ptopic_info->ptopic,
+            ptopic_info->topic_len);
+        UAGENT_DEBUG( "Payload: '%.*s' (Length: %d)",
+            ptopic_info->payload_len,
+            ptopic_info->payload,
+            ptopic_info->payload_len);
+        if (ptopic_info->payload != NULL) {
+            recv_handler(ptopic_info->payload_len, ptopic_info->payload);
+        }
+        break;
+
+    case IOTX_MQTT_EVENT_DISCONNECT:
+        UAGENT_ERR("[uA]### MQTT DISCONNECT");
+        break;
+
+    case IOTX_MQTT_EVENT_RECONNECT:
+        UAGENT_INFO(">>> MQTT RECONNECT");
+        break;
+    default:
+        UAGENT_INFO("Unknown Event Type %d", msg->event_type);
+        break;
+    }
+}
+
+int uagent_ext_comm_init(const char pk[12], const char dn[36], on_recv_handler handler)
+{
+    int rc = -EINVAL;
+    /* todo retry if construct fail or subscribe fail */
+    if (0==mqtt_comm_init && NULL != pk && NULL != dn) {
+        UAGENT_INFO("[uA]prepare start uagent comm %s %s\n", pk, dn);
+        /* Construct a MQTT client with default parameter */
+        if ((mqtt_client = IOT_MQTT_Construct(NULL)) != NULL) {
+            snprintf(mqtt_topic_up, UAGENT_TOPIC_MAX_LEN, TOPIC_AGENT_INFO, pk, dn);
+            snprintf(mqtt_topic_dn, UAGENT_TOPIC_MAX_LEN, TOPIC_AGENT_MAN, pk, dn);
+#ifdef SP_BROADCAST_TOPIC
+            char broadcast_topic_dn[UAGENT_TOPIC_MAX_LEN];
+            snprintf(broadcast_topic_dn, UAGENT_TOPIC_MAX_LEN, "/broadcast/%s/deviceman", pk);
+#endif
+            recv_handler = handler;
+            UAGENT_INFO("[uA]Subsrcibe TOPIC %s\r\n", mqtt_topic_dn);
+            /* Subscribe the specific topic */
+            rc = IOT_MQTT_Subscribe(NULL, mqtt_topic_dn, IOTX_MQTT_QOS0, uagent_handler, NULL);
+            if (rc < 0) {
+                UAGENT_ERR("[uA]IOT_MQTT_Subscribe(%s) failed, rc = %d\r\n", mqtt_topic_dn, rc);
+            } else {
+                rc = 0;
+            }
+#ifdef SP_BROADCAST_TOPIC
+            UAGENT_INFO("[uA]Subsrcibe TOPIC %s", broadcast_topic_dn);
+            rc = IOT_MQTT_Subscribe(NULL, broadcast_topic_dn, IOTX_MQTT_QOS1, uagent_handler, NULL);
+            if (rc < 0) {
+                UAGENT_ERR("[uA]IOT_MQTT_Subscribe() failed, rc = %d\r\n", rc);
+            }
+#endif
+            mqtt_comm_init = 1;
+        } else {
+            UAGENT_ERR("[uA]### MQTT construct Fail");
+        }
+
+    } else {
+        UAGENT_ERR("[uA]invalid argument or uagent_comm already build");
+    }
+    return rc;
+}
+
+int uagent_ext_comm_publish(const unsigned short len, void* data, const bool conn_orient)
+{
+    return IOT_MQTT_Publish_Simple(NULL, mqtt_topic_up, conn_orient?IOTX_MQTT_QOS1:IOTX_MQTT_QOS0, data, len);
+}
+
+char check_connect()
+{
+    char rc = 0;
+    if(NULL!=mqtt_client){
+        rc = (1==IOT_MQTT_CheckStateNormal(mqtt_client))?1:0;
+    }
+    return rc;
+}
+
---
+++ components/dm/und/src/und_packet.c
@@ -0,0 +1,123 @@
+/*
+ * Copyright (C) 2017-2019 Alibaba Group Holding Limited
+ */
+
+#ifdef FEATURE_UND_SUPPORT
+
+#include "linkkit/wrappers/wrappers.h"
+
+#include "und_log.h"
+#include "und_types.h"
+#include "und_utils.h"
+#include "und_config.h"
+#include "und_packet.h"
+#include "und_platform.h"
+
+#if defined(__cplusplus)  /* If this is a C++ compiler, use C linkage */
+extern "C" {
+#endif
+
+struct und_packet_ctx_t {
+    void *mutex;
+    int packet_id;
+};
+
+static struct und_packet_ctx_t g_und_pkt_ctx = {0};
+
+static int und_packet_id()
+{
+    int id = UND_ERR;
+    struct und_packet_ctx_t *ctx = &g_und_pkt_ctx;
+
+    UND_PTR_SANITY_CHECK(ctx->mutex, id);
+
+    undp_mutex_lock(ctx->mutex);
+    id = ctx->packet_id ++;
+    if (ctx->packet_id == 0)
+        ctx->packet_id = 1;
+    undp_mutex_unlock(ctx->mutex);
+
+    return id;
+}
+
+int und_packet_ctx_init()
+{
+    struct und_packet_ctx_t *ctx = &g_und_pkt_ctx;
+    if (ctx->mutex) {
+        und_debug("und pkt ctx is ready inited\n");
+        return UND_SUCCESS;
+    }
+
+    ctx->mutex = undp_mutex_new();
+
+    UND_PTR_SANITY_CHECK(ctx->mutex, UND_MEM_ERR);
+
+    undp_mutex_lock(ctx->mutex);
+    ctx->packet_id = 1;
+    undp_mutex_unlock(ctx->mutex);
+    return UND_SUCCESS;
+}
+
+int und_packet_ctx_deinit()
+{
+    struct und_packet_ctx_t *ctx = &g_und_pkt_ctx;
+    void *mutex = ctx->mutex;
+
+    UND_PTR_SANITY_CHECK(ctx->mutex, UND_ERR);
+
+    undp_mutex_lock(mutex);
+    aos_memset(ctx, 0, sizeof(*ctx));
+    undp_mutex_unlock(mutex);
+    undp_mutex_free(mutex);
+
+    return UND_SUCCESS;
+}
+
+int und_build_topic(const char *fmt, char *topic_buf, int topic_buf_len)
+{
+    int len = 0;
+    char pk[IOTX_PRODUCT_KEY_LEN + 1] = {0};
+    char dn[IOTX_DEVICE_NAME_LEN + 1] = {0};
+
+    UND_PTR_SANITY_CHECK(topic_buf, UND_PARAM_ERR);
+    UND_PARAM_RANGE_SANITY_CHECK(topic_buf_len, UND_TOPIC_BUF_LEN_MAX, 1, UND_PARAM_ERR);
+
+    undp_get_product_key(pk);
+    undp_get_device_name(dn);
+
+    len = aos_snprintf(topic_buf, topic_buf_len, fmt, pk, dn);
+
+    return len;
+}
+
+int und_build_packet(const char *ver, const char *param, char *buf, int buf_len)
+{
+    int len = 0;
+
+    UND_PTR_SANITY_CHECK(ver, UND_PARAM_ERR);
+    UND_PTR_SANITY_CHECK(param, UND_PARAM_ERR);
+    UND_PTR_SANITY_CHECK(buf, UND_PARAM_ERR);
+    UND_PARAM_RANGE_SANITY_CHECK(buf_len, UND_REPORT_BUF_LEN_MAX, 1, UND_PARAM_ERR);
+
+    len = aos_snprintf(buf, buf_len, UND_REPORT_FMT, und_packet_id(), UND_ALINK_VER, param);
+
+    return len;
+}
+
+int und_build_packet_param(const char *content, char *param, int param_len)
+{
+    int len = 0;
+
+    UND_PTR_SANITY_CHECK(content, UND_PARAM_ERR);
+    UND_PTR_SANITY_CHECK(param, UND_PARAM_ERR);
+    UND_PARAM_RANGE_SANITY_CHECK(param_len, UND_REPORT_BUF_LEN_MAX, 1, UND_PARAM_ERR);
+
+    len = aos_snprintf(param, param_len, UND_REPORT_PARAM_FMT, content);
+
+    return len;
+}
+
+#if defined(__cplusplus)  /* If this is a C++ compiler, use C linkage */
+}
+#endif
+#endif
