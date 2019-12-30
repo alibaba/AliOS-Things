7ccaf9da484b1a2c986c83a8119ff7e1ffaef11d
hongye.jhy <hongye.jhy@alibaba-inc.com>
2019-12-30 23:20:31 +0800

    BugID:23266431:AOS code update for folder adjustmen
    Change-Id: Ifae2fd1b890b898d8e4e5bcfc23b528761842977

--- 3rdparty/experimental/gui/littlevGL/src/src.mk
+++
@@ -1,7 +0,0 @@
-include ./3rdparty/experimental/gui/littlevGL/src/lv_core/lv_core.mk
-include ./3rdparty/experimental/gui/littlevGL/src/lv_draw/lv_draw.mk
-include ./3rdparty/experimental/gui/littlevGL/src/lv_font/lv_font.mk
-include ./3rdparty/experimental/gui/littlevGL/src/lv_hal/lv_hal.mk
-include ./3rdparty/experimental/gui/littlevGL/src/lv_misc/lv_misc.mk
-include ./3rdparty/experimental/gui/littlevGL/src/lv_objx/lv_objx.mk
-include ./3rdparty/experimental/gui/littlevGL/src/lv_themes/lv_themes.mk
--- network/bluetooth/tmall_model/aos.mk
+++
@@ -1,9 +0,0 @@
-NAME := tmall_model
-
-$(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.1
-$(NAME)_SUMMARY := Tmall BLE Mesh model implementation.
-
-$(NAME)_COMPONENTS-y := bt_mesh
-
-$(NAME)_SOURCES-y := tmall_model.c
