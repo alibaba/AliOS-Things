30810babb2d7963103b317508fc0abf3780f9bd5
jianzhou <jianzhou.cjz@alibaba-inc.com>
2019-12-31 00:12:14 +0800

    BugID:23266668: lwm2m example simplified
    Change-Id: I548218b82bc3b1b6ea09d27a970a593759ddb018

--- components/network/lwm2m/aos.mk
+++ components/network/lwm2m/aos.mk
@@ -11,6 +11,10 @@ $(NAME)_INCLUDES += ./include \
 
 LWM2M_SRC_PATH := ./src/wakaama-1.0
 
+LWM2M_CLIENT_SRC_PATH = ./src/ref-impl/client
+
+LWM2M_SHARED_SRC_PATH = ./src/ref-impl/shared
+
 ifeq (y,$(LWM2M_BOOTSTRAP_CLIENT_SUPPORT))
 GLOBAL_DEFINES += LWM2M_BOOTSTRAP
 endif
@@ -31,6 +35,22 @@ $(NAME)_SOURCES := $(LWM2M_SRC_PATH)/block1.c \
                    $(LWM2M_SRC_PATH)/objects.c \
                    $(LWM2M_SRC_PATH)/registration.c \
                    $(LWM2M_SRC_PATH)/uri.c
+
+
+$(NAME)_SOURCES += $(LWM2M_CLIENT_SRC_PATH)/object_server.c   \
+                   $(LWM2M_CLIENT_SRC_PATH)/object_security.c \
+                   $(LWM2M_CLIENT_SRC_PATH)/object_firmware.c \
+                   $(LWM2M_CLIENT_SRC_PATH)/object_location.c \
+                   $(LWM2M_CLIENT_SRC_PATH)/object_device.c   \
+                   $(LWM2M_CLIENT_SRC_PATH)/lwm2mclient.c
+
+
+$(NAME)_SOURCES += $(LWM2M_SHARED_SRC_PATH)/platform.c \
+                   $(LWM2M_SHARED_SRC_PATH)/mbedconnection.c
+
+GLOBAL_INCLUDES += $(LWM2M_SHARED_SRC_PATH)
+
+$(NAME)_INCLUDES += $(LWM2M_CLIENT_SRC_PATH)
 
 
 $(NAME)_COMPONENTS += libcoap
---
+++ components/network/lwm2m/src/ref-impl/client/lwm2mclient.h
@@ -0,0 +1,80 @@
+/*******************************************************************************
+ *
+ * Copyright (c) 2014 Bosch Software Innovations GmbH, Germany.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v20.html
+ * The Eclipse Distribution License is available at
+ *    http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Bosch Software Innovations GmbH - Please refer to git log
+ *
+ *******************************************************************************/
+/*
+ * lwm2mclient.h
+ *
+ *  General functions of lwm2m test client.
+ *
+ *  Created on: 22.01.2015
+ *  Author: Achim Kraus
+ *  Copyright (c) 2015 Bosch Software Innovations GmbH, Germany. All rights reserved.
+ */
+
+#ifndef LWM2MCLIENT_H_
+#define LWM2MCLIENT_H_
+
+#include <lwm2m.h>
+
+/*
+ * object_device.c
+ */
+lwm2m_object_t * get_object_device(void);
+void free_object_device(lwm2m_object_t * objectP);
+uint8_t device_change(lwm2m_data_t * dataArray, lwm2m_object_t * objectP);
+void display_device_object(lwm2m_object_t * objectP);
+
+/*
+ * object_firmware.c
+ */
+lwm2m_object_t * get_object_firmware(void);
+void free_object_firmware(lwm2m_object_t * objectP);
+void display_firmware_object(lwm2m_object_t * objectP);
+
+/*
+ * object_location.c
+ */
+lwm2m_object_t * get_object_location(void);
+void free_object_location(lwm2m_object_t * object);
+void display_location_object(lwm2m_object_t * objectP);
+
+/*
+ * object_server.c
+ */
+lwm2m_object_t * get_server_object(int serverId, const char* binding, int lifetime, bool storing);
+void clean_server_object(lwm2m_object_t * object);
+void display_server_object(lwm2m_object_t * objectP);
+void copy_server_object(lwm2m_object_t * objectDest, lwm2m_object_t * objectSrc);
+
+/*
+ * lwm2mclient.c
+ */
+void handle_value_changed(lwm2m_context_t* lwm2mH, lwm2m_uri_t* uri, const char * value, size_t valueLength);
+/*
+ * system_api.c
+ */
+void init_value_change(lwm2m_context_t * lwm2m);
+
+/*
+ * object_security.c
+ */
+lwm2m_object_t * get_security_object(int serverId, const char* serverUri, char * bsPskId, char * psk, uint16_t pskLen, bool isBootstrap);
+void clean_security_object(lwm2m_object_t * objectP);
+char * get_server_uri(lwm2m_object_t * objectP, uint16_t secObjInstID);
+void display_security_object(lwm2m_object_t * objectP);
+void copy_security_object(lwm2m_object_t * objectDest, lwm2m_object_t * objectSrc);
+
+#endif /* LWM2MCLIENT_H_ */
