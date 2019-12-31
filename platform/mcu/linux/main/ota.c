5e26b7895ddbdc671cf432b94a66533a7aeb492f
zixun.lyq <zixun.lyq@alibaba-inc.com>
2019-12-31 20:24:18 +0800

    BugID:24237416:Support OTA for OS3.1 Release
    Change-Id: I4208b17932f8572eb3cf9f656a3d1e2843c2fec1

--- platform/mcu/linux/main/ota.c
+++ platform/mcu/linux/main/ota.c
@@ -7,7 +7,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
-#include "ota/ota_service.h"
+#include "ota/ota_hal.h"
 
 static FILE *ota_fd     = NULL;
 #if defined PLAT_RASPBERRY /*raspberry*/
