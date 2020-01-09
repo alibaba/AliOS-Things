e82c6edb7dd83b8dbdcfa03319d26a29740026c5
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-01-09 13:54:12 +0800

    BugID:23480136: [app]add hal app demo: gpio、timer;dir app
    Change-Id: Iac7f55aea130259613c1dd16b676d2e218a7b9a2

---
+++ application/example/halapp/k_app_config.h
@@ -0,0 +1,9 @@
+#ifndef K_APP_CONFIG_H
+#define K_APP_CONFIG_H
+
+/* user space */
+#ifndef RHINO_CONFIG_USER_SPACE
+#define RHINO_CONFIG_USER_SPACE              0
+#endif
+
+#endif /* K_APP_CONFIG_H */
