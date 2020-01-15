ba69b2b474722f0750392aef2a360db755a5035a
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-01-15 11:11:32 +0800

    BugID:24057741: add mal app
    Change-Id: I9782e31153adf31006d538f20b19420f074c07b3

---
+++ application/example/mal_app/aos.mk
@@ -0,0 +1,8 @@
+NAME := mal_app
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 1.0.1
+$(NAME)_SUMMARY := MQTT Adapter Layer (MAL) app
+$(NAME)_SOURCES := mal_app.c
+
+$(NAME)_COMPONENTS += libiot_sign cli yloop
