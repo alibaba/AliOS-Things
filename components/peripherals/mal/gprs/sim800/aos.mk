665ee77093c43259bfa30ce04e88e717c7b04768
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-01-15 09:58:00 +0800

    BugID:24057741: add gprs sim800 mal hal impl
    Change-Id: I580104dabdc05b24d78f551790b096c9f192dc5b

---
+++ components/peripherals/mal/gprs/sim800/aos.mk
@@ -0,0 +1,14 @@
+NAME := device_mal_sim800
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION := 1.0.1
+$(NAME)_SUMMARY := mal hal implmentation for sim800
+
+GLOBAL_DEFINES += DEV_MAL_SIM800
+GLOBAL_CFLAGS += -D__MAL_DEV_NAME=sim800
+
+$(NAME)_COMPONENTS += at
+
+$(NAME)_SOURCES += sim800.c
+
+GLOBAL_INCLUDES += ./
