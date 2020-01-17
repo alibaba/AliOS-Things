1323cd0f50b8022f8164a766bab802ca7894a69c
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-01-17 14:58:14 +0800

    BugID:24381709:[app]modify stm32f429 hal timer、hal pwm, and and and halapp;app
    Change-Id: I80e0b7238d3c8bea5b77556c870157b0a7e29bfa

--- application/example/halapp/halapp.c
+++ application/example/halapp/halapp.c
@@ -8,6 +8,7 @@
 #include "aos/init.h"
 #include "aos/hal/gpio.h"
 #include "aos/hal/timer.h"
+#include "aos/hal/pwm.h"
 #include "board.h"
 #include <k_api.h>
 
@@ -26,8 +27,16 @@
 #define HALAPP_GPIO_INT     9
 #endif
 
-#ifndef HALAPP_TIME_PORT
-#define HALAPP_TIME_PORT    3   //TIMER3
+#ifndef ON_BOARD_LED02
+#define ON_BOARD_LED02      23 //PB0
+#endif
+
+#ifndef PORT_TIMER5
+#define PORT_TIMER5         5   //logic num, define corresponding physical port for each board
+#endif
+
+#ifndef PORT_PWM_4
+#define PORT_PWM_4          3   //logic num, define corresponding physical port for each board
 #endif
 
 /*
@@ -41,6 +50,8 @@ static void hal_gpio_app_out(void)
     gpio_dev_t gpio_out; /*GPIO8*/
     gpio_dev_t gpio_in; /*GPIO9*/
 
+    printf("hal_gpio_app_out in\r\n");
+
     gpio_out.port = HALAPP_GPIO_OUT;
     gpio_out.config = OUTPUT_PUSH_PULL;
     gpio_out.priv = NULL;
@@ -63,6 +74,7 @@ static void hal_gpio_app_out(void)
         }
         aos_msleep(10);
     }
+
 }
 
 /*
@@ -76,6 +88,8 @@ static void hal_gpio_app_toggle(void)
     gpio_dev_t gpio_out; /*GPIO8*/
     gpio_dev_t gpio_in; /*GPIO9*/
 
+    printf("hal_gpio_app_toggle in\r\n");
+
     gpio_out.port = HALAPP_GPIO_OUT;
     gpio_out.config = OUTPUT_PUSH_PULL;
     gpio_out.priv = NULL;
@@ -98,6 +112,7 @@ static void hal_gpio_app_toggle(void)
         }
         aos_msleep(10);
     }
+
 }
 
 static void hal_gpio_int_fun(gpio_dev_t * gpio)
@@ -105,6 +120,7 @@ static void hal_gpio_int_fun(gpio_dev_t * gpio)
     if(NULL == gpio) {
         return;
     }
+
     hal_gpio_output_toggle(gpio);
 }
 
@@ -118,6 +134,9 @@ static void hal_gpio_app_int(void)
     if(NULL == pgpio_out) {
         return;
     }
+
+    printf("hal_gpio_app_int in\r\n");
+
     memset(pgpio_out,0, sizeof(gpio_dev_t));
     gpio_dev_t gpio_int; /*GPIO9*/
 
@@ -141,15 +160,18 @@ static void hal_timer_app_run(void)
     if(NULL == pgpio_out) {
         return;
     }
+
+    printf("hal_timer_app_run in\r\n");
+
     memset(pgpio_out,0, sizeof(gpio_dev_t));
 
-    pgpio_out->port = HALAPP_GPIO_OUT;
+    pgpio_out->port = ON_BOARD_LED02;
     pgpio_out->config = OUTPUT_PUSH_PULL;
     pgpio_out->priv = NULL;
     hal_gpio_init(pgpio_out);
 
-    time.port = HALAPP_TIME_PORT;
-    time.config.period = 20; //ms
+    time.port = PORT_TIMER5;
+    time.config.period = 1000*1000; //us
     time.config.reload_mode = TIMER_RELOAD_AUTO;
     time.config.cb = hal_gpio_int_fun;
     time.config.arg = pgpio_out;
@@ -158,9 +180,70 @@ static void hal_timer_app_run(void)
     if(ret){
         return;
     }
+
     hal_timer_start(&time);
 
+    aos_msleep(5000);
+    hal_timer_stop(&time);
 }
+
+/* output one pwm signal */
+static void hal_pwm_app_out(void)
+{
+    int32_t ret;
+    pwm_dev_t tim = {0};
+
+    printf("hal_pwm_app_out in\r\n");
+
+    tim.port = PORT_PWM_4;
+    tim.config.freq = 1000;
+    tim.config.duty_cycle = 0.5;
+    tim.priv = NULL;
+
+    ret = hal_pwm_init(&tim);
+    if(ret){
+        printf("hal_pwm_init fail,ret:%d\r\n",ret);
+        return;
+    }
+
+    hal_pwm_start(&tim);
+
+    aos_msleep(50000);
+
+    hal_pwm_stop(&tim);
+    hal_pwm_finalize(&tim);
+}
+
+static void hal_pwm_chg_args(void)
+{
+    int32_t ret;
+    pwm_dev_t tim = {0};
+
+    printf("hal_pwm_chg_args in\r\n");
+
+    pwm_config_t para = {0.25, 1000};
+    tim.port = PORT_PWM_4;
+    tim.config.freq = 1000;
+    tim.config.duty_cycle = 0.5;
+    tim.priv = NULL;
+
+    ret = hal_pwm_init(&tim);
+    if(ret){
+        return;
+    }
+
+    hal_pwm_start(&tim);
+
+    aos_msleep(50000);
+
+    hal_pwm_para_chg(&tim, para);
+
+    aos_msleep(50000);
+
+    hal_pwm_stop(&tim);
+    hal_pwm_finalize(&tim);
+}
+
 
 int application_start(int argc, char *argv[])
 {
@@ -168,6 +251,7 @@ int application_start(int argc, char *argv[])
 
     printf("hal app entry here!\r\n");
 
+    #if 1
     /*GPIO demo*/
     hal_gpio_app_out();
     hal_gpio_app_toggle();
@@ -176,6 +260,10 @@ int application_start(int argc, char *argv[])
     /*timer*/
     hal_timer_app_run();
 
+    /*pwm*/
+    hal_pwm_app_out();
+    hal_pwm_chg_args();
+    #endif
     printf("hal app end %d \r\n", count++);
 
     while(1) {
