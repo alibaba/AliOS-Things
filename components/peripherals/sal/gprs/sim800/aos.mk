049b8fc88d30f73a6a669432e73973af381f1d88
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-01-18 16:47:20 +0800

    BugID:23823314: refine mal/sal config.in
    Change-Id: Ic0f64e61320e5376bda674d98ec46b926a06ee0b

--- components/peripherals/sal/gprs/sim800/aos.mk
+++ components/peripherals/sal/gprs/sim800/aos.mk
@@ -7,8 +7,6 @@ $(NAME)_SUMMARY := sal hal implmentation for sim800
 GLOBAL_DEFINES += DEV_SAL_SIM800
 GLOBAL_CFLAGS += -D__SAL_DEV_NAME=sim800
 
-$(NAME)_COMPONENTS += yloop
-
 $(NAME)_SOURCES += sim800.c
 
 GLOBAL_INCLUDES += ./
