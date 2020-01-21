3629162cb172b305379f96beec4e54672b7db506
zhuqing.zq <zhuqing.zq@alibaba-inc.com>
2020-01-21 11:41:52 +0800

    BugID:24656372:modify example to example_legacy, board to board_legacy;platform
    Change-Id: Ibe54ce98633cca209ee246768b26e350f911feca

---
+++ platform/board/board_legacy/aaboard_demo/aos.mk
@@ -0,0 +1,44 @@
+NAME := board_aaboard_demo
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.0
+$(NAME)_SUMMARY    := configuration for board aaboard_demo
+MODULE             := 1062
+HOST_ARCH          := Cortex-M4
+HOST_MCU_FAMILY    := mcu_aamcu_demo
+SUPPORT_MBINS       := no
+HOST_MCU_NAME      := aamcu1_demo
+ENABLE_VFP         := 1
+
+$(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) kernel_init
+
+GLOBAL_CFLAGS  += -DSTM32F429xx -DCENTRALIZE_MAPPING
+
+$(NAME)_SOURCES += config/k_config.c       \
+                   config/partition_conf.c \
+                   drivers/msp_conf.c      \
+                   startup/board.c         \
+                   startup/startup.c
+
+ifeq ($(COMPILER), armcc)
+$(NAME)_SOURCES    += startup/startup_keil.s
+$(NAME)_LINK_FILES := startup/startup_keil.o
+GLOBAL_LDFLAGS += -L --scatter=platform/board/board_legacy/aaboard_demo/aaboard_demo.sct
+else ifeq ($(COMPILER), iar)
+$(NAME)_SOURCES    += startup/startup_iar.s
+GLOBAL_LDFLAGS += --config platform/board/board_legacy/aaboard_demo/aaboard_demo.icf
+else
+$(NAME)_SOURCES    += startup/startup_gcc.s
+GLOBAL_LDFLAGS += -T platform/board/board_legacy/aaboard_demo/aaboard_demo.ld
+endif
+
+GLOBAL_INCLUDES += .        \
+                   config/  \
+                   drivers/ \
+                   startup/
+
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_aaboard_demo
+CONFIG_SYSINFO_DEVICE_NAME   := aaboard_demo
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
--- platform/board/mt7682s/config/board.h
+++
@@ -1,4 +0,0 @@
-#define HARDWARE_REVISION   "V1.0"
-#define MODEL               "MTK7682"
-
-
