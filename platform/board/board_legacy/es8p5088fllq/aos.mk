b9bfee72b0bec25a7a32c2520a23d314724d6b00
zhuqing.zq <zhuqing.zq@alibaba-inc.com>
2020-01-21 11:41:52 +0800

    BugID:24656372:modify example to example_legacy, board to board_legacy;platform
    Change-Id: Ibe54ce98633cca209ee246768b26e350f911feca

---
+++ platform/board/board_legacy/es8p5088fllq/aos.mk
@@ -0,0 +1,39 @@
+NAME := board_es8p5088fllq
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.1
+$(NAME)_SUMMARY    := configuration for board es8p5088fllq
+MODULE             := 1062
+HOST_ARCH          := Cortex-M0
+HOST_MCU_FAMILY    := mcu_es8p508x
+SUPPORT_MBINS      := no
+HOST_MCU_NAME      := es8p508x
+ENABLE_VFP         := 0
+
+$(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) newlib_stub
+
+$(NAME)_SOURCES += config/k_config.c \
+                   startup/board.c   \
+                   startup/startup.c \
+                   drivers/irqhandler.c
+
+ifeq ($(COMPILER), armcc)
+$(NAME)_SOURCES    += startup/startup_keil.s
+$(NAME)_LINK_FILES := startup/startup_keil.o
+GLOBAL_LDFLAGS += -L --scatter=platform/board/board_legacy/es8p5088fllq/es8p508x.sct
+endif
+
+GLOBAL_INCLUDES += .    \
+                   config/   \
+                   drivers/  \
+                   startup/  \
+
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_ES8P5088FLLQ
+CONFIG_SYSINFO_DEVICE_NAME   := ES8P5088
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
+
+# Keil project support
+$(NAME)_KEIL_VENDOR = ES
+$(NAME)_KEIL_DEVICE = ES8P508xFLXX
