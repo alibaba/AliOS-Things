dceb0bdf2e274c2f36e31e3b3948036b468872d6
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-01 22:38:33 +0800

    BugID:24120957: add coap yts test case
    Change-Id: Iac418e4b5f9278d05c3d602cd7e13fc276cf84b1

---
+++ test/testcase/network/coap/aos.mk
@@ -0,0 +1,7 @@
+NAME := coap_yts_test
+
+$(NAME)_MBINS_TYPE := app
+$(NAME)_VERSION := 1.0.1
+$(NAME)_SUMMARY := YTS testcase for coap
+$(NAME)_COMPONENTS := libcoap
+$(NAME)_SOURCES := coap_yts_test.c
