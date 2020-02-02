9532abb2b9dffb14257642521f6b56745035cf21
Li Cheng <lc122798@alibaba-inc.com>
2020-02-03 02:13:03 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Ieb9fdb64277a833d50edff5162a3f44957752a95

--- platform/board/board_legacy/numaker-iot-m487/ucube.py
+++ platform/board/board_legacy/numaker-iot-m487/ucube.py
@@ -1,3 +1,3 @@
 
-linux_only_targets="blink coapapp helloworld http2app httpapp httpclient_app jsengine_app linkkit_gateway linkkitapp lwm2m_app meshapp modbus_app mqttapp otaapp udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp ulog_app websoc_app yloop_app yts"
+linux_only_targets="blink coapapp helloworld http2app httpapp httpclient_app httpdns_app jsengine_app linkkit_gateway linkkitapp lwm2m_app meshapp modbus_app mqttapp otaapp udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp ulog_app websoc_app yloop_app yts"
 windows_only_targets="helloworld|COMPILER=armcc linkkitapp|COMPILER=armcc"
