b0277ee072875486201e9c6de36d6a73bac9170a
Li Cheng <lc122798@alibaba-inc.com>
2020-02-03 02:13:03 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Ieb9fdb64277a833d50edff5162a3f44957752a95

--- platform/board/board_legacy/lpcxpresso54102/ucube.py
+++ platform/board/board_legacy/lpcxpresso54102/ucube.py
@@ -8,4 +8,4 @@ component = aos_board_component('board_lpcxpresso54102', 'lpc54102', src)
 component.add_global_macros('CPU_LPC54102J512BD64_cm4', '__USE_CMSIS', '__MULTICORE_MASTER', '__NEWLIB__')
 aos_global_config.arch = 'Cortex-M4'
 
-linux_only_targets="at_app coapapp debug_app helloworld http2app jsengine_app kernel_app linkkit_gateway linkkitapp modbus_app mqttapp otaapp sal_app udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udataapp ulocation.baseapp ulog_app yloop_app yts"
+linux_only_targets="at_app coapapp debug_app helloworld http2app kernel_app linkkit_gateway linkkitapp modbus_app mqttapp otaapp sal_app udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udataapp ulocation.baseapp ulog_app yloop_app yts"
