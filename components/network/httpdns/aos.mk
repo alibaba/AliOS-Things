281eea3d7ee9c687ac2e83db119eeacaeb319551
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-03 17:23:04 +0800

    BugID:24671568: extract posix dependency from httpdns
    Change-Id: I99d46caf0a88dd53d107d3a7d69e87b720c949f2

--- components/network/httpdns/aos.mk
+++ components/network/httpdns/aos.mk
@@ -4,11 +4,13 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := httpdns component
 
-$(NAME)_SOURCES := ./src/httpdns.c    \
-                   ./src/iplist.c     \
-                   ./src/dict.c       \
-                   ./src/http_ctx.c   \
-                   ./src/sds.c
+$(NAME)_SOURCES := ./src/httpdns.c         \
+                   ./src/iplist.c          \
+                   ./src/dict.c            \
+                   ./src/http_ctx.c        \
+                   ./src/sds.c             \
+                   ./src/httpdns_aos.c     \
+                   ./src/httpdns_opts.c    \
 
 $(NAME)_INCLUDES += ./include/
 
--- components/network/httpdns/include/httpdns_opts.h
+++ components/network/httpdns/include/httpdns_opts.h
@@ -9,8 +9,24 @@
 #define HTTP_DNS_USER_ID "100000"
 #endif
 
+#ifndef HTTP_DNS_TIMEOUT
+#define HTTP_DNS_TIMEOUT 10
+#endif
+
+#ifndef HTTP_DNS_CONNECT_TIMEOUT
+#define HTTP_DNS_CONNECT_TIMEOUT 5
+#endif
+
+#ifndef HTTP_DNS_MAX_CACHE_AGE
+#define HTTP_DNS_MAX_CACHE_AGE (15 * 60)
+#endif
+
+#ifndef HTTP_DNS_MAX_SERVER_NUM
+#define HTTP_DNS_MAX_SERVER_NUM 6
+#endif
+
 #ifndef HTTP_DNS_TASK_PRIO
-#define HTTP_DNS_TASK_PRIO  32
+#define HTTP_DNS_TASK_PRIO   32
 #endif
 
 #ifndef HTTP_DNS_TASK_STACK
@@ -20,14 +36,5 @@
 #ifndef HTTP_DNS_QRY_INTV_MS
 #define HTTP_DNS_QRY_INTV_MS 50
 #endif
-
-const char server_list[ ][16] = {
-    "203.107.1.1",
-    "203.107.1.33",
-    "203.107.1.34",
-    "203.107.1.35",
-    "203.107.1.65",
-    "203.107.1.66"
-};
 
 #endif
