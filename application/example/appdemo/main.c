9dc842788a38bb637a7e9d0c9b468b44459247f6
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-03 13:54:59 +0800

    BugID:24656372:add example appdemo , board stm32f103rb-demo;app
    Change-Id: I02991344532914cced733ae532b4dc4570e8def3

---
+++ application/example/appdemo/main.c
@@ -0,0 +1,61 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <aos/kernel.h>
+#include "aos/init.h"
+#include "board.h"
+#include <k_api.h>
+
+#ifndef AOS_BINS
+extern int application_start(int argc, char *argv[]);
+#endif
+
+/*
+main task stask size(byte)
+*/
+#define OS_MAIN_TASK_STACK 1024
+
+/*
+If board have no component for example board_xx_init, it indicates that this app does not support this board.
+Set the correspondence in file platform\board\aaboard_demo\ucube.py.
+*/
+extern void board_basic_init(void);
+
+/*  For user config
+    kinit.argc = 0;
+    kinit.argv = NULL;
+    kinit.cli_enable = 1;
+*/
+static kinit_t kinit = {0, NULL, 1};
+static ktask_t *g_main_task;
+
+void maintask(void)
+{
+    board_init();
+    aos_components_init(&kinit);
+
+#ifndef AOS_BINS
+    application_start(kinit.argc, kinit.argv);  /* jump to app entry */
+#endif
+}
+
+int main(void)
+{
+    /* board basic init: CLK, heap, define in board\aaboard_demo\startup\board.c */
+    board_basic_init();
+
+    /* kernel init, malloc can use after this! */
+    aos_init();
+
+    /* main task to run */
+    aos_task_new("main_task", maintask, NULL ,OS_MAIN_TASK_STACK);
+
+    /* kernel start schedule! */
+    aos_start();
+
+    /* never run here */
+    return 0;
+}
