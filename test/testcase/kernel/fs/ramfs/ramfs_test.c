bcfd500f98e721e41406c1f012fb252cefa787dd
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-03 22:51:57 +0800

    BugID:23823143: Added testcase for ramfs
    Change-Id: Ifbdd74e0503b8ef85a5563e1776ad3da0b013b5a

--- test/testcase/kernel/fs/ramfs/ramfs_test.c
+++ test/testcase/kernel/fs/ramfs/ramfs_test.c
@@ -14,6 +14,8 @@
 #include "yunit/yts.h"
 
 extern int32_t ramfs_register(const char *mount_path);
+extern int32_t ramfs_unregister(const char *mount_path);
+
 
 static const char *g_string         = "ramfs test string.";
 static const char *g_filepath       = "/RAMFS/test.txt";
@@ -22,6 +24,141 @@ static const char *g_dirtest_1      = "/RAMFS/testDir/test_1.txt";
 static const char *g_dirtest_2      = "/RAMFS/testDir/test_2.txt";
 static const char *g_dirtest_3      = "/RAMFS/testDir/test_3.txt";
 static const char *g_new_filepath   = "/RAMFS/testDir/newname.txt";
+
+/**
+ * ramfs has been registered when init the test case.
+ * here we need to unregister ramfs firstly, and before
+ * leaving the testcase, register ramfs again.
+ */
+static void test_ramfs_mount(void)
+{
+    int ret;
+    const int cnt = 100;
+
+    printf("ramfs register/unregister test start\r\n");
+
+    ret = ramfs_unregister(NULL);
+    YUNIT_ASSERT(ret == 0);
+
+    for (int i = 0; i < cnt; i++) {
+        ret = ramfs_register(NULL);
+        YUNIT_ASSERT(ret == 0);
+
+        ret = ramfs_unregister(NULL);
+        YUNIT_ASSERT(ret == 0);
+    }
+
+    ret = ramfs_register(NULL);
+    YUNIT_ASSERT(ret == 0);
+
+    printf("ramfs register/unregister test OK\r\n");
+}
+
+static void test_ramfs_rw(void)
+{
+    int fd;
+    int *buf;
+    int buf_len;
+    int write_len, read_len;
+    int temp_size;
+    int ret;
+    const int file_size = 328 * sizeof(int);
+
+    printf("ramfs test_ramfs_rw test start\r\n");
+
+    fd = aos_open(g_filepath, O_WRONLY | O_CREAT | O_TRUNC);
+    YUNIT_ASSERT(fd > 0);
+
+    buf_len = 32*sizeof(int);
+    buf = malloc(buf_len);
+    YUNIT_ASSERT(buf != NULL);
+    memset(buf, 0, buf_len);
+    for (int i = 0; i < buf_len/sizeof(int); i++) {
+        buf[i] = i;
+    }
+
+    for (write_len = 0; write_len < file_size; ) {
+        int remain = file_size - write_len;
+        temp_size = (remain > buf_len)? buf_len: remain;
+        ret = aos_write(fd, buf, temp_size);
+        YUNIT_ASSERT(ret  == temp_size);
+        write_len += temp_size;
+    }
+
+    ret = aos_sync(fd);
+    YUNIT_ASSERT(ret == 0);
+    ret = aos_close(fd);
+    YUNIT_ASSERT(ret == 0);
+
+    fd = aos_open(g_filepath, O_RDONLY);
+    YUNIT_ASSERT(fd > 0);
+
+    int file_end = aos_lseek(fd, 0, SEEK_END);
+    YUNIT_ASSERT(file_end == file_size - 1);
+
+    ret = aos_lseek(fd, 0, SEEK_SET);
+    YUNIT_ASSERT(ret == 0);
+
+    int ctx_err = 0;
+    for (read_len = 0; read_len < file_size; ) {
+        int remain = file_size - read_len;
+        temp_size = (remain > buf_len)? buf_len: remain;
+        ret = aos_read(fd, buf, temp_size);
+        YUNIT_ASSERT(ret  == temp_size);
+        for (int i = 0; i < temp_size/sizeof(int); i++) {
+            if (buf[i] != i) {
+                ctx_err = 1;
+                break;
+            }
+        }
+        if (ctx_err != 0) {
+            break;
+        }
+        read_len += temp_size;
+    }
+
+    YUNIT_ASSERT(ctx_err == 0);
+
+    ret = aos_close(fd);
+    YUNIT_ASSERT(ret == 0);
+
+    ret = aos_unlink(g_filepath);
+    YUNIT_ASSERT(ret == 0);
+
+    printf("ramfs test_ramfs_rw test OK\r\n");
+}
+
+static void test_ramfs_dir(void)
+{
+    int ret;
+    int fd;
+
+    ret = aos_mkdir(g_dirpath);
+    YUNIT_ASSERT(ret == 0);
+
+    /* the dir already exists, returan fail */
+    ret = aos_mkdir(g_dirpath);
+    YUNIT_ASSERT(ret != 0);
+
+    fd = aos_open(g_dirtest_1, O_RDWR | O_CREAT);
+    YUNIT_ASSERT(fd > 0);
+
+    ret = aos_write(fd, g_string, strlen(g_string));
+    YUNIT_ASSERT(ret > 0);
+
+    ret = aos_close(fd);
+    YUNIT_ASSERT(ret == 0);
+
+    ret = aos_rmdir(g_dirpath);
+    YUNIT_ASSERT(ret != 0);
+
+    ret = aos_unlink(g_dirtest_1);
+    YUNIT_ASSERT(ret == 0);
+
+    ret = aos_rmdir(g_dirpath);
+    YUNIT_ASSERT(ret == 0);
+
+}
 
 static void test_ramfs_case(void)
 {
@@ -42,6 +179,7 @@ static void test_ramfs_case(void)
         aos_close(fd);
     }
 
+
     /* ramfs read test */
     fd = aos_open(g_filepath, O_RDONLY);
     YUNIT_ASSERT(fd > 0);
@@ -54,6 +192,10 @@ static void test_ramfs_case(void)
 
         aos_close(fd);
     }
+
+    /* ramfs mkdir test */
+    ret = aos_mkdir(g_dirpath);
+    YUNIT_ASSERT(ret == 0);
 
     /* ramfs readdir test */
     fd = aos_open(g_dirtest_1, O_RDWR | O_CREAT | O_TRUNC);
@@ -86,6 +228,7 @@ static void test_ramfs_case(void)
     }
     aos_closedir(dp);
 
+
     /* ramfs rename test */
     ret = aos_rename(g_filepath, g_new_filepath);
     YUNIT_ASSERT(ret == 0);
@@ -116,11 +259,17 @@ static int init(void)
 
     ret = ramfs_register(NULL);
     YUNIT_ASSERT(ret == 0);
+
     return 0;
 }
 
 static int cleanup(void)
 {
+    int ret;
+
+    ret = ramfs_unregister(NULL);
+    YUNIT_ASSERT(ret == 0);
+
     return 0;
 }
 
@@ -135,6 +284,9 @@ static void teardown(void)
 }
 
 static yunit_test_case_t aos_ramfs_testcases[] = {
+    {"mount_test", test_ramfs_mount},
+    {"read_write_test", test_ramfs_rw},
+    {"dir_operation", test_ramfs_dir},
     { "ramfs_test", test_ramfs_case},
     YUNIT_TEST_CASE_NULL
 };
