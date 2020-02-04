af00dd2abbfd892010bebd28426cfbf0363f7393
Li Cheng <lc122798@alibaba-inc.com>
2020-02-04 18:01:55 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: I6639aee912a4252ee508803efc5d2221c143c864

--- platform/board/board_legacy/esp32sram/ucube.py
+++ platform/board/board_legacy/esp32sram/ucube.py
@@ -6,4 +6,4 @@ if aos_global_config.get('hci_h4', 0):
     component.add_global_macros('CONFIG_BLE_HCI_H4_UART_PORT=1')
 
 
-linux_only_targets="blink bluetooth.bleadv bluetooth.bleperipheral bluetooth.breezeapp coapapp debug_app helloworld http2app httpapp httpclient_app jsengine_app kernel_app linkkit_gateway linkkitapp lwm2m_app meshapp mqttapp otaapp udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udataapp ulocation.baseapp ulog_app websoc_app yloop_app yts"
+linux_only_targets="blink bluetooth.bleadv bluetooth.bleperipheral bluetooth.breezeapp coapapp debug_app helloworld http2app httpapp httpclient_app httpdns_app jsengine_app kernel_app linkkit_gateway linkkitapp lwm2m_app meshapp mqttapp otaapp udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udataapp ulocation.baseapp ulog_app websoc_app yloop_app yts"
