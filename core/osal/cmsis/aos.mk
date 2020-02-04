41460a32fdc146f46ca949c68275e90e2e8b4eca
magao.xwj <magao.xwj@alibaba-inc.com>
2020-02-04 16:57:39 +0800

    BugID:24814458:upgrade component version for publish
    Change-Id: Ic79fb34689890ffd97ca6634d2be7fd6d4710ee1

--- core/osal/cmsis/aos.mk
+++ core/osal/cmsis/aos.mk
@@ -1,7 +1,7 @@
 NAME := osal_cmsis
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.1
+$(NAME)_VERSION    := 1.0.2
 $(NAME)_SUMMARY    := cmsis for alios things
 #if component's header files under another directory, add RPM_INCLUDE_DIR to indicate where the header file folder is located
 RPM_INCLUDE_DIR    := cmsis
