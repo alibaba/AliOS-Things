cd3f1d20c5bf27fa47e6cba9b0de8d4fa8f172d5
magao.xwj <magao.xwj@alibaba-inc.com>
2020-02-04 16:55:58 +0800

    BugID:24814458:upgrade component version for publish
    Change-Id: I21074fbe1ed59c32c79b889673f14201e5a619df
    Reviewed-on: http://yuncode.alibaba-inc.com/c/aos/code/security/+/253899
    Tested-by: <iot-scm@alibaba-inc.com>
    Reviewed-by: 徐文杰 magao.xwj <magao.xwj@alibaba-inc.com>

--- components/security/irot/tee/aos.mk
+++ components/security/irot/tee/aos.mk
@@ -2,5 +2,5 @@ NAME := libkm_tee
 $(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/libkm_tee.a
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.0
+$(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := secure os for IoT device
