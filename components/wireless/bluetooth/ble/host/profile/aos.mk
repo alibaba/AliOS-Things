2aefb15fa736d7d923ff11f29dffe5db7832bb22
magao.xwj <magao.xwj@alibaba-inc.com>
2020-02-04 16:57:22 +0800

    BugID:24814458:upgrade component version for publish
    Change-Id: I658c6c8623cb4de735a06a363793190bb1ab0d50

--- components/wireless/bluetooth/ble/host/profile/aos.mk
+++ components/wireless/bluetooth/ble/host/profile/aos.mk
@@ -1,7 +1,7 @@
 NAME := bt_profile
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.1
+$(NAME)_VERSION := 1.0.2
 $(NAME)_SUMMARY := BLE commonly used profiles.
 
 $(NAME)_SOURCES-y := bas.c cts.c dis.c hrs.c
