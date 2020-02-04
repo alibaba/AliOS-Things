68d84f6a86732c5ba9b438a9dd557794d0bcbfbf
magao.xwj <magao.xwj@alibaba-inc.com>
2020-02-04 16:57:39 +0800

    BugID:24814458:upgrade component version for publish
    Change-Id: Ic79fb34689890ffd97ca6634d2be7fd6d4710ee1

--- core/init/aos.mk
+++ core/init/aos.mk
@@ -1,7 +1,7 @@
 NAME := kernel_init
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.1
+$(NAME)_VERSION    := 1.0.2
 $(NAME)_SUMMARY    := kernel init unit
 
 $(NAME)_SOURCES := aos_init.c
--- core/mbins/kmbins/aos.mk
+++ core/mbins/kmbins/aos.mk
@@ -1,7 +1,7 @@
 NAME := kmbins
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.1
+$(NAME)_VERSION    := 1.0.2
 $(NAME)_SUMMARY    := mbins for kernel
 
 $(NAME)_CFLAGS += -Wall -Werror
