5ce58dc7a7dcdd8b410fac5a16cd006de2bb0dfc
magao.xwj <magao.xwj@alibaba-inc.com>
2020-02-04 16:53:25 +0800

    BugID:24814458:upgrade component version for publish
    Change-Id: Idc923ee2b1dbf3bff4623d98d892016cf7e66741

--- components/fs/uffs/aos.mk
+++ components/fs/uffs/aos.mk
@@ -1,7 +1,7 @@
 NAME := uffs
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.1
+$(NAME)_VERSION    := 1.0.2
 $(NAME)_SUMMARY    := Ultra-low-cost Flash File System
 
 $(NAME)_SOURCES +=  uffs_aos.c 
