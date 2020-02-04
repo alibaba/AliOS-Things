969fbfb19d2e1e3d334384920936de8be3a11be5
magao.xwj <magao.xwj@alibaba-inc.com>
2020-02-04 16:32:15 +0800

    BugID:24814458:upgrade component version for publish
    Change-Id: Iaba8df25471f381129fe25ad6729e1535ec6f0e7

--- application/example/example_legacy/uart/aos.mk
+++ application/example/example_legacy/uart/aos.mk
@@ -1,7 +1,7 @@
 NAME := uart
 
 $(NAME)_MBINS_TYPE := app
-$(NAME)_VERSION := 1.0.0
+$(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := this is a uart sample form hal uart.h
 $(NAME)_SOURCES := uart.c
 
--- application/example/example_legacy/udata_demo/udata_cloud_demo/aos.mk
+++ application/example/example_legacy/udata_demo/udata_cloud_demo/aos.mk
@@ -1,7 +1,7 @@
 NAME := udata_cloud_demo
 
 $(NAME)_MBINS_TYPE := app
-$(NAME)_VERSION    := 1.0.0
+$(NAME)_VERSION    := 1.0.1
 $(NAME)_SUMMARY    := uData Cloud demo
 
 $(NAME)_SOURCES :=   udata_cloud_demo.c
