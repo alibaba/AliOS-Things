f735f811a424deff622b7b82bc77332c15f075b0
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-05 15:18:21 +0800

    BugID:24656372:sort board esp8266 for init codess,add app_nomain app;board
    Change-Id: I6494c45ecbae5e8f7e9372b061d1eae9fa3a0d55

---
+++ platform/board/esp8266-demo/startup/board.c
@@ -0,0 +1,79 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "aos/hal/uart.h"
+#include "network/hal/wifi.h"
+
+#include "k_config.h"
+#include "board.h"
+
+uart_dev_t uart_0 = { .port = 0,};
+
+extern hal_wifi_module_t aos_wifi_esp8266;
+extern char _bss_start;
+extern char _bss_end;
+extern int32_t hal_uart_init(uart_dev_t *uart);
+extern void key_gpio_init(void);
+extern int ets_printf(const char *fmt, ...);
+extern void PendSV( char req );
+
+/**
+  * @general board init entry board_basic_init
+  * @retval None
+  */
+void board_basic_init(void)
+{
+
+}
+
+/**
+  * @general board tick init entry board_tick_init
+  * @retval None
+  */
+void board_tick_init(void)
+{
+
+}
+
+/**
+* @init the default uart init example.
+*/
+void board_stduart_init(void)
+{
+    hal_uart_init(&uart_0);
+}
+
+/**
+  * Enable DMA controller clock
+  */
+void board_dma_init(void)
+{
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+void board_gpio_init(void)
+{
+    key_gpio_init();
+}
+
+/**
+  * @brief WIFI Initialization Function
+  * @param None
+  * @retval None
+  */
+void board_wifi_init(void)
+{
+    int ret;
+    hal_wifi_register_module(&aos_wifi_esp8266);
+    ret = hal_wifi_init();
+    if (ret){
+        printf("waring: wifi init fail ret is %d \r\n", ret);
+    }
+}
+
