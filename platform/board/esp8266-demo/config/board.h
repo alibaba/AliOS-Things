0e66117e0b4780b167757d06de8f07cd7254d55f
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-06 17:43:44 +0800

    BugID:24656372:sort hal app to seperate according macro;board
    Change-Id: I5832c3359bb378942b81fc3bd0108f911108a78e

--- platform/board/esp8266-demo/config/board.h
+++ platform/board/esp8266-demo/config/board.h
@@ -9,4 +9,25 @@
 #define AOS_MAIN_TASK_STACK_SIZE (6*1024)
 #endif
 
+/*
+    hal support list
+*/
+#define AOS_HAL_GPIO_ENABLED        1
+#define AOS_HAL_UART_ENABLED        1
+#define AOS_HAL_SPI_ENABLED         0
+#define AOS_HAL_I2C_ENABLED         1
+#define AOS_HAL_FLASH_ENABLED       1
+#define AOS_HAL_PWM_ENABLED         0
+#define AOS_HAL_TIMER_ENABLED       1
+#define AOS_HAL_WDG_ENABLED         1
+#define AOS_HAL_RTC_ENABLED         0
+#define AOS_HAL_ADC_ENABLED         1
+#define AOS_HAL_DAC_ENABLED         0
+#define AOS_HAL_RNG_ENABLED         0
+#define AOS_HAL_I2S_ENABLED         0
+#define AOS_HAL_QSPI_ENABLED        0
+#define AOS_HAL_CAN_ENABLED         0
+#define AOS_HAL_NAND_ENABLED        0
+#define AOS_HAL_NOR_ENABLED         0
+
 #endif /* __BOARD_H */
