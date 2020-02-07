76a9fd4e707221c952c2bc2785ef15ed6576d301
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-07 15:08:48 +0800

    BugID:24656372:sort hal gpio\led\key defined and modify halapp;board
    Change-Id: Ic43cf399118095b75c8d7c69ba98f344789166d0

--- platform/board/esp8266-demo/config/board.h
+++ platform/board/esp8266-demo/config/board.h
@@ -1,6 +1,26 @@
 #ifndef __BOARD_H
 #define __BOARD_H
 
+typedef enum {
+    PORT_TIMER_0,
+    PORT_TIMER_1,
+    PORT_TIMER_2,
+    PORT_TIMER_3,
+    PORT_TIMER_4,
+    PORT_TIMER_5,
+    PORT_TIMER_SIZE,
+    PORT_TIMER_INVALID = 255
+};
+
+typedef enum{
+    PORT_PWM_1,
+    PORT_PWM_2,
+    PORT_PWM_3,
+    PORT_PWM_4,
+    PORT_PWM_SIZE,
+    PORT_PWM_INVALID = 255,
+}PORT_PWM_TYPE;
+
 #if defined(SUPPORT_SINGAPORE_DOMAIN)
 #define AOS_MAIN_TASK_STACK_SIZE (7.5*1024)
 #elif defined(ESP8266_CHIPSET)
@@ -10,7 +30,7 @@
 #endif
 
 /*
-    hal support list
+hal support list
 */
 #define AOS_HAL_GPIO_ENABLED        1
 #define AOS_HAL_UART_ENABLED        1
@@ -29,5 +49,11 @@
 #define AOS_HAL_CAN_ENABLED         0
 #define AOS_HAL_NAND_ENABLED        0
 #define AOS_HAL_NOR_ENABLED         0
+
+//LED
+#define LED2    0xff //
+
+//KEY
+#define KEY1    14  //GPIOA-PIN14
 
 #endif /* __BOARD_H */
