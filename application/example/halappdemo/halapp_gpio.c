a5a8531d528632b1b12b96f7e1444829566623f6
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-07 18:18:52 +0800

    BugID:24843276: code clean for halapp_gpio
    Change-Id: I5f08c2496531f9f8623a5fcbd32059d6236e82a8

--- application/example/halappdemo/halapp_gpio.c
+++ application/example/halappdemo/halapp_gpio.c
@@ -11,57 +11,42 @@
 
 #if (AOS_HAL_GPIO_ENABLED > 0)
 
-/* gpio app macro default value
-   need to ajust for each board
-*/
-#ifndef HALAPP_GPIO_OUT
-#define HALAPP_GPIO_OUT     8   //PA8
-#endif
-
-#ifndef HALAPP_GPIO_IN
-#define HALAPP_GPIO_IN      9   //PA9
-#endif
-
-#ifndef HALAPP_GPIO_INT
-#define HALAPP_GPIO_INT     9   //PA9
-#endif
-
 /*
 Set gpio_out equal to gpio_in
 */
 void hal_gpio_app_out(void)
 {
-    int cnt = 20;
-    int ret;
-    uint32_t val;
-    gpio_dev_t gpio_out; /*GPIO8*/
-    gpio_dev_t gpio_in; /*GPIO9*/
+    int        cnt = 20;
+    uint32_t   val;
+    gpio_dev_t gpio_out;
+    gpio_dev_t gpio_in;
+    int        ret;
 
-    printf("hal_gpio_app_out in\r\n");
+    printf("hal_gpio_app_out start\r\n");
 
-    gpio_out.port = HALAPP_GPIO_OUT;
+    gpio_out.port   = HALAPP_GPIO_OUT;
     gpio_out.config = OUTPUT_PUSH_PULL;
-    gpio_out.priv = NULL;
+    gpio_out.priv   = NULL;
     hal_gpio_init(&gpio_out);
 
-    gpio_in.port = HALAPP_GPIO_IN;
+    gpio_in.port   = HALAPP_GPIO_IN;
     gpio_in.config = INPUT_HIGH_IMPEDANCE;
-    gpio_in.priv = NULL;
+    gpio_in.priv   = NULL;
     hal_gpio_init(&gpio_in);
 
-    while((cnt--) > 0){
+    while ((cnt--) > 0) {
         ret = hal_gpio_input_get(&gpio_in, &val);
-
-        if(0 == ret) {
-            if(0 == val){
+        if (0 == ret) {
+            if (0 == val) {
                 hal_gpio_output_low(&gpio_out);
-            }else if( 1 == val ){
+            } else if (1 == val) {
                 hal_gpio_output_high(&gpio_out);
             }
         }
-        aos_msleep(10);
+        aos_msleep(1000);
     }
 
+    printf("hal_gpio_app_out end\r\n");
 }
 
 /*
@@ -69,42 +54,45 @@ Set gpio_out opposite to gpio_in
 */
 void hal_gpio_app_toggle(void)
 {
-    int cnt = 20;
-    int ret;
-    uint32_t val;
-    gpio_dev_t gpio_out; /*GPIO8*/
-    gpio_dev_t gpio_in; /*GPIO9*/
+    int        cnt = 20;
+    uint32_t   val;
+    gpio_dev_t gpio_out;
+    gpio_dev_t gpio_in;
+    int        ret;
 
-    printf("hal_gpio_app_toggle in\r\n");
+    printf("hal_gpio_app_toggle start\r\n");
 
-    gpio_out.port = HALAPP_GPIO_OUT;
+    gpio_out.port   = HALAPP_GPIO_OUT;
     gpio_out.config = OUTPUT_PUSH_PULL;
-    gpio_out.priv = NULL;
+    gpio_out.priv   = NULL;
     hal_gpio_init(&gpio_out);
 
-    gpio_in.port = HALAPP_GPIO_IN;
+    gpio_in.port   = HALAPP_GPIO_IN;
     gpio_in.config = INPUT_HIGH_IMPEDANCE;
-    gpio_in.priv = NULL;
+    gpio_in.priv   = NULL;
     hal_gpio_init(&gpio_in);
 
-    while((cnt--) > 0){
+    while ((cnt--) > 0) {
         ret = hal_gpio_input_get(&gpio_in, &val);
 
-        if(0 == ret) {
-            if(0 == val){
+        if (0 == ret) {
+            if (0 == val) {
                 hal_gpio_output_high(&gpio_out);
-            }else if( 1 == val ){
+            } else if (1 == val) {
                 hal_gpio_output_low(&gpio_out);
             }
         }
-        aos_msleep(10);
+        aos_msleep(1000);
     }
 
+    printf("hal_gpio_app_toggle end\r\n");
 }
 
-static void hal_gpio_int_fun(gpio_dev_t * gpio)
+static gpio_dev_t g_gpio_out;
+
+static void hal_gpio_int_fun(gpio_dev_t *gpio)
 {
-    if(NULL == gpio) {
+    if (NULL == gpio) {
         return;
     }
 
@@ -114,28 +102,30 @@ static void hal_gpio_int_fun(gpio_dev_t * gpio)
 /* toggle gpio_out in interrupt of gpio_int */
 void hal_gpio_app_int(void)
 {
-    int cnt = 20;
-    int ret;
-    uint32_t val;
-    gpio_dev_t * pgpio_out = malloc(sizeof(gpio_dev_t)); /*GPIO8*/
-    if(NULL == pgpio_out) {
-        return;
-    }
+    uint32_t   val;
+    gpio_dev_t gpio_int;
+    int        ret;
 
-    printf("hal_gpio_app_int in\r\n");
+    printf("hal_gpio_app_int start\r\n");
 
-    memset(pgpio_out,0, sizeof(gpio_dev_t));
-    gpio_dev_t gpio_int; /*GPIO9*/
+    memset(&g_gpio_out, 0, sizeof(gpio_dev_t));
 
-    pgpio_out->port = HALAPP_GPIO_OUT;
-    pgpio_out->config = OUTPUT_PUSH_PULL;
-    pgpio_out->priv = NULL;
-    hal_gpio_init(pgpio_out);
+    g_gpio_out.port   = HALAPP_GPIO_OUT;
+    g_gpio_out.config = OUTPUT_PUSH_PULL;
+    g_gpio_out.priv   = NULL;
+    hal_gpio_init(&g_gpio_out);
 
-    gpio_int.port = HALAPP_GPIO_IN;
+    gpio_int.port   = HALAPP_GPIO_IN;
     gpio_int.config = IRQ_MODE;
-    gpio_int.priv = NULL;
-    hal_gpio_enable_irq(&gpio_int, IRQ_TRIGGER_RISING_EDGE, hal_gpio_int_fun, pgpio_out);
+    gpio_int.priv   = NULL;
+    hal_gpio_enable_irq(&gpio_int, IRQ_TRIGGER_RISING_EDGE, hal_gpio_int_fun, &g_gpio_out);
+
+    /* sleep 10s */
+    aos_msleep(10000);
+
+    hal_gpio_disable_irq(&gpio_int);
+
+    printf("hal_gpio_app_int end\r\n");
 }
 
 #endif
