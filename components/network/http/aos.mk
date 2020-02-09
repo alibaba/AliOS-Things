27785b659279f08fe595b626f79ffcfdeb0429d9
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-09 17:36:42 +0800

    BugID:24820156: Support legacy app and board, update http client component for config sample
    Change-Id: I8aa14bb0b7ec0ea72b87304165fb2617d3e4c576

--- components/network/http/aos.mk
+++ components/network/http/aos.mk
@@ -1,16 +1,28 @@
+# component name
 NAME := http
-
+# component information
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := http client component
-
+# source files and the folder of internal include files
 $(NAME)_INCLUDES += include
-GLOBAL_DEFINES += CONFIG_AOS_HTTP
-
 $(NAME)_SOURCES := src/http_client.c \
                    src/http_string.c \
                    src/http_parser.c \
-                   src/http_upload.c
+                   src/http_upload.c \
+				   wrappers/http_aos_wrapper.c
+# the folder of API files
+GLOBAL_INCLUDES += ../../../include/network/http
+# mandatory dependencies
+# $(NAME)_COMPONENTS += http_wrapper
+# optional dependencies
+$(NAME)_COMPONENTS-$(CONFIG_HTTP_SECURE_TLS) += mbedtls
+$(NAME)_COMPONENTS-$(CONFIG_HTTP_SECURE_ITLS) += itls
+# NOTE
+# 1. do NOT import optional dependencies as below
+# ifeq (y,$(CONFIG_HTTP_SECURE_TLS))
+# $(NAME)_COMPONENTS += mbedtls
+# endif
+# 2. do NOT define macro in this file. put it into Config.in
+# GLOBAL_DEFINES += CONFIG_HTTP_DEBUG
 
-include $($(NAME)_LOCATION)/wrappers/http_wrapper.mk
-#$(NAME)_COMPONENTS += http_wrapper
