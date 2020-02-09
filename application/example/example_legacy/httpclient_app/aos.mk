b68a4ec5c2e9c2b27c186e4e2134e48bad89e5af
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-09 17:35:10 +0800

    BugID:24820156: Support legacy app and board, update http client component for config sample
    Change-Id: I4495ad7daadf993379942bd03148ddeff33094dc

--- application/example/example_legacy/httpclient_app/aos.mk
+++ application/example/example_legacy/httpclient_app/aos.mk
@@ -5,7 +5,7 @@ $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := HTTP client app
 
 $(NAME)_SOURCES := httpapp.c
-$(NAME)_COMPONENTS := http netmgr cli
+$(NAME)_COMPONENTS := http netmgr lwip cli
 
 GLOBAL_DEFINES += BUILD_AOS
 
