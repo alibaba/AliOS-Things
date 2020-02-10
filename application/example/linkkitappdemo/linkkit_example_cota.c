44a60ab28055fe1a7b47ead644636cb6149df7c9
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-10 20:45:28 +0800

    BugID:24656372:add neww linkkitappdemo and compiles STM32f429 ok;app
    Change-Id: I53de3820aeacccf619e89d799e8e08601712c5bb

---
+++ application/example/linkkitappdemo/linkkit_example_cota.c
@@ -0,0 +1,210 @@
+/*
+ * Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+
+#include "stdio.h"
+#include "linkkit/infra/infra_types.h"
+#include "linkkit/infra/infra_defs.h"
+#include "linkkit/infra/infra_compat.h"
+#include "linkkit/dev_model_api.h"
+#include "linkkit/wrappers/wrappers.h"
+
+#ifdef INFRA_MEM_STATS
+    #include "linkkit/infra/infra_mem_stats.h"
+#endif
+#include "app_entry.h"
+
+#define PRODUCT_KEY      "a1X2bEnP82z"
+#define PRODUCT_SECRET   "7jluWm1zql7bt8qK"
+#define DEVICE_NAME      "test_0109_82z_service_kj"
+#define DEVICE_SECRET    "8woc2VehSB8HI3Isd6R9xZXBvBaKfqzV"
+
+#define USE_CUSTOME_DOMAIN              (0)
+
+#if USE_CUSTOME_DOMAIN
+    #define CUSTOME_DOMAIN_MQTT         "iot-as-mqtt.cn-shanghai.aliyuncs.com"
+    #define CUSTOME_DOMAIN_HTTP         "iot-auth.cn-shanghai.aliyuncs.com"
+#endif
+
+#define USER_EXAMPLE_YIELD_TIMEOUT_MS   (1000)
+
+#define EXAMPLE_TRACE(...) \
+    do { \
+        HAL_Printf("\033[1;32;40m%s.%d: ", __func__, __LINE__); \
+        HAL_Printf(__VA_ARGS__); \
+        HAL_Printf("\033[0m\r\n"); \
+    } while (0)
+
+typedef struct {
+    int     master_devid;
+    int     cloud_connected;
+    int     master_initialized;
+} user_example_ctx_t;
+
+static user_example_ctx_t g_user_example_ctx;
+
+static user_example_ctx_t *user_example_get_ctx(void)
+{
+    return &g_user_example_ctx;
+}
+
+/** type:
+  *
+  * 0 - new config exist
+  *
+  */
+static int user_cota_event_handler(int type, const char *config_id, int config_size, const char *get_type,
+                                   const char *sign, const char *sign_method, const char *url)
+{
+    char buffer[128] = {0};
+    int buffer_length = 128;
+    user_example_ctx_t *user_example_ctx = user_example_get_ctx();
+
+    if (type == 0) {
+        EXAMPLE_TRACE("New Config ID: %s", config_id);
+        EXAMPLE_TRACE("New Config Size: %d", config_size);
+        EXAMPLE_TRACE("New Config Type: %s", get_type);
+        EXAMPLE_TRACE("New Config Sign: %s", sign);
+        EXAMPLE_TRACE("New Config Sign Method: %s", sign_method);
+        EXAMPLE_TRACE("New Config URL: %s", url);
+
+        IOT_Linkkit_Query(user_example_ctx->master_devid, ITM_MSG_QUERY_COTA_DATA, (unsigned char *)buffer, buffer_length);
+    }
+
+    return 0;
+}
+
+static int user_master_dev_available(void)
+{
+    user_example_ctx_t *user_example_ctx = user_example_get_ctx();
+
+    if (user_example_ctx->cloud_connected && user_example_ctx->master_initialized) {
+        return 1;
+    }
+
+    return 0;
+}
+
+void user_post_property(void)
+{
+    static int example_index = 0;
+    int res = 0;
+    user_example_ctx_t *user_example_ctx = user_example_get_ctx();
+    char *property_payload = "NULL";
+
+    if (example_index == 0) {
+        /* Normal Example */
+        property_payload = "{\"LightSwitch\":1}";
+        example_index++;
+    } else if (example_index == 1) {
+        /* Wrong Property ID */
+        property_payload = "{\"LightSwitchxxxx\":1}";
+        example_index++;
+    } else if (example_index == 2) {
+        /* Wrong Value Format */
+        property_payload = "{\"LightSwitch\":\"test\"}";
+        example_index++;
+    } else if (example_index == 3) {
+        /* Wrong Value Range */
+        property_payload = "{\"LightSwitch\":10}";
+        example_index++;
+    } else if (example_index == 4) {
+        /* Missing Property Item */
+        property_payload = "{\"RGBColor\":{\"Red\":45,\"Green\":30}}";
+        example_index++;
+    } else if (example_index == 5) {
+        /* Wrong Params Format */
+        property_payload = "\"hello world\"";
+        example_index++;
+    } else if (example_index == 6) {
+        /* Wrong Json Format */
+        property_payload = "hello world";
+        example_index = 0;
+    }
+
+    res = IOT_Linkkit_Report(user_example_ctx->master_devid, ITM_MSG_POST_PROPERTY,
+                             (unsigned char *)property_payload, strlen(property_payload));
+
+    EXAMPLE_TRACE("Post Property Message ID: %d", res);
+}
+
+void set_iotx_info()
+{
+    char _device_name[IOTX_DEVICE_NAME_LEN + 1] = {0};
+    HAL_GetDeviceName(_device_name);
+    if (strlen(_device_name) == 0) {
+        HAL_SetProductKey(PRODUCT_KEY);
+        HAL_SetProductSecret(PRODUCT_SECRET);
+        HAL_SetDeviceName(DEVICE_NAME);
+        HAL_SetDeviceSecret(DEVICE_SECRET);
+    }
+}
+
+int linkkit_main(void *paras)
+{
+
+    int res = 0;
+    uint64_t time_now_sec = 0;
+    user_example_ctx_t *user_example_ctx = user_example_get_ctx();
+    iotx_linkkit_dev_meta_info_t master_meta_info;
+
+#if !defined(WIFI_PROVISION_ENABLED) || !defined(BUILD_AOS)
+    set_iotx_info();
+#endif
+
+    memset(user_example_ctx, 0, sizeof(user_example_ctx_t));
+    IOT_SetLogLevel(IOT_LOG_DEBUG);
+
+    /* Register Callback */
+    IOT_RegisterCallback(ITE_COTA, user_cota_event_handler);
+
+    memset(&master_meta_info, 0, sizeof(iotx_linkkit_dev_meta_info_t));
+    HAL_GetProductKey(master_meta_info.product_key);
+    HAL_GetDeviceName(master_meta_info.device_name);
+    HAL_GetProductSecret(master_meta_info.product_secret);
+    HAL_GetDeviceSecret(master_meta_info.device_secret);
+
+    /* Choose Login Server, domain should be configured before IOT_Linkkit_Open() */
+#if USE_CUSTOME_DOMAIN
+    IOT_Ioctl(IOTX_IOCTL_SET_MQTT_DOMAIN, (void *)CUSTOME_DOMAIN_MQTT);
+    IOT_Ioctl(IOTX_IOCTL_SET_HTTP_DOMAIN, (void *)CUSTOME_DOMAIN_HTTP);
+#else
+    int domain_type = IOTX_CLOUD_REGION_SHANGHAI;
+    IOT_Ioctl(IOTX_IOCTL_SET_DOMAIN, (void *)&domain_type);
+#endif
+
+    /* Choose Login Method */
+    int dynamic_register = 0;
+    IOT_Ioctl(IOTX_IOCTL_SET_DYNAMIC_REGISTER, (void *)&dynamic_register);
+
+    /* Create Master Device Resources */
+    user_example_ctx->master_devid = IOT_Linkkit_Open(IOTX_LINKKIT_DEV_TYPE_MASTER, &master_meta_info);
+    if (user_example_ctx->master_devid < 0) {
+        EXAMPLE_TRACE("IOT_Linkkit_Open Failed\n");
+        return -1;
+    }
+
+    /* Start Connect Aliyun Server */
+    res = IOT_Linkkit_Connect(user_example_ctx->master_devid);
+    if (res < 0) {
+        EXAMPLE_TRACE("IOT_Linkkit_Connect Failed\n");
+        return -1;
+    }
+
+    //IOT_Linkkit_Query(user_example_ctx->master_devid, ITM_MSG_REQUEST_COTA, NULL, 0);
+
+    while (1) {
+        IOT_Linkkit_Yield(USER_EXAMPLE_YIELD_TIMEOUT_MS);
+        EXAMPLE_TRACE("yield loop\n");
+        /* Post Proprety Example */
+        if (time_now_sec % 11 == 0 && user_master_dev_available()) {
+            user_post_property();
+        }
+    }
+
+    IOT_Linkkit_Close(user_example_ctx->master_devid);
+
+    IOT_SetLogLevel(IOT_LOG_NONE);
+
+    return 0;
+}
