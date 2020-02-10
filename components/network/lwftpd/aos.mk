a85129638931c3fdb3302d182ae0ad748dfbd861
wanglu.luwang <wanglu.luwang@alibaba-inc.com>
2020-02-10 11:09:54 +0800

    BugID:24948835:Remove not used lwftpd component
    Change-Id: Ib980045094d08f52c8351ff31aee87ff20a3500e

--- components/network/lwftpd/aos.mk
+++
@@ -1,9 +0,0 @@
-NAME := lwftpd
-
-$(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY := ftp server component
-
-GLOBAL_DEFINES += CONFIG_AOS_LWFTPD
-
-$(NAME)_SOURCES := server.c handles.c
