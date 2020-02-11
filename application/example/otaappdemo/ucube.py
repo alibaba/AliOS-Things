7b7190e77a58d2978fbe04f60134123779ab166e
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-11 09:36:23 +0800

    BugID:24656372:add neww otaappdemo and compiles STM32f429 ok;app
    Change-Id: I79ec363052dd22c7fc6ed1afa545741e5a18e552

---
+++ application/example/otaappdemo/ucube.py
@@ -0,0 +1,25 @@
+src =Split('''
+    otaapp.c
+''')
+
+component =aos_component('otaapp', src)
+
+dependencis =Split('''
+    framework/protocol/linkkit/sdk
+    framework/protocol/linkkit/hal
+    framework/netmgr
+    framework/common
+    utility/cjson
+    kernel/cli
+''')
+for i in dependencis:
+    component.add_comp_deps(i)
+
+global_macros =Split('''
+    MQTT_DIRECT
+    ALIOT_DEBUG
+    IOTX_DEBUG
+    USE_LPTHREAD
+''')
+for i in global_macros:
+    component.add_global_macros(i)
