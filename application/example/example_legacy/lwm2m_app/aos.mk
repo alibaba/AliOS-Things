46112f1b5a2e3bc5ae77017923cc0cfa307750ff
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-11 21:16:02 +0800

    BugID:24962324: update relate component app config.in/aos.mk
    Change-Id: I00005ef2632a8bf8e68e6cab0263b58883fec76c

--- application/example/example_legacy/lwm2m_app/aos.mk
+++ application/example/example_legacy/lwm2m_app/aos.mk
@@ -6,9 +6,5 @@ $(NAME)_SUMMARY := lwm2m app
 $(NAME)_SOURCES := lwm2m_app.c \
                    app_entry.c
 
-$(NAME)_COMPONENTS += osal_aos lwm2m cli netmgr 
-
-ifeq (y,$(LWM2M_WITH_DTLS))
-$(NAME)_COMPONENTS += mbedtls
-GLOBAL_DEFINES-y += USE_EXTERNAL_MEBDTLS WITH_MBEDTLS LWM2M_WITH_MBEDTLS
-endif
+$(NAME)_COMPONENTS-y += osal_aos lwm2m cli netmgr 
+$(NAME)_COMPONENTS-$(LWM2M_WITH_DTLS) += mbedtls
--- application/example/example_legacy/sal_app/aos.mk
+++ application/example/example_legacy/sal_app/aos.mk
@@ -2,7 +2,7 @@ NAME := sal_app
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY :=
+$(NAME)_SUMMARY := socket adapter layer app
 $(NAME)_SOURCES := sal_app.c
 
 $(NAME)_COMPONENTS := yloop cli netmgr sal
