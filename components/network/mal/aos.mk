3821d385483fe942d5f63cf63cb81a923214f646
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-11 15:16:43 +0800

    BugID:24979467: update mal config.in and aos.mk
    Change-Id: Idc8d3ffa8437f741cb0f09fd0a9d6cedced66e6a

--- components/network/mal/aos.mk
+++ components/network/mal/aos.mk
@@ -1,16 +1,22 @@
 NAME := mal
 
+# component information
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 3.0.1
 $(NAME)_SUMMARY := mqtt adapter layer
 
-$(NAME)_SOURCES := ./mal.c     \
-                   ./mqtt_api.c
+# source files and the folder of internal include files
+$(NAME)_SOURCES-y := ./mal.c     \
+                     ./mqtt_api.c
 
-ifneq (y,$(strip $(AOS_COMP_SDK_WRAPPERS)))
-$(NAME)_SOURCES +=  ./mal_aos.c
-endif
-
-GLOBAL_DEFINES += WITH_MAL CONFIG_NO_TCPIP
+$(NAME)_SOURCES-$(MAL_AOS_HAL_IMPL) += ./mal_aos.c
 
 $(NAME)_INCLUDES += ./
+
+# the folder of API files
+GLOBAL_INCLUDES += ../../../include/network/mal
+
+# mandatory dependencies
+$(NAME)_COMPONENTS := device_mal
+
+# optional dependencies
