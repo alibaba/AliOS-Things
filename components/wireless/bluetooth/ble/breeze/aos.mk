4e372dae62e5fbdf7d408533904ad1459f289a1c
绪光 <xuguang.lxg@alibaba-inc.com>
2020-02-11 17:14:28 +0800

    BugID:23027947:[breeze] Refine breeze's Config.in, remove components dependence.
    Change-Id: I903d26657850ded5e3f5c0d1fea74b77458a740f

--- components/wireless/bluetooth/ble/breeze/aos.mk
+++ components/wireless/bluetooth/ble/breeze/aos.mk
@@ -7,6 +7,7 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.2
 $(NAME)_SUMMARY := breeze provides secure BLE connection to Alibaba IoT cloud and services.
 
+$(NAME)_SOURCES-y += api/breeze_export.c
 $(NAME)_SOURCES-y += core/src/core.c
 $(NAME)_SOURCES-y += core/src/transport.c
 $(NAME)_SOURCES-y += core/src/ble_service.c
@@ -15,35 +16,17 @@ $(NAME)_SOURCES-y += core/src/extcmd.c
 
 $(NAME)_INCLUDES += include \
                     core/include 
+GLOBAL_INCLUDES += ../../../../../include/wireless/bluetooth/breeze
 
 
-ifeq ($(CONFIG_COMP_BZ_SECURE_ADV), y)
-GLOBAL_DEFINES-y += CONFIG_AIS_SECURE_ADV
-endif
-
-ifeq ($(CONFIG_COMP_BZ_MODEL_SEC), y)
-GLOBAL_DEFINES-y += CONFIG_MODEL_SECURITY
-endif
-
-$(NAME)_SOURCES-y += api/breeze_export.c
-
-ifeq ($(CONFIG_COMP_BZ_EN_AUTH), y)
-GLOBAL_DEFINES-y += EN_AUTH
+ifeq ($(EN_AUTH), y)
 $(NAME)_SOURCES-y += core/src/auth.c
-ifeq ($(CONFIG_COMP_BZ_EN_AUTH_OFFLINE), y)
-GLOBAL_DEFINES-y += EN_AUTH_OFFLINE
-endif
 endif
 
-ifeq ($(CONFIG_COMP_BZ_EN_AWSS), y)
-ifneq ($(CONFIG_COMP_BZ_EN_AUTH), y)
+ifeq ($(EN_COMBO_NET), y)
+ifneq ($(EN_AUTH), y)
 $(error awss need authentication, please set "bz_en_auth = 1")
 endif
-GLOBAL_DEFINES-y += EN_COMBO_NET
 $(NAME)_SOURCES-y += api/breeze_awss_export.c
-endif
-
-ifeq ($(CONFIG_COMP_BZ_EN_LONG_MTU), y)
-GLOBAL_DEFINES-y += EN_LONG_MTU
 endif
 
