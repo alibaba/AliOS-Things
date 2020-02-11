5ed8eee711d132c3241f745ed78cc8cbd453a20c
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-11 12:07:38 +0800

    BugID:24974357: update httpdns config.in aos.mk
    Change-Id: I2bce5b4c9b50a74586e62a1d5e6fbf2426895e26

--- components/network/httpdns/aos.mk
+++ components/network/httpdns/aos.mk
@@ -1,16 +1,23 @@
 NAME := httpdns
 
+# component information
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := httpdns component
 
+# source files and the folder of internal include files
 $(NAME)_SOURCES := ./src/httpdns.c         \
                    ./src/iplist.c          \
                    ./src/dict.c            \
                    ./src/http_ctx.c        \
                    ./src/sds.c             \
                    ./src/httpdns_aos.c     \
-                   ./src/httpdns_opts.c    \
+                   ./src/httpdns_opts.c
 
 $(NAME)_INCLUDES += ./include/
 
+# the folder of API files
+GLOBAL_INCLUDES += ../../../include/network/httpdns
+
+# mandatory dependencies
+$(NAME)_COMPONENTS += http cjson
