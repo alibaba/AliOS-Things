ee4988380541c851f3df8d31500ec5a137b75505
绪光 <xuguang.lxg@alibaba-inc.com>
2020-02-12 16:21:50 +0800

    BugID:23027947:[blemesh] ble mesh app refine according to new standards.
    Change-Id: I99f5ba62a49d2fe9405cc69a8e60c98e7357ab83

--- application/example/example_legacy/bluetooth/blemesh_tmall/aos.mk
+++ application/example/example_legacy/bluetooth/blemesh_tmall/aos.mk
@@ -10,7 +10,7 @@ BLE = 1
 EN_BT_SMP = 1
 bt_mesh = 1
 
-$(NAME)_COMPONENTS-y += tmall_model yloop cli
+$(NAME)_COMPONENTS-y += tmall_model bt_host yloop cli
 GLOBAL_DEFINES-y     += AOS_NO_WIFI CONFIG_BT_TINYCRYPT_ECC CONFIG_BT_CONN
 
 $(NAME)_INCLUDES-y    += ../
