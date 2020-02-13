eff0d782bf3f841f23b55c50cf49c98bc5c172ac
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-02-13 21:36:52 +0800

    BugID:24656372:modify i2c for stm32f429 and f103;board
    Change-Id: Ie232fa46dc97eabdb9d62cf94ffcd3864d0b8ac0

--- platform/board/stm32f103rb-demo/drivers/src/i2c.c
+++ platform/board/stm32f103rb-demo/drivers/src/i2c.c
@@ -39,35 +39,12 @@
 
 /* Includes ------------------------------------------------------------------*/
 #include "aos/hal/i2c.h"
-#include "stm32f4xx_hal.h"
+#include "stm32f1xx_hal.h"
 #include "aos/hal/gpio.h"
 
 /* USER CODE BEGIN 0 */
 
 /* USER CODE END 0 */
-
-I2C_HandleTypeDef hi2c1;
-
-/* I2C1 init function */
-void MX_I2C1_Init(void)
-{
-
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
-  {
-    Error_Handler(__FILE__, __LINE__);
-  }
-
-}
-
 void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
 {
 
@@ -85,8 +62,8 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
         GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
         GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull = GPIO_PULLUP;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
+        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+        //GPIO_InitStruct.Alternate = GPIO_AF4_I2C1;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
         /* I2C1 clock enable */
@@ -94,35 +71,6 @@ void HAL_I2C_MspInit(I2C_HandleTypeDef* i2cHandle)
         /* USER CODE BEGIN I2C1_MspInit 1 */
 
         /* USER CODE END I2C1_MspInit 1 */
-    }
-
-    if(i2cHandle->Instance==I2C2)
-    {
-        /* USER CODE BEGIN I2C2_MspInit 0 */
-
-        /* USER CODE END I2C2_MspInit 0 */
-
-          /**I2C2 GPIO Configuration
-          PF1     ------> I2C2_SCL
-          PF0     ------> I2C2_SDA
-          */
-          GPIO_InitStruct.Pin = GPIO_PIN_0|GPIO_PIN_1;
-          GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-          GPIO_InitStruct.Pull = GPIO_PULLUP;
-          GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-          GPIO_InitStruct.Alternate = GPIO_AF4_I2C2;
-          HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
-
-          /* I2C2 clock enable */
-          __HAL_RCC_I2C2_CLK_ENABLE();
-        /* USER CODE BEGIN I2C2_MspInit 1 */
-
-        /* USER CODE END I2C2_MspInit 1 */
-    }
-
-    if(i2cHandle->Instance==I2C3)
-    {
-
     }
 
 }
@@ -149,24 +97,6 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef* i2cHandle)
   /* USER CODE END I2C1_MspDeInit 1 */
   }
 
-  if(i2cHandle->Instance==I2C2)
-  {
-  /* USER CODE BEGIN I2C2_MspDeInit 0 */
-
-  /* USER CODE END I2C2_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_I2C2_CLK_DISABLE();
-
-    /**I2C1 GPIO Configuration
-    PF1     ------> I2C2_SCL
-    PF0     ------> I2C2_SDA
-    */
-    HAL_GPIO_DeInit(GPIOF, GPIO_PIN_0|GPIO_PIN_1);
-
-  /* USER CODE BEGIN I2C2_MspDeInit 1 */
-
-  /* USER CODE END I2C2_MspDeInit 1 */
-  }
 }
 
 /* USER CODE BEGIN 1 */
