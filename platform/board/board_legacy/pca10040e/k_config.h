4b540b4654b4c0561542ac32195482222747dff3
huangzhen <wuqi.hz@alibaba-inc.com>
2020-02-13 18:52:06 +0800

    bugid:23823264:reconfig kernel, change default config.
    Change-Id: Ib47e015fb3ef666f5b34b543eaec038760a012fd

--- platform/board/board_legacy/pca10040e/k_config.h
+++ platform/board/board_legacy/pca10040e/k_config.h
@@ -5,25 +5,7 @@
 #ifndef K_CONFIG_H
 #define K_CONFIG_H
 
-/* kernel feature conf */
-#ifndef RHINO_CONFIG_SEM
-#define RHINO_CONFIG_SEM                     1
-#endif
-#ifndef RHINO_CONFIG_QUEUE
-#define RHINO_CONFIG_QUEUE                   1
-#endif
-#ifndef RHINO_CONFIG_TASK_SEM
-#define RHINO_CONFIG_TASK_SEM                1
-#endif
-#ifndef RHINO_CONFIG_EVENT_FLAG
-#define RHINO_CONFIG_EVENT_FLAG              1
-#endif
-#ifndef RHINO_CONFIG_TIMER
-#define RHINO_CONFIG_TIMER                   1
-#endif
-#ifndef RHINO_CONFIG_BUF_QUEUE
-#define RHINO_CONFIG_BUF_QUEUE               1
-#endif
+/* heap conf */
 #ifndef RHINO_CONFIG_MM_BLK
 #define RHINO_CONFIG_MM_BLK                  1
 #endif
@@ -36,14 +18,11 @@
 #ifndef RHINO_CONFIG_MM_TLF_BLK_SIZE
 #define RHINO_CONFIG_MM_TLF_BLK_SIZE         2048
 #endif
+
 /* kernel task conf */
 #ifndef RHINO_CONFIG_TASK_INFO
 #define RHINO_CONFIG_TASK_INFO               1
 #endif
-#ifndef RHINO_CONFIG_TASK_DEL
-#define RHINO_CONFIG_TASK_DEL                1
-#endif
-
 #ifndef RHINO_CONFIG_TASK_STACK_OVF_CHECK
 #define RHINO_CONFIG_TASK_STACK_OVF_CHECK    1
 #endif
@@ -60,14 +39,6 @@
 #define RHINO_CONFIG_USER_PRI_MAX            (RHINO_CONFIG_PRI_MAX - 2)
 #endif
 
-/* kernel workqueue conf */
-#ifndef RHINO_CONFIG_WORKQUEUE
-#define RHINO_CONFIG_WORKQUEUE               0
-#endif
-#ifndef RHINO_CONFIG_WORKQUEUE_STACK_SIZE
-#define RHINO_CONFIG_WORKQUEUE_STACK_SIZE    512
-#endif
-
 /* kernel mm_region conf */
 #ifndef RHINO_CONFIG_MM_REGION_MUTEX
 #define RHINO_CONFIG_MM_REGION_MUTEX         1
@@ -81,9 +52,6 @@
 /*must reserve enough stack size for timer cb will consume*/
 #ifndef RHINO_CONFIG_TIMER_TASK_STACK_SIZE
 #define RHINO_CONFIG_TIMER_TASK_STACK_SIZE   128
-#endif
-#ifndef RHINO_CONFIG_TIMER_TASK_PRI
-#define RHINO_CONFIG_TIMER_TASK_PRI          5
 #endif
 
 /* kernel dyn alloc conf */
