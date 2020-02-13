527d6e14ba50c87eaeedfe3df8a507636e17be85
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-13 17:04:50 +0800

    BugID:24412528: add component template and script
    Change-Id: Id0ac9fd62963171953aa1273f6b3932d756cf6d8

---
+++ build/scripts/templates/new_component_template/componentname/componentname.h
@@ -0,0 +1,35 @@
+/**
+ * @file @componentname@.h
+ *
+ * @author    @authorname@
+ * @version   V1.0
+ * @date      2020-01-01
+ * @copyright Copyright (C) 2015-2020 @manufacturename@
+ */
+#ifndef @COMPONENTNAME@_API_H
+#define @COMPONENTNAME@_API_H
+/** @addtogroup aos_@componentname@ @componentname@ 
+ *  This is the brief description of the component.
+ *
+ *  This is the detailed description, and it's optional.
+ *  @{
+ */
+
+/* Here is Macro and struct definition*/
+
+
+/* Here is API and callback definition*/
+/**
+ * @componentname@ initialization.
+ * 
+ * @retval           0: initial success; -1: initial failed.
+ *
+ * @note
+ */
+int @componentname@_init(void);
+
+
+
+/** @} */
+#endif
+
---
+++ build/scripts/templates/new_component_template/src/componentname.c
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2015-2020 @manufacturename@
+ */
+
+#include <stdio.h>
+#include <aos/kernel.h>
+
+int @componentname@_init(void)
+{
+    /*add your code*/
+
+    return 0;
+}
+
