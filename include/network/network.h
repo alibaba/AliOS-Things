d51a807b7552c552fd43f604637e3b1a85b23d39
yajun.syj <yajun.syj@alibaba-inc.com>
2020-02-14 11:04:26 +0800

    BugID:23823399: Optimize lwip Config.in and aos.mk
    Change-Id: Ia6ec9d1f3649e759f7cbe9bce8f72de633a1e64f

--- include/network/network.h
+++ include/network/network.h
@@ -18,23 +18,24 @@ extern "C" {
 #include <fcntl.h>
 
 /* network */
-#ifdef WITH_LWIP
+#if defined(CONFIG_AOS_LWIP) || defined(CONFIG_VENDOR_LWIP)
 #include <lwip/def.h>
 #include <lwip/netdb.h>
 #include <lwip/sockets.h>
-#elif defined(_WIN32)
+#elif defined(CONFIG_NO_TCPIP) && defined(WITH_SAL)
+#include <sal/sal_arch.h>
+#include <sal/sal_def.h>
+#include <sal/sal_ipaddr.h>
+#include <sal/sal_sockets.h>
+#elif !defined(CONFIG_NO_TCPIP)
+#if defined(_WIN32)
 #include "unistd.h"
 #define read(fd, buf, len)         recv(fd, (char*)buf, (int)len, 0)
 #define write(fd, buf, len)        send(fd, (char*)buf, (int)len, 0)
 #define close(fd)                  closesocket(fd)
 #define inet_aton(strptr, addrptr) inet_pton(AF_INET, strptr, addrptr)
 #define bzero(b, len)              memset((b), '\0', (len))
-#elif defined(WITH_SAL)
-#include <sal/sal_arch.h>
-#include <sal/sal_def.h>
-#include <sal/sal_ipaddr.h>
-#include <sal/sal_sockets.h>
-#elif !defined(CONFIG_NO_TCPIP)
+#else
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -42,7 +43,8 @@ extern "C" {
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <sys/eventfd.h>
-#endif /* WITH_LWIP */
+#endif
+#endif /* CONFIG_AOS_TCPIP */
 
 #ifdef __cplusplus
 }
