e2d9dd7cfb5828f3695d9f987ff256fcc360cbb2
yajun.syj <yajun.syj@alibaba-inc.com>
2020-02-14 11:05:40 +0800

    BugID:23823399: Optimzie lwip Config.in and aos.mk
    Change-Id: I630f6e8e83c29d8a42764ba2c16f3b313a48ac56

--- core/libc/newlib_stub.c
+++ core/libc/newlib_stub.c
@@ -15,7 +15,7 @@
 
 #include "vfs_conf.h"
 #include "network/network.h"
-#ifdef WITH_LWIP_TELNETD
+#ifdef TELNETD_ENABLED
 #include "lwip/apps/telnetserver.h"
 #endif
 
@@ -23,7 +23,7 @@
 #define FD_VFS_END   (FD_VFS_START + VFS_MAX_FILE_NUM - 1)
 
 #ifdef POSIX_DEVICE_IO_NEED
-#ifdef WITH_LWIP
+#ifdef CONFIG_AOS_LWIP
 #define FD_SOCKET_START FD_AOS_SOCKET_OFFSET
 #define FD_SOCKET_END   (FD_AOS_SOCKET_OFFSET + FD_AOS_NUM_SOCKETS - 1)
 #define FD_EVENT_START  FD_AOS_EVENT_OFFSET
@@ -43,7 +43,7 @@ int _fcntl_r(struct _reent *ptr, int fd, int cmd, int arg)
     if ((fd >= FD_VFS_START) && (fd <= FD_VFS_END)) {
         return aos_fcntl(fd, cmd, arg);
 #ifdef POSIX_DEVICE_IO_NEED
-#ifdef WITH_LWIP
+#ifdef CONFIG_AOS_LWIP
     } else if ((fd >= FD_SOCKET_START) && (fd <= FD_EVENT_END)) {
         return lwip_fcntl(fd, cmd, arg);
 #endif
@@ -107,7 +107,7 @@ int _close_r(struct _reent *ptr, int fd)
     if ((fd >= FD_VFS_START) && (fd <= FD_VFS_END)) {
         return aos_close(fd);
 #ifdef POSIX_DEVICE_IO_NEED
-#ifdef WITH_LWIP
+#ifdef CONFIG_AOS_LWIP
     } else if ((fd >= FD_SOCKET_START) && (fd <= FD_EVENT_END)) {
         return lwip_close(fd);
 #endif
@@ -122,7 +122,7 @@ _ssize_t _read_r(struct _reent *ptr, int fd, void *buf, size_t nbytes)
     if ((fd >= FD_VFS_START) && (fd <= FD_VFS_END)) {
         return aos_read(fd, buf, nbytes);
 #ifdef POSIX_DEVICE_IO_NEED
-#ifdef WITH_LWIP
+#ifdef CONFIG_AOS_LWIP
     } else if ((fd >= FD_SOCKET_START) && (fd <= FD_EVENT_END)) {
         return lwip_read(fd, buf, nbytes);
 #endif
@@ -151,7 +151,7 @@ _ssize_t _write_r(struct _reent *ptr, int fd, const void *buf, size_t nbytes)
     if ((fd >= FD_VFS_START) && (fd <= FD_VFS_END)) {
         return aos_write(fd, buf, nbytes);
 #ifdef POSIX_DEVICE_IO_NEED
-#ifdef WITH_LWIP
+#ifdef CONFIG_AOS_LWIP
     } else if ((fd >= FD_SOCKET_START) && (fd <= FD_EVENT_END)) {
         return lwip_write(fd, buf, nbytes);
 #endif
@@ -159,13 +159,13 @@ _ssize_t _write_r(struct _reent *ptr, int fd, const void *buf, size_t nbytes)
     } else if ((fd == STDOUT_FILENO) || (fd == STDERR_FILENO)) {
         for (i = 0; i < nbytes; i++) {
             if (*tmp == '\n') {
-#ifdef WITH_LWIP_TELNETD
+#ifdef TELNETD_ENABLED
                 TelnetWrite('\r');
 #endif
                 hal_uart_send(&uart_stdio, (void *)"\r", 1, AOS_WAIT_FOREVER);
             }
 
-#ifdef WITH_LWIP_TELNETD
+#ifdef TELNETD_ENABLED
             TelnetWrite(*tmp);
 #endif
             hal_uart_send(&uart_stdio, (void *)tmp, 1, AOS_WAIT_FOREVER);
@@ -191,7 +191,7 @@ int ioctl(int fildes, int request, ... /* arg */)
         (fildes <= (VFS_FD_OFFSET + VFS_MAX_FILE_NUM - 1))) {
         arg = va_arg(args, int);
         return aos_ioctl(fildes, request, arg);
-#ifdef WITH_LWIP
+#ifdef CONFIG_AOS_LWIP
     } else if ((fildes >= FD_AOS_SOCKET_OFFSET) &&
                (fildes <= (FD_AOS_EVENT_OFFSET + FD_AOS_NUM_EVENTS - 1))) {
         argp = va_arg(args, void *);
