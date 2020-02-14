21f1c506e60a65050f261dbe93db50b6bf173214
zhangtian <dongren.zt@alibaba-inc.com>
2020-02-14 14:26:59 +0800

    BugID:24882677:[umesh2]improve lwip adapter, fix some bugs
    Change-Id: I244df8505fc4e02978480e1d8e7364972d29654c

--- components/network/umesh2/aos.mk
+++ components/network/umesh2/aos.mk
@@ -4,13 +4,10 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := uMesh provides the ability to form local area mesh network.
 
-#GLOBAL_DEFINES += 
-
 $(NAME)_DEFINES += UTILS_AES_BUILDIN
 
 
 $(NAME)_DEFINES += USE_ACTION_FRAME
-$(NAME)_COMPONENTS += yloop
 
 $(NAME)_INCLUDES += include/core       \
                     include/adapter    \
@@ -20,13 +17,6 @@ $(NAME)_SOURCES := src/core/*.c        \
                    src/adapter/*.c     \
                    src/utils/*.c                   
 
+$(NAME)_DEFINES += LWIP_IPV6=1
 
-
-GLOBAL_DEFINES += LWIP_IPV6=0
-
-#default gcc
-ifeq ($(COMPILER),)
-$(NAME)_CFLAGS      += -Wall -Werror
-else ifeq ($(COMPILER),gcc)
-$(NAME)_CFLAGS      += -Wall -Werror
-endif
+$(NAME)_COMPONENTS-$(UMESH_USE_YLOOP) := yloop
