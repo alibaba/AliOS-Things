31030ae6196f9b3f9d3c6f7abf97080d38c1c17d
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-14 11:19:16 +0800

    BugID:24843276: Debug halapp_pwm on stm32f429 board
    Change-Id: I9c79cb92b0bfe8366a685026bb2a408e21e093ed

--- application/example/halappdemo/halapp_pwm.c
+++ application/example/halappdemo/halapp_pwm.c
@@ -12,68 +12,84 @@
 
 #if (AOS_HAL_PWM_ENABLED > 0)
 
-/* gpio app macro default value
-   need to ajust for each board
-*/
-#ifndef HALAPP_PWM_TEST
-#define HALAPP_PWM_TEST          PORT_PWM_4   //logic num, define corresponding physical port for each board
-#endif
-
 /* output one pwm signal */
-void hal_pwm_app_out(void)
+void hal_pwm_app_static_out(void)
 {
+    int cnt;
     int32_t ret;
-    pwm_dev_t tim = {0};
+    pwm_dev_t pwm = {0};
 
-    printf("hal_pwm_app_out in\r\n");
+    printf(" hal_pwm_app_static_out start\r\n");
 
-    tim.port = HALAPP_PWM_TEST;
-    tim.config.freq = 1000;
-    tim.config.duty_cycle = 0.5;
-    tim.priv = NULL;
+    pwm.port = HALAPP_PWM_TEST;
+    pwm.config.freq = 1000;
+    pwm.config.duty_cycle = 0.5;
+    pwm.priv = NULL;
 
-    ret = hal_pwm_init(&tim);
+    ret = hal_pwm_init(&pwm);
     if(ret){
         printf("hal_pwm_init fail,ret:%d\r\n",ret);
         return;
     }
 
-    hal_pwm_start(&tim);
+    hal_pwm_start(&pwm);
 
-    aos_msleep(50000);
+    aos_msleep(5000);
 
-    hal_pwm_stop(&tim);
-    hal_pwm_finalize(&tim);
+    hal_pwm_stop(&pwm);
+
+    hal_pwm_finalize(&pwm);
+
+    printf("hal_pwm_app_static_out end\r\n");
 }
 
-void hal_pwm_chg_args(void)
+void hal_pwm_app_dynamic_out(void)
 {
+    int cnt;
     int32_t ret;
-    pwm_dev_t tim = {0};
+    pwm_dev_t pwm = {0};
 
-    printf("hal_pwm_chg_args in\r\n");
+    printf("hal_pwm_app_dynamic_out start\r\n");
 
-    pwm_config_t para = {0.25, 1000};
-    tim.port = HALAPP_PWM_TEST;
-    tim.config.freq = 1000;
-    tim.config.duty_cycle = 0.5;
-    tim.priv = NULL;
+    pwm.port = HALAPP_PWM_TEST;
+    pwm.config.freq = 1000;
+    pwm.config.duty_cycle = 0.00;
+    pwm.priv = NULL;
 
-    ret = hal_pwm_init(&tim);
+    ret = hal_pwm_init(&pwm);
     if(ret){
+        printf("hal_pwm_init fail,ret:%d\r\n",ret);
         return;
     }
 
-    hal_pwm_start(&tim);
+    hal_pwm_start(&pwm);
 
-    aos_msleep(50000);
+    cnt = 10;
+    while (cnt--) {
+        printf("duty_cycle count up\r\n");
+        pwm_config_t para = {0.000, 1000};
+        for (int i = 0; i < 100; i++) {
+            para.duty_cycle += 0.01;
+            aos_msleep(10);
+            hal_pwm_para_chg(&pwm, para);
+        }
 
-    hal_pwm_para_chg(&tim, para);
+        printf("duty_cycle count down\r\n");
 
-    aos_msleep(50000);
+        para.duty_cycle = 1.0;
+        para.freq = 1000;
+        for (int i = 0; i < 100; i++) {
+            para.duty_cycle -= 0.01;
+            aos_msleep(10);
+            hal_pwm_para_chg(&pwm, para);
+        }
+    }
 
-    hal_pwm_stop(&tim);
-    hal_pwm_finalize(&tim);
+    hal_pwm_stop(&pwm);
+
+    hal_pwm_finalize(&pwm);
+
+    printf("hal_pwm_app_dynamic_out end\r\n");
 }
 
 #endif
