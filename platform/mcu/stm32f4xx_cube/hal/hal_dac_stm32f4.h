02bc6890b8fba0f1ddadd3accce74cc332e7f711
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-16 23:20:55 +0800

    BugID:24843276:added hal dac for stm32f4xx_cube
    Change-Id: I9badf2232c7bb8ca569b10e091fea2924ff290c0

---
+++ platform/mcu/stm32f4xx_cube/hal/hal_dac_stm32f4.h
@@ -0,0 +1,21 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+
+#ifndef __HAL_DAC_STM32F4_H
+#define __HAL_DAC_STM32F4_H
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#define HAL_DAC_1            (0x00)
+#define HAL_DAC_SIZE         (0x01)
+#define HAL_DAC_INVALID      (0xff)
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __HAL_UART_STM32L4_H */
+
