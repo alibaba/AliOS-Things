4944c1cbd458c8f74b8cc694b088640a708b1e20
薪翁 <xinweng.ljh@alibaba-inc.com>
2020-02-16 22:00:12 +0800

    BugID:23824812: standard the aos.mk and config.in file
    Change-Id: I80bac3d744852268b165d79ede2c53014102add7

--- components/service/ulocation/aos.mk
+++ components/service/ulocation/aos.mk
@@ -1,10 +1,21 @@
+# component name
 NAME := ulocation
-
-$(NAME)_TYPE := framework
+# component information
+$(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.2
-$(NAME)_SUMMARY := locating framework
+$(NAME)_SUMMARY := location component
+# source files and the folder of internal include files
+$(NAME)_INCLUDES += . include ./hal
+$(NAME)_SOURCES += \
+                   ulocation_north.c        \
+                   ulocation_main.c         \
+                   ulocation_south.c        \
+                   hal/ulocation_hal.c
 
-ifeq ($(QXWZ_ENABLED),y)
+# the folder of API files
+GLOBAL_INCLUDES += ../../../include/service/ulocation
+
+ifeq ($(CONFIG_QXWZ_ENABLED),y)
 $(NAME)_SOURCES += \
                    qianxun/ulocation_qxwz_socket.c \
                    qianxun/ulocation_qxwz_utils.c  \
@@ -14,25 +25,16 @@ $(NAME)_INCLUDES += ./qianxun/ ./qianxun/include
 $(NAME)_PREBUILT_LIBRARY := ./qianxun/lib/$(HOST_ARCH)/qxwz.a
 endif
 
-ifeq ($(ULOCATION_GPS),y)
+ifeq ($(CONFIG_ULOCATION_GPS),y)
 $(NAME)_SOURCES += hal/ulocation_hal_gps.c
 endif
 
-ifeq ($(ULOCATION_GPRS),y)
+ifeq ($(CONFIG_ULOCATION_GPRS),y)
 $(NAME)_SOURCES += hal/ulocation_hal_gprs.c
-else ifeq ($(ULOCATION_WIFI),y)
+else ifeq ($(CONFIG_ULOCATION_WIFI),y)
 $(NAME)_SOURCES += hal/ulocation_hal_wifi.c
 endif
 
-$(NAME)_SOURCES += \
-                   ulocation_north.c        \
-                   ulocation_main.c         \
-                   ulocation_south.c        \
-                   hal/ulocation_hal.c
-
-$(NAME)_COMPONENTS := sensor cjson
-
-$(NAME)_INCLUDES += . include
-$(NAME)_INCLUDES += ./hal
-
-GLOBAL_DEFINES += AOS_ULOCATION
+# optional dependencies
+$(NAME)_COMPONENTS-$(CONFIG_ULOCATION_GPS) += sensor cjson
+$(NAME)_COMPONENTS-$(CONFIG_ULOCATION_AGPS) += cjson
