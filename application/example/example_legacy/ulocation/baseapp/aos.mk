4b866f1a298cb5238c355e7ad3b0de29d2285ba6
薪翁 <xinweng.ljh@alibaba-inc.com>
2020-02-16 22:34:12 +0800

    BugID:23824812: standard the aos.mk and config.in file
    Change-Id: I746a203fedb89f07c2712bbfb00c03a5a66c296d

--- application/example/example_legacy/ulocation/baseapp/aos.mk
+++ application/example/example_legacy/ulocation/baseapp/aos.mk
@@ -5,26 +5,7 @@ $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := ulocation base app
 
 $(NAME)_SOURCES :=   ./linkkit/linkkit_example_solo.c ./ulocation_demo.c
-$(NAME)_SOURCES += ./linkkit/linkkit_example_solo.c
-
-$(NAME)_COMPONENTS += linkkit_sdk_c
-
-$(NAME)_COMPONENTS += netmgr cjson
-$(NAME)_COMPONENTS += ulocation
-
-$(NAME)_COMPONENTS += ota
-
-# GLOBAL_DEFINES += ON_PRE
-
-ifeq ($(print_heap),1)
-$(NAME)_DEFINES += CONFIG_PRINT_HEAP
-endif
-
-ifneq ($(HOST_MCU_FAMILY),mcu_esp8266)
-$(NAME)_COMPONENTS  += cli
-else
-GLOBAL_DEFINES += ESP8266_CHIPSET
-endif
+$(NAME)_COMPONENTS += ulocation linkkit_sdk_c netmgr cjson
 
 $(NAME)_INCLUDES += ./ ./linkkit
 
