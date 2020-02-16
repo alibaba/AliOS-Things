7571834f0361fb76d1e1127090c574dbdaea2a36
薪翁 <xinweng.ljh@alibaba-inc.com>
2020-02-16 22:07:06 +0800

    BugID:23824812: standard the aos.mk and config.in file
    Change-Id: I90eeba6796b1b8888a051d0f96cee662c5e1a10a

--- components/bus/canopen/aos.mk
+++ components/bus/canopen/aos.mk
@@ -1,31 +1,12 @@
+# component name
 NAME := canopen
-
-ifeq ($(CANFESTIVAL_ENABLED),y)
+# component information
 $(NAME)_MBINS_TYPE  := kernel
 $(NAME)_VERSION     := 1.0.2
 $(NAME)_SUMMARY     := canfestival canopen stack
-
-$(NAME)_SOURCES     := src/dcf.c
-$(NAME)_SOURCES     += src/emcy.c
-$(NAME)_SOURCES     += src/lifegrd.c
-$(NAME)_SOURCES     += src/lss.c
-$(NAME)_SOURCES     += src/nmtMaster.c
-$(NAME)_SOURCES     += src/nmtSlave.c
-$(NAME)_SOURCES     += src/objacces.c
-$(NAME)_SOURCES     += src/pdo.c
-$(NAME)_SOURCES     += src/sdo.c
-$(NAME)_SOURCES     += src/states.c
-$(NAME)_SOURCES     += src/sync.c
-$(NAME)_SOURCES     += src/timer.c
-
-$(NAME)_SOURCES     += adapter/co_adapter_can.c
-$(NAME)_SOURCES     += adapter/co_adapter_timer.c
-$(NAME)_SOURCES     += wrap/canopen_wrap.c
-
-ifeq ($(HOST_ARCH),linux)
-$(NAME)_DEFINES     += IO_NEED_TRAP
-endif
-
+# source files and the folder of internal include files
+GLOBAL_INCLUDES     += wrap adapter include
+# armcc & iar without -Wall and -Werror
 #default gcc
 ifeq ($(COMPILER),)
 $(NAME)_CFLAGS      += -Wall -Werror
@@ -34,7 +15,26 @@ $(NAME)_CFLAGS      += -Wall -Werror
 else ifeq ($(COMPILER),armcc)
 GLOBAL_DEFINES      += __BSD_VISIBLE
 endif
+ifeq ($(HOST_ARCH),linux)
+$(NAME)_DEFINES     += IO_NEED_TRAP
+endif
+# the folder of API files
+GLOBAL_INCLUDES += ../../../include/bus/canopen
 
-GLOBAL_INCLUDES     += wrap adapter include
-GLOBAL_DEFINES      += AOS_CANOPEN
+ifeq ($(CONFIG_CANFESTIVAL_ENABLED),y)
+$(NAME)_SOURCES     := src/dcf.c \
+                       src/emcy.c \
+                       src/lifegrd.c \
+                       src/lss.c \
+                       src/nmtMaster.c \
+                       src/nmtSlave.c \
+                       src/objacces.c \
+                       src/pdo.c \
+                       src/sdo.c \
+                       src/states.c \
+                       src/sync.c \
+                       src/timer.c \
+                       adapter/co_adapter_can.c \
+                       adapter/co_adapter_timer.c \
+                       wrap/canopen_wrap.c
 endif
