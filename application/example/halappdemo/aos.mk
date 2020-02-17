86eb309d1c4b0f9c0633a9ed9882310e1985b44a
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-17 17:05:52 +0800

    BugID:24843276: Added halapp DAC demo
    Change-Id: I37c5ccc818a79c6a2835398bc77f9473d216aa84

--- application/example/halappdemo/aos.mk
+++ application/example/halappdemo/aos.mk
@@ -8,6 +8,7 @@ $(NAME)_SOURCES := halappdemo.c maintask.c
 $(NAME)_SOURCES += halapp_gpio.c \
                    halapp_pwm.c  \
                    halapp_timer.c \
+                   halapp_dac.c
 
 $(NAME)_COMPONENTS += osal_aos
 
