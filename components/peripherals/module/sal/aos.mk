5b5669c5b43727e8ccea090710552a1fa7d54b65
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-17 16:15:06 +0800

    BugID:25059212: update component sal/aos.mk
    Change-Id: I2e42dfbcb4ec92daeb44c088fa9a45bfd74990aa

--- components/peripherals/module/sal/aos.mk
+++ components/peripherals/module/sal/aos.mk
@@ -5,8 +5,9 @@ $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := general component name for sal devices
 
 $(NAME)_COMPONENTS-y += sal
-$(NAME)_COMPONENTS-$(AOS_SAL_GPRS_SIM800) := device_sal_sim800
-$(NAME)_COMPONENTS-$(AOS_SAL_LTE_M02H) := device_sal_m02h
-$(NAME)_COMPONENTS-$(AOS_SAL_NBIOT_M5310A) := device_sal_m5310a
-$(NAME)_COMPONENTS-$(AOS_SAL_WIFI_BK7231) := device_sal_bk7231
-$(NAME)_COMPONENTS-$(AOS_SAL_WIFI_MK3060) := device_sal_mk3060
+$(NAME)_COMPONENTS-$(AOS_SAL_GPRS_SIM800) += device_sal_sim800
+$(NAME)_COMPONENTS-$(AOS_SAL_LTE_M02H) += device_sal_m02h
+$(NAME)_COMPONENTS-$(AOS_SAL_NBIOT_M5310A) += device_sal_m5310a
+$(NAME)_COMPONENTS-$(AOS_SAL_WIFI_BK7231) += device_sal_bk7231
+$(NAME)_COMPONENTS-$(AOS_SAL_WIFI_MK3060) += device_sal_mk3060
+$(NAME)_COMPONENTS-$(AOS_SAL_GPRS_M26) += device_sal_m26
