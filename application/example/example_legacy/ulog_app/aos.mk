14e304590859646ab5897ee131d410184f9520a8
gonghui.hui <gonghui.hui@alibaba-inc.com>
2020-02-17 10:39:02 +0800

    BugID:23824803:update ulog app based on new rule of Config.in and aos.mk
    Change-Id: I4fae2865ec5ba6f0401a38d050437e3531d78807

--- application/example/example_legacy/ulog_app/aos.mk
+++ application/example/example_legacy/ulog_app/aos.mk
@@ -5,37 +5,14 @@ $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := demo for ulog
 $(NAME)_SOURCES := ulog_app.c
 
-$(NAME)_COMPONENTS  += cli osal_aos
-ifeq ($(ULOGAPP_CONFIG_UDP),y)
-
-$(info >>> switch on test case of ulog: pop out via udp)
-$(NAME)_COMPONENTS  += netmgr
-endif
-
-ifeq ($(ULOGAPP_CONFIG_SPIFFS),y)
-ifeq ($(CSP_LINUXHOST), y)
-else
-$(info >>> switch on test case of ulog: pop out to file system-spiffs)
-$(NAME)_COMPONENTS  += vfs spiffs
-endif
-endif
-
-ifeq ($(ULOGAPP_CONFIG_FATFS),y)
-
-$(NAME)_COMPONENTS  += vfs
-ifneq ($(ULOG_APP_CONFIG_SD_CARD),y)
-$(info >>> switch on test case of ulog: pop out to file system fatfs)
-ifneq ($(findstring esp32, $(BUILD_STRING)), esp32)
-$(info >>> not esp32 device, select fatfs)
-$(NAME)_COMPONENTS  += fatfs
-endif
-GLOBAL_DEFINES += CONFIG_AOS_FATFS_SUPPORT_RAW_FLASH
-else
-$(info >>> switch on test case of ulog: pop out log files from SD card)
-GLOBAL_DEFINES += CONFIG_AOS_FATFS_SUPPORT_MMC
-$(NAME)_COMPONENTS  += fatfs
-endif
-endif
-$(NAME)_INCLUDES += ./
+$(NAME)_COMPONENTS  += cli ulog
 
 
+# optional dependencies
+$(NAME)_COMPONENTS-$(ULOGAPP_CONFIG_UDP) += netmgr
+
+$(NAME)_COMPONENTS-$(ULOGAPP_CONFIG_SPIFFS) += vfs spiffs
+
+$(NAME)_COMPONENTS-$(ULOGAPP_CONFIG_RAW_FATFS) += vfs fatfs
+
+$(NAME)_COMPONENTS-$(ULOGAPP_CONFIG_SD_CARD) += vfs fatfs
