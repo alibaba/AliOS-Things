85ef8c028fc095c1c851ea070cf4de7547347d8d
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2020-02-19 20:05:53 +0800

    BugID:24656372:Add asr5501 and mk3072 board to the new specification
    Change-Id: I4e0f14ce29f0a4c5d00ad78553f0cc04b73015dc

---
+++ platform/board/asr5501-demo/board_api.h
@@ -0,0 +1,105 @@
+/**
+ * @file      board_api.h
+ * @copyright Copyright (C) 2015-2018 Alibaba Group Holding Limited
+ */
+
+#ifndef BOARD_API_H
+#define BOARD_API_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "k_api.h"
+
+/**
+  * @brief Board Initialization Function. This function is defined in app,
+  * it initializes the peripherals required by the application.
+  *
+  * @param None
+  * @retval None
+  */
+void board_init(void);
+
+/**
+  * @general Board basic initialization. At the time of this function call, the kernel has not been initialized, so the system interface cannot be used.
+  *
+  * @param None
+  * @retval None
+  */
+void board_basic_init(void);
+
+/**
+  * @general Initialize the system ticks
+  *
+  * @param None
+  * @retval None
+  */
+void board_tick_init(void);
+
+/**
+  * @general Initializes the standard serial port, which is normally used as
+  * the standard input/output
+  *
+  * @param None
+  * @retval None
+  */
+void board_stduart_init(void);
+
+/**
+  * @brief Initialize the watchdog, which will also complete the setting for feeding the dog.
+  * After calling this function to start the watchdog, the user does not need to perform the
+  * dog-feeding operation, so the initialization here means that all the initialization related
+  * to the watchdog has been completed.
+  *
+  * @param None
+  * @retval None
+  */
+void board_wdg_init(void);
+
+/**
+  * @brief Initialize GPIO pins, such as initializing GPIO controller, pin mapping.
+  *
+  * @param None
+  * @retval None
+  */
+void board_gpio_init(void);
+
+/**
+  * @brief flash control Initialization
+  * @param None
+  * @retval None
+  */
+void board_flash_init(void);
+
+/**
+  * @brief WIFI Initialization Function
+  *
+  * @param None
+  * @retval None
+  */
+void board_wifi_init(void);
+
+/**
+  * @brief ota Initialization Function. Perform OTA related processing during the
+  * initialization phase, such as detecting whether a rollback is required.
+  *
+  * @param None
+  * @retval None
+  */
+void board_ota_init(void);
+
+/**
+  * @brief Initialize the DMA controller.
+  *
+  * @param None
+  * @retval None
+  */
+void board_dma_init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
