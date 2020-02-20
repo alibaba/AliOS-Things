793f370f0fba9e04ea11861ceeb3784c1387a13d
zhuqing.zq <zhuqing.zq@alibaba-inc.com>
2020-02-20 20:15:51 +0800

    BugID:24656372:add i2c spi hal app demo code;app
    Change-Id: I0c90505fa691ab5c0c73ad30cc6da7d722635ba4

---
+++ application/example/halappdemo/halapp_i2c.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015-2017 Alibaba Group Holding Limited
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <aos/kernel.h>
+#include "aos/init.h"
+#include "aos/hal/gpio.h"
+#include "aos/hal/i2c.h"
+#include "board.h"
+#include <k_api.h>
+
+#if (AOS_HAL_I2C_ENABLED > 0)
+#ifndef HALAPP_I2C_TEST
+#define HALAPP_I2C_TEST             PORT_I2C_1
+#endif
+
+#define HAL_AT24CXX_DEV_WRITE_ADDR  0XA0
+#define HAL_AT24CXX_DEV_READ_ADDR   0XA1
+#define HAL_AT24CXX_TEST_MEM_ADDR   0
+
+static char *halapp_i2c_test_data = "halapp_i2c test OK!";
+static i2c_dev_t i2c_dev = {0};
+
+/* toggle gpio_out periodically in timer handler */
+void hal_i2c_app_run(void)
+{
+    int32_t ret;
+    uint16_t size = strlen(halapp_i2c_test_data) + 1;
+    char data_in[40] = {0};
+
+    printf("hal_i2c_app_run in\r\n");
+    i2c_dev.port = HALAPP_I2C_TEST;
+    i2c_dev.config.address_width = I2C_HAL_ADDRESS_WIDTH_7BIT;
+    i2c_dev.config.freq = I2C_BUS_BIT_RATES_100K;
+    i2c_dev.config.mode = I2C_MODE_MASTER;
+    ret = hal_i2c_init(&i2c_dev);
+
+    if(ret){
+        printf("hal_i2c_app_run: hal_i2c_init error!\r\n");
+        return;
+    }
+    ret = hal_i2c_mem_write(&i2c_dev, HAL_AT24CXX_DEV_WRITE_ADDR, HAL_AT24CXX_TEST_MEM_ADDR,
+                              size, halapp_i2c_test_data, size,20);
+    if(ret){
+        printf("hal_i2c_app_run: hal_i2c_mem_write error!\r\n");
+        return;
+    }
+
+    ret = hal_i2c_mem_read(&i2c_dev, HAL_AT24CXX_DEV_READ_ADDR, HAL_AT24CXX_TEST_MEM_ADDR,
+                             size, data_in, size, 20);
+    if(ret){
+        printf("hal_i2c_app_run: hal_i2c_mem_read error!\r\n");
+        return;
+    }
+    ret = strncmp(data_in,halapp_i2c_test_data,size);
+    if(ret){
+        printf("hal_i2c_app_run: read data not equal to write data!\r\n");
+        return;
+    }
+
+    printf("hal_i2c_app_run end\r\n");
+}
+
+#endif
