d06f22bdb08f504bbc5f1d7248dcf7cd3721e1b0
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-20 14:28:39 +0800

    BugID:24843276:modified hal adc API hal_adc_value_get
    Changes:
        1. change the second arg of the API to uint32_t *, we assume
           that the resolution of ADC on the SOC is no more than 32 bits
        2. The API should support ADC which consists of multiple channels,
           the results of all the channel are copied into the buffer one
           by one according to the sequence of channels registered from ADC
           mapping table
    
    Change-Id: I03272fc56ae2e0f4bf3a8ede92e38a2c28f53760

--- include/aos/hal/adc.h
+++ include/aos/hal/adc.h
@@ -45,13 +45,17 @@ int32_t hal_adc_init(adc_dev_t *adc);
 /**
  * Takes a single sample from an ADC interface
  *
+ * @note If the ADC is configured with mutiple channels, the result of each channel is
+ *       copied to output buffer one by one according to the sequence of the channel registered,
+ *       and each result takes sizeof(uint32_t) bytes.
+ *
  * @param[in]   adc      the interface which should be sampled
  * @param[out]  output   pointer to a variable which will receive the sample
  * @param[in]   timeout  ms timeout
  *
  * @return  0 : on success,  otherwise is error
  */
-int32_t hal_adc_value_get(adc_dev_t *adc, void *output, uint32_t timeout);
+int32_t hal_adc_value_get(adc_dev_t *adc, uint32_t *output, uint32_t timeout);
 
 /**
  * De-initialises an ADC interface, Turns off an ADC hardware interface
