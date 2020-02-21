fe7e0db3facc81c8f68600285d21395f16188704
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-21 20:51:55 +0800

    BugID:25211717: update xr871 for new build system
    Change-Id: I5cc974b34baf269861f3d4be4164740eeac04024

--- platform/mcu/xr871/mkimage.mk
+++ platform/mcu/xr871/mkimage.mk
@@ -3,16 +3,26 @@
 
 include $(SOURCE_ROOT)/platform/mcu/xr871/config.mk
 
+ifeq ($(APPDIR),)
+BINARY_DIR ?= $(SOURCE_ROOT)out/$(CLEANED_BUILD_STRING)/binary
+XR_MCU_LOCATION := $(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)
+XR_CFG_FILE := image-${APP}
+else
+BINARY_DIR ?= $(APPDIR)/out/$(CLEANED_BUILD_STRING)/binary
+XR_MCU_LOCATION := $($(HOST_MCU_FAMILY)_LOCATION)
+XR_CFG_FILE := image
+endif
+
 ifeq ($(HOST_OS),Win32)
-MKIMAGE_TOOL := "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/tools/mkimage.exe"
+MKIMAGE_TOOL := "$(XR_MCU_LOCATION)/drivers/tools/mkimage.exe"
 else  # Win32
 ifeq ($(HOST_OS),Linux32)
-MKIMAGE_TOOL := "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/tools/mkimage"
-XZ_TOOL := "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/tools/xz"
+MKIMAGE_TOOL := "$(XR_MCU_LOCATION)/drivers/tools/mkimage"
+XZ_TOOL := "$(XR_MCU_LOCATION)/drivers/tools/xz"
 else # Linux32
 ifeq ($(HOST_OS),Linux64)
-MKIMAGE_TOOL := "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/tools/mkimage_64"
-XZ_TOOL := "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/tools/xz64"
+MKIMAGE_TOOL := "$(XR_MCU_LOCATION)/drivers/tools/mkimage_64"
+XZ_TOOL := "$(XR_MCU_LOCATION)/drivers/tools/xz64"
 else # Linux64
 $(error not surport for $(HOST_OS))
 endif # Linux64
@@ -39,16 +49,16 @@ else
 IMAGE_OTA :=
 endif
 
-IMAGE_CFG_FILE ?= "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/image_cfg/image-${APP}${IMAGE_XZ}.cfg"
-IMAGE_PACK_DIR ?= "$(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/pack"
-BINARY_DIR ?= $(SOURCE_ROOT)out/$(CLEANED_BUILD_STRING)/binary
+IMAGE_CFG_FILE ?= "$(XR_MCU_LOCATION)/drivers/image_cfg/$(XR_CFG_FILE)${IMAGE_XZ}.cfg"
+IMAGE_PACK_DIR ?= "$(XR_MCU_LOCATION)/drivers/pack"
+
 
 mkimage:
 	$(OBJCOPY) -O binary -R .xip -R .eh_frame -R .init -R .fini -R .comment -R .ARM.attributes $(BINARY_DIR)/$(CLEANED_BUILD_STRING).elf $(BINARY_DIR)/$(CLEANED_BUILD_STRING).bin
 	$(OBJCOPY) -O binary -j .xip $(BINARY_DIR)/$(CLEANED_BUILD_STRING).elf $(BINARY_DIR)/$(CLEANED_BUILD_STRING).xip.bin
-	$(CP) -vf $(SOURCE_ROOT)/$($(HOST_MCU_FAMILY)_LOCATION)/drivers/bin/xr871/*.bin  $(IMAGE_PACK_DIR)/
-	$(CP) -vf $(SOURCE_ROOT)out/$(CLEANED_BUILD_STRING)/binary/$(CLEANED_BUILD_STRING).bin  $(IMAGE_PACK_DIR)/app.bin
-	$(CP) -vf $(SOURCE_ROOT)out/$(CLEANED_BUILD_STRING)/binary/$(CLEANED_BUILD_STRING).xip.bin  $(IMAGE_PACK_DIR)/app-xip.bin
+	$(CP) -vf $(XR_MCU_LOCATION)/drivers/bin/xr871/*.bin  $(IMAGE_PACK_DIR)/
+	$(CP) -vf $(BINARY_DIR)/$(CLEANED_BUILD_STRING).bin  $(IMAGE_PACK_DIR)/app.bin
+	$(CP) -vf $(BINARY_DIR)/$(CLEANED_BUILD_STRING).xip.bin  $(IMAGE_PACK_DIR)/app-xip.bin
 ifneq ($(no_with_image_compress),1)
 	$(RM) -vf $(IMAGE_PACK_DIR)/*.bin.xz
 	$(XZ_TOOL) -vk --check=crc32 --lzma2=preset=6e,dict=32KiB $(IMAGE_PACK_DIR)/net.bin
@@ -56,5 +66,5 @@ ifneq ($(no_with_image_compress),1)
 endif
 	$(CP) -vf $(MKIMAGE_TOOL)  $(IMAGE_PACK_DIR)/mkimage
 	$(CP) -vf $(IMAGE_CFG_FILE)  $(IMAGE_PACK_DIR)/
-	cd $(IMAGE_PACK_DIR) && ./mkimage ${IMAGE_OTA} -c image-${APP}${IMAGE_XZ}.cfg
+	cd $(IMAGE_PACK_DIR) && ./mkimage ${IMAGE_OTA} -c $(XR_CFG_FILE)${IMAGE_XZ}.cfg
 
