9bb8f2667c294ed0fa14de6fafaca06e43a4d32e
zhangtian <dongren.zt@alibaba-inc.com>
2020-02-21 14:41:12 +0800

    BugID:24882677:[include]:add umesh2 related header files
    Change-Id: I12f52fc38fe86e7e22c6c6a637865814d8c61930

---
+++ include/network/umesh2/mdns/mdns.h
@@ -0,0 +1,85 @@
+
+#ifndef __MDNS_H__
+#define __MDNS_H__
+
+
+#include <stdbool.h>
+#include "network/umesh2/mdns/mdns_rr.h"
+
+# ifdef __cplusplus
+extern "C" {
+# endif
+
+
+struct mdns_ctx;
+
+#define MDNS_PORT        5353
+#define MDNS_ADDR_IPV6   "FF02::FB"
+
+#define GET_RCODE(x)    (x&0x000f)
+#define SET_RCODE(x,c)  (x|(c&0x000f))
+
+#define GET_OPCODE(x)   (x&0x7800)
+#define SET_OPCODE(x,c) (x|(c&0x7800))
+
+enum mdns_hdr_flag {
+    FLAG_QR = 1 << 15, // Question/Response
+    FLAG_AA = 1 << 10, // Authoritative Answer
+    FLAG_TC = 1 <<  9, // Truncated Response
+    FLAG_RD = 1 <<  8, // Recursion Desired
+    FLAG_RA = 1 <<  7, // Recursion Available
+    FLAG_Z  = 1 <<  6, // Reserved
+    FLAG_AD = 1 <<  5, // Authentic Data
+    FLAG_CD = 1 <<  4, // Checking Disabled
+};
+
+struct mdns_hdr {
+    uint16_t id;
+    uint16_t flags;
+    uint16_t num_qn;
+    uint16_t num_ans_rr;
+    uint16_t num_auth_rr;
+    uint16_t num_add_rr;
+};
+
+struct mdns_ip {
+    unsigned int family;
+    union {
+        struct {
+            struct in_addr addr;
+        } ipv4;
+        struct {
+            struct in6_addr addr;
+        } ipv6;
+    };
+};
+
+typedef void (*mdns_listen_callback)(void *, int, const struct mdns_entry *);
+typedef void (*mdns_announce_callback)(void *, int, const struct mdns_ip *, const struct mdns_entry *);
+
+typedef bool (*mdns_stop_func)(void *);
+
+int mdns_init(struct mdns_ctx **ctx, const char *addr, unsigned short port);
+
+int mdns_destroy(struct mdns_ctx *ctx);
+
+int mdns_entries_send(const struct mdns_ctx *ctx, const struct mdns_hdr *hdr, const struct mdns_entry *entries);
+
+void mdns_entries_print(const struct mdns_entry *);
+
+int mdns_listen(const struct mdns_ctx *ctx, const char *const names[],
+                unsigned int nb_names, enum mdns_type type,
+                unsigned int interval, mdns_stop_func stop,
+                mdns_listen_callback callback, void *context);
+
+
+int mdns_announce(struct mdns_ctx *ctx, const char *service, enum mdns_type type,
+                  mdns_announce_callback callback, void *context);
+
+int mdns_serve(struct mdns_ctx *ctx, mdns_stop_func stop, void *context);
+
+# ifdef __cplusplus
+}
+# endif
+
+#endif /* __MDNS_H__ */
