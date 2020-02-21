012a90fd1f4b4705e676041ddf9e3beb5d07ca77
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-21 10:24:31 +0800

    BugID:25211717: add deps comp in aos.mk
    Change-Id: I4b097e41b5a8fc76bf8c6cd7b19884a313e1dd6c

--- application/example/example_legacy/yts/aos.mk
+++ application/example/example_legacy/yts/aos.mk
@@ -9,7 +9,8 @@ $(NAME)_COMPONENTS := testcase ulog vfs yloop
 $(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable
 
 ifneq (,$(findstring linux, $(BUILD_STRING)))
-$(NAME)_COMPONENTS += lwip netmgr fatfs
+$(NAME)_COMPONENTS-$(AOS_BOARD_LINUXHOST) += lwip netmgr fatfs
+$(NAME)_COMPONENTS-$(AOS_BOARD_ARMHFLINUX) += lwip netmgr fatfs
 
 GLOBAL_LDFLAGS += -lreadline -lncurses -lrt
 GLOBAL_DEFINES += CONFIG_AOS_MESHYTS DEBUG YTS_LINUX
