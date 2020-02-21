46d1174f758cd91a1f642e41236d34f5307254ff
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-21 15:45:59 +0800

    BugID:24843276: Restore adc mapping table according to team meeting
    Change-Id: I7e0203b172be13110125293021e67b201dbbe087

--- platform/board/stm32f429zi-nucleo-demo/drivers/Src/adc.c
+++ platform/board/stm32f429zi-nucleo-demo/drivers/Src/adc.c
@@ -38,69 +38,31 @@
   */
 
 /* Includes ------------------------------------------------------------------*/
-#include <string.h>
-#include "aos/hal/adc.h"
 #include "stm32f4xx_hal.h"
-#include "aos/hal/gpio.h"
-#include "hal_adc_stm32f4.h"
+
+#ifdef HAL_ADC_MODULE_ENABLED
 
 void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
 {
-    GPIO_InitTypeDef GPIO_InitStruct;
-    stm32_adc_dev_t *adc_dev;
-    ADC_MAPPINT     *entry;
-    GPIO_TypeDef    *GPIOx;
-    uint32_t         pin;
-
-    adc_dev = entryof_stm32_adc_dev(hadc);
-    entry = hal_adc_get_mapping_entry(adc_dev->port);
-
-    /* Only adc channle 0 ~ channel 16 are gpio mapped */
-    if (entry->hal_adc_channel <= HAL_ADC_CHANNEL_16) {
-        if (hal_gpio_group(entry->hal_gpio_pin, &GPIOx)) {
-           return;
-        }
-
-        pin = hal_gpio_pin(entry->hal_gpio_pin);
-        hal_gpio_enable_clk(GPIOx);
-
-        memset(&GPIO_InitStruct, 0, sizeof(GPIO_InitStruct));
-
-        GPIO_InitStruct.Pin = pin;
-        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
-    }
-
     if (hadc->Instance == ADC1) {
         __HAL_RCC_ADC1_CLK_ENABLE();
     } else if (hadc->Instance == ADC2) {
         __HAL_RCC_ADC2_CLK_ENABLE();
+    } else if (hadc->Instance == ADC3) {
+        __HAL_RCC_ADC3_CLK_ENABLE();
     }
 }
 
 void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
 {
-    GPIO_InitTypeDef GPIO_InitStruct;
-    stm32_adc_dev_t *adc_dev;
-    ADC_MAPPINT     *entry;
-    GPIO_TypeDef    *GPIOx;
-    uint16_t         pin;
-
-    adc_dev = entryof_stm32_adc_dev(hadc);
-    entry = hal_adc_get_mapping_entry(adc_dev->port);
-    pin = hal_gpio_pin(entry->hal_gpio_pin);
-    if (hal_gpio_group(entry->hal_gpio_pin, &GPIOx)) {
-       return;
-    }
-
-
     if (hadc->Instance == ADC1) {
         __HAL_RCC_ADC1_CLK_DISABLE();
     } else if (hadc->Instance == ADC2) {
         __HAL_RCC_ADC2_CLK_DISABLE();
+    } else if (hadc->Instance == ADC) {
+        __HAL_RCC_ADC3_CLK_DISABLE();
     }
-
-    HAL_GPIO_DeInit(GPIOx, pin);
 }
+
+#endif /* HAL_ADC_MODULE_ENABLED */
 
