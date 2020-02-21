256be2d68f69339c8aa88bed5f69ccd3d082d278
zhangtian <dongren.zt@alibaba-inc.com>
2020-02-21 14:26:06 +0800

    BugID:24882677:[umesh2]:improve code,fix some bugs
    Change-Id: Ifb25726c902b59a6497fea9a696c51d95c2a17a2

--- components/network/umesh2/aos.mk
+++ components/network/umesh2/aos.mk
@@ -2,21 +2,7 @@ NAME := umesh2
 
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.0
-$(NAME)_SUMMARY := uMesh provides the ability to form local area mesh network.
+$(NAME)_SUMMARY := mesh2 implement.
 
-$(NAME)_DEFINES += UTILS_AES_BUILDIN
-
-
-$(NAME)_DEFINES += USE_ACTION_FRAME
-
-$(NAME)_INCLUDES += include/core       \
-                    include/adapter    \
-                    include/utils  
-
-$(NAME)_SOURCES := src/core/*.c        \
-                   src/adapter/*.c     \
-                   src/utils/*.c                   
-
-$(NAME)_DEFINES += LWIP_IPV6=1
-
-$(NAME)_COMPONENTS-$(UMESH_USE_YLOOP) := yloop
+$(NAME)_COMPONENTS-$(UMESH_WITH_DEFAULT_CORE) += umesh2_core
+$(NAME)_COMPONENTS-$(UMESH_WITH_DEFAULT_MDNS) += mdns
--- components/network/umesh2/include/utils/utils_crc32.h
+++
@@ -1,8 +0,0 @@
-#ifndef _UMESH_UTILS_CRC32_H_
-#define _UMESH_UTILS_CRC32_H_
-
-#include <stdint.h>
-
-uint32_t umesh_crc32(const void *buf, unsigned long size);
-
-#endif /* CRC32_H_ */
