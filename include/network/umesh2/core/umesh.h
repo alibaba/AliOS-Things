8fca1942caa7eea0bb42ce3bda68c49ef3c0b0fa
zhangtian <dongren.zt@alibaba-inc.com>
2020-02-21 14:41:12 +0800

    BugID:24882677:[include]:add umesh2 related header files
    Change-Id: I12f52fc38fe86e7e22c6c6a637865814d8c61930

---
+++ include/network/umesh2/core/umesh.h
@@ -0,0 +1,10 @@
+#ifndef UMESH_H_
+#define UMESH_H_
+
+
+void *umesh_network_init();
+
+int umesh_recv_ip_data(struct umesh_state *state, const uint8_t *data, uint16_t data_len, uint8_t *dst_addr);
+void umesh_network_deinit(void *handle);
+
+#endif /* UMESH_SCHEDULE_H_ */
