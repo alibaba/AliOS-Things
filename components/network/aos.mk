6abd06fc5ad3b631b4329319ff56c87f46cec255
yajun.syj <yajun.syj@alibaba-inc.com>
2020-02-22 10:37:44 +0800

    BugID:23823399: Fix aos create project network/lwip compile error
    Change-Id: I9a480f97dafa42798b78caf21fdc1e6c15d567dc

--- components/network/aos.mk
+++ components/network/aos.mk
@@ -4,6 +4,4 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := network
 
-ifeq ($(CONFIG_AOS_LWIP), y)
-$(NAME)_COMPONETNS += lwip
-endif
+$(NAME)_COMPONENTS-$(CONFIG_AOS_LWIP) += lwip
