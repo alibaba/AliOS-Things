f9e9e805e17a195524b57405a69f9c16859e9fb7
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2020-02-25 12:16:15 +0800

    BugID:24381325:Adjust to support the new specification board
    Change-Id: I4b75cac5444c540cac5f4547d7c3213a768ec6e6

---
+++ application/example/basicfunction_app/aos_api_app/timer_app/timer_new.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+
+#include <aos/kernel.h>
+#include <ulog/ulog.h>
+
+/**
+ * This use case demonstrates timer creation using the aos_timer_new() interface.
+ */
+
+/* module name used by ulog */
+#define MODULE_NAME "timer_app"
+
+/* timer handle */
+static aos_timer_t timer1;
+
+/**
+ * timer callback function.
+ * The current use case is executed every 1300ms.
+ */
+static void timer1_func(void *timer, void *arg)
+{
+    /*
+    * Warning: an interface that causes a block is not allowed to be called within this function.
+    * Blocking within this function will cause other timers to run incorrectly.
+    * The printf function may also block on some platforms, so be careful how you call it.
+    */
+    printf("[timer_new]timer expires\r\n");
+}
+
+void timer_new(void)
+{
+    int status;
+
+    /**
+     * Create timer. Timer starts automatically after successful creation.
+     * some of the parameters are as follows:
+     * fn:      timer1_func (this function is called when the timer expires)
+     * ms:      1500 (the cycle of the timer)
+     * repeat:  1 (set to periodic timer)
+     */
+    status = aos_timer_new(&timer1, timer1_func, NULL, 1300, 1);
+    if (status != 0) {
+        LOGE(MODULE_NAME, "create timer error");
+        return;
+    }
+}
+
