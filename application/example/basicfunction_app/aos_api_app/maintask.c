7bb12a88fbe43e6fc1a5e7e1236c54dd5870f474
zhuqing.zq <zhuqing.zq@alibaba-inc.com>
2020-02-26 11:42:35 +0800

    BugID:24656372:modify linuxhost of mcu linuxhost to 2020 bsp standard;app
    Change-Id: I44b19cf793d7b4a53ad20e2022cb40c00bb1ee55

--- application/example/basicfunction_app/aos_api_app/maintask.c
+++ application/example/basicfunction_app/aos_api_app/maintask.c
@@ -22,6 +22,7 @@ extern void board_stduart_init(void);
 extern void board_dma_init(void);
 extern void board_gpio_init(void);
 extern void board_wifi_init(void);
+extern int board_kinit_init(kinit_t* init_args);
 
 /*  For user config
     kinit.argc = 0;
@@ -44,6 +45,7 @@ void board_init(void)
 void aos_maintask(void* arg)
 {
     board_init();
+	board_kinit_init(&kinit);
     aos_components_init(&kinit);
 
 #ifndef AOS_BINS
