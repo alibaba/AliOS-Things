3845f063f4f18cf5b2a228666d5e3d3ef9d44d58
zixun.lyq <zixun.lyq@alibaba-inc.com>
2020-02-26 21:33:29 +0800

    BugID:23824859: Add Keil and IAR OTA libs stm32f429zi
    Change-Id: Ifdadc5bb0a33de9588f6b1457b76ee393dac9f22

--- components/dm/ota/aos.mk
+++ components/dm/ota/aos.mk
@@ -12,21 +12,29 @@ $(NAME)_CFLAGS      += -Wall -Werror
 else ifeq ($(COMPILER),gcc)
 $(NAME)_CFLAGS      += -Wall -Werror
 endif
+ifeq ($(COMPILER),armcc)
+$(NAME)_PREBUILT_LIBRARY := lib/keil/$(HOST_ARCH)/ota_agent.a
+else 
+ifeq ($(COMPILER),iar)
+$(NAME)_PREBUILT_LIBRARY := lib/iar/$(HOST_ARCH)/ota_agent.a
+else
 ifneq (,$(filter breeze,$(COMPONENTS)))
 ifneq (,$(filter mcu_nrf52xxx mcu_esp32,$(HOST_MCU_FAMILY)))
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/ota_agent_ble.a
+$(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent_ble.a
 endif
 else
 ifneq (,$(filter itls,$(COMPONENTS)))
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/ota_agent_itls.a
+$(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent_itls.a
 else
 ifneq (,$(filter mcu_esp32,$(HOST_MCU_FAMILY)))
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/ota_agent_esp32.a #esp32
+$(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent_esp32.a #esp32
 else
 ifneq (,$(filter mcu_stm32l4xx_cube mcu_stm32f4xx_cube mcu_mtk7682 mcu_m487jidae mcu_msp432p4xx, $(HOST_MCU_FAMILY)))
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/ota_agent_softabi.a #mt7682s
+$(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent_softabi.a #mt7682s
 else
-$(NAME)_PREBUILT_LIBRARY := lib/$(HOST_ARCH)/ota_agent.a #mk3080
+$(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent.a #mk3080
+endif
+endif
 endif
 endif
 endif
