728c961278d8600cb0d4f2c3dc2fe422882733c2
wencheng.ywc <wencheng.ywc@alibaba-inc.com>
2020-02-26 13:55:13 +0800

    BugID:23823143: Added vfs app demo
    Change-Id: I68300c83d4064beb3270aa1a3e4caec93eb51617

---
+++ application/example/vfs_demo/README.md
@@ -0,0 +1,20 @@
+## Overview
+
+## Feature of Board
+
+## Directories
+
+```sh
+├── aos.mk
+├── Config.in
+├── k_app_config.h
+├── maintask.c
+├── README.md
+└── vfs.c
+```
+
+## Board Hardware Resources
+
+## Update log
+
+## Reference
---
+++ application/example/vfs_demo/maintask.c
@@ -0,0 +1,51 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <aos/kernel.h>
+#include "aos/init.h"
+#include "board.h"
+#include <k_api.h>
+
+#ifndef AOS_BINS
+extern int application_start(int argc, char *argv[]);
+#endif
+
+/*
+If board have no component for example board_xx_init, it indicates that this app does not support this board.
+Set the correspondence in file platform\board\aaboard_demo\ucube.py.
+*/
+extern void board_tick_init(void);
+extern void board_stduart_init(void);
+
+/*  For user config
+    kinit.argc = 0;
+    kinit.argv = NULL;
+    kinit.cli_enable = 1;
+*/
+static kinit_t kinit = {0, NULL, 1};
+
+/**
+  * @brief Board Initialization Function
+  * @param None
+  * @retval None
+  */
+void board_init(void)
+{
+    board_tick_init();
+    board_dma_init();
+    board_stduart_init();
+}
+
+void aos_maintask(void* arg)
+{
+    board_init();
+    aos_components_init(&kinit);
+
+#ifndef AOS_BINS
+    application_start(kinit.argc, kinit.argv);  /* jump to app entry */
+#endif
+}
+
