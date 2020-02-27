4c54ba99b1024ca9e4a2d6c7ef9b1a2b3a89d436
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-27 21:23:09 +0800

    BugID:25211717: update deps comp in aos.mk
    Change-Id: I5d9ea97083f11c2957a22a613253c035701ba2fa

--- platform/board/board_legacy/stm32f411re-nucleo/aos.mk
+++ platform/board/board_legacy/stm32f411re-nucleo/aos.mk
@@ -11,7 +11,7 @@ HOST_MCU_NAME      := STM32F411RET
 ENABLE_VFP         := 1
 
 $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) kernel_init network
-$(NAME)_COMPONENTS-$(BSP_SUPPORT_EXTERNAL_MODULE) = external_module
+$(NAME)_COMPONENTS-$(BSP_SUPPORT_EXTERNAL_MODULE) += external_module
 
 ifneq ($(ENABLE_USPACE),1)
 $(NAME)_SOURCES += aos/aos.c
--- platform/board/board_legacy/stm32l476rg-nucleo/aos.mk
+++ platform/board/board_legacy/stm32l476rg-nucleo/aos.mk
@@ -11,7 +11,7 @@ HOST_MCU_NAME      := STM32L476RG-Nucleo
 ENABLE_VFP         := 1
 
 $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) kernel_init network
-$(NAME)_COMPONENTS-$(BSP_SUPPORT_EXTERNAL_MODULE) = external_module
+$(NAME)_COMPONENTS-$(BSP_SUPPORT_EXTERNAL_MODULE) += external_module
 
 $(NAME)_SOURCES += aos/board_partition.c \
                    aos/soc_init.c
