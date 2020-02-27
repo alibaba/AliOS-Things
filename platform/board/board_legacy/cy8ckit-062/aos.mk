197e2b2199756a552ab15f40197a2c308d5256c2
hongye.jhy <hongye.jhy@alibaba-inc.com>
2020-02-27 21:23:09 +0800

    BugID:25211717: update deps comp in aos.mk
    Change-Id: I5d9ea97083f11c2957a22a613253c035701ba2fa

--- platform/board/board_legacy/cy8ckit-062/aos.mk
+++ platform/board/board_legacy/cy8ckit-062/aos.mk
@@ -11,7 +11,7 @@ HOST_MCU_FAMILY    := mcu_cy8c6347
 SUPPORT_MBINS      := no
 
 $(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) kernel_init network
-$(NAME)_COMPONENTS-$(BSP_SUPPORT_EXTERNAL_MODULE) = external_module
+$(NAME)_COMPONENTS-$(BSP_SUPPORT_EXTERNAL_MODULE) += external_module
 
 $(NAME)_SOURCES := board.c
 
