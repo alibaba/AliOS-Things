b366cd10eb61e9d9edf23afc1b1861effb0e4c17
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-28 10:40:52 +0800

    BugID:25320122: update linkkit wrapper config.in
    Change-Id: I365d37af36baedf9e2cb0a71d1771c3734576385

--- components/linkkit/wrappers/aos.mk
+++ components/linkkit/wrappers/aos.mk
@@ -4,29 +4,24 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 3.0.2
 $(NAME)_SUMMARY := HAL for AliOS Things
 
-$(NAME)_COMPONENTS += libiot_infra
+$(NAME)_DEFINES +=  USE_YLOOP
+
+ifeq (y,$(strip $(TLS_SAVE_TICKET)))
+$(NAME)_DEFINES += os/TLS_SAVE_TICKET
+endif
 
 $(NAME)_SOURCES := os/HAL_OS_rhino.c \
                    os/HAL_TCP_rhino.c \
-                   os/HAL_PRODUCT_rhino.c \
-
-$(NAME)_DEFINES +=  USE_YLOOP 
+                   os/HAL_PRODUCT_rhino.c
 
 $(NAME)_SOURCES-$(FS_ENABLED) += os/HAL_FS_rhino.c
+$(NAME)_SOURCES-$(DEV_BIND_ENABLED) += os/HAL_AWSS_rhino.c
+$(NAME)_SOURCES-$(COAP_SERVER) +=  os/HAL_UDP_rhino.c
 
-$(NAME)_COMPONENTS += ulog activation chip_code netmgr
+# mandatory dependencies
+$(NAME)_COMPONENTS += libiot_infra ulog activation chip_code netmgr
 
-GLOBAL_DEFINES += BUILD_AOS
-$(NAME)_DEFINES += USE_YLOOP
-
-ifeq (y,$(strip $(DEV_BIND_ENABLED)))
-$(NAME)_SOURCES += os/HAL_AWSS_rhino.c
-endif
-
-ifeq (y,$(strip $(COAP_SERVER)))
-$(NAME)_SOURCES += os/HAL_UDP_rhino.c
-endif
-
+# optional dependencies
 $(NAME)_COMPONENTS-$(SUPPORT_TLS) += libiot_certs
 $(NAME)_COMPONENTS-$(USE_MBEDTLS) +=  mbedtls 
 $(NAME)_COMPONENTS-$(USE_ITLS) +=  itls id2 alicrypto
@@ -44,7 +39,6 @@ ifeq (y,$(strip $(SUPPORT_TLS)))
     endif
 endif
 
-
 $(NAME)_COMPONENTS-$(COAP_DTLS_SUPPORT) += mbedtls libiot_certs
 ifeq (y,$(strip $(COAP_COMM_ENABLED)))
 ifeq (y,$(strip $(COAP_DTLS_SUPPORT)))
@@ -53,9 +47,4 @@ $(NAME)_SOURCES += os/HAL_DTLS_mbedtls.c
 $(NAME)_DEFINES += USE_PUBLIC_MBEDTLS
 endif
 $(NAME)_SOURCES += os/HAL_UDP_rhino.c
-endif 
-
-ifeq (y,$(strip $(TLS_SAVE_TICKET)))
-$(NAME)_DEFINES += os/TLS_SAVE_TICKET
 endif
-
