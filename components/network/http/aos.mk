8e7df8e4518d686ba2ab849238920beb7fd4319e
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-02-28 17:55:42 +0800

    BugID:23823350: update http api to avoid conflict
    Change-Id: I46979d9d7ff4e61a3b675f29606b2382efff59e2

--- components/network/http/aos.mk
+++ components/network/http/aos.mk
@@ -5,18 +5,21 @@ $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.1
 $(NAME)_SUMMARY := http client component
 # source files and the folder of internal include files
+
+ifeq (y, $(CONFIG_NEW_HTTP_API))
 $(NAME)_INCLUDES += include
 $(NAME)_SOURCES := src/http_client.c \
                    wrapper/http_aos_wrapper.c \
                    src/http_method_api.c \
                    src/http_formdata.c
-
+else
 DEPRECATED_PATH := ./deprecated
 $(NAME)_INCLUDES += $(DEPRECATED_PATH)/include
 $(NAME)_SOURCES += $(DEPRECATED_PATH)/src/http_client.c \
                    $(DEPRECATED_PATH)/src/http_parser.c \
                    $(DEPRECATED_PATH)/src/http_string.c \
                    $(DEPRECATED_PATH)/wrapper/http_aos_wrapper.c
+endif
 
 # the folder of API files
 GLOBAL_INCLUDES += ../../../include/network/http
