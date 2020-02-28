368abe34423c0250fe21b3769abe2e9651350ad1
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-28 19:53:05 +0800

    BugID:24843276:Changed hal_adc_value_get arg type
    Details:
        Changed the second arg of hal_adc_value_get from
        void * to uint32_t *, as most on chip ADC resolution
        is no more than 32 bits
    
    Change-Id: I763ab90946849e3a3a1d4eddff5e139402dd1768

--- platform/board/mk3072/config/board.h
+++ platform/board/mk3072/config/board.h
@@ -1,5 +1,6 @@
 #ifndef __BOARD_H__
 #define __BOARD_H__
+
 #include "aos/hal/flash.h"
 
 extern hal_logic_partition_t hal_partitions[];
@@ -14,6 +15,63 @@ typedef enum {
     PORT_UART_TEMP,
     PORT_UART_SIZE,
     PORT_UART_INVALID = 255
-}PORT_UART_TYPE;
+} PORT_UART_TYPE;
 
-#endif //__BOARD_H__
+typedef enum{
+    PORT_I2C_1 = 0,
+    PORT_I2C_SIZE,
+    PORT_I2C_INVALID = 255,
+    PORT_I2C_DEMO = PORT_I2C_1,
+}PORT_I2C_TYPE;
+
+typedef enum{
+    PORT_SPI_1,
+    PORT_SPI_SIZE,
+    PORT_SPI_INVALID =255,
+    PORT_SPI_DEMO = PORT_SPI_1,
+}PORT_SPI_TYPE;
+
+typedef enum {
+    PORT_ADC_1,
+    PORT_ADC_2,
+    PORT_ADC_SIZE,
+    PORT_ADC_INVALID =255,
+    PORT_ADC_DEMO = PORT_ADC_1,
+} PORT_ADC_TYPE;
+
+/*
+    hal support list
+*/
+#define AOS_HAL_GPIO_ENABLED        1
+#define AOS_HAL_UART_ENABLED        1
+#define AOS_HAL_SPI_ENABLED         0
+#define AOS_HAL_I2C_ENABLED         0
+#define AOS_HAL_FLASH_ENABLED       0
+#define AOS_HAL_PWM_ENABLED         0
+#define AOS_HAL_TIMER_ENABLED       0
+#define AOS_HAL_WDG_ENABLED         0
+#define AOS_HAL_RTC_ENABLED         0
+#define AOS_HAL_ADC_ENABLED         0
+#define AOS_HAL_DAC_ENABLED         0
+#define AOS_HAL_RNG_ENABLED         0
+#define AOS_HAL_I2S_ENABLED         0
+#define AOS_HAL_QSPI_ENABLED        0
+#define AOS_HAL_CAN_ENABLED         0
+#define AOS_HAL_NAND_ENABLED        0
+#define AOS_HAL_NOR_ENABLED         0
+
+#if (AOS_HAL_GPIO_ENABLED > 0)
+#ifndef HALAPP_GPIO_OUT
+#define HALAPP_GPIO_OUT (0UL)
+#endif
+
+#ifndef HALAPP_GPIO_IN
+#define HALAPP_GPIO_IN  (0UL)
+#endif
+
+#ifndef HALAPP_GPIO_INT
+#define HALAPP_GPIO_INT (0UL)
+#endif
+#endif /* AOS_HAL_GPIO_ENABLED */
+
+#endif /* __BOARD_H__ */
