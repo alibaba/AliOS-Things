430ef3a584f78684d9053db35d94701e6ae9d91f
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-02-28 16:57:21 +0800

    BugID:24843276:Misc commit for stm32f4xx and stm32f1xx hal API
    Changes:
        1. define device logical name to it's physical address,
           related hal periphrals are adc, dac
    
    Change-Id: I3cb10b1f46c6b68ea83c7871c4600378042b220c

--- platform/mcu/stm32f1xx/hal/include/hal_dac_stm32f1.h
+++ platform/mcu/stm32f1xx/hal/include/hal_dac_stm32f1.h
@@ -9,20 +9,24 @@
  extern "C" {
 #endif
 
-#define HAL_DAC_1            ((uint8_t)0) /**< Maps to DAC1 */
-#define HAL_DAC_CNT          ((uint8_t)1) /**< Total DACs */
+#include "stm32f1xx_hal.h"
 
-#define HAL_DAC_CHANNEL_1    ((uint8_t)0) /**< Maps to DACx_CHANNEL_1 */
-#define HAL_DAC_CHANNEL_2    ((uint8_t)1) /**< Maps to DACx_CHANNEL_2 */
-#define HAL_DAC_CHANNEL_CNT  ((uint8_t)2) /**< Total DAC channels */
+#ifdef HAL_ADC_MODULE_ENABLED
+
+#define HAL_DAC_1            ((uint32_t)DAC1) /**< Maps to DAC1 */
+#define HAL_DAC_CNT          ((uint32_t)1) /**< Total DACs */
+
+#define HAL_DAC_CHANNEL_1    ((uint32_t)DAC1_CHANNEL_1) /**< Maps to DACx_CHANNEL_1 */
+#define HAL_DAC_CHANNEL_2    ((uint32_t)DAC1_CHANNEL_2) /**< Maps to DACx_CHANNEL_2 */
+#define HAL_DAC_CHANNEL_CNT  ((uint32_t)2) /**< Total DAC channels */
 
 /**
  * @brief The structure describes how the DAC channel and external
  *        Pin are related.
  */
 typedef struct {
-    uint8_t channel; /**< The logic DAC channel */
-    uint8_t pin;     /**< The logic pin */
+    uint32_t channel; /**< The logic DAC channel */
+    uint8_t  pin;     /**< The logic pin */
 } gpio_dac_pin_config_t;
 
 /**
@@ -31,12 +35,14 @@ typedef struct {
  */
 typedef struct {
     uint8_t                port;         /**< User defined DAC logic port number */
-    uint8_t                hal_dac;      /**< DAC logic port number */
+    uint32_t               hal_dac;      /**< DAC logic port number */
     gpio_dac_pin_config_t *channel_conf; /**< Poiter to channel mapping table */
     uint8_t                channel_cnt;  /**< the number of entries in channel mapping table */
 } DAC_MAPPING;
 
 extern DAC_MAPPING DAC_MAPPING_TABLE[];
+
+#endif /* HAL_ADC_MODULE_ENABLED */
 
 #ifdef __cplusplus
 }
