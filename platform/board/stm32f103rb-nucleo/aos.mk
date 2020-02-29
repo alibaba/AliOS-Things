25a70ef1b6efad8b86d04d70d6fad287d9e4096b
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2020-02-29 18:32:57 +0800

    BugID:24656372:remove old board esp8266 linuxhost mk3080 stm32f103rb-nucleo stm32f429-nucleo
    Change-Id: Ib21aa72bf15578a8797f30c309d2f4cda8299e12

---
+++ platform/board/stm32f103rb-nucleo/aos.mk
@@ -0,0 +1,70 @@
+NAME := board_stm32f103rb-necleo
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.0
+$(NAME)_SUMMARY    := configuration for board stm32f103rb-nucleo
+MODULE             := 1062
+HOST_ARCH          := Cortex-M3
+HOST_MCU_FAMILY    := mcu_stm32f1xx
+SUPPORT_MBINS      := no
+HOST_MCU_NAME      := STM32F103RBTx
+ENABLE_VFP         := 0
+
+GLOBAL_DEFINES += HAL_VERSION=30100
+
+$(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) osal_aos kernel_init network
+
+$(NAME)_SOURCES += config/k_config.c \
+                   config/partition_conf.c \
+                   drivers/stm32f1xx_hal_msp.c \
+                   drivers/system_stm32f1xx.c \
+                   drivers/src/adc.c \
+                   drivers/src/i2c.c \
+                   drivers/src/spi.c \
+                   drivers/src/timer.c \
+                   drivers/src/pwm.c \
+                   startup/board.c \
+                   startup/startup.c \
+
+ywss_support ?= 0
+
+#depends on sal module if select sal function via build option "AOS_NETWORK_SAL=y"
+AOS_NETWORK_SAL	?= n
+
+ifeq ($(COMPILER), armcc)
+$(NAME)_SOURCES += startup/startup_stm32f103xb_keil.s
+$(NAME)_LINK_FILES := startup/startup_stm32f103xb_keil.o
+$(NAME)_LINK_FILES += drivers/stm32f1xx_hal_msp.o
+
+GLOBAL_LDFLAGS += -L --scatter=platform/board/stm32f103rb-nucleo/stm32f103rb-nucleo.sct
+else ifeq ($(COMPILER), iar)
+$(NAME)_SOURCES += startup/startup_stm32f103xb_iar.s
+GLOBAL_LDFLAGS += --config platform/board/stm32f103rb-nucleo/stm32f103xb_flash.icf
+else
+#GLOBAL_CFLAGS += -DUSE_DIRECT_UART_PUTC
+$(NAME)_SOURCES += startup/startup_stm32f103xb.s
+GLOBAL_LDFLAGS += -T platform/board/stm32f103rb-nucleo/STM32F103RBTx_FLASH.ld
+endif
+
+GLOBAL_INCLUDES += . \
+                   config/ \
+                   drivers/
+
+#GLOBAL_CFLAGS += -DSTM32F103xB -DCENTRALIZE_MAPPING -DHAL_SRAM_MODULE_ENABLED
+GLOBAL_CFLAGS += -DSTM32F103xB
+GLOBAL_ASMFLAGS += -DSTM32F103xB
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_stm32f103rb-nucleo
+CONFIG_SYSINFO_DEVICE_NAME   := NUCLEO-F103RB
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
+GLOBAL_CFLAGS += -DSYSINFO_OS_VERSION=\"$(CONFIG_SYSINFO_OS_VERSION)\"
+GLOBAL_CFLAGS += -DSYSINFO_ARCH=\"ARM_Cortex-M3\"
+GLOBAL_CFLAGS += -DSYSINFO_MCU=\"$(HOST_MCU_NAME)\"
+
+# Keil project support
+$(NAME)_KEIL_VENDOR = STMicroelectronics
+$(NAME)_KEIL_DEVICE = STM32F103RB
+
+# Iar project support: OGChipSelectEditMenu
+$(NAME)_IAR_OGCMENU = STM32F103RB ST STM32F103RB
---
+++ platform/board/stm32f429zi-nucleo/aos.mk
@@ -0,0 +1,81 @@
+NAME := board_stm32f429zi-nucleo
+
+$(NAME)_MBINS_TYPE := kernel
+$(NAME)_VERSION    := 1.0.2
+$(NAME)_SUMMARY    := configuration for board stm32f429zi-nucleo
+MODULE             := 1062
+HOST_ARCH          := Cortex-M4
+HOST_MCU_FAMILY    := mcu_stm32f4xx_cube
+SUPPORT_MBINS       := no
+HOST_MCU_NAME      := STM32F429ZIT6
+ENABLE_VFP         := 1
+
+GLOBAL_DEFINES += HAL_VERSION=30100
+
+$(NAME)_COMPONENTS += $(HOST_MCU_FAMILY) kernel_init netmgr network
+
+
+$(NAME)_SOURCES += config/partition_conf.c \
+                   config/k_config.c \
+                   startup/board.c \
+                   startup/startup.c
+
+$(NAME)_SOURCES += drivers/Src/stm32f4xx_hal_msp.c \
+                   drivers/Src/can.c               \
+                   drivers/Src/timer.c             \
+                   drivers/Src/i2c.c               \
+                   drivers/Src/eth.c               \
+                   drivers/Src/gpio.c              \
+                   drivers/Src/pwm.c               \
+                   drivers/Src/usb_otg.c           \
+                   drivers/Src/dma.c               \
+                   drivers/Src/adc.c               \
+                   drivers/Src/dac.c               \
+                   drivers/Src/spi.c
+
+$(NAME)_SOURCES += drivers/drv/board_drv_led.c
+ywss_support    ?= 0
+
+GLOBAL_DEFINES += KV_CONFIG_TOTAL_SIZE=32768 #32kb
+GLOBAL_DEFINES += KV_CONFIG_BLOCK_SIZE_BITS=14 #(1 << 14) = 16kb
+
+ifneq (y,$(strip $(BSP_SUPPORT_EXTERNAL_MODULE)))
+$(NAME)_SOURCES    += drivers/ethernetif.c
+$(NAME)_SOURCES    += drivers/httpserver-netconn.c
+endif
+
+ifeq ($(COMPILER), armcc)
+$(NAME)_SOURCES    += startup/startup_stm32f429xx_keil.s
+$(NAME)_LINK_FILES := startup/startup_stm32f429xx_keil.o
+else ifeq ($(COMPILER), iar)
+$(NAME)_SOURCES    += startup/startup_stm32f429xx_iar.s
+else
+$(NAME)_SOURCES    += startup/startup_stm32f429xx.s
+endif
+
+GLOBAL_INCLUDES += config \
+                   drivers/Inc \
+                   drivers
+
+GLOBAL_CFLAGS  += -DSTM32F429xx -DCENTRALIZE_MAPPING
+
+ifeq ($(COMPILER),armcc)
+GLOBAL_LDFLAGS += -L --scatter=platform/board/stm32f429zi-nucleo/STM32F429ZITx.sct
+else ifeq ($(COMPILER),iar)
+GLOBAL_LDFLAGS += --config platform/board/stm32f429zi-nucleo/STM32F429.icf
+else
+GLOBAL_LDFLAGS += -T platform/board/stm32f429zi-nucleo/STM32F429ZITx_FLASH.ld
+endif
+
+CONFIG_SYSINFO_PRODUCT_MODEL := ALI_AOS_f429-nucleo
+CONFIG_SYSINFO_DEVICE_NAME   := f429-nucleo
+
+GLOBAL_CFLAGS += -DSYSINFO_PRODUCT_MODEL=\"$(CONFIG_SYSINFO_PRODUCT_MODEL)\"
+GLOBAL_CFLAGS += -DSYSINFO_DEVICE_NAME=\"$(CONFIG_SYSINFO_DEVICE_NAME)\"
+
+# Keil project support
+$(NAME)_KEIL_VENDOR = STMicroelectronics
+$(NAME)_KEIL_DEVICE = STM32F429ZITx
+
+# Iar project support: OGChipSelectEditMenu
+$(NAME)_IAR_OGCMENU = STM32F429ZI   ST STM32F429ZI
