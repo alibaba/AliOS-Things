507abbcb5f7e7ca1e1f20be8ab1ed73fcabdbe93
zhangtian <dongren.zt@alibaba-inc.com>
2020-03-02 13:16:41 +0800

    BugID:24882677:[umesh2]improve example umesh2app
    Change-Id: Ic80cd71c9000e3436cad9980162c944eb7a86bce

--- application/example/umesh2app/aos.mk
+++ application/example/umesh2app/aos.mk
@@ -5,14 +5,9 @@ $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := uMesh2 sample application
 
-$(NAME)_SOURCES     := main.c
+$(NAME)_SOURCES     :=aos_start.c mesh_example.c
 
 $(NAME)_COMPONENTS  += umesh2 cli netmgr ulog lwip
 GLOBAL_DEFINES      += TAPIF_DEFAULT_OFF DEBUG 
 
 $(NAME)_INCLUDES += ./
-
-ifneq ($(HOST_MCU_FAMILY),mcu_esp8266)
-else
-GLOBAL_DEFINES += ESP8266_CHIPSET 
-endif
