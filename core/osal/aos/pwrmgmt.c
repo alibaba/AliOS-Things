c5af0ac256f7c6f98887c4c1ca47c34319449724
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2020-03-02 17:35:01 +0800

    BugID:22740795:Optimized low power interface return value processing
    Change-Id: I17a324f7c5e318a90cfe7c75c218d2c9903c8913

--- core/osal/aos/pwrmgmt.c
+++ core/osal/aos/pwrmgmt.c
@@ -8,47 +8,61 @@
 
 int32_t aos_pwrmgmt_lowpower_suspend(uint32_t suspend_module)
 {
-    pwrmgmt_lowpower_suspend(suspend_module);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_lowpower_suspend(suspend_module);
+    return ret;
 }
 
 int32_t aos_pwrmgmt_lowpower_resume(uint32_t resume_module)
 {
-    pwrmgmt_lowpower_resume(resume_module);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_lowpower_resume(resume_module);
+    return ret;
 }
 
 #if (PWRMGMT_CONFIG_CPU_LOWPOWER > 0)
 int32_t aos_pwrmgmt_cpu_lowpower_suspend(uint32_t suspend_module)
 {
-    pwrmgmt_cpu_lowpower_suspend(suspend_module);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_cpu_lowpower_suspend(suspend_module);
+    return ret;
 }
 
 int32_t aos_pwrmgmt_cpu_lowpower_resume(uint32_t resume_module)
 {
-    pwrmgmt_cpu_lowpower_resume(resume_module);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_cpu_lowpower_resume(resume_module);
+    return ret;
 }
 #endif /* PWRMGMT_CONFIG_CPU_LOWPOWER > 0 */
 
 #if (WIFI_CONFIG_SUPPORT_LOWPOWER > 0)
 int32_t aos_pwrmgmt_wifi_powersave_enable(void)
 {
-    pwrmgmt_wifi_powersave_enable();
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_wifi_powersave_enable();
+    return ret;
 }
 
 int32_t aos_pwrmgmt_wifi_powersave_resume(uint32_t resume_module)
 {
-    pwrmgmt_wifi_powersave_resume(resume_module);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_wifi_powersave_resume(resume_module);
+    return ret;
 }
 
 int32_t aos_pwrmgmt_wifi_powersave_suspend(uint32_t suspend_module)
 {
-    pwrmgmt_wifi_powersave_suspend(suspend_module);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_wifi_powersave_suspend(suspend_module);
+    return ret;
 }
 #endif /* WIFI_CONFIG_SUPPORT_LOWPOWER > 0 */
 
@@ -56,8 +70,10 @@ int32_t aos_pwrmgmt_wifi_powersave_suspend(uint32_t suspend_module)
 #if (PWRMGMT_CONFIG_MINISLEEP > 0)
 int32_t aos_pwrmgmt_cpu_minisleep_set(uint32_t time_ms)
 {
-    pwrmgmt_cpu_minisleep_msec_set(time_ms);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_cpu_minisleep_msec_set(time_ms);
+    return ret;
 }
 
 uint32_t aos_pwrmgmt_cpu_minisleep_get(void)
@@ -69,8 +85,10 @@ uint32_t aos_pwrmgmt_cpu_minisleep_get(void)
 #if (PWRMGMT_CONFIG_CPU_ACTIVE > 0)
 int32_t aos_pwrmgmt_cpu_active_set(uint32_t active_time)
 {
-    pwrmgmt_cpu_active_msec_set(active_time);
-    return 0;
+    int32_t ret;
+
+    ret = pwrmgmt_cpu_active_msec_set(active_time);
+    return ret;
 }
 #endif /* PWRMGMT_CONFIG_CPU_ACTIVE > 0 */
 
