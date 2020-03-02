3d1b62c44646511c1edd94b5c02ccca55e347e8c
zhuqing.zq <zhuqing.zq@alibaba-inc.com>
2020-03-02 11:39:21 +0800

    BugID:24656372:add asr5501 mcu hal spi and esp8266 pwm hal and spi hal;board
    Change-Id: I7ade5bcf5898f4a77944a95b2758ecce20ac27e8

--- platform/board/board_legacy/esp8285/startup/board.c
+++ platform/board/board_legacy/esp8285/startup/board.c
@@ -6,3 +6,10 @@
 
 #include "k_config.h"
 #include "board.h"
+#include "hal_spi.h"
+#include "spi_interface.h"
+
+SPI_MAPPING SPI_MAPPING_TABLE[PORT_SPI_SIZE] = {
+{PORT_SPI_1, SpiNum_SPI, SpiSubMode_0, SpiBitOrder_MSBFirst},
+{PORT_SPI_2, SpiNum_HSPI, SpiSubMode_0, SpiBitOrder_MSBFirst},
+};
