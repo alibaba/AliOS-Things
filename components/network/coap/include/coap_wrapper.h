0460815bb37be2e1ed885b953b1b8875a9d9c496
jianzhou <jianzhou.cjz@alibaba-inc.com>
2020-03-03 16:00:12 +0800

    BugID:23589222: reduce dependency on sdk infra
    Change-Id: Ib71d93c3e302b187d74dbe2548f21808316b9339

--- components/network/coap/include/coap_wrapper.h
+++ components/network/coap/include/coap_wrapper.h
@@ -9,35 +9,32 @@
 extern "C" {
 #endif
 
-#ifdef INFRA_AES
-#include "linkkit/infra/infra_aes.h"
-p_Aes128_t coap_wrapper_aes128_init(const uint8_t *key, const uint8_t *iv, AES_DIR_t dir);
-int coap_wrapper_aes128_destroy(p_Aes128_t aes);
-int coap_wrapper_aes128_cbc_decrypt(p_Aes128_t aes, const void *src, size_t blockNum, void *dst);
-int coap_wrapper_aes128_cfb_decrypt(p_Aes128_t aes, const void *src, size_t length, void *dst);
-int coap_wrapper_aes128_cfb_encrypt(p_Aes128_t aes, const void *src, size_t length, void *dst);
-int coap_wrapper_aes128_cbc_encrypt(p_Aes128_t aes, const void *src, size_t blockNum, void *dst);
-#endif /* INFRA_AES */
-
-#ifdef INFRA_CJSON
-#include "linkkit/infra/infra_cjson.h"
-int coap_wrapper_cjson_parse(const char *src, int src_len, lite_cjson_t *lite);
-int coap_wrapper_cjson_object_item(lite_cjson_t *lite, const char *key, int key_len, lite_cjson_t *lite_item);
-#endif /* INFRA_CJSON */
-
-#ifdef INFRA_MD5
-void coap_wrapper_hmac_md5(const char *msg, int msg_len, char *digest, const char *key, int key_len);
+#ifndef bool
+#define bool unsigned char
 #endif
 
-#ifdef COAP_WITH_ALI_AUTH
-#ifdef INFRA_JSON_PARSER
-char *coap_wrapper_json_get_value(char *p_cJsonStr, int iStrLen, char *p_cName, int *p_iValueLen, int *p_iValueType);
-char *coap_wrapper_json_value_of(char *key, char *src, int magic, char* module_name);
-#endif /* INFRA_JSON_PARSER */
+#ifndef true
+#define true 1
+#endif
 
-#if defined(INFRA_LOG) && !defined(INFRA_LOG_ALL_MUTED) 
-void coap_wrapper_setloglevel(int level);
-#endif /* defined(INFRA_LOG) && !defined(INFRA_LOG_ALL_MUTED) */
+#ifndef false
+#define false 0
+#endif
+
+void * coap_wrapper_cjson_parse(const char *src);
+void * coap_wrapper_cjson_object_item(void *root, const char *key);
+char * coap_wrapper_cjson_value_string(void *node);
+int coap_wrapper_cjson_value_int(void *node);
+void coap_wrapper_cjson_release(void *root);
+
+void coap_wrapper_hmac_md5(const char *msg, int msg_len, char *digest, const char *key, int key_len);
+
+void * coap_wrapper_aes128_init(const unsigned char *key, const unsigned char *iv, bool  is_encrypt);
+int coap_wrapper_aes128_destroy(void *aes);
+int coap_wrapper_aes128_cbc_decrypt(void *aes, const void *src, unsigned int blockNum,void *dst);
+int coap_wrapper_aes128_cbc_encrypt(void *aes, const void *src, unsigned int blockNum, void *dst);
+
+void coap_wrapper_sha256(const unsigned char *input, unsigned int ilen, unsigned char output[32]);
 
 #ifdef INFRA_REPORT
 #include "linkkit/infra/infra_report.h"
@@ -46,11 +43,6 @@ int coap_wrapper_report_devinfo(void *pclient);
 int coap_wrapper_report_firmware_version(void *pclient);
 int coap_wrapper_report_mid(void *pclient);
 #endif /* INFRA_REPORT */
-
-#ifdef INFRA_SHA256
-void coap_wrapper_sha256(const uint8_t *input, uint32_t ilen, uint8_t output[32]);
-#endif /* INFRA_SHA256 */
-#endif /* COAP_WITH_ALI_AUTH */
 
 #if defined(__cplusplus)
 }
