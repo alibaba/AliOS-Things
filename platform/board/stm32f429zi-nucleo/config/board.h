165af81c64236b0ae03eeef1c636fb95f8bf37c4
alixiyuan <wencheng.ywc@alibaba-inc.com>
2020-03-04 14:32:40 +0800

    BugID:24843276:debug stm32f4xx hal spi on stm32f429zi-nucleo
    Change-Id: I20426f7d8c62873cc60c6c336188f19e7b1c9654

--- platform/board/stm32f429zi-nucleo/config/board.h
+++ platform/board/stm32f429zi-nucleo/config/board.h
@@ -15,7 +15,7 @@
 #define SOC_PACKAGE_PIN_MAX      144
 #define TOTAL_GPIO_NUM            7
 
-typedef enum{
+typedef enum {
     PORT_UART_STD,
     PORT_UART_AT,
     PORT_UART_RS485,
@@ -26,13 +26,6 @@ typedef enum{
     PORT_UART_DEMO = PORT_UART_AT,
 } PORT_UART_TYPE;
 
-typedef enum{
-    PORT_SPI_1,
-    PORT_SPI_2,
-    PORT_SPI_SIZE,
-    PORT_SPI_INVALID =255,
-    PORT_SPI_DEMO = PORT_SPI_2,
-} PORT_SPI_TYPE;
 
 typedef enum {
     PORT_ADC_1,
@@ -49,14 +42,14 @@ typedef enum {
     PORT_DAC_DEMO = PORT_DAC_1,
 } PORT_DAC_TYPE;
 
-typedef enum{
+typedef enum {
     PORT_CAN_NORMAL,
     PORT_CAN_CANOPEN,
     PORT_CAN_SIZE,
     PORT_CAN_INVALID = 255,
 } PORT_CAN_TYPE;
 
-typedef enum{
+typedef enum {
     PORT_TIMER_CANOPEN = 0,
     PORT_TIMER_0 = 0,
     PORT_TIMER_1 = 1,
@@ -69,7 +62,7 @@ typedef enum{
     PORT_TIMER_DEMO = PORT_TIMER_3,
 } PORT_TIMER_TYPE;
 
-typedef enum{
+typedef enum {
     PORT_PWM_1,
     PORT_PWM_2,
     PORT_PWM_3,
@@ -79,18 +72,26 @@ typedef enum{
     PORT_PWM_DEMO = PORT_PWM_3,
 } PORT_PWM_TYPE;
 
-typedef enum{
+typedef enum {
     PORT_I2C_1 = 0,
     PORT_I2C_2,
     PORT_I2C_SIZE,
     PORT_I2C_INVALID = 255,
 } PORT_I2C_TYPE;
 
-typedef enum{
+typedef enum {
+    PORT_SPI_1,
+    PORT_SPI_2,
+    PORT_SPI_SIZE,
+    PORT_SPI_INVALID =255,
+    PORT_SPI_DEMO = PORT_SPI_1,
+} PORT_SPI_TYPE;
+
+typedef enum {
     PORT_WDG_0 = 0,
     PORT_WDG_SIZE,
     PORT_WDG_INVALID = 255,
-}PORT_WDG_TYPE;
+} PORT_WDG_TYPE;
 
 typedef enum {
     PORT_RTC_0 = 0,
@@ -103,7 +104,7 @@ typedef enum {
 */
 #define AOS_HAL_GPIO_ENABLED        1
 #define AOS_HAL_UART_ENABLED        1
-#define AOS_HAL_SPI_ENABLED         0
+#define AOS_HAL_SPI_ENABLED         1
 #define AOS_HAL_I2C_ENABLED         1
 #define AOS_HAL_FLASH_ENABLED       0
 #define AOS_HAL_TIMER_ENABLED       1
@@ -135,58 +136,58 @@ typedef enum {
 /* Button */
 #define KEY1     HAL_GPIO_45 // USER <--> PC13
 
-#define USER_Btn_Pin GPIO_PIN_13
-#define USER_Btn_GPIO_Port GPIOC
+#define USER_Btn_Pin                GPIO_PIN_13
+#define USER_Btn_GPIO_Port          GPIOC
 
 /* NIC */
-#define MCO_Pin GPIO_PIN_0
-#define MCO_GPIO_Port GPIOH
-#define RMII_MDC_Pin GPIO_PIN_1
-#define RMII_MDC_GPIO_Port GPIOC
-#define RMII_REF_CLK_Pin GPIO_PIN_1
-#define RMII_REF_CLK_GPIO_Port GPIOA
-#define RMII_MDIO_Pin GPIO_PIN_2
-#define RMII_MDIO_GPIO_Port GPIOA
-#define RMII_CRS_DV_Pin GPIO_PIN_7
-#define RMII_CRS_DV_GPIO_Port GPIOA
-#define RMII_RXD0_Pin GPIO_PIN_4
-#define RMII_RXD0_GPIO_Port GPIOC
-#define RMII_RXD1_Pin GPIO_PIN_5
-#define RMII_RXD1_GPIO_Port GPIOC
-#define RMII_TXD1_Pin GPIO_PIN_13
-#define RMII_TXD1_GPIO_Port GPIOB
-#define RMII_TX_EN_Pin GPIO_PIN_11
-#define RMII_TX_EN_GPIO_Port GPIOG
-#define RMII_TXD0_Pin GPIO_PIN_13
-#define RMII_TXD0_GPIO_Port GPIOG
+#define MCO_Pin                     GPIO_PIN_0
+#define MCO_GPIO_Port               GPIOH
+#define RMII_MDC_Pin                GPIO_PIN_1
+#define RMII_MDC_GPIO_Port          GPIOC
+#define RMII_REF_CLK_Pin            GPIO_PIN_1
+#define RMII_REF_CLK_GPIO_Port      GPIOA
+#define RMII_MDIO_Pin               GPIO_PIN_2
+#define RMII_MDIO_GPIO_Port         GPIOA
+#define RMII_CRS_DV_Pin             GPIO_PIN_7
+#define RMII_CRS_DV_GPIO_Port       GPIOA
+#define RMII_RXD0_Pin               GPIO_PIN_4
+#define RMII_RXD0_GPIO_Port         GPIOC
+#define RMII_RXD1_Pin               GPIO_PIN_5
+#define RMII_RXD1_GPIO_Port         GPIOC
+#define RMII_TXD1_Pin               GPIO_PIN_13
+#define RMII_TXD1_GPIO_Port         GPIOB
+#define RMII_TX_EN_Pin              GPIO_PIN_11
+#define RMII_TX_EN_GPIO_Port        GPIOG
+#define RMII_TXD0_Pin               GPIO_PIN_13
+#define RMII_TXD0_GPIO_Port         GPIOG
 
 /* STLINK */
-#define STLK_RX_Pin GPIO_PIN_8
-#define STLK_RX_GPIO_Port GPIOD
-#define STLK_TX_Pin GPIO_PIN_9
-#define STLK_TX_GPIO_Port GPIOD
+#define STLK_RX_Pin                 GPIO_PIN_8
+#define STLK_RX_GPIO_Port           GPIOD
+#define STLK_TX_Pin                 GPIO_PIN_9
+#define STLK_TX_GPIO_Port           GPIOD
 
 /* USB */
-#define USB_PowerSwitchOn_Pin GPIO_PIN_6
+#define USB_PowerSwitchOn_Pin       GPIO_PIN_6
 #define USB_PowerSwitchOn_GPIO_Port GPIOG
-#define USB_OverCurrent_Pin GPIO_PIN_7
-#define USB_OverCurrent_GPIO_Port GPIOG
-#define USB_SOF_Pin GPIO_PIN_8
-#define USB_SOF_GPIO_Port GPIOA
-#define USB_VBUS_Pin GPIO_PIN_9
-#define USB_VBUS_GPIO_Port GPIOA
-#define USB_ID_Pin GPIO_PIN_10
-#define USB_ID_GPIO_Port GPIOA
-#define USB_DM_Pin GPIO_PIN_11
-#define USB_DM_GPIO_Port GPIOA
-#define USB_DP_Pin GPIO_PIN_12
-#define USB_DP_GPIO_Port GPIOA
+#define USB_OverCurrent_Pin         GPIO_PIN_7
+#define USB_OverCurrent_GPIO_Port   GPIOG
+#define USB_SOF_Pin                 GPIO_PIN_8
+#define USB_SOF_GPIO_Port           GPIOA
+#define USB_VBUS_Pin                GPIO_PIN_9
+#define USB_VBUS_GPIO_Port          GPIOA
+#define USB_ID_Pin                  GPIO_PIN_10
+#define USB_ID_GPIO_Port            GPIOA
+#define USB_DM_Pin                  GPIO_PIN_11
+#define USB_DM_GPIO_Port            GPIOA
+#define USB_DP_Pin                  GPIO_PIN_12
+#define USB_DP_GPIO_Port            GPIOA
 
 /* DEBUG */
-#define TMS_Pin GPIO_PIN_13
-#define TMS_GPIO_Port GPIOA
-#define TCK_Pin GPIO_PIN_14
-#define TCK_GPIO_Port GPIOA
+#define TMS_Pin                     GPIO_PIN_13
+#define TMS_GPIO_Port               GPIOA
+#define TCK_Pin                     GPIO_PIN_14
+#define TCK_GPIO_Port               GPIOA
 
 #if (AOS_HAL_GPIO_ENABLED > 0)
 
