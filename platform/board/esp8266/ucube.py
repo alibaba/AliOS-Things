5285b202a7d820028f12244899ab7b095da6b6b8
wencheng.ywc <wencheng.ywc@alibaba-inc.com>
2020-03-04 12:08:21 +0800

    BugID:23823143: Added vfs_demo into stm32f103rb-nucleo and esp8266 pre-build list
    Change-Id: Ibe2a19b186a611b8563ff597e4bbd67727eb990e

--- platform/board/esp8266/ucube.py
+++ platform/board/esp8266/ucube.py
@@ -5,4 +5,4 @@ src     = Split('''
 component = aos_board_component('board_esp8266-demo', 'esp8266', src)
 
 platform_options="wifi=1"
-linux_only_targets="blink_demo hal_demo helloworld_demo kernel_demo.krhino_api_demo kernel_demo.aos_api_demo linkkit_demo linkkit_gateway_demo mqtt_demo ota_demo yts_demo"
+linux_only_targets="blink_demo hal_demo helloworld_demo kernel_demo.krhino_api_demo kernel_demo.aos_api_demo linkkit_demo linkkit_gateway_demo mqtt_demo ota_demo vfs_demo yts_demo"
