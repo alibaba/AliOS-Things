8479f990eecf4b8081f235ef9864329c93409722
zhangtian <dongren.zt@alibaba-inc.com>
2020-03-04 23:02:43 +0800

    BugID:24882677:[umesh2]:add some api to improve configurability
    Change-Id: Ibbd2bee63021a4b4b9f9f3a5b690f61a4312786d

--- include/network/umesh2/core/umesh.h
+++ include/network/umesh2/core/umesh.h
@@ -1,9 +1,13 @@
-#ifndef UMESH_H_
-#define UMESH_H_
+#ifndef __UMESH_H__
+#define __UMESH_H__
+#include <string.h>
+#include <stdint.h>
 
+typedef int (*umesh_ap_info_cb)(const char *ssid, const  char *pwd, const uint8_t *bssid, void *context);
 
 void *umesh_network_init();
-int umesh_recv_ip_data(struct umesh_state *state, const uint8_t *data, uint16_t data_len, uint8_t *dst_addr);
-void umesh_network_deinit(void *handle);
-
-#endif /* UMESH_SCHEDULE_H_ */
+int umesh_is_connected(void *hanlde);
+int umesh_network_deinit(void *handle);
+int umesh_zero_config_request(void *handle);
+int umesh_zero_config_set_cb(void *hanlde, umesh_ap_info_cb cb, void *context);
+#endif /* __UMESH_H__ */
