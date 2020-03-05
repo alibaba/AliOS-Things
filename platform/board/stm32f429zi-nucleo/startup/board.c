01c52b2478eb968ef2aff47a13ca51fca9eec1c9
yajun.syj <yajun.syj@alibaba-inc.com>
2020-03-05 14:28:29 +0800

    BugID:25059354: Add AOS_NET_WITH_ETH to support ethernet
    Change-Id: I423c9556bf05397aa6e8990216dd8efb7769dc3c

--- platform/board/stm32f429zi-nucleo/startup/board.c
+++ platform/board/stm32f429zi-nucleo/startup/board.c
@@ -432,9 +432,9 @@ static void I2C1_init()
 void board_network_init(void)
 {
 #ifndef WITH_SAL
-        /*enable ethernet*/
-        //MX_ETH_Init();
-        //lwip_tcpip_init();
+    /*enable ethernet*/
+    MX_ETH_Init();
+    lwip_tcpip_init();
 #endif
 
     hw_start_hal();
