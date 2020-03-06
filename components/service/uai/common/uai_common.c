4dd1d59de4271604168ef144f09fc97256a83121
william.wgj <william.wgj@alibaba-inc.com>
2020-03-06 16:49:43 +0800

    BugID:23252751: uAI S2 function, support ODLA
    Change-Id: I1b29b5252bd9e59462427e6939009096eabfb6fb

---
+++ components/service/uai/common/uai_common.c
@@ -0,0 +1,9 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ */
+#include "uai_common.h"
+
+
+#ifdef UAI_ODLA_SUPPORT
+
+#endif
---
+++ components/service/uai/odla/include/uai_odla_adapt.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ *
+ */
+
+#ifndef _UAI_ODLA_C_H_
+#define _UAI_ODLA_C_H_
+
+#include "uai_function.h"
+#include "uai_load.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef int8_t _odla_Value;
+typedef void (*odla_helper_func)(void *comp);
+
+#ifndef UAI_ODLA_SUPPORT_FREE_MEM
+typedef struct uai_mem_list{
+    struct uai_mem_list *next;
+    struct uai_mem_list *tail;
+    void *mem_ptr;
+}uai_mem_list_t;
+#endif
+
+struct _odla_Computation {
+    int32_t layer_id;
+    uai_tensor_s *input;
+    uai_tensor_s *output;
+#ifdef UAI_PRELOAD_SCALE_ONCE
+    uai_quant_scale **quant_scale;
+#endif
+    odla_helper_func helper_func;
+#ifndef UAI_ODLA_SUPPORT_FREE_MEM
+    uai_mem_list_t *mem_list;
+#endif
+};
+
+struct _odla_Session {
+    struct _odla_Computation *comp;
+};
+
+uai_mem_list_t *uai_odla_init_memlist();
+int uai_odla_add_memlist(uai_mem_list_t *memlist, void *ptr);
+void uai_odla_free_memlist(uai_mem_list_t *memlist);
+
+#ifdef __cplusplus
+} /* C extern */
+#endif
+
+
+#endif
