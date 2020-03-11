c0a7e8f3f0299c63e82b0baf4a31ba9b544e1ba0
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-03-11 14:17:34 +0800

    BugID:25059227: update copyright info
    Change-Id: Ie3be779c8fd262effca5324246935f796d70729a

--- components/network/httpdns/include/iplist.h
+++ components/network/httpdns/include/iplist.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ */
+
 #ifndef IPV4_LIST_H
 #define IPV4_LIST_H
 
--- components/network/httpdns/include/list.h
+++ components/network/httpdns/include/list.h
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2015-2020 Alibaba Group Holding Limited
+ */
+
 #ifndef _LINUX_LIST_H
 #define _LINUX_LIST_H
 
