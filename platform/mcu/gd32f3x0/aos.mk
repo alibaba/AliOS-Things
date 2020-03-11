4a9f7de77fc4eb1ba9d91a3890cba8ea82ae817e
magao.xwj <magao.xwj@alibaba-inc.com>
2020-03-11 17:08:39 +0800

    BugID:22083675:update component version for 3.1.0 release
    Change-Id: I44eb7c912fc7965775bba9f9df2539361f1c03c9

--- platform/mcu/gd32f3x0/aos.mk
+++ platform/mcu/gd32f3x0/aos.mk
@@ -2,7 +2,7 @@ NAME := mcu_gd32f3x0
 HOST_OPENOCD := gd32f3x0
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.0
+$(NAME)_VERSION    := 1.0.1
 $(NAME)_SUMMARY    := driver & sdk for platform/mcu gd32f3x0
 
 $(NAME)_COMPONENTS += arch_armv7m rhino
