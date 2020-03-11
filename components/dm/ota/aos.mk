885078b32dd9c4596319b820d2855a4bc09aaf7f
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-03-11 11:33:39 +0800

    BugID:25714458:add new board of Infineon XMC4800-F144x2048 gcc keil pro; dm
    Change-Id: I1dabba4716a535bad667780fedbc234b42cb90d5

--- components/dm/ota/aos.mk
+++ components/dm/ota/aos.mk
@@ -30,7 +30,7 @@ else
 ifneq (,$(filter mcu_esp32,$(HOST_MCU_FAMILY)))
 $(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent_esp32.a #esp32
 else
-ifneq (,$(filter mcu_stm32l4xx_cube mcu_stm32f4xx_cube mcu_mtk7682 mcu_m487jidae mcu_msp432p4xx, $(HOST_MCU_FAMILY)))
+ifneq (,$(filter mcu_stm32l4xx_cube mcu_stm32f4xx_cube mcu_mtk7682 mcu_m487jidae mcu_msp432p4xx mcu_xmc, $(HOST_MCU_FAMILY)))
 $(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent_softabi.a #mt7682s
 else
 $(NAME)_PREBUILT_LIBRARY := lib/gcc/$(HOST_ARCH)/ota_agent.a #mk3080
