7e39aa5e45be0dfebd6a3f0c8d1875ca1f7432d4
magao.xwj <magao.xwj@alibaba-inc.com>
2020-03-11 17:08:39 +0800

    BugID:22083675:update component version for 3.1.0 release
    Change-Id: I44eb7c912fc7965775bba9f9df2539361f1c03c9

--- platform/mcu/es8p508x/aos.mk
+++ platform/mcu/es8p508x/aos.mk
@@ -2,7 +2,7 @@ NAME := mcu_es8p508x
 HOST_OPENOCD := es8p508x
 
 $(NAME)_MBINS_TYPE := kernel
-$(NAME)_VERSION    := 1.0.0
+$(NAME)_VERSION    := 1.0.1
 $(NAME)_SUMMARY    := driver & sdk for platform/mcu es8p508x
 
 $(NAME)_COMPONENTS += arch_armv6m rhino osal_aos
