48c35fec5e48d1795b626a3c697ab4776eab7198
jianniu.ljg <jianniu.ljg@alibaba-inc.com>
2020-03-11 15:54:41 +0800

    BugID:24815495:Modified app's Config.in to suit to new compilation framework
    Change-Id: I685ee64df1d9efcfc4669f327a8de23f77a473f4

--- application/example/debug_demo/aos.mk
+++ application/example/debug_demo/aos.mk
@@ -1,8 +1,8 @@
-NAME := debug_app
+NAME := debug_demo
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := demo for debug
-$(NAME)_SOURCES := debug_app.c
+$(NAME)_SOURCES := debug_app.c maintask.c
 
 $(NAME)_COMPONENTS  += debug osal_aos cli kv
