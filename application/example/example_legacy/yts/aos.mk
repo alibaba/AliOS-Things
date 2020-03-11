98981b143d93798bdf2e6b2cc9265085916d03fe
wencheng.ywc <wencheng.ywc@alibaba-inc.com>
2020-03-11 17:42:58 +0800

    BugID:24815495:Modified app config.in  to suit to new build framework
    Change-Id: Ia6dc59658926c4684de0c0ac0e56bc977aa81345

--- application/example/example_legacy/yts/aos.mk
+++ application/example/example_legacy/yts/aos.mk
@@ -4,18 +4,16 @@ $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := AliOS Things test suite
 $(NAME)_SOURCES := main.c
+
 $(NAME)_COMPONENTS := testcase ulog vfs yloop
+$(NAME)_COMPONENTS-$(AOS_BOARD_LINUXHOST||AOS_BOARD_ARMHFLINUX) += lwip netmgr fatfs
+$(NAME)_COMPONENTS-$(!AOS_BOARD_LINUXHOST&&!AOS_BOARD_ARMHFLINUX) += cli
 
 $(NAME)_CFLAGS += -Wall -Werror -Wno-unused-variable
 
 ifneq (,$(findstring linux, $(BUILD_STRING)))
-$(NAME)_COMPONENTS-$(AOS_BOARD_LINUXHOST) += lwip netmgr fatfs
-$(NAME)_COMPONENTS-$(AOS_BOARD_ARMHFLINUX) += lwip netmgr fatfs
-
 GLOBAL_LDFLAGS += -lreadline -lncurses -lrt
 GLOBAL_DEFINES += CONFIG_AOS_MESHYTS DEBUG YTS_LINUX
-else
-$(NAME)_COMPONENTS += cli
 endif
 
 $(NAME)_INCLUDES += ./
