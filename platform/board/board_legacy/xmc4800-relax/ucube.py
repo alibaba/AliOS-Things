090f1bc47b1f68d3d9db08c67f6bd3dd530a0455
Li Cheng <lc122798@alibaba-inc.com>
2020-03-12 11:23:12 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Iea99e053481c01911d295579a47e1df4fe0497d1

--- platform/board/board_legacy/xmc4800-relax/ucube.py
+++ platform/board/board_legacy/xmc4800-relax/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="helloworld linkkitapp"
+linux_only_targets="at_app coapapp helloworld http2app httpapp httpclient_app jsengine_app linkkit_gateway linkkitapp lwm2m_app modbus_app mqttapp otaapp sal_app udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp ulocation.qianxunapp ulog_app websoc_app wifi_at_app yloop_app yts"
