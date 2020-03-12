b6f5c1b91d6345d2788522e25f6064f3d490366f
Li Cheng <lc122798@alibaba-inc.com>
2020-03-12 11:23:12 +0800

    bugid:16575206:[BuildCoverage] auto generated build targets
    Change-Id: Iea99e053481c01911d295579a47e1df4fe0497d1

--- platform/board/board_legacy/frdmkl27z/ucube.py
+++ platform/board/board_legacy/frdmkl27z/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="helloworld modbus_app udata_demo.sensor_local_demo ulog_app"
+linux_only_targets="helloworld modbus_app udata_demo.sensor_local_demo ulog_app wifi_at_app"
--- platform/board/stm32f103rb-nucleo/ucube.py
+++ platform/board/stm32f103rb-nucleo/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="blink_demo hal_demo helloworld_demo kernel_demo.krhino_api_demo kernel_demo.aos_api_demo vfs_demo"
+linux_only_targets="blink_demo debug_demo hal_demo helloworld_demo kernel_demo.aos_api_demo kernel_demo.krhino_api_demo modbus_demo ulog_demo vfs_demo yts_demo"
