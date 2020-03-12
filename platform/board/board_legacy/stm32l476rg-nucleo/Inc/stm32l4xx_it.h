32073685ef75a6bf79ad79e394d45584ab2b65d6
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-03-12 21:50:46 +0800

    BugID:25773629: enable stm32l476rg UART3 DMA
    Change-Id: I4eeef173cec1352c1dbf66a2eed317a6b748e097
    (cherry picked from commit 56d8561d3d7c3c7e319e57df1e9da14fb5fd6f24)

--- platform/board/board_legacy/stm32l476rg-nucleo/Inc/stm32l4xx_it.h
+++ platform/board/board_legacy/stm32l476rg-nucleo/Inc/stm32l4xx_it.h
@@ -60,6 +60,7 @@ void DMA1_Channel4_IRQHandler(void);
 void DMA1_Channel5_IRQHandler(void);
 void USART1_IRQHandler(void);
 void USART2_IRQHandler(void);
+void USART3_IRQHandler(void);
 
 #ifdef __cplusplus
 }
