33faf14063d67aa6649c40de4563ba25183cb6c0
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-03-13 20:06:10 +0800

    Enable stm32f103 AT UART

--- board/stm32f103rb-nucleo/config/board.h
+++ board/stm32f103rb-nucleo/config/board.h
@@ -61,6 +61,7 @@ extern "C" {
 
 typedef enum{
     PORT_UART_STD,
+    PORT_UART_AT,
     PORT_UART_SIZE,
     PORT_UART_INVALID = 255,
 }PORT_UART_TYPE;
@@ -88,6 +89,10 @@ void Error_Handler(void);
 #define USART_RX_GPIO_Port GPIOA
 #define LD2_Pin GPIO_PIN_5
 #define LD2_GPIO_Port GPIOA
+#define USART3_TX_Pin GPIO_PIN_10
+#define USART3_TX_GPIO_Port GPIOC
+#define USART3_RX_Pin GPIO_PIN_11
+#define USART3_RX_GPIO_Port GPIOC
 #define TMS_Pin GPIO_PIN_13
 #define TMS_GPIO_Port GPIOA
 #define TCK_Pin GPIO_PIN_14
