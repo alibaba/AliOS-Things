a89bc152b877c28b4d5e7c77fe7b1be5d730499c
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-03-13 12:25:00 +0800

    BugID:25545741: update sal wifi atcmd hal
    Change-Id: I8748a4faabd2bd6266ea5a67896724e8b7af7b24
    (cherry picked from commit 016c8c98ba10f5971c9439fb49e5d0c8c76f98d1)

--- components/peripherals/module/sal/wifi/bk7231/wifi_atcmd_bk7231.c
+++ components/peripherals/module/sal/wifi/bk7231/wifi_atcmd_bk7231.c
@@ -69,7 +69,6 @@ static int wifi_init(hal_wifi_module_t *m)
 };
 
 #define MAC_STR_LEN 12
-// str: char[12], mac: hex[6]
 static void mac_str_to_hex(char *str, uint8_t *mac)
 {
     int i, k;
@@ -102,7 +101,6 @@ static void mac_str_to_hex(char *str, uint8_t *mac)
     }
 }
 
-// mac - hex[6]
 static void wifi_get_mac_addr(hal_wifi_module_t *m, uint8_t *mac)
 {
     char mac_str[MAC_STR_LEN + 1] = {0};
@@ -237,76 +235,6 @@ static int get_ip_stat(hal_wifi_module_t *m, hal_wifi_ip_stat_t *out_net_para, h
     return 0;
 }
 
-static int get_link_stat(hal_wifi_module_t *m, hal_wifi_link_stat_t *out_stat)
-{
-    return 0;
-}
-
-static void start_scan(hal_wifi_module_t *m)
-{
-
-}
-
-static void start_scan_adv(hal_wifi_module_t *m)
-{
-}
-
-static int power_off(hal_wifi_module_t *m)
-{
-    return 0;
-}
-
-static int power_on(hal_wifi_module_t *m)
-{
-    return 0;
-}
-
-static int suspend(hal_wifi_module_t *m)
-{
-    return 0;
-}
-
-static int suspend_station(hal_wifi_module_t *m)
-{
-    return 0;
-}
-
-static int suspend_soft_ap(hal_wifi_module_t *m)
-{
-
-    return 0;
-}
-
-static int set_channel(hal_wifi_module_t *m, int ch)
-{
-    return 0;
-}
-
-static void start_monitor(hal_wifi_module_t *m)
-{
-
-}
-
-static void stop_monitor(hal_wifi_module_t *m)
-{
-
-}
-
-static void register_monitor_cb(hal_wifi_module_t *m, monitor_data_cb_t fn)
-{
-
-}
-
-static void register_wlan_mgnt_monitor_cb(hal_wifi_module_t *m, monitor_data_cb_t fn)
-{
-
-}
-
-static int wlan_send_80211_raw_frame(hal_wifi_module_t *m, uint8_t *buf, int len)
-{
-    return 0;
-}
-
 hal_wifi_module_t aos_wifi_module_bk7231 = {
     .base.name           = "aos_wifi_module_bk7231",
     .init                =  wifi_init,
@@ -314,19 +242,4 @@ hal_wifi_module_t aos_wifi_module_bk7231 = {
     .start               =  wifi_start,
     .start_adv           =  wifi_start_adv,
     .get_ip_stat         =  get_ip_stat,
-    .get_link_stat       =  get_link_stat,
-    .start_scan          =  start_scan,
-    .start_scan_adv      =  start_scan_adv,
-    .power_off           =  power_off,
-    .power_on            =  power_on,
-    .suspend             =  suspend,
-    .suspend_station     =  suspend_station,
-    .suspend_soft_ap     =  suspend_soft_ap,
-    .set_channel         =  set_channel,
-    .start_monitor       =  start_monitor,
-    .stop_monitor        =  stop_monitor,
-    .register_monitor_cb =  register_monitor_cb,
-    .register_wlan_mgnt_monitor_cb = register_wlan_mgnt_monitor_cb,
-    .wlan_send_80211_raw_frame = wlan_send_80211_raw_frame
 };
-
