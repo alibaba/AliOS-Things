47b0d8d2bf1632f41abf67c868a9c32dfbb2798c
jianzhou.cjz <jianzhou.cjz@alibaba-inc.com>
2020-03-17 00:00:48 +0800

    BugID:25545741: update lwm2m api and config.in
    Change-Id: I1bb6f39eb35b953798fe001289f4a6ce3c38dc79
    (cherry picked from commit ed35281fd14da1f568c23b8797a2b56701635de0)

--- application/example/lwm2m_demo/app_entry.c
+++ application/example/lwm2m_demo/app_entry.c
@@ -8,6 +8,10 @@
 #include <netmgr.h>
 #include "ulog/ulog.h"
 
+#ifdef WITH_SAL
+#include <atcmd_config_module.h>
+#endif
+
 static int lwm2m_client_started = 0;
 
 extern int lwm2m_client(void);
@@ -32,7 +36,17 @@ static void wifi_service_event(input_event_t *event, void *priv_data)
 int application_start(int argc, char *argv[])
 {
 #ifdef WITH_SAL
-    sal_add_dev(NULL, NULL);
+    sal_device_config_t data = {0};
+
+    data.uart_dev.port = 1;
+    data.uart_dev.config.baud_rate = 115200;
+    data.uart_dev.config.data_width = DATA_WIDTH_8BIT;
+    data.uart_dev.config.parity = NO_PARITY;
+    data.uart_dev.config.stop_bits  = STOP_BITS_1;
+    data.uart_dev.config.flow_control = FLOW_CONTROL_DISABLED;
+    data.uart_dev.config.mode = MODE_TX_RX;
+
+    sal_add_dev(NULL, &data);
     sal_init();
 #endif
 
