adc8a1b180565214602cdf5dbc62ff05736184aa
zhangtian <dongren.zt@alibaba-inc.com>
2020-03-20 17:17:04 +0800

    BugID:25993956:[umesh2_demo]add imdependent zero config test
    Change-Id: Ie7aec10331dc2544d9b7c05b76a60d9445c72e11

--- application/example/umesh2_demo/aos.mk
+++ application/example/umesh2_demo/aos.mk
@@ -6,7 +6,9 @@ $(NAME)_VERSION := 1.0.0
 $(NAME)_SUMMARY := uMesh2 sample application
 
 $(NAME)_SOURCES     :=aos_start.c
-$(NAME)_SOURCES     += mesh_example.c
+$(NAME)_SOURCES-$(UMESH2_SERVICE)     += mesh_service_example.c
+$(NAME)_SOURCES-$(UMESH2_ZERO_CONFIG)     += mesh_zero_config_example.c
+
 
 $(NAME)_COMPONENTS  += umesh2 cli netmgr ulog lwip
 GLOBAL_DEFINES      += TAPIF_DEFAULT_OFF DEBUG 
