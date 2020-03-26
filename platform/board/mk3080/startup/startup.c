4fe289e33d9bd0315aec855ef413831aff008b4b
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-03-26 12:05:54 +0800

    BugID:24656372:modify app blink_demo,aaboard、aamcu boards directory; use krhino api in startup.c;board
    Change-Id: I19781ad3d66af8c17edf9d68a2cbb2b617da7090

--- platform/board/mk3080/startup/startup.c
+++ platform/board/mk3080/startup/startup.c
@@ -10,28 +10,37 @@
 #include "aos/init.h"
 #include "aos/kernel.h"
 
-/*
-main task stask size(byte)
-*/
-#ifndef AOS_MAIN_TASK_STACK_SIZE
-#define AOS_MAIN_TASK_STACK_SIZE (2048 * 4)
-#endif
-
 extern void board_basic_init();
 extern void aos_maintask(void* arg);
+
+/*
+main task stask size(cpu_stack_t)
+*/
+#ifndef AOS_MAIN_TASK_STACK_SIZE
+#define AOS_MAIN_TASK_STACK_SIZE (2048)
+#endif
+
+/*
+main task pri
+*/
+#define AOS_MAIN_TASK_PRI 32
+
+static ktask_t *g_main_task;
+
 int main(void)
 {
     /* board basic init: CLK, heap, define in board\aaboard_demo\startup\board.c */
     board_basic_init();
 
     /* kernel init, malloc can use after this! */
-    aos_init();
+    krhino_init();
 
     /* main task to run */
-    aos_task_new("main_task", aos_maintask, NULL ,AOS_MAIN_TASK_STACK_SIZE);
+    krhino_task_dyn_create(&g_main_task, "main_task", 0, AOS_MAIN_TASK_PRI, 0,
+                            AOS_MAIN_TASK_STACK_SIZE, (task_entry_t)aos_maintask, 1);
 
     /* kernel start schedule! */
-    aos_start();
+    krhino_start();
 
     /* never run here */
     return 0;
--- platform/board/stm32f103rb-nucleo/startup/startup.c
+++ platform/board/stm32f103rb-nucleo/startup/startup.c
@@ -12,11 +12,17 @@
 extern void board_basic_init(void);
 extern void aos_maintask(void* arg);
 /*
-main task stask size(byte)
+main task stask size(cpu_stack_t)
 */
 #ifndef AOS_MAIN_TASK_STACK_SIZE
-#define AOS_MAIN_TASK_STACK_SIZE (1024 * 4)
+#define AOS_MAIN_TASK_STACK_SIZE (1024)
 #endif
+/*
+main task pri
+*/
+#define AOS_MAIN_TASK_PRI 32
+
+static ktask_t *g_main_task;
 
 int main(void)
 {
@@ -24,13 +30,14 @@ int main(void)
     board_basic_init();
 
     /* kernel init, malloc can use after this! */
-    aos_init();
+    krhino_init();
 
     /* main task to run */
-    aos_task_new("main_task", aos_maintask, NULL ,AOS_MAIN_TASK_STACK_SIZE);
+    krhino_task_dyn_create(&g_main_task, "main_task", 0, AOS_MAIN_TASK_PRI, 0,
+                            AOS_MAIN_TASK_STACK_SIZE, (task_entry_t)aos_maintask, 1);
 
     /* kernel start schedule! */
-    aos_start();
+    krhino_start();
 
     /* never run here */
     return 0;
