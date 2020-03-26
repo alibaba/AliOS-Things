61f066fe6177835e0a230dd4d36fe5e937f23272
zhuqing <zhuqing.zq@alibaba-inc.com>
2020-03-26 12:05:54 +0800

    BugID:24656372:modify app blink_demo,aaboard、aamcu boards directory; use krhino api in startup.c;board
    Change-Id: I19781ad3d66af8c17edf9d68a2cbb2b617da7090

--- platform/mcu/aamcu_demo/hal/include/hal_spi.h
+++ platform/mcu/aamcu_demo/hal/include/hal_spi.h
@@ -15,6 +15,31 @@
 #include <stdbool.h>
 #include "board.h"
 
+#define HAL_SPI_GPIO_NEED_MAP 0xfe
+
+typedef enum {
+    SPI_PIN_CS,
+    SPI_PIN_CLK,
+    SPI_PIN_MOSI,
+    SPI_PIN_MISO
+} SPI_PIN_NAME_TYPE;
+
+typedef struct {
+    uint8_t  pin_name;
+    uint16_t pin;
+} gpio_spi_pin_config_t;
+
+typedef struct{
+    PORT_SPI_TYPE          spiFuncP;
+    void*                  spiPhyP;
+    spiAttribute           attr;
+    uint16_t               needmap;
+    gpio_spi_pin_config_t *pin_conf;
+    uint16_t               pin_cnt;
+}SPI_MAPPING;
+
+extern SPI_MAPPING SPI_MAPPING_TABLE[PORT_SPI_SIZE];
+
 #ifdef __cplusplus
 }
 #endif
