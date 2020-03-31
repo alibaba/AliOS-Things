3d33071de339dc3fa5d740e0e7ae808f76c19b49
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:20:51 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: Ibc0604fe7fcee44a4a0b6c698bbec26c250b69ca
    (cherry picked from commit f39e3b2addd2f680fb47d3121dc28b7ffe82550b)

--- application/example/example_legacy/jsengine_app/ucube.py
+++
@@ -1,6 +0,0 @@
-src     = Split('''
-        jsengine_app.c
-''')
-
-component = aos_component('jsengine_app', src)
-component.add_comp_deps('kernel/yloop', 'tools/cli')
--- application/example/helloworld_demo/ucube.py
+++
@@ -1,6 +0,0 @@
-src     = Split('''
-        main.c
-''')
-
-component = aos_component('appdemo', src)
-component.add_global_macros('AOS_NO_WIFI')
