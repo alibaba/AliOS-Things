e5c4a37fa3033456f05058fd5f106864f1c6d774
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:20:24 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: I5a3650a343b817eb47de231c0fbd39285d63585c
    (cherry picked from commit 182ab14884255a3c2d69bafb567140d7a23ad47b)

--- platform/arch/linux/ucube.py
+++
@@ -1,9 +0,0 @@
-src = []
-
-if aos_global_config.get('osal') != 'posix':
-    src.append('cpu_impl.c')
-
-if aos_global_config.board == 'linuxhost':
-    src.append('swap.S')
-
-aos_component('linux', src)
--- platform/board/esp8266/ucube.py
+++ platform/board/esp8266/ucube.py
@@ -1,8 +1,1 @@
-src     = Split('''
-        board.c
-''')
-
-component = aos_board_component('board_esp8266-demo', 'esp8266', src)
-
-platform_options="wifi=1"
 linux_only_targets="blink_demo yloop_demo coap_demo debug_demo hal_demo helloworld_demo http2_demo http_demo kernel_demo.aos_api_demo kernel_demo.krhino_api_demo linkkit_demo linkkit_gateway_demo mqtt_demo ota_demo ulocation.base_demo ulog_demo vfs_demo yts_demo"
