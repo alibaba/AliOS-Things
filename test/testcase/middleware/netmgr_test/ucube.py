a56dd14d01f0f2f8f1f063784697b591d93c4576
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:27:53 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: I985739078e19c1564e63d2f145803f978241e90d

--- test/testcase/middleware/netmgr_test/ucube.py
+++
@@ -1,8 +0,0 @@
-src = Split('''
-    netmgr_test.c
-''')
-
-component = aos_component('netmgr_test', src)
-
-if aos_global_config.board == 'linuxhost':
-    component.add_global_macros('CONFIG_AOS_NETMGRYTS_NOSMARTCONFIG')
