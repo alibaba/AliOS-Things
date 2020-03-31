2c91df3f756273fd473c2b71b09eb52b7541d4b4
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:20:24 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: I5a3650a343b817eb47de231c0fbd39285d63585c
    (cherry picked from commit 182ab14884255a3c2d69bafb567140d7a23ad47b)

--- platform/board/board_legacy/stm32l496g-discovery/ucube.py
+++ platform/board/board_legacy/stm32l496g-discovery/ucube.py
@@ -1,20 +1,2 @@
-src     = ['aos/board_partition.c', 'aos/soc_init.c', 'Src/stm32l4xx_hal_msp.c']
-
-component = aos_board_component('stm32l496g-discovery', 'stm32l4xx_cube', src)
-
-if aos_global_config.compiler == "armcc":
-    component.add_sources('startup_stm32l496xx_keil.s')
-    component.add_prebuilt_objs('startup_stm32l496xx_keil.o', 'Src/stm32l4xx_hal_msp.o')
-elif aos_global_config.compiler == "iar":
-    component.add_sources('startup_stm32l496xx_iar.s')
-else:
-    component.add_sources('startup_stm32l496xx.s')
-    aos_global_config.add_ld_files('STM32L496AGIx_FLASH.ld')
-
-component.add_global_includes('hal', 'aos', 'Inc')
-component.add_global_macros('STM32L496xx', 'SYSINFO_PRODUCT_MODEL=\\"ALI_AOS_496-discovery\\"', 'SYSINFO_DEVICE_NAME=\\"496-discovery\\"')
-
-if aos_global_config.get('sal', 1) == 1:
-    component.add_comp_deps('network/sal')
 linux_only_targets="at_app blink coapapp helloworld http2app httpapp httpclient_app jsengine_app linkkit_gateway linkkitapp lwm2m_app modbus_app mqttapp otaapp sal_app udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udata_demo.udata_cloud_demo udata_demo.udata_local_demo udataapp ulocation.baseapp ulog_app websoc_app wifi_at_app yloop_app yts"
 windows_only_targets="helloworld|COMPILER=armcc helloworld|COMPILER=iar"
