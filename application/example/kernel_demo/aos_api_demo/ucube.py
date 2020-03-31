657bdecfc34a57af717ffa27c6391d48440e1d6c
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:20:51 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: Ibc0604fe7fcee44a4a0b6c698bbec26c250b69ca
    (cherry picked from commit f39e3b2addd2f680fb47d3121dc28b7ffe82550b)

--- application/example/kernel_demo/aos_api_demo/ucube.py
+++
@@ -1,8 +0,0 @@
-src     = Split('''
-        kernel_app.c
-        task_app.c
-''')
-
-component = aos_component('kernel_app', src)
-component.add_comp_deps('kernel/cli')
-component.add_global_macros('AOS_NO_WIFI')
