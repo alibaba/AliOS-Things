c1535cb2f31babfc3c290b371e55d63f5b37cb67
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:20:24 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: I5a3650a343b817eb47de231c0fbd39285d63585c
    (cherry picked from commit 182ab14884255a3c2d69bafb567140d7a23ad47b)

--- platform/board/board_legacy/lpcxpresso54102/ucube.py
+++ platform/board/board_legacy/lpcxpresso54102/ucube.py
@@ -1,11 +1,1 @@
-src     = Split('''
-        board.c
-        clock_config.c
-        pin_mux.c
-''')
-
-component = aos_board_component('board_lpcxpresso54102', 'lpc54102', src)
-component.add_global_macros('CPU_LPC54102J512BD64_cm4', '__USE_CMSIS', '__MULTICORE_MASTER', '__NEWLIB__')
-aos_global_config.arch = 'Cortex-M4'
-
 linux_only_targets="at_app coapapp helloworld http2app httpapp httpclient_app linkkit_gateway linkkitapp lwm2m_app modbus_app mqttapp otaapp sal_app udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udataapp ulocation.baseapp ulog_app websoc_app wifi_at_app yloop_app yts"
