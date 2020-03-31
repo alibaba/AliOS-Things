20dce16467d5efb07279c122b9ce115f3730373c
licheng <lc122798@alibaba-inc.com>
2020-03-31 12:20:24 +0800

    BugID:25679598:remove non-longer used scons build config file
    Change-Id: I5a3650a343b817eb47de231c0fbd39285d63585c
    (cherry picked from commit 182ab14884255a3c2d69bafb567140d7a23ad47b)

--- platform/board/board_legacy/dh5021a_evb/ucube.py
+++ platform/board/board_legacy/dh5021a_evb/ucube.py
@@ -1,15 +1,1 @@
-src = ['board_init.c', 'net/ethernetif.c']
-
-component = aos_board_component('board_hobbit1_2', 'csky', src)
-
-component.add_global_includes('include')
-component.add_global_macros('STDIO_UART=0')
-component.add_global_macros('MBEDTLS_AES_ROM_TABLES=1')
-
-# component.set('MODULE', 'HOBBIT1_2')
-# component.set('HOST_CHIP', 'hobbit1_2')
-component.set_global_arch('ck802')
-component.add_global_cflags('-std=gnu99')
-
-aos_global_config.add_ld_files('gcc_csky.ld')
 linux_only_targets="coapapp helloworld http2app httpapp httpclient_app httpdns_app linkkit_gateway linkkitapp modbus_app mqttapp otaapp udata_demo.sensor_cloud_demo udata_demo.sensor_local_demo udataapp ulocation.baseapp wifi_at_app yloop_app yts"
--- platform/mcu/stm32f4xx/spi_flash/ucube.py
+++
@@ -1,6 +0,0 @@
-src = Split('''
-        spi_flash.c
-        spi_flash_platform.c 
-''')
-
-component = aos_component('Lib_SPI_Flash_Library', src)
