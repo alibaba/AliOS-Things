3c39b6ce12bf9e505dcf27c4373f037dfdca03be
licheng <lc122798@alibaba-inc.com>
2020-04-02 21:54:18 +0800

    BugID:25679598:disable building lorawan.linkwan app for asr6501
    Change-Id: Idce75b9a111efac3abf8f568b25a120951daa46a

--- platform/board/board_legacy/asr6501/ucube.py
+++ platform/board/board_legacy/asr6501/ucube.py
@@ -1,1 +1,1 @@
-linux_only_targets="helloworld lorawan.linkwan modbus_app udata_demo.sensor_local_demo ulog_app wifi_at_app"
+linux_only_targets="helloworld modbus_app udata_demo.sensor_local_demo ulog_app wifi_at_app"
