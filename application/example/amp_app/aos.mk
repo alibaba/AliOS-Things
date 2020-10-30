760c7d984b7dfe4d6bd3ffe821fcd6e90ed6a4b9
robin95021023 <471699212@qq.com>
2020-10-30 11:50:05 +0800

    rename amp_app, update libamp and haas100 uart driver (#1226)
    Co-authored-by: xinweng.ljh <xinweng.ljh@alibaba-inc.com>

--- application/example/amp_app/aos.mk
+++ application/example/amp_app/aos.mk
@@ -1,9 +1,9 @@
-NAME := ampapp
+NAME := amp_app
 
 $(NAME)_MBINS_TYPE := app
 $(NAME)_VERSION := 1.0.1
-$(NAME)_SUMMARY := amp demo
-$(NAME)_SOURCES := maintask.c ampdemo.c
+$(NAME)_SUMMARY := amp app
+$(NAME)_SOURCES := maintask.c amp_app.c
 
 $(NAME)_COMPONENTS += osal_aos kv littlefs amp cli
 
--- components/amp/aos.mk
+++ components/amp/aos.mk
@@ -4,8 +4,6 @@ NAME := amp
 $(NAME)_MBINS_TYPE := kernel
 $(NAME)_VERSION := 1.0.2
 $(NAME)_SUMMARY := amp component
-# source files and the folder of internal include files
-$(NAME)_INCLUDES += . include ./hal
 
-$(NAME)_PREBUILT_LIBRARY := ./libamp.a
+$(NAME)_PREBUILT_LIBRARY := libamp.a
 
