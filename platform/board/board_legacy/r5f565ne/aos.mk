39beb9cf131ac829104db881721db6a5ee239e01
caveguy <dongren.zt@alibaba-inc.com>
2021-02-25 15:22:38 +0800

    Dev tt 3.1 (#1284)
    * [linkkit]fix compile err while save ticket enabled
    
    * [platform]Delete boards and mcu that cannot be compiled
    
    * [components]Delete components that cannot be compiled

--- platform/board/board_legacy/r5f565ne/aos.mk
+++
@@ -1,6 +0,0 @@
-NAME := board_r5f565ne
-
-$(NAME)_VERSION := 1.0.1
-$(NAME)_SUMMARY := configuration for board r5f565ne
-
-AOS_BUILD_COMPATIBLE := no
--- platform/mcu/freedom-e/freedom-e-sdk/bsp/env/freedom-e300-arty/settings.mk
+++
@@ -1,3 +0,0 @@
-# Describes the CPU on this board to the rest of the SDK.
-RISCV_ARCH := rv32imac
-RISCV_ABI  := ilp32
