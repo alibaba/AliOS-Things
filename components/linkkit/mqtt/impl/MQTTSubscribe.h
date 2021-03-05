a5294825216ec2f53c7d0b1224d5f1de51af480e
Jinliang Li <jinliang.li@linux.alibaba.com>
2021-03-05 20:03:52 +0800

    Modify some comments
    Signed-off-by: Jinliang Li <jinliang.li@linux.alibaba.com>

--- components/linkkit/mqtt/impl/MQTTSubscribe.h
+++ components/linkkit/mqtt/impl/MQTTSubscribe.h
@@ -1,6 +1,19 @@
-/*
- * Copyright (C) 2015-2018 Alibaba Group Holding Limited
- */
+/*******************************************************************************
+ * Copyright (c) 2014 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    Ian Craggs - initial API and implementation and/or initial documentation
+ *    Xiang Rong - 442039 Add makefile to Embedded C client
+ *******************************************************************************/
 
 #ifndef MQTTSUBSCRIBE_H_
 #define MQTTSUBSCRIBE_H_
