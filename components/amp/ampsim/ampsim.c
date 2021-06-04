48d0d81f933fcb423d44300bb77c593d43381048
caiyan.cai <caiyan.cai@alibaba-inc.com>
2021-06-04 11:17:17 +0800

    delete amp_demo and components/amp
    Signed-off-by: caiyan.cai <caiyan.cai@alibaba-inc.com>

--- components/amp/ampsim/ampsim.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-extern int amp_main(void);
-int main(int argc, char **argv)
-{
-    amp_main();
-    return 0;
-}
--- components/amp/example-js/system.js
+++
@@ -1,5 +0,0 @@
-console.log('system version is: ' + system.version())
-console.log('system platform is: ' + system.platform())
-console.log('system uptime is: ' + system.uptime())
-console.log('system heapTotal is: ' + system.memory().heapTotal)
-console.log('system heapUsed is: ' + system.memory().heapUsed)
