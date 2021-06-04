1383ee8eded1b29e2ed281c553257845fb323dff
caiyan.cai <caiyan.cai@alibaba-inc.com>
2021-06-04 11:17:17 +0800

    delete amp_demo and components/amp
    Signed-off-by: caiyan.cai <caiyan.cai@alibaba-inc.com>

--- components/amp/components/smartcard/alismartcard/scripts/code_fmt.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-for i in $(find . -name "*.[ch]"); do
-    astyle --style=linux -Sw -fpXHU -jk3 -n -z2 $i
-done
--- components/amp/utils/libqrencode/tests/rsecc_decoder.h
+++
@@ -1,7 +0,0 @@
-#ifndef RSECC_DECODER_H
-#define RSECC_DECODER_H
-
-void RSECC_decoder_init();
-int RSECC_decoder_checkSyndrome(int dl, unsigned char *data, int el, unsigned char *ecc);
-
-#endif /* RSECC_DECODER_H */
