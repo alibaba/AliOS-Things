94454f488731fc86c712d493b5e44cfbffa81f40
caiyan.cai <caiyan.cai@alibaba-inc.com>
2021-08-12 14:25:54 +0800

    Remove haaseduk1 ’s drivers from eduk1_demo
    Signed-off-by: caiyan.cai <caiyan.cai@alibaba-inc.com>

--- components/drivers/external_device/sh1106/include/sh1106_hdconfig.h
+++ components/drivers/external_device/sh1106/include/sh1106_hdconfig.h
@@ -3,21 +3,21 @@
  */
 #ifndef _SH1106_HDCONFIG_H
 #define _SH1106_HDCONFIG_H
-
-#define USE_SOFT_SPI 1
-
-#if  USE_SOFT_SPI == 1
+#if USE_SOFT_SPI == 1
+#ifdef BOARD_HAASEDUK1
+#define OLED_CLK_PIN    HAL_IOMUX_PIN_P3_7
+#define OLED_SDA_PIN    HAL_IOMUX_PIN_P3_5
+#define OLED_RES_PIN    HAL_IOMUX_PIN_P3_6
+#define OLED_DC_PIN     HAL_IOMUX_PIN_P3_4
+#else
 #define OLED_CLK_PIN    HAL_IOMUX_PIN_P0_5
 #define OLED_SDA_PIN    HAL_IOMUX_PIN_P4_0
 #define OLED_RES_PIN    HAL_IOMUX_PIN_P4_1
 #define OLED_DC_PIN     HAL_IOMUX_PIN_P2_7
+#endif
 #else
 #define HARD_SPI_PORT 1
 #define SPI1_DC_PIN     HAL_IOMUX_PIN_P3_4
 #define SPI1_RESET_PIN  HAL_IOMUX_PIN_P3_6
 #endif
-
-#define RECORD_SCREEN   0
-#define FB_FRAME_EN     0
-
 #endif
