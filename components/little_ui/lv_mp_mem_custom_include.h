113cfd4e2e1aece72ad8d5e8451512c77c1accf6
caiyan.cai <caiyan.cai@alibaba-inc.com>
2022-02-24 10:05:39 +0800

    delete components/little_ui
    Signed-off-by: caiyan.cai <caiyan.cai@alibaba-inc.com>

--- components/little_ui/lv_mp_mem_custom_include.h
+++
@@ -1,8 +0,0 @@
-#ifndef __LV_MP_MEM_CUSTOM_INCLUDE_H
-#define __LV_MP_MEM_CUSTOM_INCLUDE_H
-
-#include <py/mpconfig.h>
-#include <py/misc.h>
-#include <py/gc.h>
-
-#endif //__LV_MP_MEM_CUSTOM_INCLUDE_H
