c23e26ee4f852bb06890b40eec8764e9ea8ade5c
yanxiaoyong.yxy <yanxiaoyong.yxy@alibaba-inc.com>
2022-04-24 14:13:59 +0800

    BugID:40799085: add python kv.seti and kv.geti api.
    [Detail]
    none
    
    [Verified Cases]
    Build Pass: <none>
    Test Pass: <none>

--- components/py_engine/modules/kv/modkv.c
+++ components/py_engine/modules/kv/modkv.c
@@ -81,6 +81,58 @@ STATIC mp_obj_t obj_getStorageSync(size_t n_args, const mp_obj_t *args)
 }
 STATIC MP_DEFINE_CONST_FUN_OBJ_VAR(kv_obj_getStorageSync, 1, obj_getStorageSync);
 
+STATIC mp_obj_t obj_setiStorageSync(size_t n_args, const mp_obj_t *args)
+{
+    LOGD(LOG_TAG, "entern  %s; n_args = %d;\n", __func__, n_args);
+    int ret = -1;
+    char *key;
+    int value;
+    int32_t value_len = 0;
+
+    if (n_args < 2) {
+        LOGE(LOG_TAG, "%s: args num is illegal :n_args = %d;\n", __func__, n_args);
+        return MP_ROM_INT(-EINVAL);
+    }
+
+    key = (char *)mp_obj_str_get_str(args[0]);
+    value = (int)mp_obj_get_int(args[1]);
+    value_len = sizeof(value);
+
+    ret = aos_kv_set(key, &value, value_len, 1);
+    if (ret != 0) {
+        LOGE(LOG_TAG, "%s:aos_kv_set failed\n", __func__);
+    }
+    LOGD(LOG_TAG, "%s:out\n", __func__);
+
+    return MP_ROM_INT(ret);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR(kv_obj_setiStorageSync, 2, obj_setiStorageSync);
+
+STATIC mp_obj_t obj_getiStorageSync(size_t n_args, const mp_obj_t *args)
+{
+    LOGD(LOG_TAG, "entern  %s; n_args = %d;\n", __func__, n_args);
+    int ret = -1;
+    int32_t value_len = KV_BUFFER_MAX_LEN;
+    char *key;
+    int value;
+
+    if (n_args < 1) {
+        LOGE(LOG_TAG, "%s: args num is illegal :n_args = %d;\n", __func__, n_args);
+        return MP_ROM_INT(-EINVAL);
+    }
+
+    key = (char *)mp_obj_str_get_str(args[0]);
+    ret = aos_kv_get(key, &value, &value_len);
+    if (ret != 0) {
+        LOGE(LOG_TAG, "aos_kv_get failed key = %s;\n", key);
+        return MP_ROM_INT(ret);
+    }
+
+    return mp_obj_new_int(value);
+}
+STATIC MP_DEFINE_CONST_FUN_OBJ_VAR(kv_obj_getiStorageSync, 1, obj_getiStorageSync);
+
+
 STATIC mp_obj_t obj_removeStorageSync(size_t n_args, const mp_obj_t *args)
 {
     LOGD(LOG_TAG, "entern  %s; n_args = %d;\n", __func__, n_args);
@@ -110,6 +162,8 @@ STATIC const mp_rom_map_elem_t kv_module_globals_table[] = {
     { MP_OBJ_NEW_QSTR(MP_QSTR___name__), MP_ROM_QSTR(MP_QSTR_kv) },
     { MP_ROM_QSTR(MP_QSTR_set), MP_ROM_PTR(&kv_obj_setStorageSync) },
     { MP_ROM_QSTR(MP_QSTR_get), MP_ROM_PTR(&kv_obj_getStorageSync) },
+    { MP_ROM_QSTR(MP_QSTR_seti), MP_ROM_PTR(&kv_obj_setiStorageSync) },
+    { MP_ROM_QSTR(MP_QSTR_geti), MP_ROM_PTR(&kv_obj_getiStorageSync) },
     { MP_ROM_QSTR(MP_QSTR_remove), MP_ROM_PTR(&kv_obj_removeStorageSync) },
 };
 
