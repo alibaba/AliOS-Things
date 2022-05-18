e3c3560784eb4ec1b20887dc74c884c0840f05e7
guoliang.wgl <guoliang.wgl@alibaba-inc.com>
2022-05-18 21:14:18 +0800

    fix build err

--- components/amp_adapter/platform/esp_idf/peripheral/aos_hal_system.c
+++ components/amp_adapter/platform/esp_idf/peripheral/aos_hal_system.c
@@ -4,3 +4,15 @@ void aos_hal_reboot(void)
 {
     esp_restart();
 }
+
+char g_chip_id[32];
+const char *aos_get_device_name(void)
+{
+    uint8_t mac[32] = {0};
+    extern esp_err_t esp_read_mac(uint8_t *mac, esp_mac_type_t type);
+    esp_read_mac(mac, ESP_MAC_WIFI_STA);
+    memset(g_chip_id, 0, 32);
+    snprintf(g_chip_id, 32, "haas_%02x%02x%02x%02x%02x%02x", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    return g_chip_id;
+}
+
