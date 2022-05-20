12edc80455ba7f19031793b5c395a138a28ce5b6
wusheng.tws <wusheng.tws@alibaba-inc.com>
2022-05-20 12:03:24 +0800

    create driver and api for the fheartbeat sensor #1660

---
+++ haas_lib_bundles/python/libraries/fheartbeat/fheartbeat.py
@@ -0,0 +1,27 @@
+"""
+Copyright (C) 2015-2021 Alibaba Group Holding Limited
+
+    HaaS Python driver for fheartbeat
+
+    Author: HaaS
+    Date:   2022/05/15
+"""
+
+from driver import ADC
+
+class FHEARTBEAT(object):
+    """
+    This class implements FHEARTBEAT chip's defs.
+    """
+
+    def __init__(self, adcObj):
+        self._adcObj = None
+        if not isinstance(adcObj, ADC):
+            raise ValueError("parameter is not an adcObj object")
+        self._adcObj = adcObj
+
+    def getVoltage(self):
+        if self._adcObj is None:
+            raise ValueError("invalid ADC object")
+        value = self._adcObj.readVoltage()
+        return value
