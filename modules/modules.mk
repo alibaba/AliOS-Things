bf8cb3cb12d40f654398cf5ea40a5c7726bd187b
yong.jy <yong.jy@alibaba-inc.com>
2022-06-08 21:10:19 +0800

    Update source code for external and modules
    Change-Id: I80bac554c6ffea7a49e464a73a149cdb0e8cd61b

--- modules/modules.mk
+++ modules/modules.mk
@@ -13,13 +13,13 @@ ifeq ($(MICROPY_PY_ALIYUNIOT), y)
 
     CFLAGS_HAAS += -DMICROPY_PY_ALIYUNIOT=1 -Wno-discarded-qualifiers -Wno-unused-function -Wno-incompatible-pointer-types -Wno-pointer-sign -Wno-implicit-function-declaration
     SRCDIRS_HAAS += $(MODULES_DIR)/aliyunIoT
-    INCLUDEDIRS_HAAS += $(MODULES_DIR)/aliyunIoT
+    INC_HAAS += $(MODULES_DIR)/aliyunIoT
 endif
 
 ifeq ($(MICROPY_PY_BLE), y)
     CFLAGS_HAAS += -DMICROPY_PY_BLE=1 -DCONFIG_BT=1
     SRCDIRS_HAAS += $(wildcard $(MODULES_DIR)/ble/*.c)
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
         $(MODULES_DIR)/ble \
 		$(AOS_DIR)/ble_host/bt_host/include \
     	$(AOS_DIR)/ble_host/include \
@@ -29,41 +29,41 @@ endif
 ifeq ($(MICROPY_PY_HTTP), y)
     CFLAGS_HAAS += -DMICROPY_PY_HTTP=1
     SRCDIRS_HAAS += network/http/esp32/modhttp.c
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
         $(MICROPY_MODULES_PORT_DIR)/network/http
 endif
 
 ifeq ($(MICROPY_PY_MODBUS), y)
     CFLAGS_HAAS += -DMICROPY_PY_MODBUS=1
     SRCDIRS_HAAS += modbus/modbus.c
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
         $(MICROPY_MODULES_PORT_DIR)/modbus/include
 endif
 
 ifeq ($(MICROPY_PY_KV), y)
     CFLAGS_HAAS += -DMICROPY_PY_KV=1
     SRCDIRS_HAAS += kv/modkv.c
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
         $(MICROPY_MODULES_PORT_DIR)/kv/include
 endif
 
 ifeq ($(MICROPY_PY_OTA), y)
     CFLAGS_HAAS += -DMICROPY_PY_OTA=1
     SRCDIRS_HAAS += oat/modota.c
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
         $(MICROPY_MODULES_PORT_DIR)/ota/include
 endif
 
 ifeq ($(MICROPY_PY_OSS), y)
     CFLAGS_HAAS += -DMICROPY_PY_OSS=1
     SRCDIRS_HAAS += oss/modoss.c
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
 		$(MICROPY_MODULES_PORT_DIR)/oss/src \
 		$(MICROPY_MODULES_PORT_DIR)/oss/include
 else ifeq ($(MICROPY_PY_OSS_HTTP), y)
     CFLAGS_HAAS += -DMICROPY_PY_OSS_HTTP=1
     SRCDIRS_HAAS += $(patsubst oss/*.c)
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
 		oss/byhttp \
 		$(MICROPY_MODULES_PORT_DIR)/http/include
 endif
@@ -71,7 +71,7 @@ endif
 ifeq ($(MICROPY_PY_UCAMERA), y)
     CFLAGS_HAAS += -DMICROPY_PY_UCAMERA=1
     SRCDIRS_HAAS += ucamera/moducamera.c
-    INCLUDEDIRS_HAAS += \
+    INC_HAAS += \
 		$(AOS_DIR)/ucamera/include \
 		$(AOS_DIR)/ucamera/include/device/wifi \
 		$(AOS_DIR)/ucamera/include/device/uart
@@ -80,7 +80,7 @@ endif
 ifeq ($(MICROPY_PY_DRIVER), y)
 	CFLAGS_HAAS += -DMICROPY_PY_DRIVER=1
 	SRCDIRS_HAAS += $(wildcard $(MODULES_DIR)/driver/*.c)
-	INCLUDEDIRS_HAAS += \
+	INC_HAAS += \
 		$(AOS_DIR)/cjson/include
 endif
 
@@ -92,7 +92,7 @@ ifeq ($(MICROPY_PY_SNTP), y)
 	CFLAGS_HAAS += -DMICROPY_PY_SNTP=1
 	ifeq ($(HAAS_PLATFORM), y)
 		SRCDIRS_HAAS += sntp/aos/modsntp.c
-		INCLUDEDIRS_HAAS += $(EXTERNAL_DIR)/sntp/include
+		INC_HAAS += $(EXTERNAL_DIR)/sntp/include
 	else ifeq ($(ESP_PLATFORM), y)
 		SRCDIRS_HAAS += sntp/esp32/modsntp.c
 	endif
@@ -101,7 +101,7 @@ endif
 ifeq ($(MICROPY_PY_DISPLAY), y)
 	CFLAGS_HAAS += -DMICROPY_PY_DISPLAY=1 -DLV_USE_SJPG=1
 	SRCDIRS_HAAS += $(wildcard $(MODULES_DIR)/display/*.c)
-	INCLUDEDIRS_HAAS += \
+	INC_HAAS += \
 		display \
     	display/esp32 \
     	$(AOS_DIR)/little_ui \
@@ -110,7 +110,7 @@ ifeq ($(MICROPY_PY_DISPLAY), y)
 
 	ifeq ($(MICROPY_PY_DISPLAY_LVGL), y)
 		CFLAGS_HAAS += -DMICROPY_PY_DISPLAY_LVGL=1
-		INCLUDEDIRS_HAAS += \
+		INC_HAAS += \
 			$(AOS_DIR)/little_ui/driver/axp192 \
 			$(AOS_DIR)/little_ui/driver/lvgl_esp32_drivers
     endif
@@ -132,7 +132,7 @@ endif
 ifeq ($(MICROPY_PY_NETMGR), y)
 	CFLAGS_HAAS += -DMICROPY_PY_NETMGR=1
 	SRCDIRS_HAAS += netmgr/modnetmgr.c
-	INCLUDEDIRS_HAAS += \
+	INC_HAAS += \
         $(AOS_DIR)/uservice/include \
         $(AOS_DIR)/netmgr/include
 endif
@@ -140,7 +140,7 @@ endif
 ifeq ($(MICROPY_PY_ULOG), y)
 	CFLAGS_HAAS += -DMICROPY_PY_ULOG=1
 	SRCDIRS_HAAS += ulog/modulog.c
-	INCLUDEDIRS_HAAS += \
+	INC_HAAS += \
 		$(AOS_DIR)/ulog/include
 endif
 
@@ -149,5 +149,5 @@ SRC_HAAS += $(foreach dir, $(SRCDIRS_HAAS), $(wildcard $(dir)/*.c))
 # $(info 'SRC_HAAS = $(SRC_HAAS)')
 
 
-INC += $(addprefix -I, $(INCLUDEDIRS_HAAS))
+INC += $(addprefix -I, $(INC_HAAS))
 # $(info 'INC = $(INC)')
