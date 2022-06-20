085f6ebcdcceb46c4f03f01d40832a9662e9e760
Jinliang Li <ljl150821@alibaba-inc.com>
2022-06-20 10:36:17 +0800

    oss: add oss python module
    [Detail]
    - Add oss python module
    - Remove oss c module

--- ports/stm32/boards/COLUMBUS/manifest.py
+++ ports/stm32/boards/COLUMBUS/manifest.py
@@ -4,6 +4,8 @@ freeze("$(MPY_DIR)/drivers/dht", "dht.py")
 freeze("$(MPY_DIR)/drivers/onewire", "onewire.py")
 freeze("$(MPY_DIR)/ports/stm32/modules", "neopixel.py")
 
+# upip
+freeze("$(MPY_DIR)/tools", ("upip.py", "upip_utarfile.py"))
 
 # boardparser
 freeze("$(MPY_DIR)/modules/boardparser", "boardparser.py")
@@ -13,3 +15,16 @@ include("$(MPY_DIR)/modules/driver/manifest.py")
 
 # modbus
 include("$(MPY_DIR)/modules/modbus/manifest.py")
+
+# ulinksdk
+include("$(MPY_DIR)/modules/ulinksdk/manifest.py")
+
+# http
+freeze("$(MPY_DIR)/modules/http", "http.py")
+
+# oss
+include("$(MPY_DIR)/modules/oss/manifest.py")
+
+# ukv
+freeze("$(MPY_DIR)/modules/ukv", ("kv.py", "systemAdaptor.py"))
+
